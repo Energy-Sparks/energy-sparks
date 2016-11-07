effa6ad95817b44ff7d50c26d088f1411c40e08d
Matt Helsby <matt@citeab.com>
2016-11-07 20:24:29 +0000

    First set of merit rules

---
+++ app/models/merit/badge_rules.rb
@@ -0,0 +1,38 @@
+# Be sure to restart your server when you modify this file.
+#
+# +grant_on+ accepts:
+# * Nothing (always grants)
+# * A block which evaluates to boolean (recieves the object as parameter)
+# * A block with a hash composed of methods to run on the target object with
+#   expected values (+votes: 5+ for instance).
+#
+# +grant_on+ can have a +:to+ method name, which called over the target object
+# should retrieve the object to badge (could be +:user+, +:self+, +:follower+,
+# etc). If it's not defined merit will apply the badge to the user who
+# triggered the action (:action_user by default). If it's :itself, it badges
+# the created object (new user for instance).
+#
+# The :temporary option indicates that if the condition doesn't hold but the
+# badge is granted, then it's removed. It's false by default (badges are kept
+# forever).
+
+module Merit
+  class BadgeRules
+    include Merit::BadgeRulesMethods
+
+    def initialize
+      # Sign up to EnergySparks
+      grant_on 'schools#create', badge: 'registered', to: :itself
+
+      # Record 10 activities
+      grant_on 'activities#create', badge: 'ten-activities', multiple: true, to: :school do |activity|
+        activity.school.activities.count.remainder(10).zero?
+      end
+
+      # Sign up to eco-schools programme i.e. eco_school_status is not null
+      grant_on ['schools#create', 'schools#update'], badge: 'eco-programme' do |school|
+        school.eco_school_status.present? && !school.eco_school_status.zero?
+      end
+    end
+  end
+end
---
+++ app/models/merit/point_rules.rb
@@ -0,0 +1,24 @@
+# Be sure to restart your server when you modify this file.
+#
+# Points are a simple integer value which are given to "meritable" resources
+# according to rules in +app/models/merit/point_rules.rb+. They are given on
+# actions-triggered, either to the action user or to the method (or array of
+# methods) defined in the +:to+ option.
+#
+# 'score' method may accept a block which evaluates to boolean
+# (recieves the object as parameter)
+
+module Merit
+  class PointRules
+    include Merit::PointRulesMethods
+
+    def initialize
+
+      # Award points to new schools and for activities
+      score 10, on: [
+        'schools#create',
+        'activities#create'
+      ]
+    end
+  end
+end
