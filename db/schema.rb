e76ce4b2a6deaf4c54e56e73b5496d8491770a4e
ldodds <leigh@ldodds.com>
2016-12-11 15:54:26 +0000

    Allow meters to be named. Closes #114

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210154449) do
+ActiveRecord::Schema.define(version: 20161211154744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20161210154449) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.boolean  "active",     default: true
+    t.string   "name"
     t.index ["meter_no"], name: "index_meters_on_meter_no", using: :btree
     t.index ["meter_type"], name: "index_meters_on_meter_type", using: :btree
     t.index ["school_id"], name: "index_meters_on_school_id", using: :btree
