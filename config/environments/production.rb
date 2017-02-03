2a10d817f07d5cf7f21969db14ae38625350b829
ldodds <leigh@ldodds.com>
2017-02-03 15:02:04 +0000

    Enable dependency loading #180

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.enable_dependency_loading = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
