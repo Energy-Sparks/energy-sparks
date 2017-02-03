d72fcde27078261aa37520f019778383e963f91d
ldodds <leigh@ldodds.com>
2017-02-03 14:48:50 +0000

    Don't symlink bin directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
