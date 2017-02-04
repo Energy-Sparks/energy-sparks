bc96c05ca923f70e18bf5523bbc756e4cbef0f35
ldodds <leigh@ldodds.com>
2017-02-04 16:10:42 +0000

    Integrate mailgun for sending emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+      api_key: ENV['MG_API_KEY'],
+      domain: ENV['MG_DOMAIN']
+  }
 end
