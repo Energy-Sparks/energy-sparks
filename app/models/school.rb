45721854991e71e333625320718ee3aa2622e7b1
ldodds <leigh@ldodds.com>
2017-02-05 14:37:33 +0000

    Tidy up score board, show schools with increasing scores

--- app/models/school.rb
+++ app/models/school.rb
@@ -99,6 +99,10 @@ class School < ApplicationRecord
       .map(&:badge)
   end
 
+  def points_since(since = 1.month.ago)
+    self.score_points.where("created_at > '#{since}'").sum(:num_points)
+  end
+
   def self.top_scored(dates: nil, limit: nil)
     if dates.present?
       start_date = dates.first.beginning_of_day
