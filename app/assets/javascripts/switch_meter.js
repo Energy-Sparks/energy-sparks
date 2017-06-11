1eea71d0e115b95ef96b56fc973321f129d09ca1
ldodds <leigh@ldodds.com>
2017-06-11 15:17:31 +0100

    Move event handlers outside of turbolinks:load

--- app/assets/javascripts/switch_meter.js
+++ app/assets/javascripts/switch_meter.js
@@ -20,23 +20,24 @@ $(document).on("turbolinks:load", function() {
             orientation: 'bottom'
     });
 
-    function updateChart(el) {
-        console.log(el.id);
-        chart_id = el.form.id.replace("-filter", "");
-        chart = Chartkick.charts[chart_id];
-        current_source = chart.getDataSource();
-        new_source = current_source.split("?")[0] + "?" + $(el.form).serialize();
-        chart.updateData(new_source);
-        chart.getChartObject().showLoading();
-    }
-    $(document).on('change', '.meter-filter', function() {
-        updateChart(this);
-    });
-    $(document).on('change', '#first_date_picker', function() {
-        updateChart(this);
-    });
-    $(document).on('change', '#to_date_picker', function() {
-        updateChart(this);
-    });
-
 });
+
+function updateChart(el) {
+    console.log(el.id);
+    chart_id = el.form.id.replace("-filter", "");
+    chart = Chartkick.charts[chart_id];
+    current_source = chart.getDataSource();
+    new_source = current_source.split("?")[0] + "?" + $(el.form).serialize();
+    chart.updateData(new_source);
+    chart.getChartObject().showLoading();
+}
+$(document).on('change', '.meter-filter', function() {
+    updateChart(this);
+});
+$(document).on('change', '#first_date_picker', function() {
+    updateChart(this);
+});
+$(document).on('change', '#to_date_picker', function() {
+    updateChart(this);
+});
+
