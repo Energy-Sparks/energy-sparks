9a486772f831abf3e772cd2148f47c337e2ba7d6
ldodds <leigh@ldodds.com>
2017-08-23 13:38:30 +0100

    Reorganise activity category page

--- app/controllers/activity_categories_controller.rb
+++ app/controllers/activity_categories_controller.rb
@@ -74,6 +74,6 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def activity_category_params
-    params.require(:activity_category).permit(:name)
+    params.require(:activity_category).permit(:name, :description)
   end
 end
--- app/models/activity_category.rb
+++ app/models/activity_category.rb
@@ -2,10 +2,11 @@
 #
 # Table name: activity_categories
 #
-#  created_at :datetime         not null
-#  id         :integer          not null, primary key
-#  name       :string
-#  updated_at :datetime         not null
+#  created_at  :datetime         not null
+#  description :string
+#  id          :integer          not null, primary key
+#  name        :string
+#  updated_at  :datetime         not null
 #
 
 class ActivityCategory < ApplicationRecord
@@ -13,8 +14,8 @@ class ActivityCategory < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  def sorted_activity_types
-    types = activity_types.order(:name).to_a
+  def sorted_activity_types(by: :name)
+    types = activity_types.order(by).to_a
     other = types.index { |x| x.name.casecmp("other") == 0 }
     types.insert(-1, types.delete_at(other)) if other.present?
     types
