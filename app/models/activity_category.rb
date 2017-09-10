5e881ad7249f639ee60c6de10d36cb250de3ff7c
ldodds <leigh@ldodds.com>
2017-09-10 15:17:38 +0100

    Don't show inactive activity types, or allow them to be recorded

--- app/models/activity_category.rb
+++ app/models/activity_category.rb
@@ -17,7 +17,7 @@ class ActivityCategory < ApplicationRecord
   validates_uniqueness_of :badge_name, allow_blank: true, allow_nil: true
 
   def sorted_activity_types(by: :name)
-    types = activity_types.order(by).to_a
+    types = activity_types.where(active: true).order(by).to_a
     other = types.index { |x| x.name.casecmp("other") == 0 }
     types.insert(-1, types.delete_at(other)) if other.present?
     types
