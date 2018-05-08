9055349d55c1a61c3cf29b8f0063c75dc5447bb7
James Jefferies <james@shedcode.co.uk>
2018-05-08 21:45:32 +0100

    Get these tests working

--- spec/factories/activity_types.rb
+++ spec/factories/activity_types.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :activity_type do
     activity_category
-    name 'test activity_type name'
+    sequence(:name) {|n| "test activity_type name #{n}"}
     score 25
     description 'test_activity_type description'
   end
--- spec/system/activity_category_spec.rb
+++ spec/system/activity_category_spec.rb
@@ -1,64 +1,33 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "activity type", type: :system, js: true do
-#   let!(:admin)  { create(:user, role: 'admin')}
-#   let!(:activity_category_1) { create(:activity_category, name: 'cat1')}
-#   let!(:activity_type) { create(:activity_type, activity_category: activity_category_1)}
+RSpec.describe "activity type", type: :system, js: true do
+  let!(:admin)  { create(:user, role: 'admin')}
 
-#   let!(:activity_category_2) { create(:activity_category, name: 'cat2')}
-#   let!(:ks1_tag) { ActsAsTaggableOn::Tag.create(name: 'KS1') }
-#   let!(:ks2_tag) { ActsAsTaggableOn::Tag.create(name: 'KS2') }
-#   let!(:ks3_tag) { ActsAsTaggableOn::Tag.create(name: 'KS3') }
+  let!(:ks1_tag) { ActsAsTaggableOn::Tag.create(name: 'KS1') }
+  let!(:ks2_tag) { ActsAsTaggableOn::Tag.create(name: 'KS2') }
+  let!(:ks3_tag) { ActsAsTaggableOn::Tag.create(name: 'KS3') }
 
-#   it 'can not access it unless logged in' do
-#     visit new_activity_type_path
-#     expect(page.has_content?("Sign in")).to be true
-#   end
+  let!(:activity_category_1) { create(:activity_category, name: 'cat1')}
+  let!(:activity_type_1) { create(:activity_type, activity_category: activity_category_1, key_stages: [ks1_tag, ks2_tag])}
 
-#   describe 'when logged in' do
-#     before(:each) do
-#       sign_in(admin)
-#       visit new_activity_type_path
-#     end
+  let!(:activity_category_2) { create(:activity_category, name: 'cat2')}
+  let!(:activity_type_2) { create(:activity_type, activity_category: activity_category_2, key_stages: [ks3_tag])}
 
-#     it 'can add a new activity for KS1' do
-#       fill_in('Name', with: 'New activity')
-#       first('input#activity_type_description', visible: false).set("the description")
-#       fill_in('Score', with: 20)
-#       fill_in('Badge name', with: 'Badddddger')
+  describe 'activities can be filtered by tag' do
+    before(:each) do
+      visit activity_categories_path
+    end
 
-#       check('KS1')
+    it 'by defaults shows cat 1 activity types' do
+      expect(page.has_content?(activity_type_1.name)).to be true
+      expect(page.has_content?(activity_type_2.name)).to_not be true
+    end
 
-#       click_on('Create Activity type')
-
-#       activity_type = ActivityType.find_by(name: 'New activity')
-
-#       expect(activity_type.key_stage_list).to     include('KS1')
-#       expect(activity_type.key_stage_list).to_not include('KS2')
-#       expect(activity_type.key_stage_list).to_not include('KS3')
-
-#       expect(page.has_content?("Activity type was successfully created.")).to be true
-#       expect(ActivityType.count).to be 1
-#     end
-
-#     it 'can edit a new activity and add KS2 and KS3' do
-#       activity_type = create(:activity_type, activity_category: activity_category)
-#       visit edit_activity_type_path(activity_type)
-#       expect(page.has_content?(activity_type.name)).to be true
-
-#       check('KS2')
-#       check('KS3')
-
-#       click_on('Update Activity type')
-
-#       activity_type.reload
-
-#       expect(activity_type.key_stage_list).to_not include('KS1')
-#       expect(activity_type.key_stage_list).to     include('KS2')
-#       expect(activity_type.key_stage_list).to     include('KS3')
-
-#       expect(page.has_content?("Activity type was successfully updated.")).to be true
-#       expect(ActivityType.count).to be 1
-#     end
-#   end
-# end
+    it 'shows cat 2 activity types if selected' do
+      click_on('cat2')
+      assert_text(activity_type_2.name)
+      expect(page.has_content?(activity_type_1.name)).to_not be true
+      expect(page.has_content?(activity_type_2.name)).to be true
+    end 
+  end
+end
