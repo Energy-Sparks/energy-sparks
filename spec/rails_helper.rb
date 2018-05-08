e8605da91e8f4bcbaf63d5c76b41830059d641b4
James Jefferies <james@shedcode.co.uk>
2018-05-08 16:44:41 +0100

    Set up for running JS system tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,7 +10,7 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'pry'
 require 'capybara/rspec'
-require 'capybara-screenshot/rspec'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,9 +1,8 @@
 VCR.configure do |config|
-
   # When running javascript tests, ignore any calls to the
   # test server!
   config.ignore_request do |request|
-    URI(request.uri).port == 7777
+    URI(request.uri).port == 9516 || URI(request.uri).port == 7777
   end
 
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
