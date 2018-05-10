8b103c2bb057a1c9fd1e2079ce1f672c04f9b501
James Jefferies <james@shedcode.co.uk>
2018-05-10 12:07:26 +0100

    Add further tests for checkboxes

--- spec/system/activity_category_spec.rb
+++ spec/system/activity_category_spec.rb
@@ -7,12 +7,62 @@ RSpec.describe "activity type", type: :system do
   let!(:ks2_tag) { ActsAsTaggableOn::Tag.create(name: 'KS2') }
   let!(:ks3_tag) { ActsAsTaggableOn::Tag.create(name: 'KS3') }
 
+  let!(:unlikely_school) { create(:school, key_stages: [ks1_tag, ks3_tag])}
+  let!(:unlikely_school_user) { create(:user, school: unlikely_school)}
+
   let!(:activity_category_1) { create(:activity_category, name: 'cat1')}
   let!(:activity_type_1) { create(:activity_type, activity_category: activity_category_1, key_stages: [ks1_tag, ks2_tag])}
   let!(:activity_type_3) { create(:activity_type, activity_category: activity_category_1, key_stages: [ks3_tag])}
 
   let!(:activity_category_2) { create(:activity_category, name: 'cat2')}
   let!(:activity_type_2) { create(:activity_type, activity_category: activity_category_2, key_stages: [ks3_tag])}
+
+
+  describe 'filters are set depending on user and if logged in or not' do
+    before(:each) do
+      visit activity_categories_path
+    end
+
+    it 'defaults to all if not signed in' do
+      visit activity_categories_path
+      expect(page).to have_checked_field('KS1')
+      expect(page).to have_checked_field('KS2')
+      expect(page).to have_checked_field('KS3') # or have_unchecked_field
+      uncheck('KS2')
+      uncheck('KS3')
+      click_on('Filter Activity Types')
+      expect(page).to have_checked_field('KS1')
+      expect(page).to have_unchecked_field('KS2')
+      expect(page).to have_unchecked_field('KS3')
+    end
+
+    it 'defaults to all if signed in as a non-school user' do
+      sign_in(admin)
+      visit activity_categories_path
+      expect(page).to have_checked_field('KS1')
+      expect(page).to have_checked_field('KS2')
+      expect(page).to have_checked_field('KS3') # or have_unchecked_field
+      uncheck('KS2')
+      uncheck('KS3')
+      click_on('Filter Activity Types')
+      expect(page).to have_checked_field('KS1')
+      expect(page).to have_unchecked_field('KS2')
+      expect(page).to have_unchecked_field('KS3')
+    end
+
+    it 'defaults to all if signed in as a non-school user' do
+      sign_in(unlikely_school_user)
+      visit activity_categories_path
+      expect(page).to have_checked_field('KS1')
+      expect(page).to have_unchecked_field('KS2')
+      expect(page).to have_checked_field('KS3') # or have_unchecked_field
+      uncheck('KS3')
+      click_on('Filter Activity Types')
+      expect(page).to have_checked_field('KS1')
+      expect(page).to have_unchecked_field('KS2')
+      expect(page).to have_unchecked_field('KS3')
+    end
+  end
 
   describe 'activities can be filtered by tag' do
     before(:each) do
