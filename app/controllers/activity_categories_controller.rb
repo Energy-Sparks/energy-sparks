ec4e0307316ff5cfe6d7195b4a98123c6fe80e3c
James Jefferies <james@shedcode.co.uk>
2018-05-11 08:55:31 +0100

    Refactor default filters

--- app/controllers/activity_categories_controller.rb
+++ app/controllers/activity_categories_controller.rb
@@ -87,17 +87,21 @@ private
     key_stage_tag = activity_category_filter_params[:key_stage_tag]
 
     if key_stage_tag.nil?
-      if current_user.nil? || current_user.school.nil?
-        # TODO remove this hardcoding
-        %w(KS1 KS2 KS3)
-      else
-        # Set for the school defaults
-        current_user.school.key_stage_list
-      end
+      default_filters
     else
       filters = key_stage_tag[:key_stage_names]
       filters.delete("")
       filters
+    end
+  end
+
+  def default_filters
+    if current_user.nil? || current_user.school.nil?
+      # TODO remove this hardcoding
+      %w(KS1 KS2 KS3)
+    else
+      # Set for the school defaults
+      current_user.school.key_stage_list
     end
   end
 end
