7c8121f1d3f8610fe799fbd2fc0a69f638b05a53
James Jefferies <james@shedcode.co.uk>
2018-05-11 09:05:29 +0100

    Refactor suggestor to make it less complicated

--- app/services/next_activity_suggester.rb
+++ app/services/next_activity_suggester.rb
@@ -1,4 +1,6 @@
 class NextActivitySuggester
+  NUMBER_OF_SUGGESTIONS = 5
+
   def initialize(school, first_activity = false)
     @school = school
     @suggestions = []
@@ -7,20 +9,35 @@ class NextActivitySuggester
 
   def suggest
     if @first_activity
-      ActivityTypeSuggestion.initial.order(:id).each do |ats|
-        @suggestions << ats.suggested_type
-      end
+      get_initial_suggestions
     else
-      last_activity_type = @school.activities.order(:created_at).last.activity_type
-      last_activity_type.activity_type_suggestions.each do |ats|
-        @suggestions << ats.suggested_type unless @school.activities.exists?(activity_type: ats.suggested_type)
-      end
+      get_suggestions_based_on_last_activity
     end
+
     #ensure minimum of five suggestions
-    if @suggestions.length < 5
-      more = ActivityType.where(active: true, custom: false, data_driven: true, repeatable: true).sample(5 - @suggestions.length)
-      @suggestions = @suggestions + more
-    end
+    top_up_if_not_enough_suggestions if @suggestions.length < NUMBER_OF_SUGGESTIONS
+
     @suggestions
+  end
+
+private
+
+  def get_initial_suggestions
+    ActivityTypeSuggestion.initial.order(:id).each do |ats|
+      @suggestions << ats.suggested_type
+    end
+  end
+
+  def get_suggestions_based_on_last_activity
+    last_activity_type = @school.activities.order(:created_at).last.activity_type
+
+    last_activity_type.activity_type_suggestions.each do |ats|
+      @suggestions << ats.suggested_type unless @school.activities.exists?(activity_type: ats.suggested_type)
+    end
+  end
+
+  def top_up_if_not_enough_suggestions
+    more = ActivityType.where(active: true, custom: false, data_driven: true, repeatable: true).sample(5 - @suggestions.length)
+    @suggestions += more
   end
 end
