ff546e34386f53dedc4b29a7a6b372c37ac4c04b
James Jefferies <james@shedcode.co.uk>
2018-05-15 13:39:28 +0100

    Fix styles

--- lib/loader/calendars.rb
+++ lib/loader/calendars.rb
@@ -6,18 +6,15 @@ module Loader
     def self.load!(csv_file = 'etc/banes-default-calendar.csv')
       raise 'File not found' unless File.exist?(csv_file)
       england = Area.where(title: 'England and Wales').first_or_create
-      pp england
       area = Area.where(title: 'Bristol and North East Somerset (BANES)', parent_area: england).first_or_create
-      pp area
       calendar = Calendar.where(default: true, area: area, title: area.title, template: true).first_or_create
-      pp calendar.errors
 
-term1 = CalendarEventType.where(title: 'Term 1', description: 'Autumn Half Term 1', occupied: true, term_time: true).first_or_create
-term2 = CalendarEventType.where(title: 'Term 2', description: 'Autumn Half Term 2', occupied: true, term_time: true).first_or_create
-term3 = CalendarEventType.where(title: 'Term 3', description: 'Spring Half Term 1', occupied: true, term_time: true).first_or_create
-term4 = CalendarEventType.where(title: 'Term 4', description: 'Spring Half Term 2', occupied: true, term_time: true).first_or_create
-term5 = CalendarEventType.where(title: 'Term 5', description: 'Summer Half Term 1', occupied: true, term_time: true).first_or_create
-term6 = CalendarEventType.where(title: 'Term 6', description: 'Autumn Half Term 2', occupied: true, term_time: true).first_or_create
+      CalendarEventType.where(title: 'Term 1', description: 'Autumn Half Term 1', occupied: true, term_time: true).first_or_create
+      CalendarEventType.where(title: 'Term 2', description: 'Autumn Half Term 2', occupied: true, term_time: true).first_or_create
+      CalendarEventType.where(title: 'Term 3', description: 'Spring Half Term 1', occupied: true, term_time: true).first_or_create
+      CalendarEventType.where(title: 'Term 4', description: 'Spring Half Term 2', occupied: true, term_time: true).first_or_create
+      CalendarEventType.where(title: 'Term 5', description: 'Summer Half Term 1', occupied: true, term_time: true).first_or_create
+      CalendarEventType.where(title: 'Term 6', description: 'Autumn Half Term 2', occupied: true, term_time: true).first_or_create
 
       CSV.foreach(csv_file, headers: true) do |row|
         next if row["Term"].nil?
