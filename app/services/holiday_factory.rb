aa235d4339b6920fb86dea67aa1b7709806e221a
James Jefferies <james@shedcode.co.uk>
2018-05-18 14:27:02 +0100

    Tweak holidays and order of json so everything gets displayed

--- app/services/holiday_factory.rb
+++ app/services/holiday_factory.rb
@@ -4,18 +4,18 @@ class HolidayFactory
   end
 
   def build
-    holiday_type = CalendarEventType.where(title: 'Holiday', holiday: true, colour: 'rgb(56, 112, 53)', occupied: false, term_time: false).first_or_create
+    holiday_type = CalendarEventType.where(title: 'Holiday', holiday: true, colour: 'rgb(68, 183, 62)', occupied: false, term_time: false).first_or_create
     events = @calendar.calendar_events
     terms = events.eager_load(:calendar_event_type).where('calendar_event_types.term_time = true').order(start_date: :asc)
 
     terms.each_with_index do |term, index|
-      # pp term.title
-      # pp terms[index + 1].title
-      # pp index
       next_term = terms[index + 1]
       next if next_term.nil?
-      pp "create holiday for #{term.end_date} #{next_term.start_date}"
-      CalendarEvent.where(calendar: @calendar, calendar_event_type: holiday_type, start_date: term.end_date, end_date: next_term.start_date).first_or_create
+
+      holiday_start_date = term.end_date + 1.day
+      holiday_end_date = next_term.start_date - 1.day
+      pp "create holiday for #{holiday_start_date} #{holiday_end_date}"
+      CalendarEvent.where(calendar: @calendar, calendar_event_type: holiday_type, start_date: holiday_start_date, end_date: holiday_end_date).first_or_create
     end
   end
 end
--- app/views/calendars/_calendar.json.jbuilder
+++ app/views/calendars/_calendar.json.jbuilder
@@ -1,7 +1,8 @@
 # json.extract! calendar, :id, :name, :created_at, :updated_at
 # json.url calendar_url(calendar, format: :json)
 
-json.calendar_events @calendar.calendar_events do |event|
+
+json.calendar_events @calendar.calendar_events.order(:start_date) do |event|
   json.id event.id
   json.name event.calendar_event_type.description
   json.color event.calendar_event_type.colour
