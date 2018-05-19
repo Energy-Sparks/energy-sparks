39f61de6c8e1e54d3777b0d3c2bd2a99a1dadbd5
James Jefferies <james@shedcode.co.uk>
2018-05-19 12:31:05 +0100

    Get calendar creation working properly

--- app/controllers/schools/calendars_controller.rb
+++ app/controllers/schools/calendars_controller.rb
@@ -12,9 +12,37 @@ class Schools::CalendarsController < CalendarsController
     @calendar = CalendarFactory.new(@school.calendar).build
   end
 
+  # POST /calendars
+  # POST /calendars.json
+  def create
+    @calendar = Calendar.new(calendar_params)
+
+    # oijoij
+    respond_to do |format|
+      if @calendar.save
+        @school.update(calendar: @calendar)
+        format.html { redirect_to [@school, @calendar], notice: 'Calendar was successfully created.' }
+        format.json { render :show, status: :created, location: @calendar }
+      else
+        format.html { render :new }
+        format.json { render json: @calendar.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def calendar_params
+    params.require(:calendar).permit(:title, calendar_events_attributes: calendar_event_params)
+  end
+
+  def calendar_event_params
+    [:id, :title, :start_date, :end_date, :calendar_event_type_id]
+  end
 
   def set_school
     @school = School.find(params[:school_id])
   end
 end
+
