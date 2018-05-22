6f10aadfb4e3ca53e1ca435a7cb45a0b8a84d164
James Jefferies <james@shedcode.co.uk>
2018-05-22 10:26:20 +0100

    fix broken test and update legend

--- spec/services/calendar_factory_from_event_hash_spec.rb
+++ spec/services/calendar_factory_from_event_hash_spec.rb
@@ -1,27 +1,21 @@
 require 'rails_helper'
 
 describe CalendarFactoryFromEventHash do
-  include CalendarData
+  include_context 'calendar data'
 
   describe 'creates calendars from hashes' do
-    let!(:area) { Area.create(title: 'this area') }
-    let(:bank_holiday_date) { '01-08-2018'}
-    let(:bank_holiday_title) { 'SuperBH' }
-    let!(:bank_holiday) { BankHoliday.create(area: area, holiday_date: bank_holiday_date, title: bank_holiday_title)}
-    let!(:calendar_event_types) { CalendarEventTypeFactory.create }
-
-    let!(:academic_years) { AcademicYearFactory.new(1990, 2023).create }
-    let(:calendar) { CalendarFactoryFromEventHash.new(CalendarData::EXAMPLE_CALENDAR_HASH, area) }
-
-    it 'creates full calendar with academic years' do 
-      expect(Calendar.count).to be 0
-      calendar = CalendarFactoryFromEventHash.new(CalendarData::EXAMPLE_CALENDAR_HASH, area).create
+    it 'creates full calendar with academic years' do
       expect(Calendar.count).to be 1
+
+      area = Area.create(title: 'this new area')
+      calendar = CalendarFactoryFromEventHash.new(EXAMPLE_CALENDAR_HASH, area).create
+
+      expect(Calendar.count).to be 2
       expect(calendar.terms.count).to be 24
 
       # 1 Bank Holiday and 23 holidays between tterms
       expect(calendar.holidays.count).to be (calendar.terms.count - 1)
-      expect(calendar.inset_days.count).to be 0   
+      expect(calendar.inset_days.count).to be 0
       expect(calendar.calendar_events.count).to be 48
     end
   end
