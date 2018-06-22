478897755da5a0854205ddf889b06ddeefb5a302
James Jefferies <james@shedcode.co.uk>
2018-06-22 12:28:27 +0100

    Add AMR report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,23 @@ class ReportsController < AdminController
   def index
   end
 
+  def amr_data_index
+    @meters = Meter.where(active: true).includes(:school).order('schools.name')
+  end
+
+  def amr_data_show
+    @meter = Meter.includes(:meter_readings).find(params[:meter_id])
+    @first_reading = @meter.first_reading
+    @reading_summary = @meter.meter_readings.order('read_at::date').group('read_at::date').count
+    @missing_array = (@first_reading.read_at.to_date..Date.today).collect do |day|
+      if ! @reading_summary.key?(day)
+        [ day, 'No readings' ]
+      elsif @reading_summary.key?(day) && @reading_summary[day] < 48
+        [ day, 'Partial readings' ]
+      end
+    end.reject! { |c| c.blank? }
+  end
+
   def loading
     @schools = School.enrolled.order(:name)
   end
