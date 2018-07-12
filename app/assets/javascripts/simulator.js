9b50475f2111c0f6c6b6127e61496788f1e3ff24
James Jefferies <james@shedcode.co.uk>
2018-07-12 15:07:39 +0100

    Get the show page working, plus the new with the button to update and the button to submit the form - persist simulator configuration

--- app/assets/javascripts/simulator.js
+++ app/assets/javascripts/simulator.js
@@ -2,19 +2,15 @@
 
 $(document).ready(function() {
   if ($("div.simulator-chart").length ) {
-    $('#new_simulator').on('submit', function(e) {
+    $('button#update-simulator').on('click', function(e) {
       e.preventDefault();
       var data = $("#new_simulator :input").serializeArray();
-
-      var currentPath = window.location.href;
-      var dataPath = currentPath.substr(0, currentPath.lastIndexOf("/")) + '.json';
-
-      $.post(dataPath, data).done(function(data) {
+      var dataPath = window.location.href + '.json';
+      $.get(dataPath, data).done(function(data) {
         $.each(data.charts, function( index, value ) {
-
           var chart = $('div#chart_' + index).highcharts();
-          console.log(data);
-          chart.series[1].setData(value.series_data[0].data);
+          chart.series[0].setData(value.series_data[0].data);
+          chart.series[1].setData(value.series_data[1].data);
         });
       });
     });
