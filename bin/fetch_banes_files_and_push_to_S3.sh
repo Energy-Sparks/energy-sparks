94cc79f5d89f966dc2737517928793eee3828719
James Jefferies <james@shedcode.co.uk>
2018-10-23 14:28:54 +0100

    Working on importer

--- bin/fetch_banes_files_and_push_to_S3.sh
+++ bin/fetch_banes_files_and_push_to_S3.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo `date -u` "Getting any files"
 
-cd ~/bucket
+cd amr_files_bucket
 
 echo "connecting to: $BANES_SFTP_SERVER as $BANES_SFTP_USER"
 
 # Get and then delete files
 lftp sftp://$BANES_SFTP_USER:$BANES_SFTP_PASSWORD@$BANES_SFTP_SERVER:22 <<EOF
 set xfer:log 1
-set xfer:log-file ~/log/lftp.log
+set xfer:log-file ../log/lftp.log
 get 30days.zip
-get 30says.csv
+get 30days.csv
 EOF
 
 shopt -s nullglob dotglob     # To include hidden files
@@ -23,7 +23,14 @@ then
     echo `date -u` "Got a zip file, unzip it"
     unzip 30days.zip
   fi
-  filename=30days-$(date +%d-%m-%Y).csv
+
+  if [ "$(uname)" == "Darwin" ]
+  then
+    filename=30days-$(date -v-1d +%d-%m-%Y).csv
+  else
+    filename=30days-$(date -d yesterday +%d-%m-%Y).csv
+  fi
+
   mv 30days.csv $filename
   aws s3 cp $filename s3://$AWS_S3_AMR_DATA_FEEDS_BUCKET/banes/
 else
--- lib/tasks/amr_importer/import_csv.rake
+++ lib/tasks/amr_importer/import_csv.rake
@@ -5,7 +5,7 @@ namespace :amr_importer do
     puts DateTime.now.utc
     banes_config = AmrDataFeedConfig.set_up_banes
 
-    file_name = '02-10-2018-last30days.csv'
+    file_name = '30days.csv'
     importer = CsvImporter.new(banes_config, file_name)
     importer.parse
 
