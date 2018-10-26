d75eaecf1d97e2d2e86153ca3a07543e5580303e
James Jefferies <james@shedcode.co.uk>
2018-10-26 20:54:35 +0100

    Sorting out caching the aggregated version, but no longer need to validate first time around

--- app/controllers/concerns/school_aggregation.rb
+++ app/controllers/concerns/school_aggregation.rb
@@ -7,7 +7,7 @@ private
     cache_key = "#{school.name.parameterize}-aggregated_meter_collection"
     Rails.cache.fetch(cache_key, expires_in: 3.hours) do
       meter_collection = MeterCollection.new(school)
-      AggregateDataService.new(meter_collection).validate_and_aggregate_meter_data
+      AggregateDataService.new(meter_collection).aggregate_heat_and_electricity_meters
     end
   end
 end
