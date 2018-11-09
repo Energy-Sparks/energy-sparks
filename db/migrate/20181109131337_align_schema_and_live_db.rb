5320488ac955162b9c6f3febfac94fc22486883f
James Almond <james@jamesalmond.com>
2018-11-09 13:22:45 +0000

    Check for index before creation in merit points score

--- db/migrate/20181109131337_align_schema_and_live_db.rb
+++ db/migrate/20181109131337_align_schema_and_live_db.rb
@@ -4,6 +4,6 @@ class AlignSchemaAndLiveDb < ActiveRecord::Migration[5.2]
     change_column_null :sashes, :updated_at, false
     change_column_null :merit_actions, :created_at, false
     change_column_null :merit_actions, :updated_at, false
-    add_index :merit_score_points, :score_id
+    add_index :merit_score_points, :score_id unless index_exists?(:merit_score_points, :score_id)
   end
 end
