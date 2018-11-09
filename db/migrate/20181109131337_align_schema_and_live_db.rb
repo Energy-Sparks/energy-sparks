ebaf8003367b762b36920875f4c1f34d60fecaab
James Almond <james@jamesalmond.com>
2018-11-09 13:18:22 +0000

    Align schema.rb and production database

---
+++ db/migrate/20181109131337_align_schema_and_live_db.rb
@@ -0,0 +1,9 @@
+class AlignSchemaAndLiveDb < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :sashes, :created_at, false
+    change_column_null :sashes, :updated_at, false
+    change_column_null :merit_actions, :created_at, false
+    change_column_null :merit_actions, :updated_at, false
+    add_index :merit_score_points, :score_id
+  end
+end
