34b232eaf5a47ed5d88e81c30d9db2801ce2d468
James Jefferies <james@shedcode.co.uk>
2018-11-23 16:53:27 +0000

    Consolidate school aggregation code

--- app/controllers/concerns/school_aggregation.rb
+++ app/controllers/concerns/school_aggregation.rb
@@ -4,11 +4,6 @@ module SchoolAggregation
 private
 
   def aggregate_school(school)
-    cache_key = "#{school.name.parameterize}-aggregated_meter_collection"
-    Rails.cache.fetch(cache_key, expires_in: 3.hours) do
-      meter_collection = AmrValidatedMeterCollection.new(school)
-      AggregateDataService.new(meter_collection).validate_and_aggregate_meter_data
-      meter_collection
-    end
+    AggregateSchoolService.new(school).aggregate_school
   end
 end
