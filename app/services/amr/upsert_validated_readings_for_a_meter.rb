fa563ccf08252949f654757a4d1fe14d43da0c9c
James Jefferies <james@shedcode.co.uk>
2018-12-04 22:03:03 +0000

    Add functionality to remove validated readings which are no longer valid

--- app/services/amr/upsert_validated_readings_for_a_meter.rb
+++ app/services/amr/upsert_validated_readings_for_a_meter.rb
@@ -8,12 +8,17 @@ module Amr
 
     def perform
       Rails.logger.info "Processing: #{@dashboard_meter} with mpan_mprn: #{@dashboard_meter.mpan_mprn} id: #{@dashboard_meter.external_meter_id}"
+      amr_data = @dashboard_meter.amr_data
+
       Upsert.batch(AmrValidatedReading.connection, AmrValidatedReading.table_name) do |upsert|
-        amr_data = @dashboard_meter.amr_data
         amr_data.values.each do |one_day_read|
           upsert_from_one_day_reading(@dashboard_meter.external_meter_id, upsert, one_day_read)
         end
       end
+
+      dates_to_delete = existing_dates_to_be_deleted(amr_data.keys)
+      deleted_count = delete_by_dates(dates_to_delete)
+      Rails.logger.info "Processing: #{@dashboard_meter} - deleted: #{deleted_count}"
     end
 
   private
@@ -29,6 +34,15 @@ module Amr
         status: one_day_reading.type,
         upload_datetime: one_day_reading.upload_datetime
       )
+    end
+
+    def existing_dates_to_be_deleted(new_validated_dates)
+      array_of_existing_validated_dates = AmrValidatedReading.where(meter_id: @dashboard_meter.external_meter_id).pluck(:reading_date)
+      array_of_existing_validated_dates - new_validated_dates
+    end
+
+    def delete_by_dates(dates_to_delete)
+      AmrValidatedReading.where(meter_id: @dashboard_meter.external_meter_id, reading_date: dates_to_delete).delete_all
     end
 
     def is_nan?(one_day_reading)
