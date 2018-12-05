23aad9dcea1d10505ccdb57dd19251b16dce3454
James Jefferies <james@shedcode.co.uk>
2018-12-05 13:36:17 +0000

    Separate out functions and rename accordingly

--- app/services/amr/csv_parser_and_upserter.rb
+++ app/services/amr/csv_parser_and_upserter.rb
@@ -12,7 +12,9 @@ module Amr
       amr_data_feed_import_log = AmrDataFeedImportLog.create(amr_data_feed_config_id: @config.id, file_name: @file_name, import_time: DateTime.now.utc)
 
       array_of_rows = CsvParser.new(@config, @file_name).perform
-      @inserted_record_count = CsvUpserter.new(@config, array_of_rows, amr_data_feed_import_log.id).perform
+      array_of_rows = DataFeedValidator.new(@config, array_of_rows).perform
+      array_of_data_feed_reading_hashes = DataFeedTranslator.new(@config, array_of_rows).perform
+      @inserted_record_count = DataFeedUpserter.new(array_of_data_feed_reading_hashes, amr_data_feed_import_log.id).perform
 
       Rails.logger.info "Loaded: #{@config.local_bucket_path}/#{@file_name} records inserted: #{@inserted_record_count}"
       amr_data_feed_import_log.update(records_imported: @inserted_record_count)
