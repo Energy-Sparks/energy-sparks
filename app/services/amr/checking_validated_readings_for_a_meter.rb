4168633699fe3daf52fa7a30bd05fba82dc489cc
James Jefferies <james@shedcode.co.uk>
2018-12-05 14:52:35 +0000

    Keep count for readings to delete

--- app/services/amr/checking_validated_readings_for_a_meter.rb
+++ app/services/amr/checking_validated_readings_for_a_meter.rb
@@ -9,20 +9,11 @@ module Amr
     def perform
       pp "Checking: #{@dashboard_meter} with mpan_mprn: #{@dashboard_meter.mpan_mprn} id: #{@dashboard_meter.external_meter_id}"
       amr_data = @dashboard_meter.amr_data
-      dates_to_delete = existing_dates_to_be_deleted(amr_data.keys)
-      deleted_count = delete_by_dates(dates_to_delete)
+
+      readings_to_delete = AmrValidatedReading.where(meter_id: @dashboard_meter.external_meter_id).where.not(reading_date: amr_data.keys)
+      deleted_count = readings_to_delete.count
+      readings_to_delete.delete_all
       pp "Checked: #{@dashboard_meter} - deleted: #{deleted_count}"
-    end
-
-  private
-
-    def existing_dates_to_be_deleted(new_validated_dates)
-      array_of_existing_validated_dates = AmrValidatedReading.where(meter_id: @dashboard_meter.external_meter_id).pluck(:reading_date)
-      array_of_existing_validated_dates - new_validated_dates
-    end
-
-    def delete_by_dates(dates_to_delete)
-      AmrValidatedReading.where(meter_id: @dashboard_meter.external_meter_id, reading_date: dates_to_delete).delete_all
     end
   end
 end
