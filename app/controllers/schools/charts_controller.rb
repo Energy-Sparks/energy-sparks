52b79651027c4ae21e5d3a761b10589a762216cf
James Jefferies <james@shedcode.co.uk>
2018-12-11 16:43:48 +0000

    Start moving stuff out of the controller

--- app/controllers/schools/charts_controller.rb
+++ app/controllers/schools/charts_controller.rb
@@ -10,26 +10,13 @@ class Schools::ChartsController < ApplicationController
     chart_type = params[:chart_type].to_sym
     y_axis_units = params[:chart_y_axis_units].to_sym if params[:chart_y_axis_units]
 
-    @charts = [chart_type]
-    @title = chart_type.to_s.humanize
-    @number_of_charts = 1
-
-    @output = [{ chart_type: chart_type, data: get_chart_data(chart_type, current_user.try(:admin?), y_axis_units) }]
+    @output = ChartData.new(aggregate_school(@school), chart_type, show_benchmark_figures?, y_axis_units).data
   end
 
 private
 
-  def get_chart_data(chart_type, show_benchmark_figures, y_axis_units = :kwh)
-    aggregated_school = aggregate_school(@school)
-    chart_manager = ChartManager.new(aggregated_school, show_benchmark_figures)
-    chart_config = chart_manager.resolve_chart_inheritance(ChartManager::STANDARD_CHART_CONFIGURATION[chart_type])
-
-    if chart_config.key?(:yaxis_units) && chart_config[:yaxis_units] == :kwh
-      chart_config[:yaxis_units] = y_axis_units
-      chart_config[:yaxis_units] = :£ if y_axis_units == :pounds
-    end
-
-    chart_manager.run_chart(chart_config, chart_type)
+  def show_benchmark_figures?
+    current_user.try(:admin?)
   end
 
   def set_school
