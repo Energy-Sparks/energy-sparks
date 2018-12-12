81e8591a662eefd98a1ba99e522c4b78b53e57c0
James Jefferies <james@shedcode.co.uk>
2018-12-12 16:35:32 +0000

    WIP

--- app/models/chart_data.rb
+++ app/models/chart_data.rb
@@ -13,6 +13,26 @@ class ChartData
     [chart_manager.run_chart(chart_config, @chart_type)]
   end
 
+  class Values
+    attr_reader :anaylsis_type, :title, :chart1_type, :chart1_subtype, :y_axis_label, :x_axis_categories, :advice_header, :advice_footer, :y2_axis_label, :series_data, :x_axis_ranges
+
+    def initialize(chart)
+      @title = chart[:title]
+      @x_axis_categories = chart[:x_axis]
+      @x_axis_ranges = chart[:x_axis_ranges] # Not actually used but range of actual dates
+      @chart1_type    = chart[:chart1_type]
+      @chart1_subtype = chart[:chart1_subtype]
+      @y_axis_label   = chart[:y_axis_label]
+      @x_axis_categories = chart[:x_axis]
+      @configuration = chart[:configuration]
+      @advice_header = chart[:advice_header]
+      @advice_footer = chart[:advice_footer]
+
+      @y2_axis_label = '?'
+      @series_data   = '?' # series array
+    end
+  end
+
 private
 
   def customised_chart_config(chart_manager)
@@ -24,3 +44,30 @@ private
     chart_config
   end
 end
+
+# {:title=>"Breakdown by type of day/time: Electricity 18,464 kWh",
+#  :x_axis=>["No Dates"],
+#  :x_axis_ranges=>[[Mon, 11 Dec 2017, Sun, 09 Dec 2018]],
+#  :x_data=>
+#   {"Holiday"=>[2444.9270000000015],
+#    "Weekend"=>[2380.080999999999],
+#    "School Day Open"=>[9527.59599999999],
+#    "School Day Closed"=>[4111.204999999987]},
+#  :chart1_type=>:pie,
+#  :chart1_subtype=>nil,
+#  :y_axis_label=>"kWh",
+#  :config_name=>:daytype_breakdown_electricity,
+#  :configuration=>
+#   {:name=>"Breakdown by type of day/time: Electricity",
+#    :chart1_type=>:pie,
+#    :meter_definition=>:allelectricity,
+#    :x_axis=>:nodatebuckets,
+#    :series_breakdown=>:daytype,
+#    :yaxis_units=>:kwh,
+#    :yaxis_scaling=>:none,
+#    :timescale=>:year,
+#    :min_combined_school_date=>Thu, 17 Jun 2010,
+#    :max_combined_school_date=>Sun, 09 Dec 2018,
+#    :y_axis_label=>"kWh"},
+#  :advice_header=>
+#  :advice_footer=>
