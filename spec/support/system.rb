71456c559d982280aa725cd04eec31fa0353920a
James Jefferies <james@shedcode.co.uk>
2018-12-13 13:15:34 +0000

    Use the new way of setting options for chrome in system tests

--- spec/support/system.rb
+++ spec/support/system.rb
@@ -4,6 +4,28 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+    Capybara.register_driver :headless_chrome do |app|
+      # options explained https://peter.sh/experiments/chromium-command-line-switches/
+      # no-sandbox
+      #   because the user namespace is not enabled in the container by default
+      # headless
+      #   run w/o actually launching gui
+      # disable-gpu
+      #   Disables graphics processing unit(GPU) hardware acceleration
+      # window-size
+      #   sets default window size in case the smaller default size is not enough
+      #   we do not want max either, so this is a good compromise
+
+      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+        chromeOptions: { args: %w[no-sandbox headless disable-gpu disable-dev-shm-usage window-size=1400,1400] }
+      )
+
+      Capybara::Selenium::Driver.new(
+        app,
+        browser:              :chrome,
+        desired_capabilities: capabilities
+      )
+    end
+    driven_by :headless_chrome
   end
 end
