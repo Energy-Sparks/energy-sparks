994f62aac035e4e889c0a1701555dfb62c972810
James Almond <james@jamesalmond.com>
2018-12-14 16:54:24 +0000

    Use base controller for main onboarding action

--- app/controllers/onboarding/base_controller.rb
+++ app/controllers/onboarding/base_controller.rb
@@ -7,9 +7,9 @@ module Onboarding
 
     def load_school_onboarding
       @school_onboarding = if current_user
-                             current_user.school_onboardings.find_by_uuid!(params[:onboarding_id])
+                             current_user.school_onboardings.find_by_uuid!(params.fetch(:onboarding_id) { params[:id] })
                            else
-                             SchoolOnboarding.find_by_uuid!(params[:onboarding_id])
+                             SchoolOnboarding.find_by_uuid!(params.fetch(:onboarding_id) { params[:id] })
                            end
       authorize! :manage, @school_onboarding
     end
--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -1,7 +1,6 @@
-class OnboardingController < ApplicationController
+class OnboardingController < Onboarding::BaseController
   skip_before_action :authenticate_user!
 
   def show
-    @school_onboarding = SchoolOnboarding.find_by_uuid!(params[:id])
   end
 end
