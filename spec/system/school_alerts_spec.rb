822caf72a96f4f0bf598bf50f84202e575a86262
James Jefferies <james@shedcode.co.uk>
2018-12-20 12:59:22 +0000

    Amend specs accordingly

--- spec/system/school_alerts_spec.rb
+++ spec/system/school_alerts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "school alerts", type: :system do
 
-  let(:alerts_button) { 'Manage alerts' }
+  let(:alerts_button) { 'Manage alert subscriptions' }
   let(:school_name) { 'Oldfield Park Infants'}
   let!(:school)     { create(:school, name: school_name)}
   let!(:admin)      { create(:user, role: 'admin')}
@@ -18,32 +18,32 @@ RSpec.describe "school alerts", type: :system do
 
     describe 'shows alerts' do
 
-      let!(:alert) { create(:alert, school: school) }
+      let!(:alert_subscription) { create(:alert_subscription, school: school) }
       it 'shows me a page with all possible alerts' do
-        expect(alert.alert_type.fuel_type).to be_in AlertType.fuel_types.keys
-        expect(alert.alert_type.sub_category).to be_in AlertType.sub_categories.keys
-        expect(alert.school).to eq school
+        expect(alert_subscription.alert_type.fuel_type).to be_in AlertType.fuel_types.keys
+        expect(alert_subscription.alert_type.sub_category).to be_in AlertType.sub_categories.keys
+        expect(alert_subscription.school).to eq school
         click_on(school_name)
         expect(page).to have_content alerts_button
         click_on alerts_button
-        expect(page).to have_content alert.alert_type.title
+        expect(page).to have_content alert_subscription.alert_type.title
         expect(page).to have_content 'No one allocated'
       end
 
       it 'shows me a page with an allocated contact' do
         contact = create(:contact_with_name_email)
-        alert.contacts << contact
+        alert_subscription.contacts << contact
         click_on(school_name)
         expect(page).to have_content alerts_button
         click_on alerts_button
-        expect(page).to have_content alert.alert_type.title
+        expect(page).to have_content alert_subscription.alert_type.title
         expect(page).to have_content contact.name
       end
     end
 
     describe 'existing contacts' do
       let!(:contact) { create(:contact_with_name_email, school: school) }
-      let!(:alert) { create(:alert, school: school, contacts: [contact]) }
+      let!(:alert_subscription) { create(:alert_subscription, school: school, contacts: [contact]) }
 
       it 'shows me the contacts on the page' do
         click_on(school_name)
