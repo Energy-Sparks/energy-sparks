25c1481c3040605cf0ca4d40c7eb04af0d658676
James Almond <james@jamesalmond.com>
2018-12-20 17:28:40 +0000

    Better looking emails

--- app/mailers/onboarding_mailer.rb
+++ app/mailers/onboarding_mailer.rb
@@ -1,18 +1,18 @@
 class OnboardingMailer < ApplicationMailer
   def onboarding_email
     @school_onboarding = params[:school_onboarding]
-    mail(to: @school_onboarding.contact_email, subject: "Set up your school on Energy Sparks")
+    make_bootstrap_mail(to: @school_onboarding.contact_email, subject: "Set up your school on Energy Sparks")
   end
 
   def completion_email
     @school_onboarding = params[:school_onboarding]
     if @school_onboarding.created_by
-      mail(to: @school_onboarding.created_by.email, subject: "#{@school_onboarding.school_name} has completed the onboarding process")
+      make_bootstrap_mail(to: @school_onboarding.created_by.email, subject: "#{@school_onboarding.school_name} has completed the onboarding process")
     end
   end
 
   def reminder_email
     @school_onboarding = params[:school_onboarding]
-    mail(to: @school_onboarding.contact_email, subject: "Don't forget to set up your school on Energy Sparks")
+    make_bootstrap_mail(to: @school_onboarding.contact_email, subject: "Don't forget to set up your school on Energy Sparks")
   end
 end
