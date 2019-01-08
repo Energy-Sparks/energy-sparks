ed940803beb460ddc2eeb5635ac0d7cd340d4105
James Jefferies <james@shedcode.co.uk>
2019-01-08 15:46:55 +0000

    Title is now optional unless it's required

--- spec/system/add_activity_spec.rb
+++ spec/system/add_activity_spec.rb
@@ -4,10 +4,13 @@ describe 'adding a new activity' do
 
   let(:school_name) { 'Active school'}
   let(:activity_type_name) { 'Exciting activity' }
+  let(:other_activity_type_name) { 'Exciting activity (please specify)' }
   let(:activity_description) { 'What we did' }
+  let(:custom_title) { 'Custom title' }
   let!(:school) { create_active_school(name: school_name)}
   let!(:admin)  { create(:user, role: 'school_user', school: school)}
   let!(:activity_type) { create(:activity_type, name: activity_type_name, description: "It's An #{activity_type_name}") }
+  let!(:other_activity_type) { create(:activity_type, name: other_activity_type_name, description: nil, custom: true) }
 
   before(:each) do
     sign_in(admin)
@@ -16,32 +19,32 @@ describe 'adding a new activity' do
     click_on('Record your activity')
   end
 
-  it 'allows an activity to be created', js: true do
-  #  fill_in :activity_happened_on, with: Date.today.strftime("%d/%m/%Y")
-    expect(page.has_content?(activity_type_name))
+  it 'allows an activity to be created without title', js: true do
     select(activity_type_name, from: 'Activity type')
-    fill_in :activity_title, with: 'The title'
+
     editor = find('trix-editor')
     editor.click.set(activity_description)
     click_on 'Save activity'
     expect(page.has_content?('Activity was successfully created.')).to be true
     expect(page.has_content?(activity_description)).to be true
+    expect(page.has_content?(activity_type_name)).to be true
     expect(page.has_content?(Date.today.strftime("%A, %d %B %Y"))).to be true
+  end
 
-    activity = Activity.first
-    expect(activity.title).to eq 'The title'
-    expect(activity.description).to eq "<div>#{activity_description}</div>"
-    expect(activity.happened_on).to eq Date.today
-
-    expect(Activity.count).to be 1
+  it 'allows an activity to be created with custom title', js: true do
+    select(other_activity_type_name, from: 'Activity type')
+    fill_in :activity_title, with: custom_title
+    editor = find('trix-editor')
+    editor.click.set(activity_description)
+    click_on 'Save activity'
+    expect(page.has_content?('Activity was successfully created.')).to be true
+    expect(page.has_content?(activity_description)).to be true
+    expect(page.has_content?(other_activity_type_name)).to be false
+    expect(page.has_content?(custom_title)).to be true
+    expect(page.has_content?(Date.today.strftime("%A, %d %B %Y"))).to be true
   end
 
   it 'defaults activity date to current date' do
     expect(find_field(:activity_happened_on).value).to eq Date.today.strftime("%d/%m/%Y")
   end
-
-  it 'has an activity type to select' do
-    expect(page.has_content?(activity_type_name))
-  end
-
 end
