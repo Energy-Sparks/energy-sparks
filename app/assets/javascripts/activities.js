1d2af1a967b77a10f03d5dc7805255b0ff984ee4
James Jefferies <james@shedcode.co.uk>
2019-01-08 14:05:22 +0000

    Make sure edit works ok too

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -2,12 +2,15 @@
 
 $(document).ready(function() {
  if ($("form.activity-form").length) {
+  console.log('hello');
     var $datePickerDiv = $('div#activity_date_picker_field');
+    var defaultDate = moment($datePickerDiv.data('default-date'), 'DD/MM/YYYY')
+
     if ($datePickerDiv.length) {
       $datePickerDiv.datetimepicker({
         format: 'DD/MM/YYYY',
         allowInputToggle: true,
-        debug: true
+        date: defaultDate
       });
     }
   }
