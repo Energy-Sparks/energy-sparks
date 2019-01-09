c7dfff71fd79ba9900fab934aba2bf2ea4ea091b
James Almond <james@jamesalmond.com>
2019-01-09 15:35:46 +0000

    Remvove calendar bang saves

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -69,20 +69,20 @@ private
   def update_neighbour_start
     if calendar_event_type.term_time
       following_holiday = calendar.holidays.find_by(start_date: end_date_before_last_save + 1.day)
-      following_holiday.update!(start_date: end_date + 1.day) if following_holiday.present?
+      following_holiday.update(start_date: end_date + 1.day) if following_holiday.present?
     elsif calendar_event_type.holiday
       following_term = calendar.terms.find_by(start_date: end_date_before_last_save + 1.day)
-      following_term.update!(start_date: end_date + 1.day) if following_term.present?
+      following_term.update(start_date: end_date + 1.day) if following_term.present?
     end
   end
 
   def update_neighbour_end
     if calendar_event_type.term_time
       previous_holiday = calendar.holidays.find_by(end_date: start_date_before_last_save - 1.day)
-      previous_holiday.update!(end_date: start_date - 1.day) if previous_holiday.present?
+      previous_holiday.update(end_date: start_date - 1.day) if previous_holiday.present?
     elsif calendar_event_type.holiday
       previous_term = calendar.terms.find_by(end_date: start_date_before_last_save - 1.day)
-      previous_term.update!(end_date: start_date - 1.day) if previous_term.present?
+      previous_term.update(end_date: start_date - 1.day) if previous_term.present?
     end
   end
 end
