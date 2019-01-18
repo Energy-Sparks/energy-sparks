0733c4c67393cecef78d64ee479b082a78a04b68
James Jefferies <james@shedcode.co.uk>
2019-01-18 16:15:30 +0000

    Let adapter return an alert object

--- app/services/alerts/build_alerts.rb
+++ app/services/alerts/build_alerts.rb
@@ -1,5 +1,3 @@
-require 'dashboard'
-
 module Alerts
   class BuildAlerts
     def initialize(
@@ -18,42 +16,24 @@ module Alerts
     end
 
     def perform
-      @alerts << run_alerts(AlertType.no_fuel, Time.zone.today)
+      @alerts << get_alerts(AlertType.no_fuel, Time.zone.today)
 
       if @school.meters_with_readings(:electricity).any?
-        @alerts << run_alerts(AlertType.electricity, @electricity_analysis_date)
+        @alerts << get_alerts(AlertType.electricity, @electricity_analysis_date)
       end
 
       if @school.meters_with_readings(:gas).any?
-        @alerts << run_alerts(AlertType.gas, @gas_analysis_date)
+        @alerts << get_alerts(AlertType.gas, @gas_analysis_date)
       end
       @alerts.flatten
     end
 
   private
 
-    def run_alerts(alert_types, analysis_date)
+    def get_alerts(alert_types, analysis_date)
       alert_types.map do |alert_type|
-        alert_framework_instance = @alert_framework_adapter.new(alert_type, @aggregate_school).alert_instance
-        alert_framework_instance.analyse(analysis_date)
-
-        Alert.new(
-          school_id: @school.id,
-          alert_type_id: alert_type.id,
-          run_on: analysis_date,
-          status: alert_framework_instance.analysis_report.status,
-          summary: alert_framework_instance.analysis_report.summary,
-          data: data_hash(alert_framework_instance.analysis_report),
-        )
+        @alert_framework_adapter.new(alert_type, @school, @aggregate_school, analysis_date).analyse
       end
-    end
-
-    def data_hash(analysis_report)
-      {
-        help_url: analysis_report.help_url,
-        detail: analysis_report.detail,
-        rating: analysis_report.rating,
-      }
     end
   end
 end
--- app/services/alerts/framework_adapter.rb
+++ app/services/alerts/framework_adapter.rb
@@ -1,12 +1,42 @@
+require 'dashboard'
+
 module Alerts
   class FrameworkAdapter
-    def initialize(alert_type, aggregate_school)
+    def initialize(alert_type, school, aggregate_school, analysis_date)
       @alert_type = alert_type
+      @school = school
       @aggregate_school = aggregate_school
+      @analysis_date = analysis_date
     end
 
+    def analyse
+      analysis_report = alert_instance.new(@aggregate_school).analyse(@analysis_date)
+      convert_to_alert(analysis_report)
+    end
+
+  private
+
     def alert_instance
-      @alert_type.class_name.constantize.new(@aggregate_school)
+      @alert_type.class_name.constantize
+    end
+
+    def convert_to_alert(analysis_report)
+      Alert.new(
+        school_id: @school.id,
+        alert_type_id: @alert_type.id,
+        run_on: @analysis_date,
+        status: analysis_report.status,
+        summary: analysis_report.summary,
+        data: data_hash(analysis_report),
+      )
+    end
+
+    def data_hash(analysis_report)
+      {
+        help_url: analysis_report.help_url,
+        detail: analysis_report.detail,
+        rating: analysis_report.rating,
+      }
     end
   end
 end
