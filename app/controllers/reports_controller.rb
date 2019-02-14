d520b3a623515ad2572eba5be31a29ca80ef1be8
James Jefferies <james@shedcode.co.uk>
2019-02-14 16:33:24 +0000

    Get CSV download working on admin reports pages

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,10 @@
 require 'dashboard'
 
 class ReportsController < AdminController
+  include CsvDownloader
+
   COLOUR_ARRAY = ['#5cb85c', "#9c3367", "#67347f", "#501e74", "#935fb8", "#e676a3", "#e4558b", "#7a9fb1", "#5297c6", "#97c086", "#3f7d69", "#6dc691", "#8e8d6b", "#e5c07c", "#e9d889", "#e59757", "#f4966c", "#e5644e", "#cd4851", "#bd4d65", "#515749", "#e5644e", "#cd4851", "#bd4d65", "#515749"].freeze
+  CSV_HEADER = "Reading Date,00:30,01:00,01:30,02:00,02:30,03:00,03:30,04:00,04:30,05:00,05:30,06:00,06:30,07:00,07:30,08:00,08:30,09:00,09:30,10:00,10:30,11:00,11:30,12:00,12:30,13:00,13:30,14:00,14:30,15:00,15:30,16:00,16:30,17:00,17:30,18:00,18:30,19:00,19:30,20:00,20:30,21:00,21:30,22:00,22:30,23:00,23:30,24:00".freeze
 
   def index
   end
@@ -33,6 +36,14 @@ class ReportsController < AdminController
     @first_read = @data_feed.first_reading(@feed_type)
     @reading_summary = get_data_feed_readings(@data_feed, @feed_type)
     @missing_array = get_missing_array(@first_read, @reading_summary)
+    respond_to do |format|
+      format.html
+      format.csv do
+        sql_query = sql_query_for_downloading(@data_feed.id, @feed_type)
+        send_data readings_to_csv(sql_query, CSV_HEADER), filename: "data-feed-readings-#{@data_feed.title.parameterize}-#{@feed_type.to_s.parameterize}.csv"
+      end
+      format.json
+    end
   end
 
   def loading
@@ -65,5 +76,11 @@ class ReportsController < AdminController
       end
     end
     missing_array.reject!(&:blank?)
+  end
+
+private
+
+  def sql_query_for_downloading(data_feed_id, feed_type)
+    DataFeedReading.download_query(data_feed_id, feed_type)
   end
 end
