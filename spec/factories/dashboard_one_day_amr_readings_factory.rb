b84e90dec5a861b3eaa61cf45fbf727dc9bff37f
James Jefferies <james@shedcode.co.uk>
2019-02-20 14:28:21 +0000

    Fix factory bot deprecation warnings

--- spec/factories/dashboard_one_day_amr_readings_factory.rb
+++ spec/factories/dashboard_one_day_amr_readings_factory.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
   factory :dashboard_one_day_amr_reading, class: 'OneDayAMRReading' do
     transient do
       association :dashboard_meter, factory: :dashboard_gas_meter
-      date Date.yesterday
-      kwh_data_x48 { Array.new(48, rand) }
-      one_day_kwh { kwh_data_x48.sum }
-      status { 'ORIG'}
+      date          { Date.yesterday }
+      kwh_data_x48  { Array.new(48, rand) }
+      one_day_kwh   { kwh_data_x48.sum }
+      status        { 'ORIG'}
     end
 
     # Note the nil is the substitute date
