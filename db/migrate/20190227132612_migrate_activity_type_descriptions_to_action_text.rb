01bdbdeffb169acb427c5e85ac7f99941e5e9d4d
James Jefferies <james@shedcode.co.uk>
2019-02-28 13:41:10 +0000

    Migrations should be reversible

--- db/migrate/20190227132612_migrate_activity_type_descriptions_to_action_text.rb
+++ db/migrate/20190227132612_migrate_activity_type_descriptions_to_action_text.rb
@@ -1,9 +1,15 @@
 class MigrateActivityTypeDescriptionsToActionText < ActiveRecord::Migration[6.0]
-  def change
+  def up
     execute <<-SQL
       INSERT INTO action_text_rich_texts(name, record_type, record_id, body, created_at, updated_at)
       SELECT 'description', 'ActivityType', id, description, created_at, updated_at
       FROM activity_types;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DELETE FROM action_text_rich_texts WHERE record_type = 'ActivityType';
     SQL
   end
 end
