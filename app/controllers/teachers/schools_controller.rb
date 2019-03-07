f529d81f786f5c7a24d544f974749d0e4d6a9ff3
James Jefferies <james@shedcode.co.uk>
2019-03-07 16:11:08 +0000

    Don't warm aggregate school cache in controller

--- app/controllers/teachers/schools_controller.rb
+++ app/controllers/teachers/schools_controller.rb
@@ -1,5 +1,4 @@
 class Teachers::SchoolsController < SchoolsController
-  include SchoolAggregation
   # GET /schools/1
   def show
     redirect_to enrol_path unless @school.active? || (current_user && current_user.manages_school?(@school.id))
@@ -8,8 +7,5 @@ class Teachers::SchoolsController < SchoolsController
 
     @charts = [:electricity_by_day_of_week, :gas_by_day_of_week]
     @number_of_charts = @charts.size
-
-    # Get this loaded and warm the cache before starting the chart rendering
-    aggregate_school(@school)
   end
 end
