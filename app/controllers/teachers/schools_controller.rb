5760f0a73e0bc62f4f0b1618c420914076388953
James Jefferies <james@shedcode.co.uk>
2019-03-07 14:57:23 +0000

    Add alerts and clear out the views

--- app/controllers/teachers/schools_controller.rb
+++ app/controllers/teachers/schools_controller.rb
@@ -1,2 +1,8 @@
 class Teachers::SchoolsController < SchoolsController
+  # GET /schools/1
+  def show
+    redirect_to enrol_path unless @school.active? || (current_user && current_user.manages_school?(@school.id))
+    @activities = @school.activities.order("happened_on DESC")
+    @latest_alerts_sample = @school.alerts.usable.latest.sample
+  end
 end
