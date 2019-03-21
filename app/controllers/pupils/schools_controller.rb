565e5c66f9279c0f2fb9c6f98fe30e9f30a2b4cf
James Jefferies <james@shedcode.co.uk>
2019-03-21 14:30:44 +0000

    Let's get that lightbulb in

--- app/controllers/pupils/schools_controller.rb
+++ app/controllers/pupils/schools_controller.rb
@@ -5,5 +5,19 @@ class Pupils::SchoolsController < SchoolsController
     if @scoreboard
       @surrounding_schools = @scoreboard.surrounding_schools(@school)
     end
+    # Temporary until equivalences are set up
+    electricity_card = MeterCard.create(school: @school, supply: :electricity)
+    gas_card = MeterCard.create(school: @school, supply: :gas)
+
+    electricity_message = "Based on our latest data, your school used an average of #{electricity_card.values.average_usage} kWh of electricity per day"
+    gas_message = "Based on our latest data, your school used an average of #{gas_card.values.average_usage} kWh of gas per day"
+
+    @message = if @school.fuel_types == :electric_and_gas
+                 [electricity_message, gas_message].sample
+               elsif @school.fuel_types == :electric_only
+                 electricity_message
+               else
+                 gas_message
+               end
   end
 end
