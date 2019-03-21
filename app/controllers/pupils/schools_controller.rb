3f6f4fdd2903d448c8620c0141bb02d6014fdd0d
James Jefferies <james@shedcode.co.uk>
2019-03-21 14:52:32 +0000

    Look out for no values

--- app/controllers/pupils/schools_controller.rb
+++ app/controllers/pupils/schools_controller.rb
@@ -9,12 +9,12 @@ class Pupils::SchoolsController < SchoolsController
 
     if @school.meters?(:electricity)
       electricity_card = MeterCard.create(school: @school, supply: :electricity)
-      electricity_message = "Based on our latest data, your school used an average of #{electricity_card.values.average_usage} kWh of electricity per day"
+      electricity_message = "Based on our latest data, your school used an average of #{electricity_card.values.average_usage} kWh of electricity per day" if electricity_card.values
     end
 
     if @school.meters?(:gas)
       gas_card = MeterCard.create(school: @school, supply: :gas)
-      gas_message = "Based on our latest data, your school used an average of #{gas_card.values.average_usage} kWh of gas per day"
+      gas_message = "Based on our latest data, your school used an average of #{gas_card.values.average_usage} kWh of gas per day" if gas_card.values
     end
 
     @message = if electricity_message && gas_message
