5d58cf0b31bd1b1ceaefcbe4d1e260cec390ce47
James Jefferies <james@shedcode.co.uk>
2019-03-22 14:53:09 +0000

    WIP

---
+++ app/models/email.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: emails
+#
+#  contact_id :bigint(8)        not null
+#  created_at :datetime         not null
+#  id         :bigint(8)        not null, primary key
+#  sent_at    :datetime
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_emails_on_contact_id  (contact_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (contact_id => contacts.id) ON DELETE => cascade
+#
+
+class Email < ApplicationRecord
+  belongs_to :contact
+  has_many   :alert_subscription_events
+end
