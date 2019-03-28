256272414cc44b46c447522df67b44f87f10f884
James Jefferies <james@shedcode.co.uk>
2019-03-28 19:03:12 +0000

    Sort out daily cron job running

--- app/services/alerts/generate_and_save_alerts.rb
+++ app/services/alerts/generate_and_save_alerts.rb
@@ -8,34 +8,22 @@ module Alerts
       @alert_framework_adapter = alert_framework_adapter
     end
 
-    def weekly_alerts
-      perform(AlertType.weekly)
-    end
+    def perform
+      generate(AlertType.no_fuel)
 
-    def termly_alerts
-      perform(AlertType.termly)
-    end
+      if @school.meters_with_validated_readings(:electricity).any?
+        generate(AlertType.electricity)
+      end
 
-    def before_holiday_alerts
-      perform(AlertType.before_each_holiday)
+      if @school.meters_with_validated_readings(:gas).any?
+        generate(AlertType.gas)
+      end
     end
 
   private
 
-    def perform(alert_types_by_frequency)
-      generate(alert_types_by_frequency.no_fuel)
-
-      if @school.meters_with_validated_readings(:electricity).any?
-        generate(alert_types_by_frequency.electricity)
-      end
-
-      if @school.meters_with_validated_readings(:gas).any?
-        generate(alert_types_by_frequency.gas)
-      end
-    end
-
     def generate(alert_types)
-      alert_types.map do |alert_type|
+      alert_types.each do |alert_type|
         alert = @alert_framework_adapter.new(alert_type, @school).analyse
         alert.save
       end
