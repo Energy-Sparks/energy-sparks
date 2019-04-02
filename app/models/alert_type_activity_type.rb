5deddcee4a019a0ebac1cb6a24113a236a0dac6c
James Almond <james@jamesalmond.com>
2019-04-02 10:44:48 +0100

    Anotate alert type activity type model

--- app/models/alert_type_activity_type.rb
+++ app/models/alert_type_activity_type.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: alert_type_activity_types
+#
+#  activity_type_id :bigint(8)        not null
+#  alert_type_id    :bigint(8)        not null
+#  id               :bigint(8)        not null, primary key
+#  position         :integer          default(0), not null
+#
+# Indexes
+#
+#  activity_alert_uniq  (alert_type_id,activity_type_id) UNIQUE
+#
+
 class AlertTypeActivityType < ApplicationRecord
   belongs_to :activity_type
   belongs_to :alert_type
