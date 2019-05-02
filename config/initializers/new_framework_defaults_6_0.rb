2c327441c5598eff4800706b25b0c56afdc10fd8
James Jefferies <james@shedcode.co.uk>
2019-05-02 19:06:51 +0100

    Some of the defaults set up

--- config/initializers/new_framework_defaults_6_0.rb
+++ config/initializers/new_framework_defaults_6_0.rb
@@ -7,7 +7,7 @@
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
 
 # Don't force requests from old versions of IE to be UTF-8 encoded.
-# Rails.application.config.action_view.default_enforce_utf8 = false
+Rails.application.config.action_view.default_enforce_utf8 = false
 
 # Embed purpose and expiry metadata inside signed and encrypted
 # cookies for increased security.
@@ -17,11 +17,11 @@
 # Rails.application.config.action_dispatch.use_cookies_with_metadata = true
 
 # Return false instead of self when enqueuing is aborted from a callback.
-# Rails.application.config.active_job.return_false_on_aborted_enqueue = true
+Rails.application.config.active_job.return_false_on_aborted_enqueue = true
 
 # Send Active Storage analysis and purge jobs to dedicated queues.
-# Rails.application.config.active_storage.queues.analysis = :active_storage_analysis
-# Rails.application.config.active_storage.queues.purge    = :active_storage_purge
+Rails.application.config.active_storage.queues.analysis = :active_storage_analysis
+Rails.application.config.active_storage.queues.purge    = :active_storage_purge
 
 # Use ActionMailer::MailDeliveryJob for sending parameterized and normal mail.
 #
