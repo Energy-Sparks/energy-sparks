147371b81f39d97d8184cdc3ac5cfcf31cc6fa92
James Almond <james@jamesalmond.com>
2019-05-08 10:35:04 +0100

    Migrate data feeds to area and set oxford to bath

--- app/models/data_feeds/solar_pv_tuos.rb
+++ app/models/data_feeds/solar_pv_tuos.rb
@@ -2,19 +2,13 @@
 #
 # Table name: data_feeds
 #
-#  area_id       :bigint(8)
 #  configuration :json             not null
 #  description   :text
 #  id            :bigint(8)        not null, primary key
 #  title         :text
 #  type          :text             not null
 #
-# Indexes
-#
-#  index_data_feeds_on_area_id  (area_id)
-#
 
 # TUOS is The University of Sheffield
 class DataFeeds::SolarPvTuos < DataFeed
-  belongs_to :solar_pv_tuos_area
 end
