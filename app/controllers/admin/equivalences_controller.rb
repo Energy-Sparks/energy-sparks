2daea91b7aea9739ea1dd08ea28b714bdc000e98
James Almond <james@jamesalmond.com>
2019-05-20 17:23:41 +0100

    Generate equivalences for a specific school

--- app/controllers/admin/equivalences_controller.rb
+++ app/controllers/admin/equivalences_controller.rb
@@ -18,6 +18,9 @@ module Admin
     end
 
     def create
+      school = School.find(params[:school_id])
+      Equivalences::GenerateEquivalences.new(school, EnergyConversions).perform
+      redirect_back fallback_location: admin_equivalence_types_path
     end
 
   private
