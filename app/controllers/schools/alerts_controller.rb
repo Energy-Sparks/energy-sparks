c83b1b39cb6e2a292ecf4db36208ad84c43727ee
James Almond <james@jamesalmond.com>
2019-05-21 16:26:24 +0100

    Show alerts again

---
+++ app/controllers/schools/alerts_controller.rb
@@ -0,0 +1,10 @@
+module Schools
+  class AlertsController < ApplicationController
+    load_and_authorize_resource :school
+
+    def show
+      @alert = Alert.find(params[:id])
+      authorize! :read, @alert
+    end
+  end
+end
