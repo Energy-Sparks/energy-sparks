c0da7d16e4e0cf42d75467c35aa70a5629f64140
James Almond <james@jamesalmond.com>
2019-05-22 11:50:15 +0100

    Set the data rating for holiday alerts

--- db/migrate/20190521131820_update_alert_structure.rb
+++ db/migrate/20190521131820_update_alert_structure.rb
@@ -6,6 +6,8 @@ class UpdateAlertStructure < ActiveRecord::Migration[6.0]
     add_column :alerts, :rating, :decimal
 
     connection.execute "UPDATE alerts SET rating = (data#>>'{template_data,rating}')::decimal WHERE data#>'{template_data,rating}' IS NOT NULL"
+    # we don't have the rating in the template for holiday alerts
+    connection.execute "UPDATE alerts SET rating = (data#>>'{rating}')::decimal WHERE data#>'{template_data,holiday_start_date}' IS NOT NULL"
     connection.execute "UPDATE alerts SET template_data = data->'template_data' WHERE data->'template_data' IS NOT NULL"
     connection.execute "UPDATE alerts SET chart_data = data->'chart_data' WHERE data->'chart_data' IS NOT NULL"
     connection.execute "UPDATE alerts SET table_data = data->'table_data' WHERE data->'table_data' IS NOT NULL"
