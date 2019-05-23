f4c715a071979dde915c51adb9c88303b8da3988
James Jefferies <james@shedcode.co.uk>
2019-05-23 13:48:06 +0100

    Safe delete for school admins, else proper delete

--- app/controllers/schools/meters_controller.rb
+++ app/controllers/schools/meters_controller.rb
@@ -59,8 +59,15 @@ module Schools
     end
 
     def destroy
-      MeterManagement.new(@meter).delete_meter!
+      if can? :delete, @meter
+        MeterManagement.new(@meter).delete_meter!
+      else
+        @meter.safe_destroy
+      end
+
       redirect_to school_meters_path(@school)
+    rescue EnergySparks::SafeDestroyError => e
+      redirect_to school_meters_path(@school), alert: "Delete failed: #{e.message}"
     end
 
   private
