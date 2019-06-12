c5def1b5eb6e2f809ece17fdb0cfc4fad19d6637
James Jefferies <james@shedcode.co.uk>
2019-06-12 17:17:04 +0100

    Add awesomecomplete
    Remove 'Aim' content
    
    Autocomplete and sort observation spec

--- app/controllers/schools/observations_controller.rb
+++ app/controllers/schools/observations_controller.rb
@@ -4,6 +4,7 @@ module Schools
     load_and_authorize_resource through: :school
 
     skip_before_action :authenticate_user!, only: [:index, :show]
+    before_action :set_location_names, only: [:new, :create]
 
     def new
       @observation = @school.observations.build
@@ -12,6 +13,8 @@ module Schools
     end
 
     def create
+      @observation = process_locations(@observation)
+
       if @observation.save
         redirect_to school_observations_path(@school)
       else
@@ -26,6 +29,19 @@ module Schools
     end
 
   private
+
+    def set_location_names
+      @location_names = @school.locations.pluck(:name).join(',')
+    end
+
+    def process_locations(observation)
+      observation.temperature_recordings.each do |temperature_recording|
+        location = temperature_recording.location
+        existing_location = @school.locations.find_by(name: location.name)
+        temperature_recording.location = existing_location if existing_location.present?
+      end
+      observation
+    end
 
     def observation_params
       params.require(:observation).permit(:school_id, :description, :at, temperature_recordings_attributes: [:id, :centigrade, location_attributes: [:id, :name, :school_id]])
