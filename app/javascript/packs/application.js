522ee27b5c2f0d31120ac44b9d87951154ba4d34
James Jefferies <james@shedcode.co.uk>
2019-06-12 21:13:21 +0100

    WIP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,71 @@ console.log('Hello World from Webpacker')
 
 require("trix")
 require("@rails/actiontext")
+
+import 'bootstrap/dist/js/bootstrap';
+
+global.moment = require('moment');
+//require("popper");
+require('tempusdominus-bootstrap-4');
+
+$(function () {
+  console.log('Hello Jquery from Webpacker');
+});
+//require("babel-loader")
+require("chart.js")
+require("highcharts")
+
+import Highcharts from 'highcharts';
+
+require("chartkick")
+
+//require("chartkick").use(require("highcharts"))
+
+import '../src/activities.js';
+import '../src/data_explorer.js';
+import '../src/schools.js';
+import '../src/users.js';
+import '../src/calendar_year_view.js';
+import '../src/common_chart_options.js';
+import '../src/analysis.js';
+import '../src/simulator.js';
+import '../src/alert_reports.js';
+
+import '../src/content.js';
+
+import '../src/aggregated_meter_collection.js';
+import '../src/date_pickers.js';
+
+
+
+//= require highcharts
+//= require highcharts-modules/exporting
+//= require highcharts-modules/offline-exporting
+//= require chartkick
+
+// This is used (handlebars) but slight overkill I think
+//= require handlebars
+
+//= require activities
+//= require data_explorer
+//= require schools
+//= require users
+
+// from https://github.com/neivars/bootstrap-year-calendar
+//= require bootstrap-year-calendar
+//= require calendar_year_view
+
+//= require common_chart_options
+//= require analysis
+//= require simulator
+
+//= require moment
+//= require tempusdominus-bootstrap-4.js
+
+//= require alert_reports
+//= require content
+
+//= require aggregated_meter_collection
+//= require date_pickers
+
+
