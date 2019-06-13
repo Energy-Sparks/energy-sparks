a3019440fff7302a656cee4a53109db7fbe0214c
James Jefferies <james@shedcode.co.uk>
2019-06-13 16:03:16 +0100

    Convert floor area to float

--- app/services/amr/analytics_school_factory.rb
+++ app/services/amr/analytics_school_factory.rb
@@ -11,13 +11,18 @@ module Amr
       @school_class.new(
         @active_record_school.name,
         @active_record_school.address,
-        @active_record_school.floor_area,
+        floor_area,
         @active_record_school.number_of_pupils,
         @active_record_school.school_type,
         @active_record_school.area_name,
         @active_record_school.urn,
         @active_record_school.postcode
       )
+    end
+
+  private
+    def floor_area
+      @active_record_school.floor_area.to_f
     end
   end
 end
