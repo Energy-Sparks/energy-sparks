a1d2d3688641f8bffec32f470541c73b336d85ac
James Jefferies <james@shedcode.co.uk>
2019-07-02 17:37:10 +0100

    Further test

--- spec/services/schools/generate_chart_configuration_spec.rb
+++ spec/services/schools/generate_chart_configuration_spec.rb
@@ -27,29 +27,85 @@ module Schools
                                   daytype_breakdown_gas
                                   group_by_week_gas
                                 ]
-                              }
+                              },
+                        main_dashboard_electric_and_gas: {
+                                name:   'Overview',
+                                charts: %i[
+                                  benchmark
+                                  daytype_breakdown_electricity
+                                  daytype_breakdown_gas
+                                  group_by_week_electricity
+                                  group_by_week_gas
+                                ]
+                              },
+                        gas_detail:               {
+                                name:   'Gas Detail',
+                                charts: %i[
+                                  daytype_breakdown_gas
+                                  group_by_week_gas
+                                ]
+                              },
                       }}
-    let(:fuel_configuration) { FuelConfiguration.new(fuel_types_for_analysis: :electric_only, no_meters_with_validated_readings: false) }
-    let(:dashboard_config) {{ electric_only: %i[main_dashboard_electric electricity_detail], gas_only: %i[main_dashboard_gas gas_detail boiler_control] }}
 
-    it 'returns chart config' do
-      allow_any_instance_of(ChartData).to receive(:success?).and_return(true)
-      GenerateChartConfiguration.new(school, nil, fuel_configuration, dashboard_config, page_config).generate
+    let(:dashboard_config) {{
+      electric_only: %i[main_dashboard_electric electricity_detail],
+      gas_only: %i[main_dashboard_gas gas_detail boiler_control],
+      electric_and_gas: %i[main_dashboard_electric_and_gas electricity_detail gas_detail]
+    }}
+
+    let(:electric_only_page_config) do
       page_config.delete(:main_dashboard_gas)
-
-      expect(school.configuration.analysis_charts_as_symbols).to eq page_config
+      page_config.delete(:main_dashboard_electric_and_gas)
+      page_config.delete(:gas_detail)
+      page_config
     end
 
-    it 'returns chart reduced config if a chart fails' do
-      allow_any_instance_of(ChartData).to receive(:success?) do |_amc, chart_type, _thing, _chart_config|
-        chart_type != :baseload
+    let(:electric_only_page_config_no_baseload) do
+      eopc = electric_only_page_config.clone
+      eopc[:electricity_detail][:charts].delete(:baseload)
+      eopc
+    end
+
+    let(:dual_fuel_failed_electricity) do
+      page_config.delete(:main_dashboard_electric)
+      page_config.delete(:main_dashboard_gas)
+      page_config.delete(:electricity_detail)
+      page_config[:main_dashboard_electric_and_gas][:charts].delete(:daytype_breakdown_electricity)
+      page_config[:main_dashboard_electric_and_gas][:charts].delete(:group_by_week_electricity)
+      page_config
+    end
+
+    let(:chart_data) { instance_double(ChartData) }
+
+    before(:each) do
+      allow(ChartData).to receive(:new).and_return(chart_data)
+    end
+
+    context 'electric only set up' do
+      let(:fuel_configuration) { FuelConfiguration.new(fuel_types_for_analysis: :electric_only, no_meters_with_validated_readings: false) }
+
+      it 'returns chart config' do
+        allow(chart_data).to receive(:success?).and_return(true)
+        GenerateChartConfiguration.new(school, nil, fuel_configuration, dashboard_config, page_config).generate
+        expect(school.configuration.analysis_charts_as_symbols).to eq electric_only_page_config
       end
 
-      GenerateChartConfiguration.new(school, nil, fuel_configuration, dashboard_config, page_config).generate
-      page_config.delete(:main_dashboard_gas)
-      page_config[:electricity_detail].delete(:baseload)
+      it 'returns chart reduced config if a chart fails' do
+        allow(chart_data).to receive(:success?).and_return(true, true, true, false, true, true)
+        GenerateChartConfiguration.new(school, nil, fuel_configuration, dashboard_config, page_config).generate
+        expect(school.configuration.analysis_charts_as_symbols).to eq electric_only_page_config_no_baseload
+      end
 
-      expect(school.configuration.analysis_charts_as_symbols).to eq page_config
+    end
+
+    context 'dual fuel set up' do
+      let(:fuel_configuration) { FuelConfiguration.new(fuel_types_for_analysis: :electric_and_gas, no_meters_with_validated_readings: false) }
+
+      it 'returns a single fuel main dashboard if dual fuel fails' do
+        allow(chart_data).to receive(:success?).and_return(true, false, true, false, true, false, false, true, true)
+        GenerateChartConfiguration.new(school, nil, fuel_configuration, dashboard_config, page_config).generate
+        expect(school.configuration.analysis_charts_as_symbols).to eq dual_fuel_failed_electricity
+      end
     end
   end
 end
