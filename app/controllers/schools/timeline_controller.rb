7e56b297c22ebb3085c3a9f4d59c3132109977fc
James Almond <james@jamesalmond.com>
2019-08-16 15:18:26 +0100

    Filter timeline to academic year

--- app/controllers/schools/timeline_controller.rb
+++ app/controllers/schools/timeline_controller.rb
@@ -2,7 +2,18 @@ module Schools
   class TimelineController < ApplicationController
     load_and_authorize_resource :school
     def show
-      @observations = @school.observations.order('at DESC')
+      @academic_year = if params[:academic_year]
+                         AcademicYear.find(params[:academic_year])
+                       else
+                         @school.calendar_area.academic_year_for(Time.zone.today)
+                       end
+      first_observation = @school.observations.order('at ASC').first
+      if first_observation
+        @active_academic_years = @school.calendar_area.parent_area.academic_years.where('end_date >= ? AND start_date <= ?', first_observation.at, Time.zone.today)
+      else
+        @academic_years = []
+      end
+      @observations = @school.observations.order('at DESC').where('at BETWEEN ? AND ?', @academic_year.start_date, @academic_year.end_date)
     end
   end
 end
