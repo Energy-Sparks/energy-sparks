d755eedefd5256bb5eda888f806d9f546f05a10e
James Jefferies <james@shedcode.co.uk>
2019-08-21 14:16:44 +0100

    Remove activity suggestions in controller, not required here!

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -22,7 +22,6 @@ class SchoolsController < ApplicationController
 
     setup_charts
     setup_dashboard_alerts
-    setup_activity_suggestions
     setup_timeline
   end
 
@@ -99,19 +98,6 @@ class SchoolsController < ApplicationController
   end
 
 private
-
-  def setup_activity_suggestions
-    @activities_count = @school.activities.count
-    suggester = NextActivitySuggesterWithFilter.new(@school, activity_type_filter)
-    @activities_from_programmes = suggester.suggest_from_programmes.limit(1)
-    @activities_from_alerts = suggester.suggest_from_find_out_mores.sample(1)
-    if @activities_from_programmes.empty?
-      started_programmes = @school.programmes.active
-      @suggested_programme = ProgrammeType.active.where.not(id: started_programmes.map(&:programme_type_id)).sample
-    end
-    cards_filled = [@activities_from_programmes + @activities_from_alerts + [@suggested_programme]].flatten.compact.size
-    @activities_from_activity_history = suggester.suggest_from_activity_history.slice(0, (3 - cards_filled))
-  end
 
   def set_key_stages
     @key_stages = KeyStage.order(:name)
