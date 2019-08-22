ac0dfaa012556b3d7618f054799dfa0f4f97e62e
James Jefferies <james@shedcode.co.uk>
2019-08-22 14:39:53 +0100

    Test around bank holiday loader

--- spec/factories/calendar_event_types_factory.rb
+++ spec/factories/calendar_event_types_factory.rb
@@ -2,9 +2,25 @@ FactoryBot.define do
   factory :calendar_event_type do
     trait :holiday do
       holiday { true }
+      analytics_event_type { :school_holiday }
     end
     trait :term_time do
       term_time { true }
+      analytics_event_type { :term_time }
+    end
+    trait :bank_holiday do
+      bank_holiday { true }
+      analytics_event_type { :bank_holiday }
+    end
+    trait :inset_day_in_school do
+      inset_day { true }
+      school_occupied { true }
+      analytics_event_type { :inset_day_in_school }
+    end
+    trait :inset_day_out_of_school do
+      inset_day { true }
+      school_occupied { false }
+      analytics_event_type { :inset_day_out_of_school }
     end
   end
 end
