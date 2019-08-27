dc32a3eaea3ab7f94c8c080a131a897a6e77c0d1
James Almond <james@jamesalmond.com>
2019-08-27 10:02:02 +0100

    Better styled devise messages

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'hello@energysparks.uk'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'EnergySparksDeviseMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
