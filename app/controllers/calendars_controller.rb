320f80e34ad8a8dd7842467bc153cea63c34f7aa
James Jefferies <james@shedcode.co.uk>
2019-08-28 22:14:51 +0100

    Remove unused routes

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -16,52 +16,9 @@ class CalendarsController < ApplicationController
   def show
   end
 
-  # GET /calendars/new
-  def new
-    build_terms
-  end
-
-  # GET /calendars/1/edit
-  def edit
-    build_terms
-    redirect_to calendar_path(@calendar) if @calendar.template?
-  end
-
-  # POST /calendars
-  def create
-    if @calendar.save
-      redirect_to @calendar, notice: 'Calendar was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /calendars/1
-  def update
-    if @calendar.update(calendar_params)
-      redirect_to @calendar, notice: 'Calendar was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
   # DELETE /calendars/1
   def destroy
     @calendar.update_attribute(:deleted, true)
     redirect_to calendars_url, notice: 'Calendar was marked as deleted.'
-  end
-
-  private
-
-  def calendar_params
-    params.require(:calendar).permit(:name)
-  end
-
-  def build_terms
-    number_to_build = 6 - @calendar.terms.count
-    number_to_build = 1 if number_to_build < 1
-    number_to_build.times do
-      @calendar.terms.build
-    end
   end
 end
