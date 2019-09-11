22ea792059f60b7854c0465fa16acb37ee0988f9
James Jefferies <james@shedcode.co.uk>
2019-09-11 15:09:35 +0100

    Remove unused factory

--- spec/factories/calendars_factory.rb
+++ spec/factories/calendars_factory.rb
@@ -6,18 +6,6 @@ FactoryBot.define do
     calendar_type { :school }
     calendar_area
 
-    factory :calendar_with_terms do
-      transient do
-        term_count { 5 }
-      end
-
-      after(:create) do |calendar, evaluator|
-        evaluator.term_count.times do |i|
-          create(:term, calendar: calendar, start_date: i.weeks.from_now, end_date: (i.weeks.from_now + 6.days))
-        end
-      end
-    end
-
     factory :school_calendar do
       calendar_type { :school }
     end
@@ -31,7 +19,6 @@ FactoryBot.define do
     end
 
     trait :with_terms do
-
       transient do
         term_count { 5 }
       end
