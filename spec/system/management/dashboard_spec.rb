0b4b143567af691c223a87bc35b2dab51ca6a509
James Almond <james@jamesalmond.com>
2019-09-20 12:32:36 +0100

    Flesh out management dashboard

--- spec/system/management/dashboard_spec.rb
+++ spec/system/management/dashboard_spec.rb
@@ -4,6 +4,7 @@ describe 'Management dashboard' do
 
   let!(:school){ create(:school) }
   let(:management){ create(:management, school: school) }
+  let!(:intervention){ create(:observation, school: school) }
 
   before(:each) do
     sign_in(management)
@@ -11,7 +12,10 @@ describe 'Management dashboard' do
 
   it 'allows login and access to management dashboard' do
     visit root_path
-    expect(page).to have_content("#{school.name} management dashboard")
+    expect(page).to have_content("#{school.name}")
+    expect(page).to have_content("Energy Usage")
+
+    expect(page).to have_content("Recorded temperatures")
   end
 
 end
