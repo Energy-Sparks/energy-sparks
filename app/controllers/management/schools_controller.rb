be746bcce68bf0e87c9e416fb15a95e93671e4f1
James Almond <james@jamesalmond.com>
2019-09-25 12:05:00 +0100

    Pass variables to interpolated content, update management preview

--- app/controllers/management/schools_controller.rb
+++ app/controllers/management/schools_controller.rb
@@ -22,16 +22,9 @@ module Management
 
     def setup_priorities
       @school.latest_management_priorities.limit(5).map do |priority|
-        template_variables = priority.alert.template_variables.with_indifferent_access
         TemplateInterpolation.new(
           priority.content_version,
-          with_objects: {
-            find_out_more: priority.find_out_more,
-            average_capital_cost: template_variables[:average_capital_cost],
-            average_payback_years: template_variables[:average_payback_years],
-            average_one_year_saving_gbp: template_variables[:average_one_year_saving_gbp],
-            average_ten_year_saving_gbp: template_variables[:ten_year_saving_gbp_low] # TODO: replace when variable gets implemented
-          },
+          with_objects: { find_out_more: priority.find_out_more },
           proxy: [:colour]
         ).interpolate(
           :management_priorities_title,
