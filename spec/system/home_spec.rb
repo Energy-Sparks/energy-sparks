6c6c8dd3d64626c469f5661fbf60fe7eb5c43a98
James Jefferies <james@shedcode.co.uk>
2019-09-25 15:57:17 +0100

    Fix tests and working home page

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "home", type: :system do
 
   it 'has a contact page' do
     visit root_path
-    within('.nav') do
+    within('.navbar-nav') do
       click_on('Contact')
     end
     expect(page.has_content? "Contact us")
