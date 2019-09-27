3bb763f491cd6c7ccd4d2f3663237d8232f426b0
James Almond <james@jamesalmond.com>
2019-09-27 11:19:58 +0100

    Management priorities table limit in site settings

--- app/models/site_settings.rb
+++ app/models/site_settings.rb
@@ -2,11 +2,12 @@
 #
 # Table name: site_settings
 #
-#  created_at                  :datetime         not null
-#  id                          :bigint(8)        not null, primary key
-#  management_priorities_limit :integer          default(10)
-#  message_for_no_contacts     :boolean          default(TRUE)
-#  updated_at                  :datetime         not null
+#  created_at                            :datetime         not null
+#  id                                    :bigint(8)        not null, primary key
+#  management_priorities_dashboard_limit :integer          default(5)
+#  management_priorities_page_limit      :integer          default(10)
+#  message_for_no_contacts               :boolean          default(TRUE)
+#  updated_at                            :datetime         not null
 #
 
 class SiteSettings < ApplicationRecord
