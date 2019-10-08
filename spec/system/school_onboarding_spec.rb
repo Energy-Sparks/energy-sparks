0302d6be87d857d3c6fcb8d97adda29e605f5374
James Jefferies <james@shedcode.co.uk>
2019-10-08 11:14:09 +0100

    Update sanitizer and fix test

--- spec/system/school_onboarding_spec.rb
+++ spec/system/school_onboarding_spec.rb
@@ -130,7 +130,7 @@ RSpec.describe "school onboarding", :schools, type: :system do
       click_on 'Next'
 
       click_on 'I give permission'
-      expect(page).to have_content('Please confirm agreement with the Energy Sparks privacy policy')
+      expect(page).to have_content('Please confirm')
 
       check 'I confirm agreement with the Energy Sparks privacy policy'
       click_on 'I give permission'
