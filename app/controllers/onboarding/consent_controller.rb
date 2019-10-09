d544f000015d69b02c7aa88f4fee18f297fa81db
James Jefferies <james@shedcode.co.uk>
2019-10-09 09:56:57 +0100

    Create privacy policy event

--- app/controllers/onboarding/consent_controller.rb
+++ app/controllers/onboarding/consent_controller.rb
@@ -13,7 +13,9 @@ module Onboarding
       @school_onboarding_consent = SchoolOnboardingConsent.new(school_onboarding_consent_params)
 
       if @school_onboarding_consent.valid?
+        @school_onboarding.events.create!(event: :privacy_policy_agreed)
         @school_onboarding.events.create!(event: :permission_given)
+
         redirect_to new_onboarding_account_path(@school_onboarding)
       else
         render :show
