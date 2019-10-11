3d6ab365cd8e7505b438af1a06f5810f0651040b
James Almond <james@jamesalmond.com>
2019-10-11 16:36:02 +0100

    Improve solar and storage dashboard links

--- app/controllers/concerns/dashboard_energy_charts.rb
+++ app/controllers/concerns/dashboard_energy_charts.rb
@@ -7,17 +7,19 @@ module DashboardEnergyCharts
     return charts if school_configuration.nil?
 
     if school_configuration.has_electricity
-      charts[:electricity] = { chart_type: :teachers_landing_page_electricity, explore: true, name: 'electricity' }
+      charts[:solar] = { chart_type: :teachers_landing_page_electricity }
+    elsif school_configuration.has_electricity
+      charts[:electricity] = { chart_type: :teachers_landing_page_electricity }
     end
 
     gas_dashboard_chart_type = school_configuration.gas_dashboard_chart_type.to_sym
     if gas_dashboard_chart_type != Schools::Configuration::NO_GAS_CHART
-      charts[:gas] = { chart_type: gas_dashboard_chart_type, explore: true, name: 'gas' }
+      charts[:gas] = { chart_type: gas_dashboard_chart_type }
     end
 
     storage_heater_dashboard_chart_type = school_configuration.storage_heater_dashboard_chart_type.to_sym
     if storage_heater_dashboard_chart_type != Schools::Configuration::NO_STORAGE_HEATER_CHART
-      charts[:storage_heaters] = { chart_type: storage_heater_dashboard_chart_type, explore: true, name: 'storage heater' }
+      charts[:storage_heaters] = { chart_type: storage_heater_dashboard_chart_type }
     end
 
     charts
