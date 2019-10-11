5225d0dcc23758da7185197d13a2123efd27b8b2
James Jefferies <james@shedcode.co.uk>
2019-10-11 11:58:53 +0100

    Allow users to edit, create, destroy locations

--- spec/system/temperature_recordings_spec.rb
+++ spec/system/temperature_recordings_spec.rb
@@ -9,12 +9,14 @@ describe 'temperature recordings as school admin' do
 
     let!(:user)       { create(:pupil, school: school)}
 
+    before(:each) do
+      sign_in(user)
+      visit root_path
+      click_on 'Enter temperatures'
+    end
+
+
     context 'adding recordings' do
-      before(:each) do
-        sign_in(user)
-        visit root_path
-        click_on 'Enter temperatures'
-      end
 
       it 'allows an observation to be added' do
         fill_in 'Temperature', match: :first, with: 20
@@ -76,6 +78,47 @@ describe 'temperature recordings as school admin' do
         fill_in 'observation_temperature_recordings_attributes_0_location_attributes_name', with: 'Kitchen'
         fill_in 'observation_temperature_recordings_attributes_1_centigrade', with: 18
         expect { click_on('Create temperature recordings') }.to change { Observation.count }.by(1).and change { TemperatureRecording.count }.by(2).and change { Location.count }.by(2)
+      end
+    end
+
+    context 'manage locations' do
+      it "allows a user to create a location on it's own" do
+        location_name = 'ABCDEF'
+        click_on 'Manage locations and rooms'
+        click_on 'Add new location'
+        fill_in :location_name, with: location_name
+        expect { click_on 'Create' }.to change { Location.count }.by(1)
+      end
+
+      it "allows a user to delete a location on it's own" do
+        location_name = 'ABCDEF'
+        Location.create(school: school, name: location_name)
+        click_on 'Manage locations and rooms'
+        expect(page).to have_content(location_name)
+        expect { click_on 'Delete' }.to change { Location.count }.by(-1)
+      end
+
+      it "allows a user to edit a location on it's own" do
+        location_name = 'ABCDEF'
+        new_location_name = 'GAGA'
+        Location.create(school: school, name: location_name)
+        click_on 'Manage locations and rooms'
+        expect(page).to have_content(location_name)
+        expect(page).to_not have_content(new_location_name)
+        click_on 'Edit'
+        fill_in :location_name, with: new_location_name
+        click_on 'Update Location'
+
+        expect(page).to_not have_content(location_name)
+        expect(page).to have_content(new_location_name)
+        expect(page).to have_content('Location updated')
+      end
+
+      it 'deletes an assocated temperature recording if location is nobbled' do
+        observation = create(:observation_with_temperature_recording_and_location, school: school)
+        click_on 'Manage locations and rooms'
+        expect(page).to have_content(observation.locations.first.name)
+        expect { click_on 'Delete' }.to change { Location.count }.by(-1).and change { TemperatureRecording.count }.by(-1)
       end
     end
   end
