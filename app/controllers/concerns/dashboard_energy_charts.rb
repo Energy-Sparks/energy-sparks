5fcd90e90265e00b45537b8d9a1b0864282361d9
James Almond <james@jamesalmond.com>
2019-10-11 16:20:43 +0100

    Improve navigation to pupil analysis pages

--- app/controllers/concerns/dashboard_energy_charts.rb
+++ app/controllers/concerns/dashboard_energy_charts.rb
@@ -7,17 +7,17 @@ module DashboardEnergyCharts
     return charts if school_configuration.nil?
 
     if school_configuration.has_electricity
-      charts[:electricity] = { chart_type: :teachers_landing_page_electricity, explore: true }
+      charts[:electricity] = { chart_type: :teachers_landing_page_electricity, explore: true, name: 'electricity' }
     end
 
     gas_dashboard_chart_type = school_configuration.gas_dashboard_chart_type.to_sym
     if gas_dashboard_chart_type != Schools::Configuration::NO_GAS_CHART
-      charts[:gas] = { chart_type: gas_dashboard_chart_type, explore: true }
+      charts[:gas] = { chart_type: gas_dashboard_chart_type, explore: true, name: 'gas' }
     end
 
     storage_heater_dashboard_chart_type = school_configuration.storage_heater_dashboard_chart_type.to_sym
     if storage_heater_dashboard_chart_type != Schools::Configuration::NO_STORAGE_HEATER_CHART
-      charts[:storage_heaters] = { chart_type: storage_heater_dashboard_chart_type, explore: false }
+      charts[:storage_heaters] = { chart_type: storage_heater_dashboard_chart_type, explore: true, name: 'storage heater' }
     end
 
     charts
