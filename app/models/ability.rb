d7eaea0b21772d42958f8c6618e3e2eebae89a3f
James Jefferies <james@shedcode.co.uk>
2019-10-14 16:42:17 +0100

    Rename ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,7 +26,7 @@ class Ability
       can :manage, :all
       can :analyse, :test
       can :analyse, :heating_model_fitting
-      cannot :read, :school_menu
+      cannot :read, :my_school_menu
     elsif user.school_admin? || user.group_admin?
       if user.group_admin?
         school_scope = { school_group_id: user.school_group_id }
@@ -38,6 +38,7 @@ class Ability
         can :manage, CalendarEvent do |calendar_event|
           user.school_group.calendars.include?(calendar_event.calendar)
         end
+        can :read, :my_school_menu, School, active: true
       else
         school_scope = { id: user.school_id }
         related_school_scope = { school_id: user.school_id }
@@ -46,7 +47,7 @@ class Ability
         can :manage, SchoolOnboarding do |onboarding|
           onboarding.created_user == user
         end
-        can :read, :school_menu
+        can :read, :my_school_menu, School, active: true
         can :read, :dashboard_menu
       end
       can [
@@ -78,7 +79,7 @@ class Ability
         can :crud, Programme, school: { id: user.school_id, active: true }
         can :enable_alerts, User, id: user.id
         can [:create, :update, :destroy], Contact, user_id: user.id
-        can :read, :school_menu
+        can :read, :my_school_menu, School, active: true
         can :read, :dashboard_menu
       end
     elsif user.guest?
