4e1b65bd5a301dc2864c7f51d26f993f66418756
James Jefferies <james@shedcode.co.uk>
2019-10-16 11:13:18 +0100

    Load categories as before

--- app/controllers/activity_categories_controller.rb
+++ app/controllers/activity_categories_controller.rb
@@ -1,9 +1,12 @@
 class ActivityCategoriesController < ApplicationController
   include ActivityTypeFilterable
 
+  load_and_authorize_resource
+  skip_before_action :authenticate_user!
+
   # GET /activity_categories
   def index
     @filter = activity_type_filter
-    @activity_categories = ActivityCategory.all.order(:name)
+    @activity_categories = @activity_categories.order(:name)
   end
 end
