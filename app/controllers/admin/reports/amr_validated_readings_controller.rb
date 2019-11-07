612f4f0f7007c1cecb6125a8e81d512ccb6935fc
James Jefferies <james@shedcode.co.uk>
2019-11-07 15:20:34 +0000

    show deprecated meters too

--- app/controllers/admin/reports/amr_validated_readings_controller.rb
+++ app/controllers/admin/reports/amr_validated_readings_controller.rb
@@ -7,6 +7,12 @@ module Admin
       CSV_HEADER = "School URN,Mpan Mprn,Reading Date,One Day Total kWh,Status,Substitute Date,00:30,01:00,01:30,02:00,02:30,03:00,03:30,04:00,04:30,05:00,05:30,06:00,06:30,07:00,07:30,08:00,08:30,09:00,09:30,10:00,10:30,11:00,11:30,12:00,12:30,13:00,13:30,14:00,14:30,15:00,15:30,16:00,16:30,17:00,17:30,18:00,18:30,19:00,19:30,20:00,20:30,21:00,21:30,22:00,22:30,23:00,23:30,00:00".freeze
 
       def index
+        @meter_scope = if params.key?(:all_meters)
+                         {}
+                       else
+                         { active: true }
+                       end
+
         @school_groups = SchoolGroup.includes(:schools).where.not(schools: { id: nil }).order(:name)
 
         respond_to do |format|
