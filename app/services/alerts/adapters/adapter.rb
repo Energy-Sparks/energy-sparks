28dc747de22407841bd2bfb6c2c180349870b525
James Jefferies <james@shedcode.co.uk>
2019-11-21 20:43:55 +0000

    Use new 'true' flag on analyse for benchmarks

--- app/services/alerts/adapters/adapter.rb
+++ app/services/alerts/adapters/adapter.rb
@@ -1,11 +1,12 @@
 module Alerts
   module Adapters
     class Adapter
-      def initialize(alert_type:, school:, analysis_date:, aggregate_school:)
+      def initialize(alert_type:, school:, analysis_date:, aggregate_school:, use_max_meter_date_if_less_than_asof_date: false)
         @alert_type = alert_type
         @school = school
         @analysis_date = analysis_date
         @aggregate_school = aggregate_school
+        @use_max_meter_date_if_less_than_asof_date = use_max_meter_date_if_less_than_asof_date
       end
 
       def alert_class
