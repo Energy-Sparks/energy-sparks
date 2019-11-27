710ac9e54343d730e70262009398fd53fffb26f9
James Jefferies <james@shedcode.co.uk>
2019-11-27 14:22:44 +0000

    Remove info

--- app/assets/javascripts/table_sorting.js
+++ app/assets/javascripts/table_sorting.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
   $('.table-sorted').DataTable({
     'paging': false,
-    'searching': false,
+    'searching': false, // Switch off search field
+    'info': false       // Switch off the summary of rows at the bottom of the page
   });
 });
