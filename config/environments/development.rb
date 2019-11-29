9ed0c6b41e34140c871f8cddbae9693a6a69fe64
James Jefferies <james@shedcode.co.uk>
2019-11-29 17:14:31 +0000

    Put null store back

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :file_store, "tmp/jj_cache"
+    config.cache_store = :null_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
