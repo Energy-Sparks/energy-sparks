48a33020dbe9b36647ddad783300a7f3926552f5
James Jefferies <james@shedcode.co.uk>
2019-12-02 17:03:05 +0000

    Show errors to admin users

--- app/controllers/benchmarks_controller.rb
+++ app/controllers/benchmarks_controller.rb
@@ -13,54 +13,48 @@ class BenchmarksController < ApplicationController
     respond_to do |format|
       format.html do
         @page = params[:benchmark_type].to_sym
-        #results_hash = get_content([@page])
+        @page_groups = [{ name: '', benchmarks: { @page => @page } }]
 
-        @page_groups = [{
-          name: '', benchmarks: { @page => @page }
-        }]
-
-        @errors = []
-        @form_path = benchmark_path
-
-        @content_hash = {}
-        @content_hash[@page] = filter_content(content_for_page(@page, @errors))
+        sort_content_and_page_groups(@page_groups)
       end
       format.yaml { send_data YAML.dump(@benchmark_results), filename: "benchmark_results_data.yaml" }
     end
   end
 
   def show_all
-    # @content_list = available_pages
-    # results_hash = get_content(available_pages)
-
-    # @content = results_hash[:content]
-    # @errors = results_hash[:errors]
     @title = 'All benchmark results'
     @form_path = all_benchmarks_path
 
     @page_groups = content_manager.structured_pages
 
-    @content_hash = {}
-    errors = []
-
-    @page_groups.each do |heading_hash|
-      heading_hash[:benchmarks].each do |page, _title|
-        @content_hash[page] = filter_content(content_for_page(page, errors))
-      end
-    end
+    sort_content_and_page_groups(@page_groups)
 
     render :show
   end
 
 private
 
+  def sort_content_and_page_groups(page_groups)
+    @content_hash = {}
+    @errors = []
+
+    page_groups.each do |heading_hash|
+      heading_hash[:benchmarks].each do |page, _title|
+        @content_hash[page] = filter_content(content_for_page(page, @errors))
+      end
+    end
+  end
+
   def content_for_page(page, errors = [])
     content_manager.content(@benchmark_results, page)
     # rubocop:disable Lint/RescueException
   rescue Exception => e
     # rubocop:enable Lint/RescueException
-    error_message = "Exception: #{page}: #{e.class} #{e.message} #{e.backtrace.join("\n")}"
-    errors << error_message
+    error_message = "Exception: #{page}: #{e.class} #{e.message}"
+
+    backtrace = e.backtrace.select { |b| b.include?('analytics')}.join("<br>")
+    full_html_output = "Exception: #{page}: #{e.class} #{e.message} #{backtrace}"
+    errors << { message: error_message, full_html_output: full_html_output }
     {}
   end
 
