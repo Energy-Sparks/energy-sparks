6a5f44c521fd886c85addba8e7bd5bdafa686bb5
James Jefferies <james@shedcode.co.uk>
2019-12-02 14:39:35 +0000

    Considering structured page for the other views

--- app/controllers/benchmarks_controller.rb
+++ app/controllers/benchmarks_controller.rb
@@ -7,36 +7,62 @@ class BenchmarksController < ApplicationController
 
   def index
     @page_groups = content_manager.structured_pages
-    @school_groups = SchoolGroup.all
   end
 
   def show
     respond_to do |format|
       format.html do
         @page = params[:benchmark_type].to_sym
-        results_hash = get_content([@page])
+        #results_hash = get_content([@page])
 
-        @content = results_hash[:content]
-        @errors = results_hash[:errors]
+        @page_groups = [{
+          name: '', benchmarks: { @page => @page }
+        }]
+
+        @errors = []
         @form_path = benchmark_path
+
+        @content_hash = {}
+        @content_hash[@page] = filter_content(content_for_page(@page, @errors))
       end
       format.yaml { send_data YAML.dump(@benchmark_results), filename: "benchmark_results_data.yaml" }
     end
   end
 
   def show_all
-    @content_list = available_pages
-    results_hash = get_content(available_pages)
+    # @content_list = available_pages
+    # results_hash = get_content(available_pages)
 
-    @content = results_hash[:content]
-    @errors = results_hash[:errors]
+    # @content = results_hash[:content]
+    # @errors = results_hash[:errors]
     @title = 'All benchmark results'
     @form_path = all_benchmarks_path
+
+    @page_groups = content_manager.structured_pages
+
+    @content_hash = {}
+    errors = []
+
+    @page_groups.each do |heading_hash|
+      heading_hash[:benchmarks].each do |page, _title|
+        @content_hash[page] = filter_content(content_for_page(page, errors))
+      end
+    end
 
     render :show
   end
 
 private
+
+  def content_for_page(page, errors = [])
+    content_manager.content(@benchmark_results, page)
+    # rubocop:disable Lint/RescueException
+  rescue Exception => e
+    # rubocop:enable Lint/RescueException
+    error_message = "Exception: #{page}: #{e.class} #{e.message} #{e.backtrace.join("\n")}"
+    errors << error_message
+    {}
+  end
 
   def benchmark_results
     @school_group_ids = params.dig(:benchmark, :school_group_ids) || []
@@ -73,7 +99,7 @@ private
   end
 
   def filter_content(all_content)
-    all_content.select { |content| [:chart, :html, :table_composite, :title].include?(content[:type]) && content[:content].present? }
+    all_content.select { |content| content.present? && [:chart, :html, :table_composite, :title].include?(content[:type]) && content[:content].present? }
   end
 
   def content_manager(date = Time.zone.today)
