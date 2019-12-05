f5b437bdf3bc161c43274c69488388f56658842d
James Jefferies <james@shedcode.co.uk>
2019-12-05 13:12:43 +0000

    Use school id rather than school object

--- app/services/alerts/collate_benchmark_data.rb
+++ app/services/alerts/collate_benchmark_data.rb
@@ -14,16 +14,16 @@ module Alerts
 
     def get_benchmarks_for_latest_run(latest_school_runs, benchmarks)
       latest_school_runs.each do |benchmark_result_school_generation_run|
-        school = benchmark_result_school_generation_run.school
+        school_id = benchmark_result_school_generation_run.school_id
 
         benchmark_result_school_generation_run.benchmark_results.each do |benchmark_result|
           unless benchmarks.key?(benchmark_result.asof)
-            benchmarks[benchmark_result.asof] = { school.id => {} }
+            benchmarks[benchmark_result.asof] = { school_id => {} }
           end
-          unless benchmarks[benchmark_result.asof].key?(school.id)
-            benchmarks[benchmark_result.asof][school.id] = {}
+          unless benchmarks[benchmark_result.asof].key?(school_id)
+            benchmarks[benchmark_result.asof][school_id] = {}
           end
-          benchmarks[benchmark_result.asof][school.id] = benchmarks[benchmark_result.asof][school.id].merge(benchmark_result.data)
+          benchmarks[benchmark_result.asof][school_id] = benchmarks[benchmark_result.asof][school_id].merge(benchmark_result.data)
         end
       end
     end
