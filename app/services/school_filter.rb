37ec68a07d69d94db97b7d604c68b0315a6add05
James Jefferies <james@shedcode.co.uk>
2019-12-12 10:22:33 +0000

    Remove fuel filter from benchmarks

--- app/services/school_filter.rb
+++ app/services/school_filter.rb
@@ -1,25 +1,18 @@
 class SchoolFilter
-  def initialize(school_group_ids: [], fuel_type: nil)
+  def initialize(school_group_ids: [])
     @school_group_ids = school_group_ids.reject(&:blank?)
-    @fuel_type = fuel_type
     @default_scope = School.process_data
   end
 
   def filter
-    schools = @default_scope
+    return @default_scope unless @school_group_ids.any?
 
-    schools = schools_from_school_groups(schools) if @school_group_ids.any?
-    schools = schools_from_fuel_type(schools) if @fuel_type.present?
-    schools.to_a
+    schools_from_school_groups(@default_scope).to_a
   end
 
   private
 
   def schools_from_school_groups(schools)
     schools.where(school_group_id: @school_group_ids)
-  end
-
-  def schools_from_fuel_type(schools)
-    schools.select { |school| school.send("has_#{@fuel_type}?") }
   end
 end
