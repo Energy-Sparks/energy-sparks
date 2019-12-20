5e98fb03ee3a293decce648faf7c01fa108e50c9
James Almond <james@jamesalmond.com>
2019-12-20 14:20:59 +0000

    Refactor import notifier, clearer email headings

--- app/services/import_notifier.rb
+++ app/services/import_notifier.rb
@@ -6,21 +6,11 @@ class ImportNotifier
   def data(from:, to:)
     AmrDataFeedConfig.order(:description).inject({}) do |collection, config|
       import_logs = config.amr_data_feed_import_logs.where('import_time BETWEEN ? AND ?', from, to).order(:import_time)
-      meters_running_behind = import_logs.map do |_log|
-        config.meters.select {|meter| meter.last_validated_reading && meter.last_validated_reading < config.import_warning_days.days.ago}
-      end
-      meters_with_blank_data = import_logs.map do |log|
-        log.amr_data_feed_readings.select {|reading| reading.readings.blank? || reading.readings.all?(&:blank?)}.map(&:meter).compact
-      end
-      meters_with_zero_data = import_logs.map do |log|
-        log.amr_data_feed_readings.select {|reading| reading.readings.present? && reading.readings.all? {|x48| x48.to_f == 0.0 rescue true}}.map(&:meter).compact
-      end
-
       collection[config] = {
         import_logs: import_logs,
-        meters_running_behind: meters_running_behind.flatten.sort_by(&:school_name),
-        meters_with_blank_data: meters_with_blank_data.flatten.uniq.sort_by(&:school_name),
-        meters_with_zero_data: meters_with_zero_data.flatten.uniq.sort_by(&:school_name)
+        meters_running_behind: meters_running_behind(config).sort_by(&:school_name),
+        meters_with_blank_data: meters_with_blank_data(import_logs).sort_by(&:school_name),
+        meters_with_zero_data: meters_with_zero_data(import_logs).sort_by(&:school_name)
       }
       collection
     end
@@ -28,5 +18,26 @@ class ImportNotifier
 
   def notify(from:, to:)
     ImportMailer.with(data: data(from: from, to: to), description: @description).import_summary.deliver_now
+  end
+
+
+  private
+
+  def meters_running_behind(config)
+    config.meters.select {|meter| meter.last_validated_reading && meter.last_validated_reading < config.import_warning_days.days.ago}
+  end
+
+  def meters_with_blank_data(import_logs)
+    all_log_meters = import_logs.map do |log|
+      log.amr_data_feed_readings.select {|reading| reading.readings.blank? || reading.readings.all?(&:blank?)}.map(&:meter).compact
+    end
+    all_log_meters.flatten.uniq
+  end
+
+  def meters_with_zero_data(import_logs)
+    all_log_meters = import_logs.map do |log|
+      log.amr_data_feed_readings.select {|reading| reading.readings.present? && reading.readings.all? {|x48| x48.to_f == 0.0 rescue true}}.map(&:meter).compact
+    end
+    all_log_meters.flatten.uniq
   end
 end
