2cc8a1355205ae5b4d1d68a6c7fdf272614765b2
James Jefferies <james@shedcode.co.uk>
2019-12-30 18:07:58 +0000

    Switch on whether user can see invisible schools

--- spec/services/school_filter_spec.rb
+++ spec/services/school_filter_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe SchoolFilter do
 
-  let(:school_group_a)  { create(:school_group) }
-  let(:school_group_b)  { create(:school_group) }
-  let!(:school_1)       { create(:school, school_group: school_group_a) }
-  let!(:school_2)       { create(:school, school_group: school_group_b) }
-  let!(:school_no_data) { create(:school, school_group: school_group_a, process_data: false) }
+  let(:school_group_a)      { create(:school_group) }
+  let(:school_group_b)      { create(:school_group) }
+  let!(:school_1)           { create(:school, school_group: school_group_a) }
+  let!(:school_2)           { create(:school, school_group: school_group_b) }
+  let!(:school_3_invisible) { create(:school, school_group: school_group_b, visible: false) }
+  let!(:school_no_data)     { create(:school, school_group: school_group_a, process_data: false) }
 
   it 'returns all process data schools by default' do
     expect(SchoolFilter.new.filter).to eq [school_1, school_2]
@@ -16,5 +17,9 @@ describe SchoolFilter do
     expect(SchoolFilter.new(school_group_ids: [school_group_a.id]).filter).to eq [school_1]
     expect(SchoolFilter.new(school_group_ids: [school_group_a.id, school_group_b.id]).filter).to match_array [school_1, school_2]
     expect(SchoolFilter.new(school_group_ids: [school_group_b.id]).filter).to eq [school_2]
+  end
+
+  it 'filters by visible' do
+    expect(SchoolFilter.new(include_invisible: true).filter).to eq [school_1, school_2, school_3_invisible]
   end
 end
