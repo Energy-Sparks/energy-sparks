383ae5fd00829edca99251c3735c9a402635d2d4
James Jefferies <james@shedcode.co.uk>
2019-12-30 11:15:37 +0000

    Add CSV download

--- app/controllers/admin/school_onboardings_controller.rb
+++ app/controllers/admin/school_onboardings_controller.rb
@@ -3,9 +3,16 @@ module Admin
   class SchoolOnboardingsController < AdminController
     load_and_authorize_resource
 
+    INCOMPLETE_ONBOARDING_SCHOOLS_FILE_NAME = "incomplete-onboarding-schools.csv".freeze
+
     def index
       @school_groups = SchoolGroup.order(name: :asc)
       @completed_schools = @school_onboardings.order(:created_at).select(&:complete?)
+
+      respond_to do |format|
+        format.html
+        format.csv { send_data produce_csv, filename: INCOMPLETE_ONBOARDING_SCHOOLS_FILE_NAME }
+      end
     end
 
     def new
@@ -23,6 +30,17 @@ module Admin
 
 
   private
+
+    def produce_csv
+      CSV.generate do |csv|
+        csv << ['School name', 'School Group Name', 'Contact email', 'Notes', 'Last event']
+
+        @school_onboardings.order(:school_group_id).select(&:incomplete?).each do |school_onboarding|
+          last_event = school_onboarding.events.order(event: :desc).first.event.to_s.humanize
+          csv << [school_onboarding.school_name, school_onboarding.school_group.name, school_onboarding.contact_email, school_onboarding.notes, last_event]
+        end
+      end
+    end
 
     def school_onboarding_params
       params.require(:school_onboarding).permit(:school_name, :contact_email, :school_group_id, :notes)
