d462d7725afdc0bc20bcaf49bbab2a1f95d5d325
James Almond <james@jamesalmond.com>
2020-01-10 12:03:22 +0000

    Remove PDFS from asset paths

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,6 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "pdfs")
-
 Rails.application.config.assets.precompile += %w( application_mailer.scss )
 
 # Precompile additional assets.
