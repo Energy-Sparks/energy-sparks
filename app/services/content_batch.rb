a8fdb352aceb77cb8107246581c757d88f783237
James Almond <james@jamesalmond.com>
2020-01-22 14:17:32 +0000

    Split out S3 upload task

--- app/services/content_batch.rb
+++ app/services/content_batch.rb
@@ -39,8 +39,6 @@ class ContentBatch
 
       Rails.logger.info "Generated alert content"
 
-      AggregateSchoolService.save_to_s3(aggregate_school, bucket: ENV['AGGREGATE_SCHOOL_CACHE_BUCKET']) if ENV['AGGREGATE_SCHOOL_CACHE_BUCKET'].present?
-
     rescue StandardError => e
       Rails.logger.error "There was an error for #{school.name} - #{e.message}"
       Rollbar.error(e, school_id: school.id, school_name: school.name)
--- lib/tasks/content_batch.rake
+++ lib/tasks/content_batch.rake
@@ -2,6 +2,6 @@ namespace :content do
   desc "Validate readings"
   task batch: :environment do
     Rails.cache.clear
-    ContentBatch.new(School.process_data).generate
+    ContentBatch.new(School.process_data.order(:name)).generate
   end
 end
