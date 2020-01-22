5d0e54bfcfcf2951eae13b92d4b9f7529a4b9d76
James Jefferies <james@shedcode.co.uk>
2020-01-22 12:49:41 +0000

    Drill down content and controller

---
+++ app/controllers/analysis_page_finder_controller.rb
@@ -0,0 +1,20 @@
+class AnalysisPageFinderController < ApplicationController
+  skip_before_action :authenticate_user!
+  include AnalysisPages
+
+  def show
+    urn = params[:urn]
+    analysis_class = params[:analysis_class]
+
+    school = School.find_by(urn: urn)
+    alert_type = AlertType.find_by(class_name: analysis_class)
+
+    analysis_page = school.latest_analysis_pages.includes(:alert).detect { |page| page.alert.alert_type_id == alert_type.id }
+
+    if analysis_page
+      redirect_to school_analysis_path(school_id: school.slug, id: analysis_page.id)
+    else
+      redirect_back fallback_location: benchmarks_index, notice: "We couldn't take you to the correct location, sorry."
+    end
+  end
+end
