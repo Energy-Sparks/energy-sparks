100706227c33c87fe6cb688208188eac36e7e511
James Jefferies <james@shedcode.co.uk>
2020-01-29 09:41:59 +0000

    Update swap space setup script

--- .ebextensions/setup_swap.sh
+++ .ebextensions/setup_swap.sh
@@ -3,20 +3,35 @@
 SWAPFILE=/var/swapfile
 SWAP_MEGABYTES=1024
 
-if [ -f $SWAPFILE ]; then
-	echo "Swapfile $SWAPFILE found, assuming already setup"
+BIGSWAPFILE=/var/bigswapfile
+BIGSWAP_MEGABYTES=4096
+
+if [ -f $BIGSWAPFILE ]; then
+  echo "Big swapfile $BIGSWAPFILE found, assuming already setup"
   if [[ $(swapon -s) ]]
   then
-    echo "Swapfile $SWAPFILE found, and already setup"
+    echo "Big wapfile $SWAPFILE found, and already setup"
   else
-    echo "Swapfile $SWAPFILE found, but not enabled, enable now"
-    /sbin/swapon $SWAPFILE
+    echo "Big wapfile $SWAPFILE found, but not enabled, enable now"
+    /sbin/swapon $BIGSWAPFILE
   fi
-	exit;
+  exit;
 else
-  echo "No swapfile found, so create and setup"
-  /bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
-  /bin/chmod 600 $SWAPFILE
-  /sbin/mkswap $SWAPFILE
-  /sbin/swapon $SWAPFILE
+  echo "No big swapfile found, so create and setup"
+  /bin/dd if=/dev/zero of=$BIGSWAPFILE bs=1M count=$BIGSWAP_MEGABYTES
+  /bin/chmod 600 $BIGSWAPFILE
+  /sbin/mkswap $BIGSWAPFILE
+  /sbin/swapon $BIGSWAPFILE
+fi
+
+if [ -f $SWAPFILE ]; then
+  echo "Swapfile $SWAPFILE found, assuming already setup, swith off swap and delete"
+  if [[ $(swapon -s) ]]
+  then
+    echo "Swapfile $SWAPFILE found, switch off swap"
+    /sbin/swapoff $SWAPFILE
+  fi
+  echo "Swapfile $SWAPFILE found, not swapping, so delete"
+  /bin/rm -rf $SWAPFILE
+  exit;
 fi
