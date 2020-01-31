38b5f4e1c7d2158cfd88158be5fd402a9e3beb65
James Almond <james@jamesalmond.com>
2020-01-31 10:54:26 +0000

    Remove whitespace from schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_01_30_144801) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
