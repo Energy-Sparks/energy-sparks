9a50acc2925946ebd3639ba1e1f427d699791af0
James Almond <james@jamesalmond.com>
2020-02-24 16:40:58 +0000

    Allow onboarding configuration to be edited

--- app/controllers/admin/school_onboardings/configuration_controller.rb
+++ app/controllers/admin/school_onboardings/configuration_controller.rb
@@ -14,7 +14,11 @@ module Admin
 
       def update
         @school_onboarding.update!(school_params)
-        redirect_to new_admin_school_onboarding_email_path(@school_onboarding)
+        if @school_onboarding.has_event?(:email_sent)
+          redirect_to admin_school_onboardings_path
+        else
+          redirect_to new_admin_school_onboarding_email_path(@school_onboarding)
+        end
       end
 
     private
