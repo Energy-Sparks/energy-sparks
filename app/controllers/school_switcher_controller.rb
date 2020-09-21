859f16ec574d534dda2f28c1b8af8905c4aa3f01
Julian Higman <jhigman@pobox.com>
2020-09-21 22:21:58 +0100

    Redirect to school page after switching. Only allow switch to visible schools.

--- app/controllers/school_switcher_controller.rb
+++ app/controllers/school_switcher_controller.rb
@@ -1,12 +1,7 @@
 class SchoolSwitcherController < ApplicationController
   def create
-    from_school = current_user.school
-    to_school = School.find(params[:school_id])
-
-    current_user.update(school: to_school)
-
-    url = request.referer.gsub(from_school.slug, to_school.slug)
-
-    redirect_to url, notice: "Switched to #{to_school.name}"
+    school = School.find(params[:school_id])
+    current_user.update(school: school)
+    redirect_to school_path(school), notice: "Switched to #{school.name}"
   end
 end
