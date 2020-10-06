711ced99c291631c0f2ad22536c5184dd16d7e7e
Julian Higman <jhigman@pobox.com>
2020-10-06 20:47:07 +0100

    Refactor

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,21 +3,18 @@
 function setupSchoolGroupAndClusterControls() {
   $(".user_school_group_id").hide();
   $(".user_cluster_schools").hide();
-  var role = $("#user_role option:selected").val();
-  if (role) {
-    $(".user_staff_role_id").hide();
-    switch(role) {
-      case 'staff':
-        $(".user_staff_role_id").show();
-        break;
-      case 'school_admin':
-        $(".user_staff_role_id").show();
-        $(".user_cluster_schools").show();
-        break;
-      case 'group_admin':
-        $(".user_school_group_id").show();
-        break;
-    }
+  $(".user_staff_role_id").hide();
+  switch($("#user_role option:selected").val()) {
+    case 'staff':
+      $(".user_staff_role_id").show();
+      break;
+    case 'school_admin':
+      $(".user_staff_role_id").show();
+      $(".user_cluster_schools").show();
+      break;
+    case 'group_admin':
+      $(".user_school_group_id").show();
+      break;
   }
 }
 
@@ -25,8 +22,10 @@ $(document).ready(function() {
   $("body").on("change", ".user-update", function(ev) {
     $(this.form).submit();
   });
-  $("body").on("change", "#user_role", function(ev) {
+  if ($("#user_role").length) {
+    $("body").on("change", "#user_role", function(ev) {
+      setupSchoolGroupAndClusterControls();
+    });
     setupSchoolGroupAndClusterControls();
-  });
-  setupSchoolGroupAndClusterControls();
+  }
 });
