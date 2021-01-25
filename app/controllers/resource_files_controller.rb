d07ad1021ac877c2929b074f81a27caa1aa65752
Leigh Dodds <leigh@ldodds.com>
2021-01-25 16:04:42 +0000

    Spiked implementation of streaming resources

--- app/controllers/resource_files_controller.rb
+++ app/controllers/resource_files_controller.rb
@@ -4,4 +4,13 @@ class ResourceFilesController < ApplicationController
     @resource_file_types = ResourceFileType.order(:position)
     @other_resource_files = ResourceFile.where(resource_file_type_id: nil).order(:title)
   end
+
+  def download
+    resource = ResourceFile.find(params[:id])
+    response.headers["Content-Disposition"] = "#{params[:serve]}; filename=\"#{resource.file.filename}\""
+    response.headers["Content-Type"] = resource.file.content_type
+    resource.file.download do |chunk|
+      response.stream.write(chunk)
+    end
+  end
 end
