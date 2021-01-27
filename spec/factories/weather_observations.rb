ada56c2e8bae580874104ba8950067d700c7e085
Leigh Dodds <leigh@ldodds.com>
2021-01-27 11:45:48 +0000

    fix factory

--- spec/factories/weather_observations.rb
+++ spec/factories/weather_observations.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :weather_observation do
-    weather_station { nil }
-    reading_date { "2021-01-27" }
-    temperature_celsius_x48 { "9.99" }
+    weather_station
+    reading_date            { 1.month.ago }
+    temperature_celsius_x48 { Array.new(48, rand.to_f) }
   end
 end
