d749406743f800edc6c7d3daffb89e55f6be052d
Leigh Dodds <leigh@ldodds.com>
2021-01-27 14:29:37 +0000

    First implementation of loader

--- app/models/weather_station.rb
+++ app/models/weather_station.rb
@@ -13,9 +13,10 @@
 #  updated_at  :datetime         not null
 #
 class WeatherStation < ApplicationRecord
+  METEOSTAT = "meteostat".freeze
   has_many :weather_observations, dependent: :destroy
   validates_presence_of :latitude, :longitude, :title, :provider
-  validates :provider, inclusion: { in: ["meteostat"] }
+  validates :provider, inclusion: { in: [METEOSTAT] }
   scope :by_title, -> { order(title: :asc) }
   scope :active_by_provider, ->(provider) { where(active: true, provider: provider) }
   scope :by_provider, ->(provider) { where(provider: provider) }
