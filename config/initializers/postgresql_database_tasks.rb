c8c07f2047d8dbb14a9a8a184b749550d81f8ec7
Julian Higman <jhigman@pobox.com>
2021-02-11 17:40:07 +0000

    Config var for postgres tasks has changed

--- config/initializers/postgresql_database_tasks.rb
+++ config/initializers/postgresql_database_tasks.rb
@@ -5,8 +5,8 @@ module ActiveRecord
       def drop
         if Rails.env.development? || Rails.env.test?
           establish_master_connection
-          connection.select_all "select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname='#{configuration['database']}' AND state='idle';"
-          connection.drop_database configuration['database']
+          connection.select_all "select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname='#{db_config.database}' AND state='idle';"
+          connection.drop_database(db_config.database)
         end
       end
     end
