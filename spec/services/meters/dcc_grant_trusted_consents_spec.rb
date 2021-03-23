f9df2af861fcdeccca8c01806e7e8f297ae5bbaa
Julian Higman <jhigman@pobox.com>
2021-03-23 11:23:28 +0000

    Typo

--- spec/services/meters/dcc_grant_trusted_consents_spec.rb
+++ spec/services/meters/dcc_grant_trusted_consents_spec.rb
@@ -9,7 +9,7 @@ module Meters
     let(:meter)  { create(:electricity_meter, dcc_meter: true) }
     let(:meter_review)  { create(:meter_review, meters: [meter]) }
 
-    it "should set call consent api and set consent_granted flag" do
+    it "should call consent api and set consent_granted flag" do
       expect(n3rgy_api).to receive(:grant_trusted_consent).with(meter.mpan_mprn, meter_review.consent_grant.guid).and_return(true)
       Meters::DccGrantTrustedConsents.new([meter], n3rgy_api_factory).perform
       expect(meter.reload.consent_granted).to be_truthy
