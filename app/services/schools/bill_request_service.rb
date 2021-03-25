0963ef59686aba2f0ca2cc2391c7c73f9082b440
Leigh Dodds <leigh@ldodds.com>
2021-03-25 11:14:43 +0000

    Use enums for filtering

--- app/services/schools/bill_request_service.rb
+++ app/services/schools/bill_request_service.rb
@@ -10,8 +10,8 @@ module Schools
     end
 
     def request_documentation!(users, meters = [])
-      electricity_meters = meters.select {|m| m.meter_type.to_s == "electricity"}
-      gas_meters = meters.select {|m| m.meter_type.to_s == "gas"}
+      electricity_meters = meters.select(&:electricity?)
+      gas_meters = meters.select(&:gas?)
       BillRequestMailer.with(emails: users.map(&:email), school: @school, electricity_meters: electricity_meters, gas_meters: gas_meters, subject: "Please upload a recent energy bill to Energy Sparks").request_bill.deliver_now
     end
   end
