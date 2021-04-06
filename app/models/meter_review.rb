baf855274bcd5f4f8a936afff9ef4c010f96a9e2
Leigh Dodds <leigh@ldodds.com>
2021-04-06 15:37:36 +0100

    Add public flags to models

--- app/models/meter_review.rb
+++ app/models/meter_review.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: meter_reviews
+#
+#  consent_grant_id :bigint(8)        not null
+#  created_at       :datetime         not null
+#  id               :bigint(8)        not null, primary key
+#  school_id        :bigint(8)        not null
+#  updated_at       :datetime         not null
+#  user_id          :bigint(8)        not null
+#
+# Indexes
+#
+#  index_meter_reviews_on_consent_grant_id  (consent_grant_id)
+#  index_meter_reviews_on_school_id         (school_id)
+#  index_meter_reviews_on_user_id           (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (consent_grant_id => consent_grants.id)
+#  fk_rails_...  (school_id => schools.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class MeterReview < ApplicationRecord
   belongs_to :school
   belongs_to :user
