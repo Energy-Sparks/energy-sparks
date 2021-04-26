5a2df597b0ecf321f2f33db3e45445f4d9228ded
Julian Higman <jhigman@pobox.com>
2021-04-26 18:12:51 +0100

    Refactored renamed

--- app/services/amr/n3rgy_readings_download_and_upsert.rb
+++ app/services/amr/n3rgy_readings_download_and_upsert.rb
@@ -15,27 +15,29 @@ module Amr
     end
 
     def perform
-      @import_log = create_import_log
-      n3rgy_api = @n3rgy_api_factory.data_api(@meter)
-
       unless @start_date && @end_date
         available_dates = n3rgy_api.readings_available_date_range(@meter.mpan_mprn, @meter.fuel_type)
         current_dates = readings_current_date_range(@meter)
       end
 
-      start_date = @start_date || from_date(available_dates, current_dates)
-      end_date = @end_date || to_date(available_dates)
+      start_date = @start_date || Amr::N3rgyDownloaderDates.start_date(available_dates, current_dates)
+      end_date = @end_date || Amr::N3rgyDownloaderDates.end_date(available_dates)
 
+      import_log = create_import_log
       readings = N3rgyDownloader.new(meter: @meter, start_date: start_date, end_date: end_date, n3rgy_api: n3rgy_api).readings
-      N3rgyReadingsUpserter.new(meter: @meter, config: @config, readings: readings, import_log: @import_log).perform
+      N3rgyReadingsUpserter.new(meter: @meter, config: @config, readings: readings, import_log: import_log).perform
     rescue => e
-      @import_log.update!(error_messages: "Error downloading data from #{start_date} to #{end_date} : #{e.message}")
+      import_log.update!(error_messages: "Error downloading data from #{start_date} to #{end_date} : #{e.message}")
       Rails.logger.error "Exception: downloading N3rgy data for #{@meter.mpan_mprn} from #{start_date} to #{end_date} : #{e.class} #{e.message}"
       Rails.logger.error e.backtrace.join("\n")
       Rollbar.error(e, job: :n3rgy_download, meter_id: @meter.mpan_mprn, start_date: start_date, end_date: end_date)
     end
 
     private
+
+    def n3rgy_api
+      @n3rgy_api ||= @n3rgy_api_factory.data_api(@meter)
+    end
 
     def create_import_log
       AmrDataFeedImportLog.create(
@@ -50,29 +52,6 @@ module Amr
         last = meter.amr_data_feed_readings.maximum(:reading_date)
         (Date.parse(first)..Date.parse(last))
       end
-    end
-
-    def from_date(available_range, current_range)
-      from = default_start_date
-      if available_range
-        from = available_range.first
-      end
-      if current_range && current_range.first <= from
-        from = current_range.last
-      end
-      from
-    end
-
-    def to_date(available_range)
-      available_range ? available_range.last : default_end_date
-    end
-
-    def default_start_date
-      Time.zone.today - 13.months
-    end
-
-    def default_end_date
-      Time.zone.today - 1
     end
   end
 end
