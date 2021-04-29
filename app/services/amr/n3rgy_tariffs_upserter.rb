1a420b75dc510c5390419f511c1604fa8de635ff
Julian Higman <jhigman@pobox.com>
2021-04-29 17:10:36 +0100

    Move transaction to include deletes

--- app/services/amr/n3rgy_tariffs_upserter.rb
+++ app/services/amr/n3rgy_tariffs_upserter.rb
@@ -11,10 +11,12 @@ module Amr
     def perform
       return if @tariffs.empty?
       Rails.logger.info "Deleting #{@meter.tariff_prices.count} tariff_prices and #{@meter.tariff_standing_charges.count} tariff_standing_charges for #{@meter.mpan_mprn} at #{@meter.school.name}"
-      @meter.tariff_prices.delete_all
-      @meter.tariff_standing_charges.delete_all
-      Rails.logger.info "Upserting #{kwh_tariffs} for #{@meter.mpan_mprn} at #{@meter.school.name}"
-      TariffUpserter.new(prices_array(kwh_tariffs), standing_charges_array(standing_charges), @import_log).perform
+      Rails.logger.info "Upserting #{kwh_tariffs.count} tariff_prices and #{standing_charges.count} tariff_standing_charges for #{@meter.mpan_mprn} at #{@meter.school.name}"
+      ActiveRecord::Base.transaction do
+        @meter.tariff_prices.delete_all
+        @meter.tariff_standing_charges.delete_all
+        TariffUpserter.new(prices_array(kwh_tariffs), standing_charges_array(standing_charges), @import_log).perform
+      end
       Rails.logger.info "Upserted #{@import_log.prices_updated} prices and #{@import_log.standing_charges_updated} standing charges for #{@meter.mpan_mprn} at #{@meter.school.name}"
       Rails.logger.info "Inserted #{@import_log.prices_imported} prices and #{@import_log.standing_charges_imported} standing charges for #{@meter.mpan_mprn} at #{@meter.school.name}"
     end
