04b08b64cf840a3e96bf376cd281971ad85cf74c
Julian Higman <jhigman@pobox.com>
2021-05-04 16:33:28 +0100

    Specify float for standing charge values

--- app/models/tariff_standing_charge.rb
+++ app/models/tariff_standing_charge.rb
@@ -22,4 +22,6 @@ class TariffStandingCharge < ApplicationRecord
 
   belongs_to :meter, inverse_of: :tariff_standing_charges
   belongs_to :tariff_import_log, inverse_of: :tariff_standing_charges
+
+  attribute :value, :float
 end
