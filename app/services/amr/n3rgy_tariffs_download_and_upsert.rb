fadcc9297aeec95c4739a1c2809adff1a9130e5e
Julian Higman <jhigman@pobox.com>
2021-05-10 14:22:01 +0100

    Guard against missing import log (#1342)

--- app/services/amr/n3rgy_tariffs_download_and_upsert.rb
+++ app/services/amr/n3rgy_tariffs_download_and_upsert.rb
@@ -14,7 +14,7 @@ module Amr
       tariffs = N3rgyDownloader.new(meter: @meter, start_date: start_date, end_date: end_date, n3rgy_api: n3rgy_api).tariffs
       N3rgyTariffsUpserter.new(meter: @meter, tariffs: tariffs, import_log: import_log).perform
     rescue => e
-      import_log.update!(error_messages: "Error downloading tariffs from #{start_date} to #{end_date} : #{e.message}")
+      import_log.update!(error_messages: "Error downloading tariffs from #{start_date} to #{end_date} : #{e.message}") if import_log
       Rails.logger.error "Exception: downloading N3rgy tariffs for #{@meter.mpan_mprn} from #{start_date} to #{end_date} : #{e.class} #{e.message}"
       Rails.logger.error e.backtrace.join("\n")
       Rollbar.error(e, job: :n3rgy_download, meter_id: @meter.mpan_mprn, start_date: start_date, end_date: end_date)
