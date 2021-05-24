05df3c675f4c3e30969a4af4efeb501a2153f33c
Julian Higman <jhigman@pobox.com>
2021-05-24 14:20:30 +0100

    Expanded system spec

--- spec/system/admin/school_removal_spec.rb
+++ spec/system/admin/school_removal_spec.rb
@@ -2,24 +2,48 @@ require 'rails_helper'
 
 RSpec.describe "school removal", :schools, type: :system do
 
-  let(:school) { create(:school, name: 'My High School', visible: false) }
-  let(:admin)  { create(:admin)}
+  let(:admin)  { create(:admin) }
 
   before(:each) do
     sign_in(admin)
   end
 
-  it 'removes school and shows on removals list' do
-    visit school_path(school)
-    click_on 'Remove school'
-    expect(page).to have_content('My High School REMOVAL')
-    click_button 'Remove school'
-    expect(page).to have_content('School has been removed')
-    school.reload
-    expect(school.active).to be_falsey
+  context 'if school still visible' do
 
-    visit admin_reports_path
-    click_on 'Schools removed'
-    expect(page).to have_content('My High School')
+    let(:school) { create(:school, name: 'My High School', visible: true) }
+
+    it 'shows message' do
+      visit school_path(school)
+      click_on 'Remove school'
+      expect(page).to have_content('My High School REMOVAL')
+      expect(page).to have_content('School cannot be removed while still visible')
+    end
+  end
+
+  context 'if school not visible' do
+
+    let(:school) { create(:school, name: 'My High School', visible: false) }
+    let!(:school_admin)  { create(:school_admin, school: school) }
+    let!(:electricity_meter)  { create(:electricity_meter, school: school) }
+
+    it 'removes school and shows on removals list' do
+      visit school_path(school)
+      click_on 'Remove school'
+      expect(page).to have_content('My High School REMOVAL')
+      click_button 'Remove users'
+      expect(page).to have_content('Users have been deactivated')
+      click_button 'Remove meters'
+      expect(page).to have_content('Meters have been deactivated')
+      click_button 'Remove school'
+      expect(page).to have_content('School has been removed')
+      expect(page).to have_content('My High School (INACTIVE)')
+
+      visit admin_reports_path
+      click_on 'Schools removed'
+
+      expect(page).to have_content('My High School')
+      expect(page).to have_content(Time.zone.today.to_s(:es_full))
+      expect(page).to have_link(electricity_meter.mpan_mprn)
+    end
   end
 end
