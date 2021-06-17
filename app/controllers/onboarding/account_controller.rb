0948d2fb6cb75bc82697c46f730aa5cc388a2000
Leigh Dodds <leigh@ldodds.com>
2021-06-17 11:58:53 +0100

    Ensure newsletter field is properly set, add context

--- app/controllers/onboarding/account_controller.rb
+++ app/controllers/onboarding/account_controller.rb
@@ -11,13 +11,13 @@ module Onboarding
     end
 
     def create
-      @user = User.new_school_onboarding(user_params.except(:subscribe_to_newsletter))
+      @user = User.new_school_onboarding(user_params)
       if @user.save
         @school_onboarding.update!(created_user: @user)
         @school_onboarding.events.create!(event: :onboarding_user_created)
         @school_onboarding.events.create!(event: :privacy_policy_agreed)
         sign_in(@user, scope: :user)
-        @school_onboarding.update!(subscribe_to_newsletter: user_params[:subscribe_to_newsletter] == 'on')
+        @school_onboarding.update!(subscribe_to_newsletter: newsletter_params[:subscribe_to_newsletter])
         redirect_to new_onboarding_school_details_path(@school_onboarding)
       else
         render :new
@@ -30,7 +30,7 @@ module Onboarding
     def update
       if current_user.update(user_params.reject {|key, value| key =~ /password/ && value.blank?}.except(:subscribe_to_newsletter))
         @school_onboarding.events.create!(event: :onboarding_user_updated)
-        @school_onboarding.update!(subscribe_to_newsletter: user_params[:subscribe_to_newsletter] == 'on')
+        @school_onboarding.update!(subscribe_to_newsletter: newsletter_params[:subscribe_to_newsletter])
         bypass_sign_in(current_user)
         redirect_to new_onboarding_completion_path(@school_onboarding)
       else
@@ -47,7 +47,11 @@ module Onboarding
     end
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :staff_role_id, :subscribe_to_newsletter)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :staff_role_id)
+    end
+
+    def newsletter_params
+      params.require(:newsletter).permit(:subscribe_to_newsletter)
     end
   end
 end
