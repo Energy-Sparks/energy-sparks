54fd65ae897f1b9932bfab14237b51a55d80c0c7
Julian Higman <jhigman@pobox.com>
2021-06-28 16:28:28 +0100

    Add gas tariffs

--- app/controllers/schools/user_tariffs_controller.rb
+++ app/controllers/schools/user_tariffs_controller.rb
@@ -4,17 +4,22 @@ module Schools
     load_and_authorize_resource :user_tariff
 
     def index
-      @user_tariffs = @school.user_tariffs.by_name
+      @electricity_tariffs = @school.user_tariffs.electricity.by_name
+      @gas_tariffs = @school.user_tariffs.gas.by_name
     end
 
     def new
-      @user_tariff = @school.user_tariffs.build(user_tariff_params.merge(start_date: Date.parse('2021-04-01'), end_date: Date.parse('2022-03-31')))
+      @user_tariff = @school.user_tariffs.build(user_tariff_params.merge(default_params))
     end
 
     def create
       @user_tariff = @school.user_tariffs.build(user_tariff_params)
       if @user_tariff.save
-        redirect_to choose_type_school_user_tariff_path(@school, @user_tariff)
+        if @user_tariff.gas?
+          redirect_to school_user_tariff_user_tariff_prices_path(@school, @user_tariff)
+        else
+          redirect_to choose_type_school_user_tariff_path(@school, @user_tariff)
+        end
       else
         render :new
       end
@@ -50,6 +55,10 @@ module Schools
     end
 
     private
+
+    def default_params
+      { start_date: Date.parse('2021-04-01'), end_date: Date.parse('2022-03-31'), flat_rate: true }
+    end
 
     def user_tariff_params
       params.require(:user_tariff).permit(:fuel_type, :name, :start_date, :end_date, :flat_rate, :vat_rate, meter_ids: [])
--- spec/system/schools/user_tariffs_spec.rb
+++ spec/system/schools/user_tariffs_spec.rb
@@ -5,7 +5,7 @@ describe 'user tariffs', type: :system do
   let!(:school)                   { create_active_school(name: "Big School")}
   let!(:admin)                    { create(:admin) }
   let!(:electricity_meter)        { create(:electricity_meter, school: school, mpan_mprn: '12345678901234') }
-  let!(:gas_meter)                      { create(:gas_meter, school: school, mpan_mprn: '999888777') }
+  let!(:gas_meter)                { create(:gas_meter, school: school, mpan_mprn: '999888777') }
 
   context 'as a school admin' do
     let!(:school_admin)                    { create(:school_admin, school: school) }
@@ -30,6 +30,63 @@ describe 'user tariffs', type: :system do
 
     before(:each) do
       sign_in(admin)
+    end
+
+    context 'creating flat rate gas tariffs' do
+
+      it 'can create a tariff and add prices and charges' do
+        visit school_path(school)
+        click_link('Manage tariffs')
+
+        expect(page).to have_content('All tariffs')
+
+        click_link('Add gas tariff')
+
+        expect(page).to have_content('Select meters for tariff')
+        check('999888777')
+        click_button('Next')
+
+        expect(page).to have_content('Add gas tariff')
+
+        fill_in 'Name', with: 'My First Gas Tariff'
+        select '5%', from: 'VAT rate'
+        click_button('Next')
+
+        expect(page).to have_content('Energy charges')
+        expect(page).to have_content('My First Gas Tariff gas for 2021-04-01 to 2022-03-31')
+
+        fill_in 'Rate in £/kWh', with: '1.5'
+        click_button('Next')
+
+        expect(page).to have_content('Standing charges')
+        expect(page).to have_content('My First Gas Tariff gas for 2021-04-01 to 2022-03-31')
+
+        click_link('Add standing charge')
+        select 'Fixed charge', from: 'Charge type'
+        fill_in 'Value', with: '4.56'
+        choose 'kVA'
+        click_button('Save')
+
+        click_link('Next')
+        expect(page).to have_content('Review tariff')
+        expect(page).to have_content('VAT rate: 5%')
+        expect(page).to have_content('Flat rate tariff: £1.50 per kWh')
+        expect(page).to have_content('£4.56 per kVA')
+        expect(page).not_to have_link('Delete')
+
+        click_link('Finished')
+        expect(page).to have_content('All tariffs')
+        expect(page).to have_content('999888777')
+        expect(page).to have_content('VAT rate: 5%')
+
+        user_tariff = UserTariff.last
+        expect(user_tariff.meters).to match_array([gas_meter])
+        expect(user_tariff.vat_rate).to eq('5%')
+        user_tariff_price = user_tariff.user_tariff_prices.first
+        expect(user_tariff_price.start_time.to_s(:time)).to eq('00:00')
+        expect(user_tariff_price.end_time.to_s(:time)).to eq('23:30')
+        expect(user_tariff_price.units).to eq('kwh')
+      end
     end
 
     context 'creating flat rate electricity tariffs' do
