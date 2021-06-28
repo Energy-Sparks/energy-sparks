a7083c79e68a5c2bf4f31a1793a8615f6df96008
Julian Higman <jhigman@pobox.com>
2021-06-28 10:34:21 +0100

    Fixed schema and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_122050) do
+ActiveRecord::Schema.define(version: 2021_06_24_172224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
