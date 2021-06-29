953a036afec458e4157e30405f91e489b2a2d82c
Julian Higman <jhigman@pobox.com>
2021-06-29 12:57:38 +0100

    Add gas tariffs (#1378)
    * Add gas tariffs
    
    * Only show relevant charge type units
    
    * Added duos
    
    * Removed climate change charge
    
    * Fix js for edit of charge

--- app/helpers/tariffs_helper.rb
+++ app/helpers/tariffs_helper.rb
@@ -1,9 +1,21 @@
 module TariffsHelper
-  def charge_type_humanized(charge_type)
-    UserTariffCharge::CHARGE_TYPES[charge_type.to_sym]
+  def user_tariff_charge_types
+    UserTariffCharge::CHARGE_TYPES.keys.map {|k| [k.to_s.humanize, k]}
   end
 
-  def charge_type_units_humanized(charge_type_units)
+  def user_tariff_charge_type_units
+    UserTariffCharge::CHARGE_TYPE_UNITS.map {|k, v| [v, k]}
+  end
+
+  def user_tariff_charge_type_humanized(charge_type)
+    charge_type.humanize
+  end
+
+  def user_tariff_charge_type_units_humanized(charge_type_units)
     UserTariffCharge::CHARGE_TYPE_UNITS[charge_type_units.to_sym]
+  end
+
+  def user_tariff_charge_type_units_as_json
+    UserTariffCharge::CHARGE_TYPES.to_json
   end
 end
