da2c18ebdb299794e7401e49668ac10f80a33d85
Julian Higman <jhigman@pobox.com>
2021-07-05 17:46:11 +0100

    Shared tables and layouts

--- app/controllers/schools/user_tariff_charges_controller.rb
+++ app/controllers/schools/user_tariff_charges_controller.rb
@@ -7,14 +7,6 @@ module Schools
       @user_tariff_charges = @user_tariff.user_tariff_charges
     end
 
-    def new
-      @user_tariff_charge = @user_tariff.user_tariff_charges.build
-      respond_to do |format|
-        format.html
-        format.js
-      end
-    end
-
     def create
       @user_tariff_charges = make_charges(params[:user_tariff_charges], @user_tariff)
       @user_tariff_charges.map(&:valid?)
@@ -28,32 +20,6 @@ module Schools
       else
         render :index
       end
-    end
-
-    def edit
-      @user_tariff_charge = @user_tariff.user_tariff_charges.find(params[:id])
-      respond_to do |format|
-        format.html
-        format.js
-      end
-    end
-
-    def update
-      @user_tariff_charge = @user_tariff.user_tariff_charges.find(params[:id])
-      respond_to do |format|
-        if @user_tariff_charge.update(user_tariff_charge_params)
-          format.html { redirect_to school_user_tariff_user_tariff_charges_path(@school, @user_tariff) }
-          format.js
-        else
-          format.html { render :edit }
-          format.js { render :edit }
-        end
-      end
-    end
-
-    def destroy
-      @user_tariff.user_tariff_charges.find(params[:id]).destroy
-      redirect_to school_user_tariff_user_tariff_charges_path(@school, @user_tariff)
     end
 
     private
