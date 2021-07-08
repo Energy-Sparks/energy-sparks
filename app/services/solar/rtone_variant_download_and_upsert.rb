7b5765e2557f90e1525f5d42c138e423ccd2e8f9
Leigh Dodds <leigh@ldodds.com>
2021-07-08 15:13:06 +0100

    Add tests for download/upsert. Move logic from .task file

--- app/services/solar/rtone_variant_download_and_upsert.rb
+++ app/services/solar/rtone_variant_download_and_upsert.rb
@@ -6,16 +6,51 @@ module Solar
         end_date:
       )
       @rtone_variant_installation = rtone_variant_installation
-      @start_date = start_date
-      @end_date = end_date
+      @requested_start_date = start_date
+      @requested_end_date = end_date
+      @import_log = create_import_log
     end
 
     def perform
-      readings = RtoneVariantDownloader.new(rtone_variant_installation: @rtone_variant_installation, start_date: @start_date, end_date: @end_date, low_carbon_hub_api: low_carbon_hub_api).readings
-      RtoneVariantUpserter.new(rtone_variant_installation: @rtone_variant_installation, readings: readings).perform
+      readings = RtoneVariantDownloader.new(rtone_variant_installation: @rtone_variant_installation, start_date: start_date, end_date: end_date, low_carbon_hub_api: low_carbon_hub_api).readings
+
+      RtoneVariantUpserter.new(rtone_variant_installation: @rtone_variant_installation, readings: readings, import_log: @import_log).perform
+    rescue => e
+      @import_log.update!(error_messages: "Error downloading data from #{start_date} to #{end_date} : #{e.message}")
+      Rails.logger.error "Exception: downloading Rtone data for #{meter.mpan_mprn} from #{start_date} to #{end_date} : #{e.class} #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      Rollbar.error(e, job: :rtone_variant_download, school: @rtone_variant_installation.school.name, meter_id: meter.mpan_mprn, start_date: start_date, end_date: end_date)
     end
 
     private
+
+    def start_date
+      default_start_date = Date.yesterday - 5
+      if @requested_start_date
+        @requested_start_date
+      else
+        latest_reading && latest_reading < default_start_date ? latest_reading : default_start_date
+      end
+    end
+
+    def end_date
+      @requested_end_date.present? ? @requested_end_date : Date.yesterday
+    end
+
+    def meter
+      @rtone_variant_installation.meter
+    end
+
+    def latest_reading
+      @rtone_variant_installation.latest_electricity_reading
+    end
+
+    def create_import_log
+      AmrDataFeedImportLog.create(
+        amr_data_feed_config: @rtone_variant_installation.amr_data_feed_config,
+        file_name: "Rtone Variant API import #{DateTime.now.utc}",
+        import_time: DateTime.now.utc)
+    end
 
     def low_carbon_hub_api
       @low_carbon_hub_api ||= LowCarbonHubMeterReadings.new(username, password)
--- app/services/solar/rtone_variant_upserter.rb
+++ app/services/solar/rtone_variant_upserter.rb
@@ -2,35 +2,40 @@ require 'dashboard'
 
 module Solar
   class RtoneVariantUpserter
-    def initialize(rtone_variant_installation:, readings:)
+    def initialize(rtone_variant_installation:, readings:, import_log:)
       @rtone_variant_installation = rtone_variant_installation
       @readings = readings
-      @amr_data_feed_config = @rtone_variant_installation.amr_data_feed_config
-      @amr_data_feed_import_log = AmrDataFeedImportLog.create(amr_data_feed_config_id: @amr_data_feed_config.id, file_name: "Rtone Variant API import #{DateTime.now.utc}", import_time: DateTime.now.utc)
+      @import_log = import_log
     end
 
     def perform
       Rails.logger.info "Upserting #{@readings.count} for #{@rtone_variant_installation.rtone_meter_id} at #{@rtone_variant_installation.school.name}"
 
-      readings_hash = @readings[:readings]
-      meter = @rtone_variant_installation.meter
+      Amr::DataFeedUpserter.new(data_feed_reading_array(@readings[:readings]), @import_log).perform
 
-      Amr::DataFeedUpserter.new(data_feed_reading_array(readings_hash, meter.id, meter.mpan_mprn), @amr_data_feed_import_log).perform
-      Rails.logger.info "Upserted #{@amr_data_feed_import_log.records_updated} inserted #{@amr_data_feed_import_log.records_imported}for #{@rtone_variant_installation.rtone_meter_id} at #{@rtone_variant_installation.school.name}"
+      Rails.logger.info "Upserted #{@import_log.records_updated} inserted #{@import_log.records_imported}for #{@rtone_variant_installation.rtone_meter_id} at #{@rtone_variant_installation.school.name}"
     end
 
     private
 
-    def data_feed_reading_array(readings_hash, meter_id, mpan_mprn)
+    def data_feed_reading_array(readings_hash)
       readings_hash.map do |reading_date, one_day_amr_reading|
         {
-          amr_data_feed_config_id: @amr_data_feed_config.id,
+          amr_data_feed_config_id: @rtone_variant_installation.amr_data_feed_config.id,
           meter_id: meter_id,
           mpan_mprn: mpan_mprn,
           reading_date: reading_date,
           readings: one_day_amr_reading.kwh_data_x48
         }
       end
+    end
+
+    def meter_id
+      @rtone_variant_installation.meter.id
+    end
+
+    def mpan_mprn
+      @rtone_variant_installation.meter.mpan_mprn
     end
   end
 end
