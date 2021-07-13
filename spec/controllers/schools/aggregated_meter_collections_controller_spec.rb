03a441b3bf74acfaf9e4ca28c8455c30bec12866
Leigh Dodds <leigh@ldodds.com>
2021-07-13 12:45:05 +0100

    Add tests for other user types

--- spec/controllers/schools/aggregated_meter_collections_controller_spec.rb
+++ spec/controllers/schools/aggregated_meter_collections_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Schools::AggregatedMeterCollectionsController, type: :controller do
 
-  let(:admin)   { create(:admin) }
-
   let(:school)             { create(:school, visible: visible, public: public)}
 
   context "as an admin" do
+    let(:admin)   { create(:admin) }
+
     before(:each) do
       sign_in(:admin)
     end
@@ -49,8 +49,50 @@ RSpec.describe Schools::AggregatedMeterCollectionsController, type: :controller 
           expect(response).to have_http_status(200)
         end
       end
-
     end
 
+  end
+
+  context "as a guest" do
+    describe 'with visible school' do
+      let(:visible) { true }
+
+      context "that is public" do
+        let(:public)  { true }
+        it "can trigger a load" do
+          post :post, format: :json, params: { school_id: school.id }
+          expect(response).to have_http_status(200)
+        end
+      end
+
+    end
+  end
+
+  context "as a teacher" do
+    let(:teacher)               { create(:school_admin, school: school)}
+
+    before(:each) do
+      sign_in(:teacher)
+    end
+
+    describe 'with visible school' do
+      let(:visible) { true }
+
+      context "that is public" do
+        let(:public)  { true }
+        it "can trigger a load" do
+          post :post, format: :json, params: { school_id: school.id }
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context "that is private" do
+        let(:public)  { false }
+        it "can trigger a load" do
+          post :post, format: :json, params: { school_id: school.id }
+          expect(response).to have_http_status(200)
+        end
+      end
+    end
   end
 end
