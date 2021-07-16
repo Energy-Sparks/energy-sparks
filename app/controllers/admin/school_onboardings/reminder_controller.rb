f254bbb6a102bcb2ac6218c0dac6ce61f1830cde
Julian Higman <jhigman@pobox.com>
2021-07-16 13:04:07 +0100

    1436 improve usability of automatic onboarding page for site admin (#1413)
    * Move completed onboardings to separate page, fix table sorting
    
    * Added clickable list of groups
    
    * Test for acnhor in reminder redirect
    
    * Anchor from configuration edit
    
    * Tidy

--- app/controllers/admin/school_onboardings/reminder_controller.rb
+++ app/controllers/admin/school_onboardings/reminder_controller.rb
@@ -6,7 +6,7 @@ module Admin
       def create
         OnboardingMailer.with(school_onboarding: @school_onboarding).reminder_email.deliver_now
         @school_onboarding.events.create!(event: :reminder_sent)
-        redirect_to admin_school_onboardings_path
+        redirect_to admin_school_onboardings_path(anchor: @school_onboarding.page_anchor)
       end
     end
   end
