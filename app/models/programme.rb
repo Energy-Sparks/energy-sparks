33ab146dd321a28e4fe81eeb0de553eae4ff24d1
Julian Higman <jhigman@pobox.com>
2021-09-07 16:02:10 +0100

    Show activities done and link

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -32,4 +32,8 @@ class Programme < ApplicationRecord
   scope :active, -> { joins(:programme_type).merge(ProgrammeType.active) }
 
   delegate :title, :description, :short_description, :document_link, :image, to: :programme_type
+
+  def activity_of_type(activity_type)
+    activities.where(activity_type: activity_type).last
+  end
 end
