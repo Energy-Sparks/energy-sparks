644d413dcde9d6a6f9cbd586b542c09f776e9025
Leigh Dodds <leigh@ldodds.com>
2021-09-08 12:24:33 +0100

    Add report for admins

--- config/routes.rb
+++ config/routes.rb
@@ -309,6 +309,7 @@ Rails.application.routes.draw do
       resources :tariff_import_logs, only: [:index]
       resources :amr_reading_warnings, only: [:index]
       resources :activities, only: :index
+      resources :interventions, only: :index
     end
 
     resource :settings, only: [:show, :update]
