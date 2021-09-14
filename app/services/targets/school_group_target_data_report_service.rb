ceb57f0914dc89fda7ab9f2190c59e0a930c3e4d
Leigh Dodds <leigh@ldodds.com>
2021-09-14 16:43:59 +0100

    Initial test script

--- app/services/targets/school_group_target_data_report_service.rb
+++ app/services/targets/school_group_target_data_report_service.rb
@@ -21,6 +21,68 @@ module Targets
       report
     end
 
+    #returns hash school => hash(fuel_type => results)
+    def test_targets
+      report = {}
+      fuel_type_report = {
+        enough_data: false,
+        success: false,
+        progress: nil,
+        error: nil
+      }
+      schools.each do |school|
+        report[school] = {
+          electricity: fuel_type_report.dup,
+          gas: fuel_type_report.dup,
+          storage_heaters: fuel_type_report.dup
+        }
+        begin
+          unless school.has_target?
+            target = Targets::SchoolTargetService.new(school).build_target
+            target.save!
+          end
+
+          #Create meter collection without hitting the cache
+          aggregate_school = Amr::AnalyticsMeterCollectionFactory.new(school).validated
+          AggregateDataService.new(aggregate_school).aggregate_heat_and_electricity_meters
+
+          #Loop through each fuel type
+          # rubocop:disable Performance/CollectionLiteralInLoop
+          [:electricity, :gas, :storage_heaters].each do |fuel_type|
+            target_service = ::TargetsService.new(aggregate_school, fuel_type)
+            begin
+              #If school has that fuel type and there's enough data
+              if school.send("has_#{fuel_type}?".to_sym) && target_service.enough_data_to_set_target?
+                #record we have enough data
+                report[school][fuel_type][:enough_data] = true
+                report[school][fuel_type][:recent_data] = target_service.recent_data?
+                #request the latest cumulative performance
+                progress = target_service.progress.current_cumulative_performance
+                #record success and figure
+                report[school][fuel_type].merge!(success: true, progress: progress)
+              end
+            rescue => e
+              puts "#{school.name} - #{fuel_type}"
+              puts e.message
+              puts e.backtrace
+              report[school][fuel_type][:error] = e.message
+              Rails.logger.error "Unable to generate report for #{school.name}: #{e.message}"
+              Rails.logger.error e.backtrace.join("\n")
+              Rollbar.error(e, job: :test_targets, school: school)
+            end
+          end
+          # rubocop:enable Performance/CollectionLiteralInLoop
+        rescue => e
+          puts e.message
+          puts e.backtrace
+          Rails.logger.error "Unable to generate report for #{school.name}: #{e.message}"
+          Rails.logger.error e.backtrace.join("\n")
+          Rollbar.error(e, job: :test_targets, school: school)
+        end
+      end
+      report
+    end
+
     private
 
     def schools
