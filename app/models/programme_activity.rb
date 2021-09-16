b6b58227aab71586c2eb35d923197e960fed9908
Leigh Dodds <leigh@ldodds.com>
2021-09-16 14:11:24 +0100

    Change how we use programme_activities

--- app/models/programme_activity.rb
+++ app/models/programme_activity.rb
@@ -16,8 +16,6 @@
 
 class ProgrammeActivity < ApplicationRecord
   belongs_to :activity_type
-  belongs_to :activity, optional: true
+  belongs_to :activity, optional: true #no longer optional, but existing data has this
   belongs_to :programme
-
-  delegate :name, to: :activity_type
 end
