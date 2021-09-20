31c6bee26a9c28ab0a274563fc11b53eb62a67f3
Julian Higman <jhigman@pobox.com>
2021-09-20 11:25:17 +0100

    Updated ability for pupils to start programmes (#1498)

--- spec/system/programme_type_spec.rb
+++ spec/system/programme_type_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "programme types", type: :system, include_application_helper: tru
 
   let!(:school) { create(:school)}
   let!(:school_admin) { create(:school_admin, school: school)}
+  let!(:pupil) { create(:pupil, school: school)}
 
   let!(:programme_type_1) { create(:programme_type_with_activity_types)}
   let!(:programme_type_2) { create(:programme_type, active: false)}
@@ -58,6 +59,72 @@ RSpec.describe "programme types", type: :system, include_application_helper: tru
   context 'as a school admin' do
     before(:each) do
       sign_in school_admin
+      visit programme_types_path
+    end
+
+    context 'enrolling in a programme' do
+      before(:each) do
+        click_on programme_type_1.title
+      end
+
+      it 'prompts to start' do
+        expect(page).to have_content("You can enrol your school in this programme")
+      end
+
+      it 'successfully enrols the school' do
+        expect {
+          click_link 'Start'
+        }.to change(Programme, :count).from(0).to(1)
+        expect(page).to have_content('You started this programme')
+        expect(school.reload.programmes).not_to be_empty
+      end
+    end
+
+    context 'enrolled in a programme' do
+      let(:activity_type) { programme_type_1.activity_types.first }
+      let(:activity)      { create(:activity, school: school, activity_type: activity_type, happened_on: Date.yesterday)}
+
+      before(:each) do
+        #this is because the Enroller relies on this currently
+        allow(EnergySparks::FeatureFlags).to receive(:active?).and_return(true)
+        Programmes::Enroller.new(programme_type_1).enrol(school)
+        ActivityCreator.new(activity).process
+        click_on programme_type_1.title
+      end
+
+      it 'says I have started' do
+        expect(page).to have_content("You started this programme")
+        expect(page).to have_content("Current Progress")
+        expect(page).to have_content( nice_dates(school.programmes.first.started_on) )
+      end
+
+      it 'indicates I have not completed some activities' do
+        expect(page).to have_css("i.fa-circle.text-muted")
+      end
+
+      it 'indicates I have completed an activity' do
+        expect(page).to have_css("i.fa-check-circle.text-success")
+        expect(page).to have_content( nice_dates(activity.happened_on) )
+      end
+
+      it 'doesnt link to activities that are completed' do
+        expect(page).to have_content(activity_type.name)
+        expect(page).to_not have_link(href: activity_type_path(activity_type))
+        expect(page).to have_link(href: activity_type_path(programme_type_1.activity_types.last) )
+      end
+
+      it 'indicates I am enrolled on list of programmes' do
+        click_on("View all programmes")
+        expect(page).to have_content("You have already started this programme")
+        expect(page).to have_link("Continue", href: programme_type_path(programme_type_1))
+        expect(page).to have_link("View", href: programme_type_path(programme_type_3))
+      end
+    end
+  end
+
+  context 'as a pupil' do
+    before(:each) do
+      sign_in pupil
       visit programme_types_path
     end
 
