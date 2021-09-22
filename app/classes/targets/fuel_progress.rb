7a91191ae6f56bf540eefe473680af3af1725c83
Leigh Dodds <leigh@ldodds.com>
2021-09-22 15:26:09 +0100

    Add recent data warning to school target page. Revise how progress is reported to ignore out of date data

--- app/classes/targets/fuel_progress.rb
+++ app/classes/targets/fuel_progress.rb
@@ -1,20 +1,25 @@
 module Targets
   class FuelProgress
-    attr_reader :fuel_type, :progress, :usage, :target
+    attr_reader :fuel_type, :progress, :usage, :target, :recent_data
 
-    def initialize(fuel_type:, progress:, usage:, target:)
+    def initialize(fuel_type:, progress:, usage:, target:, recent_data: true)
       @fuel_type = fuel_type
       @progress = progress
       @usage = usage
       @target = target
+      @recent_data = recent_data
     end
 
     def progress_formatted
       FormatEnergyUnit.format(:relative_percent, @progress, :html, false, true, :target) unless progress.nil?
     end
 
+    def recent_data?
+      @recent_data
+    end
+
     def achieving_target?
-      progress.present? && progress <= 0
+      @progress.present? && @progress <= 0
     end
   end
 end
