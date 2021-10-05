ec89090084e4423a02870eb9365844505bea7d42
Leigh Dodds <leigh@ldodds.com>
2021-10-05 15:52:39 +0100

    Add task and service for weekly report

--- app/services/targets/school_group_target_data_reporting_service.rb
+++ app/services/targets/school_group_target_data_reporting_service.rb
@@ -4,7 +4,7 @@ module Targets
       @school_group = school_group
     end
 
-    #returns hash school => result
+    #returns Hash of School => Struct(school:, electricity: , gas:, storage_heater:)
     def report
       report = {}
       schools.each do |school|
@@ -12,7 +12,7 @@ module Targets
           aggregate_school = AggregateSchoolService.new(school).aggregate_school
           report[school] = report_for_school(school, aggregate_school)
         rescue => e
-          report[school] = []
+          report[school] = nil
           Rails.logger.error "Unable to generate report for #{school.name}: #{e.message}"
           Rails.logger.error e.backtrace.join("\n")
           Rollbar.error(e, job: :target_data, school: school)
@@ -31,26 +31,29 @@ module Targets
       ::TargetsService.new(aggregate_school, fuel_type)
     end
 
+    #OpenStruct to group them
     def report_for_school(school, aggregate_school)
-      result = []
-      result << report_for_school_and_fuel_type(school, aggregate_school, :electricity) if school.has_electricity?
-      result << report_for_school_and_fuel_type(school, aggregate_school, :gas) if school.has_gas?
-      result << report_for_school_and_fuel_type(school, aggregate_school, :storage_heater) if school.has_storage_heaters?
+      result = OpenStruct.new(school: school)
+      result.electricity = report_for_school_and_fuel_type(school, aggregate_school, :electricity) if school.has_electricity?
+      result.gas = report_for_school_and_fuel_type(school, aggregate_school, :gas) if school.has_gas?
+      result.storage_heater = report_for_school_and_fuel_type(school, aggregate_school, :storage_heater) if school.has_storage_heaters?
       result
     end
 
+    #OpenStruct, move to target service?
     def report_for_school_and_fuel_type(school, aggregate_school, fuel_type)
       service = target_service(aggregate_school, fuel_type)
-      {
+      OpenStruct.new(
         fuel_type: fuel_type,
         holidays: service.enough_holidays?,
         temperature: service.enough_temperature_data?,
         readings: service.enough_readings_to_calculate_target?,
         estimate_needed: service.annual_kwh_estimate_required?,
         estimate_set: service.annual_kwh_estimate?,
-        target: service.target_set?,
-        current_target: school.has_current_target?
-      }
+        calculate_synthetic_data: service.can_calculate_one_year_of_synthetic_data?,
+        target_set: school.has_current_target?,
+        recent_data: service.recent_data?
+      )
     end
   end
 end
