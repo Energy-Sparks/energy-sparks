f3845bb51484b2cb491ea9f7205cdb4c8d50c563
Julian Higman <jhigman@pobox.com>
2021-10-08 17:53:25 +0100

    CRUD spec

--- app/controllers/schools/cads_controller.rb
+++ app/controllers/schools/cads_controller.rb
@@ -1,7 +1,7 @@
 module Schools
   class CadsController < ApplicationController
     load_and_authorize_resource :school
-    load_and_authorize_resource through: :school #, only: [:index, :create, :edit, :update]
+    load_and_authorize_resource through: :school
 
     skip_before_action :authenticate_user!, only: [:live_data]
 
