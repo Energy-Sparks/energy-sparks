210a89a415ab0f5586a6c9de8131b96784e96d95
Julian Higman <jhigman@pobox.com>
2021-10-08 16:30:11 +0100

    Removed cache implementation code

--- app/services/cads/synthetic_data_service.rb
+++ app/services/cads/synthetic_data_service.rb
@@ -10,26 +10,5 @@ module Cads
       @cad.update(last_reading: reading, last_read_at: Time.zone.now)
       reading
     end
-
-    # def read(*)
-    #   max_power = @cad.max_power || 100
-    #   if Rails.cache.exist?(cache_key)
-    #     reading = Rails.cache.fetch(cache_key)
-    #     reading = reading + (max_power/10).floor
-    #     if reading > max_power
-    #       reading = 0
-    #     end
-    #   else
-    #     reading = 0
-    #   end
-    #   Rails.cache.write(cache_key, reading)
-    #   reading
-    # end
-    #
-    # private
-    #
-    # def cache_key
-    #   "synth-data-#{@cad.id}"
-    # end
   end
 end
--- spec/services/cads/synthetic_data_service_spec.rb
+++ spec/services/cads/synthetic_data_service_spec.rb
@@ -26,23 +26,5 @@ module Cads
       @service.read
       expect(cad.last_read_at).to_not be_nil
     end
-
-    # context 'when max is set' do
-    #   let(:max_power) { 3 }
-    #   it "returns random number less than max" do
-    #     result = Cads::SyntheticDataService.new(cad).read
-    #     expect(result).not_to be_nil
-    #     expect(result < max_power).to be true
-    #   end
-    # end
-    #
-    # context 'when max is not set' do
-    #   let(:max_power) { }
-    #   it "returns random number less than default" do
-    #     result = Cads::SyntheticDataService.new(cad).read
-    #     expect(result).not_to be_nil
-    #     expect(result < 100).to be true
-    #   end
-    # end
   end
 end
