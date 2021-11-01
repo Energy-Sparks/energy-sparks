b181e20d665bb27e3cd6a06cdfc95734f23b18de
Julian Higman <jhigman@pobox.com>
2021-11-01 14:03:35 +0000

    Specs

--- spec/services/cads/live_data_service_spec.rb
+++ spec/services/cads/live_data_service_spec.rb
@@ -3,33 +3,66 @@ require 'rails_helper'
 module Cads
   describe LiveDataService do
 
-    let(:school)  { create(:school) }
-    let(:cad)     { create(:cad, school: school) }
+    let(:token)               { 'abc123' }
+    let(:device_identifier)   { 'xyz' }
+    let(:school)              { create(:school) }
+    let(:cad)                 { create(:cad, school: school, device_identifier: device_identifier) }
 
-    before :each do
-      expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:login)
+    context 'when api returns response' do
+
+      before :each do
+        expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:login).and_return(token)
+      end
+
+      it "triggers fast update if no timestamp returned" do
+        response = { 'powerTimestamp' => 0 }
+        expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:live_data).with(cad.device_identifier).and_return(response)
+        expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:trigger_fast_update).with(cad.device_identifier)
+        result = Cads::LiveDataService.new(cad).read
+        expect(result).to eq(0.0)
+      end
+
+      it "returns power reading" do
+        response = { 'powerTimestamp' => 123, 'power' => [{'type' => 'ELECTRICITY', 'watts' => 456}] }
+        expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:live_data).with(cad.device_identifier).and_return(response)
+        result = Cads::LiveDataService.new(cad).read
+        expect(result).to eq(456.0)
+      end
+
+      it "handles missing power reading for other type" do
+        response = { 'powerTimestamp' => 123, 'power' => [{'type' => 'ELECTRICITY', 'watts' => 456}] }
+        expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:live_data).with(cad.device_identifier).and_return(response)
+        result = Cads::LiveDataService.new(cad).read(:gas)
+        expect(result).to eq(0.0)
+      end
     end
 
-    it "triggers fast update if no timestamp returned" do
-      response = { 'powerTimestamp' => 0 }
-      expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:live_data).with(cad.device_identifier).and_return(response)
-      expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:trigger_fast_update).with(cad.device_identifier)
-      result = Cads::LiveDataService.new(cad).read
-      expect(result).to eq(0.0)
+    context 'when api raises errors' do
+
+      before :each do
+        expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:login).and_raise(StandardError.new('doh'))
+      end
+
+      it "logs error to rollbar" do
+        expect(Rollbar).to receive(:error)
+        result = Cads::LiveDataService.new(cad).read
+      end
     end
 
-    it "returns power reading" do
-      response = { 'powerTimestamp' => 123, 'power' => [{'type' => 'ELECTRICITY', 'watts' => 456}] }
-      expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:live_data).with(cad.device_identifier).and_return(response)
-      result = Cads::LiveDataService.new(cad).read
-      expect(result).to eq(456.0)
-    end
+    context 'when caching tokens' do
 
-    it "handles missing power reading for other type" do
-      response = { 'powerTimestamp' => 123, 'power' => [{'type' => 'ELECTRICITY', 'watts' => 456}] }
-      expect_any_instance_of(MeterReadingsFeeds::GeoApi).to receive(:live_data).with(cad.device_identifier).and_return(response)
-      result = Cads::LiveDataService.new(cad).read(:gas)
-      expect(result).to eq(0.0)
+      it "caches token for default period" do
+        expect(Rails.cache).to receive(:fetch).with(LiveDataService::CACHE_KEY, expires_in: 45.minutes)
+        Cads::LiveDataService.new(cad).read
+      end
+
+      it "caches token for env var period" do
+        expect(Rails.cache).to receive(:fetch).with(LiveDataService::CACHE_KEY, expires_in: 123.minutes)
+        ClimateControl.modify "GEO_API_TOKEN_EXPIRY_MINUTES" => "123" do
+          Cads::LiveDataService.new(cad).read
+        end
+      end
+
     end
   end
 end
