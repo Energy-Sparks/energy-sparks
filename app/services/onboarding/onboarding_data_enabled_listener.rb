f0911b9d51fe7109a81dc4e36a951ce366d24c3b
Julian Higman <jhigman@pobox.com>
2021-11-09 17:40:42 +0000

    Note

--- app/services/onboarding/onboarding_data_enabled_listener.rb
+++ app/services/onboarding/onboarding_data_enabled_listener.rb
@@ -1,5 +1,8 @@
 module Onboarding
   class OnboardingDataEnabledListener
+    # TDOO
+    # this message not used yet - will be broadcast when a school has data enabled..
+    # may need to review the source of the broadcast (might not be SchoolCreator)
     def school_data_enabled(school)
       ActivationEmailSender.new(school).send
     end
