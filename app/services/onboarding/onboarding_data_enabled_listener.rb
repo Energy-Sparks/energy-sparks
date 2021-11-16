2943b68bd514606fbcead63ff5ee7fd5584af552
Julian Higman <jhigman@pobox.com>
2021-11-16 09:05:13 +0000

    Disable data in new flow

--- app/services/onboarding/onboarding_data_enabled_listener.rb
+++ app/services/onboarding/onboarding_data_enabled_listener.rb
@@ -1,8 +1,9 @@
 module Onboarding
   class OnboardingDataEnabledListener
     def onboarding_completed(school_onboarding)
-      OnboardingMailer.with(school_onboarding: school_onboarding).completion_email.deliver_now
       school.update!(visible: true)
+      school.update!(data_enabled: false)
+      OnboardingMailer.with(school_onboarding: school_onboarding).completion_email.deliver_now
       ActivationEmailSender.new(school).send
     end
 
