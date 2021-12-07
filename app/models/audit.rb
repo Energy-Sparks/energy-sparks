b033071cdaee044c3e72e8b458f733b4ad64859b
Julian Higman <jhigman@pobox.com>
2021-12-07 15:23:15 +0000

    Create models for audit, audit activities, audit interventions

---
+++ app/models/audit.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: audits
+#
+#  completed_on :date
+#  created_at   :datetime         not null
+#  id           :bigint(8)        not null, primary key
+#  published    :boolean          default(FALSE)
+#  school_id    :bigint(8)        not null
+#  title        :string           not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_audits_on_school_id  (school_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (school_id => schools.id) ON DELETE => cascade
+#
+class Audit < ApplicationRecord
+  belongs_to :school, inverse_of: :activities
+  has_one_attached :file
+  has_rich_text :description
+
+  validates_presence_of :school, :title
+
+  has_many :audit_activity_types
+  has_many :activity_types, through: :audit_activity_types
+
+  has_many :audit_intervention_types
+  has_many :intervention_types, through: :audit_intervention_types
+end
---
+++ spec/models/audit_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe Audit do
+
+  let(:school) { create :school }
+  let(:title) { 'Test Audit' }
+  let(:activity_type) { create :activity_type }
+  let(:intervention_type) { create :intervention_type }
+
+  let(:audit) { Audit.create!(school: school, title: title) }
+
+  context 'when no title' do
+    let(:title) { '' }
+    it 'fails validation' do
+      expect {
+        audit.save!
+      }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  it 'has collection of activity types with notes' do
+    audit.audit_activity_types.create(activity_type: activity_type, notes: 'some activity type')
+    expect( audit.activity_types ).to eq([activity_type])
+    expect( audit.audit_activity_types.last.notes ).to eq('some activity type')
+  end
+
+  it 'has collection of intervention types with notes' do
+    audit.audit_intervention_types.create(intervention_type: intervention_type, notes: 'some intervention type')
+    expect( audit.intervention_types ).to eq([intervention_type])
+    expect( audit.audit_intervention_types.last.notes ).to eq('some intervention type')
+  end
+end
