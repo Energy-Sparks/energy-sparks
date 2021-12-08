3fe9805c2462dbc22aee4b7eeee90f9dc6d733c3
Leigh Dodds <leigh@ldodds.com>
2021-12-08 15:50:47 +0000

    config/puma.rb
    Add explicit binding

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,6 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix:///var/run/puma/my_app.sock"
+pidfile "/var/run/puma/my_app.sock"
