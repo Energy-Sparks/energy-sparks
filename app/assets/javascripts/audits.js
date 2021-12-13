05a7d0746433bb4cf872ca5d2998010babec210f
Julian Higman <jhigman@pobox.com>
2021-12-13 16:45:55 +0000

    Removed redundant js

--- app/assets/javascripts/audits.js
+++ app/assets/javascripts/audits.js
@@ -1,19 +1,7 @@
 "use strict"
 
 $(document).ready(function() {
-
   if ($("form#audit_form").length) {
     $('select.form-control.select2').select2({theme: 'bootstrap'});
   }
-
-  $("form#activity_type_form .file").change(function(event){
-    if (this.files) {
-      var reader = new FileReader();
-      reader.onload = function(e){
-        $(".upload-preview img").attr("width", '300px');
-        $(".upload-preview img").attr("src", e.target.result);
-      };
-      reader.readAsDataURL(this.files[0]);
-    }
-  });
 });
