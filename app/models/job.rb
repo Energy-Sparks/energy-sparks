781b5db6e1ad5371a9d301338af2eb4bed6d63c7
Leigh Dodds <leigh@ldodds.com>
2021-12-14 11:59:25 +0000

    Add structured data to the page

--- app/models/job.rb
+++ app/models/job.rb
@@ -16,4 +16,31 @@ class Job < ApplicationRecord
 
   scope :current_jobs, -> { where(closing_date: nil).or(where("closing_date >= ?", Time.zone.today)) }
   scope :by_created_date, -> { order(created_at: :asc) }
+
+  def to_job_posting
+    {
+      "@context" => "https://schema.org/",
+      "@type" => "JobPosting",
+      "title" => title,
+      "description" => description.to_s,
+      "identifier": {
+        "@type" => "PropertyValue",
+        "name" => "Energy Sparks",
+        "value" => id
+      },
+      "datePosted" => created_at.strftime("%Y-%m-%d"),
+      "validThrough" => closing_date.present? ? closing_date.strftime("%Y-%m-%d") : "",
+      "hiringOrganization" => {
+        "@type" => "Organization",
+        "name" => "Energy Sparks",
+        "sameAs" => "https://energysparks.uk",
+      },
+      "applicantLocationRequirements" => {
+        "@type" => "Country",
+        "name" => "UK"
+      },
+      "jobLocationType" => "TELECOMMUTE",
+      "employmentType" => voluntary ? "VOLUNTEER" : "OTHER",
+    }
+  end
 end
