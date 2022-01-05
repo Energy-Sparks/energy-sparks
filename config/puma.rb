7279497950aac382c1af3f79ebca8263ffba5886
Leigh Dodds <leigh@ldodds.com>
2022-01-05 12:10:23 +0000

    add pidfile and state_path

--- config/puma.rb
+++ config/puma.rb
@@ -38,3 +38,6 @@ preload_app!
 directory '/var/app/current'
 bind 'unix:///var/run/puma/my_app.sock'
 stdout_redirect '/var/log/puma/puma.log', '/var/log/puma/puma.log', true
+
+pidfile '/var/app/current/puma.pid'
+state_path '/var/app/current/puma.state'
