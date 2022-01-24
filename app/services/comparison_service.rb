ec9801b49764434aad861b8991a7b45b69db5930
Leigh Dodds <leigh@ldodds.com>
2022-01-24 15:42:45 +0000

    Reinstate method, remove link to scoreboard comparison

--- app/services/comparison_service.rb
+++ app/services/comparison_service.rb
@@ -3,6 +3,17 @@ class ComparisonService
     @user = user
   end
 
+  def list_scoreboards
+    if @user.present? && @user.admin?
+      return Scoreboard.order(:name).to_a
+    end
+    scoreboards = Scoreboard.is_public.order(:name).to_a
+    if @user.present? && @user.school.present? && @user.school.scoreboard.present?
+        scoreboards << @user.school.scoreboard
+    end
+    scoreboards.uniq
+  end
+
   def list_school_groups
     if @user.present? && @user.admin?
       return SchoolGroup.order(:name).to_a
--- spec/services/comparison_service_spec.rb
+++ spec/services/comparison_service_spec.rb
@@ -5,6 +5,42 @@ RSpec.describe ActivityTypeFilter, type: :service do
   let(:service) { ComparisonService.new(user) }
   let(:user)    { nil }
 
+  describe '#list_scoreboards' do
+    let!(:scoreboard)   { create(:scoreboard, name: 'Super scoreboard', public: true)}
+    let!(:school)       { create(:school, :with_school_group, scoreboard: scoreboard) }
+
+    let!(:private_scoreboard)   { create(:scoreboard, name: 'Private scoreboard', public: false)}
+    let!(:other_school)       { create(:school, :with_school_group, scoreboard: private_scoreboard) }
+
+    context 'as an admin' do
+      let(:user)   { create(:admin) }
+      it 'lists all scoreboards' do
+        expect(service.list_scoreboards).to match_array([scoreboard, private_scoreboard])
+      end
+    end
+
+    context 'as a guest' do
+      it 'lists only public scoreboards' do
+        expect(service.list_scoreboards).to match_array([scoreboard])
+      end
+    end
+
+    context 'as a staff member in a school with private scoreboard' do
+      let!(:user)         { create(:staff, school: other_school)}
+      it 'lists public and private' do
+        expect(service.list_scoreboards).to match_array([scoreboard, private_scoreboard])
+      end
+    end
+
+    context 'as a staff member in a school with a public scoreboard' do
+      let!(:user)         { create(:staff, school: school)}
+      it 'lists only public' do
+        expect(service.list_scoreboards).to match_array([scoreboard])
+      end
+    end
+
+  end
+
   describe '#list_school_groups' do
     let(:school_group)  { create(:school_group, name: "Group", public: true)}
     let!(:school)       { create(:school, school_group: school_group) }
