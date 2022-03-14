27e296b8c2e82429249eee91f8c1ae25ea6cbe5f
Julian Higman <jhigman@pobox.com>
2022-03-14 12:39:42 +0000

    Add intervention types from audits to suggestions

--- app/services/interventions/suggest_action.rb
+++ app/services/interventions/suggest_action.rb
@@ -9,17 +9,24 @@ module Interventions
     #This is just an initial implementation to hook in suggesting based on
     #alerts, needs further improvements
     def suggest(limit = 5)
-      suggestions = suggest_from_alerts.to_a
+      suggestions = suggest_from_audits.to_a
+      return suggestions.take(limit) unless suggestions.length < limit
+
+      top_up_from_list(suggest_from_alerts.to_a, suggestions)
       return suggestions.take(limit) unless suggestions.length < limit
 
       top_up_from_list(InterventionType.not_other.sample(limit), suggestions)
       suggestions.take(limit)
     end
 
+    def suggest_from_audits
+      @school.audits.map(&:intervention_types).flatten.uniq
+    end
+
     def suggest_from_alerts
       content = @school.latest_content
       if content
-        content.find_out_more_intervention_types
+        content.find_out_more_intervention_types.uniq
       else
         InterventionType.none
       end
@@ -28,7 +35,9 @@ module Interventions
     private
 
     def top_up_from_list(more, suggestions)
-      suggestions.concat(more)
+      more.each do |suggestion|
+        suggestions << suggestion unless suggestions.include?(suggestion)
+      end
     end
   end
 end
