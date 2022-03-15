70ca5ee89ae898b09e51384bb1d9defa8300369c
Deb Bassett <deb@urbanwide.com>
2022-03-15 14:23:17 +0000

    Update Transport Survey Responses factory for new fields

--- spec/factories/transport_survey_responses.rb
+++ spec/factories/transport_survey_responses.rb
@@ -2,9 +2,10 @@ FactoryBot.define do
   factory :transport_survey_response do
     transport_survey
     transport_type
-    device_identifier { "1234567890" }
+    run_identifier { "1234567890" }
     surveyed_at { DateTime.now }
     journey_minutes { 1 }
     weather { :sun }
+    passengers { 1 }
   end
 end
