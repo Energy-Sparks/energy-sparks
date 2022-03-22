425ca109829dcf1505a7073a264f8aa305eca5b9
Deb Bassett <deb@urbanwide.com>
2022-03-22 18:58:40 +0000

    Sorting out abilities and transport surveys system spec

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -92,7 +92,7 @@ class Ability
       end
       can :manage, Observation, related_school_scope
       can :manage, TransportSurvey, related_school_scope
-      can :manage, TransportSurveyResponse, { transport_survey: related_school_scope }
+      can :manage, TransportSurveyResponse, transport_survey: related_school_scope
       can :crud, Programme, related_school_scope
       can [:manage, :enable_alerts], User, related_school_scope
 
@@ -119,16 +119,14 @@ class Ability
       can :compare, SchoolGroup, { id: user.school.school_group_id }
       can :manage, Activity, school: { id: user.school_id, visible: true }
       can :manage, Observation, school: { id: user.school_id, visible: true }
-      can :manage, TransportSurvey, school: school_scope
-      can :manage, TransportSurveyResponse, transport_survey: { school: school_scope }
       can :read, Scoreboard, public: false, id: user.default_scoreboard.try(:id)
       can :read, [:my_school_menu, :school_downloads]
       can :read, Meter
       can [:start_programme], School, id: user.school_id, visible: true
       #pupils can view management dashboard for their school and others in group
       if user.pupil?
-        can [:read, :create], TransportSurvey, school: school_scope
-        can [:read, :create], TransportSurveyResponse, transport_survey: { school: school_scope }
+        can [:intro, :read, :update, :create], TransportSurvey, related_school_scope
+        can [:read, :create], TransportSurveyResponse, transport_survey: related_school_scope
         can :show_management_dash, School, id: user.school_id, visible: true
         can :show_management_dash, School, { school_group_id: user.school.school_group_id, visible: true }
       end
@@ -146,6 +144,8 @@ class Ability
         can :crud, Programme, school: { id: user.school_id, visible: true }
         can :enable_alerts, User, id: user.id
         can [:create, :update, :destroy], Contact, user_id: user.id
+        can :manage, TransportSurvey, school: { id: user.school_id, visible: true }
+        can :manage, TransportSurveyResponse, transport_survey: { school: { id: user.school_id, visible: true }}
       end
     elsif user.guest?
       can :manage, SchoolOnboarding, created_user_id: nil
--- spec/system/schools/transport_surveys_spec.rb
+++ spec/system/schools/transport_surveys_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 describe 'TransportSurveys', type: :system do
 
-  let!(:school)            { create(:school) }
+  let!(:school)            { create(:school, :with_school_group) }
   let!(:transport_type)    { create(:transport_type) }
 
   # admin / group admin / school admin / staff - can manage Transport Surveys, Transport Survey Responses
   # pupil - as above except deleting Surveys and Transport Survey Responses
   # public user - read access only for everything (but not the intro)
 
-  [:admin, :group_admin, :school_admin, :staff].each do |user_type|
+  MANAGING_USER_TYPES = [:admin, :group_admin, :school_admin, :staff]
+  SURVEYING_USER_TYPES = MANAGING_USER_TYPES + [:pupil]
+
+  SURVEYING_USER_TYPES.each do |user_type|
     describe "as a #{user_type} user who can carry out surveys" do
-      let(:user) { create(user_type) }
+      let(:user) { create(user_type, school: school) }
 
       before(:each) do
         sign_in(user)
@@ -20,7 +23,6 @@ describe 'TransportSurveys', type: :system do
       context "viewing the introduction" do
         before(:each) do
           visit intro_school_transport_surveys_path(school)
-          # save_and_open_page
         end
 
         it { expect(page).to have_content('Transport surveys') }
@@ -37,12 +39,14 @@ describe 'TransportSurveys', type: :system do
             before(:each) do
               click_button 'submit'
             end
-            it "lists responses" do 
+            it "lists responses" do
               expect(page).to have_content("Responses for: #{Date.today}")
-              # save_and_open_page
             end
             it "displays added response" do
               expect(page).to have_content(transport_type.name)
+            end
+            it "shows link to collect more responses" do
+              expect(page).to have_link('Collect more responses')
             end
           end
         end
@@ -50,32 +54,152 @@ describe 'TransportSurveys', type: :system do
     end
   end
 
-  # Surveying only (not managing) user of transport surveys
-  describe 'as a pupil' do
+  MANAGING_USER_TYPES.each do |user_type|
+    describe "as a #{user_type} user who can delete surveys and responses" do
+      let!(:user) { create(user_type, school: school) }
 
+      # not a big fan of if statements in rspec - it may be refactored out later on
+      before(:each) do
+        user.school_group = school.school_group if user_type == :group_admin
+      end
+
+      let!(:transport_survey) { create(:transport_survey, school: school) }
+      let!(:transport_survey_response) { create(:transport_survey_response, transport_survey: transport_survey) }
+
+      before(:each) do
+        sign_in(user)
+      end
+      context "viewing transport surveys index" do
+        before(:each) do
+          visit school_transport_surveys_path(school)
+        end
+
+        it "shows created transport survey" do
+          expect(page).to have_content("#{transport_survey.run_on}")
+        end
+
+        it "shows delete button" do
+          expect(page).to have_link('Delete')
+        end
+
+        context "and deleting transport survey" do
+          before(:each) do
+            click_link('Delete')
+          end
+          it "removes transport survey" do
+            expect(page).to_not have_content("#{transport_survey.run_on}")
+          end
+        end
+
+        context "and viewing responses" do
+          before(:each) do
+            click_link("#{Date.today}")
+          end
+
+          it "lists responses" do
+            expect(page).to have_content("Responses for: #{Date.today}")
+          end
+
+          it "displays added response" do
+            expect(page).to have_content(transport_survey_response.run_identifier)
+          end
+
+          context "and deleteing response" do
+            before(:each) do
+              # save_and_open_page
+              click_link('Delete')
+            end
+            it "removes response" do
+              expect(page).to_not have_content("#{transport_survey_response.run_identifier}")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  describe 'as a pupil who cannot delete transport surveys or responses' do
     let!(:pupil) { create(:pupil, school: school)}
     let!(:transport_survey) { create(:transport_survey, school: school) }
+    let!(:transport_survey_response) { create(:transport_survey_response, transport_survey: transport_survey) }
+
+    before(:each) do
+      sign_in(pupil)
+    end
 
     context "viewing transport surveys index" do
       before(:each) do
         visit school_transport_surveys_path(school)
       end
 
-      it "should be able to see a created transport survey" do
+      it "shows created transport survey" do
         expect(page).to have_content("#{transport_survey.run_on}")
+      end
+
+      it "shows surveying link" do
+        expect(page).to have_link('Start surveying')
+      end
+
+      it "doesn't show survey delete button" do
+        expect(page).to_not have_link('Delete')
+      end
+
+      context "and viewing responses" do
+        before(:each) do
+          click_link("#{Date.today}")
+        end
+
+        it "shows link to collect more responses" do
+          expect(page).to have_link('Collect more responses')
+        end
+
+        it "doesn't show delete link" do
+          expect(page).to_not have_link('Delete')
+        end
       end
     end
   end
 
-  # Read-only user of transport surveys
-  describe 'as a public user' do
+  describe 'as a public user with read only access' do
     context "viewing the introduction" do
       before :each do
         visit intro_school_transport_surveys_path(school)
       end
-
       it { expect(page).to_not have_content('Transport surveys') }
     end
 
+    context "viewing transport surveys" do
+      let!(:transport_survey) { create(:transport_survey, school: school) }
+
+      before(:each) do
+        visit school_transport_surveys_path(school)
+      end
+
+      it "shows created transport survey" do
+        expect(page).to have_content("#{transport_survey.run_on}")
+      end
+
+      it "doesn't show surveying link" do
+        expect(page).to_not have_link('Start surveying')
+      end
+
+      it "doesn't show survey delete button" do
+        expect(page).to_not have_link('Delete')
+      end
+
+      context "and viewing responses" do
+        before(:each) do
+          click_link("#{Date.today}")
+        end
+
+        it "doesn't show link to collect more responses" do
+          expect(page).to_not have_link('Collect more responses')
+        end
+
+        it "doesn't show delete link" do
+          expect(page).to_not have_link('Delete')
+        end
+      end
+    end
   end
 end
