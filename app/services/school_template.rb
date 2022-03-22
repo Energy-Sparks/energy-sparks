3468cc02cbe35e1f0252bbad2457fd42b83fd0e2
Julian Higman <jhigman@pobox.com>
2022-03-22 17:26:14 +0000

    Render analysis controls into embedded chart markup

--- app/services/school_template.rb
+++ app/services/school_template.rb
@@ -13,6 +13,14 @@ class SchoolTemplate < Mustache
     chart_type, y_axis_unit = chart_config.split('|')
     chart_config = {}
     chart_config['y_axis_units'] = (y_axis_unit || '£')
-    chart_tag(@school, chart_type, chart_config: chart_config, show_advice: false, html_class: 'analysis-chart embedded-chart')
+
+    ret = "<div id=\"chart_wrapper_#{chart_type}\" class=\"chart-wrapper\">"
+    ret += ApplicationController.render(
+      partial: 'shared/analysis_controls',
+      locals: { chart_type: chart_type, axis_controls: true, analysis_controls: true }
+    )
+    ret += chart_tag(@school, chart_type, chart_config: chart_config, wrap: false, show_advice: false, html_class: 'analysis-chart embedded-chart')
+    ret += "</div>"
+    ret
   end
 end
