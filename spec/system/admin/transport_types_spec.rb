3a428b59b544c208ca6fa3a4e11465cd7afe3471
Deb Bassett <deb@urbanwide.com>
2022-03-24 14:56:03 +0000

    Transport types admin - show page

--- spec/system/admin/transport_types_spec.rb
+++ spec/system/admin/transport_types_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "admin transport type", type: :system do
+describe "admin transport type", type: :system, include_application_helper: true do
 
   let!(:admin)  { create(:admin) }
 
@@ -22,17 +22,59 @@ describe "admin transport type", type: :system do
       sign_in(admin)
     end
 
-    describe "Viewing the index" do
+    let!(:transport_type) { create(:transport_type) }
+    let(:minimum_attributes) { [:image, :name, :speed_km_per_hour, :kg_co2e_per_km] }
 
-      let!(:transport_type) { create(:transport_type, name: 'sonic jet') }
+    describe "Viewing the index" do
 
       before(:each) do
         visit admin_transport_types_path
-        @table = find(:table)
       end
 
       it "lists created transport type" do
-        expect(@table).to have_selector(:table_row, ['sonic jet'])
+        within('table') do
+          minimum_attributes.each do |attribute|
+            expect(page).to have_content(transport_type[attribute])
+          end
+        end
+      end
+
+      context "and clicking the transport type link" do
+        before(:each) do
+          click_link(transport_type.name)
+        end
+
+        it "shows transport type page" do
+          expect(page).to have_current_path(admin_transport_type_path(transport_type))
+        end
+      end
+    end
+
+    describe "Viewing a transport type" do
+      before(:each) do
+        visit admin_transport_type_path(transport_type)
+      end
+
+      it "shows the minimum attributes" do
+        minimum_attributes.each do |attribute|
+          expect(page).to have_content(transport_type[attribute])
+        end
+      end
+      it { expect(page).to have_content(y_n(transport_type[:can_share])) }
+      it { expect(page).to have_content(nice_date_times(transport_type[:created_at]))}
+      it { expect(page).to have_content(nice_date_times(transport_type[:updated_at]))}
+
+      context "with some buttons" do
+        it { expect(page).to have_link('Back') }
+
+        context "and clicking on the back button" do
+          before(:each) do
+            click_link('Back')
+          end
+          it "shows index page" do
+            expect(page).to have_current_path(admin_transport_types_path)
+          end
+        end
       end
     end
   end
