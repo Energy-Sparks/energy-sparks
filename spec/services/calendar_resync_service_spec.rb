8acc7f5ec2dcb246b862abd791e12c38ffa97fc7
Julian Higman <jhigman@pobox.com>
2022-04-25 17:41:56 +0100

    Moved specs to calendar factory and calendar resync

--- spec/services/calendar_resync_service_spec.rb
+++ spec/services/calendar_resync_service_spec.rb
@@ -3,30 +3,53 @@ require 'rails_helper'
 describe CalendarResyncService do
 
   let(:parent) { create(:regional_calendar) }
-  let!(:calendar_1) { create(:school_calendar, based_on: parent) }
-  let!(:calendar_2) { create(:school_calendar, based_on: parent) }
+  let!(:calendar) { create(:school_calendar, based_on: parent) }
 
   let!(:parent_event) { create(:holiday, calendar: parent, description: 'parent event') }
 
   context 'when child has no events' do
     it 'creates new events based on parent' do
-      expect(calendar_1.calendar_events.count).to eq(0)
-      expect(calendar_2.calendar_events.count).to eq(0)
+      expect(calendar.calendar_events.count).to eq(0)
       CalendarResyncService.new(parent).resync
-      expect(calendar_1.calendar_events.first.description).to eq('parent event')
-      expect(calendar_2.calendar_events.first.description).to eq('parent event')
+      expect(calendar.calendar_events.count).to eq(1)
+      expect(calendar.calendar_events.first.description).to eq('parent event')
+      expect(calendar.calendar_events.first.based_on).to eq(parent_event)
     end
   end
 
   context 'when child has event based on parent' do
-    let!(:calendar_event_1) { create(:holiday, calendar: calendar_1, description: 'calendar event 1', based_on: parent_event, start_date: parent_event.start_date, end_date: parent_event.end_date) }
-    let!(:calendar_event_2) { create(:holiday, calendar: calendar_2, description: 'calendar event 2', based_on: parent_event, start_date: parent_event.start_date, end_date: parent_event.end_date) }
+    let!(:calendar_event) { create(:holiday, calendar: calendar, description: 'calendar event', based_on: parent_event) }
     it 'updates child events' do
-      expect(calendar_1.calendar_events.count).to eq(1)
-      expect(calendar_2.calendar_events.count).to eq(1)
+      expect(calendar.calendar_events.count).to eq(1)
       CalendarResyncService.new(parent).resync
-      expect(calendar_1.calendar_events.first.description).to eq('parent event')
-      expect(calendar_2.calendar_events.first.description).to eq('parent event')
+      expect(calendar.calendar_events.count).to eq(1)
+      expect(calendar.calendar_events.first.description).to eq('parent event')
+      expect(calendar.calendar_events.first.based_on).to eq(parent_event)
+    end
+  end
+
+  context 'when child has conflicting event' do
+    let!(:parent_event) { create(:term, calendar: parent, description: 'parent event', start_date: '2021-01-01', end_date: '2021-02-01') }
+    let!(:calendar_event) { create(:term, calendar: calendar, description: 'calendar event', start_date: '2021-01-01', end_date: '2021-02-01') }
+
+    it 'skips child with conflicting events' do
+      expect(calendar.calendar_events.count).to eq(1)
+      CalendarResyncService.new(parent).resync
+      expect(calendar.calendar_events.count).to eq(1)
+      expect(calendar.calendar_events.first.description).to eq('calendar event')
+      expect(calendar.calendar_events.first.based_on).to be_nil
+    end
+
+    it 'still updates other child where no conflicting events' do
+      other_calendar = create(:school_calendar, based_on: parent)
+      other_calendar_event = create(:term, calendar: other_calendar, description: 'calendar event', start_date: '2019-01-01', end_date: '2019-02-01')
+      expect(other_calendar.calendar_events.count).to eq(1)
+      CalendarResyncService.new(parent).resync
+      expect(other_calendar.calendar_events.count).to eq(2)
+      expect(other_calendar.calendar_events.first.description).to eq('calendar event')
+      expect(other_calendar.calendar_events.first.based_on).to be_nil
+      expect(other_calendar.calendar_events.last.description).to eq('parent event')
+      expect(other_calendar.calendar_events.last.based_on).to eq(parent_event)
     end
   end
 end
