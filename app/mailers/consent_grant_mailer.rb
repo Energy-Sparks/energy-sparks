afa99612a4a61b3915205dd76ab7d5298f7c1651
Leigh Dodds <leigh@ldodds.com>
2022-05-05 14:38:53 +0100

    Add school name in title

--- app/mailers/consent_grant_mailer.rb
+++ app/mailers/consent_grant_mailer.rb
@@ -1,6 +1,7 @@
 class ConsentGrantMailer < ApplicationMailer
     def email_consent
       @consent_grant = params[:consent_grant]
+      @title = @consent_grant.school.name
       make_bootstrap_mail(to: @consent_grant.user.email, subject: t(:subject, scope: [:consent_grant_mailer]))
     end
 end
