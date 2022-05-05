cddacea46a4303afdfcc9ba34e50d15f4116428d
Leigh Dodds <leigh@ldodds.com>
2022-05-05 14:42:40 +0100

    Set titles, fix link in reminder email

--- app/mailers/onboarding_mailer.rb
+++ app/mailers/onboarding_mailer.rb
@@ -3,11 +3,13 @@ class OnboardingMailer < ApplicationMailer
 
   def onboarding_email
     @school_onboarding = params[:school_onboarding]
+    @title = @school_onboarding.school_name
     make_bootstrap_mail(to: @school_onboarding.contact_email, subject: subject(:onboarding_email))
   end
 
   def completion_email
     @school_onboarding = params[:school_onboarding]
+    @title = @school_onboarding.school_name
     if @school_onboarding.created_by
       make_bootstrap_mail(to: 'operations@energysparks.uk', subject: subject(:completion_email, school: @school_onboarding.school_name))
     end
@@ -15,11 +17,13 @@ class OnboardingMailer < ApplicationMailer
 
   def reminder_email
     @school_onboarding = params[:school_onboarding]
+    @title = @school_onboarding.school_name
     make_bootstrap_mail(to: @school_onboarding.contact_email, subject: "Don't forget to set up your school on Energy Sparks")
   end
 
   def activation_email
     @school = params[:school]
+    @title = @school.name
     @to = params[:to]
     @target_prompt = params[:target_prompt]
     make_bootstrap_mail(to: @to, subject: subject(:activation_email, school: @school.name))
@@ -27,12 +31,14 @@ class OnboardingMailer < ApplicationMailer
 
   def onboarded_email
     @school = params[:school]
+    @title = @school.name
     @to = params[:to]
     make_bootstrap_mail(to: @to, subject: subject(:onboarded_email, school: @school.name))
   end
 
   def data_enabled_email
     @school = params[:school]
+    @title = @school.name
     @to = params[:to]
     @target_prompt = params[:target_prompt]
     make_bootstrap_mail(to: @to, subject: subject(:data_enabled_email, school: @school.name))
@@ -41,6 +47,7 @@ class OnboardingMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @school = @user.school
+    @title = @school.name
     make_bootstrap_mail(to: @user.email, subject: subject(:welcome_email))
   end
 
