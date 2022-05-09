4201cce5f677fdc6abd6a50507104c8db4892dfe
Julian Higman <jhigman@pobox.com>
2022-05-09 12:43:50 +0100

    Report ids of matched calendar events

--- app/services/calendar_init_service.rb
+++ app/services/calendar_init_service.rb
@@ -10,7 +10,7 @@ class CalendarInitService
     @calendar.transaction do
       @calendar.calendar_events.where(based_on: nil).each do |calendar_event|
         if (parent_event = find_matching_event(@calendar.based_on, calendar_event))
-          messages << "Matched child #{calendar_event.display_title} with parent #{parent_event.display_title}"
+          messages << "Matched child #{calendar_event.display_title} (#{calendar_event.id}) with parent #{parent_event.display_title} (#{parent_event.id})"
           calendar_event.update!(based_on: parent_event)
         end
       end
