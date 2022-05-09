4796925a8413555b861179d46b85e44a4a236b36
Julian Higman <jhigman@pobox.com>
2022-05-09 10:21:27 +0100

    Removed unused scope

--- app/models/calendar_event_type.rb
+++ app/models/calendar_event_type.rb
@@ -25,7 +25,6 @@ class CalendarEventType < ApplicationRecord
   scope :holiday,           -> { where(holiday: true) }
   scope :outside_term_time, -> { where(term_time: false) }
   scope :bank_holiday,      -> { where(bank_holiday: true) }
-  scope :not_bank_holiday,  -> { where(bank_holiday: false) }
 
   enum analytics_event_type: [:term_time, :school_holiday, :bank_holiday, :inset_day_in_school, :inset_day_out_of_school]
 
