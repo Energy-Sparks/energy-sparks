53f85b8dcbadbffbd6f277f8d45befdb8043fc4f
Leigh Dodds <leigh@ldodds.com>
2022-05-10 11:41:32 +0100

    Change logic for display of dashboard charts

--- app/controllers/concerns/dashboard_energy_charts.rb
+++ app/controllers/concerns/dashboard_energy_charts.rb
@@ -4,10 +4,10 @@ module DashboardEnergyCharts
   def setup_energy_overview_charts(configuration)
     return {} unless configuration
     {
-      electricity: { chart: [:analysis_charts, :electricity_detail, :management_dashboard_group_by_week_electricity], units: :£ },
-      gas: { chart: [:analysis_charts, :gas_detail, :management_dashboard_group_by_week_gas], units: :£ },
-      storage_heater: { chart: [:analysis_charts, :storage_heaters, :management_dashboard_group_by_week_storage_heater], units: :£ },
-      solar: { chart: [:analysis_charts, :solar_pv, :management_dashboard_group_by_month_solar_pv], units: :kwh }
-    }.select {|_energy, chart_config| configuration.can_show_analysis_chart?(*chart_config[:chart])}
+      electricity: { chart: :management_dashboard_group_by_week_electricity, units: :£ },
+      gas: { chart: :management_dashboard_group_by_week_gas, units: :£ },
+      storage_heater: { chart: :management_dashboard_group_by_week_storage_heater, units: :£ },
+      solar: { chart: :management_dashboard_group_by_month_solar_pv, units: :kwh }
+    }.select {|_energy, chart_config| configuration.dashboard_charts.include?(chart_config[:chart].to_s)}
   end
 end
