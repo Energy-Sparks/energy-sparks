2ec2daa1102bc6f0c0253d3db44b45fb30e70bb8
Leigh Dodds <leigh@ldodds.com>
2022-05-11 09:06:00 +0100

    Convert all hard tabs to spaces

--- app/javascript/packs/transport_surveys/carbon.js
+++ app/javascript/packs/transport_surveys/carbon.js
@@ -1,38 +1,38 @@
 // logic mostly lifted from the old app.
 
 export const carbonExamples = [
-	{
-		name: 'Tree',
+  {
+    name: 'Tree',
     emoji: '🌳',
     equivalentStatement: function(carbonKgs) {
       const treeAbsorbsionKgPerDay = 0.06;
       let days = Math.round(carbonKgs / treeAbsorbsionKgPerDay);
-			return `1 tree would absorb this amount of CO2 in ${days} day(s) 🌳!`;
-		}
+      return `1 tree would absorb this amount of CO2 in ${days} day(s) 🌳!`;
+    }
   }, {
-		name: 'TV',
+    name: 'TV',
     emoji: '📺',
     equivalentStatement: function(carbonKgs) {
       const tvKgPerHour = 0.008;
       let hours = Math.round(carbonKgs / tvKgPerHour);
       return `That's the same as ${hours} hour${hours === 1 ? '' : 's'} of TV 📺!`;
-		},
-	}, {
+    },
+  }, {
     name: 'Gaming',
     emoji: '🎮',
     equivalentStatement: function(carbonKgs) {
       const gamingKgPerHour = 0.008;
       let hours = Math.round(carbonKgs / gamingKgPerHour);
       return `That's the same as playing ${hours} hour${hours === 1 ? '' : 's'} of computer games 🎮!`;
-		},
-	}, {
+    },
+  }, {
     name: 'Meat dinners',
     emoji: '🍲',
     equivalentStatement: function(carbonKgs) {
       const kgPerMeatDinner = 1;
       let meatDinners = Math.round(carbonKgs / kgPerMeatDinner);
       return `That's the same as ${meatDinners} meat dinner${meatDinners === 1 ? '' : 's'} 🍲!`;
-		},
+    },
   }, {
     name: 'Veggie dinners',
     emoji: '🥗',
@@ -40,7 +40,7 @@ export const carbonExamples = [
       const kgPerVeggieDinner = 0.5;
       let veggieDinners = Math.round(carbonKgs / kgPerVeggieDinner);
       return `That's the same as ${veggieDinners} veggie dinner${veggieDinners === 1 ? '' : 's'} 🥗!`;
-		},
+    },
   },
 ];
 
@@ -54,15 +54,15 @@ export const funWeight = function(carbonKgs) {
 };
 
 const parkAndStrideTimeMins = function(timeMins) {
-	// take 15 mins off a park and stride journey
-	return (timeMins > 15 ? timeMins - 15 : 0);
+  // take 15 mins off a park and stride journey
+  return (timeMins > 15 ? timeMins - 15 : 0);
 }
 
 export const carbonCalc = function(transport, timeMins, passengers) {
-	if (transport) {
-		timeMins = transport.image === '🚶🚘' ? parkAndStrideTimeMins(timeMins) : timeMins; // need a better way of identifying park and stride!
-		return (((transport.speed_km_per_hour * timeMins) / 60) * transport.kg_co2e_per_km) / passengers ;
-	} else {
-		return 0;
-	}
+  if (transport) {
+    timeMins = transport.image === '🚶🚘' ? parkAndStrideTimeMins(timeMins) : timeMins; // need a better way of identifying park and stride!
+    return (((transport.speed_km_per_hour * timeMins) / 60) * transport.kg_co2e_per_km) / passengers ;
+  } else {
+    return 0;
+  }
 }
