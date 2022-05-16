19f832728a4640a3b2ffaa59b82d410aaa129e80
Deb Bassett <deb@urbanwide.com>
2022-05-16 11:06:48 +0100

    Allow park_and_stride to be returned in json

--- app/controllers/transport_types_controller.rb
+++ app/controllers/transport_types_controller.rb
@@ -2,6 +2,6 @@ class TransportTypesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with TransportType.select(:id, :name, :image, :kg_co2e_per_km, :speed_km_per_hour, :can_share).index_by(&:id)
+    respond_with TransportType.select(:id, :name, :image, :kg_co2e_per_km, :speed_km_per_hour, :can_share, :park_and_stride).index_by(&:id)
   end
 end
