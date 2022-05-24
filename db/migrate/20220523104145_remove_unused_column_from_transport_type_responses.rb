c9802d0ca80e929811dbccbf65239933e7f9238c
Deb Bassett <deb@urbanwide.com>
2022-05-24 10:43:57 +0100

    For a cleaner rollback, this needs to be exactly the same as when created

--- db/migrate/20220523104145_remove_unused_column_from_transport_type_responses.rb
+++ db/migrate/20220523104145_remove_unused_column_from_transport_type_responses.rb
@@ -1,5 +1,5 @@
 class RemoveUnusedColumnFromTransportTypeResponses < ActiveRecord::Migration[6.0]
   def change
-    remove_column :transport_survey_responses, :integer, :integer
+    remove_column :transport_survey_responses, :integer, :integer, null: false, default: 1
   end
 end
