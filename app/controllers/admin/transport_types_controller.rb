ac19568296622c4fef8c189cc6f319b8dcfd1ab4
Deb Bassett <deb@urbanwide.com>
2022-05-24 13:23:35 +0100

    Add position to strong params

--- app/controllers/admin/transport_types_controller.rb
+++ app/controllers/admin/transport_types_controller.rb
@@ -32,7 +32,7 @@ module Admin
     private
 
     def transport_type_params
-      params.require(:transport_type).permit(:name, :category, :image, :kg_co2e_per_km, :speed_km_per_hour, :can_share, :park_and_stride, :note)
+      params.require(:transport_type).permit(:name, :category, :image, :kg_co2e_per_km, :speed_km_per_hour, :can_share, :park_and_stride, :position, :note)
     end
   end
 end
