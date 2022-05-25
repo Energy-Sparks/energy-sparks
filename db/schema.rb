4e1f691d9974e09b629ddf93e0ab8dc630462b50
Deb Bassett <deb@urbanwide.com>
2022-05-25 14:03:52 +0100

    Should be latest schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_135758) do
+ActiveRecord::Schema.define(version: 2022_05_23_152918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
