1022f84f0ede6d08b5654f8752593cd5f7a787fa
Deb Bassett <deb@urbanwide.com>
2022-05-25 12:15:58 +0100

    Add some carbon calcs and re-organise response class

--- app/models/transport_survey_response.rb
+++ app/models/transport_survey_response.rb
@@ -27,16 +27,6 @@ class TransportSurveyResponse < ApplicationRecord
   belongs_to :transport_survey
   belongs_to :transport_type, inverse_of: :responses
 
-  enum weather: [:sun, :cloud, :rain, :snow]
-
-  def self.weather_symbols
-    { sun: '☀️', cloud: '⛅', rain: '🌧️', snow: '❄️' }
-  end
-
-  def self.passenger_symbol
-    '👤'
-  end
-
   def self.journey_minutes_options
     [5, 10, 15, 20, 30, 45, 60]
   end
@@ -45,11 +35,35 @@ class TransportSurveyResponse < ApplicationRecord
     [1, 2, 3, 4, 5, 6]
   end
 
+  validates :transport_survey_id, :transport_type_id, :passengers, :run_identifier, :surveyed_at, :journey_minutes, :weather, presence: true
+  validates :journey_minutes, inclusion: { in: journey_minutes_options }
+  validates :passengers, inclusion: { in: passengers_options }
+
+  enum weather: [:sun, :cloud, :rain, :snow]
+
+  def self.weather_symbols
+    { sun: '☀️', cloud: '⛅', rain: '🌧️', snow: '❄️' }
+  end
+
   def weather_symbol
     self.class.weather_symbols[weather.to_sym]
   end
 
-  validates :transport_survey_id, :transport_type_id, :passengers, :run_identifier, :surveyed_at, :journey_minutes, :weather, presence: true
-  validates :journey_minutes, inclusion: { in: journey_minutes_options }
-  validates :passengers, inclusion: { in: passengers_options }
+  def self.passenger_symbol
+    '👤'
+  end
+
+  def carbon_per_group
+    transport_type.can_share? ? carbon_calc : (carbon_calc * passengers)
+  end
+
+  def carbon_per_passenger
+    transport_type.can_share? ? (carbon_calc / passengers) : carbon_calc
+  end
+
+  private
+
+  def carbon_calc
+    ((transport_type.speed_km_per_hour * time) / 60) * transport_type.kg_co2e_per_km
+  end
 end
