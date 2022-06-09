56e2d4ad2b3c8ebd45177be3b622f784d06b5308
Julian Higman <jhigman@pobox.com>
2022-06-09 15:23:46 +0100

    Move translated strings to language files

--- app/helpers/locale_helper.rb
+++ app/helpers/locale_helper.rb
@@ -1,14 +1,6 @@
 module LocaleHelper
-  def t_language(locale)
-    t('language_name', locale: locale)
-  end
-
   def t_field(sym, locale)
     "#{sym}_#{Mobility.normalize_locale(locale)}".to_sym
-  end
-
-  def t_label(str, locale)
-    "#{str} (#{t_language(locale)})"
   end
 
   def t_params(fields, locales = I18n.available_locales)
--- spec/helpers/locale_helper_spec.rb
+++ spec/helpers/locale_helper_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 describe LocaleHelper do
-  describe '.t_language' do
-    it 'returns a string language name' do
-      expect(helper.t_language(:cy)).to eq('Welsh')
-    end
-  end
-
   describe '.t_field' do
     it 'makes a locale-aware field symbol' do
       expect(helper.t_field(:name, :cy)).to eq(:name_cy)
@@ -14,11 +8,8 @@ describe LocaleHelper do
     it 'handles unfriendly locales' do
       expect(helper.t_field(:name, 'pt-BR')).to eq(:name_pt_br)
     end
-  end
-
-  describe '.t_label' do
-    it 'makes a locale-aware field label' do
-      expect(helper.t_label('Description', :cy)).to eq('Description (Welsh)')
+    it 'makes translation strings' do
+      expect(helper.t_field('activity_type.description', :cy)).to eq('activity_type.description_cy')
     end
   end
 
