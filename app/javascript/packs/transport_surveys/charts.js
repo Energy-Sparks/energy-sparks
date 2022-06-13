b0ba792c7ebf33202777223897b70a5c2bf669b4
Deb Bassett <deb@urbanwide.com>
2022-06-13 13:33:50 +0100

    js tidy

--- app/javascript/packs/transport_surveys/charts.js
+++ app/javascript/packs/transport_surveys/charts.js
@@ -15,29 +15,26 @@ const chart = ( function() {
       renderChart(data, pieChart);
     })
     .error(function() {
-      renderError();
+      renderError(pieChart);
     })
-  }
-
-  function renderError() {
-    alert("error!");
-    let titleH3 = $('#transport_surveys_pie h3');
-    titleH3.text('There was a problem loading this chart');
-    $('#transport_surveys_pie').remove();
   }
 
   function renderChart(data, pieChart) {
     let chartConfig =  { y_axis_label: '%' };
     let series = { data: data };
 
-    pie(chartConfig, pieChart, series, $(chartId));
+    pie(chartConfig, pieChart, series, $(pieChart.renderTo));
     pieChart.hideLoading();
+  }
+
+  function renderError(pieChart) {
+    $(pieChart.renderTo).add("h3").text('There was a problem loading this chart');
+    $(pieChart.renderTo).remove();
   }
 
   function ajaxCall() {
     return $.ajax({
       type: 'GET',
-      async: true,
       dataType: "json",
       url: window.location.href + '.json'
     })
