09efbe2ae3f4792a44bd78937c64277097595b11
Julian Higman <jhigman@pobox.com>
2022-06-14 15:37:16 +0100

    Added initial resources methods

--- lib/transifex_api/client.rb
+++ lib/transifex_api/client.rb
@@ -7,21 +7,48 @@ module TransifexApi
     class NotAuthorised < StandardError; end
 
     BASE_URL = 'https://rest.api.transifex.com/'.freeze
+    ORGANIZATION = 'energy-sparks'.freeze
 
-    def initialize(api_key, connection = nil)
+    def initialize(api_key, project, connection = nil)
       @api_key = api_key
+      @project = project
       @connection = connection
     end
 
-    def get_organizations
-      url = 'organizations'
+    def get_languages
+      url = make_url("projects/#{project_id}/languages")
       get_data(url)
+    end
+
+    def list_resources
+      url = add_filter("resources")
+      get_data(url)
+    end
+
+    def create_resource(name, slug)
+      url = make_url("resources")
+      post_data(url, resource_data(name, slug, project_id))
     end
 
     private
 
     def headers
-      { 'Authorization' => "Bearer #{@api_key}" }
+      {
+        'Authorization' => "Bearer #{@api_key}",
+        'Content-Type' => 'application/vnd.api+json'
+      }
+    end
+
+    def add_filter(path)
+      path + "?filter[project]=#{project_id}"
+    end
+
+    def make_url(path)
+      path
+    end
+
+    def project_id
+      "o:#{ORGANIZATION}:p:#{@project}"
     end
 
     def connection
@@ -30,6 +57,15 @@ module TransifexApi
 
     def get_data(url)
       response = connection.get(url)
+      process_response(response)
+    end
+
+    def post_data(url, data)
+      response = connection.post(url, data.to_json)
+      process_response(response)
+    end
+
+    def process_response(response)
       raise BadRequest.new(error_message(response)) if response.status == 400
       raise NotAuthorised.new(error_message(response)) if response.status == 401
       raise NotAllowed.new(error_message(response)) if response.status == 403
@@ -49,6 +85,33 @@ module TransifexApi
     rescue
       #problem parsing or traversing json, return original api error
       response.body
+    end
+
+    def resource_data(name, slug, project_id)
+      {
+        "data": {
+          "attributes": {
+            "accept_translations": true,
+            "name": name,
+            "slug": slug,
+          },
+          "relationships": {
+            "i18n_format": {
+              "data": {
+                "id": "YML_KEY",
+                "type": "i18n_formats"
+              }
+            },
+            "project": {
+              "data": {
+                "id": project_id,
+                "type": "projects"
+              }
+            }
+          },
+          "type": "resources"
+        }
+      }
     end
   end
 end
