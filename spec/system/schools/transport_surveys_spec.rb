3212428cfdd6e3f961429780aba2ae637c139502
Deb Bassett <deb@urbanwide.com>
2022-06-15 13:08:58 +0100

    Updating the rspec for Nav changes

--- spec/system/schools/transport_surveys_spec.rb
+++ spec/system/schools/transport_surveys_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+include ApplicationHelper
 
 describe 'TransportSurveys', type: :system do
+
 
   let!(:school)            { create(:school, :with_school_group) }
   let!(:transport_type)    { create(:transport_type, can_share: true) }
@@ -13,7 +15,6 @@ describe 'TransportSurveys', type: :system do
     MANAGING_USER_TYPES = [:admin, :group_admin, :school_admin, :staff]
     SURVEYING_USER_TYPES = MANAGING_USER_TYPES + [:pupil]
 
-=begin
     SURVEYING_USER_TYPES.each do |user_type|
       describe "as a #{user_type} user who can carry out surveys" do
         let(:user) { create(user_type, school: school) }
@@ -31,22 +32,20 @@ describe 'TransportSurveys', type: :system do
           it { expect(page).to have_content('Survey today') }
           it { expect(page).to have_content('Javascript must be enabled to use this functionality.') }
           it { expect(page).to have_link('View all transport surveys') }
+          it { expect(page).to_not have_css('#survey_nav') }
 
           context "and clicking the 'View all transport surveys' button" do
             before(:each) do
               click_link 'View all transport surveys'
             end
-
-            it { expect(page).to have_content('Travel to School Surveys') }
-            it { expect(page).to have_content('Survey Date') }
+            it { expect(page).to have_content('Survey date') }
           end
         end
       end
     end
-=end
 
     MANAGING_USER_TYPES.each do |user_type|
-      describe "as a #{user_type} user who can delete surveys and responses" do
+      describe "as a #{user_type} user who can delete surveys and manage & delete responses" do
         let!(:user) { create(user_type, school: school) }
 
         before(:each) do
@@ -65,50 +64,67 @@ describe 'TransportSurveys', type: :system do
           end
 
           it "shows created transport survey" do
-            expect(page).to have_content("#{transport_survey.run_on}")
+            expect(page).to have_content(nice_dates(transport_survey.run_on))
+          end
+
+          it "shows summary button" do
+            expect(page).to have_link('Summary')
+          end
+
+          it "shows manage button" do
+            expect(page).to have_link('Manage')
           end
 
           it "shows delete button" do
             expect(page).to have_link('Delete')
           end
 
-          context "and deleting transport survey" do
+          context "and managing responses" do
             before(:each) do
-              click_link('Delete')
-            end
-            it "removes transport survey" do
-              expect(page).to_not have_content("#{transport_survey.run_on}")
-            end
-          end
-
-          context "and viewing responses" do
-            before(:each) do
-              click_link("#{transport_survey.run_on}")
+              # save_and_open_page
+              within('table') do
+                click_link("Manage")
+              end
             end
 
-            it "lists responses" do
-              expect(page).to have_content("Responses for: #{transport_survey.run_on}")
+            it "shows responses page" do
+              within('h3') do
+                expect(page).to have_content("Manage responses")
+              end
             end
 
             it "displays added response" do
-              expect(page).to have_content(transport_survey_response.run_identifier)
+              expect(page).to have_content(nice_date_times(transport_survey_response.surveyed_at))
             end
 
-            context "and deleteing response" do
+            context "and deleting response" do
               before(:each) do
-                click_link('Delete')
+                within('table') do
+                  click_link('Delete')
+                end
               end
               it "removes response" do
-                expect(page).to_not have_content("#{transport_survey_response.run_identifier}")
+                expect(page).to_not have_content(nice_date_times(transport_survey_response.surveyed_at))
               end
             end
           end
+
+          context "and deleting transport survey" do
+            before(:each) do
+              within('table') do
+                click_link('Delete')
+              end
+            end
+            it "removes transport survey" do
+              expect(page).to_not have_content(nice_dates(transport_survey.run_on))
+            end
+          end
+
         end
       end
     end
 
-=begin
-    describe 'as a pupil who cannot delete transport surveys or responses' do
+    describe 'as a pupil who cannot delete transport surveys or manage responses' do
       let!(:pupil) { create(:pupil, school: school)}
       let!(:transport_survey) { create(:transport_survey, school: school) }
       let!(:transport_survey_response) { create(:transport_survey_response, transport_survey: transport_survey, transport_type: transport_type) }
@@ -123,34 +139,48 @@ describe 'TransportSurveys', type: :system do
         end
 
         it "shows created transport survey" do
-          expect(page).to have_content("#{transport_survey.run_on}")
+          expect(page).to have_content(nice_dates(transport_survey.run_on))
         end
 
         it "shows surveying link" do
-          expect(page).to have_link('Start a travel to school survey')
+          expect(page).to have_link('Start surveying today')
+        end
+
+        it "shows summary button" do
+          expect(page).to have_link('Summary')
+        end
+
+        it "doesn't show manage button" do
+          expect(page).to_not have_link('Manage')
         end
 
         it "doesn't show survey delete button" do
           expect(page).to_not have_link('Delete')
         end
 
-        context "and viewing responses" do
+        context "and viewing summary" do
           before(:each) do
-            click_link("#{transport_survey.run_on}")
+            click_link("Summary")
           end
 
-          it "shows link to collect more responses" do
-            expect(page).to have_link('Collect more responses')
+          it "shows results summary" do
+            within("h3") do
+              expect(page).to have_content("Results summary")
+            end
           end
 
-          it "shows link to View survey responses by date" do
-            expect(page).to have_link('View survey responses by date')
+          it "doesn't show link to manage responses" do
+            expect(page).to_not have_link('Manage responses')
           end
 
-          it "doesn't show delete link" do
-            expect(page).to_not have_link('Delete')
+          it "shows surveying links" do
+            expect(page).to_not have_link('Start surveying today')
+            expect(page).to have_link('Survey today')
           end
 
+          it "shows link to View all transport surveys" do
+            expect(page).to have_link('View all transport surveys')
+          end
         end
       end
     end
@@ -171,39 +201,46 @@ describe 'TransportSurveys', type: :system do
         end
 
         it "shows created transport survey" do
-          expect(page).to have_content("#{transport_survey.run_on}")
+          expect(page).to have_content(nice_dates(transport_survey.run_on))
+        end
+
+        it "shows summary button" do
+          expect(page).to have_link('Summary')
         end
 
         it "doesn't show surveying link" do
-          expect(page).to_not have_link('Start a travel to school survey')
+          expect(page).to_not have_link('Start surveying today')
+          expect(page).to_not have_link('Survey today')
         end
 
         it "doesn't show survey delete button" do
           expect(page).to_not have_link('Delete')
         end
 
-        context "and viewing responses" do
+        it "doesn't show manage button" do
+          expect(page).to_not have_link('Manage')
+        end
+
+        context "and viewing summary" do
           before(:each) do
-            click_link("#{transport_survey.run_on}")
+            click_link("Summary")
           end
 
-          it "doesn't show link to collect more responses" do
-            expect(page).to_not have_link('Collect more responses')
+          it "shows results summary" do
+            within("h3") do
+              expect(page).to have_content("Results summary")
+            end
           end
 
-          it "shows link to View responses by date" do
-            expect(page).to have_link('View survey responses by date')
+          it "doesn't show link to Survey today" do
+            expect(page).to_not have_link('Survey today')
           end
 
-          it "doesn't show delete link" do
-            expect(page).to_not have_link('Delete')
+          it "doesn't show link to Manage responses" do
+            expect(page).to_not have_link('Manage responses')
           end
         end
       end
     end
-
-=end
-
   end
-
 end
