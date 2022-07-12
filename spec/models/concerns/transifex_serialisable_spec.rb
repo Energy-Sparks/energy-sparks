cd9eb4d05d50ce0c632c1ee355fb4bc7f77bad78
Leigh Dodds <leigh@ldodds.com>
2022-07-12 12:59:47 +0100

    Change method for iterating over classes

--- spec/models/concerns/transifex_serialisable_spec.rb
+++ spec/models/concerns/transifex_serialisable_spec.rb
@@ -7,7 +7,7 @@ describe TransifexSerialisable do
     #check for included modules fails
     Dir[Rails.root.join("app/models/**/*.rb")].each { |f| require f }
 
-    tx_serialisables = ObjectSpace.each_object(Class).select { |c| c.included_modules.include? TransifexSerialisable }
+    tx_serialisables = ApplicationRecord.descendants.select { |c| c.included_modules.include? TransifexSerialisable }
 
     tx_serialisables.each do |clazz|
       expect(clazz.column_names).to include("created_at", "updated_at"), "expected #{clazz.name} to have timestamps"
