346c94c4f78717391d9542c75cb9f4bc12a3e487
Leigh Dodds <leigh@ldodds.com>
2022-07-25 17:42:02 +0100

    Add report for individual activity

--- app/controllers/admin/reports/activity_types_controller.rb
+++ app/controllers/admin/reports/activity_types_controller.rb
@@ -8,8 +8,16 @@ module Admin
       end
 
       def show
+        @activities = Activity.where(activity_type: @activity_type).order(:created_at)
         @recorded = Activity.where(activity_type: @activity_type).count
         @school_count = Activity.select(:school_id).where(activity_type: @activity_type).distinct.count
+        @group_by_school = group_by_school.to_a.sort {|a, b| b[1] <=> a[1]}
+      end
+
+      private
+
+      def group_by_school
+        Activity.where(activity_type: @activity_type).group(:school).count
       end
     end
   end
