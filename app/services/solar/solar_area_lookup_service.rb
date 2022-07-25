2cbd72778e733e302258135e58ff425322c8cb8a
Leigh Dodds <leigh@ldodds.com>
2022-07-25 11:59:14 +0100

    Add solar lookup service and add to school creator

---
+++ app/services/solar/solar_area_lookup_service.rb
@@ -0,0 +1,40 @@
+require 'dashboard'
+
+module Solar
+  class SolarAreaLookupService
+    def initialize(school, school_onboarding = nil)
+      @school = school
+      @school_onboarding = school_onboarding
+    end
+
+    def lookup
+      if EnergySparks::FeatureFlags.active?(:auto_assign_solar_area)
+        find_nearest_area
+      else
+        #original behaviour was to use area assigned from the onboarding
+        @school_onboarding.present? ? @school_onboarding.solar_pv_tuos_area : nil
+      end
+    end
+
+    def assign
+      solar_area = lookup
+      solar_area.update(active: true) unless solar_area.active
+      @school.update(solar_pv_tuos_area: solar_area)
+      solar_area
+    end
+
+    private
+
+    def find_nearest_area
+      #nearest is last in list
+      sorted_list = SolarPvTuosArea.all.sort do |a, b|
+        distance_from_school_km(b) <=> distance_from_school_km(a)
+      end
+      sorted_list.last
+    end
+
+    def distance_from_school_km(area)
+      LatitudeLongitude.distance(@school.latitude, @school.longitude, area.latitude, area.longitude)
+    end
+  end
+end
