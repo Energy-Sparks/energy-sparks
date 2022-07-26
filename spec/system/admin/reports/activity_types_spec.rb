39b3b15241c0aadf4e7eb4f7bc627c0ee67aa54b
Leigh Dodds <leigh@ldodds.com>
2022-07-26 15:47:15 +0100

    Add spec

---
+++ spec/system/admin/reports/activity_types_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe 'activity type reports', type: :system do
+
+  let(:admin)              { create(:admin) }
+
+  let!(:activity_type)     { create(:activity_type) }
+  let!(:school)            { create(:school) }
+  let!(:activity)          { create(:activity, school: school, activity_type: activity_type) }
+
+  before(:each) do
+    sign_in(admin)
+    visit admin_reports_path
+  end
+
+  context 'the activity type management report' do
+    before(:each) do
+      click_on "Activity type management report"
+    end
+    it 'displays the report' do
+      expect(page).to have_content("Activity Type Management Report")
+      expect(page).to have_content(activity_type.name)
+      expect(page).to have_link(activity_type.name, href: admin_reports_activity_type_path(activity_type))
+      expect(page).to have_link("Edit", href: edit_admin_activity_type_path(activity_type))
+    end
+  end
+
+  context 'the activity type report' do
+    before(:each) do
+      visit admin_reports_path
+      click_on "Activity type management report"
+    end
+
+    it 'displays the report' do
+      click_on activity_type.name
+
+      expect(page).to have_content activity_type.name
+      expect(page).to have_content school.name
+      expect(page).to have_link(school.name, href: school_timeline_path(school))
+    end
+
+    context 'when the activity has been recorded multiple times' do
+      let!(:activity2)          { create(:activity, school: school, activity_type: activity_type) }
+
+      it 'includes a summary' do
+        click_on activity_type.name
+        expect(page).to have_content("This activity has been recorded multiple times by some schools")
+      end
+    end
+  end
+
+end
