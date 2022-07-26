e09730f35a083b478844293168a3b8ca580e97bb
Julian Higman <jhigman@pobox.com>
2022-07-26 11:00:48 +0100

    Add locale to preview urls (#1911)
    * Add locale to preview urls
    
    * Fix specs to use locale-specific editor fields
    
    * Change nesting of locale tabs and preview section
    
    * Helper method
    
    * Helper to check locales and fixed spec selectors
    
    * Specs for helpers
    
    * Reverted interpolation class check
    
    * Rename helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -360,4 +360,17 @@ module ApplicationHelper
   def calendar_event_status(calendar_event)
     calendar_event.based_on ? 'inherited' : ''
   end
+
+  def current_locale?(locale)
+    locale.to_s == I18n.locale.to_s
+  end
+
+  def path_with_locale(preview_url, locale)
+    if preview_url.include?('?')
+      preview_url += '&'
+    else
+      preview_url += '?'
+    end
+    preview_url + "locale=#{locale}"
+  end
 end
