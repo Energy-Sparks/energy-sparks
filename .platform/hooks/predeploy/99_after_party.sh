70751cc51159d9d88451eb70285cd36b383a3a34
Leigh Dodds <leigh@ldodds.com>
2022-08-03 09:59:35 +0100

    Add missing environment variables

--- .platform/hooks/predeploy/99_after_party.sh
+++ .platform/hooks/predeploy/99_after_party.sh
@@ -8,6 +8,10 @@ set +x
 export $(/opt/elasticbeanstalk/bin/get-config --output YAML environment | sed -r 's/: /=/' | xargs)
 set -x
 
+PATH=/opt/elasticbeanstalk/.rbenv/shims:/opt/elasticbeanstalk/.rbenv/bin:$PATH
+RBENV_ROOT=/opt/elasticbeanstalk/.rbenv
+RBENV_VERSION=$(cat $RBENV_ROOT/version)
+
 cd $EB_APP_STAGING_DIR
 
 if [ "$EB_IS_COMMAND_LEADER" = "true" ]; then
