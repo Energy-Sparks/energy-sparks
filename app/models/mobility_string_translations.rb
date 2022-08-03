9218bb89645b0387ebf7c6ed2809db31c99e92b5
Ian Townsend <ian.townsend@gmail.com>
2022-08-03 17:28:35 +0100

    Use pg search in mobility string translations

--- app/models/mobility_string_translations.rb
+++ app/models/mobility_string_translations.rb
@@ -1,9 +1,14 @@
 class MobilityStringTranslations < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search,
+                  against: [:value]
+
   def self.activity_type
     where(translatable_type: 'ActivityType')
   end
 
-  def self.search(query)
-    where("value ILIKE ?", '%' + query + '%')
-  end
+  # def self.search(query)
+  #   where("value ILIKE ?", '%' + query + '%')
+  # end
 end
