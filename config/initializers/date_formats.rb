e420aa171532651be993a1ca5eac8bb22f6ff3a6
Ian Townsend <iant@users.noreply.github.com>
2022-08-09 09:35:11 +0100

    Use I18N to format dates and times in the application (#1927)
    * Use I18N to format dates and times in the application

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,5 +1,19 @@
 Time::DATE_FORMATS.merge!({ es_short: '%d %b %Y' })
-Time::DATE_FORMATS.merge!({ es_full: lambda { |date| date.strftime("%a #{date.day.ordinalize} %b %Y")}})
+Time::DATE_FORMATS.merge!(
+  {
+    es_full:
+      lambda do |date|
+        I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y")
+      end
+  }
+)
 Date::DATE_FORMATS.merge!({ es_short: '%d %b %Y' })
 Date::DATE_FORMATS.merge!({ es_compact: '%d/%m/%Y' })
-Date::DATE_FORMATS.merge!({ es_full: lambda { |date| date.strftime("%a #{date.day.ordinalize} %b %Y")}})
+Date::DATE_FORMATS.merge!(
+  {
+    es_full:
+      lambda do |date|
+        I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y")
+      end
+  }
+)
