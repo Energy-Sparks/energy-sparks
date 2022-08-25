c5126056beb94c59f902f964f0e2be7b6c25c137
Ian Townsend <ian.townsend@gmail.com>
2022-08-25 16:56:55 +0100

    Clean up

--- spec/models/chart_data_values_spec.rb
+++ spec/models/chart_data_values_spec.rb
@@ -132,8 +132,6 @@ describe ChartDataValues do
   describe '#colour_lookup' do
     chart_data_values = ChartDataValues.new(EXAMPLE_CONFIG, :teachers_landing_page_gas).process
 
-    puts chart_data_values.colour_lookup
-
     it 'returns a hash with colours assigned to chart series names' do
       expect(chart_data_values.colour_lookup).to eq(
         {"Degree Days" => "#232b49", "Temperature" => "#232b49", "School Day Closed" => "#3bc0f0", "School Day Open" => "#5cb85c", "Holiday" => "#ff4500", "Weekend" => "#ffac21", "Heating on in cold weather" => "#3bc0f0", "Hot Water (& Kitchen)" => "#5cb85c", "Hot Water Usage" => "#3bc0f0", "Wasted Hot Water Usage" => "#ff4500", "Solar PV (consumed onsite)" => "#ffac21", "Electricity" => "#02B8FF", "Gas" => "#FFB138", "storage heaters" => "#501e74", "£" => "#232B49", "Electricity consumed from solar pv" => "#5cb85c", "translation missing: en.analytics.series_data_manager.series_name.Electricity consumed from mains" => "#02B8FF", "translation missing: en.analytics.series_data_manager.series_name.Exported solar electricity (not consumed onsite)" => "#FCB43A", "Solar irradiance (brightness of sunshine)" => "#FFB138", "Rating" => "#232b49"}
