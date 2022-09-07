1162048bdfc6167a790e45774d3278558743451f
Ian Townsend <iant@users.noreply.github.com>
2022-09-07 08:18:59 +0100

    Chart annotations fix (#1992)

--- app/services/charts/annotate.rb
+++ app/services/charts/annotate.rb
@@ -6,7 +6,11 @@ module Charts
 
     def annotate_weekly(x_axis_categories)
       return if x_axis_categories.empty?
-      date_categories = x_axis_categories.map {|x_axis_category| Date.parse(x_axis_category)}
+
+      date_categories = x_axis_categories.map do |x_axis_category|
+        date_for(x_axis_category)
+      end
+
       first_date = date_categories.min
       last_date = date_categories.max + 6.days
 
@@ -40,6 +44,23 @@ module Charts
           icon: intervention.intervention_type.intervention_type_group.icon
         }
       end
+    end
+
+    private
+
+    def abbr_month_name_lookup
+      @abbr_month_name_lookup ||= I18n.t('date.abbr_month_names').map.with_index do |abbr_month_name, index|
+        [abbr_month_name, I18n.t('date.abbr_month_names', locale: 'en')[index]]
+      end.to_h
+    end
+
+    def date_for(x_axis_category)
+      return Date.parse(x_axis_category) if I18n.locale.to_s == 'en'
+
+      # Date.parse doesn't work with localised date strings as passed in x_axis_categories (e.g. '01 Chwe 2022')
+      # so we need to "de-localise" to the default locale ('en') first (e.g. '01 Feb 2022').
+      delocalised_date = x_axis_category.gsub(/\w+/) { |date_string| abbr_month_name_lookup.fetch(date_string, date_string) }
+      Date.parse(delocalised_date)
     end
   end
 end
