96d1de8a1585b8202613b4d9f47dc24c6f92cb79
Ian Townsend <ian.townsend@gmail.com>
2022-09-09 14:05:39 +0100

    Remove unused factory for now

--- spec/factories/alert_subscription_event_factory.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :alert_subscription_event do
-    # association :content_generation_run
-    association :content_version, factory: :alert_type_rating_content_version
-    alert
-  end
-end
