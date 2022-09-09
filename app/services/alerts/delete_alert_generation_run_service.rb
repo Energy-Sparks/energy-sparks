8ee486559417e26e476ce9739aa2553bdf507692
Ian Townsend <ian.townsend@gmail.com>
2022-09-09 09:05:12 +0100

    Use a transaction

--- app/services/alerts/delete_alert_generation_run_service.rb
+++ app/services/alerts/delete_alert_generation_run_service.rb
@@ -8,7 +8,10 @@ module Alerts
     end
 
     def delete!
-      AlertGenerationRun.where("created_at <= ?", @older_than).destroy_all
+      ActiveRecord::Base.transaction do
+        alert_generation_runs = AlertGenerationRun.where("created_at <= ?", @older_than)
+        alert_generation_runs.destroy_all
+      end
     end
   end
 end
