a6eec74b03cb0c5d714a281469aaa066cd130d09
Ian Townsend <ian.townsend@gmail.com>
2022-09-23 15:16:34 +0100

    Add a model test

---
+++ spec/models/manual_data_load_run_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ManualDataLoadRun, type: :model do
+  before { @ManualDataLoadRun }
+
+  describe '#delete' do
+    before do
+      ManualDataLoadRun.delete_all
+      @new_manual_data_load_run = ManualDataLoadRun.create!(amr_uploaded_reading: create(:amr_uploaded_reading), status: 'done')
+      ManualDataLoadRunLogEntry.create(manual_data_load_run: @new_manual_data_load_run, message: "SUCCESS")
+      ManualDataLoadRunLogEntry.create(manual_data_load_run: @new_manual_data_load_run, message: "SUCCESS")
+    end
+
+    it 'destroys a manual data load run and all associated manual data load run log entries' do
+      expect(ManualDataLoadRun.count).to eq(1)
+      expect(@new_manual_data_load_run.manual_data_load_run_log_entries.count).to eq(2)
+      expect do
+        @new_manual_data_load_run.destroy
+      end.to change(ManualDataLoadRun, :count).by(-1) & change(ManualDataLoadRunLogEntry, :count).by(-2)
+    end
+  end
+end
