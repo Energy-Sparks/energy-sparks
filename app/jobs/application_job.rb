539accbc0e3337f0ac290fdbbd50865a8ef2c195
Ian Townsend <ian.townsend@gmail.com>
2022-09-28 17:16:56 +0100

    Add aws metrics to application job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,6 +2,23 @@ class ApplicationJob < ActiveJob::Base
   after_perform :send_metrics_to_cloudwatch
 
   def send_metrics_to_cloudwatch
-    Aws::Metrics.new(namespace: '', aws_metrics: '').send_to_cloudwatch
+    Aws::Metrics.new(namespace: '', aws_metrics: aws_metrics).send_to_cloudwatch
+  end
+
+  private
+
+  def aws_metrics
+    {
+      metric_name: '',
+      dimensions: [
+        {
+          name: 'hostname',
+          value: ''
+        }
+      ],
+      timestamp: DateTime.now,
+      value: '',
+      unit: ''
+    }
   end
 end
