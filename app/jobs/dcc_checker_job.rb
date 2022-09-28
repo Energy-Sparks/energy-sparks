bcd1b14ec441732ad0a6c9da1437e55ea1eb74fb
Ian Townsend <ian.townsend@gmail.com>
2022-09-28 14:30:18 +0100

    Migrate DccCheckerJob to DelayedJob

--- app/jobs/dcc_checker_job.rb
+++ app/jobs/dcc_checker_job.rb
@@ -1,7 +1,10 @@
 class DccCheckerJob < ApplicationJob
+  self.queue_adapter = :delayed_job
   queue_as :default
 
   def perform(meter)
-    Meters::DccChecker.new([meter]).perform
+    ActiveRecord::Base.transaction do
+      Meters::DccChecker.new([meter]).perform
+    end
   end
 end
