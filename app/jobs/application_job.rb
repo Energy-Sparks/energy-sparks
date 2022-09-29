60f2b52313bdb7b7c0c7c79f240b3577b78914fa
Ian Townsend <ian.townsend@gmail.com>
2022-09-29 10:01:33 +0100

    Move aws payload builder to class

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,11 +2,29 @@ class ApplicationJob < ActiveJob::Base
   after_perform :send_metrics_to_cloudwatch
 
   def send_metrics_to_cloudwatch
-    Aws::Metrics.new(namespace: 'delayed-job', aws_metrics: counts_of_delayed_job_work_processes).send_to_cloudwatch
-    Aws::Metrics.new(namespace: 'delayed-job', aws_metrics: memory_usage_of_the_delayed_job_processes).send_to_cloudwatch
+    send_memory_usage_of_the_delayed_job_processes_to_cloudwatch
+    send_counts_of_delayed_job_work_processes_to_cloudwatch
   end
 
   private
+
+  def send_memory_usage_of_the_delayed_job_processes_to_cloudwatch
+    Aws::Metrics.new(
+      namespace: 'delayed-job',
+      metric_name: 'DelayedJobMemoryUsage',
+      value: delayed_job_memory_usage,
+      aws_unit_string: 'Kilobytes'
+    ).send_to_cloudwatch
+  end
+
+  def send_counts_of_delayed_job_work_processes_to_cloudwatch
+    Aws::Metrics.new(
+      namespace: 'delayed-job',
+      metric_name: 'DelayedJobWorkerProcesses',
+      value: pids.size,
+      aws_unit_string: 'Count'
+    ).send_to_cloudwatch
+  end
 
   def pids
     `pgrep -f jobs:work`.split
@@ -14,28 +32,5 @@ class ApplicationJob < ActiveJob::Base
 
   def delayed_job_memory_usage
     pids.split.map { |pid| `ps -o rss= -p #{pid}`.to_i }.sum # Total in Kilobytes
-  end
-
-  def memory_usage_of_the_delayed_job_processes
-    build_aws_metrics_for(metric_name: 'DelayedJobMemoryUsage', value: delayed_job_memory_usage, aws_unit_string: 'Kilobytes')
-  end
-
-  def counts_of_delayed_job_work_processes
-    build_aws_metrics_for(metric_name: 'DelayedJobWorkerProcesses', value: pids.size, aws_unit_string: 'Count')
-  end
-
-  def build_aws_metrics_for(metric_name:, value:, aws_unit_string:)
-    {
-      metric_name: metric_name,
-      dimensions: [
-        {
-          name: 'hostname',
-          value: Socket.gethostname
-        }
-      ],
-      timestamp: Time.now.utc,
-      value: value,
-      unit: aws_unit_string
-    }
   end
 end
--- app/services/aws/metrics.rb
+++ app/services/aws/metrics.rb
@@ -1,18 +1,48 @@
 module Aws
   class Metrics
-    def initialize(namespace:, aws_metrics:)
+    VALID_UNITS = [
+      'Seconds', 'Microseconds', 'Milliseconds',
+      'Bytes', 'Kilobytes', 'Megabytes', 'Gigabytes', 'Terabytes',
+      'Bits', 'Kilobits', 'Megabits', 'Gigabits', 'Terabits',
+      'Percent', 'Count',
+      'Bytes/Second', 'Kilobytes/Second', 'Megabytes/Second', 'Gigabytes/Second', 'Terabytes/Second',
+      'Bits/Second', 'Kilobits/Second', 'Megabits/Second', 'Gigabits/Second', 'Terabits/Second',
+      'Count/Second',
+      'None'
+    ].freeze
+
+    def initialize(namespace:, metric_name:, value:, aws_unit_string:)
+      raise 'Invalid CloudWatch Unit' unless VALID_UNITS.include?(aws_unit_string)
+
       @namespace = namespace
-      @aws_metrics = aws_metrics
+      @metric_name = metric_name
+      @value = value
+      @aws_unit_string = aws_unit_string
     end
 
     def send_to_cloudwatch
       cloudwatch_client.put_metric_data(
         namespace: @namespace,
-        metric_data: @aws_metrics
+        metric_data: aws_metrics_payload
       )
     end
 
     private
+
+    def aws_metrics_payload
+      {
+        metric_name: @metric_name,
+        dimensions: [
+          {
+            name: 'hostname',
+            value: Socket.gethostname
+          }
+        ],
+        timestamp: Time.now.utc,
+        value: @value,
+        unit: @aws_unit_string
+      }
+    end
 
     def cloudwatch_client
       Aws::CloudWatch::Client.new(region: 'eu-west-2')
