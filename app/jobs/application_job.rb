bfb197397b272daa4afc6174491a01bb4209ed7c
Ian Townsend <ian.townsend@gmail.com>
2022-09-29 09:10:53 +0100

    Add some job metric methods

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,23 +2,40 @@ class ApplicationJob < ActiveJob::Base
   after_perform :send_metrics_to_cloudwatch
 
   def send_metrics_to_cloudwatch
-    Aws::Metrics.new(namespace: '', aws_metrics: aws_metrics).send_to_cloudwatch
+    Aws::Metrics.new(namespace: 'delayed-job', aws_metrics: counts_of_delayed_job_work_processes).send_to_cloudwatch
+    Aws::Metrics.new(namespace: 'delayed-job', aws_metrics: memory_usage_of_the_delayed_job_processes).send_to_cloudwatch
   end
 
   private
 
-  def aws_metrics
+  def pids
+    `pgrep -f jobs:work`.split
+  end
+
+  def delayed_job_memory_usage
+    pids.split.map { |pid| `ps -o rss= -p #{pid}`.to_i }.sum # Total in Kilobytes
+  end
+
+  def memory_usage_of_the_delayed_job_processes
+    build_aws_metrics_for(metric_name: 'DelayedJobMemoryUsage', value: delayed_job_memory_usage, aws_unit_string: 'Kilobytes')
+  end
+
+  def counts_of_delayed_job_work_processes
+    build_aws_metrics_for(metric_name: 'DelayedJobWorkerProcesses', value: pids.size, aws_unit_string: 'Count')
+  end
+
+  def build_aws_metrics_for(metric_name:, value:, aws_unit_string:)
     {
-      metric_name: '',
+      metric_name: metric_name,
       dimensions: [
         {
           name: 'hostname',
-          value: ''
+          value: Socket.gethostname
         }
       ],
-      timestamp: DateTime.now,
-      value: '',
-      unit: ''
+      timestamp: Time.now.utc,
+      value: value,
+      unit: aws_unit_string
     }
   end
 end
