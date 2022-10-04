092277e24b920a10d9f4302f6d0e47c2aadc0a5f
Ian Townsend <ian.townsend@gmail.com>
2022-10-04 10:56:30 +0100

    Some delayed job fixes

--- app/jobs/dcc_grant_trusted_consents_job.rb
+++ app/jobs/dcc_grant_trusted_consents_job.rb
@@ -3,8 +3,6 @@ class DccGrantTrustedConsentsJob < ApplicationJob
   queue_as :default
 
   def perform(meters)
-    ActiveRecord::Base.transaction do
-      Meters::DccGrantTrustedConsents.new(meters).perform
-    end
+    Meters::DccGrantTrustedConsents.new(meters).perform
   end
 end
--- app/jobs/school_batch_run_job.rb
+++ app/jobs/school_batch_run_job.rb
@@ -3,10 +3,8 @@ class SchoolBatchRunJob < ApplicationJob
   queue_as :default
 
   def perform(school_batch_run)
-    ActiveRecord::Base.transaction do
-      school_batch_run.update(status: :running)
-      ContentRegenerator.new(school_batch_run.school, school_batch_run).perform
-      school_batch_run.update(status: :done)
-    end
+    school_batch_run.update(status: :running)
+    ContentRegenerator.new(school_batch_run.school, school_batch_run).perform
+    school_batch_run.update(status: :done)
   end
 end
