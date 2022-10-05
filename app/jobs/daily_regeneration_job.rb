881980cd75726209a7bfe023ac63577a5aabaf58
Ian Townsend <ian.townsend@gmail.com>
2022-10-05 15:50:58 +0100

    Update method names

--- app/jobs/daily_regeneration_job.rb
+++ app/jobs/daily_regeneration_job.rb
@@ -3,13 +3,13 @@ class DailyRegenerationJob < ApplicationJob
 
   def perform(school:)
     GoodJob.logger.info("#{DateTime.now.utc} Regeneration for school #{school.name} start")
-    run_validate_and_persist_for(school)
-    run_aggregate_school_services_for(school)
+    run_validate_and_persist_readings_service_for(school)
+    run_aggregate_school_service_for(school)
     run_content_batch_for(school)
     GoodJob.logger.info("#{DateTime.now.utc} Regeneration for school #{school.name} end")
   end
 
-  def run_aggregate_school_services_for(school)
+  def run_aggregate_school_service_for(school)
     AggregateSchoolService.new(school).invalidate_cache
   rescue => e
     GoodJob.logger.error "Exception: running AggregateSchoolService in DailyRegenerationJob for #{school.name}: #{e.class} #{e.message}"
@@ -17,7 +17,7 @@ class DailyRegenerationJob < ApplicationJob
     Rollbar.error(e, job: :daily_regeneration_job, school_id: school.id, school: school.name)
   end
 
-  def run_validate_and_persist_for(school)
+  def run_validate_and_persist_readings_service_for(school)
     Amr::ValidateAndPersistReadingsService.new(school).perform
   rescue => e
     GoodJob.logger.error "Exception: running Amr::ValidateAndPersistReadingsService in DailyRegenerationJob for #{school.name}: #{e.class} #{e.message}"
