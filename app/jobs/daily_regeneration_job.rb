d4f922f4eed047336288537dd3ff4db8de23b336
Ian Townsend <ian.townsend@gmail.com>
2022-10-05 15:38:57 +0100

    Add some error handling

--- app/jobs/daily_regeneration_job.rb
+++ app/jobs/daily_regeneration_job.rb
@@ -3,9 +3,29 @@ class DailyRegenerationJob < ApplicationJob
 
   def perform(school:)
     GoodJob.logger.info("#{DateTime.now.utc} Regeneration for school #{school.name} start")
-    Amr::ValidateAndPersistReadingsService.new(school).perform
-    AggregateSchoolService.new(school).invalidate_cache
-    ContentBatch.new([school], GoodJob.logger).regenerate
+    run_validate_and_persist_for(school)
+    run_aggregate_school_services_for(school)
+    run_content_batch_for(school)
     GoodJob.logger.info("#{DateTime.now.utc} Regeneration for school #{school.name} end")
+  end
+
+  def run_aggregate_school_services_for(school)
+    AggregateSchoolService.new(school).invalidate_cache
+  rescue => e
+    GoodJob.logger.error "Exception: running AggregateSchoolService in DailyRegenerationJob for #{school.name}: #{e.class} #{e.message}"
+    GoodJob.logger.error e.backtrace.join("\n")
+    Rollbar.error(e, job: :daily_regeneration_job, school_id: school.id, school: school.name)
+  end
+
+  def run_validate_and_persist_for(school)
+    Amr::ValidateAndPersistReadingsService.new(school).perform
+  rescue => e
+    GoodJob.logger.error "Exception: running Amr::ValidateAndPersistReadingsService in DailyRegenerationJob for #{school.name}: #{e.class} #{e.message}"
+    GoodJob.logger.error e.backtrace.join("\n")
+    Rollbar.error(e, job: :daily_regeneration_job, school_id: school.id, school: school.name)
+  end
+
+  def run_content_batch_for(school)
+    ContentBatch.new([school], GoodJob.logger).regenerate
   end
 end
