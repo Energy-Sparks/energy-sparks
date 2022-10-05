d49301d0cb3f6547560871674b7157f092cbb7e5
Ian Townsend <ian.townsend@gmail.com>
2022-10-05 15:11:53 +0100

    Update logging

--- app/jobs/daily_regeneration_job.rb
+++ app/jobs/daily_regeneration_job.rb
@@ -2,18 +2,10 @@ class DailyRegenerationJob < ApplicationJob
   queue_as :regeneration
 
   def perform(school:)
-    total_amr_readings_before = AmrValidatedReading.count
-
-    GoodJob.logger.info("Validating and persisting #{school.name}...")
+    GoodJob.logger.info("#{DateTime.now.utc} Regeneration for school #{school.name} start")
     Amr::ValidateAndPersistReadingsService.new(school).perform
-
-    GoodJob.logger.info("Clearing cache for #{school.name}...")
     AggregateSchoolService.new(school).invalidate_cache
-
-    total_amr_readings_after = AmrValidatedReading.count
-    GoodJob.logger.info("Total AMR Readings after #{school.name}: #{total_amr_readings_after} - inserted: #{total_amr_readings_after - total_amr_readings_before}")
-
     ContentBatch.new([school], GoodJob.logger).regenerate
-    GoodJob.logger.info("Finished daily regeneration for #{school.name}")
+    GoodJob.logger.info("#{DateTime.now.utc} Regeneration for school #{school.name} end")
   end
 end
