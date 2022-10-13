7608eaca36096ecf6de772397d388cc4b365b933
Julian Higman <jhigman@pobox.com>
2022-10-13 14:51:59 +0100

    Keep confirmed flag. Show whether user receives alerts on index page.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,6 @@
 class PasswordsController < Devise::PasswordsController
+  before_action :set_confirmed
+
   include AlertContactCreator
   include NewsletterSubscriber
 
@@ -6,12 +8,12 @@ class PasswordsController < Devise::PasswordsController
     super
     # the resource is NOT the actual user - have to find it ourselves
     user = User.with_reset_password_token(params[:reset_password_token])
-    @allow_subscriptions = allow_subscriptions?(user, params)
+    @allow_subscriptions = allow_subscriptions?(user)
   end
 
   def update
     super do |resource|
-      @allow_subscriptions = allow_subscriptions?(resource, params)
+      @allow_subscriptions = allow_subscriptions?(resource)
       if resource.errors.empty? && @allow_subscriptions
         create_or_update_alert_contact(resource.school, resource) if auto_create_alert_contact?
         subscribe_newsletter(resource.school, resource) if auto_subscribe_newsletter?
@@ -21,8 +23,12 @@ class PasswordsController < Devise::PasswordsController
 
   private
 
-  def allow_subscriptions?(user, params)
+  def set_confirmed
+    @confirmed = params[:confirmed].present?
+  end
+
+  def allow_subscriptions?(user)
     return false unless user
-    user.school_id.present? && params[:confirmed].present?
+    user.school_id.present? && @confirmed
   end
 end
