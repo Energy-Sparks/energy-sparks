b4aa1ee2c26b8d9d90e5a04acdf6b99c4c296d61
Julian Higman <jhigman@pobox.com>
2022-10-15 18:05:41 +0100

    Send email after dcc checks and preview email contents

--- spec/services/meters/dcc_checker_spec.rb
+++ spec/services/meters/dcc_checker_spec.rb
@@ -21,5 +21,19 @@ module Meters
       expect(meter.reload.dcc_meter).to be_falsey
       expect(meter.reload.dcc_checked_at).not_to be nil
     end
+
+    it 'should generate an email if status changed' do
+      expect(n3rgy_api).to receive(:find).with(meter.mpan_mprn).and_return(true)
+      expect{
+        Meters::DccChecker.new([meter], n3rgy_api_factory).perform
+      }.to change(ActionMailer::Base.deliveries, :count).from(0).to(1)
+    end
+
+    it 'should not generate an email if status not changed' do
+      expect(n3rgy_api).to receive(:find).with(meter.mpan_mprn).and_return(false)
+      expect{
+        Meters::DccChecker.new([meter], n3rgy_api_factory).perform
+      }.not_to change(ActionMailer::Base.deliveries, :count)
+    end
   end
 end
