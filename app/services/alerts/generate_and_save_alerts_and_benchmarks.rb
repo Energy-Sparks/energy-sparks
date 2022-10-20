5d09fddff50b42b944f4d5284769d0c4b6b1ab5c
Ian Townsend <ian.townsend@gmail.com>
2022-10-20 09:22:41 +0100

    Refactor code

--- app/services/alerts/generate_and_save_alerts_and_benchmarks.rb
+++ app/services/alerts/generate_and_save_alerts_and_benchmarks.rb
@@ -1,65 +1,77 @@
 module Alerts
   class GenerateAndSaveAlertsAndBenchmarks
-    def initialize(school, aggregate_school, benchmark_result_generation_run)
+    def initialize(school:, aggregate_school: nil)
       @school = school
-      @aggregate_school = aggregate_school || AggregateSchoolService.new(school).aggregate_school
-      @benchmark_result_generation_run = benchmark_result_generation_run || BenchmarkResultGenerationRun.latest
+      @aggregate_school = aggregate_school || create_new_aggregate_school_service_for(school)
     end
 
     def perform
       ActiveRecord::Base.transaction do
-        build_alerts_and_alert_types
-      end
-    end
-
-    def build_alerts_and_alert_types
-      # Run though all of the schools alerts
-      @alert_generation_run = AlertGenerationRun.create!(school: @school)
-
-      relevant_alert_types.each do |alert_type|
+        create_a_new_school_alert_generation_run
+        generate_alert_type_run_results_for_relevant_alert_types
       end
     end
 
     private
 
-    def relevant_alert_types
+    def create_new_aggregate_school_service_for(school)
+      AggregateSchoolService.new(school).aggregate_school
+    end
+
+    def generate_alert_type_run_results_for_relevant_alert_types
+      find_relevant_alert_types.each { |alert_type| generate_alert_type_run_results_for(alert_type) }
+    end
+
+    def generate_alert_type_run_results_for(alert_type)
+      alert_type_run_result = GenerateAlertTypeRunResult.new(
+        school: @school,
+        aggregate_school: @aggregate_school,
+        alert_type: alert_type
+                              ).perform
+
+      process_alert_type_run_result(alert_type_run_result)
+    end
+
+    def create_a_new_school_alert_generation_run
+      @alert_generation_run = AlertGenerationRun.create!(school: @school)
+    end
+
+    def find_relevant_alert_types
       RelevantAlertTypes.new(@school).list
     end
 
-    # def perform(asof_date = Time.zone.today)
-    #   ActiveRecord::Base.transaction do
-    #     @alert_generation_run = AlertGenerationRun.create!(school: @school)
-    #     @benchmark_result_school_generation_run = BenchmarkResultSchoolGenerationRun.create!(school: @school, benchmark_result_generation_run: @benchmark_result_generation_run)
-
-    #     relevant_alert_types.each do |alert_type|
-    #       service = alert_type_run_result = GenerateAlertTypeRunResult.new(school: @school, aggregate_school: @aggregate_school, alert_type: alert_type, use_max_meter_date_if_less_than_asof_date: true).perform
-
-    #       service.benchmark_dates(asof_date).each do |benchmark_date|
-    #         alert_type_run_result = service.perform(benchmark_date)
-    #         process_alert_type_run_result(alert_type_run_result)
-    #       end
-    #     end
-    #   end
-    # end
-
     def process_alert_type_run_result(alert_type_run_result)
-      asof_date = alert_type_run_result.asof_date
-      alert_type = alert_type_run_result.alert_type
+      # Create error messages
+      alert_type_run_result.error_messages.each { |error_message| create_alert_error_for(alert_type_run_result.asof_date, error_message, alert_type_run_result.alert_type) }
 
-      alert_type_run_result.error_messages.each do |error_message|
-        AlertError.create!(alert_generation_run: @alert_generation_run, asof_date: asof_date, information: error_message, alert_type: alert_type)
-      end
+      # Process alert type run reports
+      alert_type_run_result.reports.each { |alert_report| process_alert_report(alert_type_run_result.alert_type, alert_report, alert_type_run_result.asof_date) }
+    end
 
-      alert_type_run_result.reports.each do |alert_report|
-        process_alert_report(alert_type, alert_report, asof_date)
-      end
+    def create_alert_error_for(asof_date, error_message, alert_type)
+      AlertError.create!(
+        alert_generation_run: @alert_generation_run,
+        asof_date: asof_date,
+        information: error_message,
+        alert_type: alert_type
+      )
+    end
+
+    def create_alert_for(asof_date, alert_report, alert_type)
+      alert_attributes = build_alert_attributes_for(alert_report, alert_type, asof_date)
+
+      Alert.create!(alert_attributes)
+    end
+
+    def build_alert_attributes_for(alert_report, alert_type, asof_date)
+      AlertAttributesFactory.new(@school, alert_report, @alert_generation_run, alert_type, asof_date).generate
     end
 
     def process_alert_report(alert_type, alert_report, asof_date)
       if alert_report.valid
-        Alert.create(AlertAttributesFactory.new(@school, alert_report, @alert_generation_run, alert_type, asof_date).generate)
+        create_alert_for(asof_date, alert_report, alert_type)
       else
-        AlertError.create!(alert_generation_run: @alert_generation_run, asof_date: asof_date, information: "INVALID. Relevance: #{alert_report.relevance}", alert_type: alert_type)
+        create_alert_error_for(asof_date, "INVALID. Relevance: #{alert_report.relevance}", alert_type)
       end
     end
   end
