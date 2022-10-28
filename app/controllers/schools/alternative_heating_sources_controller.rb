3890755f5afa3911f38e265eda5c4a520fda9822
Ian Townsend <ian.townsend@gmail.com>
2022-10-28 16:51:14 +0100

    Use load and authorize school

--- app/controllers/schools/alternative_heating_sources_controller.rb
+++ app/controllers/schools/alternative_heating_sources_controller.rb
@@ -1,27 +1,20 @@
 module Schools
   class AlternativeHeatingSourcesController < ApplicationController
-    before_action :set_school
+    load_and_authorize_resource :school
 
     def index
-      authorize! :manage, @school
-
       @alternative_heating_sources = @school.alternative_heating_sources.all
     end
 
     def new
-      authorize! :manage, @school
-
       @alternative_heating_source = @school.alternative_heating_sources.new
     end
 
     def edit
-      authorize! :manage, @school
-
       @alternative_heating_source = @school.alternative_heating_sources.find(params[:id])
     end
 
     def create
-      authorize! :manage, @school
       @alternative_heating_source = @school.alternative_heating_sources.new(alternative_heating_source_params)
 
       if @alternative_heating_source.save
@@ -32,8 +25,6 @@ module Schools
     end
 
     def update
-      authorize! :manage, @school
-
       @alternative_heating_source = @school.alternative_heating_sources.find(params[:id])
       if @alternative_heating_source.update(alternative_heating_source_params)
         redirect_to(school_alternative_heating_sources_url(@school.slug), notice: "Alternative heating source was successfully updated.")
@@ -43,8 +34,6 @@ module Schools
     end
 
     def destroy
-      authorize! :manage, @school
-
       if @school.alternative_heating_sources.find(params[:id]).delete
         redirect_to(school_alternative_heating_sources_url(@school.slug), notice: "Alternative heating source was successfully deleted.")
       else
@@ -53,11 +42,6 @@ module Schools
     end
 
     private
-
-    def set_school
-      @school = School.friendly.find(params[:school_id])
-      authorize! :manage_school_alternative_heating_sources, @school
-    end
 
     def alternative_heating_source_params
       params.require(:alternative_heating_source).permit(:source, :percent_of_overall_use, :notes)
