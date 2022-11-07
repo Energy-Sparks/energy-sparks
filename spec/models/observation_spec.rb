5a5142b96e58d97eb89aa9f1c0132d2c21cf818e
Leigh Dodds <leigh@ldodds.com>
2022-11-07 11:36:04 +0000

    Allow future observations (#2178)

--- spec/models/observation_spec.rb
+++ spec/models/observation_spec.rb
@@ -5,18 +5,6 @@ describe Observation do
   let(:school_name) { 'Active school'}
   let!(:school)     { create(:school, name: school_name) }
 
-  it 'can have a date in the past' do
-    expect(Observation.new(at: Date.yesterday, school: school).valid?).to be true
-  end
-
-  it 'can have a date today' do
-    expect(Observation.new(at: DateTime.now, school: school).valid?).to be true
-  end
-
-  it 'cannot have a date in the future' do
-    expect(Observation.new(at: Date.tomorrow, school: school).valid?).to be false
-  end
-
   context 'interventions' do
     let!(:intervention_type){ create(:intervention_type, score: 50) }
 
