8d1317fdd5d66af466976be842e9a45b6b8a90f5
Ian Townsend <ian.townsend@gmail.com>
2022-11-09 17:47:08 +0000

    Fix typo and add swimming pool

--- db/migrate/20221109172835_add_alternative_heating_sources_to_schools.rb
+++ db/migrate/20221109172835_add_alternative_heating_sources_to_schools.rb
@@ -1,7 +1,8 @@
 class AddAlternativeHeatingSourcesToSchools < ActiveRecord::Migration[6.0]
   def change
+    add_column :schools, :swimming_pool, :boolean, default: false, null: false
     add_column :schools, :alternative_heating_oil, :boolean, default: false, null: false
-    add_column :schools, :alternative_heating_llpg, :boolean, default: false, null: false
+    add_column :schools, :alternative_heating_lpg, :boolean, default: false, null: false
     add_column :schools, :alternative_heating_biomass, :boolean, default: false, null: false
     add_column :schools, :alternative_heating_district_heating, :boolean, default: false, null: false
   end
