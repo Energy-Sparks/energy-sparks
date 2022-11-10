be0ad98c3866d8f1b23598d3ecd643653d053f7d
Deb Bassett <deb@urbanwide.com>
2022-11-10 17:22:05 +0000

    Better define this

--- app/models/concerns/csv_exportable.rb
+++ app/models/concerns/csv_exportable.rb
@@ -2,6 +2,10 @@ module CsvExportable
   extend ActiveSupport::Concern
 
   class_methods do
+    def csv_attributes
+      []
+    end
+
     def csv_headers
       csv_attributes.map do |attr|
         (attr, relation) = attr.split('.').reverse
@@ -17,8 +21,8 @@ module CsvExportable
     def to_csv
       CSV.generate(headers: true) do |csv|
         csv << csv_headers
-        all.find_each do |response|
-          csv << csv_attributes.map { |attr| attr.split('.').inject(response, :try) }
+        all.find_each do |record|
+          csv << csv_attributes.map { |attr| attr.split('.').inject(record, :try) }
         end
       end
     end
