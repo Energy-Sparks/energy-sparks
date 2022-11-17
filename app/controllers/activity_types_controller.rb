566029a506f53231097a82c9f55479aff5d9efc9
Julian Higman <jhigman@pobox.com>
2022-11-17 16:00:02 +0000

    Update activity type controller

--- app/controllers/activity_types_controller.rb
+++ app/controllers/activity_types_controller.rb
@@ -19,10 +19,6 @@ class ActivityTypesController < ApplicationController
     @school_count = Activity.select(:school_id).where(activity_type: @activity_type).distinct.count
     if current_user_school
       @activity_type_content = load_content(@activity_type, current_user_school)
-      #ensure that embedded charts, etc are working
-      if show_data_enabled_activity_type?(@activity_type, current_user_school)
-        @activity_type_content = @activity_type_content.body.to_html.html_safe
-      end
     else
       @activity_type_content = @activity_type.description
     end
@@ -43,13 +39,9 @@ class ActivityTypesController < ApplicationController
     end
   end
 
-  def show_data_enabled_activity_type?(activity_type, school)
-    activity_type.data_driven? && !school.data_enabled?
-  end
-
   def load_content(activity_type, school)
     interpolator = TemplateInterpolation.new(activity_type, render_with: SchoolTemplate.new(school))
-    if show_data_enabled_activity_type?(activity_type, school)
+    if activity_type.data_driven? && !school.data_enabled?
       interpolator.interpolate(:description).description
     else
       interpolator.interpolate(:school_specific_description_or_fallback).school_specific_description_or_fallback
