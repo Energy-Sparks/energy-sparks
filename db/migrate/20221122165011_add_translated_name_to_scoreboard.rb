19c08ab1aa06a4f324d35e5d43efc68f11da69e3
Ian Townsend <ian.townsend@gmail.com>
2022-11-23 08:44:18 +0000

    Update migration

--- db/migrate/20221122165011_add_translated_name_to_scoreboard.rb
+++ db/migrate/20221122165011_add_translated_name_to_scoreboard.rb
@@ -1,7 +1,7 @@
 class AddTranslatedNameToScoreboard < ActiveRecord::Migration[6.0]
   def change
     Scoreboard.all.each do |scoreboard|
-      scoreboard.update(name: scoreboard[:name])
+      scoreboard.update(name_en: scoreboard[:name])
     end
   end
 end
