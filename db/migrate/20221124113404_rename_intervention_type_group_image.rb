3bc27179be484fd4787e275f26e1da93f6f7265e
Ian Townsend <ian.townsend@gmail.com>
2022-11-24 11:34:46 +0000

    Add migration

---
+++ db/migrate/20221124113404_rename_intervention_type_group_image.rb
@@ -0,0 +1,8 @@
+class RenameInterventionTypeGroupImage < ActiveRecord::Migration[6.0]
+  def up
+    ActiveStorage::Attachment.where(record_type: "InterventionTypeGroup", name: 'image').update(name: 'image_en')
+  end
+  def down
+    ActiveStorage::Attachment.where(record_type: "InterventionTypeGroup", name: 'image_en').update(name: 'image')
+  end
+end
