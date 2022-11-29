2f58bc5c78283dd4090038a7defbe8e1282395d7
Ian Townsend <iant@users.noreply.github.com>
2022-11-29 08:23:41 +0000

    Support alternative images by locale for Activity Categories (#2235)

--- app/controllers/admin/activity_categories_controller.rb
+++ app/controllers/admin/activity_categories_controller.rb
@@ -36,8 +36,8 @@ module Admin
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def activity_category_params
-      translated_params = t_params(ActivityCategory.mobility_attributes)
-      params.require(:activity_category).permit(translated_params, :name, :description, :image, :featured, :pupil, :live_data)
+      translated_params = t_params(ActivityCategory.mobility_attributes + ActivityCategory.t_attached_attributes)
+      params.require(:activity_category).permit(translated_params, :name, :description, :featured, :pupil, :live_data)
     end
   end
 end
