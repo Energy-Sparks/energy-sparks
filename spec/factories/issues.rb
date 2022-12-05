c08f4dbb9da45fb5ff38a97cf64e4d49c6935e3d
Deb Bassett <deb@urbanwide.com>
2022-12-05 15:24:33 +0000

    Rspec and tidying for filter

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     sequence(:title) {|n| "Title #{n}"}
     sequence(:description) {|n| "Description #{n}"}
     status { :open }
+    fuel_type { :gas }
     created_by { association :user }
     updated_by { association :user }
   end
