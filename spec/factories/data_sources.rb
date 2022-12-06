e7e681f030f1efbc8e58ae09bb46d4dab03d8739
Deb Bassett <deb@urbanwide.com>
2022-12-06 19:51:41 +0000

    Provide better test data & a couple of model tests

--- spec/factories/data_sources.rb
+++ spec/factories/data_sources.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :data_source do
     name { "MyString" }
-    organisation_type { "" }
-    contact_name { "MyString" }
-    email { "MyString" }
-    loa_contact_details { "MyText" }
-    data_prerequisites { "MyText" }
-    data_feed_type { "MyString" }
-    new_area_data_feed { "MyText" }
-    add_existing_data_feed { "MyText" }
-    data_issues_contact { "MyText" }
-    historic_data { "MyText" }
-    loa_expiry_procedure { "MyText" }
-    comments { "MyText" }
+    organisation_type { :energy_supplier }
+    contact_name { "Contact name" }
+    contact_email { "contact@email.com" }
+    loa_contact_details { "Some text, could include an email address" }
+    data_prerequisites { "Need to have list of MPANs and LOA" }
+    data_feed_type { "30 day rolling csv file" }
+    new_area_data_feed { "Ask the school or MAT to contact the Council" }
+    add_existing_data_feed { "Send an email to Steve" }
+    data_issues_contact_details { "Source Profile team" }
+    historic_data { "Can be accessed via portal" }
+    loa_expiry_procedure { "Send updated LOA to Source Profile team" }
+    comments { "GDST data feed covers their whole portfolio" }
   end
 end
--- spec/models/data_source_spec.rb
+++ spec/models/data_source_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe DataSource, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject { build(:data_source) }
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'enums' do
+    it { should define_enum_for(:organisation_type).with_values([:energy_supplier, :procurement_organisation, :meter_operator, :council, :solar_monitoring_provider]) }
+  end
 end
