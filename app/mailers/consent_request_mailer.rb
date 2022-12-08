4d9348daa9f8221d5f7b0026d06a92c680900028
Julian Higman <jhigman@pobox.com>
2022-12-08 16:55:09 +0000

    Wrap a locale around the emails for consent, for the moment

--- app/mailers/consent_request_mailer.rb
+++ app/mailers/consent_request_mailer.rb
@@ -1,6 +1,9 @@
 class ConsentRequestMailer < ApplicationMailer
   def request_consent
     @school = params[:school]
-    make_bootstrap_mail(to: params[:emails])
+    # ensure that emails are all in English for the moment
+    I18n.with_locale(:en) do
+      make_bootstrap_mail(to: params[:emails])
+    end
   end
 end
