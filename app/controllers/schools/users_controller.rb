1edecd6fc6b33cca08543a4165cf0b186faef940
Ian Townsend <ian.townsend@gmail.com>
2022-12-08 13:52:55 +0000

    Add preferred locale to form and table

--- app/controllers/schools/users_controller.rb
+++ app/controllers/schools/users_controller.rb
@@ -66,7 +66,7 @@ module Schools
     private
 
     def user_params
-      params.require(:user).permit(:name, :email, :staff_role_id, :role)
+      params.require(:user).permit(:name, :email, :staff_role_id, :role, :preferred_locale)
     end
   end
 end
