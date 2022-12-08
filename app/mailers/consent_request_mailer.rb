a42b790c30313892ac19e2ee03c11f3f5ce38528
Julian Higman <jhigman@pobox.com>
2022-12-08 15:55:14 +0000

    Translations and previews for consent request amd grant

--- app/mailers/consent_request_mailer.rb
+++ app/mailers/consent_request_mailer.rb
@@ -1,7 +1,6 @@
 class ConsentRequestMailer < ApplicationMailer
   def request_consent
     @school = params[:school]
-    @title = @school.name
-    make_bootstrap_mail(to: params[:emails], subject: params[:subject])
+    make_bootstrap_mail(to: params[:emails])
   end
 end
