54d0a26942cb409e2920ecad7ba7ba400aa1de8f
Julian Higman <jhigman@pobox.com>
2022-12-09 11:18:15 +0000

    Content in consent statements is translated and displayed (#2283)
    * Content in consent statements is translated and displayed
    
    * Translations and previews for consent request amd grant
    
    * Corrected identifier class for consent grant text editor
    
    * Allow translated params in controller
    
    * Wrap a locale around the emails for consent, for the moment
    
    * Check that consent statements are synced to transifex
    
    * Test that locales are only en when sending consent emails

--- app/mailers/consent_grant_mailer.rb
+++ app/mailers/consent_grant_mailer.rb
@@ -2,6 +2,9 @@ class ConsentGrantMailer < ApplicationMailer
     def email_consent
       @consent_grant = params[:consent_grant]
       @title = @consent_grant.school.name
-      make_bootstrap_mail(to: @consent_grant.user.email)
+      # ensure that emails are all in English for the moment
+      I18n.with_locale(:en) do
+        make_bootstrap_mail(to: @consent_grant.user.email)
+      end
     end
 end
--- app/mailers/consent_request_mailer.rb
+++ app/mailers/consent_request_mailer.rb
@@ -1,7 +1,9 @@
 class ConsentRequestMailer < ApplicationMailer
   def request_consent
     @school = params[:school]
-    @title = @school.name
-    make_bootstrap_mail(to: params[:emails], subject: params[:subject])
+    # ensure that emails are all in English for the moment
+    I18n.with_locale(:en) do
+      make_bootstrap_mail(to: params[:emails])
+    end
   end
 end
