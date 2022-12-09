b90ed5ed905eac69eb9e5a41dd3ac666249ec362
Julian Higman <jhigman@pobox.com>
2022-12-09 15:37:22 +0000

    Restored title to consent request email

--- app/mailers/consent_request_mailer.rb
+++ app/mailers/consent_request_mailer.rb
@@ -1,6 +1,7 @@
 class ConsentRequestMailer < ApplicationMailer
   def request_consent
     @school = params[:school]
+    @title = @school.name
     make_bootstrap_mail_en(to: params[:emails])
   end
 end
