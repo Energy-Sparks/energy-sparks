bd6201ef113d24fc0a44d736905291042f8b9fc1
Julian Higman <jhigman@pobox.com>
2022-12-13 10:58:40 +0000

    2646 translate uploading and managing bills page mailer and forms (#2287)

--- app/mailers/consent_grant_mailer.rb
+++ app/mailers/consent_grant_mailer.rb
@@ -1,10 +1,7 @@
 class ConsentGrantMailer < ApplicationMailer
-    def email_consent
-      @consent_grant = params[:consent_grant]
-      @title = @consent_grant.school.name
-      # ensure that emails are all in English for the moment
-      I18n.with_locale(:en) do
-        make_bootstrap_mail(to: @consent_grant.user.email)
-      end
-    end
+  def email_consent
+    @consent_grant = params[:consent_grant]
+    @title = @consent_grant.school.name
+    make_bootstrap_mail_en(to: @consent_grant.user.email)
+  end
 end
