e893c9ff4d26e8ac5415bf12050eb390e57c9fb9
Deb Bassett <deb@urbanwide.com>
2022-12-15 20:45:54 +0000

    Rspec for meter and school counts

--- spec/system/admin/data_sources_spec.rb
+++ spec/system/admin/data_sources_spec.rb
@@ -97,6 +97,17 @@ RSpec.describe 'Data Sources admin', :school_groups, type: :system, include_appl
             end
           end
 
+          context "Summary panel" do
+            let(:school) { create(:school) }
+            let(:active_meters) { 3.times { create(:gas_meter, active: true, data_source: existing_data_source, school: school) } }
+            let(:inactive_meters) { 2.times { create(:gas_meter, active: false, data_source: existing_data_source) } }
+            let(:setup_data) { [active_meters, inactive_meters] }
+
+            it { expect(page).to have_content("Active meters 3") }
+            it { expect(page).to have_content("Inactive meters 2") }
+            it { expect(page).to have_content("Associated schools 3") }
+          end
+
           it_behaves_like "a displayed data source" do
             let(:data_source) { existing_data_source }
           end
