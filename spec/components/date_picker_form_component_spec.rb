fa2b319d88b86872d03986630c74f6fb263ebd86
Ian Townsend <ian.townsend@gmail.com>
2022-12-22 16:38:39 +0000

    Add some tests

---
+++ spec/components/date_picker_form_component_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+include ActionView::Helpers::FormHelper
+
+RSpec.describe DatePickerFormComponent, type: :component do
+  it "renders a datepicker form component" do
+    form = ActionView::Helpers::FormBuilder.new(:job, OpenStruct.new(start_date: nil), nil, {})
+    expect(
+      render_inline(described_class.new(form: form, field: :start_date, label: 'This is the Start Date label')).to_html
+    ).to include(
+      '<input class="form-control datetimepicker-input" data-target="#datetimepickerform_start_date" type="text" name="job[start_date]" id="job_start_date">'
+    ).and include(
+      '<label class="form-label" for="job_start_date">This is the Start Date label</label>'
+    )
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'pry'
+require 'view_component/test_helpers'
 require 'capybara/rspec'
 require 'webdrivers'
 require 'capybara/email/rspec'
@@ -70,6 +71,18 @@ RSpec.configure do |config|
 
   # Wisper gem helpers
   config.include(Wisper::RSpec::BroadcastMatcher)
+
+
+  config.include ViewComponent::TestHelpers, type: :component
+  config.include Capybara::RSpecMatchers, type: :component
+
+  config.include Devise::Test::ControllerHelpers, type: :component
+
+  config.before(:each, type: :component) do
+    @request = controller.request
+  end
+
+
 end
 
 Shoulda::Matchers.configure do |config|
