66f59e634f693a094b86b91148d269c2047e0988
Ian Townsend <ian.townsend@gmail.com>
2022-12-23 10:36:25 +0000

    Update test

--- spec/components/date_picker_form_component_spec.rb
+++ spec/components/date_picker_form_component_spec.rb
@@ -8,22 +8,12 @@ RSpec.describe DatePickerFormComponent, type: :component do
       ActionController::Base.render DatePickerFormComponent.new(form: OpenStruct.new(object_name: 'job'), field_name: :start_date, value: '01/12/2022')
     ).to eq(
       <<~HTML.chomp
-        <div class="input-group date" id="datetimepickerform_start_date" data-target-input="nearest">
-          <input class="form-control datetimepicker-input" data-target="#datetimepickerform_start_date" type="text" name="job[start_date]" id="job_start_date" value="01/12/2022" />
-          <div class="input-group-append" data-target="#datetimepickerform_start_date" data-toggle="datetimepicker">
+        <div class="input-group date" id="datepickerformcomponent_start_date" data-target-input="nearest">
+          <input class="form-control datetimepicker-input" data-target="#datepickerformcomponent_start_date" type="text" name="job[start_date]" id="job_start_date" value="01/12/2022" />
+          <div class="input-group-append" data-target="#datepickerformcomponent_start_date" data-toggle="datetimepicker">
             <div class="input-group-text"><i class="fa fa-calendar"></i></div>
           </div>
         </div>
-
-        <script type="text/javascript">
-          $(function () {
-            $('#datetimepickerform_start_date').datetimepicker({
-              format: 'DD/MM/YYYY',
-              allowInputToggle: true,
-              locale: moment.locale()
-            });
-          });
-        </script>
 
       HTML
     )
