3b7bc45c3247553d009a3b2a08c750b3e4a1c35f
Ian Townsend <iant@users.noreply.github.com>
2023-01-03 15:49:10 +0000

    Translate school target email templates (#2329)

---
+++ spec/mailers/previews/target_mailer_preview.rb
@@ -0,0 +1,13 @@
+class TargetMailerPreview < ActionMailer::Preview
+  def first_target
+    TargetMailer.with(to: 'test@blah.com', school: School.first).first_target
+  end
+
+  def first_target_reminder
+    TargetMailer.with(to: 'test@blah.com', school: School.first).first_target_reminder
+  end
+
+  def review_target
+    TargetMailer.with(to: 'test@blah.com', school: SchoolTarget.last.school).review_target
+  end
+end
