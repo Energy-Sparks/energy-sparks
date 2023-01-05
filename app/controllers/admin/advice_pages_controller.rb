723fa542c7efa882db416a8dd6734ea60577f416
Julian Higman <jhigman@pobox.com>
2023-01-05 15:14:42 +0000

    Show key when editing and test restricted checkbox

--- app/controllers/admin/advice_pages_controller.rb
+++ app/controllers/admin/advice_pages_controller.rb
@@ -6,9 +6,6 @@ module Admin
     def index
     end
 
-    def show
-    end
-
     def edit
     end
 
--- spec/system/admin/advice_pages_spec.rb
+++ spec/system/admin/advice_pages_spec.rb
@@ -21,19 +21,21 @@ describe 'advice page management', type: :system do
 
       click_on 'Edit'
 
-      expect(page).to have_content('Editing Advice Page')
+      expect(page).to have_content('Editing Advice Page: baseload-summary')
 
       fill_in_trix '#advice_page_learn_more_en', with: 'english text here'
       fill_in_trix '#advice_page_learn_more_cy', with: 'welsh text here'
+      check 'Restricted'
+
       click_on 'Save'
 
       expect(page).to have_content('Advice Page updated')
 
       advice_page.reload
+      expect(advice_page.restricted).to be_truthy
       expect(advice_page.learn_more.to_s).to include('english text here')
       expect(advice_page.learn_more_en.to_s).to include('english text here')
       expect(advice_page.learn_more_cy.to_s).to include('welsh text here')
     end
-
   end
 end
