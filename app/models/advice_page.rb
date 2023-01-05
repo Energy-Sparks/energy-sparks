bf2fdf3e03399a78a49d8490a5ccd0b56f34e275
Julian Higman <jhigman@pobox.com>
2023-01-05 12:30:39 +0000

    Annotated advice page model

--- app/models/advice_page.rb
+++ app/models/advice_page.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: advice_pages
+#
+#  created_at :datetime         not null
+#  id         :bigint(8)        not null, primary key
+#  key        :string           not null
+#  restricted :boolean          default(FALSE)
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_advice_pages_on_key  (key) UNIQUE
+#
 class AdvicePage < ApplicationRecord
   extend Mobility
   include TransifexSerialisable
