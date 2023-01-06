cbe5df75e4530bc62ab3e73c9d4f6d578dd1ec04
Julian Higman <jhigman@pobox.com>
2023-01-06 14:38:55 +0000

    Initial controller and views, task to add baseload advice page

--- app/controllers/schools/advice_controller.rb
+++ app/controllers/schools/advice_controller.rb
@@ -1,5 +1,13 @@
 module Schools
   class AdviceController < ApplicationController
     load_and_authorize_resource :school
+
+    def index
+    end
+
+    def show
+      @advice_page = AdvicePage.find_by_key(params[:key])
+      @tab = params[:tab] || 'insights'
+    end
   end
 end
