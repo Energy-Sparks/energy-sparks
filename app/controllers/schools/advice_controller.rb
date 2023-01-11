bafb3a0bd6e93bdeb963143e7c3439f806989563
Julian Higman <jhigman@pobox.com>
2023-01-11 12:22:03 +0000

    Added restriction for advice page (#2346)
    * Added restriction for advice page
    
    * Name tweak

--- app/controllers/schools/advice_controller.rb
+++ app/controllers/schools/advice_controller.rb
@@ -1,6 +1,10 @@
 module Schools
   class AdviceController < ApplicationController
     load_and_authorize_resource :school
+    skip_before_action :authenticate_user!
+
+    before_action :load_advice_page, only: :show
+    before_action :check_authorisation, only: :show
 
     def index
       @advice_pages = AdvicePage.all
@@ -8,8 +12,19 @@ module Schools
 
     def show
       @advice_pages = AdvicePage.all
-      @advice_page = AdvicePage.find_by_key(params[:key])
       @tab = params[:tab] || 'insights'
+    end
+
+    private
+
+    def load_advice_page
+      @advice_page = AdvicePage.find_by_key(params[:key])
+    end
+
+    def check_authorisation
+      if @advice_page.restricted && cannot?(:read_restricted_advice, @school)
+        redirect_to school_advice_path(@school), notice: 'Only an admin or staff user for this school can access this content'
+      end
     end
   end
 end
