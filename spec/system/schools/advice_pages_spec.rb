d549c9884862ab31b68271e4f634e2bc0b7b94ad
Deb Bassett <deb@urbanwide.com>
2023-01-12 11:46:49 +0000

    Remove comment

--- spec/system/schools/advice_pages_spec.rb
+++ spec/system/schools/advice_pages_spec.rb
@@ -63,8 +63,6 @@ RSpec.describe "advice page", type: :system do
       click_on key
       within '.advice-page-nav' do
         expect(page).to have_content("Menu")
-        # remove for now as I need to check if menu items are auto generated
-        # expect(page).to have_link(key)
       end
     end
 
