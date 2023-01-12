140c739f76d1c3db4a035feaa66ac9da4fac8298
Julian Higman <jhigman@pobox.com>
2023-01-12 14:43:19 +0000

    Use polymorphic path to render paths

--- app/helpers/advice_page_helper.rb
+++ app/helpers/advice_page_helper.rb
@@ -1,4 +1,8 @@
 module AdvicePageHelper
+  def advice_page_path(school, advice_page, tab = nil)
+    polymorphic_path([tab, school, :advice, advice_page.key.to_sym])
+  end
+
   def advice_baseload_high?(val)
     val > 0.0
   end
