125718c4da09b96e789367a687566cb4822bbb22
Julian Higman <jhigman@pobox.com>
2023-01-13 14:08:14 +0000

    TOC is linked to anchors

--- app/helpers/advice_page_helper.rb
+++ app/helpers/advice_page_helper.rb
@@ -3,6 +3,18 @@ module AdvicePageHelper
     polymorphic_path([tab, school, :advice, advice_page.key.to_sym])
   end
 
+  def chart_start_month_year(date = Time.zone.today)
+    month_year(date.last_month - 1.year)
+  end
+
+  def chart_end_month_year(date = Time.zone.today)
+    month_year(date.last_month)
+  end
+
+  def month_year(date)
+    I18n.t('date.month_names')[date.month] + " " + date.year.to_s
+  end
+
   def advice_baseload_high?(val)
     val > 0.0
   end
