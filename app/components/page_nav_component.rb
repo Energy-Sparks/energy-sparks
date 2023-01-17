0a28530650fc8bc783a8d8812c5b782ff3bfff47
Deb Bassett <deb@urbanwide.com>
2023-01-17 16:00:55 +0000

    Re-jig again for new changes. Css still not right after changes

--- app/components/page_nav_component.rb
+++ app/components/page_nav_component.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 class PageNavComponent < ViewComponent::Base
-  renders_many :sections, 'SectionComponent'
+  renders_many :sections, ->(**args) do
+    args[:options] = options
+    SectionComponent.new(**args)
+  end
 
-  attr_reader :name, :bgcolor, :href
+  attr_reader :name, :bgcolor, :href, :options
 
-  def initialize(name: "Menu", icon: 'home', bgcolor: nil, href: nil)
+  def initialize(name: "Menu", icon: 'home', bgcolor: nil, href: nil, options: {})
     @name = name
     @icon = icon
     @bgcolor = bgcolor
     @href = href
+    @options = options
   end
 
   def icon
@@ -17,19 +21,23 @@ class PageNavComponent < ViewComponent::Base
   end
 
   class SectionComponent < ViewComponent::Base
-    renders_many :items, 'PageNavComponent::ItemComponent'
+    renders_many :items, ->(**args) do
+      args[:options] = options
+      PageNavComponent::ItemComponent.new(**args)
+    end
 
-    attr_reader :bgcolor, :icon, :name, :visible
+    attr_reader :bgcolor, :icon, :name, :visible, :options
 
-    def initialize(name: nil, bgcolor: nil, icon: nil, visible: true)
+    def initialize(name: nil, bgcolor: nil, icon: nil, visible: true, options: {})
       @name = name
       @bgcolor = bgcolor
       @icon = icon
       @visible = visible
+      @options = options
     end
 
     def call
-      args = { class: 'small nav-link toggler', 'data-toggle': 'collapse', 'data-target': "##{id}" }
+      args = { class: 'nav-link small toggler', 'data-toggle': 'collapse', 'data-target': "##{id}" }
       args[:style] = "background-color: #{bgcolor};" if bgcolor
       link_to(name_text.html_safe, "##{id}", args)
     end
@@ -50,21 +58,50 @@ class PageNavComponent < ViewComponent::Base
   end
 
   class ItemComponent < ViewComponent::Base
-    attr_reader :name, :href
+    attr_reader :name, :href, :options
 
-    def initialize(name:, href:)
+    def initialize(name:, href:, options: { match_controller: false })
       @name = name
       @href = href
+      @options = options
+    end
+
+    def current_controller?(href)
+      controller_path == Rails.application.routes.recognize_path(href)[:controller]
     end
 
     def call
-      args = { class: "small nav-link" }
-      args[:class] += ' current' if current_page?(href)
+      args = { class: "nav-link small" }
+      if (options[:match_controller] && current_controller?(href)) || !options[:match_controller] && current_page?(href)
+        args[:class] += ' current'
+      end
       link_to(name, href, args)
     end
 
     def render?
       name
+    end
+  end
+
+  class CollapseButton < ViewComponent::Base
+    attr_reader :display
+
+    def initialize(icon: 'bars', display: 'block')
+      @icon = icon
+      @display = display
+    end
+
+    def id
+      'page-nav'
+    end
+
+    def icon
+      @icon ? helpers.fa_icon(@icon) : ''
+    end
+
+    def call
+      args = { class: "nav-link d-md-none d-#{display}", 'data-toggle': 'collapse', 'data-target': "##{id}" }
+      link_to(icon, '', args)
     end
   end
 end
