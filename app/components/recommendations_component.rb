6dc8f54c87625886a41c6e96311e7740d31695f2
Deb Bassett <deb@urbanwide.com>
2023-01-25 23:29:15 +0000

    Simplify things

--- app/components/recommendations_component.rb
+++ app/components/recommendations_component.rb
@@ -1,38 +1,14 @@
 # frozen_string_literal: true
 
 class RecommendationsComponent < ViewComponent::Base
-  attr_reader :recommendations
+  attr_reader :title, :recommendations
 
-  def initialize(recommendations: [])
+  def initialize(title: nil, recommendations: [])
+    @title = title
     @recommendations = recommendations
   end
 
-  class ImageComponent < ViewComponent::Base
-    attr_reader :recommendation
-
-    def initialize(recommendation, classes: '')
-      @recommendation = recommendation
-      @classes = classes
-    end
-
-    def classes
-      css_classes = 'card-img-top img-fluid'
-      css_classes += " #{@classes}" if @classes
-      css_classes
-    end
-
-    def image
-      if I18n.locale.to_s == 'cy' && activity_type.t_attached(:image, :cy).present?
-        recommendation.image_cy
-      elsif recommendation.t_attached(:image, :en).present?
-         recommendation.image_en
-      else
-        "placeholder300x200.png"
-      end
-    end
-
-    def call
-      image_tag image, class: classes
-    end
+  def render?
+    recommendations.any?
   end
 end
