fdba50b0f31a38e435db41008709aae575c945d6
Julian Higman <jhigman@pobox.com>
2023-02-02 10:12:16 +0000

    Simple test

--- spec/components/chart_component_spec.rb
+++ spec/components/chart_component_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe ChartComponent, type: :component, include_url_helpers: true do
   let(:school) { create(:school) }
   let(:html) { render_inline(ChartComponent.new(**params)) }
   let(:chart_type) { :baseload }
-  let(:all_params) { { school: school, chart_type: chart_type, title: 'Title text', subtitle: 'Subtitle text' } }
+  let(:all_params) { { school: school, chart_type: chart_type, title: 'Title text', subtitle: 'Subtitle text', html_class: 'usage-chart' } }
 
   context "with all params" do
     let(:params) { all_params }
     it { expect(html).to have_selector("h4", text: "Title text") }
     it { expect(html).to have_selector("h5", text: "Subtitle text") }
     it { expect(html).to have_selector("div", id: "chart_wrapper_baseload") }
+    it { expect(html).to have_selector("div", class: "usage-chart") }
   end
 end
