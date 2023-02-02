4580bbdc67f95654db5c567fffafd2374d5351d3
Julian Higman <jhigman@pobox.com>
2023-02-02 10:10:11 +0000

    Replace all analysis charts with component and tweak for usage charts

--- app/components/chart_component.rb
+++ app/components/chart_component.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ChartComponent < ViewComponent::Base
-  attr_reader :school, :title, :subtitle, :chart_type, :chart_config, :analysis_controls, :no_zoom
+  attr_reader :school, :title, :subtitle, :chart_type, :chart_config, :analysis_controls, :no_zoom, :html_class
 
   include ChartHelper
 
-  def initialize(chart_type: '', school: nil, title: '', subtitle: '', chart_config: {}, analysis_controls: false, no_zoom: true)
+  def initialize(chart_type: '', school: nil, title: '', subtitle: '', chart_config: {}, analysis_controls: false, no_zoom: true, html_class: 'analysis-chart')
     @chart_type = chart_type
     @school = school
     @title = title
@@ -13,5 +13,6 @@ class ChartComponent < ViewComponent::Base
     @chart_config = chart_config
     @analysis_controls = analysis_controls
     @no_zoom = no_zoom
+    @html_class = html_class
   end
 end
