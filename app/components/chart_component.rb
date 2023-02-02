aa7f993d37a4ff1e0cad3cd8b499184bb043d3c5
Julian Higman <jhigman@pobox.com>
2023-02-02 10:52:34 +0000

    Title and subtitle are slots

--- app/components/chart_component.rb
+++ app/components/chart_component.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class ChartComponent < ViewComponent::Base
+  renders_one :title
+  renders_one :subtitle
   renders_one :header
   renders_one :footer
 
-  attr_reader :school, :title, :subtitle, :chart_type, :chart_config, :analysis_controls, :no_zoom, :html_class
+  attr_reader :school, :chart_type, :chart_config, :analysis_controls, :no_zoom, :html_class
 
   include ChartHelper
 
-  def initialize(chart_type: '', school: nil, title: '', subtitle: '', chart_config: {}, analysis_controls: false, no_zoom: true, html_class: 'analysis-chart')
+  def initialize(chart_type: '', school: nil, chart_config: {}, analysis_controls: false, no_zoom: true, html_class: 'analysis-chart')
     @chart_type = chart_type
     @school = school
-    @title = title
-    @subtitle = subtitle
     @chart_config = chart_config
     @analysis_controls = analysis_controls
     @no_zoom = no_zoom
--- spec/components/chart_component_spec.rb
+++ spec/components/chart_component_spec.rb
@@ -5,23 +5,25 @@ require "rails_helper"
 RSpec.describe ChartComponent, type: :component, include_url_helpers: true do
   let(:school) { create(:school) }
   let(:chart_type) { :baseload }
-  let(:params) { { school: school, chart_type: chart_type, title: 'Title text', subtitle: 'Subtitle text', html_class: 'usage-chart' } }
+  let(:params) { { school: school, chart_type: chart_type, html_class: 'usage-chart' } }
 
   context "with all params" do
     let(:html) { render_inline(ChartComponent.new(**params)) }
-    it { expect(html).to have_selector("h4", text: "Title text") }
-    it { expect(html).to have_selector("h5", text: "Subtitle text") }
     it { expect(html).to have_selector("div", id: "chart_wrapper_baseload") }
     it { expect(html).to have_selector("div", class: "usage-chart") }
   end
 
-  context "with header and footer slots" do
+  context "with title, subtitle, header and footer slots" do
     let(:html) do
       render_inline ChartComponent.new(**params) do |c|
-        c.with_header { "I'm a header" }
-        c.with_footer { "I'm a footer" }
+        c.with_title    { "I'm a title" }
+        c.with_subtitle { "I'm a subtitle" }
+        c.with_header   { "I'm a header" }
+        c.with_footer   { "I'm a footer" }
       end
     end
+    it { expect(html).to have_selector("h4", text: "I'm a title") }
+    it { expect(html).to have_selector("h5", text: "I'm a subtitle") }
     it { expect(html).to have_selector("p", text: "I'm a header") }
     it { expect(html).to have_selector("p", text: "I'm a footer") }
   end
