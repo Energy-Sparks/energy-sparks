742db12c91dffb63636830792ad52f4dfb5ba11f
Julian Higman <jhigman@pobox.com>
2023-02-03 17:22:11 +0000

    Tidied controller, use only filterable meters, pass text as header

--- app/controllers/schools/advice/electricity_recent_changes_controller.rb
+++ app/controllers/schools/advice/electricity_recent_changes_controller.rb
@@ -7,49 +7,19 @@ module Schools
       end
 
       def analysis
-        set_measurement_options
-        @measurement = measurement_unit(params[:measurement])
-
-        @supply = :electricity
-        @period = get_period
-        @split_meters = params[:split_meters].present?
-        @show_measurements = @period == :weekly
-
-        if @school.send("has_#{@supply}?")
-          @meters = setup_meters(@school, @supply)
-          @chart_config = setup_chart_config(@supply)
-          @title_key = title_key(@supply, @period, @split_meters)
-          # render :show
-        else
-          redirect_to school_path(@school), notice: 'No suitable supply could be found'
-        end
+        @meters = @school.filterable_meters.electricity
+        @chart_config = setup_chart_config
       end
 
       private
 
-      def setup_chart_config(_supply)
+      def setup_chart_config
         {
           weekly: :calendar_picker_electricity_week_example_comparison_chart,
           daily: :calendar_picker_electricity_day_example_comparison_chart,
           earliest_reading:  aggregate_school.aggregate_meter(:electricity).amr_data.start_date,
           last_reading:  aggregate_school.aggregate_meter(:electricity).amr_data.end_date,
         }
-      end
-
-      def setup_meters(school, _supply)
-        # school.filterable_meters.electricity
-        school.meters.electricity
-      end
-
-      def title_key(supply, period, split_meters)
-        "charts.usage.titles.#{supply}.#{period}.#{split_meters ? 'split' : 'not_split'}"
-      end
-
-      def get_period
-        return :weekly
-        # period = params.require(:period).to_sym
-        # raise ActionController::RoutingError, "Period #{period} not valid" unless [:weekly, :daily].include?(period)
-        # period
       end
 
       def advice_page_key
