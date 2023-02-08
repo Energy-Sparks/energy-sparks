aa8ad7373f061bc705a38133f779821ea5b25092
Ian Townsend <ian.townsend@gmail.com>
2023-02-08 10:33:23 +0000

    Normalise i18n

--- app/controllers/schools/advice/electricity_out_of_hours_controller.rb
+++ app/controllers/schools/advice/electricity_out_of_hours_controller.rb
@@ -1,20 +1,28 @@
 module Schools
   module Advice
     class ElectricityOutOfHoursController < AdviceBaseController
+      before_action :set_annual_usage_breakdown
+      before_action :set_usage_categories
+
       def insights
       end
 
       def analysis
-        @usage_categories = set_usage_categories
-        @annual_usage_breakdown = ::Usage::AnnualUsageBreakdownService.new(meter_collection: aggregate_school, fuel_type: :electricity).usage_breakdown
       end
 
       private
 
+      def set_annual_usage_breakdown
+        @annual_usage_breakdown = ::Usage::AnnualUsageBreakdownService.new(
+          meter_collection: aggregate_school,
+          fuel_type: :electricity
+        ).usage_breakdown
+      end
+
       def set_usage_categories
-        usage_categories = [:holiday, :weekend, :school_day_open, :school_day_closed]
-        usage_categories += [:community] if @school.school_times.community_use.any?
-        usage_categories
+        @usage_categories = [:holiday, :weekend, :school_day_open, :school_day_closed]
+        @usage_categories += [:community] if @school.school_times.community_use.any?
+        @usage_categories
       end
 
       def advice_page_key
