dc4239766e85087489aca7c673990db471a07944
Ian Townsend <ian.townsend@gmail.com>
2023-02-08 09:23:43 +0000

    Extract table

--- app/controllers/schools/advice/electricity_out_of_hours_controller.rb
+++ app/controllers/schools/advice/electricity_out_of_hours_controller.rb
@@ -5,10 +5,17 @@ module Schools
       end
 
       def analysis
+        @usage_categories = set_usage_categories
         @annual_usage_breakdown = ::Usage::AnnualUsageBreakdownService.new(meter_collection: aggregate_school, fuel_type: :electricity).usage_breakdown
       end
 
       private
+
+      def set_usage_categories
+        usage_categories = [:holidays, :weekends, :school_day_open, :school_day_closed]
+        usage_categories += [:community] if @school.school_times.community_use.any?
+        usage_categories
+      end
 
       def advice_page_key
         :electricity_out_of_hours
