e76a8030c34d32376b602de0fb1fdb2584ab21aa
Julian Higman <jhigman@pobox.com>
2023-02-08 14:37:30 +0000

    Added recent weeks, days and degree days charts

--- app/controllers/schools/advice/gas_recent_changes_controller.rb
+++ app/controllers/schools/advice/gas_recent_changes_controller.rb
@@ -5,9 +5,18 @@ module Schools
       end
 
       def analysis
+        @meters = @school.filterable_meters.gas
+        @chart_config = setup_chart_config
       end
 
       private
+
+      def setup_chart_config
+        {
+          earliest_reading:  aggregate_school.aggregate_meter(:gas).amr_data.start_date,
+          last_reading:  aggregate_school.aggregate_meter(:gas).amr_data.end_date,
+        }
+      end
 
       def advice_page_key
         :gas_recent_changes
