bd03dd0c5046921428d0539fd2bcd35463e5c41f
Julian Higman <jhigman@pobox.com>
2023-02-09 13:01:52 +0000

    Refactor to reuse analysis dates

--- app/controllers/schools/advice/electricity_intraday_controller.rb
+++ app/controllers/schools/advice/electricity_intraday_controller.rb
@@ -16,18 +16,6 @@ module Schools
         )
       end
 
-      def analysis_dates
-        start_date = aggregate_school.aggregated_electricity_meters.amr_data.start_date
-        end_date = aggregate_school.aggregated_electricity_meters.amr_data.end_date
-        OpenStruct.new(
-          start_date: start_date,
-          end_date: end_date,
-          one_years_data: one_years_data?(start_date, end_date),
-          recent_data: recent_data?(end_date),
-          months_analysed: months_analysed(start_date, end_date)
-        )
-      end
-
       def advice_page_key
         :electricity_intraday
       end
