72fce40423e042be21332ba892429ff07486ef7e
Julian Higman <jhigman@pobox.com>
2023-02-09 11:49:09 +0000

    Added spec and set up initial dates for 7 day range

--- spec/system/schools/advice_pages/electricity_intraday_spec.rb
+++ spec/system/schools/advice_pages/electricity_intraday_spec.rb
@@ -19,10 +19,32 @@ RSpec.describe "electricity intraday advice page", type: :system do
       before { click_on 'Insights' }
       it_behaves_like "an advice page tab", tab: "Insights"
     end
+
     context "clicking the 'Analysis' tab" do
-      before { click_on 'Analysis' }
+      let(:start_date)          { Date.parse('20190101')}
+      let(:end_date)            { Date.parse('20210101')}
+      let(:amr_data)            { double('amr-data', start_date: start_date, end_date: end_date) }
+      let(:aggregate_meter)     { double('aggregate_meter', amr_data: amr_data) }
+      let(:aggregate_school)    { double('meter-collection') }
+
+      before do
+        allow(aggregate_school).to receive(:aggregated_electricity_meters).and_return(aggregate_meter)
+        allow(aggregate_school).to receive(:aggregate_meter).and_return(aggregate_meter)
+        allow_any_instance_of(AggregateSchoolService).to receive(:aggregate_school).and_return(aggregate_school)
+        click_on 'Analysis'
+      end
+
       it_behaves_like "an advice page tab", tab: "Analysis"
+
+      it "shows titles" do
+        expect(page).to have_content("Electricity consumption during the school day over the last 12 months")
+      end
+
+      it "shows dates for 7 day period up to end date" do
+        expect(page).to have_content("graph shows how the electricity consumption for your school varies during the day between 26 Dec 2020 and 01 Jan 2021")
+      end
     end
+
     context "clicking the 'Learn More' tab" do
       before { click_on 'Learn More' }
       it_behaves_like "an advice page tab", tab: "Learn More"
