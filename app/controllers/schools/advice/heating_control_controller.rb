e70255f1938de22a93691cee4c98ddbf8601ac2a
Leigh Dodds <leigh@ldodds.com>
2023-02-14 16:09:00 +0000

    Add intraday chart

--- app/controllers/schools/advice/heating_control_controller.rb
+++ app/controllers/schools/advice/heating_control_controller.rb
@@ -6,11 +6,35 @@ module Schools
 
       def analysis
         @multiple_meters = multiple_meters?
+        @analysis_dates = analysis_dates
         @last_week_start_times = heating_control_service.last_week_start_times
         @estimated_savings = heating_control_service.estimated_savings
       end
 
       private
+
+      def analysis_dates
+        start_date = aggregate_meter.amr_data.start_date
+        end_date = aggregate_meter.amr_data.end_date
+        OpenStruct.new(
+          start_date: start_date,
+          end_date: end_date,
+          one_year_before_end: end_date - 1.year,
+          last_full_week_start_date: last_full_week_start_date(end_date),
+          last_full_week_end_date: last_full_week_end_date(end_date),
+          recent_data: recent_data?(end_date)
+        )
+      end
+
+      #for charts that use the last full week
+      def last_full_week_start_date(end_date)
+        end_date.prev_year.end_of_week
+      end
+
+      #for charts that use the last full week
+      def last_full_week_end_date(end_date)
+        end_date.prev_week.end_of_week - 1
+      end
 
       def advice_page_key
         :heating_control
@@ -18,6 +42,10 @@ module Schools
 
       def multiple_meters?
         @school.meters.active.gas.count > 1
+      end
+
+      def aggregate_meter
+        aggregate_school.aggregated_heat_meters
       end
 
       def heating_control_service
