42d534625677425f784472f0b35b311e3eee27ea
Ian Townsend <ian.townsend@gmail.com>
2023-02-14 16:46:44 +0000

    Update gemfile and use new calculator

--- app/controllers/schools/advice/electricity_recent_changes_controller.rb
+++ app/controllers/schools/advice/electricity_recent_changes_controller.rb
@@ -2,10 +2,7 @@ module Schools
   module Advice
     class ElectricityRecentChangesController < AdviceBaseController
       def insights
-        @recent_usage = ::Usage::RecentUsageComparisonService.new(
-          meter_collection: aggregate_school,
-          fuel_type: :electricity
-          ).recent_usage
+        @recent_usage = build_recent_usage
       end
 
       def analysis
@@ -14,6 +11,36 @@ module Schools
       end
 
       private
+
+      def build_recent_usage
+        OpenStruct.new(
+          last_week: last_week,
+          previous_week: previous_week,
+          change: change
+        )
+      end
+
+      def change
+        Usage::CombinedUsageMetricComparison.new(
+          last_week.combined_usage_metric,
+          previous_week.combined_usage_metric
+        ).compare
+      end
+
+      def last_week
+        @last_week ||= recent_usage_calculation.recent_usage(period_range: 0..0)
+      end
+
+      def previous_week
+        @previous_week ||= recent_usage_calculation.recent_usage(period_range: -1..-1)
+      end
+
+      def recent_usage_calculation
+        @recent_usage_calculation ||= Usage::RecentUsagePeriodCalculationService.new(
+          meter_collection: aggregate_school,
+          fuel_type: :electricity
+        )
+      end
 
       def advice_page_key
         :electricity_recent_changes
