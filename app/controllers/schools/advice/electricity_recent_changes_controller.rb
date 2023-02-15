aba853387d9f43d53debc789d4ef3cda3ebcaa8e
Ian Townsend <ian.townsend@gmail.com>
2023-02-15 09:30:43 +0000

    Remove analysis dates method

--- app/controllers/schools/advice/electricity_recent_changes_controller.rb
+++ app/controllers/schools/advice/electricity_recent_changes_controller.rb
@@ -13,18 +13,6 @@ module Schools
 
       private
 
-      def analysis_dates
-        start_date = aggregate_school.aggregated_electricity_meters.amr_data.start_date
-        end_date = aggregate_school.aggregated_electricity_meters.amr_data.end_date
-        OpenStruct.new(
-          start_date: start_date,
-          end_date: end_date,
-          one_years_data: one_years_data?(start_date, end_date),
-          recent_data: recent_data?(end_date),
-          months_analysed: months_analysed(start_date, end_date)
-        )
-      end
-
       def build_recent_usage
         OpenStruct.new(
           last_week: last_week,
