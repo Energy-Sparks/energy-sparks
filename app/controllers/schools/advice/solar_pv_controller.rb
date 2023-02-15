f86a8ef5ce7faeb83fa97af18a627cd106cf68fc
Ian Townsend <ian.townsend@gmail.com>
2023-02-15 12:25:16 +0000

    Implement solar PV page

--- app/controllers/schools/advice/solar_pv_controller.rb
+++ app/controllers/schools/advice/solar_pv_controller.rb
@@ -9,6 +9,12 @@ module Schools
 
       private
 
+      def check_has_fuel_type
+        # Skip fuel type check here as there are two versions of the solar pv page:
+        # one version for when the school has solar pv, the other for when they don’t.
+        true
+      end
+
       def advice_page_key
         :solar_pv
       end
