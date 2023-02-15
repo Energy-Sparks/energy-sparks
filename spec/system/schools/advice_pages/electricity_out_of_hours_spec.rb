b89e81b2e6af7f1b5c2c5906a7b55092375c68ad
Ian Townsend <ian.townsend@gmail.com>
2023-02-15 08:34:09 +0000

    Improve system tests for electricity and gas out of hours advice pages

--- spec/system/schools/advice_pages/electricity_out_of_hours_spec.rb
+++ spec/system/schools/advice_pages/electricity_out_of_hours_spec.rb
@@ -9,7 +9,26 @@ RSpec.describe "electricity out of hours advice page", type: :system do
     let(:user)  { create(:school_admin, school: school) }
 
     before do
-      allow_any_instance_of(Usage::AnnualUsageBreakdownService).to receive(:usage_breakdown) { nil }
+      combined_usage_metric = CombinedUsageMetric.new(
+        £: 12.0,
+        kwh: 12.0,
+        co2: 12.0,
+        percent: 0.4
+      )
+
+      allow_any_instance_of(Usage::AnnualUsageBreakdownService).to receive(:usage_breakdown) do
+        Usage::AnnualUsageCategoryBreakdown.new(
+          holiday: combined_usage_metric,
+          school_day_closed: combined_usage_metric,
+          school_day_open: combined_usage_metric,
+          weekend: combined_usage_metric,
+          out_of_hours: combined_usage_metric,
+          community: combined_usage_metric,
+          fuel_type: :electricity
+        )
+      end
+      allow_any_instance_of(Usage::AnnualUsageCategoryBreakdown).to receive(:total) { combined_usage_metric }
+      allow_any_instance_of(Usage::AnnualUsageCategoryBreakdown).to receive(:potential_savings) { combined_usage_metric }
 
       sign_in(user)
       visit school_advice_electricity_out_of_hours_path(school)
@@ -20,11 +39,25 @@ RSpec.describe "electricity out of hours advice page", type: :system do
     context "clicking the 'Insights' tab" do
       before { click_on 'Insights' }
       it_behaves_like "an advice page tab", tab: "Insights"
+
+      it 'shows expected content' do
+        expect(page).to have_content('What is out of hours usage?')
+        expect(page).to have_content('Your out of hours usage')
+        expect(page).to have_content('How do you compare?')
+        expect(page).to have_content('What should you do next?')
+      end
     end
+
     context "clicking the 'Analysis' tab" do
       before { click_on 'Analysis' }
       it_behaves_like "an advice page tab", tab: "Analysis"
+
+      it 'shows expected content' do
+        expect(page).to have_content('Last 12 months')
+        expect(page).to have_content('Usage by day of week')
+      end
     end
+
     context "clicking the 'Learn More' tab" do
       before { click_on 'Learn More' }
       it_behaves_like "an advice page tab", tab: "Learn More"
