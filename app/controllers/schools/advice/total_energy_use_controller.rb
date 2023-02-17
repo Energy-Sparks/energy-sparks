149db1915742acfea34c0284a68f3351f4f8715a
Leigh Dodds <leigh@ldodds.com>
2023-02-17 13:07:55 +0000

    Add first chart

--- app/controllers/schools/advice/total_energy_use_controller.rb
+++ app/controllers/schools/advice/total_energy_use_controller.rb
@@ -5,12 +5,25 @@ module Schools
       end
 
       def analysis
+        @analysis_dates = analysis_dates
       end
 
       private
 
       def advice_page_key
         :total_energy_use
+      end
+
+      def aggregate_meters
+        [aggregate_school.aggregated_electricity_meters, aggregate_school.aggregated_heat_meters].compact
+      end
+
+      def analysis_start_date
+        analysis_end_date - 1.year
+      end
+
+      def analysis_end_date
+        aggregate_meters.map { |meter| meter.amr_data.end_date }.min
       end
     end
   end
