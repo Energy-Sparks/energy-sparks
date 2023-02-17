a308ea527a86a10ae6d3e3abf992e0f115a4778b
Ian Townsend <ian.townsend@gmail.com>
2023-02-17 14:49:41 +0000

    Implement thermostatic analysis page

--- app/controllers/schools/advice/thermostatic_control_controller.rb
+++ app/controllers/schools/advice/thermostatic_control_controller.rb
@@ -2,12 +2,21 @@ module Schools
   module Advice
     class ThermostaticControlController < AdviceBaseController
       def insights
+        @heating_thermostatic_analysis = build_heating_thermostatic_analysis
       end
 
       def analysis
       end
 
       private
+
+      def build_heating_thermostatic_analysis
+        Heating::HeatingThermostaticAnalysisService.new(meter_collection: aggregate_school).create_model
+      end
+
+      def set_insights_next_steps
+        @advice_page_insights_next_steps = t("advice_pages.#{advice_page_key}.insights.next_steps_html").html_safe
+      end
 
       def advice_page_key
         :thermostatic_control
