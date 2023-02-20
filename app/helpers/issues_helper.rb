73a1e53eba365358e247a9e5e23735625cc32fbc
Deb Bassett <deb@urbanwide.com>
2023-02-20 22:22:05 +0000

    Use icons to make things more compact

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,6 +1,20 @@
 module IssuesHelper
-  def issue_type_icon(issue_type)
-    fa_icon("#{Issue.issue_type_image(issue_type)} text-secondary")
+  def issue_type_icon(issue_type, count = 0)
+    colour = count > 0 ? Issue.issue_type_classes[issue_type.to_sym] : "secondary"
+    fa_icon("#{Issue.issue_type_image(issue_type)} text-#{colour}")
+  end
+
+  def issue_type_icons(issues, hide_empty: false)
+    counts = []
+    icons = []
+    Issue.issue_types.each_key do |issue_type|
+      count = issues.for_issue_types(issue_type).count
+      counts << pluralize(count, issue_type.to_s) unless count == 0 && hide_empty
+      icons << issue_type_icon(issue_type, count) unless count == 0 && hide_empty
+    end
+    content_tag(:span, title: counts.join(" & "), data: { toggle: "tooltip" }) do
+      icons.join(" ").html_safe
+    end
   end
 
   def issueable_icon(issueable)
