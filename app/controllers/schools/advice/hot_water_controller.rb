1c390be1984e74bdf4934c7cf8603da7e298c385
Ian Townsend <ian.townsend@gmail.com>
2023-02-21 14:49:10 +0000

    Implement hot water advice page

--- app/controllers/schools/advice/hot_water_controller.rb
+++ app/controllers/schools/advice/hot_water_controller.rb
@@ -9,6 +9,10 @@ module Schools
 
       private
 
+      def set_insights_next_steps
+        @advice_page_insights_next_steps = t("advice_pages.#{advice_page_key}.insights.next_steps_html").html_safe
+      end
+
       def advice_page_key
         :hot_water
       end
