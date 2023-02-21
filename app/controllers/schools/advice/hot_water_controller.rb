b2c78c6220539a7385bf07ef30e2642a1c8b6ce2
Ian Townsend <ian.townsend@gmail.com>
2023-02-21 17:36:44 +0000

    Some more text updates

--- app/controllers/schools/advice/hot_water_controller.rb
+++ app/controllers/schools/advice/hot_water_controller.rb
@@ -2,12 +2,17 @@ module Schools
   module Advice
     class HotWaterController < AdviceBaseController
       def insights
+        @gas_hot_water = gas_hot_water_service.create_model
       end
 
       def analysis
       end
 
       private
+
+      def gas_hot_water_service
+        @gas_hot_water_service ||= HotWater::GasHotWaterService.new(meter_collection: aggregate_school)
+      end
 
       def set_insights_next_steps
         @advice_page_insights_next_steps = t("advice_pages.#{advice_page_key}.insights.next_steps_html").html_safe
