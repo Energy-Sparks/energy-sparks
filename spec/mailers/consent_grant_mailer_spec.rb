b4ff23a128d888e2d167d6bc65916fd264a304aa
Julian Higman <jhigman@pobox.com>
2023-02-23 10:36:39 +0000

    Use locale mailer for consent grant mailer

--- spec/mailers/consent_grant_mailer_spec.rb
+++ spec/mailers/consent_grant_mailer_spec.rb
@@ -2,43 +2,39 @@ require 'rails_helper'
 
 RSpec.describe ConsentGrantMailer do
 
-  let(:user) { create(:user) }
+  let(:user) { create(:user, preferred_locale: :cy) }
   let(:school) { create(:school, name: 'Test School') }
   let(:consent_grant) { create(:consent_grant, user: user, school: school) }
 
-  describe '#email_consent' do
-
-    context 'when locale is cy' do
-
-      let(:cy_translations) do
-        {
-          consent_grant_mailer:
-            {
-              email_consent:
-                {
-                  subject: "Welsh subject line should not be used (yet)",
-                  message_1: "Welsh message_1 for %{school_name} should not be used (yet)"
-                }
-            }
-        }
+  around do |example|
+    ClimateControl.modify SEND_AUTOMATED_EMAILS: 'true' do
+      ClimateControl.modify FEATURE_FLAG_EMAILS_WITH_PREFERRED_LOCALE: enable_locale_emails do
+        example.run
       end
+    end
+  end
 
-      before do
-        I18n.locale = 'cy'
-        I18n.backend.store_translations("cy", cy_translations)
+  before :each do
+    ConsentGrantMailer.with_user_locales(users: [user], consent_grant: consent_grant) { |mailer| mailer.email_consent.deliver_now }
+    @email = ActionMailer::Base.deliveries.last
+  end
+
+  context 'when locale emails disabled' do
+    let(:enable_locale_emails) { 'false' }
+    describe '#email_consent' do
+      it 'sends an email with en strings' do
+        expect(@email.subject).to eql ("Your grant of consent to Energy Sparks")
+        expect(@email.body.to_s).to include("Thank you for granting permission for Energy Sparks to access data for Test School")
       end
+    end
+  end
 
-      after do
-        I18n.locale = 'en'
-      end
-
-      it 'sends an email with en strings even if I18n is cy' do
-        ClimateControl.modify SEND_AUTOMATED_EMAILS: 'true' do
-          ConsentGrantMailer.with(consent_grant: consent_grant).email_consent.deliver_now
-        end
-        email = ActionMailer::Base.deliveries.last
-        expect(email.subject).to eql ("Your grant of consent to Energy Sparks")
-        expect(email.body.to_s).to include("Thank you for granting permission for Energy Sparks to access data for Test School")
+  context 'when locale emails enabled' do
+    let(:enable_locale_emails) { 'true' }
+    describe '#email_consent' do
+      it 'sends an email with cy strings' do
+        expect(@email.subject).to eql ("Eich caniatâd i Sbarcynni")
+        expect(ActionController::Base.helpers.sanitize(@email.body.to_s)).to include("Diolch am roi caniatâd i Sbarcynni gael mynediad at ddata ar gyfer Test School")
       end
     end
   end
