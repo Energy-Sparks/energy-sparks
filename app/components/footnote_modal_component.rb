079ef0c1dab5267a2cd0735713d04c9a9d5c1dca
Ian Townsend <ian.townsend@gmail.com>
2023-02-23 14:14:54 +0000

    Add optional modal dialog classes

--- app/components/footnote_modal_component.rb
+++ app/components/footnote_modal_component.rb
@@ -3,9 +3,10 @@
 class FootnoteModalComponent < ViewComponent::Base
   renders_one :body_content
 
-  def initialize(title:, modal_id:)
+  def initialize(title:, modal_id:, modal_dialog_classes: 'modal-lg modal-dialog-centered')
     @title = title
     @modal_id = modal_id
+    @modal_dialog_classes = modal_dialog_classes
   end
 
   def icon
