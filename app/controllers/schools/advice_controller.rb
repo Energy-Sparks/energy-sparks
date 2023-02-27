15082290f06ecd3c0340ff55652f4681dae7dcf5
Leigh Dodds <leigh@ldodds.com>
2023-02-27 17:10:24 +0000

    Quick hack to show overview

--- app/controllers/schools/advice_controller.rb
+++ app/controllers/schools/advice_controller.rb
@@ -6,8 +6,21 @@ module Schools
     before_action :load_advice_pages
 
     include SchoolAggregation
+    include DashboardAlerts
 
     def show
+      @management_priorities = @school.latest_management_priorities.by_priority.limit(site_settings.management_priorities_page_limit).map do |priority|
+        TemplateInterpolation.new(
+          priority.content_version,
+          with_objects: { find_out_more: priority.find_out_more },
+          proxy: [:colour]
+        ).interpolate(
+          :management_priorities_title,
+          with: priority.alert.template_variables
+        )
+      end
+
+      @dashboard_alerts = setup_alerts(@school.latest_dashboard_alerts.management_dashboard, :management_dashboard_title, limit: nil)
     end
 
     private
