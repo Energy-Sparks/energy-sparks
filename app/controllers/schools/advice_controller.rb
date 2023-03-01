87a61125947ab95ca67e94f7e6fa194eb4864644
Leigh Dodds <leigh@ldodds.com>
2023-03-01 17:40:20 +0000

    Refactor control, add badges

--- app/controllers/schools/advice_controller.rb
+++ app/controllers/schools/advice_controller.rb
@@ -5,40 +5,20 @@ module Schools
 
     before_action :load_advice_pages
     before_action :set_tab_name
+    before_action :set_content
 
-    include SchoolAggregation
     include DashboardAlerts
+    include DashboardPriorities
 
     def show
-      @management_priorities = @school.latest_management_priorities.by_priority.limit(site_settings.management_priorities_page_limit).map do |priority|
-        TemplateInterpolation.new(
-          priority.content_version,
-          with_objects: { find_out_more: priority.find_out_more },
-          proxy: [:colour]
-        ).interpolate(
-          :management_priorities_title,
-          with: priority.alert.template_variables
-        )
-      end
-
-      @dashboard_alerts = setup_alerts(@school.latest_dashboard_alerts.management_dashboard, :management_dashboard_title, limit: nil)
     end
 
     def priorities
-      @management_priorities = @school.latest_management_priorities.by_priority.limit(site_settings.management_priorities_page_limit).map do |priority|
-        TemplateInterpolation.new(
-          priority.content_version,
-          with_objects: { find_out_more: priority.find_out_more },
-          proxy: [:colour]
-        ).interpolate(
-          :management_priorities_title,
-          with: priority.alert.template_variables
-        )
-      end
+      @management_priorities = setup_priorities(latest_management_priorities, limit: nil)
     end
 
     def alerts
-      @dashboard_alerts = setup_alerts(@school.latest_dashboard_alerts.management_dashboard, :management_dashboard_title, limit: nil)
+      @dashboard_alerts = setup_alerts(latest_dashboard_alerts, :management_dashboard_title, limit: nil)
     end
 
     private
@@ -49,6 +29,19 @@ module Schools
 
     def load_advice_pages
       @advice_pages = AdvicePage.all.by_key
+    end
+
+    def set_content
+      @priority_count = latest_management_priorities.count
+      @alert_count = latest_dashboard_alerts.count
+    end
+
+    def latest_dashboard_alerts
+      @latest_dashboard_alerts ||= @school.latest_dashboard_alerts.management_dashboard
+    end
+
+    def latest_management_priorities
+      @latest_management_priorities ||= @school.latest_management_priorities
     end
   end
 end
