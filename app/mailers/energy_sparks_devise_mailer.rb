c62160231b55b4d7a2d50601d32b6697a09e55dd
Julian Higman <jhigman@pobox.com>
2023-03-01 12:25:59 +0000

    Custom devise method to send multilingual user invites

--- app/mailers/energy_sparks_devise_mailer.rb
+++ app/mailers/energy_sparks_devise_mailer.rb
@@ -2,9 +2,23 @@ class EnergySparksDeviseMailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   include DefaultUrlOptionsHelper
+  include LocaleMailerHelper
+
   default template_path: 'devise/mailer'
 
   layout 'mailer'
+
+  def confirmation_instructions(record, token, opts = {})
+    action = :confirmation_instructions
+    template = :confirmation_instructions_content
+    @token = token
+    @title = t(:title, scope: [:devise, :mailer, action], default: "")
+    initialize_from_record(record)
+    locales = active_locales([:en, record.preferred_locale.to_sym]).uniq
+    subject = for_each_locale(locales) { subject_for(action) }.join(" / ")
+    @body = for_each_locale(locales) { render template, layout: nil }.join("<hr>")
+    make_bootstrap_mail headers_for(action, opts.merge(subject: subject))
+  end
 
   protected
 
@@ -14,9 +28,5 @@ class EnergySparksDeviseMailer < Devise::Mailer
     I18n.with_locale(active_locale(record.preferred_locale)) do
       make_bootstrap_mail headers_for(action, opts), &block
     end
-  end
-
-  def active_locale(locale)
-    EnergySparks::FeatureFlags.active?(:emails_with_preferred_locale) ? locale : :en
   end
 end
--- app/mailers/locale_mailer.rb
+++ app/mailers/locale_mailer.rb
@@ -1,4 +1,6 @@
 class LocaleMailer < ApplicationMailer
+  include LocaleMailerHelper
+
   def self.with_user_locales(users:, **args)
     I18n.available_locales.each do |locale|
       locale_users = users_for_locale(users, locale)
@@ -16,23 +18,11 @@ class LocaleMailer < ApplicationMailer
     users.select {|u| u.preferred_locale.to_sym == locale.to_sym}
   end
 
-  def for_each_locale(locales)
-    locales.map { |locale| I18n.with_locale(locale) { yield } }
-  end
-
   # send with locale if preferred locales are enabled
   def make_bootstrap_mail(*args)
     I18n.with_locale(active_locale(locale_param)) do
       super(*args)
     end
-  end
-
-  def active_locale(locale)
-    EnergySparks::FeatureFlags.active?(:emails_with_preferred_locale) ? locale : :en
-  end
-
-  def active_locales(locales)
-    EnergySparks::FeatureFlags.active?(:emails_with_preferred_locale) ? locales : [:en]
   end
 
   def locale_param
