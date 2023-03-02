abac718716a6445bdcd81efdd4a8b6b3d4428642
Leigh Dodds <leigh@ldodds.com>
2023-03-02 11:29:01 +0000

    Add specs

--- spec/factories/advice_pages.rb
+++ spec/factories/advice_pages.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :advice_page do
     sequence(:key)        {|n| "Advice Page #{n}"}
     sequence(:learn_more) {|n| "Learn more #{n}"}
+    restricted  { false }
     fuel_type { "electricity" }
   end
 end
