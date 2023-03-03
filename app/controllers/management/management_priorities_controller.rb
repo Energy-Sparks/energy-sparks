d896c95edb3854493675b8cee6c5158486f81bac
Leigh Dodds <leigh@ldodds.com>
2023-03-03 12:13:46 +0000

    Update links for mgt priorities

--- app/controllers/management/management_priorities_controller.rb
+++ app/controllers/management/management_priorities_controller.rb
@@ -1,19 +1,12 @@
 module Management
   class ManagementPrioritiesController < ApplicationController
+    include DashboardPriorities
+
     load_and_authorize_resource :school
 
     def index
       authorize! :show_management_dash, @school
-      @management_priorities = @school.latest_management_priorities.by_priority.limit(site_settings.management_priorities_page_limit).map do |priority|
-        TemplateInterpolation.new(
-          priority.content_version,
-          with_objects: { find_out_more: priority.find_out_more },
-          proxy: [:colour]
-        ).interpolate(
-          :management_priorities_title,
-          with: priority.alert.template_variables
-        )
-      end
+      @management_priorities = setup_priorities(@school.latest_management_priorities, limit: site_settings.management_priorities_page_limit)
     end
   end
 end
--- app/helpers/schools_helper.rb
+++ app/helpers/schools_helper.rb
@@ -19,22 +19,30 @@ module SchoolsHelper
     meter.pseudo && action_name == 'edit'
   end
 
+  def dashboard_alert_buttons(school, alert_content)
+    path = find_out_more_path_from_alert_content(school, alert_content)
+    return {} if path.nil?
+    { t('schools.show.find_out_more') => path }
+  end
+
+  #Switches between linking to the old find out more pages and the
+  #new advice pages.
+  def find_out_more_path_from_alert_content(school, alert_content)
+    if EnergySparks::FeatureFlags.active?(:replace_find_out_mores)
+      alert_type = alert_content.alert.alert_type
+      return nil unless alert_type.advice_page.present?
+      advice_page_path_from_alert_type(school, alert_type)
+    else
+      return nil unless alert_content.find_out_more.present?
+      school_find_out_more_path(school, alert_content.find_out_more)
+    end
+  end
+
   def advice_page_path_from_alert_type(school, alert_type)
     advice_page = alert_type.advice_page
     polymorphic_path(
       [alert_type.advice_page_tab_for_link_to, school, :advice, advice_page.key.to_sym],
       anchor: alert_type.link_to_section
     )
-  end
-
-  def dashboard_alert_buttons(school, alert_content)
-    if EnergySparks::FeatureFlags.active?(:replace_find_out_mores)
-      alert_type = alert_content.alert.alert_type
-      return {} unless alert_type.advice_page.present?
-      { t('schools.show.find_out_more') => advice_page_path_from_alert_type(school, alert_type) }
-    else
-      return {} unless alert_content.find_out_more.present?
-      { t('schools.show.find_out_more') => school_find_out_more_path(school, alert_content.find_out_more) }
-    end
   end
 end
