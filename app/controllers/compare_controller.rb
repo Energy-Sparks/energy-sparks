62a0ecbcd26e2c4c78abe375bfb98782a19d5b5b
Deb Bassett <deb@urbanwide.com>
2023-03-09 16:44:54 +0000

    Save in a working place

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -4,6 +4,7 @@ class CompareController < ApplicationController
   before_action :header_fix_enabled
   skip_before_action :authenticate_user!
 
+  before_action :filter
   before_action :get_school_group
   before_action :latest_benchmark_run, except: [:index]
   before_action :content_manager, except: [:index]
@@ -12,19 +13,46 @@ class CompareController < ApplicationController
 
   # filters
   def index
-    params[:school_types] ||= School.school_types.keys
   end
 
   # pick benchmark
   def benchmarks
-    @school_group = SchoolGroup.find_by(slug: params[:school_group])
   end
 
-  # display results
-  def results
+  def show
+    @results = fetch_results # unless @filter[:school_group_ids].empty?
+    @benchmark = @filter[:benchmark].to_sym
+    @benchmark_groups = [{ name: '', benchmarks: { @benchmark => @benchmark } }]
+
+    sort_content_and_page_groups(@benchmark_groups)
   end
 
   private
+
+  def filter
+    @filter ||=
+      params.permit(:search, :benchmark, school_group_ids: [], school_types: [])
+        .with_defaults(school_group_ids: [], school_types: School.school_types.keys)
+        .to_hash.symbolize_keys
+  end
+
+  def all_school_types
+    School.school_types.keys
+  end
+
+  def params_for_school_filter
+    @filter.slice(:school_group_ids, :school_types)
+  end
+
+  def fetch_results
+    include_invisible = can? :show, :all_schools
+
+    splatted_params = { include_invisible: include_invisible }.merge(params_for_school_filter)
+    schools = SchoolFilter.new(**splatted_params).filter
+    schools = schools.select {|s| can?(:show, s) } unless include_invisible
+
+    Alerts::CollateBenchmarkData.new(@latest_benchmark_run).perform(schools)
+  end
 
   def latest_benchmark_run
     @latest_benchmark_run ||= BenchmarkResultGenerationRun.latest
@@ -45,5 +73,40 @@ class CompareController < ApplicationController
 
   def get_school_group
     @school_group = current_user.try(:default_school_group)
+  end
+
+  def content_for_page(benchmark, errors = [])
+    @content_manager.content(@results, benchmark, user_type: user_type_hash, online: true)
+    # rubocop:disable Lint/RescueException
+  rescue Exception => e
+    # rubocop:enable Lint/RescueException
+    # Rollbar.error(e, benchmark: benchmark)
+
+    error_message = "Exception: #{benchmark}: #{e.class} #{e.message}"
+
+    backtrace = e.backtrace.select { |b| b.include?('analytics')}.join("<br>")
+    full_html_output = "Exception: #{benchmark}: #{e.class} #{e.message} #{backtrace}"
+    errors << { message: error_message, full_html_output: full_html_output }
+    {}
+  end
+
+  def sort_content_and_page_groups(benchmark_groups)
+    @content_hash = {}
+    @errors = []
+
+    benchmark_groups.each do |heading_hash|
+      heading_hash[:benchmarks].each_key do |benchmark|
+        @content_hash[benchmark] = filter_content(content_for_page(benchmark, @errors))
+      end
+    end
+  end
+
+  def content_select?(content)
+    return false unless content.present?
+    [:chart, :html, :table_composite, :title].include?(content[:type]) && content[:content].present?
+  end
+
+  def filter_content(all_content)
+    all_content.select { |content| content_select?(content) }
   end
 end
