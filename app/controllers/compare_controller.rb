d9c633fca1501b8fec1a80638c38aba11e43e639
Deb Bassett <deb@urbanwide.com>
2023-03-09 20:51:16 +0000

    Simlify

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -7,23 +7,23 @@ class CompareController < ApplicationController
   before_action :filter
   before_action :latest_benchmark_run, except: [:index]
   before_action :content_manager, except: [:index]
-  before_action :benchmark_count, only: [:index]
-  before_action :benchmark_groups, only: [:benchmarks]
 
   # filters
   def index
+    # Count is of all available benchmarks for guest users only
+    @benchmark_count = Benchmarking::BenchmarkManager.structured_pages(user_type: user_type_hash_guest).inject(0) { |count, group| count + group[:benchmarks].count }
   end
 
   # pick benchmark
   def benchmarks
+    @benchmark_groups = @content_manager.structured_pages(school_ids: nil, filter: nil, user_type: user_type_hash)
   end
 
   def show
     @results = fetch_results # unless @filter[:school_group_ids].empty?
     @benchmark = @filter[:benchmark].to_sym
-    @benchmark_groups = [{ name: '', benchmarks: { @benchmark => @benchmark } }]
 
-    sort_content_and_page_groups(@benchmark_groups)
+    @content = filter_content(@benchmark)
   end
 
   private
@@ -33,10 +33,6 @@ class CompareController < ApplicationController
       params.permit(:search, :benchmark, school_group_ids: [], school_types: [])
         .with_defaults(school_group_ids: [], school_types: School.school_types.keys)
         .to_hash.symbolize_keys
-  end
-
-  def all_school_types
-    School.school_types.keys
   end
 
   def params_for_school_filter
@@ -61,47 +57,21 @@ class CompareController < ApplicationController
     @content_manager ||= Benchmarking::BenchmarkContentManager.new(@latest_benchmark_run.run_date)
   end
 
-  def benchmark_count
-    # Count is of all available benchmarks for guest users only
-    @benchmark_count ||= Benchmarking::BenchmarkManager.structured_pages(user_type: user_type_hash_guest).inject(0) { |count, group| count + group[:benchmarks].count }
-  end
-
-  def benchmark_groups
-    @benchmark_groups ||= @content_manager.structured_pages(school_ids: nil, filter: nil, user_type: user_type_hash)
-  end
-
-  def content_for_page(benchmark, errors = [])
+  def content_for_benchmark(benchmark)
     @content_manager.content(@results, benchmark, user_type: user_type_hash, online: true)
     # rubocop:disable Lint/RescueException
   rescue Exception => e
     # rubocop:enable Lint/RescueException
-    # Rollbar.error(e, benchmark: benchmark)
-
-    error_message = "Exception: #{benchmark}: #{e.class} #{e.message}"
-
-    backtrace = e.backtrace.select { |b| b.include?('analytics')}.join("<br>")
-    full_html_output = "Exception: #{benchmark}: #{e.class} #{e.message} #{backtrace}"
-    errors << { message: error_message, full_html_output: full_html_output }
+    Rollbar.error(e, benchmark: benchmark)
     {}
   end
 
-  def sort_content_and_page_groups(benchmark_groups)
-    @content_hash = {}
-    @errors = []
-
-    benchmark_groups.each do |heading_hash|
-      heading_hash[:benchmarks].each_key do |benchmark|
-        @content_hash[benchmark] = filter_content(content_for_page(benchmark, @errors))
-      end
-    end
+  def filter_content(benchmark)
+    content_for_benchmark(benchmark).select { |content| content_select?(content) }
   end
 
   def content_select?(content)
     return false unless content.present?
     [:chart, :html, :table_composite, :title].include?(content[:type]) && content[:content].present?
-  end
-
-  def filter_content(all_content)
-    all_content.select { |content| content_select?(content) }
   end
 end
