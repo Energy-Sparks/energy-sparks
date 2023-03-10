9a8b802a5802fddc781f30d60dc5e7958d520170
Julian Higman <jhigman@pobox.com>
2023-03-10 11:47:01 +0000

    Check permissions in downloads controller

--- app/controllers/schools/downloads_controller.rb
+++ app/controllers/schools/downloads_controller.rb
@@ -1,8 +1,18 @@
 module Schools
   class DownloadsController < ApplicationController
     load_and_authorize_resource :school
+    before_action :authorized?
 
     def index
+    end
+
+    private
+
+    def authorized?
+      unless can?(:download_school_data, @school)
+        flash[:error] = "You are not authorized to view that page."
+        redirect_to root_path
+      end
     end
   end
 end
