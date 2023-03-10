604c409c57874fe4723f0338233ace50c0bb00eb
Leigh Dodds <leigh@ldodds.com>
2023-03-10 15:31:03 +0000

    Implement meter cost table for costs page (#2520)

--- app/controllers/schools/advice/electricity_costs_controller.rb
+++ app/controllers/schools/advice/electricity_costs_controller.rb
@@ -9,12 +9,18 @@ module Schools
 
       def insights
         @annual_costs = costs_service.annual_costs
+        @monthly_costs = costs_service.calculate_costs_for_latest_twelve_months
+        @change_in_costs = costs_service.calculate_change_in_costs
       end
 
       def analysis
         @meters = @school.meters.active.electricity
         @annual_costs = costs_service.annual_costs
         @multiple_meters = costs_service.multiple_meters?
+
+        @monthly_costs = costs_service.calculate_costs_for_latest_twelve_months
+        @change_in_costs = costs_service.calculate_change_in_costs
+
         if @multiple_meters
           @annual_costs_breakdown_by_meter = costs_service.annual_costs_breakdown_by_meter
           @aggregate_meter_adapter = aggregate_meter_adapter
@@ -27,10 +33,15 @@ module Schools
         if params[:mpan_mprn] == aggregate_meter_mpan_mprn
           @mpan_mprn = aggregate_meter_mpan_mprn
           @label = aggregate_meter_label
+          @monthly_costs = costs_service.calculate_costs_for_latest_twelve_months
+          @change_in_costs = costs_service.calculate_change_in_costs
         else
           meter = @school.meters.find_by_mpan_mprn(params[:mpan_mprn])
           @mpan_mprn = params[:mpan_mprn]
           @label = meter.name_or_mpan_mprn
+          analytics_meter = costs_service.analytics_meter_for_mpan(@mpan_mprn)
+          @monthly_costs = costs_service.calculate_costs_for_latest_twelve_months(analytics_meter)
+          @change_in_costs = costs_service.calculate_change_in_costs(analytics_meter)
         end
         @analysis_dates = analysis_dates
         respond_to do |format|
