926e9bac05194161a7e7d6c3c961ac7be86f447b
Leigh Dodds <leigh@ldodds.com>
2023-03-15 16:22:55 +0000

    Implement advice page benchmark

---
+++ app/services/schools/advice_page_benchmarks/peak_usage_benchmark_generator.rb
@@ -0,0 +1,16 @@
+module Schools
+  module AdvicePageBenchmarks
+    class PeakUsageBenchmarkGenerator < SchoolBenchmarkGenerator
+      def benchmark_school
+        return unless usage_service.enough_data?
+        usage_service.benchmark_peak_usage.category
+      end
+
+      private
+
+      def usage_service
+        @usage_service ||= Schools::Advice::PeakUsageService.new(@school, @aggregate_school)
+      end
+    end
+  end
+end
