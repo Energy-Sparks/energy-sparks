fb7ea0667d13e16307395b33e5c36bd0fb5fb568
Ian Townsend <ian.townsend@gmail.com>
2023-03-16 17:43:25 +0000

    Add seasonal analysis values

--- app/controllers/schools/advice/storage_heaters_controller.rb
+++ app/controllers/schools/advice/storage_heaters_controller.rb
@@ -2,12 +2,17 @@ module Schools
   module Advice
     class StorageHeatersController < AdviceBaseController
       def insights
+        @seasonal_analysis = build_seasonal_analysis
       end
 
       def analysis
       end
 
       private
+
+      def build_seasonal_analysis
+        Heating::SeasonalControlAnalysisService.new(meter_collection: aggregate_school, fuel_type: :storage_heater).seasonal_analysis
+      end
 
       def set_insights_next_steps
         @advice_page_insights_next_steps = t("advice_pages.#{advice_page_key}.insights.next_steps_html").html_safe
