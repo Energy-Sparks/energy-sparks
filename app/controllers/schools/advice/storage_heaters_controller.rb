03bc24d994fb70376abb37cf8b53a5644e6ccf52
Ian Townsend <ian.townsend@gmail.com>
2023-03-16 17:45:36 +0000

    set seasonal analysis

--- app/controllers/schools/advice/storage_heaters_controller.rb
+++ app/controllers/schools/advice/storage_heaters_controller.rb
@@ -1,14 +1,19 @@
 module Schools
   module Advice
     class StorageHeatersController < AdviceBaseController
+      before_action :set_seasonal_analysis, only: [:insights, :analysis]
+
       def insights
-        @seasonal_analysis = build_seasonal_analysis
       end
 
       def analysis
       end
 
       private
+
+      def set_seasonal_analysis
+        @seasonal_analysis = build_seasonal_analysis
+      end
 
       def build_seasonal_analysis
         Heating::SeasonalControlAnalysisService.new(meter_collection: aggregate_school, fuel_type: :storage_heater).seasonal_analysis
