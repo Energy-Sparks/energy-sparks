84a6f2624d9db29beecc56c3ec13b426a4b981a1
Ian Townsend <ian.townsend@gmail.com>
2023-03-17 14:10:26 +0000

    Fixes & updates after review

--- app/controllers/schools/advice/storage_heaters_controller.rb
+++ app/controllers/schools/advice/storage_heaters_controller.rb
@@ -3,7 +3,7 @@ module Schools
     class StorageHeatersController < AdviceBaseController
       before_action :set_seasonal_analysis, only: [:insights, :analysis]
       before_action :set_annual_usage_breakdown, only: [:insights, :analysis]
-      before_action :set_usage_categories, only: [:analysis]
+      before_action :set_usage_categories, only: [:insights, :analysis]
       before_action :set_heating_thermostatic_analysis, only: [:insights, :analysis]
 
       def insights
@@ -35,21 +35,6 @@ module Schools
           meter_collection: aggregate_school,
           fuel_type: :storage_heater
         ).usage_breakdown
-      end
-
-      def analysis_dates
-        start_date = aggregate_school.storage_heater_meter.amr_data.start_date
-        end_date = aggregate_school.storage_heater_meter.amr_data.end_date
-        OpenStruct.new(
-          start_date: start_date,
-          end_date: end_date,
-          one_year_before_end: end_date - 1.year,
-          last_full_week_start_date: last_full_week_start_date(end_date),
-          last_full_week_end_date: last_full_week_end_date(end_date),
-          one_years_data: one_years_data?(start_date, end_date),
-          months_of_data: months_between(start_date, end_date),
-          recent_data: recent_data?(end_date)
-        )
       end
 
       def set_seasonal_analysis
