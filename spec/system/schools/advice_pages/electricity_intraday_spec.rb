39cf2e883b4bf8e7a51448130530315bfd323c15
Ian Townsend <ian.townsend@gmail.com>
2023-03-23 15:06:26 +0000

    Add some tests

--- spec/system/schools/advice_pages/electricity_intraday_spec.rb
+++ spec/system/schools/advice_pages/electricity_intraday_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe "electricity intraday advice page", type: :system do
   include_context "electricity advice page"
 
   context 'as school admin' do
-    let(:user)  { create(:school_admin, school: school) }
+    let(:user) { create(:school_admin, school: school) }
 
     before do
       sign_in(user)
-      allow_any_instance_of(Usage::PeakUsageBenchmarkingService).to receive(:average_peak_usage_kw) { 12.0 }
-      allow_any_instance_of(Usage::PeakUsageCalculationService).to receive(:average_peak_kw) { 12.0 }
+      allow_any_instance_of(Usage::PeakUsageBenchmarkingService).to receive(:average_peak_usage_kw) { 9_999_999.0 }
+      allow_any_instance_of(Usage::PeakUsageCalculationService).to receive(:average_peak_kw) { 9_999_999.0 }
 
       visit school_advice_electricity_intraday_path(school)
     end
@@ -28,9 +28,25 @@ RSpec.describe "electricity intraday advice page", type: :system do
 
       it 'shows expected content' do
         expect(page).to have_content('Your current peak electricity use')
+        expect(page).not_to have_content('Data on peak kw usage available from')
+        expect(page).to have_content('How did we calculate these figures?')
         expect(page).to have_content('How do you compare?')
+        expect(page).to have_content('How does your peak electricity use compare to other primary schools on Energy Sparks with a similar number of pupils')
         expect(page).to have_content('For more detail, compare with other schools in your group')
-        expect(page).to have_content(12)
+        expect(page).to have_content('10,000,000')
+      end
+
+      context 'when not enough data' do
+        let(:start_date) { end_date - 11.months}
+        it "shows message" do
+          expect(page).to have_content('Your current peak electricity use')
+          expect(page).to have_content('Data on peak kw usage available from')
+          expect(page).not_to have_content('How did we calculate these figures?')
+          expect(page).not_to have_content('How do you compare?')
+          expect(page).not_to have_content('How does your peak electricity use compare to other primary schools on Energy Sparks with a similar number of pupils')
+          expect(page).not_to have_content('For more detail, compare with other schools in your group')
+          expect(page).not_to have_content('10,000,000')
+        end
       end
     end
 
@@ -56,8 +72,11 @@ RSpec.describe "electricity intraday advice page", type: :system do
       context 'when not enough data' do
         let(:start_date) { end_date - 11.months}
         it "shows message" do
-          # expect page to not have table for peak kw usage if school has <1 year of data.
-          # expect page to have text "Data on peak kw usage available from %{data_available_from}"
+          expect(page).to have_css('#chart_wrapper_intraday_line_school_days_reduced_data_versus_benchmarks')
+          expect(page).to have_css('#chart_wrapper_intraday_line_school_days_reduced_data')
+          expect(page).to have_css('#chart_wrapper_intraday_line_holidays')
+          expect(page).to have_css('#chart_wrapper_intraday_line_weekends')
+          expect(page).to have_css('#chart_wrapper_intraday_line_school_last7days')
         end
       end
     end
