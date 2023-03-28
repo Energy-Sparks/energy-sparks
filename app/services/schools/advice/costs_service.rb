6dc498443ed0cd5aed33b3db109fcddd8fbd0bf2
Ian Townsend <ian.townsend@gmail.com>
2023-03-28 11:38:26 +0100

    Update table partial

--- app/services/schools/advice/costs_service.rb
+++ app/services/schools/advice/costs_service.rb
@@ -28,6 +28,7 @@ module Schools
         reporting_meters.each do |meter|
           breakdown[meter_for_mpan(meter.mpan_mprn)] = accounting_costs_service(meter).annual_cost
         end
+
         breakdown
       end
 
@@ -97,7 +98,7 @@ module Schools
       end
 
       def accounting_costs_service(meter = aggregate_meter)
-        Costs::AccountingCostsService.new(meter)
+        Costs::AccountingCostsService.new(meter, aggregate_meter.amr_data.end_date)
       end
 
       def tariff_information_service(meter = aggregate_meter)
