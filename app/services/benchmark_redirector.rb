082b70150bd949f173af6450277d0d0f148ab53d
Deb Bassett <deb@urbanwide.com>
2023-03-28 15:27:40 +0100

    Remove unnecessary bang! from method

--- app/services/benchmark_redirector.rb
+++ app/services/benchmark_redirector.rb
@@ -1,7 +1,7 @@
 # Used by the rails router for redirecting old benchmark urls to the new compare ones
 class BenchmarkRedirector
   def call(_params, request)
-    params = (request.params['benchmark'] || {}).slice('school_group_ids', 'school_types').transform_values! { |a| a.reject(&:blank?) }
+    params = (request.params['benchmark'] || {}).slice('school_group_ids', 'school_types').transform_values { |a| a.reject(&:blank?) }
     params['school_types'].map! { |id| School.school_types.key(id.to_i) } if params['school_types']
     "/compare/#{request.params['benchmark_type']}?search=groups&#{params.to_query}"
   end
