cd09c5ac5d66214a0191a2830a0156896e7af07b
Leigh Dodds <leigh@ldodds.com>
2023-03-30 15:07:47 +0100

    Check for active meters

--- app/services/schools/advice/baseload_service.rb
+++ app/services/schools/advice/baseload_service.rb
@@ -17,7 +17,7 @@ module Schools
       end
 
       def multiple_electricity_meters?
-        electricity_meters.count > 1
+        @school.meters.active.electricity.count > 1
       end
 
       def average_baseload_kw(period: :year)
