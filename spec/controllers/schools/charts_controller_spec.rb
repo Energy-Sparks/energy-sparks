d3b8fd09a9248b52cbcce0323394344c98435250
Ian Townsend <ian.townsend@gmail.com>
2023-03-31 08:11:18 +0100

    Clean up

--- spec/controllers/schools/charts_controller_spec.rb
+++ spec/controllers/schools/charts_controller_spec.rb
@@ -2,13 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Schools::ChartsController, type: :controller do
   context 'GET #show' do
-
-    let!(:rollbar) { double(Rollbar) }
-
     before do
       @school = FactoryBot.create :school, visible: false
-      allow(rollbar).to receive(:error)
-
     end
 
     describe "format json" do
@@ -19,8 +14,8 @@ RSpec.describe Schools::ChartsController, type: :controller do
 
       it 'returns a json response if a chart type param is present' do
         get :show, params: { school_id: @school.to_param, chart_type: 'pupil_dashboard_group_by_week_electricity_kwh' }, format: :json
-        expect(Rollbar).to receive(:error)
         expect(response).to have_http_status(:success)
+        expect(Rollbar.error).to eq({})
         expect(JSON.parse(response.body)['title']).to eq('We do not have enough data to display this chart at the moment: Pupil_dashboard_group_by_week_electricity_kwh chart')
       end
     end
