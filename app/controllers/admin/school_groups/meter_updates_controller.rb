ef80185ad0a10b2da70b2fe807e0f550af92bf51
Ian Townsend <ian.townsend@gmail.com>
2023-04-05 17:50:20 +0100

    Update notice

--- app/controllers/admin/school_groups/meter_updates_controller.rb
+++ app/controllers/admin/school_groups/meter_updates_controller.rb
@@ -9,7 +9,8 @@ module Admin
       def bulk_update_meters
         meters = @school_group.meters.where(meter_type: meter_types)
         if meters.update_all(data_source_id: meter_update_params['data_source_id']&.to_i)
-          redirect_to(admin_school_group_meter_updates_path(@school_group), notice: "#{meters.count} #{meter_types.to_sentence} #{'meter'.pluralize(meters.count)} successfully updated for this school group.") and return
+          notice = "#{meters.count} #{meter_types.to_sentence} #{'meter'.pluralize(meters.count)} successfully updated for this school group."
+          redirect_to(admin_school_group_meter_updates_path(@school_group), notice: notice) and return
         else
           render :index, status: :unprocessable_entity
         end
