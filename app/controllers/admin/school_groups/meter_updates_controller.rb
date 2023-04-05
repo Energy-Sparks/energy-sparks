57ebe2eb56d997d160dd9f94e12fbbbdfbe9a92d
Ian Townsend <ian.townsend@gmail.com>
2023-04-05 17:46:30 +0100

    Fix controller update

--- app/controllers/admin/school_groups/meter_updates_controller.rb
+++ app/controllers/admin/school_groups/meter_updates_controller.rb
@@ -8,7 +8,7 @@ module Admin
 
       def bulk_update_meters
         meters = @school_group.meters.where(meter_type: meter_types)
-        if meters.update_all(data_source_id: meter_update_params[:data_source_id])
+        if meters.update_all(data_source_id: meter_update_params['data_source_id']&.to_i)
           redirect_to(admin_school_group_meter_updates_path(@school_group), notice: "#{meters.count} #{meter_types.to_sentence} #{'meter'.pluralize(meters.count)} successfully updated for this school group.") and return
         else
           render :index, status: :unprocessable_entity
@@ -18,11 +18,11 @@ module Admin
       private
 
       def meter_types
-        @meter_types ||= meter_update_params['meter_update_id'] == 'solar_pv' ? %w(solar_pv exported_solar_pv) : [meter_update_params['meter_update_id']]
+        @meter_types ||= params['meter_update_id'] == 'solar_pv' ? %w(solar_pv exported_solar_pv) : [params['meter_update_id']]
       end
 
       def meter_update_params
-        params.permit(:data_source_id, :meter_update_id)
+        params.require(:meter).permit(:data_source_id)
       end
     end
   end
