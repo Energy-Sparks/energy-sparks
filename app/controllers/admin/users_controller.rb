e8a31d7782d1a2e8b42db2355bbd4f2d98a6dd3f
Leigh Dodds <leigh@ldodds.com>
2023-04-12 10:43:36 +0100

    Add school group user page (#2672)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,9 +3,13 @@ module Admin
     load_and_authorize_resource
 
     def index
-      @school_users = school_users
-      @school_group_users = @users.where.not(school_group_id: nil).order('school_groups.name', :email).includes(:school_group)
+      @school_groups = SchoolGroup.all.by_name
+      @search_users = find_users
       @unattached_users = @users.where(school_id: nil, school_group_id: nil).order(:email)
+      respond_to do |format|
+        format.html { }
+        format.csv { send_data produce_user_csv, filename: 'users.csv' }
+      end
     end
 
     def new
@@ -41,6 +45,18 @@ module Admin
 
   private
 
+    def find_users
+      if params[:search].present?
+        search = params[:search]
+        if search["email"].present?
+          return User.where("email like ?", "%#{search['email']}%").where.not(role: :pupil).limit(50)
+        else
+          return []
+        end
+      end
+      []
+    end
+
     def user_params
       params.require(:user).permit(:name, :email, :role, :school_id, :school_group_id, :staff_role_id, cluster_school_ids: [])
     end
@@ -59,6 +75,29 @@ module Admin
         end
       end
       users
+    end
+
+    def produce_user_csv
+      CSV.generate do |csv|
+        csv << [
+          'School Group',
+          'School',
+          'Name',
+          'Email',
+          'Role',
+          'Staff Role'
+        ]
+        User.where.not(role: :pupil).where.not(role: :admin).order(:email).each do |user|
+          csv << [
+            user.group_admin? ? user.school_group.name : user.school&.school_group&.name,
+            user.school.present? ? user.school.name : '',
+            user.name,
+            user.email,
+            user.role.titleize,
+            user.staff_role&.title,
+          ]
+        end
+      end
     end
   end
 end
