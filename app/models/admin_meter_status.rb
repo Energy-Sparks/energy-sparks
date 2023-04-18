67f9ef93aade40b549eee833b8ad9832e23bfcdc
Ian Townsend <iant@users.noreply.github.com>
2023-04-18 11:59:17 +0100

    Add a few missing model annotations (#2685)

--- app/models/admin_meter_status.rb
+++ app/models/admin_meter_status.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: admin_meter_statuses
+#
+#  created_at :datetime         not null
+#  id         :bigint(8)        not null, primary key
+#  label      :string
+#  updated_at :datetime         not null
+#
 class AdminMeterStatus < ApplicationRecord
   has_many :meters, foreign_key: 'admin_meter_statuses_id'
   has_many :school_groups_electricity, class_name: 'SchoolGroup', foreign_key: 'admin_meter_statuses_electricity_id'
