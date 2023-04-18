d62a653165a09ecd167d160252b77a4094b5250c
Leigh Dodds <leigh@ldodds.com>
2023-04-18 16:30:48 +0100

    Optimising import notifier (#2686)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_18_080133) do
+ActiveRecord::Schema.define(version: 2023_04_18_114415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -425,6 +425,7 @@ ActiveRecord::Schema.define(version: 2023_04_18_080133) do
     t.datetime "updated_at", null: false
     t.index ["amr_data_feed_config_id"], name: "index_amr_data_feed_readings_on_amr_data_feed_config_id"
     t.index ["amr_data_feed_import_log_id"], name: "index_amr_data_feed_readings_on_amr_data_feed_import_log_id"
+    t.index ["meter_id", "amr_data_feed_config_id"], name: "adfr_meter_id_config_id"
     t.index ["meter_id"], name: "index_amr_data_feed_readings_on_meter_id"
     t.index ["mpan_mprn", "reading_date"], name: "unique_meter_readings", unique: true
     t.index ["mpan_mprn"], name: "index_amr_data_feed_readings_on_mpan_mprn"
