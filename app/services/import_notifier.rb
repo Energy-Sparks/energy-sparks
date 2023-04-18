036df6e20bbace8c7877456516ede4d24ad8dbd6
Leigh Dodds <leigh@ldodds.com>
2023-04-18 12:37:28 +0100

    Initial attempt at reworking slowest query

--- app/services/import_notifier.rb
+++ app/services/import_notifier.rb
@@ -6,7 +6,14 @@ class ImportNotifier
   def meters_running_behind
     find_with_config do |config|
       if config.import_warning_days.present?
-        config.meters.select {|meter| meter.last_validated_reading && meter.last_validated_reading < config.import_warning_days.days.ago}
+        #Leaving this here for the moment to document original behaviour
+        #config.meters.select {|meter| meter.last_validated_reading && meter.last_validated_reading < config.import_warning_days.days.ago}
+
+        Meter.active
+          #that have ever had readings loaded from this config (~ data source)
+          .joins(:amr_data_feed_readings).where("amr_data_feed_readings.amr_data_feed_config_id=?", config.id)
+          #and where the latest validated reading is older than what we expect in the config
+          .joins(:amr_validated_readings).group('meters.id').having("MAX(amr_validated_readings.reading_date) < NOW() - INTERVAL '? days'", config.import_warning_days)
       else
         []
       end
