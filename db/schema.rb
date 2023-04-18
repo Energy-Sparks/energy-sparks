071a693107f1f80c45373b8cea414b22860d38c3
Leigh Dodds <leigh@ldodds.com>
2023-04-18 15:32:04 +0100

    Remerge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2023_04_18_080133) do
-=======
 ActiveRecord::Schema.define(version: 2023_04_18_114415) do
->>>>>>> eff42c917 (Add index to speed up meters running behind)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
