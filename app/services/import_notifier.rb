8557ed62ecd27d09427c98580961c7599cfcc914
Leigh Dodds <leigh@ldodds.com>
2023-04-19 10:40:33 +0100

    Add a date limit to the amr data feed reading lookup

--- app/services/import_notifier.rb
+++ app/services/import_notifier.rb
@@ -7,8 +7,9 @@ class ImportNotifier
     find_with_config do |config|
       if config.import_warning_days.present?
         Meter.active
-          #that have ever had readings loaded from this config (~ data source)
+          #that have readings loaded from this config (~ data source) in the last 12 months
           .joins(:amr_data_feed_readings).where("amr_data_feed_readings.amr_data_feed_config_id=?", config.id)
+          .where("amr_data_feed_readings.created_at >= NOW() - INTERVAL '1 year'")
           #and where the latest validated reading is older than what we expect in the config
           .joins(:amr_validated_readings).group('meters.id').having("MAX(amr_validated_readings.reading_date) < NOW() - INTERVAL '? days'", config.import_warning_days)
       else
