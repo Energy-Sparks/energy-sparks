ce935a45b2fd313dc5cfb49080544089ac6a64e3
Ian Townsend <ian.townsend@gmail.com>
2023-04-21 11:03:35 +0100

    Fix recent changes error for incomplete data week comparisons

--- spec/services/schools/advice/gas_recent_changes_service_spec.rb
+++ spec/services/schools/advice/gas_recent_changes_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Schools::Advice::GasRecentChangesService, type: :service do
 
   let(:school) { create(:school) }
-  let(:gas_aggregate_meter)   { double('gas-aggregated-meter', aggregate_meter?: true)}
+  let(:gas_aggregate_meter)   { double('gas-aggregated-meter', aggregate_meter?: true, amr_data: OpenStruct.new)}
   let(:meter_collection) { double(:meter_collection, aggregated_heat_meters: gas_aggregate_meter) }
   let(:meter_data_checker) { double(:meter_data_checker) }
   let(:earliest_date) { Date.parse('20220101') }
@@ -14,20 +14,40 @@ RSpec.describe Schools::Advice::GasRecentChangesService, type: :service do
     allow(service).to receive(:meter_data_checker).and_return(meter_data_checker)
   end
 
+  describe '#at_least_x_full_weeks_of_data?' do
+    context 'with at least 2 weeks of data with week starting Sunday' do
+      it 'returns true' do
+        # 01-04-2023 is a Saturday
+        allow(gas_aggregate_meter).to receive(:amr_data) { OpenStruct.new(start_date: Date.parse('2023-03-19'), end_date: Date.parse('01-04-2023')) }
+        expect(gas_aggregate_meter.amr_data.start_date.sunday?).to eq(true)
+        expect(gas_aggregate_meter.amr_data.end_date.saturday?).to eq(true)
+        expect(service.enough_data_for_full_week_comparison?).to eq(true)
+      end
+
+      it 'returns false' do
+        # 05-04-2023 is a Wednesday
+        allow(gas_aggregate_meter).to receive(:amr_data) { OpenStruct.new(start_date: Date.parse('2023-03-23'), end_date: Date.parse('05-04-2023')) }
+        expect(gas_aggregate_meter.amr_data.start_date.thursday?).to eq(true)
+        expect(gas_aggregate_meter.amr_data.end_date.wednesday?).to eq(true)
+        expect(service.enough_data_for_full_week_comparison?).to eq(false)
+      end
+    end
+  end
+
   describe '#enough_data?' do
     it 'true if the meter data checker returns true' do
-      expect(meter_data_checker).to receive(:at_least_x_days_data?).with(14).and_return(true)
+      expect(meter_data_checker).to receive(:at_least_x_days_data?).with(7).and_return(true)
       expect(service.enough_data?).to be true
     end
     it 'false if the meter data checker returns false' do
-      expect(meter_data_checker).to receive(:at_least_x_days_data?).with(14).and_return(false)
+      expect(meter_data_checker).to receive(:at_least_x_days_data?).with(7).and_return(false)
       expect(service.enough_data?).to be false
     end
   end
 
   describe '#data_available_from' do
     it 'returns date from meter checker' do
-      expect(meter_data_checker).to receive(:date_when_enough_data_available).with(14).and_return(earliest_date)
+      expect(meter_data_checker).to receive(:date_when_enough_data_available).with(7).and_return(earliest_date)
       expect(service.data_available_from).to eq(earliest_date)
     end
   end
