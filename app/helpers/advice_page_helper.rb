8288b6143ab8fd31003797f141bffbdee3c76acb
Ian Townsend <ian.townsend@gmail.com>
2023-04-27 14:40:47 +0100

    Reduce to 0.001

--- app/helpers/advice_page_helper.rb
+++ app/helpers/advice_page_helper.rb
@@ -21,7 +21,7 @@ module AdvicePageHelper
 
   def format_unit(value, units, in_table = true, user_numeric_comprehension_level = :ks2)
     # Ensure all tiny numbers are displayed as zero (e.g. -0.000000000000004736951571734001 should be shown as 0 and not -4.7e-15)
-    value = value.round if value&.between?(-1, 1)
+    value = 0.0 if value&.between?(-0.001, 0.001)
     FormatEnergyUnit.format(units, value, :html, false, in_table, user_numeric_comprehension_level).html_safe
   end
 
