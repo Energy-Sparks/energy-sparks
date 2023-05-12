96aed16153e13c2d501998f065540cdc7cccd205
Ian Townsend <ian.townsend@gmail.com>
2023-05-12 09:11:22 +0100

    Check to see if test still fails on github

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,15 +8,15 @@ class PasswordsController < Devise::PasswordsController
     super
     # the resource is NOT the actual user - have to find it ourselves
     user = User.with_reset_password_token(params[:reset_password_token])
-    if user
-      @allow_newsletters = allow_newletters?(user)
-      @allow_alerts = allow_alerts?(user)
-      @subscribe_alerts = true
-      @subscribe_newsletters = true
-      resource.preferred_locale = user.preferred_locale
-    else
-      redirect_to new_user_password_path, notice: t('errors.messages.reset_password_token_is_invalid') and return
-    end
+    # if user
+    @allow_newsletters = allow_newletters?(user)
+    @allow_alerts = allow_alerts?(user)
+    @subscribe_alerts = true
+    @subscribe_newsletters = true
+    resource.preferred_locale = user.preferred_locale
+    # else
+    #   redirect_to new_user_password_path, notice: t('errors.messages.reset_password_token_is_invalid') and return
+    # end
   end
 
   def update
