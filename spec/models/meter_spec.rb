ac467ab504dd18160ce893321cb904d3203ef504
Ian Townsend <iant@users.noreply.github.com>
2023-05-23 11:35:01 +0100

    Add meter dates to Data Source meter report (#2774)

--- spec/models/meter_spec.rb
+++ spec/models/meter_spec.rb
@@ -288,7 +288,7 @@ describe 'Meter', :meters do
   describe ".to_csv" do
     let(:data_source) { create(:data_source) }
     subject { data_source.meters.to_csv }
-    let(:header) { "School group,School,MPAN/MPRN,Meter type,Active,Created at,Updated at" }
+    let(:header) { "School group,School,MPAN/MPRN,Meter type,Active,Last validated meter reading,Created at,Updated at" }
 
     context "with meters" do
       let!(:meters) do
@@ -300,7 +300,7 @@ describe 'Meter', :meters do
       2.times do |i|
         it { expect(subject.lines[i+1].chomp).to eq([
           meters[i].school.school_group.try(:name), meters[i].school.name, meters[i].mpan_mprn,
-          meters[i].meter_type.humanize, meters[i].active, meters[i].created_at, meters[i].updated_at].join(',')) }
+          meters[i].meter_type.humanize, meters[i].active,nil, meters[i].created_at, meters[i].updated_at].join(',')) }
       end
     end
 
