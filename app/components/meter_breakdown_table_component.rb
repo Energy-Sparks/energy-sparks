243072a70c750505dbb1231d1ed04fdd428f1530
Ian Townsend <ian.townsend@gmail.com>
2023-05-26 17:58:42 +0100

    Setup config

--- app/components/meter_breakdown_table_component.rb
+++ app/components/meter_breakdown_table_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class MeterBreakdownTableComponent < ViewComponent::Base
-  def initialize(headers:, rows:, footers:, options:)
+  def initialize(headers: [], rows: [], footers: [], options: [])
     @headers = headers
     @rows = rows
     @footers = footers
