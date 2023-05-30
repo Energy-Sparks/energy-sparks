2a40a6b60dfc5d5360f5685e8f4e593c24c698fe
Ian Townsend <ian.townsend@gmail.com>
2023-05-30 12:41:19 +0100

    Build map page

--- app/controllers/school_groups_controller.rb
+++ app/controllers/school_groups_controller.rb
@@ -2,6 +2,7 @@ class SchoolGroupsController < ApplicationController
   include PartnersHelper
 
   load_and_authorize_resource
+  before_action :find_schools_and_partners
 
   skip_before_action :authenticate_user!, only: [:show, :map]
 
@@ -19,16 +20,17 @@ class SchoolGroupsController < ApplicationController
 
   private
 
-  def current_dashboard
+  def find_schools_and_partners
     @schools = @school_group.schools.visible.by_name
     @partners = @school_group.partners
+  end
+
+  def current_dashboard
     render 'current_dashboard'
   end
 
   def enhanced_dashboard
     if can?(:show, SchoolGroup)
-      @schools = @school_group.schools.visible.by_name
-      @partners = @school_group.partners
       render 'enhanced_dashboard'
     else
       redirect_to map_school_group_path(@school_group)
