aac1a5ca44940c1a9d23b252b9c423fbdb19be0f
Ian Townsend <iant@users.noreply.github.com>
2023-05-30 16:17:11 +0100

    Add school group feature flag to switch between current and new templates (#2790)

--- app/controllers/school_groups_controller.rb
+++ app/controllers/school_groups_controller.rb
@@ -2,11 +2,63 @@ class SchoolGroupsController < ApplicationController
   include PartnersHelper
 
   load_and_authorize_resource
+  before_action :find_schools_and_partners
+  before_action :build_breadcrumbs
 
-  skip_before_action :authenticate_user!, only: :show
+  skip_before_action :authenticate_user!, only: [:show, :map]
 
   def show
+    if EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+      enhanced_dashboard
+    else
+      current_dashboard
+    end
+  end
+
+  def map
+    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+  end
+
+  def recent_usage
+    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+  end
+
+  def comparisons
+    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+  end
+
+  def priority_actions
+    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+  end
+
+  def current_scores
+    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+  end
+
+  private
+
+  def build_breadcrumbs
+    @breadcrumbs = [
+      { name: 'Schools' },
+      { name: @school_group.name, href: school_group_path(@school_group) },
+      { name: I18n.t("school_groups.titles.#{action_name}") }
+    ]
+  end
+
+  def find_schools_and_partners
     @schools = @school_group.schools.visible.by_name
     @partners = @school_group.partners
+  end
+
+  def current_dashboard
+    render 'current_dashboard'
+  end
+
+  def enhanced_dashboard
+    if can?(:show, SchoolGroup)
+      render 'enhanced_dashboard'
+    else
+      redirect_to map_school_group_path(@school_group)
+    end
   end
 end
