e2f629bb91a4fc84e4b835354e34bc6659c79103
Ian Townsend <iant@users.noreply.github.com>
2023-05-31 10:08:15 +0100

    Hide school group dashboard for private school groups (#2791)

--- app/controllers/school_groups_controller.rb
+++ app/controllers/school_groups_controller.rb
@@ -1,11 +1,13 @@
 class SchoolGroupsController < ApplicationController
   include PartnersHelper
 
-  load_and_authorize_resource
+  before_action :find_school_group
+  before_action :redirect_unless_feature_enabled, only: [:map, :comparisons, :priority_actions, :current_scores]
+  before_action :redirect_unless_authorised, only: [:comparisons, :priority_actions, :current_scores]
   before_action :find_schools_and_partners
   before_action :build_breadcrumbs
 
-  skip_before_action :authenticate_user!, only: [:show, :map]
+  skip_before_action :authenticate_user!
 
   def show
     if EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
@@ -16,26 +18,30 @@ class SchoolGroupsController < ApplicationController
   end
 
   def map
-    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
-  end
-
-  def recent_usage
-    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
   end
 
   def comparisons
-    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
   end
 
   def priority_actions
-    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
   end
 
   def current_scores
-    redirect_to school_group_path(@school_group) unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
   end
 
   private
+
+  def redirect_unless_feature_enabled
+    redirect_to school_group_path(@school_group) and return unless EnergySparks::FeatureFlags.active?(:enhanced_school_group_dashboard)
+  end
+
+  def redirect_unless_authorised
+    redirect_to map_school_group_path(@school_group) and return if cannot?(:compare, @school_group)
+  end
+
+  def find_school_group
+    @school_group = SchoolGroup.find(params[:id])
+  end
 
   def build_breadcrumbs
     @breadcrumbs = [
@@ -51,14 +57,15 @@ class SchoolGroupsController < ApplicationController
   end
 
   def current_dashboard
+    authorize! :show, @school_group
     render 'current_dashboard'
   end
 
   def enhanced_dashboard
-    if can?(:show, SchoolGroup)
+    if can?(:compare, @school_group)
       render 'enhanced_dashboard'
     else
-      redirect_to map_school_group_path(@school_group)
+      redirect_to map_school_group_path(@school_group) and return
     end
   end
 end
