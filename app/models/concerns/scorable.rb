3fa375d7f4f827a91d7da61cb7e625fd0783b17e
Ian Townsend <ian.townsend@gmail.com>
2023-06-01 16:19:30 +0100

    Keep query the same

--- app/models/concerns/scorable.rb
+++ app/models/concerns/scorable.rb
@@ -2,7 +2,7 @@ module Scorable
   extend ActiveSupport::Concern
 
   def scored(recent_boundary: 1.month.ago)
-    schools.order(:name).visible.select('schools.*, SUM(observations.points) AS sum_points, MAX(observations.at) AS recent_observation').select(
+    schools.visible.select('schools.*, SUM(observations.points) AS sum_points, MAX(observations.at) AS recent_observation').select(
       self.class.sanitize_sql_array(
         ['SUM(observations.points) FILTER (WHERE observations.at > ?) AS recent_points', recent_boundary]
       )
