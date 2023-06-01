53f8a96deb6d49fb1ec339bcd441dda356c973bd
Ian Townsend <ian.townsend@gmail.com>
2023-06-01 10:04:09 +0100

    Add a scoreboard tab to the school group dashboard

--- app/models/school_group.rb
+++ app/models/school_group.rb
@@ -131,4 +131,16 @@ class SchoolGroup < ApplicationRecord
   def email_locales
     default_country == 'wales' ? [:en, :cy] : [:en]
   end
+
+  def scored_schools(recent_boundary: 1.month.ago)
+    scored = schools.order(:name).visible.select('schools.*, SUM(observations.points) AS sum_points, MAX(observations.at) AS recent_observation').select(
+      self.class.sanitize_sql_array(
+        ['SUM(observations.points) FILTER (WHERE observations.at > ?) AS recent_points', recent_boundary]
+      )
+    ).
+      order(Arel.sql('sum_points DESC NULLS LAST, MAX(observations.at) DESC, schools.name ASC')).
+      group('schools.id')
+
+    ScoredSchoolsList.new(scored.left_outer_joins(:observations))
+  end
 end
