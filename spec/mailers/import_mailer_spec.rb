f0d1792979d4c2923f5370106e9035756989f7ab
Ian Townsend <ian.townsend@gmail.com>
2023-06-06 11:06:41 +0100

    Add test

---
+++ spec/mailers/import_mailer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe ImportMailer, include_application_helper: true do
+  describe '#import_summary' do
+    let(:sheffield_school) { create(:school, :with_school_group, name: "Sheffield School")}
+    let(:bath_school) { create(:school, :with_school_group, name: "Bath School")}
+    let(:sheffield_config) { create(:amr_data_feed_config, description: 'Sheffield') }
+    let(:bath_config) { create(:amr_data_feed_config, description: 'Bath') }
+
+    it 'sends an email for ' do
+      meter_1 = create(:gas_meter_with_validated_reading_dates, :with_unvalidated_readings, start_date: 20.days.ago, end_date: 9.days.ago, school: sheffield_school, data_source: create(:data_source, import_warning_days: 5))
+      bath_import_log = create(:amr_data_feed_import_log, amr_data_feed_config: bath_config, records_imported: 200, import_time: 1.day.ago)
+      meter_2 = create(:gas_meter_with_validated_reading_dates, :with_unvalidated_readings, start_date: 20.days.ago, end_date: 9.days.ago, school: bath_school, data_source: create(:data_source, import_warning_days: 2))
+      ImportNotifier.new.notify(from: 2.days.ago, to: Time.now)
+      email = ActionMailer::Base.deliveries.last
+      expect(email.subject).to include('Energy Sparks import')
+      expect(email.html_part.body).to include("Data issues")
+      expect(email.html_part.body).to include(meter_1.mpan_mprn.to_s)
+      expect(email.html_part.body).to include(meter_1.school_name)
+      expect(email.html_part.body).to include(meter_2.mpan_mprn.to_s)
+      expect(email.html_part.body).to include(meter_2.school_name)
+      attachments = email.attachments[0]
+      expect(attachments.content_type).to include('text/csv')
+      expect(attachments.filename).to eq("[energy-sparks-unknown] Energy Sparks import report: #{Date.today.strftime('%d/%m/%Y')}.csv")
+    end
+  end
+end
