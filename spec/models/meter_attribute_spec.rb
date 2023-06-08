37847f697d36bd09d4211be84f4c1254c32d6534
Leigh Dodds <leigh@ldodds.com>
2023-06-08 11:36:44 +0100

    Add query for report

--- spec/models/meter_attribute_spec.rb
+++ spec/models/meter_attribute_spec.rb
@@ -50,4 +50,25 @@ describe MeterAttribute do
     end
 
   end
+
+  describe '.solar_panels' do
+    let(:config)  { {start_date: "2022-01-01", kwp: "10", end_date: "2023-01-01", orientation: '0', tilt: '30', shading: '6', fit_£_per_kwh: '0.3', maximum_export_level_kw: '5'} }
+    let!(:solar_attribute) { create(:meter_attribute, attribute_type: :solar_pv, input_data: config)}
+    let!(:other_attribute) { create(:meter_attribute, attribute_type: :tariff, input_data: {type: 'economy_7'})}
+    let(:solar_panels)  { MeterAttribute.solar_panels }
+    let(:panel)         { solar_panels.first }
+    it 'returns expected data' do
+      expect(solar_panels.size).to eq 1
+      expect(panel.meter_attribute_id).to eq solar_attribute.id
+      expect(panel.meter_id).to eq solar_attribute.meter.id
+      expect(panel.school_id).to eq solar_attribute.meter.school.id
+      expect(panel.start_date).to eq '2022-01-01'
+      expect(panel.end_date).to eq '2023-01-01'
+      expect(panel.orientation).to eq '0'
+      expect(panel.tilt).to eq '30'
+      expect(panel.shading).to eq '6'
+      expect(panel.fit_£_per_kwh).to eq '0.3'
+      expect(panel.maximum_export_level_kw).to eq '5'
+    end
+  end
 end
