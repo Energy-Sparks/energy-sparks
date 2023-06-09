c90915a98a1ee851fdd01943c06691f7a7a42c92
Ian Townsend <iant@users.noreply.github.com>
2023-06-09 08:44:54 +0100

    Create school group comparison page & component (#2809)

--- app/helpers/school_groups_helper.rb
+++ app/helpers/school_groups_helper.rb
@@ -6,4 +6,17 @@ module SchoolGroupsHelper
   def sort_priority_actions(list_of_savings)
     list_of_savings.sort {|a, b| a.school.name <=> b.school.name }
   end
+
+  def compare_benchmark_key_for(advice_page_key)
+    case advice_page_key
+    when :baseload then :baseload_per_pupil
+    when :electricity_intraday then :electricity_peak_kw_per_pupil
+    when :electricity_long_term then :annual_electricity_costs_per_pupil
+    when :electricity_out_of_hours then :annual_electricity_out_of_hours_use
+    when :gas_long_term then :annual_heating_costs_per_floor_area
+    when :gas_out_of_hours then :annual_gas_out_of_hours_use
+    when :heating_control then :heating_in_warm_weather #heating_coming_on_too_early
+    when :thermostatic_control then :thermostatic_control
+    end
+  end
 end
