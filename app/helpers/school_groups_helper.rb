708196ac8bff1f6c306bce6d259e0239ef86f051
Ian Townsend <ian.townsend@gmail.com>
2023-06-13 11:36:35 +0100

    Allow usage to switch between different views of the recent usage table

--- app/helpers/school_groups_helper.rb
+++ app/helpers/school_groups_helper.rb
@@ -7,6 +7,25 @@ module SchoolGroupsHelper
     list_of_savings.sort {|a, b| a.school.name <=> b.school.name }
   end
 
+  def radio_button_checked_for(metric)
+    return true if params['metric'] == metric
+    return true if params['metric'].blank? && metric == 'change'
+
+    false
+  end
+
+  def value_for(recent_usage)
+    return '' unless recent_usage
+    case params['metric']
+    when 'usage' then recent_usage.usage
+    when 'co2' then recent_usage.co2
+    when 'cost' then recent_usage.cost
+    when 'change' then recent_usage.change
+    else
+      recent_usage.change
+    end
+  end
+
   def compare_benchmark_key_for(advice_page_key)
     case advice_page_key
     when :baseload then :baseload_per_pupil
