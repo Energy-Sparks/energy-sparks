13bce43b261be51a32102e32067b0f6c9163d999
Ian Townsend <ian.townsend@gmail.com>
2023-06-14 08:03:43 +0100

    Update migration

--- db/migrate/20230613155937_add_group_type_to_group.rb
+++ db/migrate/20230613155937_add_group_type_to_group.rb
@@ -1,14 +1,9 @@
 class AddGroupTypeToGroup < ActiveRecord::Migration[6.0]
   def up
-    execute <<-SQL
-      CREATE TYPE group_types AS ENUM ('general', 'local_authority', 'multi_academy_trust');
-    SQL
-    add_column :school_groups, :group_type, :group_types, default: 'general'
+    add_column :school_groups, :group_type, :integer, default: 0
   end
+
   def down
     remove_column :school_groups, :group_type
-    execute <<-SQL
-      DROP TYPE group_types;
-    SQL
   end
 end
