bb6c323191cbaa1af90af8e844c3e3f8a5659ac2
Ian Townsend <ian.townsend@gmail.com>
2023-06-16 16:29:00 +0100

    Add CSV downloads for tables on school group dashboard: 3. Current Scores

--- app/controllers/school_groups_controller.rb
+++ app/controllers/school_groups_controller.rb
@@ -34,6 +34,13 @@ class SchoolGroupsController < ApplicationController
   end
 
   def current_scores
+    respond_to do |format|
+      format.html {}
+      format.csv do
+        send_data SchoolGroups::CurrentScoresCsvGenerator.new(school_group: @school_group).export,
+        filename: "#{@school_group.name} - #{I18n.t('school_groups.titles.current_scores')} (#{Time.zone.now.strftime('%d/%m/%Y')}).csv"
+      end
+    end
   end
 
   private
