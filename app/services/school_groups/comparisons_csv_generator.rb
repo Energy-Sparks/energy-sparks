c6749bc054288ff3506e397a8b7529c70b6f9117
Ian Townsend <ian.townsend@gmail.com>
2023-06-21 09:11:54 +0100

    Updates

--- app/services/school_groups/comparisons_csv_generator.rb
+++ app/services/school_groups/comparisons_csv_generator.rb
@@ -2,20 +2,40 @@ module SchoolGroups
   class ComparisonsCsvGenerator
     def initialize(school_group:, advice_page_keys: [])
       @school_group = school_group
-      @advice_page_keys = advice_page_keys
+      @advice_page_keys = advice_page_keys.map(&:to_sym)
     end
 
     def export
       CSV.generate(headers: true) do |csv|
         csv << headers
+        @school_group.categorise_schools.each do |fuel_type, advice_pages|
+          advice_pages.each do |advice_page_key, comparison|
+            next unless @advice_page_keys.include?(advice_page_key)
+
+            SchoolGroupComparisonComponent::CATEGORIES.each do |category|
+              comparison[category]&.each do |school|
+                csv << [
+                  I18n.t("common.#{fuel_type}"),
+                  I18n.t("advice_pages.#{advice_page_key}.page_title"),
+                  school['school_name'],
+                  I18n.t("advice_pages.benchmarks.#{category}")
+                ]
+              end
+            end
+          end
+        end
       end
     end
 
     private
 
-
     def headers
-      []
+      [
+        I18n.t('advice_pages.index.priorities.table.columns.fuel_type'),
+        I18n.t('advice_pages.index.priorities.table.columns.description'),
+        I18n.t('common.school'),
+        I18n.t('school_groups.comparisons.category')
+      ]
     end
   end
 end
