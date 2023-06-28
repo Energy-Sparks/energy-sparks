8beaae5cc053093ae0ab83c069b56d503f9b1b88
Leigh Dodds <leigh@ldodds.com>
2023-06-28 14:32:04 +0100

    Keep up to a month of batch data (#2876)

--- app/services/alerts/delete_benchmark_run_service.rb
+++ app/services/alerts/delete_benchmark_run_service.rb
@@ -1,6 +1,6 @@
 module Alerts
   class DeleteBenchmarkRunService
-    DEFAULT_OLDER_THAN = 3.months.ago.beginning_of_month
+    DEFAULT_OLDER_THAN = 1.month.ago.beginning_of_month
     attr_reader :older_than
 
     def initialize(older_than = DEFAULT_OLDER_THAN)
