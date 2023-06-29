6a9f25215b87ba739c3f9bfcd61f0ac489d0295e
Ian Townsend <ian.townsend@gmail.com>
2023-06-29 10:06:14 +0100

    Remove validation and spec

--- app/models/programme_activity.rb
+++ app/models/programme_activity.rb
@@ -18,14 +18,4 @@ class ProgrammeActivity < ApplicationRecord
   belongs_to :activity_type
   belongs_to :activity
   belongs_to :programme
-
-  validate :activity_type_is_also_a_programme_programme_type_activity_type
-
-  private
-
-  def activity_type_is_also_a_programme_programme_type_activity_type
-    return if programme.programme_type.activity_types.pluck(:id).include?(activity_type.id)
-
-    errors.add(:activity_type, "must be a programme programme type activity type")
-  end
 end
