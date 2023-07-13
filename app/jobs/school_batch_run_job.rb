dc934916ec7936ab46c327ad1e98847dfaf23371
Leigh Dodds <leigh@ldodds.com>
2023-07-13 13:21:22 +0100

    Add a failed status and exception handler

--- app/jobs/school_batch_run_job.rb
+++ app/jobs/school_batch_run_job.rb
@@ -9,5 +9,11 @@ class SchoolBatchRunJob < ApplicationJob
     school_batch_run.update(status: :running)
     ContentRegenerator.new(school_batch_run.school, school_batch_run).perform
     school_batch_run.update(status: :done)
+  rescue => e
+    school_batch_run.error(e.message)
+    school_batch_run.info("FAILED")
+    school_batch_run.update(status: :failed)
+    #ensure job is marked as failed by re-throwing exception
+    raise
   end
 end
--- app/models/school_batch_run.rb
+++ app/models/school_batch_run.rb
@@ -20,7 +20,7 @@ class SchoolBatchRun < ApplicationRecord
   belongs_to :school
   has_many :school_batch_run_log_entries
 
-  enum status: [:pending, :running, :done]
+  enum status: [:pending, :running, :done, :failed]
 
   scope :by_date, -> { order(created_at: :desc) }
 
