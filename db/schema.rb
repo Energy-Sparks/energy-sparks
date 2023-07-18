1ba3838f400424ddb6873819ae8f34dd0c2d2b69
Ian Townsend <ian.townsend@gmail.com>
2023-07-18 16:09:29 +0100

    Score points for completing a programme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_30_103731) do
+ActiveRecord::Schema.define(version: 2023_07_18_150104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -853,13 +853,13 @@ ActiveRecord::Schema.define(version: 2023_06_30_103731) do
     t.index ["replaced_by_id"], name: "index_global_meter_attributes_on_replaced_by_id"
   end
 
-  create_table "good_job_processes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "good_job_processes", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "state"
   end
 
-  create_table "good_job_settings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "good_job_settings", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "key"
@@ -867,7 +867,7 @@ ActiveRecord::Schema.define(version: 2023_06_30_103731) do
     t.index ["key"], name: "index_good_job_settings_on_key", unique: true
   end
 
-  create_table "good_jobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "good_jobs", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|
     t.text "queue_name"
     t.integer "priority"
     t.jsonb "serialized_params"
@@ -1221,6 +1221,7 @@ ActiveRecord::Schema.define(version: 2023_06_30_103731) do
     t.boolean "default", default: false
     t.datetime "created_at", precision: 6, default: "2022-07-06 12:00:00", null: false
     t.datetime "updated_at", precision: 6, default: "2022-07-06 12:00:00", null: false
+    t.integer "bonus_score", default: 0
   end
 
   create_table "programmes", force: :cascade do |t|
