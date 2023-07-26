b1d3ca38328285b2038890839fb5de0f21152127
Ian Townsend <ian.townsend@gmail.com>
2023-07-26 12:07:53 +0100

    Add some more missing helper methods

--- app/helpers/energy_tariffs_helper.rb
+++ app/helpers/energy_tariffs_helper.rb
@@ -1,4 +1,18 @@
 module EnergyTariffsHelper
+  def energy_tariff_price_title(energy_tariff_price)
+    if energy_tariff_price.description.present?
+      "#{energy_tariff_price.description} (#{energy_tariff_price.start_time.to_s(:time)} to #{energy_tariff_price.end_time.to_s(:time)})"
+    else
+      "Rate from #{energy_tariff_price.start_time.to_s(:time)} to #{energy_tariff_price.end_time.to_s(:time)}"
+    end
+  end
+
+  def energy_tariff_prices_text(energy_tariff)
+    if energy_tariff.energy_tariff_prices.map(&:description).include?(EnergyTariffPrice::NIGHT_RATE_DESCRIPTION)
+      I18n.t('schools.tariffs_helper.prices_text')
+    end
+  end
+
   def energy_tariff_charge_for_type(energy_tariff_charges, charge_type)
     energy_tariff_charges.find { |c| c.is_type?([charge_type]) } || EnergyTariffCharge.new(charge_type: charge_type)
   end
@@ -39,5 +53,21 @@ module EnergyTariffsHelper
 
   def energy_tariff_charge_type_units_for(charge_type)
     settings(charge_type).fetch(:units, []).map { |k| [EnergyTariffCharge.charge_type_units[k], k] }
+  end
+
+  def energy_tariff_charge_value(energy_tariff_charge)
+    if energy_tariff_charge.units
+      I18n.t(
+        'schools.tariffs_helper.charge_value',
+        value: number_to_currency(energy_tariff_charge.value, unit: '£'),
+        units: energy_tariff_charge_type_units_humanized(energy_tariff_charge.units)
+      )
+    else
+      energy_tariff_charge.value.to_s
+    end
+  end
+
+  def energy_tariff_charge_type_units_humanized(charge_type_units)
+    EnergyTariffCharge.charge_type_units[charge_type_units.to_sym]
   end
 end
