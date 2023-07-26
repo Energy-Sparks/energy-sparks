562392377613e29632c3247916272e978f3a5afb
Ian Townsend <ian.townsend@gmail.com>
2023-07-26 11:52:05 +0100

    Update some helper methods

--- app/helpers/energy_tariffs_helper.rb
+++ app/helpers/energy_tariffs_helper.rb
@@ -1,4 +1,20 @@
 module EnergyTariffsHelper
+  def energy_tariff_charge_for_type(energy_tariff_charges, charge_type)
+    energy_tariff_charges.find { |c| c.is_type?([charge_type]) } || EnergyTariffCharge.new(charge_type: charge_type)
+  end
+
+  def energy_tariff_charge_type_description(charge_type)
+    settings(charge_type).fetch(:name, charge_type.to_s.humanize)
+  end
+
+  def energy_tariff_charge_type_tip(charge_type)
+    settings(charge_type).fetch(:tip, '')
+  end
+
+  def energy_tariff_charge_type_value_label(charge_type, default = 'Value in £')
+    settings(charge_type).fetch(:label, default)
+  end
+
   def energy_tariff_title(energy_tariff, with_mpxn = false)
     start_date = energy_tariff.start_date.to_s(:es_compact)
     end_date = energy_tariff.end_date.to_s(:es_compact)
@@ -19,5 +35,9 @@ module EnergyTariffsHelper
     end
 
     title
+  end
+
+  def energy_tariff_charge_type_units_for(charge_type)
+    settings(charge_type).fetch(:units, []).map { |k| [EnergyTariffCharge.charge_type_units[k], k] }
   end
 end
