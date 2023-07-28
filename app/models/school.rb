45deff542fb3b373a9ee1e866dd1bcf2c32baa8d
Ian Townsend <ian.townsend@gmail.com>
2023-07-28 09:45:20 +0100

    Urgh optional true

--- app/models/school.rb
+++ app/models/school.rb
@@ -163,7 +163,7 @@ class School < ApplicationRecord
   belongs_to :scoreboard, optional: true
   belongs_to :local_authority_area, optional: true
 
-  belongs_to :funder
+  belongs_to :funder, optional: true
 
   has_one :school_onboarding
   has_one :configuration, class_name: 'Schools::Configuration'
