bffab39863f0140fb3d74cf07cc73a06d20f60a5
Ian Townsend <ian.townsend@gmail.com>
2023-08-03 17:38:08 +0100

    Add helper method and tests

--- app/controllers/concerns/energy_tariffable.rb
+++ app/controllers/concerns/energy_tariffable.rb
@@ -3,10 +3,11 @@ module EnergyTariffable
 
   def load_and_authorize_if_site_setting
     return unless request.path.start_with?('/admin/settings') || @energy_tariff&.tariff_holder_type == 'SiteSettings'
-    if can?(:manage, :admin)
+    if can?(:manage, :admin_functions)
       @site_setting = SiteSettings.current
     else
-      redirect_to new_user_session_path and return
+      flash[:error] = "You are not authorized to view that page."
+      redirect_to root_path
     end
   end
 end
--- app/helpers/energy_tariffs_helper.rb
+++ app/helpers/energy_tariffs_helper.rb
@@ -1,4 +1,19 @@
 module EnergyTariffsHelper
+  def convert_value_to_long_currency(value, currency: '£')
+    return '' unless value.is_a? Numeric
+    value_as_string = value.to_s
+    split_value = value_as_string.split('.')
+
+    value_as_formatted_currency = if split_value.size == 1
+                                    split_value.first + '.00'
+                                  elsif split_value.last.length < 2
+                                    split_value.first + '.' + split_value.last + '0'
+                                  else
+                                    value_as_string
+                                  end
+    currency + value_as_formatted_currency
+  end
+
   def energy_tariff_price_title(energy_tariff_price)
     if energy_tariff_price.description.present?
       "#{energy_tariff_price&.description} (#{energy_tariff_price&.start_time&.to_s(:time)} to #{energy_tariff_price&.end_time&.to_s(:time)})"
@@ -69,5 +84,9 @@ module EnergyTariffsHelper
 
   def energy_tariff_charge_type_units_humanized(charge_type_units)
     EnergyTariffCharge.charge_type_units[charge_type_units.to_sym]
+  end
+
+  def settings(charge_type)
+    EnergyTariffCharge.charge_types[charge_type.to_sym] || {}
   end
 end
