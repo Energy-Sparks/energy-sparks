bb06e790cb56d8015251f7da74de2a19056f4bfd
Ian Townsend <ian.townsend@gmail.com>
2023-08-09 17:55:15 +0100

    Clean up tests

--- spec/system/school_groups/energy_tariffs_spec.rb
+++ spec/system/school_groups/energy_tariffs_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 describe 'energy tariffs', type: :system do
+  # let!(:school_group)          { create(:school_group, public: true) }
+  # let!(:school) { create_active_school(name: "Big School", school_group: school_group)}
+
   let!(:school_group)          { create(:school_group, public: true) }
-  let!(:school) { create_active_school(name: "Big School", school_group: school_group)}
+  let!(:school)              { create(:school, name: 'Big School', school_group: school_group, number_of_pupils: 10, floor_area: 200.0, data_enabled: true, visible: true, active: true) }
 
   around do |example|
     ClimateControl.modify FEATURE_FLAG_USE_NEW_ENERGY_TARIFFS: 'true' do
@@ -12,15 +15,19 @@ describe 'energy tariffs', type: :system do
     end
   end
 
+  before { school_group.schools.reload }
+
   context 'as an admin' do
     let!(:current_user) { create(:admin) }
 
     it_behaves_like "the school group energy tariff forms well navigated"
   end
 
-  # context 'as a school admin' do
-  #   let!(:current_user) { create(:school_admin, school: school) }
-  # end
+  context 'as a group admin' do
+    let(:current_user)            { create(:user, role: :group_admin, school_group: school_group)}
+
+    it_behaves_like "the school group energy tariff forms well navigated"
+  end
 
   context 'as a school user' do
     let!(:current_user) { create(:user, school: school) }
