daa5d4ba84323bbf522fd59a9f3442c94008f594
Leigh Dodds <leigh@ldodds.com>
2023-08-14 12:52:39 +0100

    Refactor site setting specs

--- spec/support/energy_tariff_shared_examples.rb
+++ spec/support/energy_tariff_shared_examples.rb
@@ -121,7 +121,7 @@ RSpec.shared_examples "an electricity tariff editor with no meter selection" do
     click_button('Next')
     click_button('Simple')
 
-    visit school_group_energy_tariffs_path(school_group)
+    visit tariff_index_path
 
     expect(page).to have_content('My First Tariff')
   end
@@ -378,280 +378,6 @@ RSpec.shared_examples "a school tariff editor" do
 
 end
 
-RSpec.shared_examples "the site settings energy tariff forms well navigated" do
-  before(:each) { sign_in(current_user) }
-
-  context 'checks current user role' do
-    it 'expects the current user to be an admin' do
-      expect(['admin', 'analyst']).to include(current_user.role)
-    end
-  end
-
-  context 'has navigation links' do
-    it 'from admin page to site settings energy tariffs index' do
-      visit admin_path
-      expect(current_path).to eq("/admin")
-      click_link('Energy Tariffs')
-      expect(current_path).to eq("/admin/settings/energy_tariffs")
-      expect(page).to have_content(I18n.t('schools.user_tariffs.index.title'))
-      expect(page).not_to have_link('cost analysis pages')
-    end
-  end
-
-  context 'creating flat rate site setting gas tariffs' do
-    it 'can create a tariff and add prices and charges' do
-      visit admin_settings_energy_tariffs_path
-      expect(current_path).to eq("/admin/settings/energy_tariffs")
-      expect(page).to have_content('Manage and view tariffs')
-      expect(page).not_to have_content('My First Gas Tariff')
-
-      click_link('Add gas tariff')
-
-      expect(page).not_to have_content('Select meters for this tariff')
-      expect(page).to have_content('Choose a name and date range')
-
-      fill_in 'Name', with: 'My First Gas Tariff'
-      click_button('Next')
-
-      expect(page).to have_content('My First Gas Tariff')
-
-      fill_in "energy_tariff_price[value]", with: '1.5'
-      click_button('Next')
-
-      expect(page).to have_content('Add standing charges')
-      expect(page).to have_content('My First Gas Tariff')
-
-      fill_in "energy_tariff_charges[fixed_charge][value]", with: '4.56'
-      select 'month', from: 'energy_tariff_charges[fixed_charge][units]'
-      check 'energy_tariff_charges[energy_tariff][ccl]'
-      select '5', from: 'energy_tariff_charges[energy_tariff][vat_rate]'
-
-      click_button('Next')
-
-      expect(page).to have_content('Tariff details')
-      expect(page).to have_content('5')
-      expect(page).to have_content('Flat rate tariff')
-      expect(page).to have_content('£1.50 per kWh')
-      expect(page).to have_content('£4.56 per month')
-      expect(page).not_to have_link('Delete')
-
-      click_link('Finished')
-      expect(page).to have_content('Manage and view tariffs')
-      expect(page).to have_content('My First Gas Tariff')
-
-      energy_tariff = EnergyTariff.last
-      expect(energy_tariff.tariff_holder_type).to eq('SiteSettings')
-      expect(energy_tariff.tariff_holder).to eq(SiteSettings.current)
-      expect(energy_tariff.created_by).to eq(current_user)
-      expect(energy_tariff.updated_by).to eq(nil)
-
-      expect(energy_tariff.meters).to match_array([])
-      expect(energy_tariff.vat_rate).to eq(5)
-      expect(energy_tariff.ccl).to be_truthy
-      energy_tariff_price = energy_tariff.energy_tariff_prices.first
-      expect(energy_tariff_price.start_time.to_s(:time)).to eq('00:00')
-      expect(energy_tariff_price.end_time.to_s(:time)).to eq('23:30')
-      expect(energy_tariff_price.units).to eq('kwh')
-    end
-  end
-
-  context 'creating flat rate electricity tariffs' do
-    it 'can handle partially created tariff with bits missing' do
-      visit admin_settings_energy_tariffs_path
-      expect(current_path).to eq("/admin/settings/energy_tariffs")
-      expect(page).to have_content(I18n.t('schools.user_tariffs.index.title'))
-
-      click_link('Add electricity tariff')
-
-      expect(page).not_to have_content('Select meters for this tariff')
-
-      click_button('Next')
-
-      expect(page).to have_content('Choose a name and date range')
-      fill_in 'Name', with: 'My First Flat Tariff'
-      click_button('Next')
-      click_button('Simple')
-
-      visit admin_settings_energy_tariffs_path
-
-      expect(page).to have_content(I18n.t('schools.user_tariffs.index.title'))
-    end
-
-    it 'can create a flat rate tariff with price' do
-      visit admin_settings_energy_tariffs_path
-      expect(current_path).to eq("/admin/settings/energy_tariffs")
-      click_link('Add electricity tariff')
-
-      fill_in 'Name', with: 'My First Flat Tariff'
-      click_button('Next')
-
-      click_button('Simple')
-
-      fill_in "energy_tariff_price[value]", with: '1.5'
-      click_button('Next')
-
-      click_button('Next')
-
-      expect(page).to have_content('Tariff details')
-      expect(page).to have_content('Flat rate tariff')
-      expect(page).to have_content('£1.50 per kWh')
-      expect(page).not_to have_link('Delete')
-
-      click_link('Finished')
-      expect(page).to have_content('Manage and view tariffs')
-
-      energy_tariff = EnergyTariff.last
-      expect(energy_tariff.tariff_holder_type).to eq('SiteSettings')
-      expect(energy_tariff.tariff_holder).to eq(SiteSettings.current)
-      expect(energy_tariff.created_by).to eq(current_user)
-      expect(energy_tariff.updated_by).to eq(current_user)
-
-      expect(energy_tariff.meters).to match_array([])
-      expect(energy_tariff.tariff_type == 'flat_rate').to be_truthy
-      expect(energy_tariff.vat_rate).to eq(nil)
-      expect(energy_tariff.ccl).to be_falsey
-      energy_tariff_price = energy_tariff.energy_tariff_prices.first
-      expect(energy_tariff_price.start_time.to_s(:time)).to eq('00:00')
-      expect(energy_tariff_price.end_time.to_s(:time)).to eq('23:30')
-      expect(energy_tariff_price.value.to_s).to eq('1.5')
-      expect(energy_tariff_price.units).to eq('kwh')
-    end
-  end
-
-  context 'creating differential electricity tariffs' do
-    it 'can create a tariff and add prices and charges' do
-      visit admin_settings_energy_tariffs_path
-      expect(current_path).to eq("/admin/settings/energy_tariffs")
-      click_link('Add electricity tariff')
-
-      fill_in 'Name', with: 'My First Diff Tariff'
-      click_button('Next')
-
-      click_button('Day/Night tariff')
-
-      expect(page).to have_content('Night rate (00:00 to 07:00)')
-      expect(page).to have_content('Day rate (07:00 to 00:00)')
-      expect(page).not_to have_link('Add rate')
-      expect(page).not_to have_link('Delete')
-
-      first('.energy-tariff-show-button').click
-
-      select '00', from: 'energy_tariff_price_start_time_4i'
-      select '30', from: 'energy_tariff_price_start_time_5i'
-      select '06', from: 'energy_tariff_price_end_time_4i'
-      select '30', from: 'energy_tariff_price_end_time_5i'
-
-      fill_in 'Rate in £/kWh', with: '1.5'
-      click_button('Save')
-
-      expect(page).to have_content('Night rate (00:30 to 06:30)')
-      expect(page).to have_content('Day rate (07:00 to 00:00)')
-      expect(page).to have_content('£1.50 per kWh')
-      expect(page).to have_content('£0.00 per kWh')
-
-      click_link('Next')
-      click_button('Next')
-
-      expect(page).to have_content('Tariff details')
-      expect(page).to have_content('£1.50 per kWh')
-      expect(page).not_to have_link('Delete')
-
-      click_link('Finished')
-      expect(page).to have_content('Manage and view tariffs')
-
-      energy_tariff = EnergyTariff.last
-      expect(energy_tariff.tariff_holder_type).to eq('SiteSettings')
-      expect(energy_tariff.tariff_holder).to eq(SiteSettings.current)
-      expect(energy_tariff.created_by).to eq(current_user)
-      expect(energy_tariff.updated_by).to eq(current_user)
-      expect(energy_tariff.meters).to match_array([])
-      energy_tariff_price = energy_tariff.energy_tariff_prices.first
-      expect(energy_tariff_price.start_time.to_s(:time)).to eq('00:30')
-      expect(energy_tariff_price.end_time.to_s(:time)).to eq('06:30')
-      expect(energy_tariff_price.value.to_s).to eq('1.5')
-      expect(energy_tariff_price.units).to eq('kwh')
-      energy_tariff_price = energy_tariff.energy_tariff_prices.last
-      expect(energy_tariff_price.start_time.to_s(:time)).to eq('07:00')
-      expect(energy_tariff_price.end_time.to_s(:time)).to eq('00:00')
-      expect(energy_tariff_price.value.to_s).to eq('0.0')
-      expect(energy_tariff_price.units).to eq('kwh')
-    end
-  end
-
-  context 'adding electricity standing charges' do
-    it 'can create a tariff and add charges' do
-      visit admin_settings_energy_tariffs_path
-      expect(current_path).to eq("/admin/settings/energy_tariffs")
-
-      click_link('Add electricity tariff')
-
-      fill_in 'Name', with: 'My First Diff Tariff'
-      click_button('Next')
-
-      click_button('Simple')
-
-      fill_in "energy_tariff_price[value]", with: '1.5'
-      click_button('Next')
-
-      fill_in "energy_tariff_charges[fixed_charge][value]", with: '1.11'
-      select 'day', from: 'energy_tariff_charges[fixed_charge][units]'
-
-      fill_in "energy_tariff_charges[site_fee][value]", with: '2.22'
-      select 'month', from: 'energy_tariff_charges[site_fee][units]'
-
-      fill_in "energy_tariff_charges[duos_red][value]", with: '3.33'
-      fill_in "energy_tariff_charges[duos_amber][value]", with: '4.44'
-      fill_in "energy_tariff_charges[duos_green][value]", with: '5.55'
-
-      fill_in "energy_tariff_charges[agreed_availability_charge][value]", with: '6.66'
-      fill_in "energy_tariff_charges[excess_availability_charge][value]", with: '7.77'
-      fill_in "energy_tariff_charges[asc_limit_kw][value]", with: '8.88'
-
-      fill_in "energy_tariff_charges[reactive_power_charge][value]", with: '9.99'
-      fill_in "energy_tariff_charges[feed_in_tariff_levy][value]", with: '9.87'
-
-      fill_in "energy_tariff_charges[settlement_agency_fee][value]", with: '6.54'
-      fill_in "energy_tariff_charges[meter_asset_provider_charge][value]", with: '3.21'
-      fill_in "energy_tariff_charges[nhh_metering_agent_charge][value]", with: '1.9'
-
-      fill_in "energy_tariff_charges[nhh_automatic_meter_reading_charge][value]", with: '.12'
-      fill_in "energy_tariff_charges[data_collection_dcda_agent_charge][value]", with: '.34'
-
-      check 'energy_tariff_charges[energy_tariff][tnuos]'
-      check 'energy_tariff_charges[energy_tariff][ccl]'
-      select '20', from: 'energy_tariff_charges[energy_tariff][vat_rate]'
-
-      click_button('Next')
-      expect(page).to have_content('Tariff details')
-
-      energy_tariff = EnergyTariff.last
-      expect(energy_tariff.tariff_holder_type).to eq('SiteSettings')
-      expect(energy_tariff.tariff_holder).to eq(SiteSettings.current)
-      expect(energy_tariff.created_by).to eq(current_user)
-      expect(energy_tariff.updated_by).to eq(current_user)
-      expect(energy_tariff.tnuos).to be_truthy
-      expect(energy_tariff.ccl).to be_truthy
-      expect(energy_tariff.vat_rate).to eq(20)
-
-      expect(energy_tariff.value_for_charge(:fixed_charge)).to eq('1.11')
-      expect(energy_tariff.value_for_charge(:site_fee)).to eq('2.22')
-      expect(energy_tariff.value_for_charge(:duos_red)).to eq('3.33')
-      expect(energy_tariff.value_for_charge(:duos_amber)).to eq('4.44')
-      expect(energy_tariff.value_for_charge(:duos_green)).to eq('5.55')
-      expect(energy_tariff.value_for_charge(:agreed_availability_charge)).to eq('6.66')
-      expect(energy_tariff.value_for_charge(:excess_availability_charge)).to eq('7.77')
-      expect(energy_tariff.value_for_charge(:asc_limit_kw)).to eq('8.88')
-      expect(energy_tariff.value_for_charge(:reactive_power_charge)).to eq('9.99')
-      expect(energy_tariff.value_for_charge(:feed_in_tariff_levy)).to eq('9.87')
-      expect(energy_tariff.value_for_charge(:settlement_agency_fee)).to eq('6.54')
-      expect(energy_tariff.value_for_charge(:meter_asset_provider_charge)).to eq('3.21')
-      expect(energy_tariff.value_for_charge(:nhh_metering_agent_charge)).to eq('1.9')
-      expect(energy_tariff.value_for_charge(:nhh_automatic_meter_reading_charge)).to eq('0.12')
-      expect(energy_tariff.value_for_charge(:data_collection_dcda_agent_charge)).to eq('0.34')
-    end
-  end
-end
-
 RSpec.shared_examples "a school group energy tariff editor" do
   before(:each) { sign_in(current_user) }
 
@@ -666,12 +392,34 @@ RSpec.shared_examples "a school group energy tariff editor" do
   end
 
   context 'when creating tariffs' do
+    let(:tariff_index_path)   { school_group_energy_tariffs_path(school_group) }
     let(:tariff_holder)       { school_group }
     let(:tariff_holder_type)  { 'SchoolGroup' }
 
     before(:each) do
-      visit school_group_energy_tariffs_path(school_group)
+      visit tariff_index_path
     end
+
+    it_behaves_like "a gas tariff editor with no meter selection"
+    it_behaves_like "an electricity tariff editor with no meter selection"
+  end
+end
+
+RSpec.shared_examples "the site settings energy tariff editor" do
+  before(:each) do
+    visit admin_path
+    click_link('Energy Tariffs')
+  end
+
+  it 'has expected contents' do
+    expect(page).to have_content(I18n.t('schools.user_tariffs.index.title'))
+    expect(page).not_to have_link('cost analysis pages')
+  end
+
+  context 'when creating tariffs' do
+    let(:tariff_index_path)   { admin_settings_energy_tariffs_path }
+    let(:tariff_holder)       { SiteSettings.current }
+    let(:tariff_holder_type)  { 'SiteSettings' }
 
     it_behaves_like "a gas tariff editor with no meter selection"
     it_behaves_like "an electricity tariff editor with no meter selection"
--- spec/system/admin/settings/energy_tariffs_spec.rb
+++ spec/system/admin/settings/energy_tariffs_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe 'site settings energy tariffs', type: :system do
-  let!(:school) { create_active_school(name: "Big School")}
-
   around do |example|
     ClimateControl.modify FEATURE_FLAG_USE_NEW_ENERGY_TARIFFS: 'true' do
       example.run
@@ -12,119 +10,67 @@ describe 'site settings energy tariffs', type: :system do
   context 'as an admin user' do
     let!(:current_user) { create(:admin) }
     before(:each) { sign_in(current_user) }
-
-    context 'allows access to the admin site settings energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/admin/settings/energy_tariffs")
-      end
-
-      it_behaves_like "the site settings energy tariff forms well navigated"
-    end
+    it_behaves_like "the site settings energy tariff editor"
   end
 
   context 'as an analytics user' do
     let!(:current_user) { create(:admin) }
     before(:each) { sign_in(current_user) }
-
-    context 'allows access to the admin site settings energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/admin/settings/energy_tariffs")
-      end
-
-      it_behaves_like "the site settings energy tariff forms well navigated"
-    end
+    it_behaves_like "the site settings energy tariff editor"
   end
 
   context 'as a group_admin user' do
     let!(:current_user) { create(:group_admin) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/users/sign_in")
-      end
-    end
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'as a guest user' do
     let!(:current_user) { create(:guest) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/users/sign_in")
-      end
-    end
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'as a pupil user' do
-    let!(:current_user) { create(:pupil, school: school) }
-    before(:each) { sign_in(current_user) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/pupils/schools/#{school.slug}")
-      end
-    end
+    let!(:current_user) { create(:pupil) }
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'as a school admin user' do
-    let!(:current_user) { create(:school_admin, school: school) }
-    before(:each) { sign_in(current_user) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/schools/#{school.slug}")
-      end
-    end
+    let!(:current_user) { create(:school_admin) }
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'as a school_onboarding user' do
-    let!(:current_user) { create(:onboarding_user, school: school) }
-    before(:each) { sign_in(current_user) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/schools/#{school.slug}")
-      end
-    end
+    let!(:current_user) { create(:onboarding_user) }
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'as a staff user' do
-    let!(:current_user) { create(:staff, school: school) }
-    before(:each) { sign_in(current_user) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/schools/#{school.slug}")
-      end
-    end
+    let!(:current_user) { create(:staff) }
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'as a volunteer user' do
-    let!(:current_user) { create(:volunteer, school: school) }
-    before(:each) { sign_in(current_user) }
-
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the school index page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq("/schools/#{school.slug}")
-      end
-    end
+    let!(:current_user) { create(:volunteer) }
+    let(:path)          { admin_settings_energy_tariffs_path }
+    before              { sign_in(current_user) }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 
   context 'with no signed in user' do
-    context 'does not allow access to the energy tariffs page' do
-      it 'redirects to the sign in page' do
-        visit admin_settings_energy_tariffs_path
-        expect(current_path).to eq('/users/sign_in')
-      end
-    end
+    let!(:current_user) { nil }
+    let(:path)          { admin_settings_energy_tariffs_path }
+    it_behaves_like "the user does not have access to the tariff editor"
   end
 end
