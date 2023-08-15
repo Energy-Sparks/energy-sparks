b9764231f298cabc95c1552453af7b1c835c213f
Ian Townsend <ian.townsend@gmail.com>
2023-08-15 15:49:20 +0100

    Clean up (2)

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,16 +1,6 @@
-Time::DATE_FORMATS[:es_short] = lambda do |date|
-        I18n.l(date, format: '%d %b %Y')
-end
-Time::DATE_FORMATS[:es_compact] = lambda do |date|
-        I18n.l(date, format: '%d/%m/%Y %H:%M')
-end
-Time::DATE_FORMATS[:es_full] = lambda do |date|
-        I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y")
-end
-Date::DATE_FORMATS[:es_short] = lambda do |date|
-        I18n.l(date, format: '%d %b %Y')
-end
-Date::DATE_FORMATS[:es_compact] = '%d/%m/%Y'
-Date::DATE_FORMATS[:es_full] = lambda do |date|
-        I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y")
-end
+Time::DATE_FORMATS[:es_short]   = lambda { |date_time| I18n.l(date_time, format: '%d %b %Y') }
+Time::DATE_FORMATS[:es_compact] = lambda { |date_time| I18n.l(date_time, format: '%d/%m/%Y %H:%M') }
+Time::DATE_FORMATS[:es_full]    = lambda { |date_time| I18n.l(date_time, format: "%a #{date.day.ordinalize} %b %Y") }
+Date::DATE_FORMATS[:es_short]   = lambda { |date| I18n.l(date, format: '%d %b %Y') }
+Date::DATE_FORMATS[:es_compact] = lambda { |date| I18n.l(date, format: '%d/%m/%Y') }
+Date::DATE_FORMATS[:es_full]    = lambda { |date| I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y") }
