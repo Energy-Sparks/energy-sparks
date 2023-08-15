1ab858873ba3730904ccad1331871b1cb0968bce
Ian Townsend <ian.townsend@gmail.com>
2023-08-15 14:59:41 +0100

    Add notes section

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,33 +1,16 @@
-Time::DATE_FORMATS.merge!(
-  {
-    es_short:
-      lambda do |date|
+Time::DATE_FORMATS[:es_short] = lambda do |date|
         I18n.l(date, format: '%d %b %Y')
-      end
-  }
-)
-Time::DATE_FORMATS.merge!(
-  {
-    es_full:
-      lambda do |date|
+end
+Time::DATE_FORMATS[:es_compact] = lambda do |date|
+        I18n.l(date, format: '%d/%m/%Y %H:%M')
+end
+Time::DATE_FORMATS[:es_full] = lambda do |date|
         I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y")
-      end
-  }
-)
-Date::DATE_FORMATS.merge!(
-  {
-    es_short:
-      lambda do |date|
+end
+Date::DATE_FORMATS[:es_short] = lambda do |date|
         I18n.l(date, format: '%d %b %Y')
-      end
-  }
-)
-Date::DATE_FORMATS.merge!({ es_compact: '%d/%m/%Y' })
-Date::DATE_FORMATS.merge!(
-  {
-    es_full:
-      lambda do |date|
+end
+Date::DATE_FORMATS[:es_compact] = '%d/%m/%Y'
+Date::DATE_FORMATS[:es_full] = lambda do |date|
         I18n.l(date, format: "%a #{date.day.ordinalize} %b %Y")
-      end
-  }
-)
+end
