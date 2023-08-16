e73f355bcc02ef6835913aecf81be0cbda38d86d
Leigh Dodds <leigh@ldodds.com>
2023-08-16 15:09:34 +0100

    Replace smart meter tab

--- app/components/energy_tariffs_component.rb
+++ app/components/energy_tariffs_component.rb
@@ -6,9 +6,10 @@ class EnergyTariffsComponent < ViewComponent::Base
   renders_one :header
   renders_one :footer
 
-  def initialize(tariff_holder:, tariff_types:, show_actions: true)
+  def initialize(tariff_holder:, tariff_types:, source: :manually_entered, show_actions: true)
     @tariff_holder = tariff_holder
     @tariff_types = tariff_types
     @show_actions = show_actions
+    @source = source
   end
 end
--- app/helpers/energy_tariffs_helper.rb
+++ app/helpers/energy_tariffs_helper.rb
@@ -146,10 +146,4 @@ module EnergyTariffsHelper
   def any_smart_meters?(school)
     school.meters.dcc.any?
   end
-
-  def smart_meter_tariffs(meter)
-    smart_meter_tariff_attributes = meter.smart_meter_tariff_attributes
-    return [] unless smart_meter_tariff_attributes
-    smart_meter_tariff_attributes[:accounting_tariff_generic]
-  end
 end
