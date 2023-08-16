801899dacca9be8ba30104da283dbc5b21453333
Ian Townsend <ian.townsend@gmail.com>
2023-08-16 10:42:08 +0100

    Update validation

--- spec/models/energy_tariff_price_spec.rb
+++ spec/models/energy_tariff_price_spec.rb
@@ -5,5 +5,5 @@ describe EnergyTariffPrice do
   it { should validate_presence_of(:end_time) }
   it { should validate_presence_of(:value) }
   it { should validate_presence_of(:units) }
-  it { should validate_numericality_of(:value).is_greater_than_or_equal_to(1.0) }
+  it { should validate_numericality_of(:value).is_greater_than_or_equal_to(EnergyTariffPrice::MINIMUM_VALUE) }
 end
