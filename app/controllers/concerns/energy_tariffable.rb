eec52acec1b9a341d1c247ce5740614f5139c69f
Ian Townsend <ian.townsend@gmail.com>
2023-08-17 13:47:36 +0100

    Add breadcrumb title translations

--- app/controllers/concerns/energy_tariffable.rb
+++ app/controllers/concerns/energy_tariffable.rb
@@ -17,6 +17,10 @@ module EnergyTariffable
     end
   end
 
+  def set_page_title
+    @page_title = t("schools.#{controller_name.gsub('energy_tariff', 'user_tariff')}.#{action_name}.breadcrumb_title", default: action_name, name: @energy_tariff&.name)
+  end
+
   def site_settings_resource?
     request.path.start_with?('/admin/settings') || @energy_tariff&.tariff_holder&.site_settings?
   end
