8817424130dbfab9783f710a08fa6cb40f045872
Leigh Dodds <leigh@ldodds.com>
2023-09-05 13:50:51 +0100

    More progress

--- app/services/tariffs/tariff_lookup_service.rb
+++ app/services/tariffs/tariff_lookup_service.rb
@@ -16,21 +16,24 @@ module Tariffs
 
     private
 
-    #TODO sorting
+    #TODO sorting, to ensure we do all meter, school, school group tariffs first
     def select_tariffs(potential_tariffs, start_date, end_date, earliest_start = start_date, earliest_end = end_date)
       tariffs = {}
       return tariffs if potential_tariffs.empty?
 
       potential_tariff = potential_tariffs.first
 
-      puts outside_range?(potential_tariff, start_date, end_date)
-      puts fully_cover_range?(potential_tariff, start_date, end_date)
+      ap start_date
+      ap end_date
+      ap potential_tariff
+
       if outside_range?(potential_tariff, start_date, end_date)
         #tariff doesnt align with dates we need so continue looking
         tariffs.merge!(select_tariffs(potential_tariffs[1..], start_date, end_date, earliest_start, earliest_end))
       elsif fully_cover_range?(potential_tariff, start_date, end_date)
+        puts "FULL COVER"
         #tariff covers entire range, so we're done
-        tariffs[potential_tariff] = Range.new(start_date, end_date)
+        tariffs[Range.new(start_date, end_date)] = potential_tariff
       else
         puts "HERE"
         #...iterate with reduced list and updated dates
@@ -39,26 +42,29 @@ module Tariffs
         if (potential_tariff.start_date.nil? || potential_tariff.start_date <= start_date) && potential_tariff.end_date <= end_date
           puts "FIRST PART"
           #in which case add that, then continue looking with end of range dates
-          tariffs[potential_tariff] = Range.new(start_date, potential_tariff.end_date)
+          tariffs[Range.new(start_date, potential_tariff.end_date)] = potential_tariff
           tariffs.merge!(select_tariffs(potential_tariffs[1..], potential_tariff.end_date + 1, end_date, earliest_start, earliest_end))
         end
         #Cover the last part of the range
         if potential_tariff.start_date > start_date && potential_tariff.end_date >= end_date
           puts "LAST PART"
+          puts earliest_start
+          puts earliest_end
           #in which case add that, then continue looking with start of range dates
-          tariffs[potential_tariff] = Range.new(potential_tariff.start_date, end_date)
-          tariffs.merge!(select_tariffs(potential_tariffs[1..], earliest_start, potential_tariff.start_date, earliest_start, earliest_end))
+          tariffs[Range.new(potential_tariff.start_date, end_date)] = potential_tariff
+          tariffs.merge!(select_tariffs(potential_tariffs[1..], earliest_start, potential_tariff.start_date - 1, earliest_start, earliest_end))
         end
         #Cover the middle of the range
         if potential_tariff.start_date > start_date && potential_tariff.end_date < end_date
           puts "SPLIT"
           #in which case add that and look for first and last part of the ranges
-          tariffs[potential_tariff] = Range.new(potential_tariff.start_date, potential_tariff.end_date)
+          tariffs[Range.new(potential_tariff.start_date, potential_tariff.end_date)] = potential_tariff
           #split range, call select twice
           #for start
-          tariffs.merge!(select_tariffs(potential_tariffs[1..], earliest_start, potential_tariff.start_date, earliest_start, earliest_end))
+          tariffs.merge!(select_tariffs(potential_tariffs[1..], earliest_start, potential_tariff.start_date - 1, earliest_start, earliest_end))
           #and end
-          tariffs.merge!(select_tariffs(potential_tariffs[1..], potential_tariff.end_date, earliest_end, earliest_start, earliest_end))
+          #but earliest end should be at most earliest end, but if there's an existing range that starts later, end there
+          tariffs.merge!(select_tariffs(potential_tariffs[1..], potential_tariff.end_date + 1, earliest_end, earliest_start, earliest_end))
         end
       end
       tariffs
--- spec/services/tariffs/tariff_lookup_service_spec.rb
+++ spec/services/tariffs/tariff_lookup_service_spec.rb
@@ -26,14 +26,16 @@ describe Tariffs::TariffLookupService, type: :service do
     }
     context 'and there are completely open ended system wide tariffs' do
       it 'returns the tariffs with the applicable dates' do
-        expect(tariff_dates[system_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(system_tariff)
       end
     end
     context 'and there is an open ended system tariff' do
       let(:system_start_date)    { Date.new(2022, 1, 1) }
       let(:system_end_date)      { nil }
       it 'returns the tariffs' do
-        expect(tariff_dates[system_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(system_tariff)
       end
     end
     context 'when tariffs outside range' do
@@ -47,14 +49,16 @@ describe Tariffs::TariffLookupService, type: :service do
       let(:system_start_date)    { Date.new(2022, 6, 1) }
       let(:system_end_date)      { Date.new(2022, 12, 31) }
       it 'returns the tariffs' do
-        expect(tariff_dates[system_tariff]).to eq(system_start_date..system_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[system_start_date..system_end_date]).to eq(system_tariff)
       end
     end
     context 'when the tariff ends during the period' do
       let(:system_start_date)    { Date.new(2022, 1, 1) }
       let(:system_end_date)      { Date.new(2022, 6, 30) }
       it 'returns the tariffs' do
-        expect(tariff_dates[system_tariff]).to eq(system_start_date..system_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[system_start_date..system_end_date]).to eq(system_tariff)
       end
     end
   end
@@ -73,22 +77,29 @@ describe Tariffs::TariffLookupService, type: :service do
     }
     context 'and there are open ended group tariffs' do
       it 'returns the group tariffs' do
-        expect(tariff_dates[group_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(group_tariff)
       end
     end
     context 'when the dates only partially overlap' do
       let(:group_start_date)    { Date.new(2022, 6, 1) }
       let(:group_end_date)      { Date.new(2022, 12, 31) }
+
+      let(:lookup_start_date)    { Date.new(2022, 1, 1) }
+      let(:lookup_end_date)      { Date.new(2022, 12, 31) }
+
       it 'also returns the group and system wide tariffs' do
-        expect(tariff_dates[group_tariff]).to eq(group_start_date..lookup_end_date)
-        expect(tariff_dates[system_tariff]).to eq(lookup_start_date..group_start_date)
+        expect(tariff_dates.keys.count).to eq 2
+        expect(tariff_dates[lookup_start_date..group_start_date-1]).to eq(system_tariff)
+        expect(tariff_dates[group_start_date..lookup_end_date]).to eq(group_tariff)
       end
     end
     context 'when group tariffs are outside the range' do
       let(:group_start_date)    { Date.new(2021, 1, 1) }
       let(:group_end_date)      { Date.new(2021, 12, 31) }
       it 'returns system wide tariff' do
-        expect(tariff_dates[system_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(system_tariff)
       end
     end
   end
@@ -116,7 +127,8 @@ describe Tariffs::TariffLookupService, type: :service do
 
     context 'and school tariff covers entire range' do
       it 'returns the school tariff' do
-        expect(tariff_dates[school_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(school_tariff)
       end
     end
 
@@ -125,8 +137,9 @@ describe Tariffs::TariffLookupService, type: :service do
       let(:school_end_date)      { Date.new(2022, 12, 31) }
 
       it 'returns both tariffs' do
-        expect(tariff_dates[group_tariff]).to eq(lookup_start_date..Date.new(2022, 5, 30))
-        expect(tariff_dates[school_tariff]).to eq(school_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 2
+        expect(tariff_dates[lookup_start_date..(school_start_date - 1)]).to eq(group_tariff)
+        expect(tariff_dates[school_start_date..lookup_end_date]).to eq(school_tariff)
       end
     end
 
@@ -135,8 +148,9 @@ describe Tariffs::TariffLookupService, type: :service do
       let(:school_end_date)      { Date.new(2022, 6, 1) }
 
       it 'returns both tariffs' do
-        expect(tariff_dates[school_tariff]).to eq(lookup_start_date..school_end_date)
-        expect(tariff_dates[group_tariff]).to eq((school_end_date+1)..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 2
+        expect(tariff_dates[lookup_start_date..school_end_date]).to eq(school_tariff)
+        expect(tariff_dates[(school_end_date+1)..lookup_end_date]).to eq(group_tariff)
       end
     end
 
@@ -144,49 +158,154 @@ describe Tariffs::TariffLookupService, type: :service do
       let(:school_start_date)    { Date.new(2023, 1, 1) }
       let(:school_end_date)      { Date.new(2023, 6, 1) }
       it 'falls back to group tariff' do
-        expect(tariff_dates[group_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(group_tariff)
       end
     end
 
     context 'when we query outside school and group tariffs' do
+      let(:group_start_date)    { Date.new(2020,1,1) }
+      let(:group_end_date)      { Date.new(2022,12,31) }
+
+      let(:school_start_date)    { Date.new(2021, 1, 1) }
+      let(:school_end_date)      { Date.new(2022, 12, 31) }
+
       let(:lookup_start_date)    { Date.new(2023, 1, 1) }
       let(:lookup_end_date)      { Date.new(2023, 6, 1) }
 
-      let(:school_start_date)    { Date.new(2012, 1, 1) }
-      let(:school_end_date)      { Date.new(2022, 12, 31) }
-
       it 'falls back to system tariff tariff' do
-        expect(tariff_dates[system_tariff]).to eq(lookup_start_date..lookup_end_date)
+        expect(tariff_dates.keys.count).to eq 1
+        expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(system_tariff)
       end
     end
 
     context 'when the dates span all three tariffs' do
-      let(:lookup_start_date)    { Date.new(2020, 1, 1) }
-      let(:lookup_end_date)      { Date.new(2023, 6, 1) }
+      let(:group_start_date)    { Date.new(2021,1,1) }
+      let(:group_end_date)      { Date.new(2023,12,31) }
 
       let(:school_start_date)    { Date.new(2022, 1, 1) }
       let(:school_end_date)      { Date.new(2022, 12, 31) }
 
+      let(:lookup_start_date)    { Date.new(2020, 1, 1) }
+      let(:lookup_end_date)      { Date.new(2023, 6, 1) }
+
       it 'returns the school, group and system wide tariffs' do
-        expect(tariff_dates[school_tariff]).to eq(school_start_date..school_end_date)
-        expect(tariff_dates[group_tariff]).to eq(Date.new(2021,1,1)..(school_start_date - 1))
-        #TODO
-        expect(tariff_dates[system_tariff]).to eq(lookup_start_date..(Date.new(2021,1,1) - 1))
+        #system 2020-2021, group 2021-2022, school 2022, group 2023
+        expect(tariff_dates.keys.count).to eq 4
+        #system tariff before group tariff
+        expect(tariff_dates[lookup_start_date..Date.new(2020,12,31)]).to eq(system_tariff)
+        #then group tariff in 2021
+        expect(tariff_dates[Date.new(2021,1,1)..(school_start_date - 1)]).to eq(group_tariff)
+        #then school tariff in 2022
+        expect(tariff_dates[school_start_date..school_end_date]).to eq(school_tariff)
+        #then back to group tariff
+        expect(tariff_dates[school_end_date+1..lookup_end_date]).to eq(group_tariff)
+      end
+    end
+
+    context 'when there are multiple school tariffs within the lookup period' do
+      let(:group_start_date)    { Date.new(2021,1,1) }
+      let(:group_end_date)      { Date.new(2023,12,31) }
+
+      let(:school_start_date)    { Date.new(2022, 1, 1) }
+      let(:school_end_date)      { Date.new(2022, 12, 31) }
+
+      let(:school_start_date_2)    { Date.new(2023, 1, 1) }
+      let(:school_end_date_2)      { Date.new(2023, 12, 31) }
+
+      let!(:school_tariff_2) {
+        create(:energy_tariff, :with_flat_price, tariff_holder: school,
+              start_date: school_start_date_2, end_date: school_end_date_2)
+      }
+
+      let(:lookup_start_date)    { Date.new(2021, 1, 1) }
+      let(:lookup_end_date)      { Date.new(2023, 12, 31) }
+
+      it 'returns all the school tariffs first' do
+        #group, school, then school_2
+        expect(tariff_dates.keys.count).to eq 3
+        expect(tariff_dates[lookup_start_date..(school_start_date - 1)]).to eq(group_tariff)
+        expect(tariff_dates[school_start_date..school_end_date]).to eq(school_tariff)
+        expect(tariff_dates[school_start_date_2..school_end_date_2]).to eq(school_tariff_2)
       end
     end
 
     context 'when the tariffs dont align across the lookup period' do
-      it 'returns the full set of tariffs with gaps filled'
+      let(:group_start_date)    { Date.new(2021,1,1) }
+      let(:group_end_date)      { Date.new(2021,6,30) }
+
+      #leaves a gap of a day where we'll fall back to system tariff
+      let(:school_start_date)    { Date.new(2022, 1, 1) }
+      let(:school_end_date)      { Date.new(2022, 12, 30) }
+
+      let(:school_start_date_2)    { Date.new(2023, 1, 1) }
+      let(:school_end_date_2)      { Date.new(2023, 12, 31) }
+
+      let!(:school_tariff_2) {
+        create(:energy_tariff, :with_flat_price, tariff_holder: school, name: "School tariff 2",
+              start_date: school_start_date_2, end_date: school_end_date_2)
+      }
+
+      let(:lookup_start_date)    { Date.new(2021, 1, 1) }
+      let(:lookup_end_date)      { Date.new(2023, 12, 31) }
+
+      it 'returns the full set of tariffs with gaps filled' do
+        #group, then system, then school, then system again for a day, then school_2
+        expect(tariff_dates.keys.count).to eq 5
+        ap tariff_dates
+        expect(tariff_dates[lookup_start_date..group_end_date]).to eq(group_tariff)
+        expect(tariff_dates[(group_end_date + 1)..(school_start_date - 1)]).to eq(system_tariff)
+        expect(tariff_dates[school_start_date..school_end_date]).to eq(school_tariff)
+        expect(tariff_dates[Date.new(2022,12,31)..Date.new(2022,12,31)]).to eq(system_tariff)
+        expect(tariff_dates[school_start_date_2..school_end_date_2]).to eq(school_tariff_2)
+      end
     end
+
   end
 
   context 'with system, group, school and meter tariffs' do
     let(:meter)       { create(:electricity_meter) }
 
-    it 'can find tariffs for that meter over school tariffs'
+    let!(:system_tariff) {
+      create(:energy_tariff, :with_flat_price, tariff_holder: settings,
+            start_date: system_start_date, end_date: system_end_date)
+    }
+    let(:group_start_date)    { Date.new(2021,1,1) }
+    let(:group_end_date)      { Date.new(2023,12,31) }
 
-    context 'when there are multiple meter tariffs' do
+    let!(:group_tariff) {
+      create(:energy_tariff, :with_flat_price, tariff_holder: school_group,
+            start_date: group_start_date, end_date: group_end_date)
+    }
+
+    let(:school_start_date)    { Date.new(2010, 1, 1) }
+    let(:school_end_date)      { Date.new(2050, 1, 1) }
+
+    let!(:school_tariff) {
+      create(:energy_tariff, :with_flat_price, tariff_holder: school,
+            start_date: school_start_date, end_date: school_end_date)
+    }
+
+    let(:meter_tariff_start_date)    { Date.new(2010, 1, 1) }
+    let(:meter_tariff_end_date)      { Date.new(2050, 1, 1) }
+
+    let!(:meter_tariff) {
+      create(:energy_tariff, :with_flat_price, tariff_holder: school,
+            start_date: meter_tariff_start_date, end_date: meter_tariff_end_date, meters: [meter])
+    }
+
+    it 'can find tariffs for that meter over school tariffs' do
+      expect(tariff_dates.keys.count).to eq 1
+      expect(tariff_dates[lookup_start_date..lookup_end_date]).to eq(meter_tariff)
+    end
+
+    context 'when there are multiple meter tariffs for same range' do
       it 'selects the most recent tariff'
     end
+
+    context 'when there are multiple meter tariffs within lookup range' do
+      it 'selects all the tariffs'
+    end
+
   end
 end
