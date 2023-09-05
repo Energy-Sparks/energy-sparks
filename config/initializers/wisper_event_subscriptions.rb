fb64c4004a659e5bb40ab0adcd23c0e73d0f88fe
Ian Townsend <ian.townsend@gmail.com>
2023-09-05 08:16:43 +0100

    Tidy up feature flag: data_enabled_onboarding

--- config/initializers/wisper_event_subscriptions.rb
+++ config/initializers/wisper_event_subscriptions.rb
@@ -1,11 +1,7 @@
 # Initializer to register event listeners via the Wisper (https://github.com/krisleech/wisper) gem
 
 #Send emails when school activated by making visible
-if EnergySparks::FeatureFlags.active?(:data_enabled_onboarding)
-  Wisper.subscribe(Onboarding::OnboardingDataEnabledListener.new)
-else
-  Wisper.subscribe(Onboarding::OnboardingListener.new)
-end
+Wisper.subscribe(Onboarding::OnboardingDataEnabledListener.new)
 
 #Invalidate cached calendars when they are edited
 Wisper.subscribe(CalendarEventListener.new)
