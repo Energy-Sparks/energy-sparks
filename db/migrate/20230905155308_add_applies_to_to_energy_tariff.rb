5d9dfa1f52c55b71f67d51fb921aa796fd693fbb
Ian Townsend <ian.townsend@gmail.com>
2023-09-06 09:41:02 +0100

    Add a test for the applies to validation

--- db/migrate/20230905155308_add_applies_to_to_energy_tariff.rb
+++ db/migrate/20230905155308_add_applies_to_to_energy_tariff.rb
@@ -1,5 +1,5 @@
 class AddAppliesToToEnergyTariff < ActiveRecord::Migration[6.0]
   def change
-    add_column :energy_tariffs, :applies_to, :integer
+    add_column :energy_tariffs, :applies_to, :integer, default: 0 # enum defaults to :both
   end
 end
