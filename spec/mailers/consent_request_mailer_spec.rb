7ea38bbd06174be29a4ce5a34176871287a1d7a3
Deb Bassett <deb@urbanwide.com>
2023-09-07 10:47:59 +0100

    Update spec for removal of feature flag

--- spec/mailers/consent_request_mailer_spec.rb
+++ spec/mailers/consent_request_mailer_spec.rb
@@ -2,38 +2,37 @@ require 'rails_helper'
 
 RSpec.describe ConsentRequestMailer do
 
-  let(:user) { create(:user, preferred_locale: :cy) }
+  let(:locale) { :cy }
+  let(:user) { create(:user, preferred_locale: locale) }
   let(:school) { create(:school, name: 'Test School') }
+  let(:email)  { ActionMailer::Base.deliveries.last }
 
   around do |example|
     ClimateControl.modify SEND_AUTOMATED_EMAILS: 'true' do
-      ClimateControl.modify FEATURE_FLAG_EMAILS_WITH_PREFERRED_LOCALE: enable_locale_emails do
-        example.run
-      end
+      example.run
     end
   end
 
   before :each do
     ConsentRequestMailer.with_user_locales(users: [user], school: school) { |mailer| mailer.request_consent.deliver_now }
-    @email = ActionMailer::Base.deliveries.last
   end
 
-  context 'when locale emails disabled' do
-    let(:enable_locale_emails) { 'false' }
+  context 'user locale is :en' do
+    let(:locale) { :en }
     describe '#request_consent' do
       it 'sends an email with en strings' do
-        expect(@email.subject).to eql ("We need permission to access your school's energy data")
-        expect(@email.body.to_s).to include("Please provide permission for Energy Sparks to access data for Test School")
+        expect(email.subject).to eql ("We need permission to access your school's energy data")
+        expect(email.body.to_s).to include("Please provide permission for Energy Sparks to access data for Test School")
       end
     end
   end
 
-  context 'when locale emails enabled' do
-    let(:enable_locale_emails) { 'true' }
+  context 'user locale is :cy' do
+    let(:locale) { :cy }
     describe '#request_consent' do
       it 'sends an email with cy strings' do
-        expect(@email.subject).to eql I18n.t('consent_request_mailer.request_consent.subject', locale: :cy)
-        expect(ActionController::Base.helpers.sanitize(@email.body.to_s)).to include(I18n.t('consent_request_mailer.request_consent.description', school_name: 'Test School', locale: :cy))
+        expect(email.subject).to eql I18n.t('consent_request_mailer.request_consent.subject', locale: :cy)
+        expect(ActionController::Base.helpers.sanitize(email.body.to_s)).to include(I18n.t('consent_request_mailer.request_consent.description', school_name: 'Test School', locale: :cy))
       end
     end
   end
