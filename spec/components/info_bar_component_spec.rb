cfe330c9e310ed5ccac0271d6a0c88188a59b3f7
Ian Townsend <ian.townsend@gmail.com>
2023-09-14 13:59:40 +0100

    Add a test

--- spec/components/info_bar_component_spec.rb
+++ spec/components/info_bar_component_spec.rb
@@ -2,14 +2,30 @@
 
 require "rails_helper"
 
+include ActionView::Helpers
+
 RSpec.describe InfoBarComponent, type: :component do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it "renders a three colum info bar with an icon, title text, and some optional buttons" do
+    expect(
+      render_inline(described_class.new(icon: '<i class="fas fa-school fa-3x"></i>'.html_safe, title: 'This is an info bar', buttons: { "Click me" => "http://www.example.com" })).to_html
+    ).to include(
+      <<~HTML.chomp
+        <div class="row">
+          <div class="col-md-1 d-flex justify-content-center align-content-center">
+            <div class="d-flex align-content-center flex-wrap">
+              <i class="fas fa-school fa-3x"></i>
+            </div>
+          </div>
+            <div class="col-md-8">
+              This is an info bar
+            </div>
+              <div class="col-md-3 align-right">
+                <a class="btn btn-light btn rounded-pill font-weight-bold" style="height: fit-content;" href="http://www.example.com">Click me</a>
+              </div>
+        </div>
+      HTML
+    )
+  end
 end
