3bb2271d5b66c39c87036cd9f139e7f4ec608b30
Ian Townsend <ian.townsend@gmail.com>
2023-09-15 15:51:24 +0100

    Extract mail body content

--- app/mailers/energy_tariffs_mailer.rb
+++ app/mailers/energy_tariffs_mailer.rb
@@ -1,15 +1,22 @@
 class EnergyTariffsMailer < ApplicationMailer
   def group_admin_review_group_tariffs_reminder
     @school_group = SchoolGroup.find(params[:school_group_id])
-    subject = "Reminder: please review the information we have about your school group's energy tariffs"
+    subject = I18n.t('energy_tariffs_mailer.group_admin_review_group_tariffs_reminder.subject')
 
-    make_bootstrap_mail(to: @school_group.users.where(role: 'group_admin').map(&:email), subject: subject)
+    @school_group.users.where(role: 'group_admin').map(&:email).each do |group_admin_email|
+      make_bootstrap_mail(
+        to: group_admin_email,
+        subject: subject
+      )
+    end
   end
 
   def school_admin_review_school_tariffs_reminder
     @school = School.find(params[:school_id])
-    subject = "Reminder: please review the information we have about your school's energy tariffs"
+    subject = I18n.t('energy_tariffs_mailer.school_admin_review_school_tariffs_reminder.subject')
 
-    make_bootstrap_mail(to: @school.school_admin.map(&:email), subject: subject)
+    @school.school_admin.map(&:email).each do |school_admin_email|
+      make_bootstrap_mail(to: school_admin_email, subject: subject)
+    end
   end
 end
