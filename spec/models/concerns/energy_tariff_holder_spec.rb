825d9e67bd4a7f8bfec903006cc4e63cd692c666
Ian Townsend <ian.townsend@gmail.com>
2023-09-20 13:52:00 +0100

    Use var names

--- spec/models/concerns/energy_tariff_holder_spec.rb
+++ spec/models/concerns/energy_tariff_holder_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe EnergyTariffHolder do
-  let!(:energy_tariff) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: 'electricity', name: 'Electricity tariff both') }
+  let!(:energy_tariff_electricity_both) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: 'electricity', name: 'Electricity tariff both') }
 
   context '.energy_tariff_meter_attributes' do
     context 'with SiteSettings' do
       let(:tariff_holder) { SiteSettings.current }
 
       it 'adds the association' do
-        expect(tariff_holder.energy_tariffs.first).to eq energy_tariff
+        expect(tariff_holder.energy_tariffs.first).to eq energy_tariff_electricity_both
       end
 
       it 'has no parent' do
@@ -29,7 +29,7 @@ describe EnergyTariffHolder do
       end
 
       it 'adds the association' do
-        expect(tariff_holder.energy_tariffs.first).to eq energy_tariff
+        expect(tariff_holder.energy_tariffs.first).to eq energy_tariff_electricity_both
       end
 
       it 'has a parent' do
@@ -59,36 +59,36 @@ describe EnergyTariffHolder do
       end
 
       context 'when filtering tariffs' do
-        let!(:energy_tariff_electricty_half_hourly) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: "electricity", applies_to: "half_hourly", name: 'Electricity Tariff half_hourly') }
-        let!(:energy_tariff_electricty_non_half_hourly) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: "electricity", applies_to: "non_half_hourly", name: 'Electricity Tariff non_half_hourly') }
+        let!(:energy_tariff_electricity_half_hourly) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: "electricity", applies_to: "half_hourly", name: 'Electricity Tariff half_hourly') }
+        let!(:energy_tariff_electricity_non_half_hourly) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: "electricity", applies_to: "non_half_hourly", name: 'Electricity Tariff non_half_hourly') }
         let!(:energy_tariff_gas_both) { create(:energy_tariff, :with_flat_price, tariff_holder: tariff_holder, meter_type: "gas", applies_to: "both", name: 'Gas Tariff both') }
 
         it 'defaults and returns both when no meter system is specified' do
-          expect(tariff_holder.energy_tariff_meter_attributes.map { |m| m.input_data['name'] }).to match_array(["Electricity tariff both", "Gas Tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas]).map { |m| m.input_data['name'] }).to match_array(["Electricity tariff both", "Gas Tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['electricity']).map { |m| m.input_data['name'] }).to match_array(["Electricity tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['gas']).map { |m| m.input_data['name'] }).to match_array(["Gas Tariff both"])
+          expect(tariff_holder.energy_tariff_meter_attributes.map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_both.name, energy_tariff_gas_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas]).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_both.name, energy_tariff_gas_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['electricity']).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['gas']).map { |m| m.input_data['name'] }).to match_array([energy_tariff_gas_both.name])
         end
 
         it 'returns both when filtering for both' do
-          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas], :both).map { |m| m.input_data['name'] }).to match_array(["Electricity tariff both", "Gas Tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['electricity'], :both).map { |m| m.input_data['name'] }).to match_array(["Electricity tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['gas'], :both).map { |m| m.input_data['name'] }).to match_array(["Gas Tariff both"])
+          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas], :both).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_both.name, energy_tariff_gas_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['electricity'], :both).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['gas'], :both).map { |m| m.input_data['name'] }).to match_array([energy_tariff_gas_both.name])
         end
 
         it 'returns both and half_hourly tariffs when filtering for half_hourly' do
-          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas], :half_hourly).map { |m| m.input_data['name'] }).to match_array(["Electricity Tariff half_hourly", "Electricity tariff both", "Gas Tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['electricity'], :half_hourly).map { |m| m.input_data['name'] }).to match_array(["Electricity Tariff half_hourly", "Electricity tariff both"])
+          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas], :half_hourly).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_half_hourly.name, energy_tariff_electricity_both.name, energy_tariff_gas_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['electricity'], :half_hourly).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_half_hourly.name, energy_tariff_electricity_both.name])
         end
 
         it 'returns both and non_half_hourly tariffs when filtering for non_half_hourly' do
-          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas], :non_half_hourly).map { |m| m.input_data['name'] }).to match_array(["Electricity Tariff non_half_hourly", "Electricity tariff both", "Gas Tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['electricity'], :non_half_hourly).map { |m| m.input_data['name'] }).to match_array(["Electricity Tariff non_half_hourly", "Electricity tariff both"])
+          expect(tariff_holder.energy_tariff_meter_attributes(%w[electricity gas], :non_half_hourly).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_non_half_hourly.name, energy_tariff_electricity_both.name, energy_tariff_gas_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['electricity'], :non_half_hourly).map { |m| m.input_data['name'] }).to match_array([energy_tariff_electricity_non_half_hourly.name, energy_tariff_electricity_both.name])
         end
 
         it 'ignores meter system when requesting gas tariffs' do
-          expect(tariff_holder.energy_tariff_meter_attributes(['gas'], :half_hourly).map { |m| m.input_data['name'] }).to match_array(["Gas Tariff both"])
-          expect(tariff_holder.energy_tariff_meter_attributes(['gas'], :non_half_hourly).map { |m| m.input_data['name'] }).to match_array(["Gas Tariff both"])
+          expect(tariff_holder.energy_tariff_meter_attributes(['gas'], :half_hourly).map { |m| m.input_data['name'] }).to match_array([energy_tariff_gas_both.name])
+          expect(tariff_holder.energy_tariff_meter_attributes(['gas'], :non_half_hourly).map { |m| m.input_data['name'] }).to match_array([energy_tariff_gas_both.name])
         end
       end
 
@@ -140,7 +140,7 @@ describe EnergyTariffHolder do
   context '.default_tariff_start_date' do
     let(:tariff_holder) { SiteSettings.current }
     it 'defaults to one day later' do
-      expect(tariff_holder.default_tariff_start_date(:electricity)).to eq energy_tariff.end_date + 1.day
+      expect(tariff_holder.default_tariff_start_date(:electricity)).to eq energy_tariff_electricity_both.end_date + 1.day
     end
 
     it 'defaults to today otherwise' do
