4a380895c27faaf1dc82c97377b02440b643a406
Ian Townsend <ian.townsend@gmail.com>
2023-09-20 09:43:49 +0100

    Add some locale tests

--- app/mailers/energy_tariffs_mailer.rb
+++ app/mailers/energy_tariffs_mailer.rb
@@ -1,26 +1,29 @@
 class EnergyTariffsMailer < LocaleMailer
   def group_admin_review_group_tariffs_reminder
     @school_group = SchoolGroup.find(params[:school_group_id])
-    subject = I18n.t('energy_tariffs_mailer.group_admin_review_group_tariffs_reminder.subject')
 
-    @school_group.users.where(role: 'group_admin').map(&:email).each do |group_admin_email|
+    @school_group.users.where(role: 'group_admin').each do |group_admin|
+      params[:locale] = group_admin.preferred_locale
       make_bootstrap_mail(
-        to: group_admin_email,
-        subject: subject
+        to: group_admin.email,
+        subject: I18n.t('energy_tariffs_mailer.group_admin_review_group_tariffs_reminder.subject', locale: params[:locale]),
+        locale: params[:locale]
       )
     end
   end
 
   def school_admin_review_school_tariffs_reminder
     @school = School.find(params[:school_id])
-    subject = I18n.t('energy_tariffs_mailer.school_admin_review_school_tariffs_reminder.subject')
 
     # School staff users should not get the email, only school admins.
     # If school admins are linked to multiple accounts they should get the email for each account.
-    @school.school_admin.map(&:email).each do |school_admin_email|
+    @school.school_admin.each do |school_admin|
+      params[:locale] = school_admin.preferred_locale
+
       make_bootstrap_mail(
-        to: school_admin_email,
-        subject: subject
+        to: school_admin.email,
+        subject: I18n.t('energy_tariffs_mailer.school_admin_review_school_tariffs_reminder.subject', locale: params[:locale]),
+        locale: params[:locale]
       )
     end
   end
