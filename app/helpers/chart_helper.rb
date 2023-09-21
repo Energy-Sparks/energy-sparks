0d20fa8be05dfeb56fe760e2bfde5a5dd37706af
Leigh Dodds <leigh@ldodds.com>
2023-09-21 10:53:36 +0100

    Disable use of preferences on the pupil analysis charts only (#3069)

--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -62,10 +62,10 @@ module ChartHelper
     Charts::YAxisSelectionService.new(school, chart_name).select_y_axis || default
   end
 
-  def create_chart_config(school, chart_name, mpan_mprn = nil, export_title: '', export_subtitle: '')
+  def create_chart_config(school, chart_name, mpan_mprn = nil, apply_preferred_units: true, export_title: '', export_subtitle: '')
     config = {}
     config[:mpan_mprn] = mpan_mprn if mpan_mprn.present?
-    y_axis = select_y_axis(school, chart_name)
+    y_axis = apply_preferred_units ? select_y_axis(school, chart_name) : nil
     config[:y_axis_units] = y_axis if y_axis.present?
     config[:export_title] = export_title
     config[:export_subtitle] = export_subtitle
--- spec/helpers/chart_helper_spec.rb
+++ spec/helpers/chart_helper_spec.rb
@@ -15,4 +15,31 @@ describe ChartHelper do
       expect(result['456']).to eq('Electricity baseload from 01 Jun 2018 to 01 Jun 2021 for 456')
     end
   end
+
+  describe '.create_chart_config' do
+    let(:chart_name)  { :pupil_dashboard_group_by_week_electricity_£ }
+    let(:school)      { create(:school) }
+
+    it 'returns hash' do
+      expect( helper.create_chart_config(school, chart_name) ).to eq({export_title: '', export_subtitle: ''})
+    end
+
+    it 'adds mpan' do
+      expect( helper.create_chart_config(school, chart_name, '1234') ).to eq({mpan_mprn: '1234', export_title: '', export_subtitle: ''})
+    end
+
+    it 'adds export details' do
+      expect( helper.create_chart_config(school, chart_name, export_title: 'title', export_subtitle: 'subtitle') ).to eq({export_title: 'title', export_subtitle: 'subtitle'})
+    end
+
+    context 'when adding y-axis' do
+      let(:school)      { create(:school, chart_preference: :usage) }
+      it 'uses school preference when set' do
+        expect( helper.create_chart_config(school, chart_name) ).to eq({y_axis_units: :kwh, export_title: '', export_subtitle: ''})
+      end
+      it 'the preference can be overridden' do
+        expect( helper.create_chart_config(school, chart_name, apply_preferred_units: false) ).to eq({export_title: '', export_subtitle: ''})
+      end
+    end
+  end
 end
