6e4b7a4f4cd3937e1dfbb19d05ae2dde4a0afee4
Leigh Dodds <leigh@ldodds.com>
2023-09-26 14:09:52 +0100

    Fix up broken tests

--- spec/helpers/schools_helper_spec.rb
+++ spec/helpers/schools_helper_spec.rb
@@ -17,13 +17,7 @@ describe SchoolsHelper do
   let(:alert_content) { OpenStruct.new(alert: alert, find_out_more: find_out_more) }
 
   context '#find_out_more_path_from_alert_content' do
-    around do |example|
-      ClimateControl.modify FEATURE_FLAG_REPLACE_FIND_OUT_MORES: feature_flag do
-        example.run
-      end
-    end
-    context 'with feature on and advice path' do
-      let(:feature_flag) { 'true' }
+    context 'with advice path' do
       let(:advice_page)  { create(:advice_page, key: :baseload) }
       let(:alert_type)   { create(:alert_type, advice_page: advice_page) }
 
@@ -49,44 +43,28 @@ describe SchoolsHelper do
     end
   end
   context '#dashboard_alert_buttons' do
-    around do |example|
-      ClimateControl.modify FEATURE_FLAG_REPLACE_FIND_OUT_MORES: feature_flag do
-        example.run
+    context 'and no advice page' do
+      it 'returns empty hash' do
+        buttons = helper.dashboard_alert_buttons(school, alert_content)
+        expect(buttons).to eq({})
       end
     end
+    context 'and advice page' do
+      let(:advice_page)  { create(:advice_page, key: :baseload) }
+      let(:alert_type)   { create(:alert_type, advice_page: advice_page) }
 
-    context 'with feature off' do
       it 'returns the expected path' do
         buttons = helper.dashboard_alert_buttons(school, alert_content)
-        expect(buttons.values.first).to eq school_find_out_more_path(school, alert_content.find_out_more)
+        expect(buttons.values.first).to eq insights_school_advice_baseload_path(school)
       end
-    end
 
-    context 'with feature on' do
-      let(:feature_flag) { 'true' }
-      context 'and no advice page' do
-        it 'returns empty hash' do
-          buttons = helper.dashboard_alert_buttons(school, alert_content)
-          expect(buttons).to eq({})
-        end
-      end
-      context 'and advice page' do
-        let(:advice_page)  { create(:advice_page, key: :baseload) }
-        let(:alert_type)   { create(:alert_type, advice_page: advice_page) }
+      context 'and link_to_content' do
+        let(:anchor) { 'some-section' }
+        let(:alert_type)   { create(:alert_type, advice_page: advice_page, link_to: :analysis_page, link_to_section: anchor) }
 
         it 'returns the expected path' do
           buttons = helper.dashboard_alert_buttons(school, alert_content)
-          expect(buttons.values.first).to eq insights_school_advice_baseload_path(school)
-        end
-
-        context 'and link_to_content' do
-          let(:anchor) { 'some-section' }
-          let(:alert_type)   { create(:alert_type, advice_page: advice_page, link_to: :analysis_page, link_to_section: anchor) }
-
-          it 'returns the expected path' do
-            buttons = helper.dashboard_alert_buttons(school, alert_content)
-            expect(buttons.values.first).to eq analysis_school_advice_baseload_path(school, anchor: anchor)
-          end
+          expect(buttons.values.first).to eq analysis_school_advice_baseload_path(school, anchor: anchor)
         end
       end
     end
