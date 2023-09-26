ae4d1e4824ef413b21d7230d412d0fbe008b3b98
Ian Townsend <ian.townsend@gmail.com>
2023-09-26 08:24:39 +0100

    A little bit more refactoring

--- app/services/charts/annotate.rb
+++ app/services/charts/annotate.rb
@@ -7,13 +7,22 @@ module Charts
     def annotate_weekly(x_axis_categories)
       return if x_axis_categories.empty?
 
-      date_categories = x_axis_categories.map { |x_axis_category| date_for(x_axis_category) }
-      weekly_relevant_observations_for(x_axis_categories, date_categories)
+      weekly_relevant_observations_for(x_axis_categories, date_categories_for(x_axis_categories))
     end
 
     def annotate_daily(x_axis_start, x_axis_end)
       return if x_axis_start.blank? || x_axis_end.blank?
 
+      daily_relevant_observations_for(x_axis_start, x_axis_end)
+    end
+
+    private
+
+    def date_categories_for(x_axis_categories)
+      x_axis_categories.map { |x_axis_category| date_for(x_axis_category) }
+    end
+
+    def daily_relevant_observations_for(x_axis_start, x_axis_end)
       relevant_observations_for(Date.parse(x_axis_start), Date.parse(x_axis_end)).map do |observation|
         {
           id: observation.id,
@@ -26,25 +35,30 @@ module Charts
       end
     end
 
-    private
-
     def relevant_observations_for(start_date, end_date)
       @observations.where('at BETWEEN ? AND ?', start_date, end_date)
     end
 
     def weekly_relevant_observations_for(x_axis_categories, date_categories)
       relevant_observations_for(date_categories.min, date_categories.max + 6.days).map do |observation|
-        relevant_start_date = date_categories.find {|date| (date..(date + 6.days)).cover?(observation.at.to_date)}
-
         {
           id: observation.id,
           event: event_for(observation),
           date: observation.at.to_date,
-          x_axis_category: x_axis_categories[date_categories.index(relevant_start_date)],
+          x_axis_category: weekly_x_axis_category_for(x_axis_categories, date_categories, observation),
           icon: icon_for(observation),
           observation_type: observation.observation_type
         }
       end
+    end
+
+    def weekly_x_axis_category_for(x_axis_categories, date_categories, observation)
+      relevant_start_date = weekly_relevant_start_date_for(date_categories, observation)
+      x_axis_categories[date_categories.index(relevant_start_date)]
+    end
+
+    def weekly_relevant_start_date_for(date_categories, observation)
+      date_categories.find { |date| (date..(date + 6.days)).cover?(observation.at.to_date) }
     end
 
     def icon_for(observation)
