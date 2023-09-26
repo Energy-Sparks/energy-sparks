ca74874cb758c5a38696a3eec03b1861290c735d
Ian Townsend <ian.townsend@gmail.com>
2023-09-26 16:22:46 +0100

    Clean up

--- spec/mailers/previews/energy_tariffs_mailer_preview.rb
+++ spec/mailers/previews/energy_tariffs_mailer_preview.rb
@@ -1,6 +1,5 @@
 class EnergyTariffsMailerPreview < ActionMailer::Preview
   def group_admin_review_group_tariffs_reminder
-    # AlertMailer.with(email_address: 'test@blah.com', school: AlertSubscriptionEvent.last.alert.school, events: [AlertSubscriptionEvent.last], target_prompt: nil, locale: locale).alert_email
     EnergyTariffsMailer.with(school_group_id: SchoolGroup.first.id).group_admin_review_group_tariffs_reminder
   end
 
