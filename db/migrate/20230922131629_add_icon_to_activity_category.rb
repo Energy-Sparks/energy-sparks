0d13698b036e3a44bca1e166747de07af2b3fbf9
Ian Townsend <ian.townsend@gmail.com>
2023-09-26 13:24:59 +0100

    Change icon

--- db/migrate/20230922131629_add_icon_to_activity_category.rb
+++ db/migrate/20230922131629_add_icon_to_activity_category.rb
@@ -1,5 +1,5 @@
 class AddIconToActivityCategory < ActiveRecord::Migration[6.0]
   def change
-    add_column :activity_categories, :icon, :string, default: 'running'
+    add_column :activity_categories, :icon, :string, default: 'volleyball-ball'
   end
 end
