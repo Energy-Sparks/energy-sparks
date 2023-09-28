9e5413da7868f47ba74d8bea74d79a047d7764d8
Ian Townsend <ian.townsend@gmail.com>
2023-09-28 16:18:57 +0100

    Update connection handling

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module EnergySparks
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.active_record.legacy_connection_handling = false
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
