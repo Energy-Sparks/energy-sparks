1a08a4a9dc1fddcc762832480014252e4dde8d5e
Leigh Dodds <leigh@ldodds.com>
2023-10-10 17:16:10 +0100

    Attempt to regenerate following validation failure

--- spec/services/schools/school_regeneration_service_spec.rb
+++ spec/services/schools/school_regeneration_service_spec.rb
@@ -17,14 +17,31 @@ describe Schools::SchoolRegenerationService, type: :service do
   end
 
   context '#perform' do
-    before do
-      expect_any_instance_of(Amr::ValidateAndPersistReadingsService).to receive(:perform).and_return(meter_collection)
-      expect_any_instance_of(AggregateDataService).to receive(:aggregate_heat_and_electricity_meters)
-      expect_any_instance_of(AggregateSchoolService).to receive(:cache)
-      expect_any_instance_of(Schools::SchoolMetricsGeneratorService).to receive(:perform)
+
+    context 'when there are no errors' do
+      it 'calls validate, update cache and regenerate metrics ' do
+        expect_any_instance_of(Amr::ValidateAndPersistReadingsService).to receive(:perform).and_return(meter_collection)
+        expect_any_instance_of(Amr::AnalyticsMeterCollectionFactory).to_not receive(:validated)
+        expect_any_instance_of(AggregateDataService).to receive(:aggregate_heat_and_electricity_meters)
+        expect_any_instance_of(AggregateSchoolService).to receive(:cache)
+        expect_any_instance_of(Schools::SchoolMetricsGeneratorService).to receive(:perform)
+
+        service.perform
+      end
     end
-    it 'calls validate, update cache and regenerate metrics ' do
-      service.perform
+
+    context 'when validation fails' do
+      before do
+        allow_any_instance_of(Amr::ValidateAndPersistReadingsService).to receive(:perform).and_raise
+      end
+
+      it 'continues with the other steps' do
+        expect_any_instance_of(Amr::AnalyticsMeterCollectionFactory).to receive(:validated).and_return(meter_collection)
+        expect_any_instance_of(AggregateDataService).to receive(:aggregate_heat_and_electricity_meters)
+        expect_any_instance_of(AggregateSchoolService).to receive(:cache)
+        expect_any_instance_of(Schools::SchoolMetricsGeneratorService).to receive(:perform)
+        service.perform
+      end
     end
   end
 end
