952b32cecf36318aa67c93c9877076426fd66db4
Deb Bassett <deb@urbanwide.com>
2023-10-12 14:37:34 +0100

    Add route and template

--- app/controllers/schools/recommendations_controller.rb
+++ app/controllers/schools/recommendations_controller.rb
@@ -1,8 +1,17 @@
 module Schools
   class RecommendationsController < ApplicationController
     load_resource :school
+    before_action :set_breadcrumbs
 
     def index
+    end
+
+    private
+
+    def set_breadcrumbs
+      @breadcrumbs = [
+        { name: I18n.t('schools.recommendations.title') },
+      ]
     end
   end
 end
