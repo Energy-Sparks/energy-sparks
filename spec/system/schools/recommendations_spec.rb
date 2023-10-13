4e6ae254e5c209155c295c02c2d0a04673145f4a
Deb Bassett <deb@urbanwide.com>
2023-10-13 15:19:48 +0100

    Get page scope

--- spec/system/schools/recommendations_spec.rb
+++ spec/system/schools/recommendations_spec.rb
@@ -3,11 +3,6 @@ require 'rails_helper'
 describe 'Recommendations Page', type: :system  do
 
   let!(:school) { create :school, name: "School Name" }
-  let(:user) { create(:pupil, school: school) }
-
-  before(:each) do
-    sign_in(user)
-  end
 
   before do
     # later we should simulate navigating here
@@ -15,7 +10,15 @@ describe 'Recommendations Page', type: :system  do
   end
 
   it_behaves_like "a page with breadcrumbs", ['Schools', 'School Name','Recommended Pupil Activities & Adult Actions']
+
+  it "has the title" do
+    within("h1") do
+      expect(page).to have_content("Recommended Pupil Activities & Adult Actions")
+    end
+  end
+
   it "has the intro" do
     expect(page).to have_content("Find your next energy saving activity to score points for your school, reduce your energy usage and learn more about energy and climate change")
   end
+
 end
