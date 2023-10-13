310ab5f96c4ea57043c669cd4a9588780725a457
Deb Bassett <deb@urbanwide.com>
2023-10-13 15:33:55 +0100

    Rebase branch and rubocop autocorrect

--- spec/support/shared_examples/standard_page_elements.rb
+++ spec/support/shared_examples/standard_page_elements.rb
@@ -1,5 +1,4 @@
 RSpec.shared_examples "a page with breadcrumbs" do |breadcrumbs|
-
   it "displays breadcrumbs" do
     expect(find('ol.main-breadcrumbs').all('li').collect(&:text)).to eq(breadcrumbs)
   end
--- spec/system/schools/recommendations_spec.rb
+++ spec/system/schools/recommendations_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'Recommendations Page', type: :system  do
-
   let!(:school) { create :school, name: "School Name" }
 
   before do
@@ -9,7 +8,7 @@ describe 'Recommendations Page', type: :system  do
     visit school_recommendations_url(school)
   end
 
-  it_behaves_like "a page with breadcrumbs", ['Schools', 'School Name','Recommended Pupil Activities & Adult Actions']
+  it_behaves_like "a page with breadcrumbs", ['Schools', 'School Name', 'Recommended Pupil Activities & Adult Actions']
 
   it "has the title" do
     within("h1") do
@@ -20,5 +19,4 @@ describe 'Recommendations Page', type: :system  do
   it "has the intro" do
     expect(page).to have_content("Find your next energy saving activity to score points for your school, reduce your energy usage and learn more about energy and climate change")
   end
-
 end
