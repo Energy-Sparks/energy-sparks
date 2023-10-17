53dd22d1669fe8b6f6c0a7dfd04270bf25cb14ec
Deb Bassett <deb@urbanwide.com>
2023-10-17 09:15:29 +0100

    Make recommendations component support being passed non-active record items

--- app/components/recommendations_component.rb
+++ app/components/recommendations_component.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 class RecommendationsComponent < ViewComponent::Base
-  attr_reader :title, :recommendations, :limit, :max_lg
+  attr_reader :title, :limit, :max_lg
+
+  renders_many :items, "ItemComponent"
 
   def initialize(title: nil, recommendations: [], classes: '', limit: 4, max_lg: 3)
     @title = title
-    @recommendations = recommendations.first(limit)
     @classes = classes
     @limit = limit
     @max_lg = max_lg
+    @recommendations = recommendations
+  end
+
+  # `#helpers` can't be used during initialization as it depends on the view context that only exists once a ViewComponent is passed to the Rails render pipeline.
+  def before_render
+    @recommendations.each do |recommendation|
+      with_item(name: recommendation.name, href: helpers.url_for(recommendation), image: recommendation.t_attached_or_default(:image))
+    end
   end
 
   def classes
@@ -19,16 +28,30 @@ class RecommendationsComponent < ViewComponent::Base
     " d-none d-xl-block" if index >= max_lg # limit to max_lg for screens less than XL
   end
 
-  def default_image
-    'placeholder300x200.png'
-  end
-
   # this is for when we add activity key stages etc
   def footer
     false
   end
 
   def render?
-    recommendations.any?
+    items.any?
+  end
+
+  class ItemComponent < ViewComponent::Base
+    attr_accessor :name, :href
+
+    def initialize(name: nil, href: nil, image: nil)
+      @name = name
+      @href = href
+      @image = image
+    end
+
+    def image
+      @image || self.class.default_image
+    end
+
+    def self.default_image
+      'placeholder300x200.png'
+    end
   end
 end
