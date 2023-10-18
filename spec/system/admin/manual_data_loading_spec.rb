906e0dd901933fcd93b2a3cddde33cf721e52428
Leigh Dodds <leigh@ldodds.com>
2023-10-18 14:46:52 +0100

    Check for paging

--- spec/system/admin/manual_data_loading_spec.rb
+++ spec/system/admin/manual_data_loading_spec.rb
@@ -1,32 +1,41 @@
 require 'rails_helper'
 
 describe "manual data load", type: :system do
-  let!(:admin)              { create(:admin) }
-
-  let!(:run)                { create(:manual_data_load_run, status: :done) }
+  let!(:admin)               { create(:admin) }
+  let!(:runs)                { create_list(:manual_data_load_run, 22, status: :done) }
 
   before do
     sign_in(admin)
     visit root_path
+    click_on("Reports")
   end
 
-  describe 'report' do
-    before do
-      click_on("Reports")
-    end
+  it 'links to report' do
+    click_on("Recent manual imports")
+    expect(page).to have_content("Recent manual data loads")
+  end
 
-    it 'links to report' do
+  context 'view the manual data load report' do
+    let(:run)      { ManualDataLoadRun.first }
+    let(:last_run) { ManualDataLoadRun.last }
+
+    before do
       click_on("Recent manual imports")
-      expect(page).to have_content("Recent manual data loads")
     end
 
     it 'lists the data loads' do
-      click_on("Recent manual imports")
       expect(page).to have_content(run.amr_uploaded_reading.file_name)
+      expect(page).not_to have_content(last_run.amr_uploaded_reading.file_name)
+    end
+
+    it 'has paging' do
+      expect(page).to have_link('Next')
+      click_on('Next')
+      expect(page).not_to have_content(run.amr_uploaded_reading.file_name)
+      expect(page).to have_content(last_run.amr_uploaded_reading.file_name)
     end
 
     it 'displays the status' do
-      click_on("Recent manual imports")
       click_on("View")
       expect(page).to have_content(run.amr_uploaded_reading.amr_data_feed_config.description)
       expect(page).to have_content(run.amr_uploaded_reading.file_name)
