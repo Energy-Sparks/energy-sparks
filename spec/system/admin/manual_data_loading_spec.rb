6b35a9c851ea2ac3c9b6401bea2da8c3cfccee0a
Leigh Dodds <leigh@ldodds.com>
2023-10-18 15:41:23 +0100

    Rework specs to fix navigation errors and ordering issues

--- spec/system/admin/manual_data_loading_spec.rb
+++ spec/system/admin/manual_data_loading_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe "manual data load", type: :system do
   let!(:admin)               { create(:admin) }
-  let!(:runs)                { create_list(:manual_data_load_run, 22, status: :done) }
 
   before do
     sign_in(admin)
@@ -16,29 +15,33 @@ describe "manual data load", type: :system do
   end
 
   context 'view the manual data load report' do
-    let(:run)      { ManualDataLoadRun.first }
-    let(:last_run) { ManualDataLoadRun.last }
+    let!(:runs)           { create_list(:manual_data_load_run, 22, status: :done) }
+
+    let(:oldest_run)      { runs.first }
+    let(:newest_run)      { runs.last }
 
     before do
       click_on("Recent manual imports")
     end
 
     it 'lists the data loads' do
-      expect(page).to have_content(run.amr_uploaded_reading.file_name)
-      expect(page).not_to have_content(last_run.amr_uploaded_reading.file_name)
+      expect(page).to have_content(newest_run.amr_uploaded_reading.file_name)
+      expect(page).not_to have_content(oldest_run.amr_uploaded_reading.file_name)
     end
 
     it 'has paging' do
-      expect(page).to have_link('Next')
-      click_on('Next')
-      expect(page).not_to have_content(run.amr_uploaded_reading.file_name)
-      expect(page).to have_content(last_run.amr_uploaded_reading.file_name)
+      within '#paging-top' do
+        expect(page).to have_link('Next')
+        click_on('Next')
+      end
+      expect(page).not_to have_content(newest_run.amr_uploaded_reading.file_name)
+      expect(page).to have_content(oldest_run.amr_uploaded_reading.file_name)
     end
 
     it 'displays the status' do
-      click_on("View")
-      expect(page).to have_content(run.amr_uploaded_reading.amr_data_feed_config.description)
-      expect(page).to have_content(run.amr_uploaded_reading.file_name)
+      first(:link, text: 'View', exact_text: true).click
+      expect(page).to have_content(newest_run.amr_uploaded_reading.amr_data_feed_config.description)
+      expect(page).to have_content(newest_run.amr_uploaded_reading.file_name)
       expect(page).to have_content("done")
     end
   end
