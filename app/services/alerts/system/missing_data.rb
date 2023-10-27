8366b815c51e5026150732ddf8d97f5441019f06
Ian Townsend <ian.townsend@gmail.com>
2023-10-27 11:39:12 +0100

    Update to use aggregated meters

--- app/services/alerts/system/missing_data.rb
+++ app/services/alerts/system/missing_data.rb
@@ -11,29 +11,29 @@ module Alerts
       end
 
       def report
-        return report_empty_meters if meters.empty?
-        return report_late_running_meters_empty if late_running_meters.empty?
+        return empty_meters_report unless aggregated_meters
+        return late_running_meters_empty_report if late_running_meters?
 
-        report_meters
+        meters_report
       end
 
       private
 
-      # def aggregate_school
-      #   @aggregate_school ||= AggregateSchoolService.new(@school).aggregate_school
-      # end
+      def aggregate_school
+        @aggregate_school ||= AggregateSchoolService.new(@school).aggregate_school
+      end
 
-      def report_meters
+      def meters_report
         Adapters::Report.new(
           valid: true,
-          rating: [0.0, (meters.map(&:last_validated_reading).min - (cutoff - 10.days)).to_f].max,
+          rating: [0.0, (aggregated_meters.amr_data.end_date - (cutoff - 10.days)).to_f].max,
           enough_data: :enough,
           relevance: :relevant,
           template_data: {
-            mpan_mprns: mpan_mprns(meters)
+            mpan_mprns: mpan_mprns
           },
           template_data_cy: {
-            mpan_mprns: mpan_mprns(meters, :cy)
+            mpan_mprns: mpan_mprns(:cy)
           },
           priority_data: {
             time_of_year_relevance: 5.0
@@ -41,7 +41,7 @@ module Alerts
         )
       end
 
-      def report_late_running_meters_empty
+      def late_running_meters_empty_report
         Adapters::Report.new(
           valid: true,
           rating: 10.0,
@@ -53,7 +53,7 @@ module Alerts
         )
       end
 
-      def report_empty_meters
+      def empty_meters_report
         Adapters::Report.new(
           valid: true,
           rating: nil,
@@ -62,26 +62,25 @@ module Alerts
         )
       end
 
-      def meters
-        @meters ||= @school.meters_with_validated_readings(@meter_type)
-        # @meters ||= case @meter_type
-        #             when :electricity then aggregate_school.aggregated_electricity_meters
-        #             when :gas then aggregate_school.aggregated_heat_meters
-        #             end
+      def aggregated_meters
+        @aggregated_meters ||= case @meter_type
+                               when :electricity then aggregate_school.aggregated_electricity_meters
+                               when :gas then aggregate_school.aggregated_heat_meters
+                               end
       end
 
-      def late_running_meters
-        @late_running_meters ||= meters.select { |meter| meter.last_validated_reading < cutoff }
+      def late_running_meters?
+        aggregated_meters.amr_data.end_date < cutoff
       end
 
       def cutoff
         @cutoff ||= @today - MISSING_CUTOFF_DAYS.days
       end
 
-      def mpan_mprns(meters, locale = :en)
-        return meters.map(&:mpan_mprn).to_sentence if locale == :en
+      def mpan_mprns(locale = :en)
+        return aggregated_meters.meter_list.to_sentence if locale == :en
 
-        I18n.with_locale(locale) { meters.map(&:mpan_mprn).to_sentence }
+        I18n.with_locale(locale) { aggregated_meters.meter_list.to_sentence }
       end
     end
   end
