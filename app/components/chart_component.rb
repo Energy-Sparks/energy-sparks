5c92f1783c5ba6e59417a044d2fc2431d5d4e54f
Ian Townsend <iant@users.noreply.github.com>
2023-11-01 13:32:33 +0000

    Use chart component for all school charts (#3123)

--- app/components/chart_component.rb
+++ app/components/chart_component.rb
@@ -6,11 +6,11 @@ class ChartComponent < ViewComponent::Base
   renders_one :header
   renders_one :footer
 
-  attr_reader :school, :chart_type, :analysis_controls, :no_zoom, :axis_controls, :html_class, :fuel_type
+  attr_reader :school, :chart_type, :analysis_controls, :no_zoom, :axis_controls, :html_class, :fuel_type, :autoload_chart
 
   include ChartHelper
 
-  def initialize(chart_type:, school:, chart_config: nil, analysis_controls: true, no_zoom: true, axis_controls: true, html_class: 'analysis-chart', fuel_type: nil)
+  def initialize(chart_type:, school:, chart_config: nil, analysis_controls: true, no_zoom: true, axis_controls: true, html_class: 'analysis-chart', fuel_type: nil, autoload_chart: true)
     @chart_type = chart_type
     @school = school
     @chart_config = chart_config
@@ -19,6 +19,7 @@ class ChartComponent < ViewComponent::Base
     @axis_controls = axis_controls
     @html_class = html_class
     @fuel_type = fuel_type
+    @autoload_chart = autoload_chart
   end
 
   def chart_config
@@ -34,5 +35,25 @@ class ChartComponent < ViewComponent::Base
 
   def valid_config?
     !chart_config.nil?
+  end
+
+  def chart_config_json
+    @chart_config_json ||= build_chart_config_to_json
+  end
+
+  private
+
+  def build_chart_config_to_json
+    chart_config.merge(
+      no_zoom: no_zoom,
+      type: chart_type,
+      annotations: fuel_type ? school_annotations_path(school, fuel_type: fuel_type) : [],
+      jsonUrl: school_chart_path(school, format: :json),
+      transformations: []
+    ).to_json
+  end
+
+  def chart_id
+    chart_config[:mpan_mprn].present? ? "chart_#{chart_type}_#{@chart_config[:mpan_mprn]}" : "chart_#{chart_type}"
   end
 end
