0adfecf24a4cc4465f6f7f2c347feba0631595fb
Leigh Dodds <leigh@ldodds.com>
2023-11-02 13:53:37 +0000

    Stub csv download

--- app/controllers/admin/reports/engaged_schools_controller.rb
+++ app/controllers/admin/reports/engaged_schools_controller.rb
@@ -2,7 +2,32 @@ module Admin
   module Reports
     class EngagedSchoolsController < AdminController
       def index
-        @engaged_schools = School.engaged.joins(:school_group).order('school_groups.name asc, name asc')
+        respond_to do |format|
+          format.html do
+            @engaged_schools = ::Schools::EngagedSchoolService.list_engaged_schools
+            @visible_schools = School.visible.count
+            @percentage = percentage_engaged
+          end
+          format.csv do
+            @engaged_schools = ::Schools::EngagedSchoolService.list_engaged_schools
+            send_data csv_report(@engaged_schools), filename: "engaged-schools-report-#{Time.zone.now.iso8601}".parameterize + '.csv'
+          end
+        end
+      end
+
+      private
+
+      def percentage_engaged
+        sprintf('%.2f', @engaged_schools.size / @visible_schools.to_f * 100)
+      end
+
+      def csv_report(engaged_schools)
+        CSV.generate(headers: true) do |csv|
+          csv << []
+          engaged_schools.each do |service|
+            csv << [service.school.name]
+          end
+        end
       end
     end
   end
