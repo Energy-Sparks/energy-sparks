d6fd5ba46d1b54bcf96bc4f601cf0efdb31408f4
Leigh Dodds <leigh@ldodds.com>
2023-11-06 16:27:06 +0000

    Refactor controller

--- app/controllers/admin/reports/engaged_schools_controller.rb
+++ app/controllers/admin/reports/engaged_schools_controller.rb
@@ -2,14 +2,14 @@ module Admin
   module Reports
     class EngagedSchoolsController < AdminController
       def index
+        @engaged_schools = ::Schools::EngagedSchoolService.list_engaged_schools
+
         respond_to do |format|
           format.html do
-            @engaged_schools = ::Schools::EngagedSchoolService.list_engaged_schools
             @visible_schools = School.visible.count
             @percentage = percentage_engaged
           end
           format.csv do
-            @engaged_schools = ::Schools::EngagedSchoolService.list_engaged_schools
             send_data csv_report(@engaged_schools), filename: "engaged-schools-report-#{Time.zone.now.iso8601}".parameterize + '.csv'
           end
         end
