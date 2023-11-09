8b270fc4d17e5cc3beb17ec07d21b2b5e10fd30c
Leigh Dodds <leigh@ldodds.com>
2023-11-09 16:03:33 +0000

    Improve solar edge page with some debug links

--- spec/system/solar_edge_installation_spec.rb
+++ spec/system/solar_edge_installation_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe "Solar edge installation management", :solar_edge_installations, 
     end
 
     context 'with existing installation' do
-      let!(:api_feed) { create(:solar_edge_installation, school: school) }
+      let!(:installation) { create(:solar_edge_installation, school: school) }
 
       before do
         click_on 'Manage Solar API feeds'
@@ -56,7 +56,7 @@ RSpec.describe "Solar edge installation management", :solar_edge_installations, 
 
       it 'displays the feed config' do
         expect(page).not_to have_content("This school has no Solar Edge API feeds")
-        expect(page).to have_content(api_feed.site_id)
+        expect(page).to have_content(installation.site_id)
       end
 
       it 'allows editing' do
@@ -68,7 +68,7 @@ RSpec.describe "Solar edge installation management", :solar_edge_installations, 
 
         expect(page).to have_content("Solar Edge API feed was updated")
 
-        expect(SolarEdgeInstallation.first.mpan).to eql api_feed.mpan
+        expect(SolarEdgeInstallation.first.mpan).to eql installation.mpan
         expect(SolarEdgeInstallation.first.site_id).to eql site_id
         expect(SolarEdgeInstallation.first.api_key).to eql api_key
       end
@@ -76,10 +76,17 @@ RSpec.describe "Solar edge installation management", :solar_edge_installations, 
       it 'allows deletion' do
         expect { click_on 'Delete'}.to change { SolarEdgeInstallation.count }.by(-1)
       end
+
+      it 'allows viewing' do
+        click_on(installation.mpan)
+        expect(page).to have_content("site_details")
+        expect(page).to have_content("dates")
+        expect(page).to have_link("Data Period")
+      end
     end
 
     context 'with an installation with meters' do
-      let!(:api_feed) { create(:solar_edge_installation_with_meters_and_validated_readings, school: school) }
+      let!(:installation) { create(:solar_edge_installation_with_meters_and_validated_readings, school: school) }
 
       before do
         click_on 'Manage Solar API feeds'
