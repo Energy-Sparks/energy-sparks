0190c24a230c094e4eb8ce8e8caf6ad275e5ac85
Leigh Dodds <leigh@ldodds.com>
2023-11-10 10:51:55 +0000

    Remove catch all exception handler, scope ajax handler to buttons

--- app/assets/javascripts/loading.js
+++ app/assets/javascripts/loading.js
@@ -1,26 +1,21 @@
 $(function() {
 
   // Change the link's icon while the request is performing
-  $(document).on('click', 'a[data-remote]', function(event, b, c) {
+  $(document).on('click', 'a.check-button[data-remote]', function(event, b, c) {
     var icon = $(this).find('i');
     icon.data('old-class', icon.attr('class'));
     icon.attr('class', 'fas fa-spinner fa-spin');
   });
 
-  // Change the link's icon back after it's finished.
-  $(document).on('ajax:complete', function(e) {
-    var icon = $(e.target).find('i');
-    if (icon.data('old-class')) {
-      icon.attr('class', icon.data('old-class'));
-      icon.data('old-class', null);
-    }
-  })
-
-  // Don't fail silently
-  $(document).ajaxError(function( event, jqxhr, settings, exception ) {
-    if (jqxhr.status) {
-      alert("Something went wrong (" + jqxhr.status + ')');
-    }
-  });
+  if ($("a.check-button[data-remote]").length) {
+    // Change the link's icon back after it's finished.
+    $(document).on('ajax:complete', function(e) {
+      var icon = $(e.target).find('i');
+      if (icon.data('old-class')) {
+        icon.attr('class', icon.data('old-class'));
+        icon.data('old-class', null);
+      }
+    })
+  }
 
 })
