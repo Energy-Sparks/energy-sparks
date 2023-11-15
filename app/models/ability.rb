fc6b57dd1deb585f9ba371e955c98c17b379a09a
Leigh Dodds <leigh@ldodds.com>
2023-11-15 15:40:39 +0000

    Allow group admins to edit tariffs for schools in their group (#3220)

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -54,6 +54,8 @@ class Ability
         can :manage, SchoolGroupCluster, school_group_id: user.school_group_id
         can :manage, EnergyTariff, tariff_holder: user.school_group
 
+        can :manage, EnergyTariff, related_school_scope
+
         can [:show, :update], Calendar do |calendar|
           user.school_group.calendars.include?(calendar)
         end
