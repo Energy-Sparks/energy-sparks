45f6f8a1f1ba9afbd8622252a5e2090e0d8a8681
Deb Bassett <deb@urbanwide.com>
2023-11-16 19:00:55 +0000

    Keep original behaviour

--- app/components/info_bar_component.rb
+++ app/components/info_bar_component.rb
@@ -4,7 +4,7 @@ class InfoBarComponent < ViewComponent::Base
   include ApplicationHelper
   attr_accessor :status, :title, :icon, :buttons, :style
 
-  def initialize(status: :neutral, title:, icon: nil, buttons: [], classes: nil, style: :normal)
+  def initialize(status: :neutral, title:, icon: nil, buttons: nil, classes: nil, style: :normal)
     @status = status
     @title = title
     @icon = icon
