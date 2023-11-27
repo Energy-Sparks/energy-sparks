29f6cfb58ef1dec63b800584cf6c840cece57c31
Deb Bassett <deb@urbanwide.com>
2023-11-27 17:20:57 +0000

    Fix test

--- spec/helpers/nav_helper_spec.rb
+++ spec/helpers/nav_helper_spec.rb
@@ -5,10 +5,10 @@ describe NavHelper do
     it 'returns buttons for all available locales except the current locale' do
       I18n.locale = 'en'
       allow(helper).to receive(:url_for).and_return('http://cy.energysparks.uk/')
-      expect(helper.locale_switcher_buttons).to eq('<ul class="navbar-nav"><li class="nav-item pl-3 pr-3 nav-lozenge nav-lozenge-little-padding"><a href="http://cy.energysparks.uk/">Cymraeg</a></li></ul>')
+      expect(helper.locale_switcher_buttons).to eq('<ul class="navbar-nav navbar-expand"><li class="nav-item pl-3 pr-3 nav-lozenge nav-lozenge-little-padding"><a href="http://cy.energysparks.uk/">Cymraeg</a></li></ul>')
       I18n.locale = 'cy'
       allow(helper).to receive(:url_for).and_return('http://energysparks.uk/')
-      expect(helper.locale_switcher_buttons).to eq('<ul class="navbar-nav"><li class="nav-item pl-3 pr-3 nav-lozenge nav-lozenge-little-padding"><a href="http://energysparks.uk/">English</a></li></ul>')
+      expect(helper.locale_switcher_buttons).to eq('<ul class="navbar-nav navbar-expand"><li class="nav-item pl-3 pr-3 nav-lozenge nav-lozenge-little-padding"><a href="http://energysparks.uk/">English</a></li></ul>')
       I18n.locale = 'en'
     end
   end
