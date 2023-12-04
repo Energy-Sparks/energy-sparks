030079f8fddf3935068a824e8f5175ec60423669
Leigh Dodds <leigh@ldodds.com>
2023-12-04 16:54:03 +0000

    Change ownership

--- .platform/hooks/prebuild/10_install_packages.sh
+++ .platform/hooks/prebuild/10_install_packages.sh
@@ -3,4 +3,4 @@
 app="$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)";
 cd "${app}";
 yarn install
-chown webapp:webapp node_modules/.yarn-integrity
+chown -R webapp:webapp node_modules
