480d2d7bffc2000ad2ff08e17d479789a9b9d35c
Leigh Dodds <leigh@ldodds.com>
2023-12-04 16:51:59 +0000

    Revert "Try corepack"
    This reverts commit 74a3bba6ed7c49e7d399df9f3a392764baf037c6.

--- .platform/hooks/prebuild/10_install_packages.sh
+++ .platform/hooks/prebuild/10_install_packages.sh
@@ -2,6 +2,5 @@
 # Install dependencies
 app="$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)";
 cd "${app}";
-corepack yarn
-#yarn install
+yarn install
 chown webapp:webapp node_modules/.yarn-integrity
