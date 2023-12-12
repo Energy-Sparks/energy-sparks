f1d3a26b910beb7b0a5af6739f186ef87a374f83
Deb Bassett <deb@urbanwide.com>
2023-12-12 11:57:09 +0000

    Use an actual programme observation now we have one (assuming the backfill programme observation after party task has been run)

--- spec/components/previews/timeline_component_preview.rb
+++ spec/components/previews/timeline_component_preview.rb
@@ -1,6 +1,5 @@
 class TimelineComponentPreview < ViewComponent::Preview
   def with_observations
-    programme = Programme.last
     audit = Audit.last
 
     observations = [
@@ -9,7 +8,7 @@ class TimelineComponentPreview < ViewComponent::Preview
       Observation.new(school: audit.school, observation_type: 'audit_activities_completed', audit: audit, at: Time.zone.yesterday, points: SiteSettings.current.audit_activities_bonus_points),
       Observation.find_by(observation_type: 'intervention'),
       Observation.find_by(observable_type: 'TransportSurvey'),
-      Observation.new(school: programme.school, observation_type: 'observable', observable: programme, at: Time.zone.yesterday, points: programme.programme_type.bonus_score),
+      Observation.find_by(observable_type: 'Programme'),
       Observation.find_by(observation_type: 'school_target'),
       Observation.find_by(observation_type: 'temperature'),
     ].compact.sort_by(&:at).reverse
