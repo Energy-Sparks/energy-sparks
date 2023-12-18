9b4612244b125e225c0dd986347d3b059980b4af
Tristan Hill <t23+git@eaux.uk>
2023-12-18 18:40:45 +0000

    more

--- app/models/school_onboarding.rb
+++ app/models/school_onboarding.rb
@@ -46,7 +46,6 @@
 
 class SchoolOnboarding < ApplicationRecord
   validates :school_name, :contact_email, presence: true
-  validates :weather_station, presence: true, unless: :dark_sky_area_id?
 
   belongs_to :school, optional: true
   belongs_to :school_group, optional: true
