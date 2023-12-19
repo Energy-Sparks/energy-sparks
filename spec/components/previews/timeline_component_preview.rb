422bb5d6de15b1898cf2433b03fe1732db4c8c38
Deb Bassett <deb@urbanwide.com>
2023-12-19 19:46:20 +0000

    Make previews work

--- spec/components/previews/timeline_component_preview.rb
+++ spec/components/previews/timeline_component_preview.rb
@@ -6,7 +6,7 @@ class TimelineComponentPreview < ViewComponent::Preview
       Observation.find_by(observation_type: 'activity'),
       Observation.find_by(observation_type: 'intervention'),
       Observation.find_by(observation_type: 'temperature'),
-      Observation.new(school: audit.school, observable: audit, variation: 'ActivitiesCompleted', at: Time.zone.yesterday, points: SiteSettings.current.audit_activities_bonus_points),
+      Observation.new(school: audit.school, observable: audit, observable_variation: 'ActivitiesCompleted', at: Time.zone.yesterday, points: SiteSettings.current.audit_activities_bonus_points),
       Observation.for_observable('Audit').last,
       Observation.for_observable('TransportSurvey').last,
       Observation.for_observable('Programme').last,
