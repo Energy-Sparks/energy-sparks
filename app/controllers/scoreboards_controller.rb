4b13f0de4b9a1f276d7a1011a1fa2ead2b64fa6a
Tristan Hill <t23+git@eaux.uk>
2023-12-20 19:56:23 +0000

    wip

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,16 +1,23 @@
 class ScoreboardsController < ApplicationController
   include Scoring
 
-  load_resource
+  # load_resource
   skip_before_action :authenticate_user!
 
   # GET /scoreboards
   def index
+    @scoreboard_all = ScoreboardAll.new
     @scoreboards = ComparisonService.new(current_user).list_scoreboards
   end
 
   def show
-    authorize! :read, @scoreboard
+    # binding.pry
+    if params[:id] == ScoreboardAll::SLUG
+      @scoreboard = ScoreboardAll.new
+    else
+      @scoreboard = Scoreboard.find(params[:id])
+      authorize! :read, @scoreboard
+    end
     setup_scores_and_years(@scoreboard)
   end
 end
