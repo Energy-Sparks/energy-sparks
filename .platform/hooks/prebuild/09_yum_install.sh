cf7f0ff73732ef3f30ce784cfcf5a7842e8991b1
Leigh Dodds <leigh@ldodds.com>
2024-01-04 15:09:46 +0000

    Upgrade pg gem (#3323)

--- .platform/hooks/prebuild/09_yum_install.sh
+++ .platform/hooks/prebuild/09_yum_install.sh
@@ -3,3 +3,7 @@ wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo;
 yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 yum -y install yarn;
+
+# amazon-linux extras installed by default on AL2
+amazon-linux-extras enable postgresql14
+yum -y install postgresql-devel
