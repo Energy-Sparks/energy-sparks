f4f386393919f0e8fc678356c4c62a5d0d494e89
Leigh Dodds <leigh@ldodds.com>
2024-01-05 14:01:54 +0000

    Tidy

--- spec/models/concerns/transifex_serialisable_spec.rb
+++ spec/models/concerns/transifex_serialisable_spec.rb
@@ -73,13 +73,13 @@ describe TransifexSerialisable do
     end
 
     it 'model shows no content present if no attributes set' do
-      serialisable = ActivityCategory.new #use this as an example of implementing class
-      expect(serialisable).not_to have_content
+      #use this as an example of implementing class
+      expect(ActivityCategory.new).not_to have_content
     end
 
     it 'model shows content present if some attribute set' do
       clazz = ActivityCategory #use this as an example of implementing class
-      attr = clazz.mobility_attributes.first
+      attr = ActivityCategory.mobility_attributes.first
       serialisable = clazz.new(attr => 'something')
       expect(serialisable).to have_content
     end
