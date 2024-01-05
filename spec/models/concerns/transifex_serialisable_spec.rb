186c0c21feff3f13fe2c43d2f3c494ee79b16ce2
Tristan Hill <t23+git@eaux.uk>
2024-01-05 12:51:25 +0000

    more

--- spec/models/concerns/transifex_serialisable_spec.rb
+++ spec/models/concerns/transifex_serialisable_spec.rb
@@ -61,7 +61,7 @@ describe TransifexSerialisable do
     before do
       #we have to explicitly require the classes otherwise they're not loaded and
       #check for included modules fails
-      ActivityCategory # ensure is loaded
+      ActivityCategory # rubocop:disable Lint/Void - ensure is loaded
       @tx_serialisables = ApplicationRecord.descendants.select { |c| c.included_modules.include? TransifexSerialisable }
     end
 
