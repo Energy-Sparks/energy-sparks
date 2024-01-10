ba218cbc31ac243e7bc26fdbaf864893a7b3a9ac
Deb Bassett <deb@urbanwide.com>
2024-01-10 12:05:15 +0000

    Add join programme prompt

--- spec/support/shared_examples/prompts.rb
+++ spec/support/shared_examples/prompts.rb
@@ -31,6 +31,9 @@ RSpec.shared_examples "a complete programme prompt" do |displayed: true, with_pr
   include_examples "a standard prompt", displayed: displayed
 end
 
+#RSpec.shared_examples "a join programme prompt" do
+#end
+
 RSpec.shared_examples "a recommendations prompt" do |displayed: true|
   let(:message) { "Complete one of our recommended pupil or adult led activities to start reducing your energy usage" }
   include_examples "a standard prompt", displayed: displayed
