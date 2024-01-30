b0103c210ef34cf4271ed8830131e6ca89c2e21b
Tristan Hill <t23+git@eaux.uk>
2024-01-30 21:02:53 +0000

    more

--- config/initializers/action_mailer.rb
+++
@@ -1,6 +0,0 @@
-if Rails.application.config.action_mailer.show_previews
-  Rails.env.development? || Rails::MailersController.prepend_before_action do
-    authenticate_user!
-    redirect_to root_path unless can?(:manage, :admin_functions)
-  end
-end
