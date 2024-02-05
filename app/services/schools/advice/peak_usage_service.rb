97e11cd8bca7e17b81f1714a076e7e9afef7bbd3
Tristan Hill <878258+tbhi@users.noreply.github.com>
2024-02-05 16:10:30 +0000

    Revise electricity intraday page to work better for limited data (#3377)

--- app/services/schools/advice/peak_usage_service.rb
+++ app/services/schools/advice/peak_usage_service.rb
@@ -2,29 +2,32 @@ module Schools
   module Advice
     class PeakUsageService
       include AnalysableMixin
+      # matches the required days for base load advice pages - see BaseloadService and Baseload::BaseService it uses
+      REQUIRED_DAYS = 14
 
       def initialize(school, meter_collection)
         @school = school
         @meter_collection = meter_collection
       end
 
-      # Not yet implemented in underlying services
       def enough_data?
-        meter_data_checker.one_years_data?
+        meter_data_checker.at_least_x_days_data?(REQUIRED_DAYS)
       end
 
-      # Not yet implemented in underlying services
       def data_available_from
-        meter_data_checker.date_when_enough_data_available(365)
+        meter_data_checker.date_when_enough_data_available(REQUIRED_DAYS)
       end
 
-      def average_peak_kw
-        peak_usage_calculation_service.average_peak_kw
-      end
+      delegate :average_peak_kw, to: :peak_usage_calculation_service
+      delegate :date_range, to: :peak_usage_calculation_service
 
       def previous_year_peak_kw
+        previous_years_asof_date = asof_date - 1.year
+        previous_years_checker = Util::MeterDateRangeChecker.new(aggregate_meter, previous_years_asof_date)
+        return unless previous_years_checker.at_least_x_days_data?(REQUIRED_DAYS)
+
         previous_years_peak_usage_calculation_service = peak_usage_calculation_service(previous_years_asof_date)
-        previous_years_peak_usage_calculation_service.enough_data? ? previous_years_peak_usage_calculation_service.average_peak_kw : nil
+        previous_years_peak_usage_calculation_service.average_peak_kw
       end
 
       def percentage_change_in_peak_kw
@@ -46,6 +49,10 @@ module Schools
 
       private
 
+      def meter_data_checker
+        @meter_data_checker ||= Util::MeterDateRangeChecker.new(aggregate_meter, @asof_date)
+      end
+
       # Copied from ContentBase
       def percent_change(old_value, new_value)
         return nil if old_value.nil? || new_value.nil?
@@ -54,16 +61,8 @@ module Schools
         (new_value - old_value) / old_value
       end
 
-      def previous_years_asof_date
-        @previous_years_asof_date ||= asof_date - 1.year
-      end
-
       def aggregate_meter
         @meter_collection.aggregated_electricity_meters
-      end
-
-      def meter_data_checker
-        @meter_data_checker ||= Util::MeterDateRangeChecker.new(aggregate_meter, asof_date)
       end
 
       def peak_usage_benchmarking_service
@@ -74,10 +73,7 @@ module Schools
       end
 
       def peak_usage_calculation_service(date = asof_date)
-        ::Usage::PeakUsageCalculationService.new(
-          meter_collection: @meter_collection,
-          asof_date: date
-        )
+        Usage::PeakUsageCalculationService.new(meter_collection: @meter_collection, asof_date: date)
       end
     end
   end
