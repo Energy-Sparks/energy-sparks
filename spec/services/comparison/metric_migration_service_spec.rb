4d291e03431f1e0936aeb2a9cfe68a6c1e95cca9
Leigh Dodds <leigh@ldodds.com>
2024-02-13 10:35:35 +0000

    Add additional specs

--- spec/services/comparison/metric_migration_service_spec.rb
+++ spec/services/comparison/metric_migration_service_spec.rb
@@ -25,7 +25,38 @@ describe Comparison::MetricMigrationService do
   end
 
   describe '#fuel_type_for_metric_type' do
-    it 'does something'
+    context 'when the alert type has a fuel type' do
+      let!(:alert_type) do
+        create(:alert_type, enabled: true,
+          fuel_type: :electricity, class_name: 'AlertElectricityBaseloadVersusBenchmark')
+      end
+
+      it { expect(service.fuel_type_for_metric_type(:unknown, {}, alert_type)).to eq :electricity }
+      it { expect(service.fuel_type_for_metric_type(:gas_annual_kwh, { units: :unknown }, alert_type)).to eq :electricity }
+      it { expect(service.fuel_type_for_metric_type(:annual_kw, { units: { kw: :gas } }, alert_type)).to eq :electricity }
+    end
+
+    context 'when the alert type has no fuel type' do
+      let!(:alert_type) do
+        create(:alert_type, enabled: true,
+          fuel_type: nil, class_name: 'AlertEnergyAnnualVersusBenchmark')
+      end
+
+      context 'with a fuel type in the analytics metric definition' do
+        it { expect(service.fuel_type_for_metric_type(:annual_kw, { units: { kw: :gas } }, alert_type)).to eq :gas }
+      end
+
+      context 'when the metric name includes a fuel type' do
+        it { expect(service.fuel_type_for_metric_type(:annual_electricity_kwh, { units: :kwh }, alert_type)).to eq :electricity }
+        it { expect(service.fuel_type_for_metric_type(:annual_solar_kwh, { units: :kwh }, alert_type)).to eq :solar_pv }
+        it { expect(service.fuel_type_for_metric_type(:annual_gas_kwh, { units: :kwh }, alert_type)).to eq :gas }
+        it { expect(service.fuel_type_for_metric_type(:annual_storage_heaters_kwh, { units: :kwh }, alert_type)).to eq :storage_heater }
+      end
+
+      context 'when there is no other fuel type' do
+        it { expect(service.fuel_type_for_metric_type(:annual_kwh, { units: :kwh }, alert_type)).to eq :multiple }
+      end
+    end
   end
 
   describe '#migrate' do
@@ -52,6 +83,11 @@ describe Comparison::MetricMigrationService do
             description: 'benchmark_per_pupil description',
             units: :kwh,
             benchmark_code: 'abc'
+          },
+          school_name: {
+            description: 'should be ignored',
+            units: String,
+            benchmark_code: 'xyz'
           }
         }.freeze
 
@@ -62,16 +98,13 @@ describe Comparison::MetricMigrationService do
       # rubocop:enable RSpec/LeakyConstantDeclaration
       # rubocop:enable Lint/ConstantDefinitionInBlock
 
-
       let!(:alert_type) do
         create(:alert_type, enabled: true,
           fuel_type: :electricity, class_name: 'TestAlert')
       end
 
-      it 'creates the expected metric types' do
+      it 'correctly creates the metric types' do
         service.migrate
-        expect(Comparison::MetricType.count).to eq(2)
-
         metric = Comparison::MetricType.order(:key).first
         expect(metric.key.to_sym).to eq(:benchmark_per_pupil)
         expect(metric.label).to eq('benchmark_per_pupil description')
@@ -84,7 +117,10 @@ describe Comparison::MetricMigrationService do
         expect(metric.units.to_sym).to eq :float
       end
 
-      it 'does not create ignored metrics'
+      it 'does not create ignored metrics' do
+        service.migrate
+        expect(Comparison::MetricType.count).to eq(2)
+      end
     end
 
     context 'with a real alert type that produces benchmarks' do
