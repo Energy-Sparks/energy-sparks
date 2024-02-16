b0ec9f1fa770121ef834a85b64f09d6cae9942f9
Leigh Dodds <leigh@ldodds.com>
2024-02-16 15:04:05 +0000

    Add shared context

--- spec/models/comparison/metric_spec.rb
+++ spec/models/comparison/metric_spec.rb
@@ -154,23 +154,8 @@ RSpec.describe Comparison::Metric, type: :model do
   end
 
   describe '.order_by_school_metric_value' do
-    let!(:metric_type) { create(:metric_type) }
-
-    let!(:school_1) { create(:school) }
-    let!(:school_2) { create(:school) }
-
-    let!(:run_1) { create(:benchmark_result_school_generation_run, school: school_1)}
-    let!(:run_2) { create(:benchmark_result_school_generation_run, school: school_2)}
-
-    # Setup test data so that we have
-    # one run for each school, with one metric and metric type that we'll be sorting one
-    #
-    # school_1 has low value, school_2 has high_value
-    let!(:metric_1) { create(:metric, school: school_1, benchmark_result_school_generation_run: run_1, metric_type: metric_type, value: low_value) }
-    let!(:metric_2) { create(:metric, school: school_2, benchmark_result_school_generation_run: run_2, metric_type: metric_type, value: high_value) }
-
-    # ..plus one additional metric value that should be unused in sort
-    let!(:other_metric) { create(:metric, school: school_1, benchmark_result_school_generation_run: run_1, value: high_value) }
+    include_context 'with some schools and metrics' do
+    end
 
     shared_examples 'it sorts and returns results correctly' do
       subject(:results) { Comparison::Metric.order_by_school_metric_value(metric_type, order)}
--- spec/services/comparison/report_service_spec.rb
+++ spec/services/comparison/report_service_spec.rb
@@ -3,23 +3,7 @@ require 'rails_helper'
 describe Comparison::ReportService do
   subject(:service) { described_class.new(definition: definition) }
 
-  let!(:metric_type) { create(:metric_type) }
-
-  let!(:school_1) { create(:school) }
-  let!(:school_2) { create(:school) }
-
-  let!(:run_1) { create(:benchmark_result_school_generation_run, school: school_1)}
-  let!(:run_2) { create(:benchmark_result_school_generation_run, school: school_2)}
-
-  # Setup test data so that we have
-  # one run for each school, with one metric each, using the metric type that we'll be sorting on
-  #
-  # school_1 has low value, school_2 has high_value
-  let!(:metric_1) { create(:metric, school: school_1, benchmark_result_school_generation_run: run_1, metric_type: metric_type, value: 10) }
-  let!(:metric_2) { create(:metric, school: school_2, benchmark_result_school_generation_run: run_2, metric_type: metric_type, value: 100) }
-
-  # ..plus one additional metric value to include in the results
-  let!(:other_metric) { create(:metric, school: school_1, benchmark_result_school_generation_run: run_1, value: 100) }
+  include_context 'with some schools and metrics'
 
   let(:definition) do
     Comparison::ReportDefinition.new(
