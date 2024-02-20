d39a9d229597e9ecbef75658fc4c7e9253f53f27
Leigh Dodds <leigh@ldodds.com>
2024-02-20 16:19:16 +0000

    Add base class

--- app/models/comparison/baseload_per_pupil.rb
+++ app/models/comparison/baseload_per_pupil.rb
@@ -7,17 +7,10 @@
 #  average_baseload_last_year_gbp                :float
 #  average_baseload_last_year_kw                 :float
 #  electricity_economic_tariff_changed_this_year :boolean
-#  id                                            :bigint(8)        primary key
+#  id                                            :bigint(8)
 #  one_year_baseload_per_pupil_kw                :float
 #  one_year_saving_versus_exemplar_gbp           :float
 #  school_id                                     :bigint(8)
 #
-class Comparison::BaseloadPerPupil < ApplicationRecord
-  belongs_to :school
-
-  self.primary_key = :id
-
-  def readonly?
-    true
-  end
+class Comparison::BaseloadPerPupil < Comparison::View
 end
