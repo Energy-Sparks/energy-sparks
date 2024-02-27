8838e16738bd35ffd4f928d520219d0e83946c15
Leigh Dodds <leigh@ldodds.com>
2024-02-27 14:02:34 +0000

    Add specs for processing of results

--- app/services/amr/n3rgy_downloader.rb
+++ app/services/amr/n3rgy_downloader.rb
@@ -2,6 +2,8 @@ require 'dashboard'
 
 module Amr
   class N3rgyDownloader
+    KWH_PER_M3_GAS = 11.1 # this depends on the calorifc value of the gas and so is an approximate average
+
     def initialize(meter:, start_date:, end_date:, n3rgy_api: MeterReadingsFeeds::N3rgyData.new)
       @meter = meter
       @n3rgy_api = n3rgy_api
@@ -10,11 +12,96 @@ module Amr
     end
 
     def readings
-      @n3rgy_api.readings(@meter.mpan_mprn, @meter.meter_type, @start_date, @end_date)
+      if EnergySparks::FeatureFlags.active?(:n3rgy_v2)
+        download_readings
+      else
+        @n3rgy_api.readings(@meter.mpan_mprn, @meter.meter_type, @start_date, @end_date)
+      end
     end
 
     def tariffs
       @n3rgy_api.tariffs(@meter.mpan_mprn, @meter.meter_type, @start_date, @end_date)
+    end
+
+    private
+
+    def download_readings
+      # Turn the array of [DateTime, value] into a hash
+      readings_by_date_time = fetch_all_readings
+
+      # Creates a hash of { readings: {date => x48 array}, missing_readings: [date_time] }
+      meter_readings = X48Formatter.convert_dt_to_v_to_date_to_v_x48(@start_date.to_date,
+        @end_date.to_date, readings_by_date_time, true, nil)
+
+      # This return format matches the existing v1 code. This can be simplified as there is no
+      # need to create the one day readings to then just throw them away in the next step
+      {
+        @meter.fuel_type =>
+          {
+            mpan_mprn:        @meter.mpan_mprn,
+            readings:         make_one_day_readings(meter_readings[:readings], @meter.mpan_mprn),
+            missing_readings: meter_readings[:missing_readings]
+          }
+      }
+    end
+
+    # TODO remove, unnecessary, see note above
+    def make_one_day_readings(meter_readings_by_date, mpan_mprn)
+      meter_readings_by_date.map do |date, readings|
+        [date.to_date, OneDayAMRReading.new(mpan_mprn, date.to_date, 'ORIG', nil, DateTime.now, readings, true)]
+      end.to_h
+    end
+
+    # Query the n3rgy API in blocks of up to 90 days to fetch all of the readings
+    #
+    # Extracts the readings from each API response, adjusting units as required
+    #
+    # Returns a single hash of DateTime => half hourly reading value
+    def fetch_all_readings
+      readings = []
+      (@start_date..@end_date).each_slice(90) do |date_range_max_90days|
+        response = api_client.readings(@meter.mpan_mprn,
+          @meter.fuel_type.to_s,
+          DataFeeds::N3rgy::DataApiClient::READING_TYPE_CONSUMPTION,
+          date_range_max_90days.first,
+          date_range_max_90days.last)
+        readings += extract_readings(response)
+      end
+      readings.to_h
+    end
+
+    # Convert an n3rgy v2 API response into an array of [DateTime, value] values
+    #
+    # For gas readings, values are converted from cubic meters to kWh using
+    # a fixed conversion value.
+    def extract_readings(response)
+      return [] unless response.dig('devices', 0, 'values').present?
+
+      # TODO
+      # v2 returns an array of readings for each 'device' Unclear what these
+      # map to in practice. Needs further testing.
+      #
+      # The responses may also contain a secondaryValue which we are also ignoring
+      #
+      # Individual values can also include an additionalInformation key which we
+      # are not using
+      response['devices'][0]['values'].map do |half_hourly_reading|
+        value = case response['unit']
+                when 'm3'
+                  to_kwh(half_hourly_reading['primaryValue'])
+                else
+                  half_hourly_reading['primaryValue']
+                end
+        [DateTime.parse(half_hourly_reading['timestamp']), value]
+      end
+    end
+
+    def to_kwh(value)
+      value.nil? ? nil : KWH_PER_M3_GAS * value
+    end
+
+    def api_client
+      DataFeeds::N3rgy::DataApiClient.production_client
     end
   end
 end
--- spec/services/amr/n3rgy_downloader_spec.rb
+++ spec/services/amr/n3rgy_downloader_spec.rb
@@ -2,22 +2,114 @@ require 'rails_helper'
 
 module Amr
   describe N3rgyDownloader do
-    let(:n3rgy_api) { double('n3rgy_api') }
-    let(:meter)     { create(:electricity_meter) }
-    let(:config)    { create(:amr_data_feed_config)}
-    let(:today)     { Time.zone.today }
-    let(:yesterday) { today - 1 }
-
-    it 'invokes the api' do
-      expect(n3rgy_api).to receive(:readings)
-      loader = Amr::N3rgyDownloader.new(n3rgy_api: n3rgy_api, meter: meter, start_date: nil, end_date: nil)
-      loader.readings
+    subject(:service) do
+      Amr::N3rgyDownloader.new(n3rgy_api: n3rgy_api, meter: meter, start_date: start_date, end_date: end_date)
     end
 
-    it 'passes in correct params' do
-      expect(n3rgy_api).to receive(:readings).with(meter.mpan_mprn, meter.meter_type, today, yesterday)
-      loader = Amr::N3rgyDownloader.new(n3rgy_api: n3rgy_api, meter: meter, start_date: today, end_date: yesterday)
-      loader.readings
+    let(:meter)     { create(:electricity_meter) }
+    let(:config)    { create(:amr_data_feed_config)}
+    let(:end_date)  { Time.zone.today }
+    let(:start_date) { end_date - 1 }
+
+    around do |example|
+      ClimateControl.modify FEATURE_FLAG_N3RGY_V2: flag do
+        example.run
+      end
+    end
+
+    describe '#readings' do
+      context 'with v1' do
+        let(:flag) { 'false' }
+        let(:n3rgy_api) { double('n3rgy_api') }
+
+        it 'invokes the api' do
+          expect(n3rgy_api).to receive(:readings)
+          service.readings
+        end
+
+        it 'passes in correct params' do
+          expect(n3rgy_api).to receive(:readings).with(meter.mpan_mprn, meter.meter_type, start_date, end_date)
+          service.readings
+        end
+      end
+
+      context 'with v2' do
+        let(:flag) { 'true' }
+        let(:n3rgy_api) { nil }
+
+        let(:stub) { double('data-api-client') }
+
+        before do
+          allow(DataFeeds::N3rgy::DataApiClient).to receive(:production_client).and_return(stub)
+        end
+
+        context 'with a period of more than 90 days' do
+          let(:start_date) { Date.new(2023, 1, 1) }
+          let(:end_date) { start_date + 100.days }
+          let(:response) do
+            {
+              'devices' => [{ 'values' => [] }]
+            }
+          end
+
+          it 'makes multiple API requests' do
+            expect(stub).to receive(:readings).at_least(:twice).with(meter.mpan_mprn, meter.fuel_type.to_s, DataFeeds::N3rgy::DataApiClient::READING_TYPE_CONSUMPTION, anything, anything).and_return(response)
+            service.readings
+          end
+        end
+
+        context 'with no device readings' do
+          let(:response) do
+            {
+              'devices' => []
+            }
+          end
+
+          it 'returns empty results' do
+            allow(stub).to receive(:readings).with(meter.mpan_mprn, meter.fuel_type.to_s, DataFeeds::N3rgy::DataApiClient::READING_TYPE_CONSUMPTION, anything, anything).and_return(response)
+            readings = service.readings
+            expect(readings[meter.fuel_type][:readings]).to be_empty
+          end
+        end
+
+        context 'with successful results' do
+          subject(:readings) { service.readings }
+
+          # Match dates in fixture
+          let(:start_date) { Date.new(2012, 4, 27)}
+          let(:end_date) { Date.new(2012, 4, 28)}
+
+          let(:response) { JSON.parse(File.read('spec/fixtures/n3rgy/get-reading-type-consumption.json')) }
+
+          let(:fixture_readings) do
+            [nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, 0.214, 0.158, 0.064, 0.062, 0.1, 0.096, 0.061, 0.097, 0.102, 0.129, 0.1, 0.101, 0.123, 0.245, 0.109, 0.018, 0.058, 0.057, 0.019, 0.03, 0.107, 0.058, 0.025, 0.019, 0.1, 0.219, 0.132, 0.091, 0.105, 0.11, 0.09]
+          end
+
+          before do
+            allow(stub).to receive(:readings).with(meter.mpan_mprn, meter.fuel_type.to_s, DataFeeds::N3rgy::DataApiClient::READING_TYPE_CONSUMPTION, anything, anything).and_return(response.with_indifferent_access)
+          end
+
+          it 'extracts the readings' do
+            readings_by_day = readings[meter.fuel_type][:readings]
+            # Match readings from fixture
+            expect(readings_by_day[start_date].kwh_data_x48).to eq(fixture_readings)
+          end
+
+          context 'when returned readings are in m3' do
+            let(:response) do
+              original = JSON.parse(File.read('spec/fixtures/n3rgy/get-reading-type-consumption.json'))
+              original['unit'] = 'm3'
+              original
+            end
+
+            it 'converts to kWh' do
+              adjusted = fixture_readings.map { |r| r.nil? ? nil : r * Amr::N3rgyDownloader::KWH_PER_M3_GAS}
+              readings_by_day = readings[meter.fuel_type][:readings]
+              expect(readings_by_day[start_date].kwh_data_x48).to eq(adjusted)
+            end
+          end
+        end
+      end
     end
   end
 end
