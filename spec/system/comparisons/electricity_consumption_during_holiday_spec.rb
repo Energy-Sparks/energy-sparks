6b2168cfcf4d8215f4d0c8a181a499df2fc8e658
Tristan Hill <t23+git@eaux.uk>
2024-03-01 15:46:58 +0000

    more

--- spec/system/comparisons/electricity_consumption_during_holiday_spec.rb
+++ spec/system/comparisons/electricity_consumption_during_holiday_spec.rb
@@ -22,14 +22,10 @@ describe 'electricity_consumption_during_holiday' do
     it_behaves_like 'a school comparison report', advice_page: false do
       let(:title) { I18n.t('analytics.benchmarking.chart_table_config.electricity_consumption_during_holiday') }
       let(:expected_school) { school }
-    end
-
-    it 'displays the expected data' do
-      expect(page).to have_css('#comparison-table tr', count: 2)
-      expect(all('#comparison-table tr').map { |tr| tr.all('th,td').map(&:text) }).to eq(
+      let(:expected_table) do
         [['School', 'Projected usage by end of holiday', 'Holiday usage to date', 'Holiday'],
          [school.name, '£1', '£2', 'Holiday 1']]
-      )
+      end
     end
   end
 end
