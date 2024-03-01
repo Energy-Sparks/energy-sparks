46a4a0e425576c561bebfaefcdc1493b35684ece
Tristan Hill <t23+git@eaux.uk>
2024-03-01 10:35:31 +0000

    more

--- spec/system/comparisons/change_in_electricity_holiday_consumption_previous_years_holiday_spec.rb
+++ spec/system/comparisons/change_in_electricity_holiday_consumption_previous_years_holiday_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 describe 'electricity_peak_kw_per_pupil' do
-  let(:schools) { create_list(:school, 2) }
+  let(:schools) { create_list(:school, 3) }
 
   before do
     create(:alert, school: schools[0],
                    alert_generation_run: create(:alert_generation_run, school: schools[0]),
                    alert_type: create(:alert_type, class_name: 'AlertPreviousYearHolidayComparisonElectricity'),
                    variables: {
-                     difference_percent: 'Infinity',
+                     difference_percent: 1,
                      difference_gbpcurrent: 2,
                      difference_kwh: 3,
                      name_of_current_period: 'current',
@@ -23,9 +23,22 @@ describe 'electricity_peak_kw_per_pupil' do
                    alert_generation_run: create(:alert_generation_run, school: schools[1]),
                    alert_type: create(:alert_type, class_name: 'AlertPreviousYearHolidayComparisonElectricity'),
                    variables: {
-                     difference_percent: '-Infinity',
+                     difference_percent: 'Infinity',
                      difference_gbpcurrent: 4,
                      difference_kwh: 5,
+                     name_of_current_period: 'current',
+                     truncated_current_period: false,
+                     name_of_previous_period: 'previous',
+                     pupils_changed: false,
+                     tariff_has_changed: false
+                   })
+    create(:alert, school: schools[2],
+                   alert_generation_run: create(:alert_generation_run, school: schools[2]),
+                   alert_type: create(:alert_type, class_name: 'AlertPreviousYearHolidayComparisonElectricity'),
+                   variables: {
+                     difference_percent: '-Infinity',
+                     difference_gbpcurrent: 6,
+                     difference_kwh: 7,
                      name_of_current_period: 'current',
                      truncated_current_period: false,
                      name_of_previous_period: 'previous',
@@ -46,11 +59,12 @@ describe 'electricity_peak_kw_per_pupil' do
     end
 
     it 'displays the expected data' do
-      expect(page).to have_css('#comparison-table tr', count: 4)
+      expect(page).to have_css('#comparison-table tr', count: 5)
       expect(all('#comparison-table tr').map { |tr| tr.all('th,td').map(&:text) }).to eq(
         [['School', 'Change %', 'Change £ (latest tariff)', 'Change kWh', 'Most recent holiday', 'Previous holiday'],
-         ["#{schools[0].name} (*1) (*2) (*6)", '+Infinity%', '£2', '3', 'current (partial)', 'previous'],
-         ["#{schools[1].name} (*3)", '-Infinity%', '£4', '5', 'current', 'previous'],
+         ["#{schools[1].name} (*2)", '+Infinity%', '£4', '5', 'current', 'previous'],
+         ["#{schools[0].name} (*1) (*6)", '+100%', '£2', '3', 'current (partial)', 'previous'],
+         ["#{schools[2].name} (*3)", '-Infinity%', '£6', '7', 'current', 'previous'],
          ["Notes\n" \
           '(*1) the comparison has been adjusted because the number of pupils have changed between the two holidays. ' \
           '(*2) schools where percentage change is +Infinity is caused by the electricity consumption in the ' \
