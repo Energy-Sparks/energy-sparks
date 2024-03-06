9451b3c8db56216e7bec10434151e371677b5c38
Tristan Hill <t23+git@eaux.uk>
2024-03-06 13:14:09 +0000

    initial

--- app/controllers/comparisons/baseload_per_pupil_controller.rb
+++ app/controllers/comparisons/baseload_per_pupil_controller.rb
@@ -2,6 +2,12 @@
 
 module Comparisons
   class BaseloadPerPupilController < BaseController
+
+    def index
+      super
+      @table = Table.new
+    end
+
     private
 
     def key
@@ -19,5 +25,84 @@ module Comparisons
     def create_charts(results)
       create_single_number_chart(results, :one_year_baseload_per_pupil_kw, 1000.0, 'baseload_per_pupil_w', 'w')
     end
+
+    class Table
+      def columns
+        [School, BaseloadPerPupil, LastYearCostOfBaseload, AverageBaseloadKw, BaseloadPercent, SavingIfMatchedExemplarSchool]
+      end
+
+      class Column
+        include AdvicePageHelper
+        delegate :helpers, to: ActionController::Base
+        # delegate :url_helpers, to: 'Rails.application.routes'
+      end
+
+      class School < Column
+        def header
+          :school
+        end
+
+        def html(result, advice_page)
+          # s = helpers.link_to(result.school.name, Rails.application.routes.url_helpers.advice_page_path(result.school, advice_page, :insights))
+          s = helpers.link_to(result.school.name, '')
+          if result.electricity_economic_tariff_changed_this_year
+            s << ' <a href="#electricity_economic_tariff_changed_this_year">[t]</a>'.html_safe
+          end
+          s
+        end
+      end
+
+      class BaseloadPerPupil < Column
+        def header
+          :baseload_per_pupil_w
+        end
+
+        def html(result, _advice_page)
+          format_unit(result.one_year_baseload_per_pupil_kw * 1000.0, :kw, true, :benchmark)
+        end
+      end
+
+      class LastYearCostOfBaseload < Column
+        def header
+          :last_year_cost_of_baseload
+        end
+
+        def html(result, _advice_page)
+          format_unit(result.average_baseload_last_year_gbp, :£, true, :benchmark)
+        end
+      end
+
+      class AverageBaseloadKw < Column
+        def header
+          :average_baseload_kw
+        end
+
+        def html(result, _advice_page)
+          format_unit(result.average_baseload_last_year_kw, :kw, true, :benchmark)
+        end
+      end
+
+      class BaseloadPercent < Column
+        def header
+          :baseload_percent
+        end
+
+        def html(result, _advice_page)
+          format_unit(result.annual_baseload_percent, :percent, true, :benchmark)
+        end
+      end
+
+      class SavingIfMatchedExemplarSchool < Column
+        def header
+          :saving_if_matched_exemplar_school
+        end
+
+        def html(result, _advice_page)
+          format_unit([0.0, result.one_year_saving_versus_exemplar_gbp].max, :£, true, :benchmark)
+        end
+      end
+
+    end
+
   end
 end
