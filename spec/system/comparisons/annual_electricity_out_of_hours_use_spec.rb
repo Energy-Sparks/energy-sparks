ce01b67317cacf34426c5c9f3a031210f08baef2
Deb Bassett <deb@urbanwide.com>
2024-03-07 15:16:16 +0000

    Spec

--- spec/system/comparisons/annual_electricity_out_of_hours_use_spec.rb
+++ spec/system/comparisons/annual_electricity_out_of_hours_use_spec.rb
@@ -3,56 +3,76 @@ require 'rails_helper'
 describe 'annual_electricity_out_of_hours_use' do
   let!(:school) { create(:school) }
   let(:key) { :annual_electricity_out_of_hours_use }
-  let(:advice_page_key) { :your_advice_page_key }
+  let(:advice_page_key) { :electricity_out_of_hours }
 
   # change to your variables
   let(:variables) do
     {
-      current_year_percent_of_target_relative: +0.18699995372972533,
-      current_year_unscaled_percent_of_target_relative: -0.4799985149375391,
-      current_year_kwh: 1284.7,
-      current_year_target_kwh: 2281.8825833333326,
-      unscaled_target_kwh_to_date: 2401.9816666666666,
-      tracking_start_date: '2024-01-01'
+      schoolday_open_percent: 0.2783819813845588,
+      schoolday_closed_percent: 0.3712268903038169,
+      holidays_percent: 0.21123782178479827,
+      weekends_percent: 0.13915330652682595,
+      community_percent: 0.0,
+      community_gbp: 0.0,
+      out_of_hours_gbp: 41347.98790211005,
+      potential_saving_gbp: 13006.849331677073,
+      rating: 0.0
     }
   end
 
-  # change to your alert type (there may be more than one!)
-  let(:alert_type) { create(:alert_type, class_name: 'AlertElectricityTargetAnnual') }
+  let(:alert_type) { create(:alert_type, class_name: 'AlertOutOfHoursElectricityUsage') }
   let(:alert_run) { create(:alert_generation_run, school: school) }
   let!(:report) { create(:report, key: key) }
 
   before do
     create(:advice_page, key: advice_page_key)
     create(:alert, school: school, alert_generation_run: alert_run, alert_type: alert_type, variables: variables)
+    create(:alert, school: school, alert_generation_run: alert_run,
+                   alert_type: create(:alert_type, class_name: 'AlertAdditionalPrioritisationData'),
+                   variables: { electricity_economic_tariff_changed_this_year: true })
   end
 
   context 'when viewing report' do
     before { visit "/comparisons/#{key}" }
 
     it_behaves_like 'a school comparison report' do
-      let(:title) { report.title }
-      let(:chart) { '#chart_comparison' } # there may be more than one chart!
-      let(:expected_school) { school }
-      let(:advice_page_path) { polymorphic_path([:insights, expected_school, :advice, advice_page_key]) }
-      let(:expected_table) do
-        [['School',
-          'Percent above or below target since target set',
-          'Percent above or below last year',
-          'kWh consumption since target set',
-          'Target kWh consumption',
-          'Last year kWh consumption',
-          'Start date for target'],
-         [school.name,
-          '+18.7%',
-          '-48%',
-          '1,280',
-          '2,280',
-          '2,400',
-          'Monday 1 Jan 2024'],
-         ["Notes\nIn school comparisons 'last year' is defined as this year to date."]
+      let(:expected_report) { report }
+    end
+
+    it_behaves_like 'a school comparison report with a table' do
+      let(:headers) do
+        ['School',
+         'School Day Open',
+         'School Day Closed',
+         'Holiday',
+         'Weekend',
+         'Community',
+         'Community usage cost',
+         'Last year out of hours cost',
+         'Saving if improve to exemplar (at latest tariff)'
         ]
       end
+
+      let(:expected_report) { report }
+      let(:expected_school) { school }
+      let(:advice_page_path) { polymorphic_path([:insights, expected_school, :advice, advice_page_key]) }
+
+      let(:expected_table) do
+        [headers,
+         ["#{school.name} (*5)", '27.8&percnt;', '37.1&percnt;', '21.1&percnt;', '13.9&percnt;', '0&percnt;', '0p', '£41,300', '£13,000'],
+         ["Notes\n(*5) The tariff has changed during the last year for this school. Savings are calculated using the latest tariff but other £ values are calculated using the relevant tariff at the time\nIn school comparisons 'last year' is defined as this year to date."]
+        ]
+      end
+
+      let(:expected_csv) do
+        [headers,
+         [school.name, '0.278', '0.371', '0.211', '0.139', '0', '0', '41,300', '13,000']
+        ]
+      end
+    end
+
+    it_behaves_like 'a school comparison report with a chart' do
+      let(:chart) { '#chart_comparison' }
     end
   end
 end
