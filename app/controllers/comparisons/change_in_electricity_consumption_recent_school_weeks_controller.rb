b3a7a216b9dcc5fc5c9bdd98822ebc42232e4d4e
Tristan Hill <t23+git@eaux.uk>
2024-03-08 19:06:47 +0000

    more

--- app/controllers/comparisons/change_in_electricity_consumption_recent_school_weeks_controller.rb
+++ app/controllers/comparisons/change_in_electricity_consumption_recent_school_weeks_controller.rb
@@ -4,6 +4,13 @@ module Comparisons
   class ChangeInElectricityConsumptionRecentSchoolWeeksController < BaseController
     private
 
+    def headers
+      [t('analytics.benchmarking.configuration.column_headings.school'),
+       t('analytics.benchmarking.configuration.column_headings.change_pct'),
+       t('analytics.benchmarking.configuration.column_headings.change_£current'),
+       t('analytics.benchmarking.configuration.column_headings.change_kwh')]
+    end
+
     def key
       :change_in_electricity_consumption_recent_school_weeks
     end
--- app/views/comparisons/change_in_electricity_consumption_recent_school_weeks/_table.csv.ruby
+++ app/views/comparisons/change_in_electricity_consumption_recent_school_weeks/_table.csv.ruby
@@ -1,11 +1,5 @@
 CSV.generate do |csv|
-  csv << [
-    t('analytics.benchmarking.configuration.column_headings.school'),
-    t('analytics.benchmarking.configuration.column_headings.change_pct'),
-    t('analytics.benchmarking.configuration.column_headings.change_£current'),
-    t('analytics.benchmarking.configuration.column_headings.change_kwh')
-  ]
-
+  csv << @headers
   @results.each do |result|
     csv << [result.school.name,
             format_unit(result.difference_percent * 100, Float, true, :benchmark),
