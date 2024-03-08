d2bdce25e1fee003c447ff2a83270d7e7f3c451c
Leigh Dodds <leigh@ldodds.com>
2024-03-08 11:50:34 +0000

    Switch all current comparison tables to use comparison component (#3481)

--- app/controllers/comparisons/electricity_consumption_during_holiday_controller.rb
+++ app/controllers/comparisons/electricity_consumption_during_holiday_controller.rb
@@ -4,6 +4,15 @@ module Comparisons
   class ElectricityConsumptionDuringHolidayController < BaseController
     private
 
+    def headers
+      [
+        t('analytics.benchmarking.configuration.column_headings.school'),
+        t('analytics.benchmarking.configuration.column_headings.projected_usage_by_end_of_holiday'),
+        t('analytics.benchmarking.configuration.column_headings.holiday_usage_to_date'),
+        t('analytics.benchmarking.configuration.column_headings.holiday')
+      ]
+    end
+
     def key
       :electricity_consumption_during_holiday
     end
