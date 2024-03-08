869d6894ac2e403d6c9cb68f6a78d792f308c62c
Tristan Hill <t23+git@eaux.uk>
2024-03-08 18:20:11 +0000

    more

--- app/models/comparison/change_in_electricity_holiday_consumption_previous_holiday.rb
+++ app/models/comparison/change_in_electricity_holiday_consumption_previous_holiday.rb
@@ -4,17 +4,21 @@
 #
 # Table name: change_in_electricity_holiday_consumption_previous_holidays
 #
-#  alert_generation_run_id  :bigint(8)
-#  difference_gbpcurrent    :float
-#  difference_kwh           :float
-#  difference_percent       :float
-#  id                       :bigint(8)
-#  name_of_current_period   :text
-#  name_of_previous_period  :text
-#  pupils_changed           :boolean
-#  school_id                :bigint(8)
-#  tariff_has_changed       :boolean
-#  truncated_current_period :boolean
+#  alert_generation_run_id    :bigint(8)
+#  current_period_end_date    :date
+#  current_period_start_date  :date
+#  current_period_type        :text
+#  difference_gbpcurrent      :float
+#  difference_kwh             :float
+#  difference_percent         :float
+#  id                         :bigint(8)
+#  previous_period_end_date   :date
+#  previous_period_start_date :date
+#  previous_period_type       :text
+#  pupils_changed             :boolean
+#  school_id                  :bigint(8)
+#  tariff_has_changed         :boolean
+#  truncated_current_period   :boolean
 #
 module Comparison
   class ChangeInElectricityHolidayConsumptionPreviousHoliday < Comparison::View
