3eb13b840f09aab4a69febd44b2801faf0342b75
Leigh Dodds <leigh@ldodds.com>
2024-03-08 17:20:51 +0000

    Update to Version 2

--- app/assets/javascripts/table_sorting.js
+++ app/assets/javascripts/table_sorting.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
     'order': [],        // Default do not sort
     'paging': false,
     'searching': false, // Switch off search field
-    'info': false       // Switch off the summary of rows at the bottom of the page
+    'info': false,       // Switch off the summary of rows at the bottom of the page
+    'orderCellsTop': false
   });
 
   $('.table-large').DataTable({
