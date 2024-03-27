6da45352d12e3df9e40bdb5f94ab37d0f14f94cc
Deb Bassett <deb@urbanwide.com>
2024-03-27 15:44:19 +0000

    put footer back in

--- app/components/comparison_table_component.rb
+++ app/components/comparison_table_component.rb
@@ -31,6 +31,7 @@ class ComparisonTableComponent < ViewComponent::Base
 
   renders_many :footnotes, 'ComparisonTableComponent::FootnoteComponent'
   renders_many :notes, 'ComparisonTableComponent::NoteComponent'
+  renders_one :footer
 
   def before_render
     collect_references
