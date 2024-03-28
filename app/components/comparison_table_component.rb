28ec61296df4e3e34be6775b29de60285d54d57b
Deb Bassett <deb@urbanwide.com>
2024-03-28 16:04:43 +0000

    Don't need this

--- app/components/comparison_table_component.rb
+++ app/components/comparison_table_component.rb
@@ -200,7 +200,7 @@ class ComparisonTableComponent < ViewComponent::Base
     end
 
     def call
-      tag.sup("[#{label}]", tabindex: 0, title: title, data: { trigger: 'focus', toggle: 'popover', content: "#{label}: #{description % params}" })
+      tag.sup("[#{label}]", tabindex: 0, title: title, data: { trigger: 'focus', toggle: 'popover', content: "#{label}: #{description}" })
     end
   end
 
