3d1ad40557b3e3a8450b07c7a0ef046fb1d8f208
Leigh Dodds <leigh@ldodds.com>
2024-04-10 10:09:23 +0100

    Review feedback, plus clarify essential cookies

--- app/assets/javascripts/cookie_banner.js
+++ app/assets/javascripts/cookie_banner.js
@@ -1,11 +1,16 @@
 "use strict"
 
+const COOKIE_BANNER_ID = 'cookie-banner';
+const COOKIE_PREFERENCE_NAME = 'cookie_preference';
+const ACCEPTED = 'Accepted';
+const REJECTED = 'Rejected';
+
 function initializeCookieBanner() {
- let cookieStatus = Cookies.get('cookie_preference');
+ let cookieStatus = Cookies.get(COOKIE_PREFERENCE_NAME);
  if(cookieStatus === undefined) {
    // user has not accepted or refused; or preference cookie expired
    showCookieBanner();
- } else if (cookieStatus == 'Accepted') {
+ } else if (cookieStatus == ACCEPTED) {
    // preference cookie is current and user has accepted
    setAnalyticsConsent('granted');
  } else {
@@ -15,30 +20,30 @@ function initializeCookieBanner() {
 }
 
 function showCookieBanner() {
- let cookieBanner = document.getElementById("cookie-banner");
- cookieBanner.style.display = "block";
+ let cookieBanner = document.getElementById(COOKIE_BANNER_ID);
+ cookieBanner.style.display = 'block';
  window.acceptCookies = acceptCookies;
  window.rejectCookies = rejectCookies;
 }
 
 function hideBanner() {
-  let cookieBanner = document.getElementById("cookie-banner");
-  cookieBanner.style.display = "none";
+  let cookieBanner = document.getElementById(COOKIE_BANNER_ID);
+  cookieBanner.style.display = 'none';
 }
 
 function setPreferenceCookie(status) {
-  Cookies.remove('cookie_preference');
-  Cookies.set('cookie_preference', status, { expires: 365 });
+  Cookies.remove(COOKIE_PREFERENCE_NAME);
+  Cookies.set(COOKIE_PREFERENCE_NAME, status, { expires: 365 });
 }
 
 function acceptCookies() {
- setPreferenceCookie('Accepted');
+ setPreferenceCookie(ACCEPTED);
  setAnalyticsConsent('granted');
  hideBanner();
 }
 
 function rejectCookies() {
- setPreferenceCookie('Rejected');
+ setPreferenceCookie(REJECTED);
  setAnalyticsConsent('denied');
  hideBanner();
 }
