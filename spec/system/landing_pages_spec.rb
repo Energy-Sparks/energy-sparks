5541bcc95ec627d35294a2c5227a070ad835e10d
Leigh Dodds <leigh@ldodds.com>
2024-04-10 16:20:35 +0100

    Pass UTM parameters through workflow

--- spec/system/landing_pages_spec.rb
+++ spec/system/landing_pages_spec.rb
@@ -35,64 +35,122 @@ describe 'landing pages', type: :system do
     end
 
     it 'has book demo link' do
-      expect(page).to have_link('Book a demo')
+      expect(page).to have_link('Book a demo', href: book_demo_campaigns_path)
     end
 
     it 'has find our more link' do
-      expect(page).to have_link('Find out more')
+      expect(page).to have_link('Find out more', href: more_information_campaigns_path)
+    end
+
+    context 'with UTM parameters' do
+      let(:expected_params) do
+        {
+          utm_medium: 'email',
+          utm_campaign: 'test',
+          utm_source: 'somewhere'
+        }
+      end
+
+      before do
+        visit find_out_more_campaigns_path(expected_params)
+      end
+
+      it 'adds campaign parameters to link' do
+        expect(page).to have_link('Book a demo', href: book_demo_campaigns_path(expected_params))
+        expect(page).to have_link('Find out more', href: more_information_campaigns_path(expected_params))
+      end
     end
   end
 
+  def fill_in_form
+    fill_in('First Name', with: first_name)
+    fill_in('Last Name', with: last_name)
+    fill_in('Job Title', with: job_title)
+    fill_in('Organisation', with: organisation)
+    select(org_type, from: :contact_org_type)
+    fill_in('Email Address', with: email)
+    fill_in('Telephone Number', with: tel)
+    check(:contact_consent)
+  end
+
   context 'when completing the more information workflow' do
+    let(:expected_utm_params) { {} }
+
     before do
-      visit find_out_more_campaigns_path
+      visit find_out_more_campaigns_path(expected_utm_params)
       click_link('Find out more', :match => :first)
     end
 
-    it 'handles form submission correctly' do
-      expect(page).to have_content(I18n.t('campaigns.more_information.title'))
+    context 'with no UTM params' do
+      it 'handles form submission correctly' do
+        expect(page).to have_content(I18n.t('campaigns.more_information.title'))
+        fill_in_form
+        expect(CampaignContactHandlerJob).to receive(:perform_later).with(:more_information, expected_contact)
+        allow(CampaignContactHandlerJob).to receive(:perform_later).and_return(true)
+        click_on('Next')
+        expect(page).to have_content(I18n.t('campaigns.more_info_final.title'))
+      end
+    end
 
-      fill_in('First Name', with: first_name)
-      fill_in('Last Name', with: last_name)
-      fill_in('Job Title', with: job_title)
-      fill_in('Organisation', with: organisation)
-      select(org_type, from: :contact_org_type)
-      fill_in('Email Address', with: email)
-      fill_in('Telephone Number', with: tel)
-      check(:contact_consent)
+    context 'with UTM params' do
+      let(:expected_utm_params) do
+        {
+          utm_medium: 'email',
+          utm_campaign: 'test',
+          utm_source: 'somewhere'
+        }
+      end
 
-      expect(CampaignContactHandlerJob).to receive(:perform_later).with(:more_information, expected_contact)
-      allow(CampaignContactHandlerJob).to receive(:perform_later).and_return(true)
-
-      click_on('Next')
-      expect(page).to have_content(I18n.t('campaigns.more_info_final.title'))
+      it 'passes params to final page' do
+        expect(page).to have_content(I18n.t('campaigns.more_information.title'))
+        fill_in_form
+        allow(CampaignContactHandlerJob).to receive(:perform_later).and_return(true)
+        click_on('Next')
+        expect(page).to have_content(I18n.t('campaigns.more_info_final.title'))
+        params = Rack::Utils.parse_nested_query(URI.parse(page.current_url).query).symbolize_keys!
+        expect(params).to include(expected_utm_params)
+      end
     end
   end
 
   context 'when completing the book demo workflow' do
+    let(:expected_utm_params) { {} }
+
     before do
-      visit find_out_more_campaigns_path
+      visit find_out_more_campaigns_path(expected_utm_params)
       click_link('Book a demo', :match => :first)
     end
 
-    it 'handles form submission correctly' do
-      expect(page).to have_content(I18n.t('campaigns.book_demo.title'))
+    context 'with no UTM params' do
+      it 'handles form submission correctly' do
+        expect(page).to have_content(I18n.t('campaigns.book_demo.title'))
+        fill_in_form
+        expect(CampaignContactHandlerJob).to receive(:perform_later).with(:book_demo, expected_contact)
+        allow(CampaignContactHandlerJob).to receive(:perform_later).and_return(true)
+        click_on('Next')
+        expect(page).to have_content(I18n.t('campaigns.book_demo_final.title'))
+        expect(page).to have_css('.calendly-inline-widget')
+      end
+    end
 
-      fill_in('First Name', with: first_name)
-      fill_in('Last Name', with: last_name)
-      fill_in('Job Title', with: job_title)
-      fill_in('Organisation', with: organisation)
-      select(org_type, from: :contact_org_type)
-      fill_in('Email Address', with: email)
-      fill_in('Telephone Number', with: tel)
-      check(:contact_consent)
+    context 'with UTM params' do
+      let(:expected_utm_params) do
+        {
+          utm_medium: 'email',
+          utm_campaign: 'test',
+          utm_source: 'somewhere'
+        }
+      end
 
-      expect(CampaignContactHandlerJob).to receive(:perform_later).with(:book_demo, expected_contact)
-      allow(CampaignContactHandlerJob).to receive(:perform_later).and_return(true)
-
-      click_on('Next')
-      expect(page).to have_content(I18n.t('campaigns.book_demo_final.title'))
-      expect(page).to have_css('.calendly-inline-widget')
+      it 'passes params to final page' do
+        expect(page).to have_content(I18n.t('campaigns.book_demo.title'))
+        fill_in_form
+        allow(CampaignContactHandlerJob).to receive(:perform_later).and_return(true)
+        click_on('Next')
+        expect(page).to have_content(I18n.t('campaigns.book_demo_final.title'))
+        params = Rack::Utils.parse_nested_query(URI.parse(page.current_url).query).symbolize_keys!
+        expect(params).to include(expected_utm_params)
+      end
     end
   end
 
