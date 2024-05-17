c8d4a9927d39ec4a06eec4bddbef88223e121ad7
Deb Bassett <deb@urbanwide.com>
2024-05-17 12:29:06 +0100

    Moves transport out of packs. Can use erb colours here and also is a good move for Rails 7

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.precompile += %w( application_mailer.scss )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( transport_surveys transport_surveys/charts.js )
