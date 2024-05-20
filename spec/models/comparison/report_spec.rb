b9ed0f7c8216107761c2699f1b6aa9f70f3f8ea1
Deb Bassett <deb@urbanwide.com>
2024-05-20 17:07:54 +0100

    Auto generate comparison report key

--- spec/models/comparison/report_spec.rb
+++ spec/models/comparison/report_spec.rb
@@ -3,21 +3,37 @@ require 'rails_helper'
 RSpec.describe Comparison::Report, type: :model do
   describe 'validations' do
     context 'with standard validations' do
-      subject(:report) { create :report }
+      subject(:report) { build :report }
 
       it { expect(report).to be_valid }
-      it { expect(report).to validate_presence_of(:key) }
       it { expect(report).to validate_uniqueness_of(:key) }
       it { expect(report).to validate_presence_of(:title) }
       it { expect(report).not_to validate_presence_of(:introduction) }
       it { expect(report).not_to validate_presence_of(:notes) }
       it { expect(report).not_to validate_presence_of(:reporting_period) }
     end
+  end
 
-    context 'with relationships' do
-      subject(:report) { create :report }
+  describe 'relationships' do
+    subject(:report) { create :report }
 
-      it { expect(report).to belong_to(:report_group).optional(false) }
+    it { expect(report).to belong_to(:report_group).optional(false) }
+  end
+
+  describe '#key' do
+    context 'when there is a title' do
+      subject(:report) { create :report, title: 'Lovely title' }
+
+      it 'builds a key on create using :title' do
+        expect(report.key).to eq('lovely_title')
+      end
+    end
+
+    context 'when there is no title' do
+      subject(:report) { build :report, title: '' }
+
+      it { expect(report).to validate_presence_of(:key) }
+      it { expect(report).to validate_presence_of(:title) }
     end
   end
 
