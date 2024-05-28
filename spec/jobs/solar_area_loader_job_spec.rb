ab61a3b56b729ddabdf6797aa16643e575343a04
Leigh Dodds <leigh@ldodds.com>
2024-05-28 14:04:12 +0100

    Add some specs

--- spec/jobs/solar_area_loader_job_spec.rb
+++ spec/jobs/solar_area_loader_job_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe SolarAreaLoaderJob do
+  subject(:job) { SolarAreaLoaderJob.new }
+
   let(:area)          { create(:solar_pv_tuos_area) }
   let(:start_date)    { Date.yesterday - 2.years }
   let(:result)        { 'Imported xx records, Updated xx records' }
   let(:loader)        { double(DataFeeds::SolarPvTuosLoader, import_area: result) }
 
-  let(:job)           { SolarAreaLoaderJob.new }
+  describe '#priority' do
+    it_behaves_like 'a low priority job'
+  end
 
   describe '#perfom' do
     it 'requests 2 years data' do
