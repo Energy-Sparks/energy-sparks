7808bb3001649b39320b737decfc0bf8ca25ec1a
Tristan Hill <t23+git@eaux.uk>
2024-06-13 14:16:44 +0000

    more

--- .platform/hooks/prebuild/bundler.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# https://github.com/aws/elastic-beanstalk-roadmap/issues/139
-cd "$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)"
-bundle config set --local path /var/app/bundle
