39d087b670f6b1b6558ccb8fb97aa1815f3bd9a5
Tristan Hill <t23+git@eaux.uk>
2024-06-14 15:25:08 +0000

    more

--- app/services/activity_type_search_service.rb
+++ app/services/activity_type_search_service.rb
@@ -1,8 +1,9 @@
 class ActivityTypeSearchService
   def self.search(query, key_stages = [], subjects = [], locale = 'en')
     activity_types = ActivityType.search(query: query, locale: locale)
-    activity_types = activity_types.for_key_stages(key_stages).distinct(false) if key_stages.present?
-    activity_types = activity_types.for_subjects(subjects).distinct(false) if subjects.present?
+    activity_types = activity_types.for_key_stages(key_stages) if key_stages.present?
+    activity_types = activity_types.for_subjects(subjects) if subjects.present?
+    # binding.pry
     activity_types
   end
 end
