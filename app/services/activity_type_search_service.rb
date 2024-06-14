ddc67b2c3853f7e1a746f3c6caf920252ed2c75f
Tristan Hill <t23+git@eaux.uk>
2024-06-14 18:00:58 +0000

    more

--- app/services/activity_type_search_service.rb
+++ app/services/activity_type_search_service.rb
@@ -3,7 +3,6 @@ class ActivityTypeSearchService
     activity_types = ActivityType.search(query: query, locale: locale)
     activity_types = activity_types.for_key_stages(key_stages) if key_stages.present?
     activity_types = activity_types.for_subjects(subjects) if subjects.present?
-    # binding.pry
     activity_types
   end
 end
