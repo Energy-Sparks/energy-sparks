3121e5e2c5573aa52750c89e3742853aac8bbe2a
Leigh Dodds <leigh@ldodds.com>
2024-06-20 11:49:57 +0100

    Try removing

--- app/controllers/comparisons/holiday_and_term_controller.rb
+++ app/controllers/comparisons/holiday_and_term_controller.rb
@@ -12,35 +12,6 @@ module Comparisons
       @period_type_string = I18n.t('comparisons.period_types.periods')
     end
 
-    def headers(fuel: true, previous_period_unadjusted: false, holiday_name: false)
-      [
-        t('analytics.benchmarking.configuration.column_headings.school'),
-        fuel && t('analytics.benchmarking.configuration.column_headings.fuel'),
-        t('activerecord.attributes.school.activation_date'),
-        holiday_name && t('analytics.benchmarking.configuration.column_headings.most_recent_holiday'),
-        previous_period_unadjusted && t('comparisons.column_headings.previous_period_unadjusted'),
-        t('comparisons.column_headings.previous_period'),
-        t('comparisons.column_headings.current_period'),
-        t('analytics.benchmarking.configuration.column_headings.change_pct'),
-        t('comparisons.column_headings.previous_period'),
-        t('comparisons.column_headings.current_period'),
-        t('analytics.benchmarking.configuration.column_headings.change_pct'),
-        t('comparisons.column_headings.previous_period'),
-        t('comparisons.column_headings.current_period'),
-        t('analytics.benchmarking.configuration.column_headings.change_pct')
-      ].select(&:itself)
-    end
-
-    def colgroups(fuel: true, previous_period_unadjusted: false, holiday_name: false)
-      [
-        { label: '', colspan: fuel || holiday_name ? 3 : 2 },
-        { label: t('analytics.benchmarking.configuration.column_groups.kwh'),
-          colspan: previous_period_unadjusted ? 4 : 3 },
-        { label: t('analytics.benchmarking.configuration.column_groups.co2_kg'), colspan: 3 },
-        { label: t('analytics.benchmarking.configuration.column_groups.gbp'), colspan: 3 }
-      ]
-    end
-
     def key
       :holiday_and_term
     end
