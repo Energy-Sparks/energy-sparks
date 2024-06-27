e8022a883bbb0cfbda683b60d9f61ac46976dde1
Deb Bassett <deb@urbanwide.com>
2024-06-27 10:23:39 +0100

    Interim

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,7 @@
 //= link_tree ../images
 //= link application_assets.js
 //= link application.css
-//= link nav.css
-//= link navigation.css
+//= link redesign.css
 //= link application_mailer.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
