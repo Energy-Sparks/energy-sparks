894a59ec2b7036312e6e65e10052a5c231782486
Leigh Dodds <leigh@ldodds.com>
2024-07-03 10:54:10 +0100

    Add initial specs

--- app/controllers/schools/advice/base_meter_breakdown_controller.rb
+++ app/controllers/schools/advice/base_meter_breakdown_controller.rb
@@ -1,6 +1,8 @@
 module Schools
   module Advice
     class BaseMeterBreakdownController < AdviceBaseController
+      before_action :redirect_if_single_meter
+
       def insights
         @annual_usage_meter_breakdown = usage_service.annual_usage_meter_breakdown
         @meters_for_breakdown = sorted_meters_for_breakdown(@annual_usage_meter_breakdown)
@@ -12,6 +14,10 @@ module Schools
       end
 
       private
+
+      def redirect_if_single_meter
+        redirect_to school_advice_path(@school) if @school.meters.active.where(meter_type: fuel_type).count == 1
+      end
 
       def sorted_meters_for_breakdown(annual_usage_meter_breakdown)
         meters = @school.meters.where(mpan_mprn: annual_usage_meter_breakdown.meters).order(:mpan_mprn)
