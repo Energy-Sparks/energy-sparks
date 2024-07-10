b03fb37287e619a18a734d9edf5c54b2dd24c1c4
Deb Bassett <deb@urbanwide.com>
2024-07-10 11:16:57 +0100

    Time to bring in the new colours

--- config/initializers/colours.rb
+++ config/initializers/colours.rb
@@ -4,6 +4,11 @@ module Colours
   DARK_YELLOW = '#ffde4d'.freeze
   LIGHT_YELLOW = '#fff9b2'.freeze
 
+  # NEW YELLOWS (using in nav)
+  YELLOW_LIGHT = '#fcdc8b'.freeze
+  YELLOW_DARK = '#772d10'.freeze
+  YELLOW_VERY_DARK = '#441504'.freeze
+
   # ORANGES
   DARK_ORANGE = '#ff4500'.freeze
   LIGHT_ORANGE = '#ffac21'.freeze
@@ -14,7 +19,12 @@ module Colours
   MID_BLUE = '#3bc0f0'.freeze
   LIGHT_BLUE = '#97e6fc'.freeze
   LIGHTER_LIGHT_BLUE = '#cbf4ff'.freeze
-  BLUEY_WHITE = '#FCFFFF'.freeze
+  BLUEY_WHITE = '#fcffff'.freeze
+
+  # NEW BLUES (using in nav)
+  BLUE_VERY_DARK = '#192a52'.freeze
+  BLUE_DARK = '#334375'.freeze
+  BLUE_MEDIUM = '#cbe4fc'.freeze
 
   # GREENS
   GREEN = '#5cb85c'.freeze
