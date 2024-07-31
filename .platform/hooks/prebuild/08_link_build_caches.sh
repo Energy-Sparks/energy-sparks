63501a8af844fb0fea01b9420f0daebcc2e4a163
Tristan Hill <t23+git@eaux.uk>
2024-07-31 10:47:48 +0000

    cache bundle files on s3 for new instance

--- .platform/hooks/prebuild/08_link_build_caches.sh
+++ .platform/hooks/prebuild/08_link_build_caches.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd "$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)"
+if ! [ -d /var/app/bundle ]; then
+  aws s3 cp s3://elasticbeanstalk-eu-west-2-110304303563/bundle-$(/opt/elasticbeanstalk/bin/get-config container -k environment_name).tar.gz - | tar xzC /
+fi
 mkdir -p /var/app/bundle
+
+cd "$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)"
 ln -s /var/app/bundle vendor
 
 mkdir -p tmp/amr_files_bucket
