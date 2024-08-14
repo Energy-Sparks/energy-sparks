ee0fab616ff985a0b57a58ab9f430408e8369164
Leigh Dodds <leigh@ldodds.com>
2024-08-14 17:52:35 +0100

    Fix helper spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,6 +84,7 @@ RSpec.configure do |config|
   config.include ViewComponent::SystemTestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
   config.include Devise::Test::ControllerHelpers, type: :component
+  config.include Devise::Test::ControllerHelpers, type: :helper
   config.before(:each, type: :component) do
     @request = vc_test_controller.request
   end
