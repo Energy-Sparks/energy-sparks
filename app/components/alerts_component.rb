2beb34b725eba6b8d66a3441657056331a16faf0
Leigh Dodds <leigh@ldodds.com>
2024-08-14 18:52:49 +0100

    Initial components for new school dashboards (#3807)

--- app/components/alerts_component.rb
+++ app/components/alerts_component.rb
@@ -1,28 +1,36 @@
 # frozen_string_literal: true
 
-class AlertsComponent < ViewComponent::Base
-  attr_reader :school, :show_links, :show_icons
+class AlertsComponent < PromptListComponent
+  attr_reader :school, :show_links, :show_icons, :user
 
   include AdvicePageHelper
   include ApplicationHelper
 
-  def initialize(school:, dashboard_alerts:, alert_types:, show_links: true, show_icons: true)
+  def initialize(school:, dashboard_alerts:, alert_types: nil, audience: :adult, show_links: true, show_icons: true, id: nil, classes: '', user: nil)
+    super(id: id, classes: classes)
     @school = school
     @dashboard_alerts = dashboard_alerts
     @alert_types = alert_types
     @show_links = show_links
     @show_icons = show_icons
+    @audience = audience
+    @user = user
   end
 
   def alerts
     @alerts ||= dashboard_alerts_for(@alert_types)
   end
 
+  def render?
+    prompts? || alerts.any?
+  end
+
   def content_field
-    :management_dashboard_title
+    @audience == :adult ? :management_dashboard_title : :pupil_dashboard_title
   end
 
   def dashboard_alerts_for(alert_types)
+    return @dashboard_alerts unless alert_types.present?
     alert_type_ids = alert_types.map(&:id)
     @dashboard_alerts.select { |dashboard_alert| alert_type_ids.include?(dashboard_alert.alert.alert_type_id) }
   end
