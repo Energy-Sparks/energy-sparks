d2ee7e5b4f241eaa5201093e421e6a12b0ac8415
Leigh Dodds <leigh@ldodds.com>
2024-08-15 17:21:00 +0100

    Add new table styling

--- app/components/energy_summary_table_component.rb
+++ app/components/energy_summary_table_component.rb
@@ -6,8 +6,7 @@ class EnergySummaryTableComponent < ApplicationComponent
 
   renders_one :footer
 
-  def initialize(school:, show_savings: true, show_title: true, id: nil, classes: '', user: nil)
-    id = id || 'management-overview-table'
+  def initialize(school:, show_savings: true, show_title: true, id: 'energy-summary', classes: '', user: nil)
     super(id: id, classes: classes)
     @user = user
     @school = school
@@ -34,5 +33,26 @@ class EnergySummaryTableComponent < ApplicationComponent
   def render?
     return false unless overview_data.present?
     @user&.admin? || @school.data_enabled?
+  end
+
+  def col(size = 1)
+    "col-#{size}" if Flipper.enabled?(:new_dashboards_2024, user)
+  end
+
+  def no_data_message_class(data)
+    return data.message_class unless Flipper.enabled?(:new_dashboards_2024, user)
+    data.message_class == 'old-data' ? data.message_class : 'future-data'
+  end
+
+  def footer_link
+    if Flipper.enabled?(:new_dashboards_2024, user)
+      I18n.t('advice_pages.how_have_we_analysed_your_data.link_title')
+    else
+      "#{I18n.t('schools.show.more_information')} #{fa_icon('info-circle')}".html_safe
+    end
+  end
+
+  def footer_classes
+    Flipper.enabled?(:new_dashboards_2024, user) ? 'table-caption' : 'text-right management-overview-caption'
   end
 end
