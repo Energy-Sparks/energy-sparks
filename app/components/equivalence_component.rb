ede3d251c3aada9cf94cb72d24a5891c8e5130d1
Leigh Dodds <leigh@ldodds.com>
2024-08-22 12:09:45 +0100

    Make horizontal style responsive

--- app/components/equivalence_component.rb
+++ app/components/equivalence_component.rb
@@ -1,5 +1,10 @@
-# Handles formatting of an equivalence created by interpolating school Equivalence data
-# with formatting in EquivalenceType.
+# Handles layout of an block showing a school equivalence
+#
+# Supports two layouts: a "horizontal" layout which is the original design
+# with basically a two column view
+#
+# The vertical layout has the content stacked, so suitable for making a
+# two column view of equivalences
 class EquivalenceComponent < ApplicationComponent
   attr_reader :image_name
 
@@ -7,9 +12,19 @@ class EquivalenceComponent < ApplicationComponent
   renders_one :title
   renders_one :header
 
-  def initialize(image_name:, id: nil, classes: '')
+  def initialize(image_name:, show_fuel: false, layout: :horizontal, id: nil, classes: '')
     super(id: id, classes: classes)
     @image_name = image_name
+    @show_fuel = show_fuel
+    @layout = layout.to_sym
+  end
+
+  def show_fuel?
+    @show_fuel
+  end
+
+  def horizontal?
+    @layout == :horizontal
   end
 
   def show_image?
