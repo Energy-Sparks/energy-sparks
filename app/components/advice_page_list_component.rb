a7e10a6c45f3c4b900a7941de62da055b09ff6ae
Leigh Dodds <leigh@ldodds.com>
2024-09-02 11:15:34 +0100

    Initial changes

---
+++ app/components/advice_page_list_component.rb
@@ -0,0 +1,19 @@
+class AdvicePageListComponent < ApplicationComponent
+  attr_reader :school
+
+  include ApplicationHelper
+  include AdvicePageHelper
+
+  def initialize(school:, id: nil, classes: '')
+    super(id: id, classes: classes)
+    @school = school
+  end
+
+  def advice_page_benchmarks
+    @advice_page_benchmarks ||= @school.advice_page_school_benchmarks
+  end
+
+  def advice_pages
+    @advice_pages ||= AdvicePage.all
+  end
+end
