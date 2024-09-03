3e14b0c7ba0bd7931a8f05245d99be33aa353081
Leigh Dodds <leigh@ldodds.com>
2024-09-03 17:38:11 +0100

    Start assembling

--- app/controllers/schools/advice_controller.rb
+++ app/controllers/schools/advice_controller.rb
@@ -3,7 +3,9 @@ module Schools
     include NonPublicSchools
     include DashboardAlerts
     include DashboardPriorities
+    include DashboardEnergyCharts
     include SchoolInactive
+    include SchoolAggregation
 
     load_resource :school
     skip_before_action :authenticate_user!
@@ -15,7 +17,24 @@ module Schools
     before_action :set_breadcrumbs
 
     def show
-      @advice_page_benchmarks = @school.advice_page_school_benchmarks
+      if Flipper.enabled?(:new_dashboards_2024, current_user)
+        @overview_charts = setup_energy_overview_charts(@school.configuration)
+        @audience = :adult
+        if can_benchmark_electricity?
+          @electricity_annual_usage = electricity_usage_service.annual_usage
+          @electricity_benchmarked_usage = electricity_usage_service.benchmark_usage
+        end
+
+        if can_benchmark_gas?
+          @gas_annual_usage = gas_usage_service.annual_usage
+          @gas_benchmarked_usage = gas_usage_service.benchmark_usage
+        end
+
+        render :new_show, layout: 'dashboards'
+      else
+        @advice_page_benchmarks = @school.advice_page_school_benchmarks
+        render :show
+      end
     end
 
     def priorities
@@ -27,6 +46,26 @@ module Schools
     end
 
     private
+
+    def can_benchmark_electricity?
+      @school.has_electricity? && electricity_usage_service.enough_data?
+    end
+
+    def can_benchmark_gas?
+      @school.has_gas? && gas_usage_service.enough_data?
+    end
+
+    def gas_usage_service
+      @gas_usage_service ||= usage_service(:gas)
+    end
+
+    def electricity_usage_service
+      @electricity_usage_service ||= usage_service(:electricity)
+    end
+
+    def usage_service(fuel_type)
+      Schools::Advice::LongTermUsageService.new(@school, aggregate_school, fuel_type)
+    end
 
     def set_tab_name
       @tab = action_name.to_sym
