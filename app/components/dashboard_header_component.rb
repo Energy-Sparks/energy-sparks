8b1041fb572e39a546a0a4727dc38a34dde75cef
Leigh Dodds <leigh@ldodds.com>
2024-09-05 17:05:42 +0100

    Rework dashboard header component

--- app/components/dashboard_header_component.rb
+++ app/components/dashboard_header_component.rb
@@ -1,9 +1,21 @@
 class DashboardHeaderComponent < ApplicationComponent
-  attr_reader :school
+  attr_reader :school, :title, :intro
 
-  def initialize(school:, audience: :adult, id: nil, classes: '')
+  def initialize(school:,
+                 audience: :adult,
+                 title: 'schools.dashboards.header.title',
+                 intro: 'schools.dashboards.header.intro',
+                 show_school: true,
+                 id: nil, classes: '')
     super(id: id, classes: "#{classes} #{audience}")
     @school = school
+    @title = title
+    @intro = intro
     @audience = audience
+    @show_school = show_school
+  end
+
+  def show_school?
+    @show_school
   end
 end
