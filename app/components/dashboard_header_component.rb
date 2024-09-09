3ca683b49ac8c7ec3459fb6342ac41a0468bd017
Leigh Dodds <leigh@ldodds.com>
2024-09-09 09:48:24 +0100

    Rebuild priority actions and recent alerts page (#3866)

--- app/components/dashboard_header_component.rb
+++ app/components/dashboard_header_component.rb
@@ -1,9 +1,23 @@
 class DashboardHeaderComponent < ApplicationComponent
-  attr_reader :school
+  attr_reader :school, :title, :intro
 
-  def initialize(school:, audience: :adult, id: nil, classes: '')
+  # i18n-tasks-use t('schools.dashboards.header.title')
+  # i18n-tasks-use t('schools.dashboards.header.intro')
+  def initialize(school:,
+                 audience: :adult,
+                 title: 'schools.dashboards.header.title',
+                 intro: 'schools.dashboards.header.intro',
+                 show_school: true,
+                 id: nil, classes: '')
     super(id: id, classes: "#{classes} #{audience}")
     @school = school
+    @title = title
+    @intro = intro
     @audience = audience
+    @show_school = show_school
+  end
+
+  def show_school?
+    @show_school
   end
 end
