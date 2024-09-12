f4a16e35c5ac0186b1d9b958146f62ef3057e83e
Leigh Dodds <leigh@ldodds.com>
2024-09-12 13:39:57 +0100

    Fix iteration

---
+++ app/controllers/pupils/public_displays_controller.rb
@@ -0,0 +1,72 @@
+module Pupils
+  class PublicDisplaysController < ApplicationController
+    load_and_authorize_resource :school
+
+    include SchoolAggregation
+    include ActionView::Helpers::TagHelper
+    include ActionView::Context
+
+    skip_before_action :authenticate_user!
+    before_action :check_aggregated_school_in_cache
+
+    layout 'public_displays'
+
+    def index
+      render 'index', layout: 'application'
+    end
+
+    def equivalences
+      equivalences = equivalences_for_meter_types([params[:fuel_type].to_sym])
+      @equivalence_content = equivalences.sample
+    end
+
+    def charts
+    end
+
+    private
+
+    def equivalences_for_meter_types(meter_types)
+      @school.data_enabled? ? setup_equivalences(meter_types) : default_equivalences(meter_types)
+    end
+
+    def setup_equivalences(meter_types = :all)
+      equivalence_data = Equivalences::RelevantAndTimely.new(@school).equivalences(meter_types: meter_types)
+
+      equivalence_data.shuffle.map do |equivalence|
+        TemplateInterpolation.new(
+          equivalence.content_version,
+          with_objects: { equivalence_type: equivalence.content_version.equivalence_type },
+        ).interpolate(
+          :equivalence,
+          with: equivalence.formatted_variables
+        )
+      end
+    end
+
+    # Creates a list of default equivalences with the consumption for an "average" school.
+    # Objects in the returned array has equivalent structure to that returned by `setup_equivalences`
+    #
+    # TODO: these could later be moved into the database to allow them to be managed better
+    def default_equivalences(meter_types = :all)
+      scope = [:pupils, :default_equivalences]
+      all_defaults = [
+        { meter_type: :electricity, avg: 'equivalence_1.measure_html', title: 'equivalence_1.equivalence', img: 'kettle' },
+        { meter_type: :electricity, avg: 'equivalence_2.measure_html', title: 'equivalence_2.equivalence', img: 'onshore_wind_turbine' },
+        { meter_type: :gas, avg: 'equivalence_3.measure_html', title: 'equivalence_3.equivalence', img: 'tree' },
+        { meter_type: :gas, avg: 'equivalence_4.measure_html', title: 'equivalence_4.equivalence', img: 'meal' },
+        { meter_type: :gas, avg: 'equivalence_5.measure_html', title: 'equivalence_5.equivalence', img: 'house' }
+      ].map do |equivalence_config|
+        default_equivalence = ActiveSupport::OrderedOptions.new
+        default_equivalence.equivalence = content_tag(:div, I18n.t(equivalence_config[:avg], scope: scope).html_safe)
+        default_equivalence.equivalence = default_equivalence.equivalence + content_tag(:h3,
+                                                                                        I18n.t(equivalence_config[:title],
+                                                                                        scope: scope).html_safe)
+        default_equivalence.equivalence_type = ActiveSupport::OrderedOptions.new
+        default_equivalence.equivalence_type.meter_type = equivalence_config[:meter_type]
+        default_equivalence.equivalence_type.image_name = equivalence_config[:img]
+        default_equivalence
+      end
+      meter_types == :all ? all_defaults : all_defaults.select {|e| meter_types.include?(e.equivalence_type.meter_type)}
+    end
+  end
+end
