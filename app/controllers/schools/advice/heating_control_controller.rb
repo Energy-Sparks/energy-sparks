e673d2e4fb3477eb8bd6612f7c7ca1c45b0db943
Tristan Hill <t23+git@eaux.uk>
2024-09-16 16:33:06 +0000

    more

--- app/controllers/schools/advice/heating_control_controller.rb
+++ app/controllers/schools/advice/heating_control_controller.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 module Schools
   module Advice
     class HeatingControlController < AdviceBaseController
-      before_action :load_dashboard_alerts, only: [:insights, :analysis]
-      before_action :set_no_how_we_analysed_modal
+      before_action :load_dashboard_alerts, only: %i[insights analysis]
+      before_action :set_no_how_we_analysed_modal, only: %i[insights analysis]
 
       def insights
         @last_week_start_times = heating_control_service.last_week_start_times
         @estimated_savings = heating_control_service.estimated_savings
         @percentage_of_annual_gas = heating_control_service.percentage_of_annual_gas
         @enough_data_for_seasonal_analysis = heating_control_service.enough_data_for_seasonal_analysis?
-        if @enough_data_for_seasonal_analysis
-          @seasonal_analysis = heating_control_service.seasonal_analysis
-          @warm_weather_on_days_rating = heating_control_service.warm_weather_on_days_rating
-          @benchmark_warm_weather_days = heating_control_service.benchmark_warm_weather_days
-        end
+        return unless @enough_data_for_seasonal_analysis
+
+        @seasonal_analysis = heating_control_service.seasonal_analysis
+        @warm_weather_on_days_rating = heating_control_service.warm_weather_on_days_rating
+        @benchmark_warm_weather_days = heating_control_service.benchmark_warm_weather_days
       end
 
       def analysis
@@ -29,14 +31,14 @@ module Schools
         end
 
         @multiple_meters = heating_control_service.multiple_meters?
-        if @multiple_meters
-          @meter_selection = Charts::MeterSelection.new(@school,
-                                                        aggregate_school,
-                                                        advice_page_fuel_type,
-                                                        filter: :non_heating_only?,
-                                                        date_window: 363,
-                                                        include_whole_school: false)
-        end
+        return unless @multiple_meters
+
+        @meter_selection = Charts::MeterSelection.new(@school,
+                                                      aggregate_school,
+                                                      advice_page_fuel_type,
+                                                      filter: :non_heating_only?,
+                                                      date_window: 363,
+                                                      include_whole_school: false)
       end
 
       private
--- app/controllers/schools/advice/thermostatic_control_controller.rb
+++ app/controllers/schools/advice/thermostatic_control_controller.rb
@@ -3,7 +3,7 @@
 module Schools
   module Advice
     class ThermostaticControlController < AdviceBaseController
-      before_action :set_no_how_we_analysed_modal
+      before_action :set_no_how_we_analysed_modal, only: %i[insights analysis]
       before_action :set_analysis_dates, only: [:analysis]
 
       def insights
