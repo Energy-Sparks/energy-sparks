ecb9154936b61203f15f7b6ecf468052fdb6e6cd
Tristan Hill <t23+git@eaux.uk>
2024-09-23 11:25:12 +0000

    more

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_11_153230) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_05_174840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pgcrypto"
@@ -1896,7 +1896,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_153230) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "name"
-    t.string "pupil_password_old"
     t.bigint "staff_role_id"
     t.string "confirmation_token"
     t.datetime "confirmed_at", precision: nil
