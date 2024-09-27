c50cce323b7c94ba7b3a7911d341e684f50f205a
Tristan Hill <t23+git@eaux.uk>
2024-09-27 13:33:20 +0000

    more

---
+++ .platform/hooks/postdeploy/web.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /etc/systemd/system/web.service.d
+cat <<EOF >/etc/systemd/system/web.service.d/override.conf
+[Service]
+Nice=-10
+EOF
+systemctl daemon-reload
+systemctl restart web
