591964d2dc84ea02c7d84a46f2e5a4cb1de54564
Tristan Hill <t23+git@eaux.uk>
2024-10-04 09:53:17 +0000

    more

--- app/controllers/admin/reports/user_logins_controller.rb
+++ app/controllers/admin/reports/user_logins_controller.rb
@@ -4,13 +4,7 @@ module Admin
   module Reports
     class UserLoginsController < AdminController
       def index
-        # @school_groups = SchoolGroup.order(name: :asc)
-        # debugger if params[:id]
         @school_group = SchoolGroup.find(params[:id]) if params.key?(:id)
-      end
-
-      def show
-        debugger
       end
     end
   end
