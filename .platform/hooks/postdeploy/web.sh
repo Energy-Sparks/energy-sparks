3c57fdc8bba3c69e7402e54831879a03ba81aea4
Tristan Hill <t23+git@eaux.uk>
2024-10-04 16:59:41 +0000

    more

--- .platform/hooks/postdeploy/web.sh
+++ .platform/hooks/postdeploy/web.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
+# create override file in case of system restart but probably won't be used
 mkdir -p /etc/systemd/system/web.service.d
 cat <<EOF >/etc/systemd/system/web.service.d/override.conf
 [Service]
 Nice=-10
 EOF
-systemctl daemon-reload
-systemctl restart web
+
+renice -10 -g $(< /var/pids/web.pid)
