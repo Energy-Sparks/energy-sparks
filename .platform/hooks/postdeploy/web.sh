fdcdfb11f2e3700e33f75c59a588765d769fbcb2
Tristan Hill <t23+git@eaux.uk>
2024-10-07 09:05:40 +0000

    more

--- .platform/hooks/postdeploy/web.sh
+++ .platform/hooks/postdeploy/web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # create override file in case of system restart but probably won't be used
+# elasticbeantalk removes the override when disabling the service on deploy
 mkdir -p /etc/systemd/system/web.service.d
 cat <<EOF >/etc/systemd/system/web.service.d/override.conf
 [Service]
