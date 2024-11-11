ee223b49116a9f3ca06ead86b30586b9814783f3
Deb Bassett <deb@urbanwide.com>
2024-11-11 12:07:33 +0000

    Fix pages that use old 404.html pages and spec

--- app/controllers/resource_files_controller.rb
+++ app/controllers/resource_files_controller.rb
@@ -1,6 +1,7 @@
 class ResourceFilesController < ApplicationController
   include StorageHelper
   skip_before_action :authenticate_user!
+
   def index
     @resource_file_types = ResourceFileType.order(:position)
     @other_resource_files = ResourceFile.where(resource_file_type_id: nil).order(:title)
@@ -11,7 +12,7 @@ class ResourceFilesController < ApplicationController
     if resource.present?
       serve_from_storage(resource.file, params[:serve])
     else
-      render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+      route_not_found
     end
   end
 end
--- spec/system/case_studies_spec.rb
+++ spec/system/case_studies_spec.rb
@@ -1,40 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'case_studies', type: :system do
-  let!(:case_study) do
-    CaseStudy.create!(title: 'First Case Study', position: 1,
-    file_en: fixture_file_upload(Rails.root + 'spec/fixtures/images/newsletter-placeholder.png'))
-  end
+  context 'when there is an existing case study' do
+    let!(:case_study) do
+      CaseStudy.create!(title: 'First Case Study', position: 1,
+      file_en: fixture_file_upload(Rails.root + 'spec/fixtures/images/newsletter-placeholder.png'))
+    end
 
-  before do
-    visit case_studies_path
-  end
-
-  it 'shows me the resources page' do
-    expect(page).to have_content 'Case Studies'
-    expect(page).to have_content 'First Case Study'
-  end
-
-  it 'shows the expected link' do
-    expect(page).to have_link I18n.t('case_studies.download'), href: "/case_studies/#{case_study.id}/download?locale=en"
-  end
-
-  it 'serves the file' do
-    find("a[href='/case_studies/#{case_study.id}/download?locale=en']").click
-    expect(page).to have_http_status(:ok)
-  end
-
-  context 'a welsh download is not available' do
     before do
-      visit case_studies_path(locale: 'cy')
+      visit case_studies_path
     end
 
-    it 'the welsh link is not displayed' do
-      expect(page).to have_link I18n.t('case_studies.download', :locale => :cy), href: "/case_studies/#{case_study.id}/download?locale=en"
+    it 'shows me the resources page' do
+      expect(page).to have_content 'Case Studies'
+      expect(page).to have_content 'First Case Study'
+    end
+
+    it 'shows the expected link' do
+      expect(page).to have_link I18n.t('case_studies.download'), href: "/case_studies/#{case_study.id}/download?locale=en"
+    end
+
+    it 'serves the file' do
+      find("a[href='/case_studies/#{case_study.id}/download?locale=en']").click
+      expect(page).to have_http_status(:ok)
+    end
+
+    context 'a welsh download is not available' do
+      before do
+        visit case_studies_path(locale: 'cy')
+      end
+
+      it 'the welsh link is not displayed' do
+        expect(page).to have_link I18n.t('case_studies.download', :locale => :cy), href: "/case_studies/#{case_study.id}/download?locale=en"
+      end
     end
   end
 
-  context 'a welsh download is available' do
+  context 'when case study does not exist' do
+    before do
+      case_study_download_path('unknown')
+    end
+
+    it_behaves_like 'a 404 error page'
+  end
+
+  context 'when a welsh download is available' do
     let!(:case_study) do
       CaseStudy.create!(title: 'First Case Study', position: 1,
       file_en: fixture_file_upload(Rails.root + 'spec/fixtures/images/newsletter-placeholder.png'),
