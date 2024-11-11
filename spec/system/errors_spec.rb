74b1e7f12809485c79c85ad2ff602a22a10c8d69
Deb Bassett <deb.bassett@energysparks.uk>
2024-11-11 14:56:22 +0000

    Fix pages that use old 404.html pages and spec (#4056)

--- spec/system/errors_spec.rb
+++ spec/system/errors_spec.rb
@@ -6,11 +6,7 @@ RSpec.describe 'errors', type: :system do
       visit '/404'
     end
 
-    it { expect(page.status_code).to be 404 }
-    it { expect(page).to have_content '404' }
-    it { expect(page).to have_content 'Page not found' }
-    it { expect(page).to have_content 'Sorry, the page you are looking for does not exist' }
-    it { expect(page).to have_content 'Please continue to our home page.' }
+    it_behaves_like 'a 404 error page'
   end
 
   describe '500' do
@@ -18,11 +14,7 @@ RSpec.describe 'errors', type: :system do
       visit '/500'
     end
 
-    it { expect(page.status_code).to be 500 }
-    it { expect(page).to have_content '500' }
-    it { expect(page).to have_content 'Sorry, something has gone wrong' }
-    it { expect(page).to have_content 'The Energy Sparks team have been notified' }
-    it { expect(page).to have_content 'Please continue to our home page.' }
+    it_behaves_like 'a 500 error page'
   end
 
   describe '422' do
@@ -30,10 +22,6 @@ RSpec.describe 'errors', type: :system do
       visit '/422'
     end
 
-    it { expect(page.status_code).to be 422 }
-    it { expect(page).to have_content '422' }
-    it { expect(page).to have_content 'The change you wanted was rejected' }
-    it { expect(page).to have_content "Maybe you tried to change something you didn't have access to." }
-    it { expect(page).to have_content 'Please continue to our home page.' }
+    it_behaves_like 'a 422 error page'
   end
 end
