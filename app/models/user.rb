a3c379255352beffa8577fa0691379ab049d1a37
Leigh Dodds <leigh@ldodds.com>
2024-12-10 15:13:44 +0000

    Allow deleting of users with Observation (#4114)

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,6 +65,9 @@ class User < ApplicationRecord
   has_many :issues_admin_for, class_name: 'SchoolGroup', inverse_of: :default_issues_admin_user,
                               foreign_key: :default_issues_admin_user_id, dependent: nil
 
+  has_many :observations_created, class_name: 'Observation', inverse_of: :created_by, dependent: :nullify
+  has_many :observations_updated, class_name: 'Observation', inverse_of: :updated_by, dependent: :nullify
+
   has_and_belongs_to_many :cluster_schools, class_name: 'School', join_table: :cluster_schools_users
 
   # Include default devise modules. Others available are:
