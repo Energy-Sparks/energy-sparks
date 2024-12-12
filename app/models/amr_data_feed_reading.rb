42748b2103ec1d3c94258f1ef316baf81ba6a89c
Leigh Dodds <leigh@ldodds.com>
2024-12-12 17:14:45 +0000

    Fixes

--- app/models/amr_data_feed_reading.rb
+++ app/models/amr_data_feed_reading.rb
@@ -52,7 +52,7 @@ class AmrDataFeedReading < ApplicationRecord
 
   CSV_HEADER_DATA_FEED_READING = 'School URN,Name,Mpan Mprn,Meter Type,Reading Date,Reading Date Format,Record Last Updated,00:30,01:00,01:30,02:00,02:30,03:00,03:30,04:00,04:30,05:00,05:30,06:00,06:30,07:00,07:30,08:00,08:30,09:00,09:30,10:00,10:30,11:00,11:30,12:00,12:30,13:00,13:30,14:00,14:30,15:00,15:30,16:00,16:30,17:00,17:30,18:00,18:30,19:00,19:30,20:00,20:30,21:00,21:30,22:00,22:30,23:00,23:30,00:00'.freeze
 
-  PARSED_DATE = <<~SQL.squish.freeze.squish
+  PARSED_DATE = <<~SQL.squish.freeze
     CASE
     WHEN reading_date ~ '\\d{1,2}-(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-\\d{2}' THEN to_date(reading_date, 'DD-MON-YY')
     WHEN date_format='%d %b %Y %H:%M:%S' AND reading_date~'\\d{4}-\\d{2}-\\d{2}' THEN to_date(reading_date, 'YYYY-MM-DD')
@@ -116,7 +116,7 @@ class AmrDataFeedReading < ApplicationRecord
   end
 
   def self.meter_loading_report(mpxn)
-    AmrDataFeedReading.where(mpan_mprn: mpxn).joins(
+    where(mpan_mprn: mpxn).joins(
       :amr_data_feed_import_log,
       :amr_data_feed_config,
       'LEFT JOIN amr_uploaded_readings ON amr_uploaded_readings.file_name = amr_data_feed_import_logs.file_name'
