47f697885e8e5da21fdd21ae256fca6842fe238a
Tristan Hill <878258+tbhi@users.noreply.github.com>
2024-12-13 13:41:53 +0000

    more

--- app/services/amr/perse_upsert.rb
+++ app/services/amr/perse_upsert.rb
@@ -30,14 +30,19 @@ module Amr
       AmrDataFeedReading.where(amr_data_feed_config: config, meter: meter).maximum(:reading_date)
     end
 
-    def self.meter_history_readings(mpan, from_date)
+    private_class_method def self.meter_history_readings(mpan, from_date)
       DataFeeds::PerseApi.meter_history_realtime_data(mpan, from_date)['data']
                          &.select { |data| data_ok(data) }
-                         &.map { |data| [data['Date'], (1..48).map { |i| data["P#{i}"].to_f }] } || []
+                         &.map { |data| [data['Date'], (1..48).map { |i| to_f_if_not_nil(data["P#{i}"]) }] } || []
     end
 
-    def self.data_ok(data)
+    private_class_method def self.data_ok(data)
       data['MeasurementQuantity'] == 'AI' && (1..48).all? { |i| data["UT#{i}"] == 'A' }
+    end
+
+    private_class_method def self.to_f_if_not_nil(item)
+      # not sure these would ever be nil because of the data check above bur just to make sure
+      item.nil? ? nil : item.to_f
     end
   end
 end
