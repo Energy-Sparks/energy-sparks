596f063fd3e9c7d6d9a3e20b999a51fbd9c7cce2
Deb Bassett <deb@urbanwide.com>
2025-01-09 19:55:35 +0000

    prompts

---
+++ app/services/completables/progress.rb
@@ -0,0 +1,96 @@
+module Completables
+  class Progress
+    def self.new(completable)
+      "Completables::#{completable.class}".constantize.new(completable)
+    end
+  end
+
+  class ProgressBase
+    attr_reader :completable
+
+    def initialize(completable)
+      @completable = completable
+    end
+
+    def title
+      ''
+    end
+
+    def assignable
+      completable.assignable
+    end
+
+    def uncompleted_scores
+      completable.uncompleted_tasks.sum(&:score)
+    end
+
+    def uncompleted_count
+      completable.uncompleted_todos.count
+    end
+
+    def i18n_message_key
+      'message_html'
+    end
+
+    def message
+      I18n.t("schools.prompts.#{i18n_base}.#{i18n_message_key}",
+        title: assignable.title,
+        completed_activities_count: completable.completed_activity_types.count,
+        total_activities_count: assignable.activity_type_todos.count,
+        completed_actions_count: completable.completed_intervention_types.count,
+        total_actions_count: assignable.intervention_type_todos.count
+      )
+    end
+
+    def notification
+      (message + '<br>' + summary).html_safe
+    end
+  end
+
+  class Programme < ProgressBase
+    def title
+      assignable.title
+    end
+
+    def bonus_points
+      assignable.bonus_score
+    end
+
+    def i18n_base
+      'programme.progress'
+    end
+
+    def summary
+      key = bonus_points > 0 ? 'summary_bonus_html' : 'summary_html'
+      I18n.t("schools.prompts.#{i18n_base}.#{key}",
+        count: uncompleted_count,
+        uncompleted_scores: uncompleted_scores,
+        bonus_points: bonus_points)
+    end
+  end
+
+  class Audit < ProgressBase
+    def i18n_base
+      'audit'
+    end
+
+    def bonus_points
+      completable.available_bonus_points
+    end
+
+    def recent?
+      completable.created_at > 1.year.ago
+    end
+
+    def i18n_message_key
+      recent? ? 'message_html' : 'message_older_html'
+    end
+
+    def summary
+      I18n.t("schools.prompts.#{i18n_base}.summary_html",
+        remaining_points: uncompleted_scores,
+        count: bonus_points
+      )
+    end
+  end
+end
---
+++ spec/services/completables/progress_spec.rb
@@ -0,0 +1,296 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Completables::Progress, type: :service do
+  let!(:school) { create(:school) }
+  let(:service) { described_class.new(completable) }
+
+  let(:activity_type_factory_score) { 25 }
+  let(:intervention_type_factory_score) { 30 }
+
+  context 'when completable is a programme' do
+    let!(:assignable) { create(:programme_type, :with_todos, title: 'Programme Type title', bonus_score: 12) }
+    let!(:completable) { create(:programme, programme_type: assignable, started_on: '2020-01-01', school:) }
+
+    describe '#title' do
+      it 'returns the title' do
+        expect(service.title).to eq(assignable.title)
+      end
+    end
+
+    describe '#bonus_points' do
+      it 'returns the bonus score' do
+        expect(service.bonus_points).to eq(assignable.bonus_score)
+      end
+    end
+
+    context 'when no tasks have been completed' do
+      describe '#notification' do
+        let(:expected_score) { activity_type_factory_score * 3 + intervention_type_factory_score * 3 }
+
+        it 'shows full message' do
+          expect(service.notification).to eq('You have completed <strong>0/3</strong> of the activities and ' \
+                                  '<strong>0/3</strong> of the actions ' \
+                                  'in the <strong>Programme Type title</strong> programme<br>' \
+                                  "Complete the final <strong>6</strong> tasks now to score <strong>#{expected_score}</strong> points " \
+                                  'and <strong>12</strong> bonus points for completing the programme')
+        end
+      end
+    end
+
+    context 'when one activity has been completed' do
+      let(:recording) do
+        build(:activity, school:, activity_type: assignable.activity_type_tasks.first, happened_on: Date.yesterday)
+      end
+
+      before do
+        Tasks::Recorder.new(recording, nil).process
+      end
+
+      describe '#message' do
+        subject(:message) { service.message }
+
+        it 'returns the message' do
+          expect(message).to eq('You have completed <strong>1/3</strong> of the activities and ' \
+                                '<strong>0/3</strong> of the actions ' \
+                                'in the <strong>Programme Type title</strong> programme')
+        end
+      end
+
+      describe '#summary' do
+        subject(:summary) { service.summary }
+
+        let(:expected_score) { activity_type_factory_score * 2 + intervention_type_factory_score * 3 }
+
+        context 'with bonus points' do
+          it 'returns the summary with bonus mentioned' do
+            expect(summary).to eq("Complete the final <strong>5</strong> tasks now to score <strong>#{expected_score}</strong> points "\
+                                  'and <strong>12</strong> bonus points for completing the programme')
+          end
+        end
+
+        context 'without bonus points' do
+          before { assignable.update!(bonus_score: 0) }
+
+          it 'returns the summary without mentioning bonus' do
+            expect(summary).to eq("Complete the final <strong>5</strong> tasks now to score <strong>#{expected_score}</strong> points")
+          end
+        end
+      end
+    end
+
+    context 'when one action has been completed' do
+      let(:recording) do
+        build(:observation, :intervention, school:, intervention_type: assignable.intervention_type_tasks.first, at: Date.yesterday)
+      end
+
+      before do
+        Tasks::Recorder.new(recording, nil).process
+      end
+
+      describe '#message' do
+        subject(:message) { service.message }
+
+        it 'returns the message' do
+          expect(message).to eq('You have completed <strong>0/3</strong> of the activities and ' \
+                                '<strong>1/3</strong> of the actions ' \
+                                'in the <strong>Programme Type title</strong> programme')
+        end
+      end
+
+      describe '#summary' do
+        subject(:summary) { service.summary }
+
+        let(:expected_score) { activity_type_factory_score * 3 + intervention_type_factory_score * 2 }
+
+        context 'with bonus points' do
+          it 'returns the summary with bonus mentioned' do
+            expect(summary).to eq("Complete the final <strong>5</strong> tasks now to score <strong>#{expected_score}</strong> points "\
+                                  'and <strong>12</strong> bonus points for completing the programme')
+          end
+        end
+
+        context 'without bonus points' do
+          before { assignable.update!(bonus_score: 0) }
+
+          it 'returns the summary without mentioning bonus' do
+            expect(summary).to eq("Complete the final <strong>5</strong> tasks now to score <strong>#{expected_score}</strong> points")
+          end
+        end
+      end
+    end
+
+    context 'when all but one have been completed' do
+      before do
+        recordings = []
+        assignable.activity_type_tasks.first(2).each do |activity_type|
+          recordings << build(:activity, school:, activity_type:, happened_on: Date.yesterday)
+        end
+        assignable.intervention_type_tasks.each do |intervention_type|
+          recordings << build(:observation, :intervention, school:, intervention_type:, at: Date.yesterday)
+        end
+        recordings.each {|recording| Tasks::Recorder.new(recording, nil).process }
+      end
+
+      let(:expected_score) { activity_type_factory_score * 1}
+
+      it 'returns the message' do
+        expect(service.message).to eq('You have completed <strong>2/3</strong> of the activities and ' \
+                              '<strong>3/3</strong> of the actions ' \
+                              'in the <strong>Programme Type title</strong> programme')
+      end
+
+      it 'returns the summary with bonus mentioned' do
+        expect(service.summary).to eq("Complete the final task now to score <strong>#{expected_score}</strong> points "\
+                              'and <strong>12</strong> bonus points for completing the programme')
+      end
+    end
+  end
+
+  context 'when completable is an audit' do
+    let!(:site_settings) { SiteSettings.create!(audit_activities_bonus_points: 50) }
+
+    let(:created_at) { 3.days.ago }
+    let!(:assignable) { create(:audit, :with_todos, created_at:, school:) }
+    let!(:completable) { assignable }
+
+    describe '#bonus_points' do
+      it 'returns the bonus score' do
+        expect(service.bonus_points).to eq(50)
+      end
+    end
+
+    context 'when no tasks have been completed' do
+      describe '#notification' do
+        let(:expected_score) { activity_type_factory_score * 3 + intervention_type_factory_score * 3 }
+
+        it 'shows full message' do
+          expect(service.notification).to eq('You have completed <strong>0/3</strong> of the activities and ' \
+                                  '<strong>0/3</strong> of the actions ' \
+                                  'from your recent energy audit<br>' \
+                                  "Complete the others to score <strong>#{expected_score}</strong> points " \
+                                  'and <strong>50</strong> bonus points for completing all audit tasks')
+        end
+      end
+    end
+
+    context 'when audit was created over a year ago' do
+      let(:created_at) { 3.years.ago }
+
+      describe '#notification' do
+        it { expect(service.notification).not_to include('recent') }
+      end
+    end
+
+    context 'when one activity has been completed' do
+      let(:recording) do
+        build(:activity, school:, activity_type: assignable.activity_type_tasks.first, happened_on: Date.yesterday)
+      end
+
+      before do
+        Tasks::Recorder.new(recording, nil).process
+      end
+
+      describe '#message' do
+        subject(:message) { service.message }
+
+        it 'returns the message' do
+          expect(message).to eq('You have completed <strong>1/3</strong> of the activities and ' \
+                                '<strong>0/3</strong> of the actions ' \
+                                'from your recent energy audit')
+        end
+      end
+
+      describe '#summary' do
+        subject(:summary) { service.summary }
+
+        let(:expected_score) { activity_type_factory_score * 2 + intervention_type_factory_score * 3 }
+
+        context 'with bonus points' do
+          it 'returns the summary with bonus mentioned' do
+            expect(summary).to eq("Complete the others to score <strong>#{expected_score}</strong> points "\
+                                  'and <strong>50</strong> bonus points for completing all audit tasks')
+          end
+        end
+
+        context 'without bonus points' do
+          let!(:site_settings) { SiteSettings.create!(audit_activities_bonus_points: 0) }
+
+          it 'returns the summary without mentioning the bonus' do
+            expect(summary).to eq("Complete the others to score <strong>#{expected_score}</strong> points "\
+                                  'for completing all audit tasks')
+          end
+        end
+      end
+    end
+
+    context 'when one action has been completed' do
+      let(:recording) do
+        build(:observation, :intervention, school:, intervention_type: assignable.intervention_type_tasks.first, at: Date.yesterday)
+      end
+
+      before do
+        Tasks::Recorder.new(recording, nil).process
+      end
+
+      describe '#message' do
+        subject(:message) { service.message }
+
+        it 'returns the message' do
+          expect(message).to eq('You have completed <strong>0/3</strong> of the activities and ' \
+                                '<strong>1/3</strong> of the actions ' \
+                                'from your recent energy audit')
+        end
+      end
+
+      describe '#summary' do
+        subject(:summary) { service.summary }
+
+        let(:expected_score) { activity_type_factory_score * 3 + intervention_type_factory_score * 2 }
+
+        context 'with bonus points' do
+          it 'returns the summary' do
+            expect(summary).to eq("Complete the others to score <strong>#{expected_score}</strong> points "\
+                                  'and <strong>50</strong> bonus points for completing all audit tasks')
+          end
+        end
+
+        context 'without bonus points' do
+          let!(:site_settings) { SiteSettings.create!(audit_activities_bonus_points: 0) }
+
+          it 'returns the summary without mentioning the bonus' do
+            expect(summary).to eq("Complete the others to score <strong>#{expected_score}</strong> points "\
+                                  'for completing all audit tasks')
+          end
+        end
+      end
+    end
+
+    context 'when all but one have been completed' do
+      before do
+        recordings = []
+        assignable.activity_type_tasks.first(2).each do |activity_type|
+          recordings << build(:activity, school:, activity_type:, happened_on: Date.yesterday)
+        end
+        assignable.intervention_type_tasks.each do |intervention_type|
+          recordings << build(:observation, :intervention, school:, intervention_type:, at: Date.yesterday)
+        end
+        recordings.each {|recording| Tasks::Recorder.new(recording, nil).process }
+      end
+
+      let(:expected_score) { activity_type_factory_score * 1}
+
+      it 'returns the message' do
+        expect(service.message).to eq('You have completed <strong>2/3</strong> of the activities and ' \
+                              '<strong>3/3</strong> of the actions ' \
+                              'from your recent energy audit')
+      end
+
+      it 'returns the summary' do
+        expect(service.summary).to eq("Complete the others to score <strong>#{expected_score}</strong> points "\
+                                      'and <strong>50</strong> bonus points for completing all audit tasks')
+      end
+    end
+  end
+end
