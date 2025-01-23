4187ee5bdf07e9233c6ebc9dcbcb2b8be7023ef1
Leigh Dodds <leigh@ldodds.com>
2025-01-23 15:26:39 +0000

    initial SolisCloud integration (#4093)

---
+++ app/services/solar/solis_cloud_download_and_upsert.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module Solar
+  class SolisCloudDownloadAndUpsert < BaseDownloadAndUpsert
+    def download_and_upsert
+      readings = download
+      SolisCloudUpserter.new(installation: @installation, readings:, import_log:).perform
+    end
+
+    private
+
+    def job
+      :solis_cloud_download
+    end
+
+    def time_to_index(time)
+      split = time.split(':')
+      index = split[0].to_i * 2
+      index += 1 if split[1].to_i > 30
+      index
+    end
+
+    TO_HOUR = 5 / 60.0
+
+    def create_kwh_data_x48(data)
+      x48 = Array.new(48, 0.0)
+      data.each do |entry|
+        x48[time_to_index(entry['timeStr'])] += (entry['power'] / 1000.0) * TO_HOUR
+      end
+      x48
+    end
+
+    def start_date(station = {})
+      if @requested_start_date
+        @requested_start_date
+      elsif latest_reading
+        latest_reading - 5.days
+      elsif station['createDate']
+        Time.at(station['createDate'] / 1000.0).utc.to_date
+      else
+        raise 'unknown start date'
+      end
+    end
+
+    def download
+      api = DataFeeds::SolisCloudApi.new(@installation.api_id, @installation.api_secret)
+      stations = api.user_station_list.dig('data', 'page', 'records') || []
+      @installation.update!(station_list: stations)
+      stations.map do |station|
+        station[:readings] = (start_date(station)..end_date).filter_map do |date|
+          Rails.logger.debug { "SolisCloud download for #{station['id']} #{date}" }
+          begin
+            station_day = api.station_day(station['id'], date)
+            # sometimes the data attribute can be nil
+            [date, create_kwh_data_x48(station_day['data'])] if station_day['data']
+          rescue StandardError => e
+            e.rollbar_context = { station_id: station['id'], date:, station_day: }
+            raise
+          end
+        end
+        [:solar_pv, station]
+      end
+    end
+  end
+end
---
+++ app/services/solar/solis_cloud_upserter.rb
@@ -0,0 +1,15 @@
+require 'dashboard'
+
+module Solar
+  class SolisCloudUpserter < BaseUpserter
+    private
+
+    def meter_model_attributes(details)
+      { pseudo: true, solis_cloud_installation: @installation, meter_serial_number: details['id'] }
+    end
+
+    def synthetic_mpan(meter_type, details)
+      Dashboard::Meter.synthetic_combined_meter_mpan_mprn_from_urn(details['sno'].to_i(16), meter_type)
+    end
+  end
+end
