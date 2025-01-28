348ac24741398723cb2e538bf8215ef1368a2e1d
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-01-28 15:29:45 +0000

    more

--- spec/better_html_spec.rb
+++ spec/better_html_spec.rb
@@ -5,12 +5,17 @@ require 'rails_helper'
 RSpec.describe 'BetterHtml', :aggregate_failures do
   it 'does assert that some .html.erb templates are parseable' do
     erb_glob = Rails.root.join(
-      'app/views/[a-b]*/**/{*.htm,*.html,*.htm.erb,*.html.erb,*.html+*.erb}'
+      'app/views/**/{*.htm,*.html,*.htm.erb,*.html.erb,*.html+*.erb}'
     )
     Dir[erb_glob].each do |filename|
       data = File.read(filename)
       expect do
-        BetterHtml::BetterErb::ErubiImplementation.new(data, filename:).validate!
+        begin
+          BetterHtml::BetterErb::ErubiImplementation.new(data, filename:).validate!
+        rescue
+          p filename
+          raise
+        end
       end.not_to raise_exception
     end
   end
