36a461853202860bb71019192d89168b126db0b0
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-02-03 14:08:51 +0000

    more

--- opentofu/main.tf
+++ opentofu/main.tf
@@ -22,6 +22,12 @@ data "aws_instances" "test" {
   }
 }
 
+data "aws_db_instances" "all" {}
+
+locals {
+  test_db = [for id in data.aws_db_instances.all.instance_identifiers : id if can(regex("energy-sparks-test.*", id))][0]
+}
+
 resource "aws_cloudwatch_metric_alarm" "memory_usage_alarm" {
   alarm_name                = "Production Memory Use > 80%"
   actions_enabled           = true
@@ -73,28 +79,17 @@ resource "aws_cloudwatch_metric_alarm" "filesystem_free_space_alarm" {
   treat_missing_data  = "missing"
 }
 
-resource "aws_cloudwatch_dashboard" "cpu_and_memory" {
-  dashboard_name = "CPU_and_Memory_tf"
+resource "aws_cloudwatch_dashboard" "cpu_memory_storage" {
+  dashboard_name = "CPU_Memory_Storage"
   dashboard_body = jsonencode({
     "widgets" : [
       {
-        "height" : 12,
-        "width" : 12,
-        "y" : 0,
-        "x" : 0,
+        "height" : 10,
+        "width" : 10,
         "type" : "metric",
+        "x" : 0,
+        "y" : 0,
         "properties" : {
-          "metrics" : [
-            ["System/Linux", "mem_used_percent", "InstanceId", data.aws_instances.production.ids[0],
-            "AutoScalingGroupName", var.prod_asg_name, { "region" : "eu-west-2", "color" : "#d62728" }],
-            ["...", data.aws_instances.test.ids[0], ".", var.test_asg_name,
-            { "region" : "eu-west-2", "color" : "#1f77b4" }]
-          ],
-          "view" : "timeSeries",
-          "stacked" : false,
-          "region" : "eu-west-2",
-          "stat" : "Maximum",
-          "period" : 60,
           "annotations" : {
             "horizontal" : [
               {
@@ -104,32 +99,31 @@ resource "aws_cloudwatch_dashboard" "cpu_and_memory" {
               }
             ]
           },
+          "metrics" : [
+            ["System/Linux", "mem_used_percent", "InstanceId", data.aws_instances.production.ids[0], "AutoScalingGroupName", var.prod_asg_name, { "color" : "#d62728" }],
+            ["...", data.aws_instances.test.ids[0], ".", var.test_asg_name, { "color" : "#1f77b4" }]
+          ],
+          "period" : 60,
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "stat" : "Maximum",
           "title" : "Memory",
+          "view" : "timeSeries",
           "yAxis" : {
             "left" : {
-              "min" : 0,
-              "max" : 100
+              "max" : 100,
+              "min" : 0
             }
           }
         }
       },
       {
-        "height" : 12,
-        "width" : 11,
-        "y" : 0,
-        "x" : 12,
+        "height" : 10,
+        "width" : 10,
         "type" : "metric",
+        "x" : 10,
+        "y" : 0,
         "properties" : {
-          "metrics" : [
-            ["AWS/EC2", "CPUUtilization", "AutoScalingGroupName", var.test_asg_name,
-            { "color" : "#1f77b4", "label" : "test-4-0-9 ASG", "region" : "eu-west-2" }],
-            ["...", var.prod_asg_name, { "label" : "prod-4-0-9 ASG", "color" : "#d62728" }]
-          ],
-          "view" : "timeSeries",
-          "stacked" : false,
-          "region" : "eu-west-2",
-          "stat" : "Maximum",
-          "period" : 60,
           "annotations" : {
             "horizontal" : [
               {
@@ -139,78 +133,127 @@ resource "aws_cloudwatch_dashboard" "cpu_and_memory" {
               }
             ]
           },
+          "metrics" : [
+            ["AWS/EC2", "CPUUtilization", "AutoScalingGroupName", var.test_asg_name, { "color" : "#1f77b4", "label" : "test-4-0-9 ASG" }],
+            ["...", var.prod_asg_name, { "color" : "#d62728", "label" : "prod-4-0-9 ASG" }]
+          ],
+          "period" : 60,
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "stat" : "Maximum",
           "title" : "CPU",
+          "view" : "timeSeries",
           "yAxis" : {
             "left" : {
-              "min" : 0,
-              "max" : 100
+              "max" : 100,
+              "min" : 0
             }
           }
         }
       },
       {
-        "height" : 8,
-        "width" : 12,
-        "y" : 12,
-        "x" : 0,
+        "height" : 10,
+        "width" : 10,
         "type" : "metric",
+        "x" : 0,
+        "y" : 10,
         "properties" : {
           "metrics" : [
-            [{ "expression" : "100*(m5/4294967296)", "label" : "test-4-0-9", "id" : "e2", "region" : "eu-west-2",
-            "color" : "#1f77b4" }],
-            [{ "expression" : "100*(m4/17179869184)", "label" : "prod-4-0-9", "id" : "e3", "region" : "eu-west-2",
-            "color" : "#d62728" }],
-            ["System/Linux", "procstat_memory_rss", "pidfile", "/var/pids/worker.pid",
-              "InstanceId", data.aws_instances.production.ids[0], "process_name", "ruby3.2",
-              "AutoScalingGroupName", var.prod_asg_name,
-            { "id" : "m4", "region" : "eu-west-2", "visible" : false }],
-            ["...", data.aws_instances.test.ids[0], ".", ".", ".", var.test_asg_name,
-            { "id" : "m5", "region" : "eu-west-2", "visible" : false }],
+            [{ "color" : "#1f77b4", "expression" : "100*(m5/4294967296)", "id" : "e2", "label" : "test-4-0-9", "region" : "eu-west-2" }],
+            [{ "color" : "#d62728", "expression" : "100*(m4/17179869184)", "id" : "e3", "label" : "prod-4-0-9", "region" : "eu-west-2" }],
+            ["System/Linux", "procstat_memory_rss", "pidfile", "/var/pids/worker.pid", "InstanceId", data.aws_instances.production.ids[0], "process_name", "ruby3.2", "AutoScalingGroupName", var.prod_asg_name, { "id" : "m4", "region" : "eu-west-2", "visible" : false }],
+            ["...", data.aws_instances.test.ids[0], ".", ".", ".", var.test_asg_name, { "id" : "m5", "region" : "eu-west-2", "visible" : false }]
           ],
-          "view" : "timeSeries",
-          "stacked" : false,
-          "region" : "eu-west-2",
-          "stat" : "Average",
           "period" : 300,
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "stat" : "Average",
+          "title" : "Good Job Memory (procstat_memory_rss / total mem)",
+          "view" : "timeSeries",
           "yAxis" : {
             "left" : {
-              "showUnits" : true,
               "label" : "%",
+              "max" : 100,
               "min" : 0,
-              "max" : 100
+              "showUnits" : true
             },
             "right" : {
               "showUnits" : false
             }
-          },
-          "title" : "Good Job Memory (procstat_memory_rss / total mem)"
+          }
         }
       },
       {
-        "height" : 8,
-        "width" : 11,
-        "y" : 12,
-        "x" : 12,
+        "height" : 10,
+        "width" : 10,
+        "type" : "metric",
+        "x" : 10,
+        "y" : 10,
+        "properties" : {
+          "metrics" : [
+            ["System/Linux", "procstat_cpu_usage", "pidfile", "/var/pids/worker.pid", "InstanceId", data.aws_instances.production.ids[0], "process_name", "ruby3.2", "AutoScalingGroupName", var.prod_asg_name],
+            ["...", data.aws_instances.test.ids[0], ".", ".", ".", var.test_asg_name, { "color" : "#d62728" }]
+          ],
+          "period" : 300,
+          "region" : "eu-west-2",
+          "stacked" : false,
+          "stat" : "Average",
+          "title" : "Good Job CPU (procstat_cpu_usage)",
+          "view" : "timeSeries",
+          "yAxis" : {
+            "left" : {
+              "max" : 100,
+              "min" : 0
+            }
+          }
+        }
+      },
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 20,
+        "width" : 10,
+        "height" : 10,
+        "properties" : {
+          "view" : "timeSeries",
+          "stacked" : false,
+          "metrics" : [
+            ["AWS/RDS", "FreeStorageSpace", "DBInstanceIdentifier", "energy-sparks-production-db-2022", { "color" : "#d62728" }],
+            ["...", local.test_db, { "color" : "#1f77b4" }]
+          ],
+          "region" : "eu-west-2",
+          "yAxis" : {
+            "left" : {
+              "min" : 0
+            }
+          },
+          "title" : "Database FreeStorageSpace",
+          "period" : 300,
+          "stat" : "Maximum",
+        }
+      },
+      {
+        "height" : 10,
+        "width" : 10,
+        "x" : 10,
+        "y" : 20,
         "type" : "metric",
         "properties" : {
           "metrics" : [
-            ["System/Linux", "procstat_cpu_usage", "pidfile", "/var/pids/worker.pid",
-              "InstanceId", data.aws_instances.production.ids[0], "process_name", "ruby3.2",
-            "AutoScalingGroupName", var.prod_asg_name],
-            ["...", data.aws_instances.test.ids[0], ".", ".", ".", var.test_asg_name, { "color" : "#d62728" }]
+            ["System/Linux", "disk_free", "path", "/", "InstanceId", data.aws_instances.production.ids[0], "AutoScalingGroupName", var.prod_asg_name, "device", "nvme0n1p1", "fstype", "xfs", { "color" : "#d62728" }],
+            ["...", data.aws_instances.test.ids[0], ".", var.test_asg_name, ".", ".", ".", ".", { "color" : "#1f77b4" }]
           ],
           "view" : "timeSeries",
           "stacked" : false,
           "region" : "eu-west-2",
+          "title" : "Server disk space available",
+          "period" : 300,
+          "stat" : "Average",
           "yAxis" : {
             "left" : {
-              "min" : 0,
-              "max" : 100
+              "min" : 0
             }
-          },
-          "stat" : "Average",
-          "period" : 300,
-          "title" : "Good Job CPU (procstat_cpu_usage)"
+          }
         }
       }
     ]
