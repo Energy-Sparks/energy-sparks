e30e104cd105fbfc4fb026b86903c2352e0474f3
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-02-04 15:23:19 +0000

    Fix sending email with no stopped feeds  (#4224)

--- spec/mailers/previews/admin_mailer_preview.rb
+++ spec/mailers/previews/admin_mailer_preview.rb
@@ -3,8 +3,8 @@ class AdminMailerPreview < ActionMailer::Preview
     AdminMailer.with(email_address: 'test@blah.com', user: User.admin.last).issues_report
   end
 
-  def missing_reading_window
+  def stopped_data_feeds
     missing = [[AmrDataFeedConfig.order(:missing_reading_window).first, 10.days]]
-    AdminMailer.with(to: 'operations@energysparks.uk', missing:).missing_reading_window
+    AdminMailer.with(to: 'operations@energysparks.uk', missing:).stopped_data_feeds
   end
 end
