f6e5ac21ec4b32011a0b5a60bf3d1f2d2468bc13
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-02-04 15:23:19 +0000

    Fix sending email with no stopped feeds  (#4224)

--- spec/tasks/amr/stopped_data_feeds_email_spec.rb
+++ spec/tasks/amr/stopped_data_feeds_email_spec.rb
@@ -2,22 +2,31 @@
 
 require 'rails_helper'
 
-RSpec.describe 'amr:missing_reading_window_email' do # rubocop:disable RSpec/DescribeClass
+RSpec.describe 'amr:stopped_data_feeds_email' do # rubocop:disable RSpec/DescribeClass
   before(:all) { Rails.application.load_tasks } # rubocop:disable RSpec/BeforeAfterAll
 
-  around do |example|
-    ClimateControl.modify SEND_AUTOMATED_EMAILS: 'true' do
-      example.run
-    end
+  let(:task) do
+    task = Rake::Task[self.class.description]
+    task.reenable
+    task
   end
 
-  it 'executes successfully' do
-    reading = create(:amr_data_feed_reading, updated_at: 6.days.ago)
-    Rake::Task[self.class.description].invoke
+  around do |example|
+    ClimateControl.modify(SEND_AUTOMATED_EMAILS: 'true') { example.run }
+  end
+
+  it 'send an email with old reading' do
+    reading = travel_to(6.days.ago) { create(:amr_data_feed_reading) }
+    task.invoke
     email = Capybara.string(ActionMailer::Base.deliveries.last.html_part.decoded)
     expect(email.all('table thead tr th').map(&:text)).to \
       eq(['AMR Data Feed Configuration Name', 'Missing reading window setting', 'Last reading update'])
     expect(email.all('table tbody tr td').map(&:text)).to \
       eq([reading.amr_data_feed_config.description, '5 days', '6 days'])
+  end
+
+  it 'sends nothing with recent reading' do
+    create(:amr_data_feed_reading)
+    expect { task.invoke }.not_to change(ActionMailer::Base.deliveries, :count)
   end
 end
