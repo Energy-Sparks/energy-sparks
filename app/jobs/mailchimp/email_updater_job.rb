70e0422868f8b74955669a1b90df291dc9d11d61
Leigh Dodds <leigh@ldodds.com>
2025-02-06 15:59:36 +0000

    Fix

--- app/jobs/mailchimp/email_updater_job.rb
+++ app/jobs/mailchimp/email_updater_job.rb
@@ -2,7 +2,7 @@ module Mailchimp
   class EmailUpdaterJob < ApplicationJob
     def perform(user:, original_email:)
       contact = Mailchimp::Contact.from_user(user)
-      mailchimp_member = Mailchimp::AudienceManager.update_contact(contact, original_email)
+      mailchimp_member = Mailchimp::AudienceManager.new.update_contact(contact, original_email)
       # Update status as well seeing as its returned in the response
       user.update(
         mailchimp_updated_at: Time.zone.now,
