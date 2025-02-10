61f103e79b79dc7e81ba754f86f076d263ccf80e
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-02-10 14:31:55 +0000

    Update funder allocation report summary table to include onboarding schools (#4237)

--- app/controllers/admin/reports/funder_allocations_controller.rb
+++ app/controllers/admin/reports/funder_allocations_controller.rb
@@ -6,8 +6,12 @@ module Admin
       def show
         @funders_visible = Funder.funded_school_counts(visible: true, data_enabled: false)
         @funders_visible_and_enabled = Funder.funded_school_counts(visible: true, data_enabled: true)
+        @onboarding = Funder.joins('LEFT JOIN school_onboardings ON funders.id = school_onboardings.funder_id ' \
+                                   'AND school_onboardings.school_id IS NULL')
+                            .group(:name).count('school_onboardings.id')
         @unfunded_visible = School.visible.unfunded.count
         @unfunded_visible_and_enabled = School.visible.data_enabled.unfunded.count
+        @unfunded_onboarding = SchoolOnboarding.where(funder: nil, school: nil).count
       end
 
       def deliver
