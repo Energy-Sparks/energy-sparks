9cf25364a8ce1607939e905cb978a7530532852d
Deb Bassett <deb@urbanwide.com>
2025-02-14 20:39:33 +0000

    More

--- app/components/cards/stats_component.rb
+++ app/components/cards/stats_component.rb
@@ -1,7 +1,7 @@
 module Cards
   class StatsComponent < ApplicationComponent
     renders_one :icon, 'IconComponent'
-    renders_one :header, ->(title) { Elements::HeaderComponent.new(title: title, level: 5, classes: 'header text-white mt-2') }
+    renders_one :header, ->(title) { Elements::HeaderComponent.new(title: title, level: 5, classes: 'text-white mt-2') }
     renders_one :figure, ->(figure) { Elements::HeaderComponent.new(title: figure, level: 2, classes: 'figure text-blue-light') }
     renders_one :subtext, -> { Elements::ParagraphComponent.new(classes: 'very-small text-blue-light') }
 
