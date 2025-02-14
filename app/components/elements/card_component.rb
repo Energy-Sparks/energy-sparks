6008ca5fcb9908c59d609c00f3d47b517b6e392f
Deb Bassett <deb@urbanwide.com>
2025-02-14 17:31:28 +0000

    More

--- app/components/elements/card_component.rb
+++ app/components/elements/card_component.rb
@@ -1,9 +1,9 @@
 module Elements
   class CardComponent < ApplicationComponent
     renders_one :icon, 'IconComponent'
-    renders_one :header, ->(title) { Elements::HeaderComponent.new(title: title, level: 4, classes: 'text-white') }
-    renders_one :figure, ->(figure) { Elements::HeaderComponent.new(title: figure, level: 3, classes: 'text-blue-light') }
-    renders_one :subtext, -> { Elements::ParagraphComponent.new(classes: 'small text-blue-light') }
+    renders_one :header, ->(title) { Elements::HeaderComponent.new(title: title, level: 5, classes: 'header text-white mt-2') }
+    renders_one :figure, ->(figure) { Elements::HeaderComponent.new(title: figure, level: 2, classes: 'figure text-blue-light') }
+    renders_one :subtext, -> { Elements::ParagraphComponent.new(classes: 'very-small text-blue-light') }
 
     def initialize(id: '', classes: '')
       super(id: id, classes: classes)
