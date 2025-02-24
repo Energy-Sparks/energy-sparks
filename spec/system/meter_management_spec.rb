0186174080f6ec76bfdef7fb7519037c25bead0a
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-02-24 16:08:51 +0000

    more

--- spec/system/meter_management_spec.rb
+++ spec/system/meter_management_spec.rb
@@ -37,7 +37,7 @@ RSpec.shared_examples_for 'the show meter page' do |admin:|
     if admin
       expect(page).to have_text('Admin details')
       expect(page).to have_text('Meter system NHH AMR Data source Data Co Contract Meter status Manual reads? false ' \
-                                'Gas Units Cubic Meters')
+                                'Gas unit Cubic Meters')
     else
       expect(page).to have_no_text('Admin details')
     end
