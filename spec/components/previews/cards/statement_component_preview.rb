7bb42de8c4b85e9ff21dc8f1b3851a114ec8b2a3
Deb Bassett <deb@urbanwide.com>
2025-02-24 16:58:44 +0000

    Fix previews

--- spec/components/previews/cards/statement_component_preview.rb
+++ spec/components/previews/cards/statement_component_preview.rb
@@ -2,7 +2,7 @@ module Cards
   class StatementComponentPreview < ViewComponent::Preview
     def without_classes
       render(Cards::StatementComponent.new) do |card|
-        card.with_header 'Header'
+        card.with_header title: 'Header'
         card.with_description { 'Interesting text about the above figure' }
       end
     end
--- spec/components/previews/cards/stats_component_preview.rb
+++ spec/components/previews/cards/stats_component_preview.rb
@@ -3,7 +3,7 @@ module Cards
     def without_classes
       render(Cards::StatsComponent.new) do |card|
         card.with_icon fuel_type: :electricity, style: :circle
-        card.with_header 'Header'
+        card.with_header title: 'Header'
         card.with_figure '90%'
         card.with_subtext { 'Interesting text about the above figure' }
       end
