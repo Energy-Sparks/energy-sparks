8b11a8977a953616626ffa4ecf68cc7a931a9322
Deb Bassett <deb.bassett@energysparks.uk>
2025-03-12 14:32:52 +0000

    Add audits section to front page (#4327)

--- app/components/layout/cards/section_header_component.rb
+++ app/components/layout/cards/section_header_component.rb
@@ -2,10 +2,10 @@ module Layout
   module Cards
     class SectionHeaderComponent < ApplicationComponent
       renders_one :header, ->(**kwargs) do
-        Elements::HeaderComponent.new(**kwargs.merge({ level: 3 }))
+        Elements::HeaderComponent.new(**{ level: 3 }.merge(kwargs))
       end
       renders_one :description, ->(**kwargs) do
-        Elements::TagComponent.new(:p, **kwargs.merge({ classes: '' }))
+        Elements::TagComponent.new(:p, **{ classes: '' }.merge(kwargs))
       end
 
       def initialize(**_kwargs)
