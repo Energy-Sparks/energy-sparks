75e800e4f3f3a387b7336dc5ca453cf41aec052a
Deb Bassett <deb.bassett@energysparks.uk>
2025-03-24 10:14:04 +0000

    Add organisations section to front page (#4339)

--- app/components/layout/cards/statement_component.rb
+++ app/components/layout/cards/statement_component.rb
@@ -1,11 +1,11 @@
 module Layout
   module Cards
-    class StatementComponent < ApplicationComponent
+    class StatementComponent < LayoutComponent
       renders_one :badge, ->(*args, **kwargs) do
         Elements::BadgeComponent.new(*args, **{ classes: 'bg-teal-medium text-blue-very-dark text-uppercase mb-3' }.merge(kwargs))
       end
       renders_one :statement, ->(**kwargs) do
-        Elements::HeaderComponent.new(**{ classes: 'statement text-white card-text', level: 6 }.merge(kwargs))
+        Elements::HeaderComponent.new(**{ classes: 'statement card-text', level: 6 }.merge(kwargs))
       end
     end
   end
