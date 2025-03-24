8e236b2e4976c158fff897b269b940f610bd699c
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-03-24 14:19:53 +0000

    more

--- app/models/local_distribution_zone.rb
+++ app/models/local_distribution_zone.rb
@@ -26,13 +26,15 @@ class LocalDistributionZone < ApplicationRecord
 
   scope :by_name, -> { order(:name) }
 
-  KWH_PER_M3_GAS = 11.1 # this depends on the calorifc value of the gas and so is an approximate average
+  AVERAGE_CALORIFIC_VALUE = 39.075
   MEGAJOULES_TO_KWH = 1 / (1.hour / 1000.0)
+  CORRECTION_FACTOR = 1.02264 # https://www.eonnext.com/business/help/convert-gas-units-to-kwh
 
   def self.kwh_per_m3(local_distribution_zone, date)
     calorific_value = unless local_distribution_zone.nil? || date.nil?
                         local_distribution_zone.readings.find_by(date: date)&.calorific_value
                       end
-    calorific_value.nil? ? KWH_PER_M3_GAS : calorific_value * MEGAJOULES_TO_KWH
+    calorific_value = AVERAGE_CALORIFIC_VALUE if calorific_value.nil?
+    calorific_value * MEGAJOULES_TO_KWH * CORRECTION_FACTOR
   end
 end
