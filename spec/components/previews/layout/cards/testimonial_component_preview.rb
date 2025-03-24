c20d1f158dbcd317ba209484867bb83185e2e72c
Deb Bassett <deb@urbanwide.com>
2025-03-24 15:40:26 +0000

    Re-org testimonial component for db model

--- spec/components/previews/layout/cards/testimonial_component_preview.rb
+++ spec/components/previews/layout/cards/testimonial_component_preview.rb
@@ -3,9 +3,22 @@ module Layout
     class TestimonialComponentPreview < ViewComponent::Preview
       def default
         render(Layout::Cards::TestimonialComponent.new) do |card|
-          card.with_header title: 'Header'
-          card.with_quote { 'Interesting quote about the above title' }
-          card.with_source { '<strong>Name</strong><br>school'.html_safe }
+          card.with_header title: 'Powering a Greener Tomorrow'
+          card.with_quote { 'Every watt saved is a step towards a brighter future!' }
+          card.with_name { 'Dr Watts' }
+          card.with_role { 'Energy Champion' }
+          card.with_location { 'Greenfields Academy' }
+          card.with_button 'Primary link', '/', style: :primary
+          card.with_button 'Secondary link', '/', style: :secondary
+        end
+      end
+
+      def with_theme_and_without_role
+        render(Layout::Cards::TestimonialComponent.new(theme: :light)) do |card|
+          card.with_header title: 'Powering a Greener Tomorrow'
+          card.with_quote { 'Every watt saved is a step towards a brighter future!' }
+          card.with_name { 'Dr Watts' }
+          card.with_location { 'Greenfields Academy' }
           card.with_button 'Primary link', '/', style: :primary
           card.with_button 'Secondary link', '/', style: :secondary
         end
