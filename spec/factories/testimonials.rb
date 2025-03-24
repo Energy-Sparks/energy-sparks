7d5afc88e3e628795b62b8113783941a55e5cf54
Deb Bassett <deb@urbanwide.com>
2025-03-24 16:12:00 +0000

    Add model

---
+++ spec/factories/testimonials.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :testimonial do
+    title { 'Testimonial title' }
+    quote { 'Testimonial quote' }
+    name { 'Testimonial name' }
+    role { 'Testimonial role' }
+    location { 'Testimonial location' }
+  end
+end
