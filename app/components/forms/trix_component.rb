3d91e7ef24bb4f8255adfe32b086c64dbd93c2c6
Leigh Dodds <leigh@ldodds.com>
2025-03-25 16:13:14 +0000

    Add spec

--- app/components/forms/trix_component.rb
+++ app/components/forms/trix_component.rb
@@ -1,16 +1,17 @@
 module Forms
   class TrixComponent < ApplicationComponent
-    attr_reader :form, :field, :charts, :kwargs
+    attr_reader :form, :field, :charts, :field_kwargs
 
     def initialize(form:, field:, controls: :default, size: :default, button_size: :default, charts: nil, **kwargs)
       super
       raise ArgumentError, 'Unknown size' if size && !self.class.sizes.include?(size)
       raise ArgumentError, 'Unknown button size' if button_size && !self.class.sizes.include?(button_size)
+      raise ArgumentError, 'Unknown controls options' if controls && !self.class.controls.include?(controls)
 
       @form = form
       @field = field
       @charts = charts
-      @kwargs = kwargs
+      @field_kwargs = kwargs.except(:id, :classes)
       add_classes(size)
       add_classes("buttons-#{button_size}")
       add_classes('chart-list') if charts&.any?
@@ -24,6 +25,10 @@ module Forms
     class << self
       def sizes
         [:default, :large]
+      end
+
+      def controls
+        [:default, :simple, :advanced]
       end
     end
   end
