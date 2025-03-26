c1c846ed5f6204ca05ecb48e4c35ca5c5282f8c0
Deb Bassett <deb@urbanwide.com>
2025-03-26 16:48:10 +0000

    Fixen

--- app/components/layout/cards/testimonial_component.rb
+++ app/components/layout/cards/testimonial_component.rb
@@ -19,9 +19,11 @@ module Layout
       renders_one :organisation, ->(**kwargs) do
         Elements::TagComponent.new(:div, **merge_classes('small text-darker', kwargs))
       end
-      renders_one :case_study, ->(case_study) do
-        Elements::ButtonComponent.new(t('home.testimonials.read_case_study'),
-          case_study_download_path(case_study), style: :primary, classes: 'mb-1 mr-2')
+      renders_one :case_study, ->(case_study = nil) do
+        if case_study
+          Elements::ButtonComponent.new(t('components.testimonial.read_case_study'),
+            case_study_download_path(case_study), style: :primary, classes: 'mb-1 mr-2')
+        end
       end
     end
   end
