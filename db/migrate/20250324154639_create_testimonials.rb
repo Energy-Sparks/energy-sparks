4492ebb685dc311950b62c8f7246151ce9190cec
Deb Bassett <deb@urbanwide.com>
2025-03-27 17:23:45 +0000

    Don't need these

--- db/migrate/20250324154639_create_testimonials.rb
+++ db/migrate/20250324154639_create_testimonials.rb
@@ -1,10 +1,7 @@
 class CreateTestimonials < ActiveRecord::Migration[7.2]
   def change
     create_table :testimonials do |t|
-      #t.string :title
-      #t.text :quote
       t.string :name
-      #t.string :role
       t.string :organisation
       t.boolean :active, default: false, null: false
       t.integer :category, default: 0, null: false
