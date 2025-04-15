e91a3a409139425c6cee2bc961162eb1ae310008
Leigh Dodds <leigh@ldodds.com>
2025-04-15 15:52:01 +0100

    Revise Support Page Layout - Categories (#4384)

--- spec/system/cms/categories_spec.rb
+++ spec/system/cms/categories_spec.rb
@@ -3,6 +3,27 @@ require 'rails_helper'
 describe 'view categories' do
   let!(:category) { create(:category, :with_pages, pages_published: true, published: true) }
 
+  shared_examples 'a category page' do |pages: true|
+    let(:visible_pages) { category.pages.published }
+
+    it 'displays category title and description' do
+      within('.layout-cards-page-header-component') do
+        expect(page).to have_content(category.title)
+        expect(page).to have_content(category.description)
+      end
+    end
+
+    it 'lists the expected pages', if: pages do
+      visible_pages.each do |cms_page|
+        expect(page).to have_css("##{cms_page.slug}.cms-page-summary-component")
+      end
+    end
+
+    it 'has no visible pages', unless: pages do
+      expect(page).not_to have_css('.cms-page-summary-component')
+    end
+  end
+
   context 'with support pages feature off', without_feature: :support_pages do
     context 'when visiting categories index' do
       before do
@@ -32,7 +53,9 @@ describe 'view categories' do
 
       it 'shows all categories' do
         expect(page).to have_link(category.title, href: category_path(category))
+        expect(page).to have_content(category.description)
         expect(page).to have_link(unpublished.title, href: category_path(unpublished))
+        expect(page).to have_content(unpublished.description)
       end
     end
 
@@ -43,7 +66,9 @@ describe 'view categories' do
 
       it 'shows only published categories' do
         expect(page).to have_link(category.title, href: category_path(category))
+        expect(page).to have_content(category.description)
         expect(page).not_to have_link(unpublished.title, href: category_path(unpublished))
+        expect(page).not_to have_content(unpublished.description)
       end
     end
   end
@@ -58,8 +83,24 @@ describe 'view categories' do
           visit category_path(category)
         end
 
-        it 'shows the category' do
-          expect(page).to have_content(category.title)
+        it_behaves_like 'a category page'
+
+        it_behaves_like 'a page with a support page nav' do
+          let(:current_category) { category }
+        end
+
+        context 'when there are unpublished pages' do
+          let!(:category) { create(:category, :with_pages, pages_published: false, published: true) }
+
+          it_behaves_like 'a category page' do
+            let(:visible_pages) { category.pages }
+          end
+
+          it_behaves_like 'a page with a support page nav' do
+            let(:categories) { Cms::Category.all }
+            let(:pages) { Cms::Page.all }
+            let(:current_category) { category }
+          end
         end
       end
 
@@ -68,8 +109,20 @@ describe 'view categories' do
           visit category_path(category)
         end
 
-        it 'shows the category' do
-          expect(page).to have_content(category.title)
+        it_behaves_like 'a category page'
+
+        it_behaves_like 'a page with a support page nav' do
+          let(:current_category) { category }
+        end
+
+        context 'when there are unpublished pages' do
+          let!(:category) { create(:category, :with_pages, pages_published: false, published: true) }
+
+          it_behaves_like 'a category page', pages: false
+
+          it_behaves_like 'a page with a support page nav' do
+            let(:current_category) { category }
+          end
         end
       end
     end
@@ -83,9 +136,7 @@ describe 'view categories' do
           visit category_path(category)
         end
 
-        it 'shows the category' do
-          expect(page).to have_content(category.title)
-        end
+        it_behaves_like 'a category page'
       end
 
       context 'when logged in as any other user' do
