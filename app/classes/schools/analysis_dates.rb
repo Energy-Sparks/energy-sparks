8a72f8ab24730b8a44368664e8d0aa8c125a153d
Leigh Dodds <leigh@ldodds.com>
2025-04-24 14:11:01 +0100

    Rename methods, add spec, remove unused methods

--- app/classes/schools/analysis_dates.rb
+++ app/classes/schools/analysis_dates.rb
@@ -41,10 +41,7 @@ module Schools
       (analysis_end_date - 364) >= analysis_start_date
     end
 
-    # FIXME BaseLongTerm
-    # _analysis_comparison
-    # group_by_week_electricity_versus_benchmark
-    def last_full_week_start_date_long_term_usage
+    def last_full_week_start_date_up_to_one_year_ago
       if one_years_data?
         last_full_week_start_date
       else
@@ -52,28 +49,17 @@ module Schools
       end
     end
 
-    # for charts that use the last full week
-    # beginning of the week is Sunday
-    #
-    # FIXME BaseOutOfHours
-    # _holidays.html.erb
-    # alert_group_by_week_electricity_14_months
-    # alert_group_by_week_gas_14_months
-    # management_dashboard_group_by_week_gas
-    # management_dashboard_group_by_week_electricity
-    def last_full_week_start_date_out_of_hours
+    # Used in OutOfHours
+    def last_full_week_start_date_14_months_ago
       (analysis_end_date - 13.months).beginning_of_week - 1
     end
 
-    # for charts that use the last full week
-    # TODO BaseLongTerm
+    # Used in LongTerm
     def last_full_week_start_date
       analysis_end_date.prev_year.end_of_week
     end
 
-    # for charts that use the last full week
-    # end of the week is Saturday
-    # TODO BaseOutOfHours
+    # Used in OutOfHours
     def last_full_week_end_date
       analysis_end_date.end_of_week - 1
     end
