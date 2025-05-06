ddd1138bfa1b9a349a1a9c6e250115468d78ec1e
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-05-06 17:36:15 +0000

    job per config

--- app/jobs/amr_import_job.rb
+++ app/jobs/amr_import_job.rb
@@ -3,17 +3,25 @@
 class AmrImportJob < ApplicationJob
   queue_as :regeneration
 
+  # initially this import was sequential in the rake task
+  # it was then changed to create a job for each file
+  # now changing to a job for each config to lower database load
   def self.import_all(config, bucket)
     s3_client = Aws::S3::Client.new
     Rails.logger.info "Download all from S3 key pattern: #{config.identifier}"
-    s3_client.list_objects_v2(bucket: bucket, prefix: "#{config.identifier}/").contents.each do |object|
-      # need to ignore application/x-directory objects ending with /
-      perform_later(config, bucket, object.key) unless object.key.end_with?('/')
-    end
+    keys = s3_client.list_objects_v2(bucket: bucket, prefix: "#{config.identifier}/").contents
+                    # need to ignore application/x-directory objects ending with /
+                    .reject { |object| object.key.end_with?('/') }.map(&:key)
+    perform_later(config, bucket, keys)
   end
 
-  def perform(config, bucket, key)
-    s3_client = Aws::S3::Client.new
+  def perform(config, bucket, keys)
+    keys.each { |key| process_key(Aws::S3::Client.new, config, bucket, key) }
+  end
+
+  private
+
+  def process_key(s3_client, config, bucket, key)
     Tempfile.create([File.basename(key, File.extname(key)), File.extname(key)]) do |tempfile|
       Rails.logger.info "Downloading s3://#{bucket}/#{key}"
       s3_client.get_object(bucket:, key:, response_target: tempfile.path)
@@ -25,8 +33,6 @@ class AmrImportJob < ApplicationJob
   rescue StandardError => e
     EnergySparks::Log.exception(e, job: :amr_import_job, bucket:, config: config.identifier, key:)
   end
-
-  private
 
   def archive_file(s3_client, config, bucket, key, key_without_parent)
     archived_key = "#{config.s3_archive_folder}/#{key_without_parent}"
