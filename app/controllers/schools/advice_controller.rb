5389c4f380a188a7f3eec4a5dd8af4cbbed4b99e
Leigh Dodds <leigh@ldodds.com>
2025-05-09 13:12:05 +0100

    Remove feature flags for advice index and page nav (#4439)

--- app/controllers/schools/advice_controller.rb
+++ app/controllers/schools/advice_controller.rb
@@ -16,36 +16,22 @@ module Schools
     before_action :school_inactive
     before_action :load_advice_pages
     before_action :set_tab_name
-    before_action :set_counts
     before_action :set_breadcrumbs
     before_action :check_aggregated_school_in_cache, only: [:show]
 
     def show
-      if Flipper.enabled?(:new_dashboards_2024, current_user)
-        @overview_data = Schools::ManagementTableService.new(@school).management_data
-        render :new_show, layout: 'dashboards'
-      else
-        @advice_page_benchmarks = @school.advice_page_school_benchmarks
-        render :show
-      end
+      @overview_data = Schools::ManagementTableService.new(@school).management_data
+      render :show, layout: 'dashboards'
     end
 
     def priorities
       @management_priorities = sort_priorities
-      if Flipper.enabled?(:new_dashboards_2024, current_user)
-        render :priorities, layout: 'dashboards'
-      else
-        render :priorities
-      end
+      render :priorities, layout: 'dashboards'
     end
 
     def alerts
       @dashboard_alerts = setup_alerts(latest_dashboard_alerts, :management_dashboard_title, limit: nil)
-      if Flipper.enabled?(:new_dashboards_2024, current_user)
-        render :alerts, layout: 'dashboards'
-      else
-        render :alerts
-      end
+      render :alerts, layout: 'dashboards'
     end
 
     private
@@ -78,11 +64,6 @@ module Schools
 
     def redirect_to_pupil_dash_if_not_data_enabled
       redirect_to pupils_school_path(@school) if not_signed_in? && !@school.data_enabled
-    end
-
-    def set_counts
-      @priority_count = latest_management_priorities.count
-      @alert_count = latest_dashboard_alerts.count
     end
 
     def latest_dashboard_alerts
