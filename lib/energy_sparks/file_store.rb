3875c66f0fe148aed64744d9181009e937f48034
Leigh Dodds <leigh@ldodds.com>
2025-05-12 10:42:44 +0100

    Comments and prod

--- lib/energy_sparks/file_store.rb
+++ lib/energy_sparks/file_store.rb
@@ -2,6 +2,12 @@
 
 module EnergySparks
   class FileStore < ActiveSupport::Cache::FileStore
+    # Check to see if there is a file for the corresponding cache key.
+    #
+    # This is a more optimal way of checking whether an item is in the
+    # cache for large, unversioned entries that do not expire.
+    #
+    # Only used in AggregateSchoolService.in_cache?
     def exists_on_disk?(cache_key)
       internal_key = normalize_key(cache_key, merged_options(nil))
       File.exist?(internal_key)
