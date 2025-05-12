ce3ed003a995f404756c49665edc5e075df0aa2e
Leigh Dodds <leigh@ldodds.com>
2025-05-12 16:31:56 +0100

    Remove extra spec

--- spec/mailers/alert_mailer_spec.rb
+++ spec/mailers/alert_mailer_spec.rb
@@ -66,14 +66,6 @@ RSpec.describe AlertMailer do
         expect(email.mailgun_headers['X-Mailgun-Tag']).to eql 'alerts'
       end
 
-      it 'sends an email with a mailgun deliverytime option' do
-        AlertMailer.with(users: users, school: school, events: []).alert_email.deliver_now
-        expect(ActionMailer::Base.deliveries.count).to be 1
-        deliverytime = email.mailgun_options[:deliverytime]
-        expect(deliverytime).not_to be_nil
-        expect(DateTime.rfc2822(deliverytime)).to be_between(Time.zone.now, Time.zone.now + 15.minutes)
-      end
-
       it 'specifies a subject' do
         AlertMailer.with(users: users, school: school, events: []).alert_email.deliver_now
         expect(email.subject).to eql I18n.t('alert_mailer.alert_email.subject_2024', school_name: school.name)
