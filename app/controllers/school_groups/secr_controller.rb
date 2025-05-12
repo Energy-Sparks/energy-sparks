86abef33c8a34f13519d05acb814b686d484166b
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-05-12 19:35:38 +0000

    more

--- app/controllers/school_groups/secr_controller.rb
+++ app/controllers/school_groups/secr_controller.rb
@@ -6,6 +6,57 @@ module SchoolGroups
 
     def index
       raise CanCan::AccessDenied unless current_user.admin? || current_user.group_admin?
+
+      @start_date = MeterMonthlySummary.start_date(Time.zone.today, 2)
+      @meters = @school_group.meters.active.where('schools.active')
+      respond_to do |format|
+        format.html
+        format.csv do
+          send_data csv_report(params[:csv]), filename: EnergySparks::Filenames.csv("secr-#{params[:csv]}")
+        end
+      end
+    end
+
+    private
+
+    TYPE_MAPPING = { self: :self_consume, export: :export }.stringify_keys.freeze
+    private_constant :TYPE_MAPPING
+
+    def csv_report(type)
+      CSV.generate do |csv|
+        csv << [
+          t('common.school'),
+          'MPXN',
+          'Meter serial',
+          'Meter name',
+          'Consumption for the year',
+          ((9..12).map { |m| [@start_date.year, m] } +
+           (1..8).map { |m| [@start_date.year + 1, m] })
+               .map { |year, month| [Date.new(year, month, 1).strftime('%b-%Y'), 'Quality'] },
+          'Earliest validated reading',
+          'Latest validated reading'
+        ].flatten
+        @meters.public_send(type == 'gas' ? :gas : :electricity)
+               .order('schools.name, meters.mpan_mprn').each do |meter|
+          summary = meter.meter_monthly_summaries.find_by(year: @start_date.year,
+                                                          type: TYPE_MAPPING.fetch(type, :consumption))
+          next if summary.nil?
+
+          csv << [
+            meter.school.name,
+            meter.mpan_mprn,
+            meter.meter_serial_number,
+            meter.name,
+            summary.total.round(2),
+            ((9..12).to_a + (1..8).to_a).map do |m|
+              [summary.consumption[m]&.round(2),
+               summary.quality[m]&.[](0)&.upcase]
+            end,
+            meter.amr_validated_readings.minimum(:reading_date),
+            meter.amr_validated_readings.maximum(:reading_date)
+          ].flatten
+        end
+      end
     end
   end
 end
