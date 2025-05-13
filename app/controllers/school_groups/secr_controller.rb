dd79accf2e0f267ce07e1b5eaac5920ce0b847e6
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-05-13 17:48:31 +0000

    more

--- app/controllers/school_groups/secr_controller.rb
+++ app/controllers/school_groups/secr_controller.rb
@@ -12,30 +12,34 @@ module SchoolGroups
       respond_to do |format|
         format.html
         format.csv do
-          send_data csv_report(params[:csv]), filename: EnergySparks::Filenames.csv("secr-#{params[:csv]}")
+          type = params[:csv]
+          send_data csv_report(type), filename: EnergySparks::Filenames.csv("secr-#{type}")
         end
       end
     end
 
     private
 
+    def csv_headers
+      [t('common.school'),
+       'MPXN',
+       'Meter serial',
+       'Meter name',
+       'Consumption for the year',
+       ((9..12).map { |m| [@start_date.year, m] } +
+        (1..8).map { |m| [@start_date.year + 1, m] })
+            .map { |year, month| [Date.new(year, month, 1).strftime('%b-%Y'), 'Quality'] },
+       'Earliest validated reading',
+       'Latest validated reading'
+        ].flatten
+    end
+
     TYPE_MAPPING = { self: :self_consume, export: :export }.stringify_keys.freeze
     private_constant :TYPE_MAPPING
 
     def csv_report(type)
       CSV.generate do |csv|
-        csv << [
-          t('common.school'),
-          'MPXN',
-          'Meter serial',
-          'Meter name',
-          'Consumption for the year',
-          ((9..12).map { |m| [@start_date.year, m] } +
-           (1..8).map { |m| [@start_date.year + 1, m] })
-               .map { |year, month| [Date.new(year, month, 1).strftime('%b-%Y'), 'Quality'] },
-          'Earliest validated reading',
-          'Latest validated reading'
-        ].flatten
+        csv << csv_headers
         @meters.public_send(type == 'gas' ? :gas : :electricity)
                .order('schools.name, meters.mpan_mprn').each do |meter|
           summary = meter.meter_monthly_summaries.find_by(year: @start_date.year,
@@ -48,9 +52,8 @@ module SchoolGroups
             meter.meter_serial_number,
             meter.name,
             summary.total.round(2),
-            ((9..12).to_a + (1..8).to_a).map do |m|
-              [summary.consumption[m]&.round(2),
-               summary.quality[m]&.[](0)&.upcase]
+            ((8..11).to_a + (0..7).to_a).map do |m|
+              [summary.consumption[m]&.round(2), summary.quality[m]&.[](0)&.upcase]
             end,
             meter.amr_validated_readings.minimum(:reading_date),
             meter.amr_validated_readings.maximum(:reading_date)
