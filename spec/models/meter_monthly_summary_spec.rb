a367670cd20d52f17686a34b882373f6740c1d32
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-05-13 09:34:43 +0000

    more

--- spec/models/meter_monthly_summary_spec.rb
+++ spec/models/meter_monthly_summary_spec.rb
@@ -7,6 +7,30 @@ describe MeterMonthlySummary do
     it 'create and updates' do
       end_date = Date.new(2019, 6, 2)
       start_date = Date.new(2019, 5, 1)
+      meter_collection, meter = setup_meters(start_date, end_date)
+      travel_to(end_date + 1.day)
+      described_class.create_or_update_from_school(meter.school, meter_collection)
+      expect_summaries(meter, ([0] * 4) + [4309, 278], 4587)
+
+      create(:amr_validated_reading, meter:, reading_date: Time.zone.today)
+      described_class.create_or_update_from_school(meter.school, meter_collection)
+      expect_summaries(meter, ([0] * 4) + [4309, 417], 4726)
+    end
+
+    def expect_summaries(meter, consumption, total)
+      expect(meter.meter_monthly_summaries.reload).to contain_exactly(
+        have_attributes(year: 2018, type: 'consumption', consumption:,
+                        quality: ([nil] * 4) + %w[actual incomplete], total:),
+        have_attributes(year: 2018, type: 'generation', consumption: ([0] * 4) + [1488, 96],
+                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584),
+        have_attributes(year: 2018, type: 'self_consume', consumption: ([0] * 4) + [1488, 96],
+                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584),
+        have_attributes(year: 2018, type: 'export', consumption: ([0] * 4) + [1488, 96],
+                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584)
+      )
+    end
+
+    def setup_meters(start_date, end_date)
       sub_meters = {
         mains_consume: build(:meter, amr_data: build(:amr_data, :with_date_range, start_date:, end_date:)),
         generation: build(:meter, amr_data: build(:amr_data, :with_date_range, start_date:, end_date:,
@@ -20,33 +44,7 @@ describe MeterMonthlySummary do
       meter = create(:electricity_meter_with_validated_reading_dates, start_date:, end_date:)
       create(:solar_pv_attribute, meter:)
       meter_collection.aggregated_electricity_meters.set_mpan_mprn_id(meter.mpan_mprn)
-
-      travel_to(end_date + 1.day)
-
-      described_class.create_or_update_from_school(meter.school, meter_collection)
-      expect(meter.meter_monthly_summaries).to contain_exactly(
-        have_attributes(year: 2018, type: 'consumption', consumption: ([0] * 4) + [4309, 278],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 4587),
-        have_attributes(year: 2018, type: 'generation', consumption: ([0] * 4) + [1488, 96],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584),
-        have_attributes(year: 2018, type: 'self_consume', consumption: ([0] * 4) + [1488, 96],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584),
-        have_attributes(year: 2018, type: 'export', consumption: ([0] * 4) + [1488, 96],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584)
-      )
-
-      create(:amr_validated_reading, meter:, reading_date: Time.zone.today)
-      described_class.create_or_update_from_school(meter.school, meter_collection)
-      expect(meter.meter_monthly_summaries.reload).to contain_exactly(
-        have_attributes(year: 2018, type: 'consumption', consumption: ([0] * 4) + [4309, 417],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 4726),
-        have_attributes(year: 2018, type: 'generation', consumption: ([0] * 4) + [1488, 96],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584),
-        have_attributes(year: 2018, type: 'self_consume', consumption: ([0] * 4) + [1488, 96],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584),
-        have_attributes(year: 2018, type: 'export', consumption: ([0] * 4) + [1488, 96],
-                        quality: ([nil] * 4) + %w[actual incomplete], total: 1584)
-      )
+      [meter_collection, meter]
     end
   end
 end
