5a22dcf6a9cdb6c260684858aaef7f24be3860de
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-05-21 17:06:02 +0000

    more

--- app/controllers/school_groups/secr_controller.rb
+++ app/controllers/school_groups/secr_controller.rb
@@ -22,15 +22,14 @@ module SchoolGroups
     def csv_headers
       [t('common.school'),
        'MPXN',
-       'Meter serial',
-       'Meter name',
-       'Consumption for the year',
+       t('school_groups.secr.csv.meter_serial'),
+       t('school_groups.secr.csv.meter_name'),
+       t('school_groups.secr.csv.consumption_for_the_year'),
        ((9..12).map { |m| [@start_date.year, m] } +
         (1..8).map { |m| [@start_date.year + 1, m] })
-            .map { |year, month| [Date.new(year, month, 1).strftime('%b-%Y'), 'Quality'] },
-       'Earliest validated reading',
-       'Latest validated reading'
-        ].flatten
+            .map { |year, month| [Date.new(year, month, 1).strftime('%b-%Y'), t('school_groups.secr.csv.quality')] },
+       t('school_groups.secr.earliest_validated_reading'),
+       t('school_groups.secr.latest_validated_reading')].flatten
     end
 
     TYPE_MAPPING = { self: :self_consume, export: :export }.stringify_keys.freeze
