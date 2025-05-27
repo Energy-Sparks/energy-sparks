844813569bc274a8bb4ec7f41077238f973945bb
Leigh Dodds <leigh@ldodds.com>
2025-05-27 16:41:52 +0100

    Rework report and add spec

---
+++ spec/system/admin/reports/baseload_anomaly_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Baseload anomaly report' do
+  let(:meter) { create(:electricity_meter, school: create(:school, :with_school_group)) }
+
+  let(:reading_date) { Date.yesterday }
+
+  before do
+    create(:amr_validated_reading, meter: meter, reading_date: reading_date - 1, kwh_data_x48: Array.new(48, 10.0))
+    create(:amr_validated_reading, meter: meter, reading_date: reading_date, kwh_data_x48: Array.new(48, 0.0))
+    Report::BaseloadAnomaly.refresh
+    sign_in(create(:admin))
+    visit admin_reports_path
+    click_on 'Baseload Anomalies'
+  end
+
+  it 'displays the table' do
+    rows = all('tr').map { |tr| tr.all('th, td').map(&:text) }
+    expect(rows).to eq([
+                         ['School Group', 'School', 'Meter', 'Meter Name', 'Reading Date', 'Previous Baseload', 'Baseload', 'Chart'],
+                         [meter.school_group.name, meter.school.name, meter.mpan_mprn.to_s, meter.name, reading_date.iso8601, '20', '0', 'Chart']
+                       ])
+  end
+
+  it 'allows csv download' do
+    click_on 'Download as CSV'
+    expect(page.response_headers['content-type']).to eq('text/csv')
+    expect(body).to \
+      eq("School Group,School,Meter,Meter Name,Reading Date,Previous Baseload,Baseload,Chart\n" \
+         "#{meter.school_group.name},#{meter.school.name},#{meter.mpan_mprn},#{meter.name},#{reading_date},20,0,#{analysis_school_advice_baseload_url(meter.school, host: 'example.com')}\n")
+  end
+end
