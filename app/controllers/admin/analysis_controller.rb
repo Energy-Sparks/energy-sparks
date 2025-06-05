c3616fbd1278b97600d0dde31ba8602d033d5230
Leigh Dodds <leigh@ldodds.com>
2025-06-05 14:23:14 +0100

    Fix footnotes

--- app/controllers/admin/analysis_controller.rb
+++ app/controllers/admin/analysis_controller.rb
@@ -5,11 +5,9 @@ module Admin
     before_action :set_school
 
     include SchoolAggregation
-    include Measurements
 
     before_action :check_aggregated_school_in_cache
     before_action :build_aggregate_school
-    before_action :set_measurement_options
 
     layout 'dashboards'
 
@@ -19,16 +17,12 @@ module Admin
     end
 
     def show
+      @overview_data = ::Schools::ManagementTableService.new(@school).management_data
       extra_chart_config = {}
       extra_chart_config[:mpan_mprn] = params[:mpan_mprn] if params[:mpan_mprn].present?
-
-      @measurement = measurement_unit(params[:measurement])
-
-      @chart_config = { y_axis_units: @measurement }.merge(extra_chart_config)
-
+      @chart_config = { y_axis_units: :kwh }.merge(extra_chart_config)
       @title = title_and_chart_configuration[:name]
       @charts = title_and_chart_configuration[:charts]
-      @show_measurement_units = title_and_chart_configuration.fetch(:change_measurement_units, true)
     end
 
     private
