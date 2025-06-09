183d0bfe1abc577b9f3abff0f6c938d184569094
Deb Bassett <deb@urbanwide.com>
2025-06-10 00:53:28 +0100

    fix

--- spec/factories/case_studies_factory.rb
+++ spec/factories/case_studies_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :case_study do
-    sequence(:title)  { |n| "Case study #{n}" }
-    sequence(:description) { |n| "Description for case study #{n}" }
+    sequence(:title_en) { |n| "Case study #{n}" }
+    sequence(:description_en) { |n| "Description for case study #{n}" }
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'images', 'newsletter-placeholder.png'), 'image/png') }
     file_en { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'documents', 'fake-bill.pdf'), 'application/pdf')}
     tags { 'tag one, tag two, tag three' }
