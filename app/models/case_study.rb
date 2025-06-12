cc511adb6c67af51701401c8e0a4315309a964a4
Deb Bassett <deb@urbanwide.com>
2025-06-12 17:26:59 +0100

    Remove image validation all together for now

--- app/models/case_study.rb
+++ app/models/case_study.rb
@@ -30,10 +30,6 @@ class CaseStudy < ApplicationRecord
   t_has_one_attached :file
   has_one_attached :image # assume this doesn't need to be translatable
 
-  validates :image,
-              # content_type: ['image/png', 'image/jpeg'],
-              dimension: { width: { min: 700, max: 1400 } } # betwen half and full container width size to be conservative
-
   validates :title_en, :file_en, presence: true
   validates :position, numericality: true, presence: true
 
