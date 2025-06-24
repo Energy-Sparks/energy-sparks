f959ec5c2a40ffd9693577d7080fe0c2d680ba45
Deb Bassett <deb.bassett@energysparks.uk>
2025-06-24 12:44:37 +0100

    Remove new_home_page feature flag (#4536)

--- app/services/blog_service.rb
+++ app/services/blog_service.rb
@@ -29,8 +29,6 @@ class BlogService
 
   # To be run from cron
   def update_cache!
-    return unless Flipper.enabled?(:new_home_page)
-
     items = fetch_items
     if items&.any?
       Rails.cache.write(@key, items)
