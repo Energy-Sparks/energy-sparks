62acd787a21ec5d18766c66751eca3e02daa70c6
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-06-24 15:29:35 +0000

    more

--- spec/components/tabs_component_spec.rb
+++ spec/components/tabs_component_spec.rb
@@ -2,59 +2,57 @@
 
 require 'rails_helper'
 
-RSpec.describe TabsComponent, :include_url_helpers, type: :component do
-  def component_html(**kwargs)
+RSpec.describe TabsComponent, type: :component do
+  def render_component(**kwargs)
     render_inline(described_class.new(**kwargs)) do |component|
       component.with_tab(name: :first, label: 'First') { 'first tab content' }
       component.with_tab(name: :second, label: 'Second') { 'second tab content' }
     end
   end
 
-  def normalize_html(html)
-    html = Nokogiri::HTML5.fragment(html) if html.is_a?(String)
-    html.traverse do |node|
-      if node.text.strip.empty?
-        node.remove
-      else
-        node.content = node.text.strip
-      end
+  def expect_tab(tab, name, label, active)
+    expect(tab.attributes.transform_values(&:value).symbolize_keys).to eq(
+      { 'aria-controls': name,
+        'aria-selected': active.to_s,
+        'data-toggle': 'tab',
+        class: ['nav-link', active ? 'active' : nil].compact.join(' '),
+        href: "##{name}",
+        id: "#{name}-tab",
+        role: 'tab' }
+    )
+  end
+
+  def expect_tab_content(div, name, active, content, top_margin)
+    expect(div.attributes.transform_values(&:value).symbolize_keys).to eq(
+      { id: name,
+        class: (%w[tab-pane fade] + (active ? %w[show active] : [])).join(' '),
+        role: 'tabpanel',
+        'aria-labelledby': "#{name}-tab" }
+    )
+    if top_margin
+      expect(div).to have_css('div.mt-3', exact_text: content)
+    else
+      expect(div.text.strip).to eq(content)
     end
   end
 
-  let(:expected_html) do
-    <<~HTML
-      <ul class="here nav nav-tabs url-aware tabs-component" role="tablist">
-        <li class="nav-item">
-          <a id="first-tab" aria-controls="first" data-toggle="tab" role="tab" aria-selected="true"
-            class="nav-link active" href="#first">First</a>
-        </li>
-        <li class="nav-item">
-          <a id="second-tab" aria-controls="second" data-toggle="tab" role="tab" aria-selected="false" class="nav-link"
-            href="#second">Second</a>
-        </li>
-      </ul>
-      <div class="tab-content">
-        <div id="first" aria-labelledby="first-tab" role="tabpanel" class="tab-pane fade show active">
-          <div class="mt-3">first tab content</div>
-        </div>
-        <div id="second" aria-labelledby="second-tab" role="tabpanel" class="tab-pane fade">
-          <div class="mt-3">second tab content</div>
-        </div>
-      </div>
-    HTML
+  def expect_correct_html(html, top_margin)
+    tabs = html.css('li.nav-item a.nav-link')
+    expect(tabs.size).to eq(2)
+    expect_tab(tabs[0], 'first', 'First', true)
+    expect_tab(tabs[1], 'second', 'Second', false)
+
+    content_divs = html.css('.tab-content > div')
+    expect(content_divs.size).to eq(2)
+    expect_tab_content(content_divs[0], 'first', true, 'first tab content', top_margin)
+    expect_tab_content(content_divs[1], 'second', false, 'second tab content', top_margin)
   end
 
   it 'has the right html' do
-    expect(normalize_html(component_html)).to eq(normalize_html(expected_html))
+    expect_correct_html(render_component, true)
   end
 
   it 'has the right html with no top margin' do
-    html = render_inline(described_class.new(top_margin: false)) do |component|
-      component.with_tab(name: :first, label: 'First') { 'first tab content' }
-      component.with_tab(name: :second, label: 'Second') { 'second tab content' }
-    end
-    expected = Nokogiri::HTML5.fragment(component_html(top_margin: false))
-    expected.css('.mt-3').each { |node| node.replace(node.children) }
-    expect(normalize_html(html)).to eq(normalize_html(expected))
+    expect_correct_html(render_component(top_margin: false), false)
   end
 end
