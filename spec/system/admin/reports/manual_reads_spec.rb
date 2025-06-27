1bbaa8b151def9227d6acdf345633d198002b6da
Leigh Dodds <leigh@ldodds.com>
2025-06-27 17:59:16 +0100

    Fix spec

--- spec/system/admin/reports/manual_reads_spec.rb
+++ spec/system/admin/reports/manual_reads_spec.rb
@@ -13,16 +13,16 @@ describe 'Manual Reads Report' do
 
   it 'displays the table' do
     expect(all('tr').map { |tr| tr.all('th, td').map(&:text) }).to \
-      eq([['Group Name', 'School Name', 'Group Owner', 'MPAN', 'Meter Type', 'Data Source', 'Last Validated Date',
-           'Issues & Notes'],
-          [meter.school.school_group.name, meter.school.name, '', meter.mpan_mprn.to_s, 'gas', '', '', '']])
+      eq([['School Group', 'Admin', 'School', 'Meter', 'Meter Name', 'Meter Type', 'Data Source', 'Last Validated Date',
+           'Issues & Notes', 'Issues', 'Notes'],
+          [meter.school.school_group.name, '', meter.school.name, meter.mpan_mprn.to_s, meter.name, 'gas', '', '', '', '0', '0']])
   end
 
   it 'allows csv download' do
     click_on 'CSV'
     expect(page.response_headers['content-type']).to eq('text/csv')
     expect(body).to \
-      eq("Group Name,School Name,Group Owner,MPAN,Meter Type,Data Source,Last Validated Date,Issues,Notes\n" \
-         "#{meter.school.school_group.name},#{meter.school.name},,#{meter.mpan_mprn},gas,,,0,0\n")
+      eq("School Group,Admin,School,Meter,Meter Name,Meter Type,Data Source,Last Validated Date,Issues,Notes\n" \
+         "#{meter.school.school_group.name},,#{meter.school.name},#{meter.mpan_mprn},#{meter.name},gas,,,0,0\n")
   end
 end
