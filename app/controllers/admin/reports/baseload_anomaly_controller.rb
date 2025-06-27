9868aca1255e1a5f9a677525c803ba42926e6a0c
Leigh Dodds <leigh@ldodds.com>
2025-06-27 16:08:35 +0100

    Rework report and add framework

--- app/controllers/admin/reports/baseload_anomaly_controller.rb
+++ app/controllers/admin/reports/baseload_anomaly_controller.rb
@@ -2,47 +2,51 @@
 
 module Admin
   module Reports
-    class BaseloadAnomalyController < AdminController
-      include Columns
-      include ActionView::Helpers::UrlHelper
-      include ApplicationHelper
-
+    class BaseloadAnomalyController < MeterDataReportsController
       def index
-        @anomalies = Report::BaseloadAnomaly.all.with_meter_school_and_group.default_order
-        @columns = [
-          Column.new(:school_group,
-                     ->(anomaly) { anomaly.meter.school&.school_group&.name },
-                     ->(anomaly, csv) { csv && link_to(csv, school_group_path(anomaly.meter.school&.school_group)) }),
-          Column.new(:admin,
-                     ->(anomaly) { anomaly.meter.school&.school_group&.default_issues_admin_user&.name }),
-          Column.new(:school,
-                     ->(anomaly) { anomaly.meter.school.name },
-                     ->(anomaly, csv) { link_to(csv, school_path(anomaly.meter.school)) }),
-          Column.new(:meter,
-                     ->(anomaly) { anomaly.meter.mpan_mprn },
-                     ->(anomaly, csv) { link_to(csv, school_meter_path(anomaly.meter.school, anomaly.meter)) }),
-          Column.new(:meter_name,
-                     ->(anomaly) { anomaly.meter&.name }),
-          Column.new(:reading_date,
-                     ->(anomaly) { anomaly.reading_date }),
-          Column.new(:previous_baseload,
-                     ->(anomaly) {  FormatEnergyUnit.format(:kw, anomaly.previous_day_baseload.to_f, :html, false, true, :benchmark) },
-                     ->(anomaly) {  FormatEnergyUnit.format(:kw, anomaly.previous_day_baseload.to_f, :text, false, true, :benchmark) }),
-          Column.new(:baseload,
-                     ->(anomaly) {  FormatEnergyUnit.format(:kw, anomaly.today_baseload.to_f, :html, false, true, :benchmark) },
-                     ->(anomaly) {  FormatEnergyUnit.format(:kw, anomaly.today_baseload.to_f, :text, false, true, :benchmark) }),
-          Column.new(:chart,
-                     ->(anomaly) { analysis_school_advice_baseload_url(anomaly.meter.school) },
-                     ->(anomaly) { link_to('Chart', analysis_school_advice_baseload_path(anomaly.meter.school))},
-                     html_data: { sortable: false })
-        ]
+        @rows = Report::BaseloadAnomaly.all.with_meter_school_and_group
+        @rows = @rows.for_school_group(SchoolGroup.find(params[:school_group])) if params[:school_group].present?
+        @rows = @rows.for_admin(User.admin.find(params[:user])) if params[:user].present?
+        @rows = @rows.default_order
+
         respond_to do |format|
           format.html
           format.csv do
-            send_data(csv_report(@columns, @anomalies),
+            send_data(csv_report(@columns, @rows),
                       filename: EnergySparks::Filenames.csv('baseload-anomalies'))
           end
         end
+      end
+
+      private
+
+      def columns
+        super + [
+          Column.new(:reading_date,
+                     ->(row) { row.reading_date }),
+          Column.new(:previous_baseload,
+                     ->(row) {  FormatEnergyUnit.format(:kw, row.previous_day_baseload.to_f, :html, false, true, :benchmark) },
+                     ->(row) {  FormatEnergyUnit.format(:kw, row.previous_day_baseload.to_f, :text, false, true, :benchmark) }),
+          Column.new(:baseload,
+                     ->(row) {  FormatEnergyUnit.format(:kw, row.today_baseload.to_f, :html, false, true, :benchmark) },
+                     ->(row) {  FormatEnergyUnit.format(:kw, row.today_baseload.to_f, :text, false, true, :benchmark) }),
+          Column.new(:chart,
+                     ->(row) { analysis_school_advice_baseload_url(row.meter.school) },
+                     ->(row) { link_to('Chart', analysis_school_advice_baseload_path(row.meter.school))},
+                     html_data: { sortable: false })
+        ]
+      end
+
+      def path
+        'admin_reports_baseload_anomaly_index_path'
+      end
+
+      def description
+        'Shows sudden changes in baseload for active electricity meters over the last 30 days.'
+      end
+
+      def title
+        'Baseload anomalies'
       end
     end
   end
---
+++ app/controllers/admin/reports/meter_data_reports_controller.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module Admin
+  module Reports
+    class MeterDataReportsController < AdminController
+      include Columns
+      include ActionView::Helpers::UrlHelper
+      include ApplicationHelper
+      before_action :set_metadata
+      before_action :set_columns
+
+      private
+
+      def set_metadata
+        @title = title
+        @description = description
+        @frequency = frequency
+        @path = path
+      end
+
+      def title; end
+      def description; end
+      def path; end
+
+      def frequency
+        :daily
+      end
+
+      def set_columns
+        @columns = columns
+      end
+
+      def columns
+        [
+          Column.new(:school_group,
+                     ->(row) { row.meter.school&.school_group&.name },
+                     ->(row, csv) { csv && link_to(csv, school_group_path(row.meter.school&.school_group)) }),
+          Column.new(:admin,
+                     ->(row) { row.meter.school&.school_group&.default_issues_admin_user&.name }),
+          Column.new(:school,
+                     ->(row) { row.meter.school.name },
+                     ->(row, csv) { link_to(csv, school_path(row.meter.school)) }),
+          Column.new(:meter,
+                     ->(row) { row.meter.mpan_mprn },
+                     ->(row, csv) { link_to(csv, school_meter_path(row.meter.school, row.meter)) }),
+          Column.new(:meter_name,
+                     ->(row) { row.meter&.name })
+        ]
+      end
+
+      def set_breadcrumbs
+        @breadcrumbs = [
+          { name: 'Admin', href: admin_path },
+          { name: 'Reports', href: admin_reports_path },
+          { name: title }
+        ]
+      end
+    end
+  end
+end
