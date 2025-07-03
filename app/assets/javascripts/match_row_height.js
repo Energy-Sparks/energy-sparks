9a7123445530abbe6af77dafe176ebe0673e0e34
Deb Bassett <deb@urbanwide.com>
2025-07-03 09:55:57 +0100

    Comment

--- app/assets/javascripts/match_row_height.js
+++ app/assets/javascripts/match_row_height.js
@@ -1,3 +1,10 @@
+/**
+ * This ensures that all elements with the class `match-row-height` inside the same
+ * `.row` container have the same height as the tallest element.
+ *
+ * The function is automatically triggered on the `load` and `resize` events of the window.
+*/
+
 function matchHeightsInRows() {
   document.querySelectorAll('.row').forEach(row => {
     const items = row.querySelectorAll('.match-row-height');
