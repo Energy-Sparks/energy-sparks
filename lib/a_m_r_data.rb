774fd6522e59a676fb0b34fdb88670c841f95e94
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-07-03 17:49:49 +0000

    more

--- lib/a_m_r_data.rb
+++ lib/a_m_r_data.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'dashboard/utilities/half_hourly_data'
+
 # Timed data from a meter
 # references consumption data, costs and carbon emissions
 class AMRData < HalfHourlyData
