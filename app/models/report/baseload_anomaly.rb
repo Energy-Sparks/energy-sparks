f90122a1cd586cad4c8e78e8ef64b2c954e1c8b7
Leigh Dodds <leigh@ldodds.com>
2025-07-08 09:28:00 +0100

    spec/support/shared_examples/admin_meter_reports.rb

--- app/models/report/baseload_anomaly.rb
+++ app/models/report/baseload_anomaly.rb
@@ -16,13 +16,9 @@
 #
 module Report
   class BaseloadAnomaly < ApplicationRecord
-    self.table_name_prefix = 'report_'
+    include Metered
 
-    belongs_to :meter
-    scope :with_meter_school_and_group, -> { includes(:meter, meter: [:school, { school: :school_group }]) }
-    scope :for_school_group, ->(school_group) { where(meter: { schools: { school_group: school_group } }) }
-    scope :for_admin, ->(admin) { where(meter: { schools: { school_groups: { default_issues_admin_user: admin } } }) }
-    scope :default_order, -> { order(:meter_id, :reading_date) }
+    self.table_name_prefix = 'report_'
 
     def readonly?
       true
--- app/models/report/gas_anomaly.rb
+++ app/models/report/gas_anomaly.rb
@@ -24,12 +24,9 @@ module Report
   class GasAnomaly < ApplicationRecord
     self.table_name_prefix = 'report_'
 
-    belongs_to :meter
+    include Metered
+
     belongs_to :calendar_event_type
-    scope :with_meter_school_and_group, -> { includes(:meter, meter: [:school, { school: :school_group }]) }
-    scope :for_school_group, ->(school_group) { where(meter: { schools: { school_group: school_group } }) }
-    scope :for_admin, ->(admin) { where(meter: { schools: { school_groups: { default_issues_admin_user: admin } } }) }
-    scope :default_order, -> { order(:meter_id, :reading_date) }
 
     def readonly?
       true
