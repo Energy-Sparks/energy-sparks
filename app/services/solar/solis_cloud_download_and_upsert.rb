913e734f238cce5bdbed71b03ef051db294164ad
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-07-15 13:35:41 +0000

    more

--- app/services/solar/solis_cloud_download_and_upsert.rb
+++ app/services/solar/solis_cloud_download_and_upsert.rb
@@ -17,23 +17,6 @@ module Solar
       :solis_cloud_download
     end
 
-    def time_to_index(time)
-      split = time.split(':')
-      index = split[0].to_i * 2
-      index += 1 if split[1].to_i > 30
-      index
-    end
-
-    TO_HOUR = 5 / 60.0
-
-    def create_kwh_data_x48(data)
-      x48 = Array.new(48, 0.0)
-      data.each do |entry|
-        x48[time_to_index(entry['timeStr'])] += (entry['power'] / 1000.0) * TO_HOUR
-      end
-      x48
-    end
-
     def start_date(date_string: nil, meter: nil)
       if @requested_start_date
         @requested_start_date
