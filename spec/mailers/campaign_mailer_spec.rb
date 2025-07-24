1979c31ca43aa1b02852a42ecedcf6a4c843c455
Leigh Dodds <leigh@ldodds.com>
2025-07-24 10:37:56 +0100

    Mailer specs

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
+RSpec.shared_examples 'a campaign mail was sent' do
+  subject(:mail) { ActionMailer::Base.deliveries.last }
+
+  it 'send email with expected subject' do
+    expect(mail.subject).to eq(email_subject)
+  end
+
+  it 'includes contact name' do
+    expect(mail.html_part.decoded).to include(name)
+  end
+
+  it 'sends to correct email' do
+    expect(mail.to).to eq([email])
+  end
+end
+
 RSpec.describe CampaignMailer do
   let(:email) { ActionMailer::Base.deliveries.last }
   let(:body) { email.html_part.body.raw_source }
@@ -86,16 +102,10 @@ RSpec.describe CampaignMailer do
 
     let(:org_type) { ['primary'] }
 
-    it 'send email with expected subject' do
-      expect(email.subject).to eq(I18n.t('campaign_mailer.school_demo.subject'))
-    end
-
-    it 'includes contact name' do
-      expect(email.html_part.decoded).to include('Jane')
-    end
-
-    it 'sends to correct email' do
-      expect(email.to).to eq(['jane@example.org'])
+    it_behaves_like 'a campaign mail was sent' do
+      let(:name) { contact[:first_name] }
+      let(:email) { contact[:email] }
+      let(:email_subject) { I18n.t('campaign_mailer.school_demo.subject') }
     end
 
     it 'includes links to demos' do
@@ -120,46 +130,83 @@ RSpec.describe CampaignMailer do
 
     let(:org_type) { ['primary'] }
 
-    it 'send email with expected subject' do
-      expect(email.subject).to eq(I18n.t('campaign_mailer.send_information_school.subject'))
+    it_behaves_like 'a campaign mail was sent' do
+      let(:name) { contact[:first_name] }
+      let(:email) { contact[:email] }
+      let(:email_subject) { I18n.t('campaign_mailer.send_information_school.subject') }
     end
 
-    it 'includes contact name' do
-      expect(email.html_part.decoded).to include('Jane')
+    it 'includes the tool section' do
+      expect(body).to have_content(I18n.t('campaigns.school_info.tool.title'))
+      expect(body).to have_link(href: introductory_video_campaigns_url)
+      expect(body).to have_link(href: energy_efficiency_report_campaigns_url)
     end
 
-    it 'sends to correct email' do
-      expect(email.to).to eq(['jane@example.org'])
+    it 'includes the closer look section' do
+      expect(body).to have_content(I18n.t('campaigns.school_info.closer_look.title'))
+      expect(body).to have_link(href: short_demo_video_campaigns_url)
+      expect(body).to have_link(href: long_demo_video_campaigns_url)
+      expect(body).to have_link(href: product_url)
+      expect(body).to have_link(href: schools_url)
     end
 
-    it 'sends the right content'
+    it 'includes the next steps section' do
+      expect(body).to have_content(I18n.t('campaigns.school_info.next_steps.title'))
+      expect(body).to have_link(href: short_demo_video_campaigns_url)
+      expect(body).to have_link(href: introductory_video_campaigns_url)
+      expect(body).to have_link(href: impact_report_campaigns_url)
+      expect(body).to have_link(href: new_mailchimp_signup_url)
+      expect(body).to have_link('complete our enrolment form', href: enrol_our_school_url)
+    end
   end
 
   describe '#send_information_group' do
     before do
-      CampaignMailer.with(contact: contact).send_information_school.deliver
+      CampaignMailer.with(contact: contact).send_information_group.deliver
     end
 
     let(:org_type) { ['multi_academy_trust'] }
 
-    it 'send email with expected subject' do
-      expect(email.subject).to eq(I18n.t('campaign_mailer.send_information_school.subject'))
+    it_behaves_like 'a campaign mail was sent' do
+      let(:name) { contact[:first_name] }
+      let(:email) { contact[:email] }
+      let(:email_subject) { I18n.t('campaign_mailer.send_information_school.subject') }
     end
 
-    it 'includes contact name' do
-      expect(email.html_part.decoded).to include('Jane')
+    it 'includes the tool section' do
+      expect(body).to have_content(I18n.t('campaigns.school_info.tool.title'))
+      expect(body).to have_link(href: introductory_video_campaigns_url)
+      expect(body).to have_link(href: energy_efficiency_report_campaigns_url)
     end
 
-    it 'sends to correct email' do
-      expect(email.to).to eq(['jane@example.org'])
+    it 'includes the closer look section' do
+      expect(body).to have_content(I18n.t('campaigns.school_info.closer_look.title'))
+      expect(body).to have_link(href: example_mat_dashboard_campaigns_url)
+      expect(body).not_to have_link(href: example_la_dashboard_campaigns_url)
+      expect(body).to have_link(href: schools_url(scope: :school_groups))
+      expect(body).to have_link(href: product_url)
     end
 
-    it 'sends the right content'
+    it 'includes the next steps section' do
+      expect(body).to have_content(I18n.t('campaigns.school_info.next_steps.title'))
+      expect(body).to have_link(href: enrol_our_multi_academy_trust_url)
+      expect(body).not_to have_link(href: enrol_our_local_authority_url)
+      expect(body).to have_link(href: watch_demo_campaigns_url)
+      expect(body).to have_link(href: new_mailchimp_signup_url)
+    end
 
     context 'when sending for a local authority' do
       let(:org_type) { ['local_authority'] }
 
-      it 'sends the right content'
+      it 'includes the closer look section' do
+        expect(body).to have_link(href: example_la_dashboard_campaigns_url)
+        expect(body).not_to have_link(href: example_mat_dashboard_campaigns_url)
+      end
+
+      it 'includes the next steps section' do
+        expect(body).to have_link(href: enrol_our_local_authority_url)
+        expect(body).not_to have_link(href: enrol_our_multi_academy_trust_url)
+      end
     end
   end
 end
