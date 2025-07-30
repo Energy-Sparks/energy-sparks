9fcda5a791ee8e406d16e9d3d7823ecce68993e2
Leigh Dodds <leigh@ldodds.com>
2025-07-30 14:45:54 +0100

    Test ranking

--- spec/models/cms/section_spec.rb
+++ spec/models/cms/section_spec.rb
@@ -22,6 +22,16 @@ describe Cms::Section do
       expect(results).to be_empty
     end
 
+    context 'when ranking results' do
+      let(:query) { 'Lorem' }
+
+      it 'returns in expected order' do
+        ranked_lower = create(:section, title: 'Lorem ipsum', published: true)
+        ranked_higher = create(:section, title: 'Lorem lorem ipsum. Ipsum Lorem. Lorem', published: true)
+        expect(results.first).to eq(ranked_higher)
+      end
+    end
+
     context 'when searching for unpublished' do
       let(:show_all) { true }
 
