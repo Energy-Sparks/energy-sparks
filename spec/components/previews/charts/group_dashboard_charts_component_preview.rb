f75dc7041992276734bc67f498ead689449a77a3
Leigh Dodds <leigh@ldodds.com>
2025-08-07 16:53:39 +0100

    Improve preview

--- spec/components/previews/charts/group_dashboard_charts_component_preview.rb
+++ spec/components/previews/charts/group_dashboard_charts_component_preview.rb
@@ -1,9 +1,19 @@
 module Charts
   class GroupDashboardChartsComponentPreview < ViewComponent::Preview
-    def default
-      render(
-        Charts::GroupDashboardChartsComponent.new(school_group: SchoolGroup.find(19))
-      )
+    def example(slug: nil, title: 'Energy use')
+      school_group = school_group(slug)
+      component = Charts::GroupDashboardChartsComponent.new(school_group:)
+      component.with_title do
+        "<h1>#{title} (#{school_group.name})</h1>".html_safe
+      end
+      render(component)
+    end
+
+    private
+
+    def school_group(slug)
+      return SchoolGroup.with_visible_schools.sample unless slug.present?
+      SchoolGroup.find(slug)
     end
   end
 end
