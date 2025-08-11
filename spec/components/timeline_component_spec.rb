7225f3be1079fbec7072bc1ed21744212452c613
Leigh Dodds <leigh@ldodds.com>
2025-08-11 12:52:44 +0100

    Fix spec

--- spec/components/timeline_component_spec.rb
+++ spec/components/timeline_component_spec.rb
@@ -4,20 +4,16 @@ require 'rails_helper'
 
 RSpec.describe TimelineComponent, type: :component, include_url_helpers: true do
   let(:observation) { create(:observation, :activity, points: 10) }
-  let(:school) { observation.school }
   let(:observations) { [observation] }
-  let(:show_actions) { true }
-  let(:show_header) { true }
   let(:classes) { 'my-class' }
   let(:id) { 'my-id' }
   let(:params) do
     {
       observations: observations,
-      school: school,
       classes: classes,
       id: id,
-      show_actions: show_actions,
-      show_header: show_header,
+      show_actions: true,
+      show_header: true,
       user: user
     }
   end
@@ -25,7 +21,12 @@ RSpec.describe TimelineComponent, type: :component, include_url_helpers: true do
   let(:user) { create(:admin) }
 
   subject(:html) do
-    render_inline(TimelineComponent.new(**params))
+    render_inline(TimelineComponent.new(**params)) do |c|
+      c.with_link do
+        ActionController::Base.helpers.link_to I18n.t('activities.show.all_activities'),
+                                               school_timeline_path(observation.school)
+      end
+    end
   end
 
   it_behaves_like 'an application component' do
@@ -35,7 +36,7 @@ RSpec.describe TimelineComponent, type: :component, include_url_helpers: true do
 
   it { expect(html).to have_content(I18n.t('timeline.whats_been_going_on'))}
   it { expect(html).to have_content(I18n.t('schools.dashboards.timeline.intro'))}
-  it { expect(html).to have_link(I18n.t('activities.show.all_activities')), href: school_timeline_path(school)}
+  it { expect(html).to have_link(I18n.t('activities.show.all_activities')), href: school_timeline_path(observation.school)}
 
   it { expect(html).to have_selector(:table_row, [observation.at.to_fs(:es_short), observation.points, observation.activity.display_name])}
 
