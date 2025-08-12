ae0a4cac1425f99eb8cab3b738904a0e0c198b25
Leigh Dodds <leigh@ldodds.com>
2025-08-12 14:36:29 +0100

    Add scoreboard link

--- spec/components/scoreboards/group_summary_component_spec.rb
+++ spec/components/scoreboards/group_summary_component_spec.rb
@@ -25,8 +25,17 @@ RSpec.describe Scoreboards::GroupSummaryComponent, :include_url_helpers, type: :
 
     it { expect(html).not_to have_content(I18n.t('components.scoreboards.group_summary.podium.no_points_this_year'))}
 
-    it { expect(html).to have_content('Your highest scoring school') }
-    it { expect(html).to have_link(href: current_scores_school_group_path(school_group)) }
+    it 'shows podium correctly' do
+      within('.scoreboards-podium-component') do
+        expect(html).to have_content('Your highest scoring school')
+        expect(html).to have_link(href: current_scores_school_group_path(school_group))
+      end
+    end
+
+    it 'links to scoreboard' do
+      expect(html).to have_link(I18n.t('components.scoreboard_summary.view_scoreboard'),
+                                href: current_scores_school_group_path(school_group))
+    end
 
     it { expect(html).to have_css('.timeline-component')}
   end
