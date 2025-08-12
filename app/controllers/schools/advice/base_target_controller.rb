4afe9ec9146076604d9b5c467733195edbf6e433
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-08-12 10:44:38 +0000

    more

--- app/controllers/schools/advice/base_target_controller.rb
+++ app/controllers/schools/advice/base_target_controller.rb
@@ -3,11 +3,21 @@
 module Schools
   module Advice
     class BaseTargetController < AdviceBaseController
-      def insights
-        @target = @school.most_recent_target
-      end
+      before_action :set_target, only: %i[insights analysis]
+
+      def insights; end
+
+      def analysis; end
 
       private
+
+      def set_target
+        @target = @school.most_recent_target
+        consumption = @target.monthly_consumption(@fuel_type).reject { |month| month[:missing] }
+        @last_consumption_month = consumption.last
+        @current_consumption = consumption.sum { |month| month[:current_consumption] }
+        @target_consumption = consumption.sum { |month| month[:target_consumption] }
+      end
 
       def advice_page_key
         @fuel_type = self.class.name.split('::').last.underscore.split('_').first.to_sym
@@ -17,6 +27,12 @@ module Schools
       def set_page_subtitle
         @advice_page_subtitle = I18n.t('advice_pages.target.insights.what_is_your_target.title')
       end
+
+      def percent_change(current_consumption, target_consumption)
+        (current_consumption - target_consumption) / target_consumption
+      end
+
+      helper_method :percent_change
     end
   end
 end
