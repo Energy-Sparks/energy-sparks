f783a298eb73c4c5416266841db83824a2bc5ba1
Leigh Dodds <leigh@ldodds.com>
2025-08-12 13:48:28 +0100

    i18n, preview

--- spec/components/previews/scoreboards/group_summary_component_preview.rb
+++ spec/components/previews/scoreboards/group_summary_component_preview.rb
@@ -1,6 +1,15 @@
 class Scoreboards::GroupSummaryComponentPreview < ViewComponent::Preview
+  # @param slug select :group_options
   def example(slug: nil)
     school_group = slug ? SchoolGroup.find(slug) : SchoolGroup.with_active_schools.sample
     render(Scoreboards::GroupSummaryComponent.new(school_group: school_group, user: User.admin.first))
+  end
+
+  private
+
+  def group_options
+    {
+      choices: SchoolGroup.with_active_schools.by_name.map { |g| [g.name, g.slug] }
+    }
   end
 end
