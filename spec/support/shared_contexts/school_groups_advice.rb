e03f07ad1d83a6adb2003268c4a7f220a589de62
Leigh Dodds <leigh@ldodds.com>
2025-08-19 15:11:29 +0100

    Add priorities page spec

--- spec/support/shared_contexts/school_groups_advice.rb
+++ spec/support/shared_contexts/school_groups_advice.rb
@@ -1,3 +1,55 @@
+RSpec.shared_examples 'an access controlled group advice page' do
+  let(:expected_redirect) { "/school_groups/#{school_group.slug}/map" }
+
+  context 'when the group is private' do
+    before do
+      school_group.update!(public: false)
+      sign_in(user) if user
+      visit path
+    end
+
+    context 'when not logged in' do
+      let(:user) { nil }
+
+      it 'has redirected' do
+        expect(page).to have_current_path(expected_redirect, ignore_query: true)
+      end
+    end
+
+    context 'when logged in' do
+      let(:user) { create(:group_admin, school_group: school_group) }
+
+      it 'has not redirected' do
+        expect(page).to have_current_path(path, ignore_query: true)
+      end
+    end
+  end
+
+  context 'when the group is public but there are no active schools' do
+    before do
+      school_group.schools.update_all(active: false)
+      sign_in(user) if user
+      visit path
+    end
+
+    context 'when not logged in' do
+      let(:user) { nil }
+
+      it 'has redirected' do
+        expect(page).to have_current_path(expected_redirect, ignore_query: true)
+      end
+    end
+
+    context 'when logged in' do
+      let(:user) { create(:group_admin, school_group: school_group) }
+
+      it 'has redirected' do
+        expect(page).to have_current_path(expected_redirect, ignore_query: true)
+      end
+    end
+  end
+end
+
 RSpec.shared_examples 'a school group advice page' do
   it 'displays the right breadcrumb' do
     expect(find('ol.main-breadcrumbs').all('li').collect(&:text)).to eq([I18n.t('common.schools'), school_group.name, breadcrumb])
@@ -11,10 +63,14 @@ RSpec.shared_examples 'a school group advice page' do
   end
 end
 
-RSpec.shared_examples 'it downloads a CSV correctly' do
-  it 'has the expected filename' do
+RSpec.shared_examples 'it exports a group CSV correctly' do
+  it 'the file has the expected name' do
     header = page.response_headers['Content-Disposition']
     expect(header).to match(/^attachment/)
     "#{I18n.t('common.application').parameterize}-#{school_group.name.parameterize}-#{action_name.parameterize}-#{Time.current.iso8601.tr(':', '-')}.csv"
+  end
+
+  it 'the file has the expected content' do
+    expect(CSV.parse(page.body, liberal_parsing: true)).to eq(expected_csv)
   end
 end
--- spec/system/school_groups/advice/priorities_spec.rb
+++ spec/system/school_groups/advice/priorities_spec.rb
@@ -8,63 +8,152 @@ describe 'School group priorities page' do
     let(:school_with_saving) { school }
   end
 
-  before do
-    visit priorities_school_group_advice_path(school_group)
+  it_behaves_like 'an access controlled group advice page' do
+    let(:path) { priorities_school_group_advice_path(school_group) }
   end
 
-  it_behaves_like 'a school group advice page' do
-    let(:breadcrumb) { I18n.t('advice_pages.index.priorities.title') }
-  end
-
-  it 'displays list of actions' do
-    expect(page).to have_css('#school-group-priorities')
-    within('#school-group-priorities') do
-      expect(page).to have_content('Spending too much money on heating')
-      expect(page).to have_content('£1,000')
-      expect(page).to have_content('1,100')
-      expect(page).to have_content('2,200')
-    end
-  end
-
-  context 'when downloading as a CSV' do
+  context 'when not signed in' do
     before do
-      click_link('Download as CSV', id: 'download-priority-actions-school-group-csv')
+      visit priorities_school_group_advice_path(school_group)
     end
 
-    it_behaves_like 'it downloads a CSV correctly' do
-      let(:action_name) { I18n.t('school_groups.titles.priority_actions') }
+    it_behaves_like 'a school group advice page' do
+      let(:breadcrumb) { I18n.t('advice_pages.index.priorities.title') }
     end
 
-    it 'has the expected contents' do
-      expect(page.source).to eq "Fuel,Description,Schools,Energy (kWh),Cost (£),CO2 (kg)\nGas,Spending too much money on heating,1,\"2,200\",\"£1,000\",\"1,100\"\n"
-    end
-  end
-
-  context 'with the modal showing' do
-    before do
-      first(:link, 'Spending too much money on heating').click
-    end
-
-    it 'has a list of schools' do
-      expect(page).to have_content('Savings')
-      expect(page).to have_content('This action has been identified as a priority for the following schools')
-      expect(page).to have_content(school.name)
+    it_behaves_like 'it contains the expected data table', aligned: false, rows: false do
+      let(:table_id) { '#school-group-priorities' }
+      let(:expected_header) do
+        [
+          ['', 'Savings'],
+          ['Fuel', '', 'Schools', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)']
+        ]
+      end
     end
 
-    context 'when when downloading as a CSV' do
+    it 'has expected content in table' do
+      within('#school-group-priorities') do
+        expect(page).to have_content('Spending too much money on heating')
+        expect(page).to have_content('£1,000')
+        expect(page).to have_content('1,100')
+        expect(page).to have_content('2,200')
+      end
+    end
+
+    context 'when downloading as a CSV' do
       before do
-        click_link('Download as CSV', id: 'download-priority-actions-school-csv')
+        click_link('Download as CSV', id: 'download-priority-actions-school-group-csv')
       end
 
-      it_behaves_like 'it downloads a CSV correctly' do
+      it_behaves_like 'it exports a group CSV correctly' do
         let(:action_name) { I18n.t('school_groups.titles.priority_actions') }
-      end
-
-      it 'has the expected contents' do
-        expect(page.source).to eq "Fuel,Description,School,Number of pupils,Floor area (m2),Energy (kWh),Cost (£),CO2 (kg)\nGas,Spending too much money on heating,#{school.name},10,200.0,0,£1000,1100\n"
+        let(:expected_csv) do
+          [['Fuel', 'Description', 'Schools', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)'],
+           ['Gas', 'Spending too much money on heating', '1', '2,200', '£1,000', '1,100']
+          ]
+        end
       end
     end
 
-    it_behaves_like 'a page not showing the cluster column'
+    context 'with the modal showing' do
+      before do
+        first(:link, 'Spending too much money on heating').click
+      end
+
+      it 'displays the expected explanation' do
+        expect(page).to have_content('This action has been identified as a priority for the following schools')
+      end
+
+      it_behaves_like 'it contains the expected data table', sortable: false do
+        let(:table_id) { "#school-priorities-#{alert_type_rating.id}"  }
+        let(:expected_header) do
+          [
+            ['', 'Savings', ''],
+            ['School', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)', '']
+          ]
+        end
+        let(:expected_rows) do
+          [[school.name, '0', '£1,000', '1,100', '']]
+        end
+      end
+
+      context 'when the download button is clicked' do
+        before do
+          click_link('Download as CSV', id: 'download-priority-actions-school-csv')
+        end
+
+        it_behaves_like 'it exports a group CSV correctly' do
+          let(:action_name) { I18n.t('school_groups.titles.priority_actions') }
+          let(:expected_csv) do
+            [['Fuel', 'Description', 'School', 'Number of pupils', 'Floor area (m2)', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)'],
+             ['Gas', 'Spending too much money on heating', school.name, '10', '200.0', '0', '£1000', '1100']
+            ]
+          end
+        end
+      end
+    end
+  end
+
+  context 'when signed in as the group admin' do
+    before do
+      sign_in(create(:group_admin, school_group:))
+      visit priorities_school_group_advice_path(school_group)
+    end
+
+    context 'with the modal showing' do
+      before do
+        first(:link, 'Spending too much money on heating').click
+      end
+
+      it_behaves_like 'it contains the expected data table', sortable: false do
+        let(:table_id) { "#school-priorities-#{alert_type_rating.id}"  }
+        let(:expected_header) do
+          [
+            ['', 'Savings', ''],
+            ['School', 'Cluster', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)', '']
+          ]
+        end
+        let(:expected_rows) do
+          [[school.name, 'Not set', '0', '£1,000', '1,100', '']]
+        end
+      end
+
+      context 'when a cluster has been added' do
+        let!(:cluster) { create(:school_group_cluster, name: 'My Cluster', schools: [school]) }
+
+        before do
+          refresh
+          first(:link, 'Spending too much money on heating').click
+        end
+
+        it_behaves_like 'it contains the expected data table', sortable: false do
+          let(:table_id) { "#school-priorities-#{alert_type_rating.id}"  }
+          let(:expected_header) do
+            [
+              ['', 'Savings', ''],
+              ['School', 'Cluster', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)', '']
+            ]
+          end
+          let(:expected_rows) do
+            [[school.name, 'My Cluster', '0', '£1,000', '1,100', '']]
+          end
+        end
+      end
+
+      context 'when the download button is clicked' do
+        before do
+          click_link('Download as CSV', id: 'download-priority-actions-school-csv')
+        end
+
+        it_behaves_like 'it exports a group CSV correctly' do
+          let(:action_name) { I18n.t('school_groups.titles.priority_actions') }
+          let(:expected_csv) do
+            [['Fuel', 'Description', 'School', 'Cluster', 'Number of pupils', 'Floor area (m2)', 'Energy (kWh)', 'Cost (£)', 'CO2 (kg)'],
+             ['Gas', 'Spending too much money on heating', school.name, 'Not set', '10', '200.0', '0', '£1000', '1100']
+            ]
+          end
+        end
+      end
+    end
   end
 end
