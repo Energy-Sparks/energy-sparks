e0300787d30fe69ee99c39384c140521fd3277f6
Leigh Dodds <leigh@ldodds.com>
2025-08-21 16:05:49 +0100

    Rework dashboard titles

--- app/components/layout/cards/page_header_component.rb
+++ app/components/layout/cards/page_header_component.rb
@@ -1,10 +1,42 @@
 module Layout
   module Cards
     class PageHeaderComponent < LayoutComponent
+      renders_one :callout, ->(title:, **kwargs) do
+        CalloutComponent.new(title:, **merge_classes('px-3 py-2 rounded', kwargs))
+      end
+
       def initialize(title:, subtitle: nil, theme: :pale, **_kwargs)
         super
         @title = title
         @subtitle = subtitle
+      end
+
+      class CalloutComponent < LayoutComponent
+        renders_many :rows
+
+        def initialize(title: nil, classes: nil, theme: :pale, **_kwargs)
+          super
+          @title = title
+        end
+
+        erb_template <<-ERB
+          <%= tag.div id: id, class: classes do %>
+            <% if @title %>
+              <div class="row">
+                <div class="col">
+                  <h4 class="pt-0"><%= @title %></h4>
+                </div>
+              </div>
+            <% end %>
+            <% rows.each do |row| %>
+              <div class="row">
+                <div class="col">
+                  <%= row %>
+                </div>
+              </div>
+            <% end %>
+          <% end %>
+        ERB
       end
     end
   end
