37bcf114f46e85ca61bdd7654415a1e6c7271d32
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-08-30 07:47:44 +0000

    more

--- spec/system/schools/advice_pages/storage_heater_target_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rails_helper'
-
-RSpec.describe 'storage heater target advice page' do
-  it_behaves_like 'target advice page' do
-    let(:fuel_type) { :storage_heater }
-  end
-end
