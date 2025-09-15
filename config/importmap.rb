98ac841bbaaae0c15cc5557431334a242db3b470
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-09-15 08:03:23 +0000

    bootstrap 5 with importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,5 @@ pin 'transport_surveys/carbon', preload: false
 pin 'transport_surveys/notifier', preload: false
 pin 'transport_surveys/handlebars_helpers', preload: false
 pin 'transport_surveys/helpers', preload: false
+pin 'bootstrap' # @5.3.8
+pin '@popperjs/core', to: '@popperjs--core.js' # @2.11.8
