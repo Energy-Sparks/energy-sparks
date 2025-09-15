ab5d803870d007be80fae4ff45de61fc2661ed50
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-09-15 13:47:42 +0000

    more

--- spec/factories/school_targets.rb
+++ spec/factories/school_targets.rb
@@ -52,8 +52,11 @@ FactoryBot.define do
         target[evaluator.fuel_type] = evaluator.target
         target["#{evaluator.fuel_type}_monthly_consumption"] = (0..11).map do |i|
           month = target.start_date.month - 1 + i
-          [target.start_date.year + (month / 12), (month % 12) + 1, evaluator.current_consumption,
-           evaluator.previous_consumption, evaluator.target_consumption, evaluator.missing]
+          [target.start_date.year + (month / 12), (month % 12) + 1,
+           evaluator.current_consumption,
+           evaluator.previous_consumption,
+           evaluator.target_consumption,
+           evaluator.missing.respond_to?(:[]) ? evaluator.missing[i] : evaluator.missing]
         end
       end
     end
