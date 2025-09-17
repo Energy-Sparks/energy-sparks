1c525724e6e19ce7229b608149e498ad873304c2
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-09-17 12:28:04 +0000

    Remove "Target Progress and Data Report"

--- app/mailers/target_mailer.rb
+++ app/mailers/target_mailer.rb
@@ -19,28 +19,7 @@ class TargetMailer < LocaleMailer
     make_bootstrap_mail(to: @to)
   end
 
-  def admin_target_report
-    @to = params[:to]
-    @target_summary = params[:target_summary]
-    @progress_report = params[:progress_report]
-    @target_data_report = params[:target_data_report]
-
-    attach(@progress_report, 'progress-report')
-    attach(@target_data_report, 'target-data-report')
-
-    make_bootstrap_mail(to: @to)
-  end
-
   private
-
-  def attach(report, prefix, suffix = 'csv', mime_type = 'text/csv')
-    if report.present?
-      attachments["#{prefix}-#{timestamp}.#{suffix}"] = {
-        mime_type: mime_type,
-        content: report
-      }
-    end
-  end
 
   def timestamp
     Time.zone.today.strftime('%Y-%m-%d')
