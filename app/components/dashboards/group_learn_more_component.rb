ffd9faf532db47ae15c89e4956d7d973fcffcc1c
Deb Bassett <deb@urbanwide.com>
2025-09-22 13:46:08 +0100

    Try this

--- app/components/dashboards/group_learn_more_component.rb
+++ app/components/dashboards/group_learn_more_component.rb
@@ -11,27 +11,12 @@ module Dashboards
     end
 
     def schools
-      user&.admin? && @school_group.schools.process_data || school_group.schools.data_enabled
+      schools = @school_group.schools.order(name: :asc)
+      (user&.admin? && schools.process_data) || schools.data_enabled
     end
 
     def data_enabled?
       schools.any?
-    end
-
-    def title
-      if data_enabled?
-        I18n.t('components.dashboards.group_learn_more.title')
-      else
-        I18n.t('schools.show.coming_soon')
-      end
-    end
-
-    def intro
-      if data_enabled?
-        I18n.t('components.dashboards.group_learn_more.intro')
-      else
-        I18n.t('components.dashboards.group_learn_more.intro_no_data')
-      end
     end
   end
 end
