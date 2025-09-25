abe213645b40cc55061357d64b9154f9e5fe6b60
Leigh Dodds <leigh@ldodds.com>
2025-09-25 15:03:03 +0100

    remove references

--- analytics/test_support/run_management_summary_table.rb
+++ analytics/test_support/run_management_summary_table.rb
@@ -43,12 +43,7 @@ class RunManagementSummaryTable < RunCharts
       front_end_template_tables:      ManagementSummaryTable.front_end_template_tables,
       front_end_template_table_data:  content.front_end_template_table_data,
       raw_variables_for_saving:       content.raw_variables_for_saving,
-      html:                           combined_html(content)
     }
-  end
-
-  def combined_html(content)
-    content.html + content.multirow_html + content.meter_range_html
   end
 
   def compare_results(control, results)
