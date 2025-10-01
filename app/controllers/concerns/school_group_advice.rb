28ff7a8f000bf09962a99870bd5fb9a7c49fe309
Deb Bassett <deb@urbanwide.com>
2025-10-01 16:51:10 +0100

    Use same method everywhere

--- app/controllers/concerns/school_group_advice.rb
+++ app/controllers/concerns/school_group_advice.rb
@@ -5,11 +5,6 @@ module SchoolGroupAdvice
     @fuel_types = @school_group.fuel_types
   end
 
-  # Rely on CanCan to filter the list of schools to those that can be shown to the current user
-  def load_schools
-    @schools = @school_group.schools.active.accessible_by(current_ability, :show).by_name
-  end
-
   def set_counts
     @priority_action_count = SchoolGroups::PriorityActions.new(@schools).priority_action_count
     @alert_count = SchoolGroups::Alerts.new(@schools).summarise.count
--- app/controllers/school_groups_controller.rb
+++ app/controllers/school_groups_controller.rb
@@ -10,7 +10,7 @@ class SchoolGroupsController < ApplicationController
   load_resource
 
   before_action :find_partners
-  before_action :find_schools, except: [:map]
+  before_action :load_schools, except: [:map]
   before_action :redirect_unless_authorised, except: [:map]
   before_action :breadcrumbs
   before_action :find_school_group_fuel_types
@@ -139,12 +139,6 @@ class SchoolGroupsController < ApplicationController
 
   def find_partners
     @partners = @school_group.partners
-  end
-
-  def find_schools
-    # Rely on CanCan to filter the list of schools to those that can be shown to the current user
-    @schools = @school_group.schools.active.accessible_by(current_ability, :show).by_name
-    # @schools = current_user&.admin? ? @schools.process_data : @schools.data_enabled
   end
 
   def include_cluster
