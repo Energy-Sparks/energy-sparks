53a9a5b2ed53d85499882f06c00833f7fb2d0c0a
Deb Bassett <deb@urbanwide.com>
2025-10-02 11:18:43 +0100

    Include visible schools in timeline. Clarify redirect

--- app/controllers/concerns/school_group_access_control.rb
+++ app/controllers/concerns/school_group_access_control.rb
@@ -2,6 +2,7 @@
 
 module SchoolGroupAccessControl
   # Rely on CanCan to filter the list of schools to those that can be shown to the current user
+
   def load_schools
     @schools = @school_group.schools.active.accessible_by(current_ability, :show).by_name
   end
@@ -9,7 +10,7 @@ module SchoolGroupAccessControl
   def redirect_unless_authorised
     # no permission on group
     redirect_to map_school_group_path(@school_group) and return if cannot?(:compare, @school_group)
-    # no permissions on any current schools in group
-    redirect_to map_school_group_path(@school_group) and return if @schools&.empty?
+    # no permissions on any current schools in group. Will not redirect if @schools is nil
+    redirect_to map_school_group_path(@school_group) and return if @schools && @schools.empty?
   end
 end
