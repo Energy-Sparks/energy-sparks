6f172b87c87fbdca427076aad8944580f6eee6a6
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-10-13 17:03:14 +0000

    more

--- spec/mailers/previews/admin_mailer_preview.rb
+++ spec/mailers/previews/admin_mailer_preview.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AdminMailerPreview < ActionMailer::Preview
   def issues_report
     AdminMailer.with(email_address: 'test@blah.com', user: User.admin.last).issues_report
@@ -6,5 +8,13 @@ class AdminMailerPreview < ActionMailer::Preview
   def stopped_data_feeds
     missing = [[AmrDataFeedConfig.order(:missing_reading_window).first, 10.days]]
     AdminMailer.with(to: 'operations@energysparks.uk', missing:).stopped_data_feeds
+  end
+
+  def self.school_group_meter_data_report_params
+    { school_group_id: SchoolGroup.all.sample&.id }
+  end
+
+  def school_group_meter_data_report
+    AdminMailer.with(to: 'test@example.com', school_group: SchoolGroup.find(params[:school_group_id]), path: '/dev/null').school_group_meter_data_report
   end
 end
