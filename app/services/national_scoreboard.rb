04790e8dd5d0864e2131110c1ae02d0c8b3a8a61
Deb Bassett <deb@urbanwide.com>
2025-10-20 17:03:08 +0100

    Timeline fix for when no calendar for a group

--- app/services/national_scoreboard.rb
+++ app/services/national_scoreboard.rb
@@ -21,12 +21,16 @@ class NationalScoreboard
     SLUG
   end
 
-  def this_academic_year(today: Time.zone.today)
-    start_year = today.year - (today.month < 9 ? 1 : 0)
+  def academic_year_for(date:)
+    start_year = date.year - (date.month < 9 ? 1 : 0)
     Struct.new(:start_date, :end_date).new(Date.new(start_year, 9, 1), Date.new(start_year + 1, 6, 30))
   end
 
+  def this_academic_year(today: Time.zone.today)
+    academic_year_for(date: today)
+  end
+
   def previous_academic_year(today: Time.zone.today)
-    this_academic_year(today: today - 1.year)
+    academic_year_for(date: today - 1.year)
   end
 end
