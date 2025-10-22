b9d2c272ae4b4efabca80048f7f6b0eb0577d4a6
Leigh Dodds <leigh@ldodds.com>
2025-10-22 17:30:48 +0100

    Initial report

---
+++ app/controllers/admin/reports/new_data_inactive_meter_report_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Admin
+  module Reports
+    class NewDataInactiveMeterReportController < BaseImportReportsController
+      private
+
+      def description
+        'List of inactive meters for which we have loaded unvalidated readings in the last 30 days'
+      end
+
+      def title
+        'New data for inactive meters'
+      end
+
+      def results
+        results = Meter
+                       .inactive
+                       .joins(:school, :amr_data_feed_readings)
+                       .where(schools: { active: true })
+                       .where(admin_meter_status: AdminMeterStatus.include_in_inactive_meter_report)
+                       .where('amr_data_feed_readings.created_at >= ?', Time.zone.today - 30)
+        results = results.includes(:school, { school: :school_group })
+        results = results.for_admin(User.admin.find(params[:user])) if params[:user].present?
+        results = results.where(meter_type: params[:meter_type]) if params[:meter_type].present?
+        results = results.where(schools: { school_group: SchoolGroup.find(params[:school_group]) }) if params[:school_group].present?
+        results
+      end
+
+      def container_class
+        'container-fluid'
+      end
+    end
+  end
+end
--- app/models/admin_meter_status.rb
+++ app/models/admin_meter_status.rb
@@ -2,16 +2,20 @@
 #
 # Table name: admin_meter_statuses
 #
-#  created_at :datetime         not null
-#  id         :bigint(8)        not null, primary key
-#  label      :string
-#  updated_at :datetime         not null
+#  created_at                      :datetime         not null
+#  id                              :bigint(8)        not null, primary key
+#  ignore_in_inactive_meter_report :boolean          default(FALSE)
+#  label                           :string
+#  updated_at                      :datetime         not null
 #
 class AdminMeterStatus < ApplicationRecord
   has_many :meters, foreign_key: 'admin_meter_statuses_id'
   has_many :school_groups_electricity, class_name: 'SchoolGroup', foreign_key: 'admin_meter_statuses_electricity_id'
   has_many :school_groups_gas, class_name: 'SchoolGroup', foreign_key: 'admin_meter_statuses_gas_id'
   has_many :school_groups_solar_pv, class_name: 'SchoolGroup', foreign_key: 'admin_meter_statuses_solar_pv_id'
+
+  scope :ignore_in_inactive_meter_report, -> { where(ignore_in_inactive_meter_report: true) }
+  scope :include_in_inactive_meter_report, -> { where(ignore_in_inactive_meter_report: false) }
 
   def school_groups
     SchoolGroup.where('admin_meter_statuses_electricity_id = :id OR admin_meter_statuses_gas_id = :id OR admin_meter_statuses_solar_pv_id = :id', id: id)
