de526c5374a7eba8cf510b0cf9ed20d97f681bc6
Leigh Dodds <leigh@ldodds.com>
2025-10-30 13:47:12 +0000

    Revise dashboard and access control specs

--- spec/support/shared_examples/school_group_access_control.rb
+++ spec/support/shared_examples/school_group_access_control.rb
@@ -59,7 +59,7 @@ RSpec.shared_examples 'an access controlled group page' do
 
   context 'when the group is public but there are no active schools' do
     before do
-      school_group.schools.update_all(active: false)
+      school_group.assigned_schools.update_all(active: false)
       sign_in(user) if user
       visit path
     end
@@ -84,96 +84,51 @@ end
 
 RSpec.shared_examples 'a group page with schools filtered by permissions' do
   let(:user) { nil }
-
-  context 'with a public group' do
-    before do
-      school_group.update!(public: true)
-      sign_in(user) if user
-      visit path
-    end
-
-    context 'when not signed in' do
-      it_behaves_like 'data sharing options are applied'
-    end
-
-    context 'when signed in as school admin' do
-      let(:user) { create(:school_admin, school: create(:school, school_group:)) }
-
-      it_behaves_like 'data sharing options are applied', school_admin: true
-    end
-
-    context 'when signed in as the group admin' do
-      let(:user) { create(:group_admin, school_group:) }
-
-      it_behaves_like 'data sharing options are applied', group_admin: true
-    end
-
-    context 'when signed in as the group admin for a different group' do
-      let(:user) { create(:group_admin, school_group: create(:school_group)) }
-
-      it_behaves_like 'data sharing options are applied'
-    end
-
-    context 'when signed in as an admin' do
-      let(:user) { create(:admin) }
-
-      it_behaves_like 'data sharing options are applied', admin: true
-    end
-  end
-
-  context 'with a private group' do
-    before do
-      school_group.update!(public: false)
-      sign_in(user) if user
-      visit path
-    end
-
-    context 'when signed in as school admin' do
-      let(:user) { create(:school_admin, school: create(:school, school_group:)) }
-
-      it_behaves_like 'data sharing options are applied', school_admin: true
-    end
-
-    context 'when signed in as the group admin' do
-      let(:user) { create(:group_admin, school_group:) }
-
-      it_behaves_like 'data sharing options are applied', group_admin: true
-    end
-
-    context 'when signed in as an admin' do
-      let(:user) { create(:admin) }
-
-      it_behaves_like 'data sharing options are applied', admin: true
-    end
-  end
-end
-
-RSpec.shared_examples 'data sharing options are applied' do |admin: false, group_admin: false, school_admin: false|
-  let(:data_sharing) { :within_group }
-  let!(:filtered_school) { create(:school, school_group: school_group, data_sharing: data_sharing) }
+  let!(:filtered_school) { create(:school, :with_fuel_configuration, school_group:, data_sharing: :private) }
 
   before do
-    visit school_group_path(school_group)
+    sign_in(user) if user
+    visit path
   end
 
-  context 'with data sharing set to within_group' do
-    it 'does not show the school', unless: admin || school_admin || group_admin do
+  context 'when not signed in' do
+    it 'does not show the school' do
       expect(page).to have_no_content(filtered_school.name)
-    end
-
-    it 'shows all the schools', if: admin || school_admin || group_admin do
-      expect(page).to have_content(filtered_school.name)
     end
   end
 
-  context 'with data sharing set to private' do
-    let(:data_sharing) { :private }
+  context 'when signed in as school admin' do
+    let(:user) { create(:school_admin, school: create(:school, school_group:)) }
 
-    it 'does not show the school', unless: admin || group_admin do
+    it 'does not show the school' do
       expect(page).to have_no_content(filtered_school.name)
     end
+  end
 
-    it 'shows all the schools', if: admin || group_admin do
+  context 'when signed in as the group admin' do
+    let(:user) { create(:group_admin, school_group:) }
+
+    it 'filters correctly' do
+      if school_group.organisation?
+        expect(page).to have_content(filtered_school.name)
+      else
+        expect(page).to have_no_content(filtered_school.name)
+      end
+    end
+  end
+
+  context 'when signed in as the group admin for a different group' do
+    let(:user) { create(:group_admin, school_group: create(:school_group)) }
+
+    it 'does not show the school' do
+      expect(page).to have_no_content(filtered_school.name)
+    end
+  end
+
+  context 'when signed in as an admin' do
+    let(:user) { create(:admin) }
+
+    it 'shows all the schools' do
       expect(page).to have_content(filtered_school.name)
     end
   end
