d89f3dc4516fbadd7f44e83efa6949e0638a2da9
Leigh Dodds <leigh@ldodds.com>
2025-11-06 09:44:05 +0000

    Use new school group relationships on dashboard, advice and related analysis. (#4825)

--- spec/system/school_groups/map_spec.rb
+++ spec/system/school_groups/map_spec.rb
@@ -1,22 +1,65 @@
 require 'rails_helper'
 
 describe 'School groups map page', :school_groups do
+  shared_examples 'a group map page' do
+    it 'displays the right breadcrumb' do
+      expect(find('ol.main-breadcrumbs').all('li').collect(&:text)).to eq([I18n.t('common.schools'), school_group.name, I18n.t('school_groups.titles.map')])
+    end
+
+    it { expect(page).to have_css('div#geo-json-map') }
+    it { expect(page).to have_title("#{school_group.name} #{I18n.t('school_groups.titles.map')}") }
+
+    context 'when displaying schools' do
+      it 'displays a summary of information' do
+        expect(page).to have_link(school.name, href: school_path(school))
+        expect(page).to have_content(I18n.t("common.school_types.#{school.school_type}"))
+        expect(page).to have_content(school.address)
+        expect(page).to have_content(school.postcode)
+      end
+
+      it 'displays fuel type icons' do
+        expect(page).to have_css('i.fa-sun')
+        expect(page).to have_css('i.fa-bolt')
+        expect(page).to have_css('i.fa-fire')
+        expect(page).to have_css('i.fa-fire-alt')
+      end
+
+      it 'groups the results' do
+        expect(page).to have_css('#X-schools')
+        expect(page).not_to have_css('li.page-item.letter.disabled', text: 'X')
+        expect(page).to have_css('li.page-item.letter', text: 'X')
+      end
+    end
+  end
+
   let!(:school_group) { create(:school_group, :with_active_schools, group_type: :general, public: true) }
-  let!(:school) { create(:school, :with_fuel_configuration, name: 'Xavier School for Gifted Children', school_group: school_group) }
+  let!(:school) { create(:school, :with_fuel_configuration, name: 'Xavier school for gifted children', school_group: school_group) }
 
   before do
-    Flipper.enable(:group_dashboards_2025)
     visit map_school_group_path(school_group)
   end
 
-  it 'displays the right breadcrumb' do
-    expect(find('ol.main-breadcrumbs').all('li').collect(&:text)).to eq([I18n.t('common.schools'), school_group.name, I18n.t('school_groups.titles.map')])
+  context 'with different group types' do
+    context 'with general group' do
+      it_behaves_like 'a group map page'
+    end
+
+    context 'with project group' do
+      let!(:school) { create(:school, :with_fuel_configuration, :with_project, name: 'Xavier school for gifted children') }
+      let!(:school_group) { school.project_groups.first }
+
+      it_behaves_like 'a group map page'
+    end
+
+    context 'with an area group' do
+      let!(:school) { create(:school, :with_fuel_configuration, :with_diocese, name: 'Xavier school for gifted children') }
+      let!(:school_group) { school.area_groups.first }
+
+      it_behaves_like 'a group map page'
+    end
   end
 
-  it { expect(page).to have_css('div#geo-json-map') }
-  it { expect(page).to have_title("#{school_group.name} #{I18n.t('school_groups.titles.map')}") }
-
-  context 'when group is private' do
+  context 'when the group is private' do
     let!(:school_group) { create(:school_group, :with_active_schools, group_type: :general, public: false) }
 
     it 'still displays when not logged in' do
@@ -24,7 +67,7 @@ describe 'School groups map page', :school_groups do
     end
   end
 
-  context 'when displays group summary' do
+  context 'when displaying the group summary' do
     it 'displays expected message' do
       expect(page).to have_content('We are working with 2 schools in this group.')
     end
@@ -43,28 +86,6 @@ describe 'School groups map page', :school_groups do
       it 'displays expected message' do
         expect(page).to have_content("We are working with 2 schools in this multi-academy trust in partnership with #{school_group.partners.first.name}")
       end
-    end
-  end
-
-  context 'when displaying schools' do
-    it 'displays a summary of information' do
-      expect(page).to have_link(school.name, href: school_path(school))
-      expect(page).to have_content(I18n.t("common.school_types.#{school.school_type}"))
-      expect(page).to have_content(school.address)
-      expect(page).to have_content(school.postcode)
-    end
-
-    it 'displays fuel type icons' do
-      expect(page).to have_css('i.fa-sun')
-      expect(page).to have_css('i.fa-bolt')
-      expect(page).to have_css('i.fa-fire')
-      expect(page).to have_css('i.fa-fire-alt')
-    end
-
-    it 'groups the results' do
-      expect(page).to have_css('#X-schools')
-      expect(page).not_to have_css('li.page-item.letter.disabled', text: 'X')
-      expect(page).to have_css('li.page-item.letter', text: 'X')
     end
   end
 end
