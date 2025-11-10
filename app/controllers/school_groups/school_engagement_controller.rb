877d48b997fefe696f5c11326202ffd4a1353633
Leigh Dodds <leigh@ldodds.com>
2025-11-10 11:30:24 +0000

    Fix sorting in school engagement report

--- app/controllers/school_groups/school_engagement_controller.rb
+++ app/controllers/school_groups/school_engagement_controller.rb
@@ -35,6 +35,7 @@ module SchoolGroups
                   Column.new(I18n.t('school_groups.school_engagement.active_users'),
                              ->(service) { service.recently_logged_in_user_count }),
                   Column.new(I18n.t('school_groups.school_engagement.last_visit'),
+                             ->(service) { service.most_recent_login&.to_date&.iso8601 },
                              ->(service) { service.most_recent_login&.to_date&.to_fs(:es_compact) })]
       respond_to do |format|
         format.html
