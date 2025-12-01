430daade7ab01cabae1a276248375a352223ffbb
Leigh Dodds <leigh@ldodds.com>
2025-12-01 17:05:44 +0000

    Revise columns, improve query

--- app/controllers/admin/reports/interventions_controller.rb
+++ app/controllers/admin/reports/interventions_controller.rb
@@ -2,7 +2,7 @@ module Admin
   module Reports
     class InterventionsController < AdminController
       def index
-        @observations = Observation.intervention.recorded_in_last_year.order(created_at: :desc)
+        @observations = Observation.includes(:school, :intervention_type, :created_by).intervention.recorded_in_last_year.order(created_at: :desc)
       end
     end
   end
