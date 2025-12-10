e224475a249bec5e73f80ef8d7afa2e86dcb329a
Tristan Hill <878258+tbhi@users.noreply.github.com>
2025-12-10 13:46:37 +0000

    more

--- db/migrate/20251209155112_remove_fields_from_school_targets.rb
+++ db/migrate/20251209155112_remove_fields_from_school_targets.rb
@@ -1,12 +1,12 @@
 class RemoveFieldsFromSchoolTargets < ActiveRecord::Migration[7.2]
   def change
     change_table :school_targets, bulk: true do |t|
-      t.remove :electricity_progress, type: :json
-      t.remove :electricity_report, type: :jsonb
-      t.remove :gas_progress, type: :json
-      t.remove :gas_report, type: :jsonb
-      t.remove :storage_heaters_progress, type: :json
-      t.remove :storage_heaters_report, type: :jsonb
+      t.remove :electricity_progress, type: :json, default: {}
+      t.remove :electricity_report, type: :jsonb, default: {}
+      t.remove :gas_progress, type: :json, default: {}
+      t.remove :gas_report, type: :jsonb, default: {}
+      t.remove :storage_heaters_progress, type: :json, default: {}
+      t.remove :storage_heaters_report, type: :jsonb, default: {}
       t.remove :report_last_generated, type: :datetime
     end
   end
