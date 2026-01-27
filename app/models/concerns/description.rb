c0813ca4486c55c8133c1c6af6dcb58e9593a740
Leigh Dodds <leigh@ldodds.com>
2026-01-27 16:30:47 +0000

    Refactor to use common scope

--- app/models/concerns/description.rb
+++ app/models/concerns/description.rb
@@ -4,21 +4,11 @@ module Description
   extend ActiveSupport::Concern
 
   included do
-    # Scope: records whose description contains at least one image blob
     scope :with_image_in_description, -> {
       joins(rich_text_description: { embeds_attachments: :blob })
         .where("active_storage_blobs.content_type LIKE 'image/%'")
         .distinct
     }
-  end
-
-  # Instance method: does this record have any embedded images?
-  def has_image?
-    rich_text_description
-      .embeds_attachments
-      .joins(:blob)
-      .where("active_storage_blobs.content_type LIKE 'image/%'")
-      .exists?
   end
 
   def description_includes_images?
--- spec/system/admin/reports/images_spec.rb
+++ spec/system/admin/reports/images_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Admin Image Feed' do
+describe 'Admin Image Feed' do
   let(:admin) { create(:admin) }
 
   before do
@@ -10,26 +10,89 @@ RSpec.describe 'Admin Image Feed' do
     visit admin_reports_path
   end
 
-  context 'when viewing feed' do
-    let!(:activity_no_image) { create(:activity, description: 'Activity no image') }
-    let!(:observation_no_image) { create(:observation, :intervention, description: 'Intervention no image') }
+  context 'when viewing activities' do
+    context 'with no attached images' do
+      let!(:activity_no_image) { create(:activity, description: 'Activity no image') }
 
-    let!(:activity_with_image) do
-      create(:activity_without_creator, :with_image_in_description, school: create(:school, :with_school_group))
+      before do
+        click_on('Images')
+      end
+
+      it { expect(page).not_to have_content(activity_no_image.school.name) }
+      it { expect(page).to have_content('No results') }
     end
 
-    let!(:observation_with_image) do
-      create(:observation, :intervention_with_image_in_description, school: create(:school, :with_school_group))
+    context 'with images' do
+      let(:school_group) { create(:school_group) }
+
+      let!(:activity_with_image) do
+        create(:activity_without_creator, :with_image_in_description, school: create(:school, school_group:))
+      end
+
+      before do
+        click_on('Images')
+      end
+
+      it { expect(page).to have_content(activity_with_image.school.name) }
+
+      context 'when filtering by group' do
+        let!(:filtered_activity) do
+          create(:activity_without_creator, :with_image_in_description, school: create(:school, :with_school_group))
+        end
+
+        before do
+          select school_group.name, from: 'School group'
+          click_on('Filter')
+        end
+
+        it { expect(page).to have_content(activity_with_image.school.name) }
+        it { expect(page).not_to have_content(filtered_activity.school.name) }
+      end
+    end
+  end
+
+  context 'when viewing actions' do
+    context 'with no attached images' do
+      let!(:observation_no_image) { create(:observation, :intervention, description: 'Intervention no image') }
+
+      before do
+        click_on('Images')
+        select 'Actions', from: 'Type'
+        click_on('Filter')
+      end
+
+      it { expect(page).not_to have_content(observation_no_image.school.name) }
+      it { expect(page).to have_content('No results') }
     end
 
-    before do
-      click_on('Images')
+    context 'with images' do
+      let(:school_group) { create(:school_group) }
+
+      let!(:observation_with_image) do
+        create(:observation, :intervention_with_image_in_description, school: create(:school, school_group:))
+      end
+
+      before do
+        click_on('Images')
+        select 'Actions', from: 'Type'
+        click_on('Filter')
+      end
+
+      it { expect(page).to have_content(observation_with_image.school.name) }
+
+      context 'when filtering by group' do
+        let!(:filtered_action) do
+          create(:observation, :intervention_with_image_in_description, school: create(:school, :with_school_group))
+        end
+
+        before do
+          select school_group.name, from: 'School group'
+          click_on('Filter')
+        end
+
+        it { expect(page).to have_content(observation_with_image.school.name) }
+        it { expect(page).not_to have_content(filtered_action.school.name) }
+      end
     end
-
-    it { expect(page).not_to have_content(activity_no_image.school.name) }
-    it { expect(page).not_to have_content(observation_no_image.school.name) }
-
-    it { expect(page).to have_content(activity_with_image.school.name) }
-    it { expect(page).to have_content(observation_with_image.school.name) }
   end
 end
