57dc9e1dd218c8339f3c75b0364864097d4aee3d
Deb Bassett <deb@urbanwide.com>
2026-02-13 23:16:27 +0000

    This

--- app/assets/javascripts/bootstrap4/init_bootstrap.js
+++ app/assets/javascripts/bootstrap4/init_bootstrap.js
@@ -3,9 +3,7 @@
 $(document).ready(function() {
   // Activate tooltips
   $('[data-toggle="tooltip"]').tooltip();
-  $('[data-bs-toggle="tooltip"]').tooltip();
 
   // Activate popovers
   $('[data-toggle="popover"]').popover();
-  $('[data-bs-toggle="popover"]').popover();
 });
