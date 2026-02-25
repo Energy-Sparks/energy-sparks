666abcbbaf83afa126887ec1d178868678e802b0
workluke <workluke@Lukes-Laptop.localdomain>
2026-02-25 10:16:03 +0000

    Updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_02_19_153106) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_23_095759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pgcrypto"
