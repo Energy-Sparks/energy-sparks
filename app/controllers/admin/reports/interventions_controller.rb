b68c386d93b4014d0e4f774a35f2c3961d0bcec9
Leigh Dodds <leigh@ldodds.com>
2026-03-04 16:17:31 +0000

    Optimise activity and action report (#5042)
    Co-authored-by: Deb Bassett <deb@urbanwide.com>

--- app/controllers/admin/reports/interventions_controller.rb
+++ app/controllers/admin/reports/interventions_controller.rb
@@ -2,7 +2,7 @@ module Admin
   module Reports
     class InterventionsController < AdminController
       def index
-        @observations = Observation.includes(:school, :intervention_type, :created_by).intervention.recorded_in_last_year.order(created_at: :desc)
+        @observations = Observation.includes(:school, :intervention_type, :created_by, school: :school_group, rich_text_description: { embeds_attachments: :blob }).intervention.recorded_in_last_year.order(created_at: :desc)
       end
     end
   end
