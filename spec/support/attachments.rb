dc300ab6adcc29e353ad8672e3795bb467aa332b
Deb Bassett <deb@urbanwide.com>
2026-03-04 11:32:46 +0000

    Better again?

--- spec/support/attachments.rb
+++ spec/support/attachments.rb
@@ -1,5 +1,4 @@
 # Builds an ActionText::Content object containing a single attachment.
-#
 # Used in tests to generate rich text content with an attached file.
 #
 # Example:
@@ -20,29 +19,4 @@ def content_with_attachment(filename: 'spec/fixtures/images/placeholder.png', co
   attachment = ActionText::Attachment.from_attachable(blob)
 
   ActionText::Content.new("<div>#{attachment.to_html}</div>")
-end
-
-# Assigns an attachment to a record's rich text attribute (does not save).
-#
-# Example:
-#   add_attachment(record)
-#   record.save!
-#
-# Params:
-# - record: ActiveRecord object
-# - attribute: rich text attribute (default: :description)
-# - filename/content_type: Passed to content_with_attachment
-def add_attachment(record, attribute = :description, **kwargs)
-  record.tap do |r|
-    r.public_send("#{attribute}=", content_with_attachment(**kwargs))
-  end
-end
-
-# Assigns an attachment and saves the record.
-#
-# Example:
-#   add_attachment!(record)
-# Params: same as add_attachment
-def add_attachment!(record, attribute = :description, **kwargs)
-  add_attachment(record, attribute, **kwargs).tap(&:save!)
 end
