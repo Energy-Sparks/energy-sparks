9b0500022f15f27596aa87694a9cbb93ab989830
Leigh Dodds <leigh@ldodds.com>
2026-03-11 11:29:05 +0000

    Refactor to component

---
+++ app/components/forms/commercial/bulk_licence_editor_component.rb
@@ -0,0 +1,25 @@
+module Forms
+  module Commercial
+    class BulkLicenceEditorComponent < ApplicationComponent
+      # FIXME: specify fields to include, e.g. creating contract/renewal vs invoicing
+      # FIXME: specify whether to allow adding extra schools
+      # FIXME: specify path for adding extra schools?
+      # FIXME: bulk edit options? invoice ref, status, add all missing
+      # FIXME: always all licences?
+      # FIXME: consistent sort order
+      def initialize(contract:, form_path: nil, **kwargs)
+        super
+        @contract = contract
+        @form_path = form_path
+      end
+
+      def form_path
+        @form_path || admin_commercial_contract_licences_path(@contract)
+      end
+
+      def licences
+        @contract.licences
+      end
+    end
+  end
+end
