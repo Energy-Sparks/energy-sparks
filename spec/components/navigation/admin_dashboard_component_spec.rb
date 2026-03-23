665959f7a4534bbb9653ce19609535743bddb04e
workluke <workluke@Lukes-Laptop.localdomain>
2026-03-23 09:44:14 +0000

    Added basic spec for dashboard navigation component

---
+++ spec/components/navigation/admin_dashboard_component_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Navigation::AdminDashboardComponent, :include_url_helpers, type: :component do
+  subject(:component) do
+    described_class.new(**params)
+  end
+
+  let(:current_user) { create(:admin) }
+
+  let(:params) do
+    { current_user: }
+  end
+
+  context 'when rendering' do
+    let(:html) do
+      render_inline(component)
+    end
+
+    it { expect(html).to have_link('Overview', href: admin_dashboard_path(current_user)) }
+  end
+end
