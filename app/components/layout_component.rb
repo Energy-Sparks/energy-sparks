5db69eef06e3ceae44cc168a47228611d2724dde
Deb Bassett <deb@urbanwide.com>
2026-03-23 11:13:18 +0000

    More rubocop

--- app/components/layout_component.rb
+++ app/components/layout_component.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class LayoutComponent < ApplicationComponent
   attr_reader :theme
 
-  renders_one :placeholder, ->(*args, **kwargs) do
-    Admin::PlaceholderComponent.new(*args, **{}.merge(kwargs))
-  end
+  renders_one :placeholder, lambda { |*args, **kwargs|
+    Admin::PlaceholderComponent.new(*args, **kwargs)
+  }
 
   def initialize(*_args, theme: nil, component_classes: '', **_kwargs)
     super
@@ -16,32 +18,33 @@ class LayoutComponent < ApplicationComponent
   end
 
   def add_theme(theme)
-    if theme
-      @theme = theme
-      raise ArgumentError, 'Unknown theme' unless self.class.themes.include?(theme)
-      add_classes "theme theme-#{theme}"
-    end
+    return unless theme
+
+    @theme = theme
+    raise ArgumentError, 'Unknown theme' unless self.class.themes.include?(theme)
+
+    add_classes "theme theme-#{theme}"
   end
 
   # This is to be overriden if we want to wrap the component
   # Don't forget to call render
-  def wrap(klass, *args, **kwargs, &block)
-    render(klass.new(*args, **kwargs), &block)
+  def wrap(klass, *, **, &)
+    render(klass.new(*, **), &)
   end
 
   class << self
     # as and ks are params passed when calling the component: with_my_component(arg, key: value)
     # args and kwargs are parameters from the definitition in the component
-    def type(label, klass, *args, **kwargs)
+    def type(label, klass, *, **kwargs)
       { label => {
-          renders: ->(*_as, **ks, &block) {
-            overrides = {
-              classes: class_names(ks[:classes], kwargs[:classes], kwargs[:component_classes], @component_classes)
-            }.compact
-            wrap(klass, *args, **kwargs.merge(**ks).merge(overrides), &block)
-          },
-          as: label
-        } }
+        renders: lambda { |*_as, **ks, &block|
+          overrides = {
+            classes: class_names(ks[:classes], kwargs[:classes], kwargs[:component_classes], @component_classes)
+          }.compact
+          wrap(klass, *, **kwargs.merge(**ks).merge(overrides), &block)
+        },
+        as: label
+      } }
     end
 
     def types(*definitions)
@@ -49,7 +52,7 @@ class LayoutComponent < ApplicationComponent
     end
 
     def themes
-      [:dark, :light, :accent, :pale, :white]
+      %i[dark light accent pale white]
     end
   end
 end
