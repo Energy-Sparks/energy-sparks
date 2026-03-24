f6a9539c96c8708b488f0058fc9ab7dfbae05e32
workluke <workluke@Lukes-Laptop.localdomain>
2026-03-24 11:39:02 +0000

    Reshuffled controller layout

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,15 +2,27 @@
 
 module Admin
   class DashboardsController < AdminController
-    layout 'admin_dashboard'
+    before_action :set_user
 
-    authorize_resource class: false
+    layout 'admin_dashboard'
 
     def index
       @admins = User.where(role: 'admin').order(:name)
     end
 
-    def show
+    def show; end
+
+    def set_breadcrumbs
+      @breadcrumbs = [
+        { name: 'Admin', href: admin_path },
+        { name: 'Dashboards', href: admin_dashboards_path },
+        { name: 'ye' }
+      ]
+    end
+
+    private
+
+    def set_user
       @admin = User.where(id: params[:id]).first
     end
   end
