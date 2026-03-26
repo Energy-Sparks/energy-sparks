8aa7040efe7085113e7e21aa268b127596e507aa
Deb Bassett <deb@urbanwide.com>
2026-03-26 14:27:10 +0000

    Fixen

--- app/controllers/admin/issues_controller.rb
+++ app/controllers/admin/issues_controller.rb
@@ -3,12 +3,13 @@
 module Admin
   class IssuesController < AdminController
     include Pagy::Backend
+
     load_and_authorize_resource :school
     load_and_authorize_resource :school_group
     load_and_authorize_resource :data_source
     load_and_authorize_resource :school_onboarding, find_by: :uuid
     before_action :issueable
-    before_action :enable_bootstrap_5
+    before_action :enable_bootstrap_5, except: %i[new create edit update] # Form spacing needs work
 
     load_and_authorize_resource :issue, through: :issueable, shallow: true, except: [:meter_issues]
 
@@ -21,7 +22,8 @@ module Admin
         # Includes all school group and school issues
         @issues = @issueable.all_issues.active if @issueable.is_a?(SchoolGroup)
       else
-        @issues = @issues.active unless @issueable.is_a?(School) # Only show issues for archived schools in a school context
+        # Only show issues for archived schools in a school context
+        @issues = @issues.active unless @issueable.is_a?(School)
         @issues = @issues.for_issue_types(params[:issue_types])
         @issues = @issues.for_statuses(params[:statuses])
         @issues = @issues.for_owned_by(params[:user]) if params[:user].present?
@@ -29,7 +31,10 @@ module Admin
 
         if params[:review_date]
           @issues = @issues.where(review_date: nil) if params[:review_date] == 'review_unset'
-          @issues = @issues.where('review_date BETWEEN ? AND ?', Time.zone.now, 7.days.from_now) if params[:review_date] == 'review_next_week'
+          if params[:review_date] == 'review_next_week'
+            @issues = @issues.where('review_date BETWEEN ? AND ?', Time.zone.now,
+                                    7.days.from_now)
+          end
           @issues = @issues.where('review_date <= ?', Time.zone.now) if params[:review_date] == 'review_overdue'
         end
       end
@@ -41,7 +46,7 @@ module Admin
           @pagy, @issues = pagy(@issues.by_priority_order)
         end
         format.csv do
-          issues = @issues.with_rich_text_description.includes(meters: [:data_source, :admin_meter_status])
+          issues = @issues.with_rich_text_description.includes(meters: %i[data_source admin_meter_status])
 
           send_data issues.to_csv,
                     filename: EnergySparks::Filenames.csv('issues')
@@ -52,6 +57,8 @@ module Admin
     def new
       @issue = Issue.new(issue_type: params[:issue_type], issueable: @issueable, meter_ids: params[:meter_ids])
     end
+
+    def edit; end
 
     def create
       @issue.attributes = { created_by: current_user, updated_by: current_user }
@@ -111,7 +118,7 @@ module Admin
     end
 
     def redirect_back_or_index(notice:)
-      redirect_back fallback_location: issueable_index_url, notice: issueable_notice(notice)
+      redirect_back_or_to(issueable_index_url, notice: issueable_notice(notice))
     end
 
     def issueable_index_url
