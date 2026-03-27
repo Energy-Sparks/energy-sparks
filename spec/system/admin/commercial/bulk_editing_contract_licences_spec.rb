181a870cb83763b16d75044398f48685443d4291
Leigh Dodds <leigh@ldodds.com>
2026-03-27 14:57:47 +0000

    Specs for adding, updating, deleted newly added licences

--- spec/system/admin/commercial/bulk_editing_contract_licences_spec.rb
+++ spec/system/admin/commercial/bulk_editing_contract_licences_spec.rb
@@ -73,7 +73,7 @@ describe 'bulk editing contract licences' do
     end
   end
 
-  context 'when modifying licences for a school group', :js do
+  describe 'when modifying licences for a school group', :js do
     let!(:contract) { create(:commercial_contract, contract_holder: create(:school_group)) }
     let!(:licence) do
       create(:commercial_licence, contract:,
@@ -89,7 +89,64 @@ describe 'bulk editing contract licences' do
       before { refresh }
 
       it { expect(page).to have_content('Add schools to contract') }
-      it { expect(page).to have_content(additional_school.name) }
+
+      it 'lists the school' do
+        within("#contract-#{contract.id}-additional-schools-table") do
+          expect(page).to have_content(additional_school.name)
+        end
+      end
+
+      it 'does not show a licence for the school' do
+        within("#contract-#{contract.id}-licence-table") do
+          expect(page).to have_no_content(additional_school.name)
+        end
+      end
+
+      # rubocop:disable RSpec/NestedGroups
+      context 'when creating a new licence for the school' do
+        before do
+          click_on 'Add licence'
+          # forcing a wait
+          within("#contract-#{contract.id}-licence-table") do
+            find('tr', text: additional_school.name)
+          end
+        end
+
+        it 'updates the licence table to include the licence' do
+          within("#contract-#{contract.id}-licence-table") do
+            expect(page).to have_content(additional_school.name)
+          end
+          expect(additional_school.reload.licences.count).to eq(1)
+        end
+
+        context 'when deleting that licence' do
+          before do
+            licence = additional_school.licences.reload.last
+            within("#licence-#{licence.id}-main-row") do
+              click_on 'Delete'
+            end
+            click_on 'Save changes'
+          end
+
+          it 'deletes the licence' do
+            expect(page).to have_content('Licences updated')
+            expect(additional_school.licences.reload).to be_empty
+          end
+        end
+
+        context 'when editing that licence' do
+          before do
+            fill_in_field(additional_school.licences.reload.last, :comments, 'Some comments')
+            click_on('Save changes')
+          end
+
+          it 'updates the newly added licence' do
+            expect(page).to have_content('Licences updated')
+            expect(additional_school.licences.reload.last.comments).to eq('Some comments')
+          end
+        end
+      end
+      # rubocop:enable RSpec/NestedGroups
     end
   end
 end
