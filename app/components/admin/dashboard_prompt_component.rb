9b0a2f07b64c1e1d8140ebd40a1affaca0b26dd9
workluke <workluke@Lukes-Laptop.localdomain>
2026-03-30 11:28:35 +0100

    Basic dashboard prompt component setup

--- app/components/admin/dashboard_prompt_component.rb
+++ app/components/admin/dashboard_prompt_component.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
-class DashboardPromptComponent < PromptComponent
-  def initialize(user:, **_kwargs)
-    super
-    @user = user
+module Admin
+  class DashboardPromptComponent < ApplicationComponent
+    attr_reader :user
+
+    renders_one :title
+
+    def initialize(user:, **_kwargs)
+      super
+      @user = user
+    end
+
+    def prompt_for_issues_renewal?
+      true
+    end
+
+    def add_prompt(list:, status:, icon:, check: true, id: nil, link: nil, path: nil) # rubocop:disable Metrics/ParameterLists, Lint/UnusedMethodArgument
+      return unless check
+
+      list.with_prompt id: id, status: status, icon: icon do # rubocop:disable Style/ExplicitBlockArgument
+        yield
+      end
+    end
   end
 end
