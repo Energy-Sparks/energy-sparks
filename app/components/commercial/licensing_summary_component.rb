c8d0031550ae0c1ec2d87180a9f53c0e7d23f605
Leigh Dodds <leigh@ldodds.com>
2026-03-31 14:50:27 +0100

    Rename

--- app/components/commercial/licensing_summary_component.rb
+++ app/components/commercial/licensing_summary_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Commercial
-  class GroupLicenceSummaryComponent < ApplicationComponent
+  class LicensingSummaryComponent < ApplicationComponent
     renders_many :rows, lambda { |**kwargs|
       kwargs[:date_range] = @date_range
       RowComponent.new(**kwargs)
