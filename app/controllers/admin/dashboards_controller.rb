8b451173e7085e25bff8fa402386a66fcb6ace1b
workluke <workluke@Lukes-Laptop.localdomain>
2026-03-31 14:04:51 +0100

    Updated controller logic for redirects

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -13,6 +13,11 @@ module Admin
     def show
       set_user
       set_breadcrumbs
+      if params[:school_group_id].present?
+        redirect_to admin_dashboard_school_group_path(@dashboard_user, id: params[:school_group_id])
+      elsif params[:school_id].present?
+        redirect_to school_path(id: params[:school_id])
+      end
     end
   end
 end
