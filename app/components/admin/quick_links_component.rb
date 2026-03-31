d3c4ec8ff91ea403a28e35995696a1f169021554
workluke <workluke@Lukes-Laptop.localdomain>
2026-03-31 13:42:52 +0100

    Quick link component

--- app/components/admin/quick_links_component.rb
+++ app/components/admin/quick_links_component.rb
@@ -2,14 +2,19 @@
 
 module Admin
   class QuickLinksComponent < ApplicationComponent
+    def initialize(user:)
+      super
+      @user = user
+    end
+
     def school_groups
-      SchoolGroup.where(default_issues_admin_user: current_user).with_active_schools.order(:name)
+      SchoolGroup.where(default_issues_admin_user: @user).with_active_schools.order(:name)
     end
 
     def schools
       School.active
             .joins(:school_group)
-            .where(school_group: { default_issues_admin_user_id: current_user.id })
+            .where(school_group: { default_issues_admin_user_id: @user.id })
             .select(:id, :name)
             .order(:name)
     end
