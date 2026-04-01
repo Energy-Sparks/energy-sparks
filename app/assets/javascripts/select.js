b7eca5e657889cbac16489501755534cb655bde3
Deb Bassett <deb@urbanwide.com>
2026-04-01 10:35:27 +0100

    Fix select2

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   $.fn.select2.defaults.set('sorter', function(datas) { return datas.filter((data) => !data?.element?.hidden); });
 
   $('.bs4 select.form-control.select2').select2({theme: 'bootstrap'});
-  $('.bs5 select.form-control.select2').select2({theme: 'bootstrap-5'});
+  $('.bs5 select.form-select.select2').select2({theme: 'bootstrap-5'});
 
   // Switch on select2 after cocoon insert. Needed for todos
   $('.admin-todos').on('cocoon:after-insert', function(e, insertedItem) {
