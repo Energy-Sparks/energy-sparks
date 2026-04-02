a600ae028fe7b1b1e33906c49db4443f49397810
workluke <workluke@Lukes-Laptop.localdomain>
2026-04-02 16:28:18 +0100

    Fixed errors from renaming

--- spec/components/admin/quick_links_component_spec.rb
+++ spec/components/admin/quick_links_component_spec.rb
@@ -17,10 +17,9 @@ RSpec.describe Admin::QuickLinksComponent, :include_url_helpers, type: :componen
     describe 'select school group box' do
       let!(:school_groups) { create_list(:school_group, 2, :with_active_schools, default_issues_admin_user: user) }
 
-      it { expect(html).to have_content('Select School Group') }
-
       it 'has a school group selection box' do
-        expect(html).to have_select(:school_group_id, options: ['select'] + school_groups.sort_by(&:name).pluck(:name))
+        expect(html).to have_select(:school_group_id,
+                                    options: ['Select a school group'] + school_groups.sort_by(&:name).pluck(:name))
         expect(html).to have_css("form[action='#{admin_dashboard_path(user)}'][method='get']")
       end
     end
@@ -28,25 +27,19 @@ RSpec.describe Admin::QuickLinksComponent, :include_url_helpers, type: :componen
     describe 'select school box' do
       let!(:schools) { create_list(:school, 2, school_group: create(:school_group, default_issues_admin_user: user)) }
 
-      it { expect(html).to have_content('Select School') }
-
       it 'has a school selection box' do
-        expect(html).to have_select(:school_id, options: ['select'] + schools.sort_by(&:name).pluck(:name))
+        expect(html).to have_select(:school_id, options: ['Select a school'] + schools.sort_by(&:name).pluck(:name))
         expect(html).to have_css("form[action='#{admin_dashboard_path(user)}'][method='get']")
       end
     end
 
     describe 'find MPXN box' do
-      it { expect(html).to have_content('Find MPXN') }
-
       it 'has an MPXN search box' do
         expect(html).to have_css("form[action='#{admin_find_school_by_mpxn_index_path}'][method='get']")
       end
     end
 
     describe 'find URN box' do
-      it { expect(html).to have_content('Find URN') }
-
       it 'has a URN search box' do
         expect(html).to have_css("form[action='#{admin_find_school_by_urn_index_path}'][method='get']")
       end
