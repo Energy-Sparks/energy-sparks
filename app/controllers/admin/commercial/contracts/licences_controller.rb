f3a79b4453598f570f4c1ece3a3a5fee1bea11e0
Leigh Dodds <leigh@ldodds.com>
2026-04-13 16:15:29 +0100

    Update bulk licence editor to support adding and removing schools (#5144)

--- app/controllers/admin/commercial/contracts/licences_controller.rb
+++ app/controllers/admin/commercial/contracts/licences_controller.rb
@@ -6,6 +6,13 @@ module Admin
           class: 'Commercial::Contract'
 
         def edit
+          contracted_schools = @contract.licences.map(&:school_id)
+          @additional_schools = if @contract.contract_holder.is_a?(SchoolGroup)
+                                  scope = @contract.contract_holder.assigned_schools
+                                  scope.where.not(id: contracted_schools).by_name
+                                else
+                                  []
+                                end
         end
 
         def update
@@ -16,11 +23,23 @@ module Admin
           end
         end
 
+        def create_licence
+          @contract = ::Commercial::Contract.find(params[:contract_id])
+          @school = School.find(params[:school_id])
+          @licence = ::Commercial::LicenceManager.new(@school).school_onboarded(@contract)
+
+          respond_to do |format|
+            format.js
+            format.html { redirect_to edit_admin_commercial_contract_path(@contract) }
+          end
+        end
+
         private
 
         def contract_params
           params.require(:commercial_contract).permit(
             licences_attributes: [
+              :_destroy,
               :id, :start_date, :end_date, :status,
               :school_specific_price, :invoice_reference, :comments
             ]
