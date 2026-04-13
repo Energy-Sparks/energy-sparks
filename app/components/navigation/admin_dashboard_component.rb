80fcef4146654f1325f0afe082f72692b7ae6313
workluke <workluke@Lukes-Laptop.localdomain>
2026-04-13 14:16:39 +0100

    Added nav_sections method

--- app/components/navigation/admin_dashboard_component.rb
+++ app/components/navigation/admin_dashboard_component.rb
@@ -2,5 +2,14 @@
 
 module Navigation
   class AdminDashboardComponent < BaseComponent
+    def nav_sections
+      [
+        { name: 'My School Groups', path: admin_dashboard_school_groups_path(current_user) },
+        { name: 'My Project Groups', path: admin_dashboard_school_groups_path(current_user, group_type: 'project') },
+        { name: 'My Data Sources', path: admin_dashboard_data_sources_path(current_user) },
+        { name: 'My Data Feeds', path: admin_dashboard_amr_data_feed_configs_path(current_user) },
+        { name: 'My Issues', path: admin_dashboard_issues_path(current_user) }
+      ]
+    end
   end
 end
