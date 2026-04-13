a99c4bc638ef7b7186c5750a49e35f3ae40c7cb1
workluke <workluke@Lukes-Laptop.localdomain>
2026-04-13 16:18:24 +0100

    Initial links

--- app/components/navigation/admin_dashboard_component.rb
+++ app/components/navigation/admin_dashboard_component.rb
@@ -2,5 +2,14 @@
 
 module Navigation
   class AdminDashboardComponent < BaseComponent
+    def my_schools_section
+      [
+        { name: 'Onboarding', path: admin_school_onboardings_path },
+        { name: 'Awaiting activation', path: admin_activations_path },
+        { name: 'Recently onboarded', path: completed_admin_school_onboardings_path },
+        { name: 'Recent activities', path: admin_reports_activities_path },
+        { name: 'Recent actions', path: admin_reports_interventions_path }
+      ]
+    end
   end
 end
