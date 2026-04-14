b8451e1529bdc1f717599d974c564d2ef7120425
Leigh Dodds <leigh@ldodds.com>
2026-04-14 15:16:22 +0100

    Add links to contract and licensing history (#5169)

--- spec/components/commercial/licensing_summary_component_spec.rb
+++ spec/components/commercial/licensing_summary_component_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe Commercial::LicensingSummaryComponent, :include_application_helpe
         [
           [
             school.name,
-            'Yes',
+            '',
             licence.contract.contract_holder.name,
             school.default_contract_holder&.name,
             'Full',
