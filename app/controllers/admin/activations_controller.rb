3270f3f7b647bb6fbd0457d958cfcb686b1477fb
workluke <workluke@Lukes-Laptop.localdomain>
2026-04-16 09:59:40 +0100

    Simplified controller logic

--- app/controllers/admin/activations_controller.rb
+++ app/controllers/admin/activations_controller.rb
@@ -1,12 +1,9 @@
+# frozen_string_literal: true
+
 module Admin
   class ActivationsController < AdminController
     def index
-      @school_groups = if @dashboard_user
-                         SchoolGroup.organisation_groups.by_name.where(default_issues_admin_user: @dashboard_user)
-                       else
-                         SchoolGroup.organisation_groups.by_name
-                       end
-      @school_groups = @school_groups.select(&:has_schools_awaiting_activation?)
+      @school_groups = SchoolGroup.organisation_groups.by_name.select(&:has_schools_awaiting_activation?)
     end
   end
 end
--- app/controllers/admin/dashboard/activations_controller.rb
+++ app/controllers/admin/dashboard/activations_controller.rb
@@ -8,7 +8,8 @@ module Admin
       before_action :set_user
 
       def index
-        super
+        @school_groups = SchoolGroup.organisation_groups.where(default_issues_admin_user: @dashboard_user)
+                                    .by_name.select(&:has_schools_awaiting_activation?)
         build_breadcrumbs([
                             { name: @dashboard_user.display_name, href: admin_dashboard_path(@dashboard_user) },
                             { name: 'Activations' }
