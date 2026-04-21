e09c18ff3db60b0f9c255ea8d7380889343d3dd6
Deb Bassett <deb@urbanwide.com>
2026-04-21 10:04:53 +0100

    Fix bulk edit form

--- app/helpers/simple_form_helper.rb
+++ app/helpers/simple_form_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-# Ensures all Simple Form forms use BootstrapSwitcherFormBuilder unless explicitly overridden.
+# Ensures all Simple Form forms use CustomSimpleForm::FormBuilder unless explicitly overridden.
 # This allows us to switch between Bootstrap 4 and 5 wrappers based on the Current.bs5 flag
 module SimpleFormHelper
   def simple_form_for(record, options = {}, &)
-    options[:builder] ||= BootstrapSwitcherFormBuilder
+    options[:builder] ||= CustomSimpleForm::FormBuilder
     super
   end
 end
