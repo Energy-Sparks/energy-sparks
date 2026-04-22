9c4795b6260ee1aafe6ef0b6f85d01c20d8cfbfe
workluke <workluke@Lukes-Laptop.localdomain>
2026-04-22 16:10:12 +0100

    Activities and interventions

--- app/components/navigation/admin_dashboard_component.rb
+++ app/components/navigation/admin_dashboard_component.rb
@@ -18,8 +18,8 @@ module Navigation
         { name: 'Onboarding', path: admin_dashboard_school_onboardings_path(current_user) },
         { name: 'Awaiting activation', path: admin_dashboard_activations_path(current_user) },
         { name: 'Recently onboarded', path: completed_admin_dashboard_school_onboardings_path(current_user) },
-        { name: 'Recent activities', path: admin_dashboard_activities_path },
-        { name: 'Recent actions', path: admin_dashboard_interventions_path }
+        { name: 'Recent activities', path: admin_dashboard_activities_path(current_user) },
+        { name: 'Recent actions', path: admin_dashboard_interventions_path(current_user) }
       ]
     end
   end
