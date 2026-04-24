2687cce394f3c6491dcc99975836d437a05bc843
Leigh Dodds <leigh@ldodds.com>
2026-04-24 11:01:43 +0100

    Highlight custom contract terms

--- spec/system/admin/commercial/pricing_spec.rb
+++ spec/system/admin/commercial/pricing_spec.rb
@@ -19,10 +19,10 @@ describe 'pricing calculator' do
     context 'with default options' do
       before { click_on 'Calculate' }
 
-      it { expect(page).to have_content("Calculation based on pricing specified in #{product.name}") }
+      it { expect(page).to have_content("Calculation based on the default pricing defined in #{product.name}") }
 
       it_behaves_like 'it contains the expected data table', sortable: false, aligned: false do
-        let(:table_id) { '#pricing' }
+        let(:table_id) { '#pricing-table' }
         let(:expected_header) do
           [
             ['', 'Cost']
@@ -54,10 +54,10 @@ describe 'pricing calculator' do
         click_on 'Calculate'
       end
 
-      it { expect(page).to have_content("Calculation based on pricing specified in #{contract.name} for the #{contract.product.name} product") }
+      it { expect(page).to have_content("Calculation based on pricing defined in #{contract.name} for the #{contract.product.name} product") }
 
       it_behaves_like 'it contains the expected data table', sortable: false, aligned: false do
-        let(:table_id) { '#pricing' }
+        let(:table_id) { '#pricing-table' }
         let(:expected_header) do
           [
             ['', 'Cost']
@@ -72,58 +72,17 @@ describe 'pricing calculator' do
           ]
         end
       end
-    end
-  end
 
-  context 'when calculating a school price' do
-    let!(:school) { create(:school, number_of_pupils: 10) }
-    let!(:licence) { create(:commercial_licence, school:, school_specific_price: 1000) }
-    let(:contract) { licence.contract }
-
-    before do
-      visit admin_commercial_pricing_path(pricing: { school_id: school.id })
-    end
-
-    it { expect(page).to have_content("This page allows you to use the built-in price calculator to generate pricing for #{school.name}.")}
-
-    context 'with default options' do
-      before { click_on 'Calculate' }
-
-      it { expect(page).to have_content("Calculation based on pricing specified in #{contract.name} for the #{contract.product.name} product") }
-      it { expect(page).to have_content('Existing school pricing') }
-
-      it_behaves_like 'it contains the expected data table', sortable: false, aligned: false do
-        let(:table_id) { '#pricing' }
-        let(:expected_header) do
-          [
-            ['', 'Cost']
-          ]
+      context 'when the contract has custom terms' do
+        let!(:contract) do
+          create(:commercial_contract,
+                 invoice_terms: :pro_rata,
+                 licence_period: :custom,
+                 licence_years: 2.0)
         end
-        let(:expected_rows) do
-          [
-            ['Base Price', format_unit(contract.product.small_school_price, :£, true, :ks2, :text)],
-            ['Metering Fee', format_unit(0.0, :£, true, :ks2, :text)],
-            ['Private Account Fee', format_unit(0.0, :£, true, :ks2, :text)],
-            ['Total Price', format_unit(contract.product.small_school_price, :£, true, :ks2, :text)]
-          ]
-        end
-      end
 
-      it_behaves_like 'it contains the expected data table', sortable: false, aligned: false do
-        let(:table_id) { '#school-pricing' }
-        let(:expected_header) do
-          [
-            ['', 'Cost']
-          ]
-        end
-        let(:expected_rows) do
-          [
-            ['Base Price', format_unit(licence.school_specific_price, :£, true, :ks2, :text)],
-            ['Metering Fee', format_unit(0.0, :£, true, :ks2, :text)],
-            ['Private Account Fee', format_unit(0.0, :£, true, :ks2, :text)],
-            ['Total Price', format_unit(licence.school_specific_price, :£, true, :ks2, :text)]
-          ]
-        end
+        it { expect(page).to have_content('Pro-rata contract') }
+        it { expect(page).to have_content('Custom contract') }
       end
     end
   end
