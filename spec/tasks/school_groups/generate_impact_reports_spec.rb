db4ab4b3f8b0aadca2104cac4038ac58f1c16aff
Tristan H <878258+tbhi@users.noreply.github.com>
2026-04-27 11:15:57 +0000

    more

--- spec/tasks/school_groups/generate_impact_reports_spec.rb
+++ spec/tasks/school_groups/generate_impact_reports_spec.rb
@@ -5,17 +5,23 @@ require 'rails_helper'
 RSpec.describe 'school_groups:generate_impact_reports' do # rubocop:disable RSpec/DescribeClass
   include_context 'with a task'
 
-  it do
-    create(:school_group, :with_active_schools)
+  before do
+    school = create(:school, :with_school_group)
+    create(:school_onboarding, :with_completed, school_group: school.school_group)
+    create(:school_onboarding, school_group: school.school_group)
+    create(:user, school:)
     task.invoke
+  end
+
+  it 'creates the correct run and metric objects' do
     expect(ImpactReport::Run.all.map { |run| run.metrics.pluck(:metric_type, :value) }).to eq(
       [[['visible_schools', 1],
         ['data_visible_schools', 1],
-        ['users', 0],
-        ['active_users', 0],
+        ['users', 1],
+        ['active_users', 1],
         ['pupils', 1],
-        ['enrolled_schools', 0],
-        ['enrolling_schools', 0]]]
+        ['enrolled_schools', 1],
+        ['enrolling_schools', 1]]]
     )
   end
 end
