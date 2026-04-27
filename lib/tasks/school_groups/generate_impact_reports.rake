61c4eb9f3eda7903a18af57c7ca0a876182a4cda
Tristan H <878258+tbhi@users.noreply.github.com>
2026-04-27 16:36:30 +0000

    engagement metrics for group impact report

--- lib/tasks/school_groups/generate_impact_reports.rake
+++ lib/tasks/school_groups/generate_impact_reports.rake
@@ -7,11 +7,13 @@ namespace :school_groups do
 
       impact_report_run = ImpactReport::Run.create!(school_group:, run_date: Date.current)
       report = SchoolGroups::ImpactReport.new(school_group)
-      ImpactReport::Metric::OVERVIEW_METRICS.each do |metric_type|
-        value = report.overview.public_send(metric_type)
-        value = value.count if value.respond_to?(:count)
-        ImpactReport::Metric.create!(impact_report_run:, enough_data: true, metric_category: :overview, metric_type:,
-                                     value:)
+      [[ImpactReport::Metric::OVERVIEW_METRICS, :overview],
+       [ImpactReport::Metric::ENGAGEMENT_METRICS, :engagement]].each do |metrics, metric_category|
+        metrics.each do |metric_type|
+          value = report.public_send(metric_category).public_send(metric_type)
+          value = value.count if value.respond_to?(:count)
+          ImpactReport::Metric.create!(impact_report_run:, enough_data: true, metric_category:, metric_type:, value:)
+        end
       end
     end
   rescue StandardError => e
--- spec/tasks/school_groups/generate_impact_reports_spec.rb
+++ spec/tasks/school_groups/generate_impact_reports_spec.rb
@@ -10,18 +10,25 @@ RSpec.describe 'school_groups:generate_impact_reports' do # rubocop:disable RSpe
     create(:school_onboarding, :with_completed, school_group: school.school_group)
     create(:school_onboarding, school_group: school.school_group)
     create(:user, school:)
+    create(:activity, school:)
+    create(:observation, :intervention, school:)
+    create(:school_target, school:)
     task.invoke
   end
 
   it 'creates the correct run and metric objects' do
-    expect(ImpactReport::Run.all.map { |run| run.metrics.pluck(:metric_type, :value) }).to eq(
-      [[['visible_schools', 1],
-        ['data_visible_schools', 1],
-        ['users', 1],
-        ['active_users', 1],
-        ['pupils', 1],
-        ['enrolled_schools', 1],
-        ['enrolling_schools', 1]]]
+    # debugger
+    expect(ImpactReport::Run.all.map do |run|
+      run.metrics.pluck(:metric_category, :metric_type,
+                        :value).group_by(&:first).transform_values do |rows|
+        rows.to_h do |_, field, value|
+          [field, value]
+        end
+      end.deep_symbolize_keys
+    end).to eq(
+      [{ overview: { active_users: 1, data_visible_schools: 1, enrolled_schools: 1, enrolling_schools: 1, pupils: 1,
+                     users: 1, visible_schools: 1 },
+         engagement: { actions: 1, activities: 1, points: 65, targets: 1 } }]
     )
   end
 end
