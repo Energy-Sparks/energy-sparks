a1dbea1dc4f6c2914634e86acf3bdb290f16a189
Tristan H <878258+tbhi@users.noreply.github.com>
2026-04-27 13:39:36 +0000

    more

--- spec/tasks/school_groups/generate_impact_reports_spec.rb
+++ spec/tasks/school_groups/generate_impact_reports_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'school_groups:generate_impact_reports' do # rubocop:disable RSpe
   include_context 'with a task'
 
   before do
-    school = create(:school, :with_school_group)
+    school = create(:school, :with_school_group, number_of_pupils: 1)
     create(:school_onboarding, :with_completed, school_group: school.school_group)
     create(:school_onboarding, school_group: school.school_group)
     create(:user, school:)
