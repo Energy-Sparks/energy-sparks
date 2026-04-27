f1f3edb2b4365f66ee3e00db8799ea27193cc0ed
workluke <workluke@Lukes-Laptop.localdomain>
2026-04-27 10:03:59 +0100

    Specs

--- spec/components/admin/dashboard_prompt_component_spec.rb
+++ spec/components/admin/dashboard_prompt_component_spec.rb
@@ -10,54 +10,62 @@ RSpec.describe Admin::DashboardPromptComponent, :include_url_helpers, type: :com
   let(:user) { create(:admin) }
 
   context 'when rendering' do
-    let(:html) do
-      render_inline(component)
-    end
-
     describe 'overdue issues prompt' do
       context 'when there are no overdue issues' do
+        before do
+          render_inline(component)
+        end
+
         it 'does not display the overdue issues prompt' do
-          expect(html).to have_no_css('#overdue-issues')
+          expect(page).to have_no_text('issues overdue for review')
         end
       end
 
       context 'when there are overdue issues' do
         before do
           create_list(:issue, 2, owned_by: user, review_date: Date.current - 2)
+          render_inline(component)
         end
 
         it 'displays the overdue issues prompt' do
-          expect(html).to have_css('#overdue-issues')
-          expect(html).to have_text('You have 2 issues overdue for review')
-          expect(html).to have_link('View Issues', href: admin_dashboard_issues_path(dashboard_id: user, user: user.id))
+          expect(page).to have_text('You have 2 issues overdue for review')
+          expect(page).to have_link('View Issues', href: admin_dashboard_issues_path(dashboard_id: user, user: user.id))
         end
       end
     end
 
     describe 'weekly issues prompt' do
       context 'when there are no issues due in the coming week' do
+        before do
+          render_inline(component)
+        end
+
         it 'does not display the weekly issues prompt' do
-          expect(html).to have_no_css('#weekly-issues')
+          expect(page).to have_no_text('review in the next week')
         end
       end
 
       context 'when there are issues due in the coming week' do
         before do
           create_list(:issue, 2, owned_by: user, review_date: Date.current + 2)
+          render_inline(component)
         end
 
         it 'displays the weekly issues prompt' do
-          expect(html).to have_css('#weekly-issues')
-          expect(html).to have_text('You have 2 issues due for review in the next week')
-          expect(html).to have_link('View Issues', href: admin_dashboard_issues_path(dashboard_id: user, user: user.id))
+          expect(page).to have_text('You have 2 issues due for review in the next week')
+          expect(page).to have_link('View Issues', href: admin_dashboard_issues_path(dashboard_id: user, user: user.id))
         end
       end
     end
 
     describe 'school activation prompt' do
       context 'when no schools are awaiting activation' do
+        before do
+          render_inline(component)
+        end
+
         it 'does not display the school activation prompt' do
-          expect(html).to have_no_css('#school-activation')
+          expect(page).to have_no_text('schools awaiting activation')
         end
       end
 
@@ -65,20 +73,24 @@ RSpec.describe Admin::DashboardPromptComponent, :include_url_helpers, type: :com
         before do
           create(:school, school_group: create(:school_group, default_issues_admin_user: user), active: true,
                           visible: false, data_enabled: false)
+          render_inline(component)
         end
 
         it 'displays the school activation prompt' do
-          expect(html).to have_css('#school-activation')
-          expect(html).to have_text('You have 1 schools awaiting activation')
-          expect(html).to have_link('Activations', href: admin_activations_path)
+          expect(page).to have_text('You have 1 schools awaiting activation')
+          expect(page).to have_link('Activations', href: admin_dashboard_activations_path(dashboard_id: user))
         end
       end
     end
 
     describe 'lagging data source prompt' do
       context 'when there are no lagging data sources' do
+        before do
+          render_inline(component)
+        end
+
         it 'does not display the lagging data sources prompt' do
-          expect(html).to have_no_css('#lagging-data-sources')
+          expect(page).to have_no_text('lagging data sources')
         end
       end
 
@@ -86,20 +98,24 @@ RSpec.describe Admin::DashboardPromptComponent, :include_url_helpers, type: :com
         before do
           create(:gas_meter_with_validated_reading_dates, end_date: 11.days.ago,
                                                           data_source: create(:data_source, owned_by: user))
+          render_inline(component)
         end
 
         it 'displays the lagging data sources prompt' do
-          expect(html).to have_css('#lagging-data-sources')
-          expect(html).to have_text('You have 1 lagging data sources')
-          expect(html).to have_link('View Data Sources', href: admin_dashboard_data_sources_path(dashboard_id: user))
+          expect(page).to have_text('You have 1 lagging data sources')
+          expect(page).to have_link('View Data Sources', href: admin_dashboard_data_sources_path(dashboard_id: user))
         end
       end
     end
 
     describe 'missing data feeds prompt' do
       context 'when there are no data feeds with missing data' do
+        before do
+          render_inline(component)
+        end
+
         it 'does not display the missing data feeds prompt' do
-          expect(html).to have_no_css('#missing-data-feeds')
+          expect(page).to have_no_text('configurations with missing data')
         end
       end
 
@@ -113,12 +129,12 @@ RSpec.describe Admin::DashboardPromptComponent, :include_url_helpers, type: :com
             reading_date: 4.days.ago,
             updated_at: 4.days.ago
           )
+          render_inline(component)
         end
 
         it 'displays the missing data feeds prompt' do
-          expect(html).to have_css('#missing-data-feeds')
-          expect(html).to have_text('You have 1 amr data feed configurations with missing data')
-          expect(html).to have_link('View AMR Data Feed Configurations',
+          expect(page).to have_text('You have 1 amr data feed configurations with missing data')
+          expect(page).to have_link('View AMR Data Feed Configurations',
                                     href: admin_dashboard_amr_data_feed_configs_path(dashboard_id: user))
         end
       end
