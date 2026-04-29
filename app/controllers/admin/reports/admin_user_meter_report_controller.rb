4779c6a2273da216497b8d61119e25b17cc9790a
Tristan H <878258+tbhi@users.noreply.github.com>
2026-04-29 20:00:34 +0000

    fix csv and user filtering on admin meters report

--- app/controllers/admin/reports/admin_user_meter_report_controller.rb
+++ app/controllers/admin/reports/admin_user_meter_report_controller.rb
@@ -3,6 +3,11 @@
 module Admin
   module Reports
     class AdminUserMeterReportController < BaseImportReportsController
+      def index
+        params[:admin] = current_user.id unless params.key?(:admin)
+        super
+      end
+
       private
 
       def description
@@ -14,21 +19,30 @@ module Admin
       end
 
       def results
-        default_issues_admin_user = User.admin.find_by(id: params[:admin]) || User.admin.first
+        return nil if !params.key?(:school_group) && !params.key?(:admin)
+
         Meter.active
              .joins(:school)
              .includes(:school, { school: :school_group })
-             .where(schools: { school_groups: { default_issues_admin_user: } })
+             .then { |scope| filter_by_admin(scope) }
              .then { |scope| filter_by_meter_type(scope) }
              .then { |scope| filter_by_school_group(scope) }
       end
 
+      def filter_by_admin(scope)
+        filter(scope, params[:admin]) { scope.where(school_groups: { default_issues_admin_user_id: params[:admin] }) }
+      end
+
       def filter_by_meter_type(scope)
-        params[:meter_type].present? ? scope.where(meter_type: params[:meter_type]) : scope
+        filter(scope, params[:meter_type]) { scope.where(meter_type: params[:meter_type]) }
       end
 
       def filter_by_school_group(scope)
-        params[:school_group].present? ? scope.where(schools: { school_group_id: params[:school_group] }) : scope
+        filter(scope, params[:school_group]) { scope.where(schools: { school_group_id: params[:school_group] }) }
+      end
+
+      def filter(scope, condition)
+        condition.present? ? yield : scope
       end
 
       def container_class
