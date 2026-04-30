ad14a7611ef1be1b85cad8e2d6c379b78d2c4831
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-04-30 14:22:03 +0100

    Dashboard specific controller and template edits

--- app/controllers/admin/dashboard/engaged_groups_controller.rb
+++ app/controllers/admin/dashboard/engaged_groups_controller.rb
@@ -9,6 +9,7 @@ module Admin
 
       def index
         super
+        @engaged_groups = @engaged_groups.where(default_issues_admin_user: @dashboard_user)
         build_breadcrumbs([
                             { name: @dashboard_user.display_name, href: admin_dashboard_path(@dashboard_user) },
                             { name: 'Engaged Schools' }
