00c533d27f3d8ea4f3a38273d437140db22a6de3
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-05-06 15:44:41 +0100

    New prompt

--- app/controllers/admin/dashboard/missing_alert_contacts_controller.rb
+++ app/controllers/admin/dashboard/missing_alert_contacts_controller.rb
@@ -9,7 +9,7 @@ module Admin
 
       def index
         super
-        @schools = @schools.joins(:school_group).where(school_group: { default_issues_admin_user: @dashboard_user })
+        @schools = @schools.where(school_group: { default_issues_admin_user: @dashboard_user })
         build_breadcrumbs([
                             { name: @dashboard_user.display_name, href: admin_dashboard_path(@dashboard_user) },
                             { name: 'Missing alert contacts' }
