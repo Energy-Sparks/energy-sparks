0d1061d4af94634498b6332c702ff46c5ed7e530
LukeGouldson <156131448+LukeGouldson@users.noreply.github.com>
2026-05-07 09:35:36 +0100

    Missing alert contacts dashboard (#5254)
    Adds the 'missing alert contacts' page to the admin dashboard, and a prompt for this page

--- app/components/navigation/admin_dashboard_component.rb
+++ app/components/navigation/admin_dashboard_component.rb
@@ -19,7 +19,8 @@ module Navigation
         { name: 'Awaiting activation', path: admin_dashboard_activations_path(current_user) },
         { name: 'Recently onboarded', path: completed_admin_dashboard_school_onboardings_path(current_user) },
         { name: 'Recent activities', path: admin_dashboard_activities_path(current_user) },
-        { name: 'Recent actions', path: admin_dashboard_interventions_path(current_user) }
+        { name: 'Recent actions', path: admin_dashboard_interventions_path(current_user) },
+        { name: 'Missing alert contacts', path: admin_dashboard_missing_alert_contacts_path(current_user) }
       ]
     end
   end
