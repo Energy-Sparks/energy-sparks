9a7e373bf056d0dcaf672565efaa09e0a67d139a
Deb Bassett <deb@urbanwide.com>
2026-05-12 16:09:41 +0100

    Don't need this

--- app/models/impact_report/run.rb
+++ app/models/impact_report/run.rb
@@ -33,10 +33,6 @@ module ImpactReport
       define_method(category) do |type|
         metric(category, type)
       end
-
-      define_method("#{category}?") do |type|
-        metric(category, type).displayable?
-      end
     end
 
     def metric(category, type)
