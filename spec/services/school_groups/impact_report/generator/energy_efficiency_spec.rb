7de62bf6e7eb214568c3824761a42b1ab8468054
Tristan H <878258+tbhi@users.noreply.github.com>
2026-05-13 10:52:32 +0000

    more

--- spec/services/school_groups/impact_report/generator/energy_efficiency_spec.rb
+++ spec/services/school_groups/impact_report/generator/energy_efficiency_spec.rb
@@ -27,11 +27,16 @@ describe SchoolGroups::ImpactReport::Generator::EnergyEfficiency do
     end
 
     context 'with electricity' do
-      let(:alerts) { create(:alert, :with_run, :energy_annual_versus_benchmark, school:) }
+      let(:alerts) do
+        [create(:alert, :with_run, :energy_annual_versus_benchmark, school:),
+         create(:alert, :with_run, :energy_annual_versus_benchmark, school: create(:school))]
+      end
 
       it 'has the right metrics' do
         expect(energy.metrics).to contain_exactly(
-          electricity(:gbp, value: 800), electricity(:co2, value: 400), electricity(:kwh, value: 500),
+          electricity(:gbp, value: 800),
+          electricity(:co2, value: 400),
+          electricity(:kwh, value: 500),
           gas(:gbp, enough_data: false, number_of_schools: 0),
           gas(:co2, enough_data: false, number_of_schools: 0),
           gas(:kwh, enough_data: false, number_of_schools: 0)
@@ -54,7 +59,9 @@ describe SchoolGroups::ImpactReport::Generator::EnergyEfficiency do
           electricity(:gbp, enough_data: false, number_of_schools: 0),
           electricity(:co2, enough_data: false, number_of_schools: 0),
           electricity(:kwh, enough_data: false, number_of_schools: 0),
-          gas(:gbp, value: 800), gas(:co2, value: 400), gas(:kwh, value: 500)
+          gas(:gbp, value: 800),
+          gas(:co2, value: 400),
+          gas(:kwh, value: 500)
         )
       end
     end
