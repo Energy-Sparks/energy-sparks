3db236cec139ea1d321875170021f07a22e4be20
Leigh Dodds <leigh@ldodds.com>
2026-05-19 14:18:02 +0100

    Removed unneeded calculations

--- app/services/commercial/price_calculator.rb
+++ app/services/commercial/price_calculator.rb
@@ -7,6 +7,9 @@ module Commercial
     #
     # Used for producing an estimated price for a school that has not yet joined Energy Sparks so
     # there is not yet a school record.
+    #
+    # Does not produce pro-rata pricing at present, estimates are assumed to be for the full
+    # contracted period
     def calculate(number_of_pupils:, number_of_meters:, product: nil, contract: nil, private_account: false)
       raise unless product || contract
 
@@ -20,44 +23,6 @@ module Commercial
         metering_fee: metering_fee(product: contracted_product, number_of_meters:),
         private_account_fee: private_account_fee(product: contracted_product, private_account:)
       )
-    end
-
-    # Calculates a price for a school under an existing contract. Does not
-    # check for any existing licences to find school specific pricing, so not suitable for renewals
-    def for_school(school:, product: nil, contract: nil)
-      calculate(
-        product:,
-        contract:,
-        number_of_pupils: school.number_of_pupils,
-        number_of_meters: school.meters.main_meter.active.count,
-        private_account: !school.data_sharing_public?
-      )
-    end
-
-    # Calculates a renewal price for an individual school. Uses the school's current licence
-    # as the basis for the calculation, applying school or contract specific pricing if set.
-    #
-    # If the current school licence is for an alternate funder, then the renewal price will be based on
-    # switching to using the terms of latest contract for the school's default contract holder. Or our
-    # default product if they don't have a contract, or if the school is going to be self-funded.
-    def for_school_renewal(school:)
-      licence = school.licences.current.first
-      return nil unless licence
-
-      contract = renewal_contract(school, licence)
-      product = renewal_product(contract)
-
-      base_price = renewal_base_price(school, product, contract, licence)
-
-      if base_price == 0.0
-        Price::FREE
-      else
-        Price.new(
-          base_price:,
-          metering_fee: metering_fee(product:, number_of_meters: school.meters.main_meter.active.count),
-          private_account_fee: private_account_fee(product:, private_account: !school.data_sharing_public?)
-        )
-      end
     end
 
     private
