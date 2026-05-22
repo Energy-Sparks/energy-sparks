cbd01d93d86432869aa630dcea442b3431aab44f
Tristan H <878258+tbhi@users.noreply.github.com>
2026-05-22 20:07:59 +0000

    combine exemplar and well managed in impact report benchmark metrics

--- app/services/school_groups/impact_report/generator/benchmark.rb
+++ app/services/school_groups/impact_report/generator/benchmark.rb
@@ -14,28 +14,29 @@ module SchoolGroups
                       [[:gas], :heating_control]].freeze
         BENCHMARK_TYPES = %i[exemplar well_managed].freeze
         private_constant :BENCHMARKS, :BENCHMARK_TYPES
-        METRICS = BENCHMARKS.flat_map do |_fuel_types, benchmark|
-          BENCHMARK_TYPES.map { |type| metric_name(benchmark, type) }
-        end
+        METRICS = BENCHMARKS.map(&:second)
 
         def metrics
-          BENCHMARKS.flat_map do |fuel_types, benchmark|
-            fuel_types.flat_map do |fuel_type|
-              exemplar, well_managed, number_of_schools =
-                categorise(fuel_types.length == 1 ? nil : fuel_type, benchmark)
-              [metric(fuel_type, benchmark, :exemplar, number_of_schools, exemplar),
-               metric(fuel_type, benchmark, :well_managed, number_of_schools, well_managed)]
+          BENCHMARKS.flat_map do |fuel_types, metric_type|
+            fuel_types.map do |fuel_type|
+              good_schools, number_of_schools = categorise(fuel_types.length == 1 ? nil : fuel_type, metric_type)
+              { enough_data: !number_of_schools.zero?,
+                fuel_type:,
+                metric_category:,
+                metric_type:,
+                number_of_schools:,
+                value: good_schools }
             end
           end
         end
 
         private
 
-        def metric(fuel_type, benchmark, type, number_of_schools, value)
+        def metric(fuel_type, metric_type, number_of_schools, value)
           { enough_data: !number_of_schools.zero?,
             fuel_type:,
             metric_category:,
-            metric_type: self.class.metric_name(benchmark, type),
+            metric_type: benchmark,
             number_of_schools:,
             value: }
         end
@@ -48,8 +49,8 @@ module SchoolGroups
           key = [fuel_type, benchmark].compact.join('_')
           categories = SchoolGroups::CategoriseSchools.new(schools: @impact_report.visible_schools)
                                                       .categorise_schools_for_advice_page(AdvicePage.find_by(key:))
-          %i[exemplar_school benchmark_school].map { |category| categories[category]&.count || 0 } +
-            [categories.values.sum(&:count)]
+          [%i[exemplar_school benchmark_school].sum { |category| categories[category]&.count || 0 },
+           categories.values.sum(&:count)]
         end
       end
     end
