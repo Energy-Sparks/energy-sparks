6cb4e3d3e7af7f4597b2a4c76b6e6b315e48879e
Tristan H <878258+tbhi@users.noreply.github.com>
2026-05-22 20:11:17 +0000

    more

--- app/services/school_groups/impact_report/generator/benchmark.rb
+++ app/services/school_groups/impact_report/generator/benchmark.rb
@@ -4,16 +4,11 @@ module SchoolGroups
   class ImpactReport
     class Generator
       class Benchmark < Base
-        def self.metric_name(benchmark, type)
-          [benchmark, type].join('_').to_sym
-        end
-
         BENCHMARKS = [[%i[electricity gas], :out_of_hours],
                       [%i[electricity gas], :long_term],
                       [[:electricity], :baseload],
                       [[:gas], :heating_control]].freeze
-        BENCHMARK_TYPES = %i[exemplar well_managed].freeze
-        private_constant :BENCHMARKS, :BENCHMARK_TYPES
+        private_constant :BENCHMARKS
         METRICS = BENCHMARKS.map(&:second)
 
         def metrics
@@ -31,15 +26,6 @@ module SchoolGroups
         end
 
         private
-
-        def metric(fuel_type, metric_type, number_of_schools, value)
-          { enough_data: !number_of_schools.zero?,
-            fuel_type:,
-            metric_category:,
-            metric_type: benchmark,
-            number_of_schools:,
-            value: }
-        end
 
         def metric_category
           :energy_efficiency
