42742b70e4ab449d51bc8f0d8366544e244c54e2
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-05-27 11:31:01 +0100

    Mailer and spec changes

--- spec/mailers/issues_report_mailer_spec.rb
+++ spec/mailers/issues_report_mailer_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe IssuesReportMailer, :include_application_helper do
 
   describe '#issues_report' do
     def create_issue(**)
-      create(:issue, issue_type: :issue, status: :open, owned_by: admin, created_at: 5.days.ago, created_by: admin,
-                     review_date: 1.week.from_now, **)
+      create(:issue, :with_tags, issue_type: :issue, status: :open, owned_by: admin, created_at: 5.days.ago,
+                                 created_by: admin, review_date: 1.week.from_now, **)
     end
 
     def create_inactive_school_issue
@@ -41,35 +41,36 @@ RSpec.describe IssuesReportMailer, :include_application_helper do
       }
 
       it { expect(body).to have_link(issue.title, href: admin_school_issue_url(issue.issueable, issue)) }
-      it { expect(body).to have_content(issue.school_group.name) }
-      it { expect(body).to have_content(issue.fuel_type.capitalize) }
-      it { expect(body).to have_content(issue.issueable.name) }
-      it { expect(body).to have_content(short_dates(issue.review_date)) }
-      it { expect(body).to have_content(issue.created_by.display_name) }
-      it { expect(body).to have_content(short_dates(issue.created_at)) }
-      it { expect(body).to have_content(issue.updated_by.display_name) }
-      it { expect(body).to have_content(short_dates(issue.updated_at)) }
+      it { expect(body).to have_text(issue.school_group.name) }
+      it { expect(body).to have_text(issue.issue_tags.first.label) }
+      it { expect(body).to have_text(issue.fuel_type.capitalize) }
+      it { expect(body).to have_text(issue.issueable.name) }
+      it { expect(body).to have_text(short_dates(issue.review_date)) }
+      it { expect(body).to have_text(issue.created_by.display_name) }
+      it { expect(body).to have_text(short_dates(issue.created_at)) }
+      it { expect(body).to have_text(issue.updated_by.display_name) }
+      it { expect(body).to have_text(short_dates(issue.updated_at)) }
       it { expect(body).to have_link('Edit', href: edit_admin_school_issue_url(issue.issueable, issue)) }
 
       it {
         expect(body).to have_link("View all issues for: #{admin.display_name}", href: admin_issues_url(user: admin))
       }
 
-      it { expect(body).to have_no_content(issues[:note].title) }
-      it { expect(body).to have_no_content(issues[:closed_issue].title) }
-      it { expect(body).to have_no_content(issues[:someone_elses_issue].title) }
+      it { expect(body).to have_no_text(issues[:note].title) }
+      it { expect(body).to have_no_text(issues[:closed_issue].title) }
+      it { expect(body).to have_no_text(issues[:someone_elses_issue].title) }
     end
 
     context 'when there are new issues for user' do
       let(:issues) { [create_issue] }
 
-      it { expect(body).to have_content('new!') }
+      it { expect(body).to have_text('new!') }
     end
 
     context 'when there are only old issues for user' do
       let(:issues) { [create_issue(created_at: 8.days.ago)] }
 
-      it { expect(body).to have_no_content('new!') }
+      it { expect(body).to have_no_text('new!') }
     end
 
     context "when there aren't any issues for user" do
@@ -96,10 +97,10 @@ RSpec.describe IssuesReportMailer, :include_application_helper do
           old: create_issue(created_at: 2.weeks.ago, review_date: 8.days.from_now) }
       end
 
-      it { expect(body).to have_content(issues[:overdue].title) }
-      it { expect(body).to have_content(issues[:next_week].title) }
-      it { expect(body).to have_content(issues[:new].title) }
-      it { expect(body).to have_no_content(issues[:old].title) }
+      it { expect(body).to have_text(issues[:overdue].title) }
+      it { expect(body).to have_text(issues[:next_week].title) }
+      it { expect(body).to have_text(issues[:new].title) }
+      it { expect(body).to have_no_text(issues[:old].title) }
     end
 
     context 'with the csv report' do
@@ -111,6 +112,7 @@ RSpec.describe IssuesReportMailer, :include_application_helper do
                    'New' => 'New this week!',
                    'Group' => issue.school_group.name,
                    'Title' => issue.title,
+                   'Tags' => 'issue tag',
                    'Fuel' => 'Gas',
                    'Next Review Date' => issue.review_date.strftime('%d/%m/%Y'),
                    'Created By' => issue.created_by.display_name,
