c83557d20cf4768fab6480ee92f8116784cf7e87
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-05-28 08:31:20 +0100

    Issue factory edit

--- spec/factories/issues.rb
+++ spec/factories/issues.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :issue do
     issueable { create(:school) }
@@ -8,5 +10,17 @@ FactoryBot.define do
     fuel_type { :gas }
     created_by { association :user }
     updated_by { association :user }
+
+    trait :with_tags do
+      transient do
+        tags_count { 1 }
+      end
+
+      after(:create) do |issue, evaluator|
+        create_list(:issue_tag, evaluator.tags_count).each do |t|
+          issue.issue_tags << t
+        end
+      end
+    end
   end
 end
