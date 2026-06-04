f91ff6ba2b74e3d614651eb7fdce148623744359
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-06-04 14:33:29 +0100

    Delete duplicate js file

--- app/assets/javascripts/preserve_tab_links.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.tabs-edit-return .tab-content a.btn.edit').forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    window.location.href = `${element.href}${encodeURIComponent(window.location.hash)}`;
-  });
-});
