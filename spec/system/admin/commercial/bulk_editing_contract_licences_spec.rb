e9b1f1e0f9b089347bd4b4e956d8a1e6fc274e6c
Leigh Dodds <leigh@ldodds.com>
2026-06-09 12:15:58 +0100

    Fix specs

--- spec/system/admin/commercial/bulk_editing_contract_licences_spec.rb
+++ spec/system/admin/commercial/bulk_editing_contract_licences_spec.rb
@@ -17,12 +17,15 @@ describe 'bulk editing contract licences' do
   end
 
   before do
+    calendar = create(:calendar, :default_national)
+    create(:academic_year, calendar:)
+
     sign_in(user)
     visit admin_commercial_contract_path(contract)
     click_on('Manage All Licences')
   end
 
-  it { expect(page).to have_content("#{contract.name} Bulk Licence Editor") }
+  it { expect(page).to have_text("#{contract.name} Bulk Licence Editor") }
 
   def field_name(licence, name)
     "commercial_contract[licences_attributes][#{licence.id}][#{name}]"
@@ -39,13 +42,13 @@ describe 'bulk editing contract licences' do
       end
     end
 
-    it { expect(page).to have_content('Undo') }
+    it { expect(page).to have_text('Undo') }
 
     context 'when the changes are saved' do
       before { click_on 'Save changes' }
 
       it 'deletes the licence' do
-        expect(page).to have_content('Licences updated')
+        expect(page).to have_text('Licences updated')
         expect(Commercial::Licence.all).to eq([licence])
       end
     end
@@ -62,7 +65,7 @@ describe 'bulk editing contract licences' do
     end
 
     it 'updates the licence' do
-      expect(page).to have_content('Licences updated')
+      expect(page).to have_text('Licences updated')
       expect(licence_to_be_modified.reload).to have_attributes({
                                                                  school_specific_price: 200.0,
                                                                  status: 'confirmed',
@@ -88,18 +91,18 @@ describe 'bulk editing contract licences' do
 
       before { refresh }
 
-      it { expect(page).to have_content('Add schools to contract') }
+      it { expect(page).to have_text('Add schools to contract') }
 
       it 'lists the school' do
         within("#contract-#{contract.id}-additional-schools-table") do
-          expect(page).to have_content(additional_school.name)
-          expect(page).to have_link('View issues', href: admin_school_issues_path(additional_school))
+          expect(page).to have_text(additional_school.name)
+          expect(page).to have_link('Issues', href: admin_school_issues_path(additional_school))
         end
       end
 
       it 'does not show a licence for the school' do
         within("#contract-#{contract.id}-licence-table") do
-          expect(page).to have_no_content(additional_school.name)
+          expect(page).to have_no_text(additional_school.name)
         end
       end
 
@@ -115,7 +118,7 @@ describe 'bulk editing contract licences' do
 
         it 'updates the licence table to include the licence' do
           within("#contract-#{contract.id}-licence-table") do
-            expect(page).to have_content(additional_school.name)
+            expect(page).to have_text(additional_school.name)
           end
           expect(additional_school.reload.licences.count).to eq(1)
         end
@@ -130,7 +133,7 @@ describe 'bulk editing contract licences' do
           end
 
           it 'deletes the licence' do
-            expect(page).to have_content('Licences updated')
+            expect(page).to have_text('Licences updated')
             expect(additional_school.licences.reload).to be_empty
           end
         end
@@ -142,7 +145,7 @@ describe 'bulk editing contract licences' do
           end
 
           it 'updates the newly added licence' do
-            expect(page).to have_content('Licences updated')
+            expect(page).to have_text('Licences updated')
             expect(additional_school.licences.reload.last.comments).to eq('Some comments')
           end
         end
