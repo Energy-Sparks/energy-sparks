56f0e3ee9c565af4f8967ef96433b28e028bc15a
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-06-10 10:03:06 +0100

    Reworked specs

--- spec/system/admin/meter_attributes_spec.rb
+++ spec/system/admin/meter_attributes_spec.rb
@@ -57,7 +57,6 @@ RSpec.describe 'meter attribute management', :meters, type: :system do
       end
     end
 
-
     it 'is able to display a form for all meter attributes' do
       visit admin_school_single_meter_attribute_path(school, gas_meter)
       options = find('#type').all('option').collect(&:text)
@@ -70,42 +69,96 @@ RSpec.describe 'meter attribute management', :meters, type: :system do
       end
     end
 
-    it 'allow the admin to manage the meter attributes' do
-      visit school_path(school)
-      click_on 'Meter attributes'
-      select 'Heating model', from: 'type'
-      click_on 'New attribute'
-
-      fill_in 'Max summer daily heating kwh', with: 800
-
-      click_on 'Create'
-
-      expect(gas_meter.meter_attributes.size).to eq(1)
-      attribute = gas_meter.meter_attributes.first
-      expect { attribute.to_analytics }.not_to raise_error
-      expect(attribute.to_analytics.to_s).to include('800')
-
-
-      within '#database-meter-attributes' do
-        click_on 'Edit'
+    describe 'managing meter attributes' do
+      before do
+        visit school_path(school)
+        click_on 'Meter attributes'
       end
 
-      fill_in 'Max summer daily heating kwh', with: 200
+      context 'when creating an attribute' do
+        let(:attribute) { gas_meter.meter_attributes.first }
 
-      click_on 'Update'
+        before do
+          select 'Heating model', from: 'type'
+          click_on 'New attribute'
+          fill_in 'Max summer daily heating kwh', with: 800
+          click_on 'Create'
+        end
 
-      gas_meter.reload
-      new_attribute = gas_meter.meter_attributes.active.first
-      expect(new_attribute.to_analytics.to_s).to include('200')
-      attribute.reload
-      expect(attribute.replaced_by).to eq(new_attribute)
-
-      within '#database-meter-attributes' do
-        click_on 'Delete'
+        it 'allows creation of an attribute' do
+          expect(gas_meter.meter_attributes.size).to eq(1)
+          expect { attribute.to_analytics }.not_to raise_error
+          expect(attribute.to_analytics.to_s).to include('800')
+        end
       end
-      expect(gas_meter.meter_attributes.active.size).to eq(0)
-      new_attribute.reload
-      expect(new_attribute.deleted_by).to eq(admin)
+
+      context 'when editing an attribute' do
+        let!(:meter_attribute) do
+          create(:meter_attribute, attribute_type: 'heating_model',
+                                   input_data: { max_summer_daily_heating_kwh: 300 }, meter: gas_meter)
+        end
+
+        let(:new_attribute) { gas_meter.meter_attributes.active.first }
+
+        before do
+          refresh
+          within '#database-meter-attributes' do
+            click_on 'Edit'
+          end
+          fill_in 'Max summer daily heating kwh', with: 200
+          click_on 'Update'
+          gas_meter.reload
+        end
+
+        it 'allows editing of an attribute' do
+          meter_attribute.reload
+          expect(new_attribute.to_analytics.to_s).to include('200')
+          expect(meter_attribute.replaced_by).to eq(new_attribute)
+        end
+      end
+
+      context 'when deleting an attribute' do
+        let!(:meter_attribute) do
+          create(:meter_attribute, attribute_type: 'heating_model',
+                                   input_data: { max_summer_daily_heating_kwh: 300 }, meter: gas_meter)
+        end
+
+        before do
+          refresh
+          within '#database-meter-attributes' do
+            click_on 'Delete'
+          end
+        end
+
+        it 'allows deletion of an attribute' do
+          expect(gas_meter.meter_attributes.active.size).to eq(0)
+          meter_attribute.reload
+          expect(meter_attribute.deleted_by).to eq(admin)
+        end
+      end
+
+      context 'when restoring an attribute' do
+        let!(:meter_attribute) do
+          create(:meter_attribute, attribute_type: 'heating_model',
+                                   input_data: { max_summer_daily_heating_kwh: 300 }, meter: gas_meter)
+        end
+
+        before do
+          refresh
+          within '#database-meter-attributes' do
+            click_on 'Delete'
+          end
+          within '#deleted-meter-attributes-content' do
+            click_on 'Restore'
+          end
+        end
+
+        it 'allows restoration of an attribute' do
+          expect(gas_meter.meter_attributes.active.size).to eq(1)
+          meter_attribute.reload
+          expect(meter_attribute.deleted_by).to be_nil
+        end
+      end
     end
 
     it 'allows creating and editing of an attribute with nested TimeOfDay values' do
@@ -172,116 +225,273 @@ RSpec.describe 'meter attribute management', :meters, type: :system do
       })
     end
 
-    it 'allow the admin to manage school meter attributes' do
-      visit school_path(school)
-      click_on 'Meter attributes'
-      click_on 'School-wide attributes'
-      select 'Meter > Energy Use', from: 'type'
-      click_on 'New attribute'
-
-      check 'gas'
-      select 'hotwater_only', from: 'attribute_root'
-
-      click_on 'Create'
-
-      expect(school.meter_attributes.size).to eq(1)
-      attribute = school.meter_attributes.first
-      expect { attribute.to_analytics }.not_to raise_error
-      expect(attribute.to_analytics.to_s).to include('hotwater_only')
-
-
-      click_on 'Edit'
-
-      select 'kitchen_only', from: 'attribute_root'
-
-      click_on 'Update'
-
-      school.reload
-      new_attribute = school.meter_attributes.active.first
-      expect(new_attribute.to_analytics.to_s).to include('kitchen_only')
-      attribute.reload
-      expect(attribute.replaced_by).to eq(new_attribute)
-
-      click_on 'Delete'
-      expect(school.meter_attributes.active.size).to eq(0)
-      new_attribute.reload
-      expect(new_attribute.deleted_by).to eq(admin)
-    end
-
-    it 'allow the admin to manage school group meter attributes' do
-      visit root_path
-      click_on 'Manage'
-      click_on 'Admin'
-      click_on 'School Groups'
-      within 'table' do
-        click_on 'Manage'
+    describe 'managing school meter attributes' do
+      before do
+        visit school_path(school)
+        click_on 'Meter attributes'
+        click_on 'School-wide attributes'
       end
-      click_on 'Meter attributes'
 
-      select 'Meter > Energy Use', from: 'type'
-      click_on 'New attribute'
+      context 'when creating an attribute' do
+        let(:attribute) { school.meter_attributes.first }
 
-      check 'gas'
-      select 'hotwater_only', from: 'attribute_root'
+        before do
+          select 'Meter > Energy Use', from: 'type'
+          click_on 'New attribute'
+          check 'gas'
+          select 'hotwater_only', from: 'attribute_root'
+          click_on 'Create'
+        end
 
-      click_on 'Create'
+        it 'allows the creation of an attribute' do
+          expect(school.meter_attributes.size).to eq(1)
+          expect { attribute.to_analytics }.not_to raise_error
+          expect(attribute.to_analytics.to_s).to include('hotwater_only')
+        end
+      end
 
-      expect(school_group.meter_attributes.size).to eq(1)
-      attribute = school_group.meter_attributes.active.first
-      expect(attribute.selected_meter_types).to eq([:gas])
+      context 'when editing an attribute' do
+        let!(:school_meter_attribute) do
+          create(:school_meter_attribute, attribute_type: 'function_switch',
+                                          input_data: 'heating_only', school:)
+        end
 
-      click_on 'Edit'
+        let(:new_attribute) { school.meter_attributes.active.first }
 
-      check 'electricity'
-      uncheck 'gas'
+        before do
+          refresh
+          within '#database-group-meter-attributes-content' do
+            click_on 'Edit'
+          end
+          select 'kitchen_only', from: 'attribute_root'
+          click_on 'Update'
+          school.reload
+        end
 
-      click_on 'Update'
+        it 'allows editing an attribute' do
+          expect(new_attribute.to_analytics.to_s).to include('kitchen_only')
+          school_meter_attribute.reload
+          expect(school_meter_attribute.replaced_by).to eq(new_attribute)
+        end
+      end
 
-      school_group.reload
-      new_attribute = school_group.meter_attributes.active.first
-      expect(new_attribute.selected_meter_types).to eq([:electricity])
-      attribute.reload
-      expect(attribute.replaced_by).to eq(new_attribute)
+      context 'when deleting an attribute' do
+        let!(:school_meter_attribute) do
+          create(:school_meter_attribute, attribute_type: 'function_switch',
+                                          input_data: 'heating_only', school:)
+        end
 
-      click_on 'Delete'
-      expect(school_group.meter_attributes.active.size).to eq(0)
-      new_attribute.reload
-      expect(new_attribute.deleted_by).to eq(admin)
+        before do
+          refresh
+          within '#database-group-meter-attributes-content' do
+            click_on 'Delete'
+          end
+        end
+
+        it 'allows the deletion of an attribute' do
+          expect(school.meter_attributes.active.size).to eq(0)
+          school_meter_attribute.reload
+          expect(school_meter_attribute.deleted_by).to eq(admin)
+        end
+      end
+
+      context 'when restoring an attribute' do
+        let!(:school_meter_attribute) do
+          create(:school_meter_attribute, attribute_type: 'function_switch',
+                                          input_data: 'heating_only', school:)
+        end
+
+        before do
+          refresh
+          within '#database-group-meter-attributes-content' do
+            click_on 'Delete'
+          end
+          within '#deleted-group-meter-attributes-content' do
+            click_on 'Restore'
+          end
+        end
+
+        it 'allows restoration of an attribute' do
+          expect(school.meter_attributes.active.size).to eq(1)
+          school_meter_attribute.reload
+          expect(school_meter_attribute.deleted_by).to be_nil
+        end
+      end
     end
 
-    it 'allow the admin to manage global meter attributes' do
-      visit root_path
-      click_on 'Manage'
-      click_on 'Admin'
-      click_on 'Global Meter Attributes'
-      select 'Meter > Energy Use', from: 'type'
-      click_on 'New attribute'
+    describe 'managing school group meter attributes' do
+      let(:attribute) { school_group.meter_attributes.active.first }
 
-      check 'gas'
-      select 'hotwater_only', from: 'attribute_root'
+      before do
+        visit root_path
+        click_on 'Manage'
+        click_on 'Admin Home'
+        click_on 'School Groups'
+        within 'table' do
+          click_on 'Manage'
+        end
+        click_on 'Meter attributes'
+      end
 
-      click_on 'Create'
+      context 'when creating an attribute' do
+        before do
+          select 'Meter > Energy Use', from: 'type'
+          click_on 'New attribute'
+          check 'gas'
+          select 'hotwater_only', from: 'attribute_root'
+          click_on 'Create'
+        end
 
-      expect(GlobalMeterAttribute.count).to eq(1)
-      attribute = GlobalMeterAttribute.first
-      expect { attribute.to_analytics }.not_to raise_error
+        it 'allows the creation of an attribute' do
+          expect(school_group.meter_attributes.size).to eq(1)
+          expect(attribute.selected_meter_types).to eq([:gas])
+        end
+      end
 
-      click_on 'Edit'
+      context 'when editing an attribute' do
+        let!(:school_group_meter_attribute) do
+          create(:school_group_meter_attribute, attribute_type: 'function_switch',
+                                                input_data: 'hotwater_only', school_group:)
+        end
+        let(:new_attribute) { school_group.meter_attributes.active.first }
 
-      check 'electricity'
-      uncheck 'gas'
+        before do
+          refresh
+          click_on 'Edit'
+          check 'electricity'
+          uncheck 'gas'
+          click_on 'Update'
+          school_group.reload
+        end
 
-      click_on 'Update'
+        it 'allows editing of an attibute' do
+          expect(new_attribute.selected_meter_types).to eq([:electricity])
+          school_group_meter_attribute.reload
+          expect(school_group_meter_attribute.replaced_by).to eq(new_attribute)
+        end
+      end
 
-      new_attribute = GlobalMeterAttribute.active.first
-      expect(new_attribute.selected_meter_types).to eq([:electricity])
-      attribute.reload
-      expect(attribute.replaced_by).to eq(new_attribute)
+      context 'when deleting an attribute' do
+        let!(:school_group_meter_attribute) do
+          create(:school_group_meter_attribute, attribute_type: 'function_switch',
+                                                input_data: 'hotwater_only', school_group:)
+        end
 
-      click_on 'Delete'
-      expect(GlobalMeterAttribute.active.count).to eq(0)
-      new_attribute.reload
-      expect(new_attribute.deleted_by).to eq(admin)
+        before do
+          refresh
+          click_on 'Delete'
+        end
+
+        it 'allows deletion of an attribute' do
+          expect(school_group.meter_attributes.active.size).to eq(0)
+          school_group_meter_attribute.reload
+          expect(school_group_meter_attribute.deleted_by).to eq(admin)
+        end
+      end
+
+      context 'when restoring an attribute' do
+        let!(:school_group_meter_attribute) do
+          create(:school_group_meter_attribute, attribute_type: 'function_switch',
+                                                input_data: 'hotwater_only', school_group:)
+        end
+
+        before do
+          refresh
+          click_on 'Delete'
+          click_on 'Restore'
+        end
+
+        it 'allows restoration of an attribute' do
+          expect(school_group.meter_attributes.active.size).to eq(1)
+          school_group_meter_attribute.reload
+          expect(school_group_meter_attribute.deleted_by).to be_nil
+        end
+      end
+    end
+
+    describe 'managing global meter attributes' do
+      before do
+        visit root_path
+        click_on 'Manage'
+        click_on 'Admin Home'
+        click_on 'Global Meter Attributes'
+      end
+
+      context 'when creating an attribute' do
+        let(:attribute) { GlobalMeterAttribute.first }
+
+        before do
+          select 'Meter > Energy Use', from: 'type'
+          click_on 'New attribute'
+          check 'gas'
+          select 'hotwater_only', from: 'attribute_root'
+          click_on 'Create'
+        end
+
+        it 'allows creation of an attribute' do
+          expect(GlobalMeterAttribute.count).to eq(1)
+          expect { attribute.to_analytics }.not_to raise_error
+        end
+      end
+
+      context 'when editing an attribute' do
+        let!(:global_meter_attribute) do
+          create(:global_meter_attribute, attribute_type: 'function_switch',
+                                          input_data: 'hotwater_only')
+        end
+        let(:new_attribute) { GlobalMeterAttribute.active.first }
+
+        before do
+          refresh
+          click_on 'Edit'
+          check 'electricity'
+          uncheck 'gas'
+          click_on 'Update'
+        end
+
+        it 'allows editing of an attribute' do
+          expect(new_attribute.selected_meter_types).to eq([:electricity])
+          global_meter_attribute.reload
+          expect(global_meter_attribute.replaced_by).to eq(new_attribute)
+        end
+      end
+
+      context 'when deleting an attribute' do
+        let!(:global_meter_attribute) do
+          create(:global_meter_attribute, attribute_type: 'function_switch',
+                                          input_data: 'hotwater_only')
+        end
+
+        before do
+          refresh
+          click_on 'Delete'
+        end
+
+        it 'allows deletion of an attribute' do
+          expect(GlobalMeterAttribute.active.count).to eq(0)
+          global_meter_attribute.reload
+          expect(global_meter_attribute.deleted_by).to eq(admin)
+        end
+      end
+
+      context 'when restoring an attribute' do
+        let!(:global_meter_attribute) do
+          create(:global_meter_attribute, attribute_type: 'function_switch',
+                                          input_data: 'hotwater_only')
+        end
+
+        before do
+          refresh
+          click_on 'Delete'
+          click_on 'Restore'
+        end
+
+        it 'allows restoration of an attribute' do
+          expect(GlobalMeterAttribute.active.count).to eq(1)
+          global_meter_attribute.reload
+          expect(global_meter_attribute.deleted_by).to be_nil
+        end
+      end
     end
   end
 end
