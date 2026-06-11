f12ae1756479c4fe31f3be8f6fad68ba11972cfd
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-06-11 15:15:44 +0100

    Migrations

---
+++ db/migrate/20260610104911_create_suppliers.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateSuppliers < ActiveRecord::Migration[8.1]
+  def change
+    create_table :suppliers do |t|
+      t.string :name, index: { unique: true }
+      t.integer :owned_by_id
+      t.timestamps
+    end
+  end
+end
