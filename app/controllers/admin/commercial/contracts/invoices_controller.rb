9e2a4ccdddb9b2657a03c6345a688c46535c7a0a
Leigh Dodds <leigh@ldodds.com>
2026-06-12 15:40:27 +0100

    Initial page

---
+++ app/controllers/admin/commercial/contracts/invoices_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Admin
+  module Commercial
+    module Contracts
+      class InvoicesController < AdminController
+        load_and_authorize_resource :contract, class: 'Commercial::Contract'
+        def raise_invoice
+          @licences = @contract.licences.pending_invoice.by_start_date
+          @prices = ::Commercial::ContractPriceCalculator.new(@contract).per_school
+        end
+      end
+    end
+  end
+end
