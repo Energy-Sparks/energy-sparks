cd237e14bc64dc2e381cab85c4ec0bab5f0397cc
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-06-17 09:31:03 +0100

    Rake task and service

---
+++ app/services/meters/data_source_and_suppliers_manager.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module Meters
+  class DataSourceAndSuppliersManager
+    def import_from_csv(csv_path) # rubocop:disable Metrics/AbcSize
+      meter_data = CSV.parse(File.read(csv_path), headers: true)
+      meter_data.each do |row|
+        meter = Meter.where(mpan_mprn: row['Meter'])
+        if meter.present?
+          data_source = DataSource.where(name: row['Updated Data Source']).first
+          if data_source.blank?
+            warn "Can't find data source with name #{row['Updated Data Source']}"
+            next
+          end
+          # supplier = Supplier.where(name: row['Supplier']).first
+          # if supplier.blank?
+          #   warn "Can't find supplier with name #{row['Supplier']}"
+          #   next
+          # end
+          meter.update!(data_source:)
+          # meter.update!(data_source:, supplier:)
+        else
+          warn "Can't find meter with mpan_mprn #{row['Meter']}"
+        end
+      rescue => e # rubocop:disable Style/RescueStandardError
+        warn e.message
+      end
+    end
+  end
+end
