20ccf994e7e3d97a72512556315d291f801b4e36
Leigh Dodds <leigh@ldodds.com>
2026-06-17 15:06:06 +0100

    Create invoice

--- app/controllers/admin/commercial/contracts/invoices_controller.rb
+++ app/controllers/admin/commercial/contracts/invoices_controller.rb
@@ -11,8 +11,57 @@ module Admin
         end
 
         def new
-          # TODO: licences, nested changes
+          @prices = ::Commercial::ContractPriceCalculator.new(@contract).per_school
           @invoice = @contract.invoices.new(purchase_order_number: @contract.purchase_order_number)
+          @contract.licences.where(id: licence_params).find_each do |licence|
+            build_line_item(@invoice, licence, @prices[licence.school.id])
+          end
+        end
+
+        def create
+          @invoice = nil
+
+          ::Commercial::Invoice.transaction do
+            @invoice = ::Commercial::Invoice.create!(invoice_attributes.merge(created_by: current_user))
+
+            @invoice.licences.each do |licence|
+              licence.update!(status: :invoiced, updated_by: current_user)
+            end
+          end
+
+          redirect_to admin_commercial_invoice_path(@invoice)
+        rescue ActiveRecord::RecordInvalid
+          redirect_to admin_commercial_contract_path(@contract), notice: 'Failed to create invoice'
+        end
+
+        private
+
+        def build_line_item(invoice, licence, price)
+          invoice.line_items.build(
+            licence: licence,
+            base_price: price.base_price,
+            metering_fee: price.metering_fee,
+            private_account_fee: price.private_account_fee,
+            private_account: licence.school.data_sharing != 'public',
+            number_of_meters: licence.school.meters.main_meter.active.count
+          )
+        end
+
+        def licence_params
+          params.require(:licences)
+        end
+
+        def invoice_attributes
+          params.expect(
+            commercial_invoice: [:contract_id,
+                                 :purchase_order_number,
+                                 {
+                                   line_items_attributes: %i[
+                                     base_price licence_id metering_fee
+                                     number_of_meters private_account private_account_fee
+                                   ]
+                                 }]
+          )
         end
       end
     end
