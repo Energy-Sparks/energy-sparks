e8386680ff94d8ba93ab02d9dc6e13e5174a9dbf
Leigh Dodds <leigh@ldodds.com>
2026-06-22 14:51:11 +0100

    Reuse button

--- app/components/forms/email_report_button_component.rb
+++ app/components/forms/email_report_button_component.rb
@@ -2,10 +2,11 @@
 
 module Forms
   class EmailReportButtonComponent < ApplicationComponent
-    def initialize(path, label, **_kwargs)
+    def initialize(path, label, button_class: 'btn btn-sm', **_kwargs)
       super
       @path = path
       @label = label
+      @button_class = button_class
     end
   end
 end
