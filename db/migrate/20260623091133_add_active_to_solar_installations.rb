b6af18b9ed7801a04ed478963dd9931d028b1785
Tristan H <878258+tbhi@users.noreply.github.com>
2026-06-23 09:35:21 +0000

    Create 20260623091133_add_active_to_solar_installations.rb

---
+++ db/migrate/20260623091133_add_active_to_solar_installations.rb
@@ -0,0 +1,8 @@
+class AddActiveToSolarInstallations < ActiveRecord::Migration[8.1]
+  def change
+    add_column :solar_edge_installations, :active, :boolean, default: false, null: false
+    add_column :low_carbon_hub_installations, :active, :boolean, default: false, null: false
+    add_column :rtone_variant_installations, :active, :boolean, default: false, null: false
+    add_column :solis_cloud_installations, :active, :boolean, default: false, null: false
+  end
+end
