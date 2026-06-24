b564c33fed3c8a160dbb5ddbd3a221269b86d601
Tristan H <878258+tbhi@users.noreply.github.com>
2026-06-24 05:07:49 +0000

    Update 20260623091133_add_active_to_solar_installations.rb

--- db/migrate/20260623091133_add_active_to_solar_installations.rb
+++ db/migrate/20260623091133_add_active_to_solar_installations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddActiveToSolarInstallations < ActiveRecord::Migration[8.1]
   def change
     add_column :solar_edge_installations, :active, :boolean, default: false, null: false
