186bc19200336f9fa1bd69b627e4529ee5e19fed
Tristan H <878258+tbhi@users.noreply.github.com>
2026-06-29 20:15:49 +0000

    Update solar_pv_panels.rb

--- app/services/aggregation/solar_pv_panels.rb
+++ app/services/aggregation/solar_pv_panels.rb
@@ -43,7 +43,7 @@ module Aggregation
       print_detailed_results(pv_meter_map, 'Before solar pv calculation:')
 
       # Create the solar PV generation data (or override existing data if needed)
-      create_generation_data(pv_meter_map, false)
+      create_generation_data(pv_meter_map)
 
       # Create an export meter, with empty data, unless there is one already
       create_export_meter_if_missing(pv_meter_map)
@@ -64,7 +64,9 @@ module Aggregation
     end
 
     def create_generation_meter(mains_consume)
-      create_generation_data(SolarMeterMap.new(mains_consume), false)
+      generation = create_generation_meter_from_mains(mains_consume)
+      create_generation_amr_data(mains_consume.amr_data, generation.amr_data, mains_consume.mpan_mprn)
+      generation
     end
 
     private
@@ -91,20 +93,13 @@ module Aggregation
     # meter attributes are configured to do so
     #
     # @param [SolarMeterMap] pv_meter_map the solar meters
-    # @param [boolean] create_zero_if_no_config THIS IS ALWAYS FALSE
-    def create_generation_data(pv_meter_map, create_zero_if_no_config)
+    def create_generation_data(pv_meter_map)
       if pv_meter_map.generation.nil?
-        pv_meter_map.set_meter(:generation, create_generation_meter_from_map(pv_meter_map))
+        pv_meter_map.set_meter(:generation, create_generation_meter_from_mains(mains_consume))
       end
-
-      create_generation_amr_data(
-        pv_meter_map.mains_consume.amr_data,
-        pv_meter_map.generation.amr_data,
-        pv_meter_map.mains_consume.mpan_mprn,
-        create_zero_if_no_config
-      )
-
-      pv_meter_map.generation
+      create_generation_amr_data(pv_meter_map.mains_consume.amr_data,
+                                 pv_meter_map.generation.amr_data,
+                                 pv_meter_map.mains_consume.mpan_mprn)
     end
 
     def create_export_meter_if_missing(pv_meter_map)
@@ -143,24 +138,15 @@ module Aggregation
     # @param [AmrData] mains_amr_data data for the mains consumption meter
     # @param [AmrData] pv_amr_data data for the pv generation meter
     # @param [String] mpan mpan/mprn for the mains consumption meter
-    # @param [boolean] create_zero_if_no_config THIS IS ALWAYS FALSE?
-    def create_generation_amr_data(mains_amr_data, pv_amr_data, mpan, create_zero_if_no_config)
+    def create_generation_amr_data(mains_amr_data, pv_amr_data, mpan)
       mains_amr_data.date_range.each do |date|
         # set only where config says so, either because we are overridding actual metered solar data
         # or we are just producing synthetic solar data
-        if synthetic_data?(date, :override_generation)
-          pv = days_pv(date, mpan)
-          pv_amr_data.add(date, pv)
-          compact_print_day('override generation', date, pv.kwh_data_x48)
-        elsif create_zero_if_no_config
-          # TODO: this never seems to be used, as param is always FALSE?
-          #
-          # pad out generation data to that of mains electric meter
-          # so downstream analysis doesn't need to continually test
-          # for its existence
-          pv_amr_data.add(date, OneDayAMRReading.zero_reading(mpan, date, 'SOL0'))
-          compact_print_day('override generation zero', date, negative_only_exported_kwh_x48)
-        end
+        next unless synthetic_data?(date, :override_generation)
+
+        pv = days_pv(date, mpan)
+        pv_amr_data.add(date, pv)
+        compact_print_day('override generation', date, pv.kwh_data_x48)
       end
     end
 
@@ -327,17 +313,14 @@ module Aggregation
       end
     end
 
-    # Create a solar generation meter, based on the mains_consume meter in the map
-    def create_generation_meter_from_map(pv_meter_map)
-      _date_range, meter_to_clone, meter_collection = meter_creation_data(pv_meter_map)
-      create_meter(
-        meter_to_clone,
-        :solar_pv,
-        :solar_pv,
-        meter_collection,
-        SolarPVPanels::SOLAR_PV_PRODUCTION_METER_NAME,
-        'SOLR'
-      )
+    # Create a solar generation meter, based on the mains_consume meter
+    def create_generation_meter_from_mains(mains_consume)
+      create_meter(mains_consume,
+                   :solar_pv,
+                   :solar_pv,
+                   mains_consume.meter_collection,
+                   SolarPVPanels::SOLAR_PV_PRODUCTION_METER_NAME,
+                   'SOLR')
     end
 
     # Create a solar export meter, based on the mains_consume meter in the map
@@ -347,14 +330,12 @@ module Aggregation
     end
 
     def create_export_meter(_date_range, meter_to_clone, meter_collection)
-      create_meter(
-        meter_to_clone,
-        :exported_solar_pv,
-        :solar_pv_exported_sub_meter,
-        meter_collection,
-        SolarPVPanels::SOLAR_PV_EXPORTED_ELECTRIC_METER_NAME,
-        'SOLE'
-      )
+      create_meter(meter_to_clone,
+                   :exported_solar_pv,
+                   :solar_pv_exported_sub_meter,
+                   meter_collection,
+                   SolarPVPanels::SOLAR_PV_EXPORTED_ELECTRIC_METER_NAME,
+                   'SOLE')
     end
 
     # Create a solar self consumption meter, based on the mains_consume meter in the map
@@ -364,14 +345,12 @@ module Aggregation
     end
 
     def create_self_consumption_meter(_date_range, meter_to_clone, meter_collection)
-      create_meter(
-        meter_to_clone,
-        :solar_pv,
-        :solar_pv_consumed_sub_meter,
-        meter_collection,
-        SolarPVPanels::SOLAR_PV_ONSITE_ELECTRIC_CONSUMPTION_METER_NAME,
-        'SOLO'
-      )
+      create_meter(meter_to_clone,
+                   :solar_pv,
+                   :solar_pv_consumed_sub_meter,
+                   meter_collection,
+                   SolarPVPanels::SOLAR_PV_ONSITE_ELECTRIC_CONSUMPTION_METER_NAME,
+                   'SOLO')
     end
 
     # Creates a new meter, cloning some values from an original
