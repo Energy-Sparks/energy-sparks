4d5bac80311f520c71aca37cc8d66ebf88623b06
luke gouldson <workluke@Lukes-Laptop.localdomain>
2026-06-30 10:01:18 +0100

    allowed blanks

--- app/services/meters/supplier_data_source_status_importer.rb
+++ app/services/meters/supplier_data_source_status_importer.rb
@@ -4,29 +4,22 @@ require 'csv'
 
 module Meters
   class SupplierDataSourceStatusImporter
-    def import(data) # rubocop:disable Metrics/AbcSize
+    def import(data) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
       meter = Meter.find_by(mpan_mprn: data[:meter])
-      if meter.present?
-        data_source = DataSource.find_by(name: data[:data_source])
-        if data_source.blank?
-          warn "Can't find data source with name #{data[:data_source]}"
-          return
-        end
-        supplier = Supplier.find_by(name: data[:supplier])
-        if supplier.blank?
-          warn "Can't find supplier with name #{data[:supplier]}"
-          return
-        end
-        status = AdminMeterStatus.find_by(label: data[:status])
-        if status.blank?
-          return unless meter.admin_meter_status.label == 'Manual Request'
+      return if meter.blank?
 
-          warn 'This meter has status Manual Request: replacing with blank'
-        end
-        meter.update!(data_source:, supplier:, admin_meter_status: status)
-      else
-        warn "Can't find meter with mpan_mprn #{data[:meter]}"
+      data_source = DataSource.find_by(name: data[:data_source])
+      meter.update!(data_source:) if data_source.present?
+
+      supplier = Supplier.find_by(name: data[:supplier])
+      meter.update!(supplier:) if supplier.present?
+
+      status = AdminMeterStatus.find_by(label: data[:status])
+      if status.present? || (status.blank? && meter.admin_meter_status&.label == 'Manual Request')
+        meter.update!(admin_meter_status: status)
       end
+
+      meter
     end
   end
 end
