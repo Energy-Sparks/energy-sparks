d86b84f462c43cbb09bd1f525456d7f6e331419a
Tristan H <878258+tbhi@users.noreply.github.com>
2026-07-06 09:00:04 +0000

    lint fixes

--- app/models/report/metered_solar.rb
+++ app/models/report/metered_solar.rb
@@ -8,7 +8,7 @@ module Report
         AND #{relation}.deleted_by_id IS NULL"
     end
 
-    private_class_method  def self.first_attribute_subquery(type)
+    private_class_method def self.first_attribute_subquery(type)
       "SELECT id FROM meter_attributes
         WHERE meter_id = meters.id
         #{active_attribute(type)}"
@@ -16,18 +16,18 @@ module Report
 
     def self.query
       Meter.joins(:school)
-            .joins("JOIN meter_attributes solar_pv_mapping_attributes
+           .joins("JOIN meter_attributes solar_pv_mapping_attributes
                     ON solar_pv_mapping_attributes.meter_id = meters.id
                     #{active_attribute(:solar_pv_mpan_meter_mapping, :solar_pv_mapping_attributes)}")
-            .where(schools: { active: true })
-            .select('meters.*',
-                    'solar_pv_mapping_attributes.input_data AS solar_pv_mapping_data',
-                    "EXISTS (#{first_attribute_subquery(:solar_pv_override)}) AS has_solar_pv_override_attribute",
-                    "EXISTS (#{first_attribute_subquery(:solar_pv)}) AS has_solar_pv_attribute",
-                    "EXISTS (#{first_attribute_subquery(:modelled_solar_pv_generation)}) " \
-                    'AS has_modelled_solar_pv_generation_attribute')
-            .includes(:data_source, :supplier, :admin_meter_status,
-                      school: { school_group: %i[admin_meter_status_electricity default_issues_admin_user] })
+           .where(schools: { active: true })
+           .select('meters.*',
+                   'solar_pv_mapping_attributes.input_data AS solar_pv_mapping_data',
+                   "EXISTS (#{first_attribute_subquery(:solar_pv_override)}) AS has_solar_pv_override_attribute",
+                   "EXISTS (#{first_attribute_subquery(:solar_pv)}) AS has_solar_pv_attribute",
+                   "EXISTS (#{first_attribute_subquery(:modelled_solar_pv_generation)}) " \
+                   'AS has_modelled_solar_pv_generation_attribute')
+           .includes(:data_source, :supplier, :admin_meter_status,
+                     school: { school_group: %i[admin_meter_status_electricity default_issues_admin_user] })
     end
   end
 end
