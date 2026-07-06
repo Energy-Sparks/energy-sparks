c18b7b86cd369f4ff7c32a9cb8ae11b2c3424901
Deb Bassett <deb@urbanwide.com>
2026-07-06 15:44:48 +0100

    Being slowly driven mad

--- spec/system/activities_spec.rb
+++ spec/system/activities_spec.rb
@@ -64,7 +64,7 @@ describe 'viewing and recording activities' do
 
       it 'displays prompt to login' do
         expect(page).to have_text('Are you an Energy Sparks user?')
-        expect(page).to have_link('Sign in to record activity')
+        expect(page).to have_link('Record this activity')
       end
     end
 
@@ -72,7 +72,7 @@ describe 'viewing and recording activities' do
       let!(:staff) { create(:staff, school:) }
 
       it 'redirects back to activity after login' do
-        click_on 'Sign in to record activity'
+        click_on 'Record this activity'
         fill_in 'Email', with: staff.email
         fill_in 'Password', with: staff.password
         within '#staff' do
