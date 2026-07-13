dd60061239d52ff38ae04ae7e461c2c11d6a3a26
LukeGouldson <156131448+LukeGouldson@users.noreply.github.com>
2026-07-13 15:25:01 +0100

    Tidy admin manage menus (#5485)
    Ensures the options in the Group Settings admin section and Manage Group menu are the same, adding meter attributes and meter updates
    Adds breadcrumbs and the Manage Group menu to admin school group pages, and the Manage Group menu to admin dashboard pages

--- app/controllers/admin/commercial/contract_holder_contracts_controller.rb
+++ app/controllers/admin/commercial/contract_holder_contracts_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   module Commercial
     class ContractHolderContractsController < AdminController
+      include SchoolGroupBreadcrumbs
+
       load_and_authorize_resource :funder, instance_name: 'contract_holder'
       load_and_authorize_resource :school_group, instance_name: 'contract_holder'
       load_and_authorize_resource :school, instance_name: 'contract_holder'
@@ -9,10 +11,15 @@ module Admin
         @contracts = @contract_holder.contracts.by_start_date
         if @contract_holder.is_a?(SchoolGroup)
           @school_group = @contract_holder
+          breadcrumbs
           render :index, layout: 'group_settings'
         else
           render :index
         end
+      end
+
+      def breadcrumbs
+        build_breadcrumbs([{ name: t('school_groups.titles.contracts') }])
       end
     end
   end
