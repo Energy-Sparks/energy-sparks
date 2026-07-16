90caa1a9407fc7180e62e8cfd7eeff0d993069cb
Leigh Dodds <leigh@ldodds.com>
2026-07-16 14:17:04 +0100

    Change migration

--- db/migrate/20260716124419_remove_default_contract_holder_field.rb
+++ db/migrate/20260716124419_remove_default_contract_holder_field.rb
@@ -2,7 +2,20 @@
 
 class RemoveDefaultContractHolderField < ActiveRecord::Migration[8.1]
   def change
-    remove_column :schools, :default_contract_holder_id, :bigint
-    remove_column :schools, :default_contract_holder_type, :string
+    reversible do |dir|
+      dir.up do
+        change_table :schools, bulk: true do |t|
+          t.remove :default_contract_holder_id
+          t.remove :default_contract_holder_type
+        end
+      end
+
+      dir.down do
+        change_table :schools, bulk: true do |t|
+          t.column :default_contract_holder_id, :bigint
+          t.column :default_contract_holder_type, :string
+        end
+      end
+    end
   end
 end
