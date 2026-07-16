0ca276d09c95ecf33920e283e5b4821e95577baa
Leigh Dodds <leigh@ldodds.com>
2026-07-16 11:43:13 +0100

    Fix funder model

--- app/models/funder.rb
+++ app/models/funder.rb
@@ -15,21 +15,7 @@ class Funder < ApplicationRecord
 
   watch_mailchimp_fields :name
 
-  has_many :schools
-  has_many :school_onboardings
-
-  scope :with_schools,  -> { where.associated(:schools) }
-  scope :by_name,       -> { order(name: :asc) }
+  scope :by_name, -> { order(name: :asc) }
 
   validates :name, presence: true, uniqueness: true
-
-  # Return counts of visible schools by funder
-  # includes funders without any funded schools, but not schools without any source of funding. See Schools.unfunded.
-  def self.funded_school_counts(visible: true, data_enabled: true)
-    Funder.joins('LEFT JOIN schools ON funders.id = schools.funder_id ' \
-                 "AND #{ActiveRecord::Base.sanitize_sql_for_conditions(
-                   ['schools.visible = ? AND schools.data_enabled = ?', visible, data_enabled]
-                 )}")
-          .group(:name).count('schools.id')
-  end
 end
