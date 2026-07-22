31b86da61a152a74ab9c7345f3e3cf96f714084d
Leigh Dodds <leigh@ldodds.com>
2026-07-22 10:59:30 +0100

    Implement new "our impact" page (#5525)

---
+++ spec/factories/impact_reports/organisation_statements.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :impact_report_organisation_statement, class: 'ImpactReport::OrganisationStatement' do
+    sequence(:academic_year) { |n| "Academic Year #{n}" }
+    sequence(:efficiency_report_link) { |n| "https://example.org/#{n}" }
+    current { false }
+
+    schools { rand(100..1000) }
+    pupils { rand(20_000..100_000) }
+    staff { rand(1..1000) }
+    activities { rand(1000..10_000) }
+    actions { rand(1000..10_000) }
+    total_cost_savings { rand(1_000_000..10_000_000) }
+    total_carbon_savings { rand(1..1000) }
+
+    primary_saving_electricity { rand(1..100) }
+    primary_saving_gas { rand(1..100) }
+    primary_cost_saving { rand(100..10_000) }
+    primary_carbon_saving { rand(100..10_000) }
+
+    secondary_saving_electricity { rand(1..100) }
+    secondary_saving_gas { rand(1..100) }
+    secondary_cost_saving { rand(100..10_000) }
+    secondary_carbon_saving { rand(100..10_000) }
+
+    first_testimonial factory: [:testimonial]
+    second_testimonial factory: [:testimonial]
+
+    trait :current do
+      current { true }
+    end
+  end
+end
