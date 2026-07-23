1ae35f87848b4b3a2d980451bc5018c840c8e59c
Tristan H <878258+tbhi@users.noreply.github.com>
2026-07-23 14:27:25 +0000

    Update meter_z_download_and_upsert_spec.rb

--- spec/services/solar/meter_z_download_and_upsert_spec.rb
+++ spec/services/solar/meter_z_download_and_upsert_spec.rb
@@ -6,7 +6,7 @@ describe Solar::MeterZDownloadAndUpsert do
   subject(:download_and_upsert) { described_class.new(installation:) }
 
   let(:installation) { create(:meter_z_installation) }
-  let!(:meter) { create(:solar_pv_meter, meter_z_installation: installation, meter_serial_number: 'meter_id') }
+  let!(:meter) { create(:solar_pv_meter, meter_z_installation: installation, meter_serial_number: '123') }
 
   def every_half_hour(count)
     time = DateTime.new(2025)
@@ -21,7 +21,7 @@ describe Solar::MeterZDownloadAndUpsert do
       readings = every_half_hour(48 * 5).map.with_index do |time, i|
         { 'reading_timestamp' => time.iso8601, 'readings' => { 'accumulated_kilowatt_hours' => i.to_s } }
       end
-      stub_request(:get, 'https://api.meterz.co.uk/v1/organisations/organisation_id/sites/site_id/meters/meter_id' \
+      stub_request(:get, 'https://api.meterz.co.uk/v1/organisations/organisation_id/sites/site_id/meters/123' \
                          '/readings?start_datetime=2026-01-02&items_per_page=1000')
         .to_return(body: { readings: readings.reverse }.to_json, headers: { 'content-type' => 'application/json' })
     end
