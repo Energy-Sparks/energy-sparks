216776fce5e6452b7fcd9da5424065fdbfc82041
Leigh Dodds <leigh@ldodds.com>
2026-07-23 15:03:44 +0100

    Add specs for potential benefits

--- spec/system/school_groups/advice/solar_pv_spec.rb
+++ spec/system/school_groups/advice/solar_pv_spec.rb
@@ -5,7 +5,10 @@ require 'rails_helper'
 describe 'School group solar pv page' do
   let!(:school_group) { create(:school_group, :with_active_schools, public: true) }
   let!(:school) { create(:school, :with_fuel_configuration, school_group: school_group) }
-  let!(:report) { create(:report, key: :solar_generation_summary) }
+  let!(:no_solar_school) { create(:school, :with_fuel_configuration, school_group: school_group) }
+
+  let!(:current_benefit_report) { create(:report, key: :solar_generation_summary) }
+  let!(:potential_benefit_report) { create(:report, key: :solar_pv_benefit_estimate) }
 
   let(:solar_generation_variables) do
     {
@@ -17,6 +20,25 @@ describe 'School group solar pv page' do
     }
   end
 
+  let(:solar_benefit_variables) do
+    {
+      optimum_kwp: 44.2,
+      optimum_payback_years: 2.5,
+      optimum_mains_reduction_percent: 0.15,
+      one_year_saving_gbpcurrent: 1000
+    }
+  end
+
+  let(:additional_data_variables) do
+    {
+      electricity_economic_tariff_changed_this_year: true
+    }
+  end
+
+  include_context 'with comparison report footnotes' do
+    let(:footnotes) { [tariff_changed_last_year] }
+  end
+
   before do
     Flipper.enable(:group_solar_advice_page)
     create(:advice_page, key: :solar_pv)
@@ -24,10 +46,19 @@ describe 'School group solar pv page' do
     alert_run = create(:alert_generation_run, school: school)
 
     solar_generation_alert = create(:alert_type, class_name: 'AlertSolarGeneration')
-    create(:alert, school: school, alert_generation_run: alert_run, alert_type: solar_generation_alert,
-                   variables: solar_generation_variables)
+    create(:alert, school: school, alert_generation_run: alert_run,
+                   alert_type: solar_generation_alert, variables: solar_generation_variables)
+
+    solar_benefit_alert = create(:alert_type, class_name: 'AlertSolarPVBenefitEstimator')
+    create(:alert, school: no_solar_school, alert_generation_run: alert_run,
+                   alert_type: solar_benefit_alert, variables: solar_benefit_variables)
+
+    additional_data_alert = create(:alert_type, class_name: 'AlertAdditionalPrioritisationData')
+    create(:alert, school: no_solar_school, alert_generation_run: alert_run, alert_type: additional_data_alert,
+                   variables: additional_data_variables)
 
     Comparison::SolarGenerationSummary.refresh
+    Comparison::SolarPvBenefitEstimate.refresh
   end
 
   context 'when on the insights page' do
@@ -48,12 +79,21 @@ describe 'School group solar pv page' do
         let(:title) { I18n.t('school_groups.advice_pages.solar_pv.page_title') }
       end
 
-      it 'has the key insights section' do
+      it 'has the current benefits section' do
         expect(page).to have_text(I18n.t('school_groups.advice_pages.solar_pv.insights.current_benefits.title'))
         expect(page).to have_css('#solar_generation_summary-table')
         within('#solar_generation_summary-table') do
           expect(page).to have_text(school.name)
           expect(page).to have_text('1,000')
+        end
+      end
+
+      it 'has the potential benefits section' do
+        expect(page).to have_text(I18n.t('school_groups.advice_pages.solar_pv.insights.potential_benefits.title'))
+        expect(page).to have_css('#solar_pv_benefit_estimate-table')
+        within('#solar_pv_benefit_estimate-table') do
+          expect(page).to have_text(no_solar_school.name)
+          expect(page).to have_text('15&percnt;')
         end
       end
     end
@@ -74,13 +114,55 @@ describe 'School group solar pv page' do
         let(:title) { I18n.t('school_groups.advice_pages.solar_pv.page_title') }
       end
 
+      context 'with potential benefits section' do
+        it { expect(page).to have_text(I18n.t('school_groups.advice_pages.solar_pv.analysis.potential_savings.title')) }
+
+        it_behaves_like 'a school comparison report with a table', visit: false do
+          let(:expected_report) { potential_benefit_report }
+          let(:expected_school) { no_solar_school }
+          let(:advice_page_path) { analysis_school_advice_solar_pv_path(expected_school) }
+          let(:headers) do
+            [
+              I18n.t('analytics.benchmarking.configuration.column_headings.school'),
+              I18n.t('analytics.benchmarking.configuration.column_headings.size_kwp'),
+              I18n.t('analytics.benchmarking.configuration.column_headings.payback_years'),
+              I18n.t('analytics.benchmarking.configuration.column_headings.reduction_in_mains_consumption_pct'),
+              I18n.t('analytics.benchmarking.configuration.column_headings.saving_optimal_panels')
+            ]
+          end
+          let(:expected_table) do
+            [
+              headers,
+              ["#{no_solar_school.name} [5]",
+               '44.2',
+               '2 years 6 months',
+               '15&percnt;',
+               '£1,000'],
+              ["Notes\n" \
+               '[5] The tariff has changed during the last year for this school. Savings are calculated using the ' \
+               'latest tariff but other £ values are calculated using the relevant tariff at the time']
+            ]
+          end
+          let(:expected_csv) do
+            [
+              headers,
+              [no_solar_school.name,
+               '44.2',
+               '2.5',
+               '15',
+               '1,000']
+            ]
+          end
+        end
+      end
+
       context 'with comparison section' do
         it { expect(page).to have_text(I18n.t('school_groups.advice_pages.solar_pv.analysis.comparisons.title')) }
 
         it_behaves_like 'a school comparison report with a table', visit: false do
-          let(:expected_report) { report }
+          let(:expected_report) { current_benefit_report }
           let(:expected_school) { school }
-          let(:advice_page_path) { insights_school_advice_solar_pv_path(expected_school) }
+          let(:advice_page_path) { analysis_school_advice_solar_pv_path(expected_school) }
           let(:headers) do
             ['School', 'Mains consumption (kWh)', 'Generation (kWh)', 'Self consumption (kWh)', 'Export (kWh)',
              'Total onsite consumption (kWh)']
