a3a6df1520d09006c052c498fce7d97d73a32c8d
Leigh Dodds <leigh@ldodds.com>
2026-07-24 16:20:36 +0100

    Use synthetic data

--- spec/analytics/lib/dashboard/alerts/electricity/alert_solar_pv_benefit_estimator_spec.rb
+++ spec/analytics/lib/dashboard/alerts/electricity/alert_solar_pv_benefit_estimator_spec.rb
@@ -3,72 +3,53 @@
 require 'rails_helper'
 
 describe AlertSolarPVBenefitEstimator do
-  subject(:alert) { described_class.new(school) }
+  subject(:alert) { described_class.new(meter_collection) }
 
-  let(:school) { @acme_academy }
+  # using fixed generation and consumption to simplify calculating expected values
+  let(:solar_generation_x48) { [*([0.0] * 10), *([0.25] * 10), *([0.5] * 8), *([0.25] * 10), *([0.0] * 10)] }
+  let(:consumption_x48) { [*([0.1] * 10), *([0.005] * 10), *([0.4] * 8), *([0.25] * 10), *([0.01] * 10)] }
 
-  before(:all) do
-    @acme_academy = load_unvalidated_meter_collection(school: 'acme-academy')
+  include_context 'with an aggregated meter with tariffs and school times' do
+    let(:amr_start_date)  { Date.new(2024, 1, 1) }
+    let(:amr_end_date)    { Date.new(2025, 12, 31) }
+    let(:days_solar_pv_yield) { solar_generation_x48 }
+
+    let(:amr_data) do
+      build(:amr_data, :with_date_range, :with_grid_carbon_intensity,
+            grid_carbon_intensity: grid_carbon_intensity,
+            start_date: amr_start_date,
+            end_date: amr_end_date,
+            kwh_data_x48: consumption_x48)
+    end
   end
 
   context 'when school does not have solar' do
-    let(:pricing) { BenchmarkMetrics.default_prices }
-
     let(:expected_alert_variables) do
       {
-        optimum_kwp: '100 kWp',
-        optimum_payback_years: '8 years',
-        optimum_mains_reduction_percent: '16%',
-        one_year_saving_£current: '£11,000',
-        one_year_saving_kwh: '76,000 kWh',
-        one_year_saving_£: '£11,000',
-        one_year_saving_co2: '15,000 kg CO2',
-        ten_year_saving_co2: '150,000 kg CO2',
-        average_one_year_saving_£: '£11,000',
-        average_ten_year_saving_£: '£110,000',
-        ten_year_saving_£: '£110,000',
+        optimum_kwp: '210 kWp',
+        optimum_payback_years: '9 years',
+        optimum_mains_reduction_percent: '84%',
+        one_year_saving_£current: '£17,000',
+        one_year_saving_kwh: '2,100 kWh',
+        one_year_saving_£: '£17,000',
+        one_year_saving_co2: '69,000 kg CO2',
+        ten_year_saving_co2: '690,000 kg CO2',
+        average_one_year_saving_£: '£17,000',
+        average_ten_year_saving_£: '£170,000',
+        ten_year_saving_£: '£170,000',
         payback_years: '',
-        average_payback_years: '8 years',
-        capital_cost: '£84,000',
-        average_capital_cost: '£84,000'
+        average_payback_years: '9 years',
+        capital_cost: '£150,000',
+        average_capital_cost: '£150,000'
       }
     end
 
     before do
-      class_double('BenchmarkMetrics', pricing: pricing, default_prices: pricing).as_stubbed_const
-      alert.analyse(Date.new(2022, 7, 12))
+      alert.analyse(Date.new(2025, 12, 31))
     end
 
     it 'calculates the expected variables' do
       expect(alert.text_template_variables.slice(*expected_alert_variables.keys)).to eq(expected_alert_variables)
-    end
-
-    context 'when pricing is updated' do
-      let(:pricing) { OpenStruct.new(gas_price: 0.03, electricity_price: 0.15, solar_export_price: 0.1) }
-
-      let(:expected_alert_variables) do
-        {
-          optimum_kwp: '250 kWp',
-          optimum_payback_years: '7 years',
-          optimum_mains_reduction_percent: '28%',
-          one_year_saving_£current: '£26,000',
-          one_year_saving_kwh: '130,000 kWh',
-          one_year_saving_£: '£26,000',
-          one_year_saving_co2: '35,000 kg CO2',
-          ten_year_saving_co2: '350,000 kg CO2',
-          average_one_year_saving_£: '£26,000',
-          average_ten_year_saving_£: '£260,000',
-          ten_year_saving_£: '£260,000',
-          payback_years: '',
-          average_payback_years: '7 years',
-          capital_cost: '£170,000',
-          average_capital_cost: '£170,000'
-        }
-      end
-
-      it 'applies the export pricing' do
-        expect(alert.text_template_variables.slice(*expected_alert_variables.keys)).to eq(expected_alert_variables)
-      end
     end
   end
 end
