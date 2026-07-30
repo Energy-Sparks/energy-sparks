4b9fde1d4821baa5e2fbffb2cc8b47fb5f92f052
Leigh Dodds <leigh@ldodds.com>
2026-07-30 15:43:04 +0100

    Fix specs

--- app/models/commercial/xero_account_code.rb
+++ app/models/commercial/xero_account_code.rb
@@ -20,7 +20,8 @@ module Commercial
 
     self.table_name = 'commercial_xero_account_codes'
 
-    validates :code, presence: true, uniqueness: true
+    validates :code, presence: true
+    validates :code, uniqueness: { case_sensitive: false }
     validates :label, presence: true
 
     scope :by_code, -> { order(:code) }
