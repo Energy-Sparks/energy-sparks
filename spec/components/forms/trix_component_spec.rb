da0091feaa82e8dc023380b25905efd2e71acfea
Tristan H <878258+tbhi@users.noreply.github.com>
2026-08-04 11:35:12 +0100

    require description for actions & activities (#5560)

--- spec/components/forms/trix_component_spec.rb
+++ spec/components/forms/trix_component_spec.rb
@@ -8,99 +8,88 @@ RSpec.describe Forms::TrixComponent, :include_application_helper, type: :compone
   let(:id) { 'custom-id' }
   let(:classes) { 'extra-classes' }
   let(:kwargs) { { id: id, classes: classes } }
-
-  # rubocop:disable RSpec/VerifiedDoubles
-  # disabling as real method has extra default parameter, so can't easily stub
-  # just want to confirm that the rich_text_area helper is called
-  let(:form) { double(ActionView::Helpers::FormHelper) }
-  # rubocop:enable RSpec/VerifiedDoubles
-
+  let(:form) { SimpleForm::FormBuilder.new(:activity, Activity.new, vc_test_controller.view_context, {}) }
   let(:field) { :description }
+  let(:params) { { form:, field: } }
 
-  let(:params) do
-    { form: form, field: field }
-  end
+  def render = render_inline(component)
 
   context 'when rendering' do
-    let(:html) { render_inline(component) }
-
-    before do
-      allow(form).to receive(:rich_text_area)
-    end
-
     context 'with base params' do
+      before { render }
+
       it_behaves_like 'an application component' do
         let(:expected_classes) { classes }
         let(:expected_id) { id }
       end
     end
 
-    it 'adds rich_text_area' do
-      html
-      expect(form).to have_received(:rich_text_area).with(field)
+    it 'has a trix editor' do
+      render
+      expect(page).to have_css('trix-editor.rich_text_area')
     end
 
     context 'with size' do
-      let(:params) do
-        { form: form, field: field, size: size }
-      end
+      let(:params) { { form:, field:, size: } }
 
       context 'when the style is recognised' do
         let(:size) { :default }
 
-        it { expect(html).to have_css('div.forms-trix-component.default') }
+        before { render }
+
+        it { expect(page).to have_css('div.forms-trix-component.default') }
       end
 
       context 'when the style is unrecognised' do
         let(:size) { :notgood }
 
-        it { expect { html }.to raise_error(ArgumentError, 'Unknown size') }
+        it { expect { render }.to raise_error(ArgumentError, 'Unknown size') }
       end
     end
 
     context 'with controls' do
-      let(:params) do
-        { form: form, field: field, controls: controls }
-      end
+      let(:params) { { form:, field:, controls: } }
 
       context 'when the style is recognised' do
         let(:controls) { :default }
 
-        it { expect(html).to have_css('div.forms-trix-component.controls-default') }
+        before { render }
+
+        it { expect(page).to have_css('div.forms-trix-component.controls-default') }
       end
 
       context 'when the style is unrecognised' do
         let(:controls) { :notgood }
 
-        it { expect { html }.to raise_error(ArgumentError, 'Unknown controls options') }
+        it { expect { render }.to raise_error(ArgumentError, 'Unknown controls options') }
       end
     end
 
     context 'with button size' do
-      let(:params) do
-        { form: form, field: field, button_size: size }
-      end
+      let(:params) { { form:, field:, button_size: size } }
 
       context 'when the style is recognised' do
         let(:size) { :default }
 
-        it { expect(html).to have_css('div.forms-trix-component.buttons-default') }
+        before { render }
+
+        it { expect(page).to have_css('div.forms-trix-component.buttons-default') }
       end
 
       context 'when the style is unrecognised' do
         let(:size) { :notgood }
 
-        it { expect { html }.to raise_error(ArgumentError, 'Unknown button size') }
+        it { expect { render }.to raise_error(ArgumentError, 'Unknown button size') }
       end
     end
 
     context 'with charts' do
-      let(:params) do
-        { form: form, field: field, charts: [['Examples', :chart_identifier]] }
-      end
+      let(:params) { { form:, field:, charts: [['Examples', :chart_identifier]] } }
+
+      before { render }
 
       it 'adds data attribute' do
-        expect(html).to have_css('.forms-trix-component[data-chart-list]')
+        expect(page).to have_css('.forms-trix-component[data-chart-list]')
       end
     end
   end
