4586c2b6b3593b96cc0a0fdd27cd82c728ab9620
Leigh Dodds <leigh@ldodds.com>
2026-09-01 16:07:54 +0100

    Refactor

--- app/models/amr_data_feed_config.rb
+++ app/models/amr_data_feed_config.rb
@@ -116,9 +116,8 @@ class AmrDataFeedConfig < ApplicationRecord # rubocop:disable Metrics/ClassLengt
     safe_parse_date(date_string, date_format)
   rescue ArgumentError
     begin
-      if date_string.match(TWO_DIGIT_YEAR) # Avoid ruby default of assuming year/month/day order
-        format = date_string.include?('/') ? '%d/%m/%y' : '%d-%m-%y'
-        Date.strptime(date_string, format)
+      if date_string.match?(TWO_DIGIT_YEAR) # Avoid ruby default of assuming year/month/day order
+        normalise_and_parse_two_digit_year(date_string)
       else
         Date.parse(date_string)
       end
@@ -129,16 +128,22 @@ class AmrDataFeedConfig < ApplicationRecord # rubocop:disable Metrics/ClassLengt
 
   private_class_method def self.safe_parse_date(date_string, date_format)
     # Avoids this: Date.strptime('12-05-2022', "%Y-%m-%d") => Fri, 20 May 0012
-    return Date.parse(date_string) if YEAR_MONTH_DAY_FORMATS.include?(date_format) && date_string.match(FOUR_DIGIT_YEAR)
+    if YEAR_MONTH_DAY_FORMATS.include?(date_format) && date_string.match?(FOUR_DIGIT_YEAR)
+      return Date.parse(date_string)
+    end
 
     # Avoids this: Date.strptime('12-05-22', "%d-%m-%Y") => Fri, 20 May 0012
     # And this: Date.parse('12-05-22') => Tue, 22 May 2012
-    if DAY_MONTH_YEAR_FORMATS.include?(date_format) && date_string.match(TWO_DIGIT_YEAR)
-      format = date_string.include?('/') ? '%d/%m/%y' : '%d-%m-%y'
-      return Date.strptime(date_string, format)
+    if DAY_MONTH_YEAR_FORMATS.include?(date_format) && date_string.match?(TWO_DIGIT_YEAR)
+      return normalise_and_parse_two_digit_year(date_string)
     end
 
     Date.strptime(date_string, date_format)
+  end
+
+  private_class_method def self.normalise_and_parse_two_digit_year(date_string)
+    format = date_string.include?('/') ? '%d/%m/%y' : '%d-%m-%y'
+    Date.strptime(date_string, format)
   end
 
   def latest_reading_date
