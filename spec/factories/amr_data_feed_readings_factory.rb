0521fda736c1ee5e079feefa511f530729dc2421
Leigh Dodds <leigh@ldodds.com>
2026-09-04 14:43:06 +0100

    Change reports to use parsed_date

--- spec/factories/amr_data_feed_readings_factory.rb
+++ spec/factories/amr_data_feed_readings_factory.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :amr_data_feed_reading do
     reading_date  { Date.yesterday.strftime('%b %e %Y %I:%M%p') }
+    parsed_date { Date.yesterday }
     readings      { Array.new(48, rand) }
     association :meter, factory: :gas_meter
     mpan_mprn { Random.new.rand(240000000000000)}
