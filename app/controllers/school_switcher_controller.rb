9b0c2d843f08eca7c41639bf95a7cc6a962e9ca3
Tristan H <878258+tbhi@users.noreply.github.com>
2026-09-08 10:19:56 +0000

    Use school switcher on choose school page

--- app/controllers/school_switcher_controller.rb
+++ app/controllers/school_switcher_controller.rb
@@ -3,6 +3,6 @@ class SchoolSwitcherController < ApplicationController
     current_user.add_cluster_school(current_user.school)
     school = School.find(params[:school_id])
     current_user.update(school: school)
-    redirect_to school_path(school), notice: "Switched to #{school.name}"
+    redirect_to school_path(school) + params[:path].to_s, notice: "Switched to #{school.name}"
   end
 end
