c960ac034d1b0f68133ddc317406d03ffd650d31
Tristan H <878258+tbhi@users.noreply.github.com>
2026-09-10 15:23:56 +0000

    more

--- app/components/forms/email_report_button_component.rb
+++ app/components/forms/email_report_button_component.rb
@@ -2,20 +2,16 @@
 
 module Forms
   class EmailReportButtonComponent < ApplicationComponent
-    def initialize(path, label, button_class: 'btn btn-sm', modal_title: nil, **_kwargs)
+    def initialize(path, label, button_class: 'btn btn-sm', **kwargs)
       super
       @path = path
       @label = label
       @button_class = button_class
-      @modal_title = modal_title
+      @modal_title = kwargs[:modal_title]
       id = label.parameterize(separator: '_')
       @modal_id = "email_report_button_modal-#{id}"
-      @button_attributes = { 'data-toggle': :modal, 'data-bs-toggle': :modal,
-                             'data-target': "##{@modal_id}", 'data-bs-target': "##{@modal_id}" }
-      @button_attributes[:type] = :button # if content?
-      # debugger
       @label_id = "email_report_button_label-#{id}"
-      # @button_attributes[:type] = ''
+      @school_group = kwargs[:school_group]
     end
   end
 end
