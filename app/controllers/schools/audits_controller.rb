7863e623a93146a426495c021dd7084fdd73b8c5
Deb B <deb.bassett@energysparks.uk>
2026-09-11 15:48:02 +0100

    Remove todos_old feature flag (#5653)

--- app/controllers/schools/audits_controller.rb
+++ app/controllers/schools/audits_controller.rb
@@ -13,11 +13,9 @@ module Schools
       end
     end
 
-    def show
-    end
+    def show; end
 
-    def edit
-    end
+    def edit; end
 
     def create
       if Audits::AuditService.new(@school).process(@audit)
@@ -49,18 +47,8 @@ module Schools
 
     def audit_params
       params.require(:audit).permit(:school_id, :title, :description, :file, :published, :involved_pupils,
-          audit_activity_types_attributes: audit_activity_types_attributes, # remove when todos feature flag removed
-          audit_intervention_types_attributes: audit_intervention_types_attributes, # remove when todos feature flag removed
           activity_type_todos_attributes: todos_attributes,
           intervention_type_todos_attributes: todos_attributes)
-    end
-
-    def audit_activity_types_attributes
-      [:id, :activity_type_id, :activity_type, :notes, :_destroy]
-    end
-
-    def audit_intervention_types_attributes
-      [:id, :intervention_type_id, :intervention_type, :notes, :_destroy]
     end
 
     def todos_attributes
---
+++ lib/tasks/deployment/20260910133147_remove_todos_old_feature.rake
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+namespace :after_party do
+  desc 'Deployment task: remove_todos_old_feature'
+  task remove_todos_old_feature: :environment do
+    puts "Running deploy task 'remove_todos_old_feature'"
+
+    Flipper.remove(:todos_old)
+
+    # Update task as completed.  If you remove the line below, the task will
+    # run with every deploy (or every time you call after_party:run).
+    AfterParty::TaskRecord
+      .create version: AfterParty::TaskRecorder.new(__FILE__).timestamp
+  end
+end
