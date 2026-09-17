d8299f502b5a3b7876d0f488c2a3eedfe940bfcd
Deb Bassett <deb@urbanwide.com>
2026-09-17 15:30:53 +0100

    More

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,6 +3,7 @@
 class ActivitiesController < ApplicationController
   before_action :enable_bootstrap5
   before_action :enable_feature
+  before_action :set_breadcrumbs
 
   # load_and_authorize_resource
   skip_before_action :authenticate_user!, only: [:index]
@@ -14,5 +15,13 @@ class ActivitiesController < ApplicationController
   def enable_feature
     redirect_to root_path unless Flipper.enabled?(:activities_index, current_user)
     # Implementation for enabling feature
+  end
+
+  def set_breadcrumbs
+    @breadcrumbs = [
+      { name: t('common.labels.pupil_activities'), href: activities_path },
+      # { name: 'Student activities', href: activities_path },
+      { name: 'Discover' }
+    ].compact
   end
 end
